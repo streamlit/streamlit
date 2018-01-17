84144a272bf950e093ea0e245ee4cc36b71b3654
Adrien Treuille <adrien.g.treuille@gmail.com>
2018-01-17 12:27:01 -0800

    Everything is now working properly when no data has been received.

--- web-client/src/WebClient.js
+++ web-client/src/WebClient.js
@@ -32,11 +32,16 @@ class WebClient extends PureComponent {
   constructor(props) {
     super(props);
 
+    // Initially the state reflects that no data has been received.
     this.state = {
-      elements: List()
+      elements: List([{
+        type: 'div',
+        div: {
+          text: 'No data received.',
+          classes: 'alert alert-info',
+        }
+      }]),
     };
-
-    console.log('Created an immutable list', this.state)
 
     // Bind event handlers.
     this.handleReconnect = this.handleReconnect.bind(this);
@@ -105,30 +110,7 @@ class WebClient extends PureComponent {
           </NavItem>
         </Navbar>
         <Container className="printf-container">
-          <Row>
-            <h3>Dumb Client</h3>
-          </Row>
-
           { this.renderElements() }
-
-          {/* <Row>
-            <samp>
-              This client is printing out all messages to the console.
-            </samp>
-          </Row>
-          <Row>
-            <samp>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce ac sollicitudin ex. Sed pretium tincidunt justo, non blandit augue condimentum in. Integer ullamcorper sem urna. Aliquam magna massa, volutpat eu tincidunt ut, aliquam a arcu. Duis pulvinar pharetra malesuada. Nam bibendum maximus faucibus. Etiam eu nisi mattis, facilisis enim at, dapibus magna. Phasellus a nulla a tortor ultrices auctor.
-
-Nulla molestie ante lorem, nec tempor dolor vulputate quis. Suspendisse euismod nulla leo, et finibus erat aliquet et. Nulla augue ligula, vehicula ac odio id, fermentum dictum erat. Quisque lobortis aliquam hendrerit. Aliquam et iaculis nulla. Donec blandit finibus est, nec bibendum magna commodo sit amet. Curabitur ultrices eu ligula non luctus. Phasellus molestie bibendum tortor sed malesuada. Donec accumsan mi in tempor vestibulum. In vitae quam vel ex ultrices consequat nec nec ipsum. Etiam venenatis dapibus feugiat. Praesent rhoncus enim eget quam accumsan, ut venenatis sem tristique.
-
-Sed porttitor magna ac est luctus, ac commodo justo facilisis. Proin id scelerisque odio. Donec vulputate magna sed dolor vestibulum, eget luctus lorem vestibulum. In condimentum mi a ligula posuere, consectetur auctor sapien mollis. Quisque dignissim vel turpis vitae euismod. Morbi venenatis ultrices metus, vel fermentum nisl placerat eu. Aliquam lobortis felis risus, eget pharetra nibh scelerisque sed. Sed elementum ex magna, ut sagittis leo eleifend vel. Proin id aliquam neque. Aenean arcu ex, finibus eu sodales et, sagittis ac velit. Nam euismod dignissim neque, at consequat nisl venenatis id. Aliquam pretium semper justo, id posuere est bibendum a.
-
-Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Donec vulputate auctor tortor, et faucibus odio finibus vitae. Morbi ut iaculis quam. Vivamus dictum libero in ligula efficitur, hendrerit dignissim lorem mattis. Ut pharetra ante a odio auctor viverra. Aliquam ac tellus leo. In pulvinar fringilla commodo. Sed aliquet varius quam id auctor. Donec dapibus consectetur mi vel cursus.
-
-Sed auctor, arcu sit amet hendrerit sodales, odio leo aliquet nisl, quis accumsan eros urna eu erat. Sed vitae augue velit. Vestibulum volutpat consectetur tempus. Nam vitae imperdiet sapien, vitae ultricies dui. Sed consequat augue tellus, ac tristique lacus tincidunt ac. Sed accumsan, ex sit amet tempus elementum, quam augue auctor nibh, quis ultrices urna nulla sit amet magna. Mauris venenatis laoreet suscipit. Duis libero enim, feugiat id congue id, consequat vitae felis.
-            </samp>
-          </Row> */}
         </Container>
 
       </div>
