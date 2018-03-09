143c26c8287c2ac55f3a8cc2983ae45f618bcbe8
Adrien Treuille <adrien.g.treuille@gmail.com>
2018-03-08 16:20:34 -0800

    Finding that websocket polling is difficult. Moving to a local server model.

--- shared/client/src/PersistentWebsocket.js
+++ shared/client/src/PersistentWebsocket.js
@@ -7,7 +7,7 @@ import React, { PureComponent } from 'react';
 import { UncontrolledTooltip } from 'reactstrap';
 
 const NORMAL_CLOSURE = 1000;
-const RECONNECT_TIMEOUT = 500.0;
+const RECONNECT_TIMEOUT = 200.0;
 const DISCONNECTED_STATE = 'disconnected';
 const CONNECTED_STATE = 'connected';
 const ERROR_STATE = 'error'
