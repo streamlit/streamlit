26b8e58826ae4a450bcc768ced8f59329587fbf2
Adrien Treuille <adrien.g.treuille@gmail.com>
2018-03-12 18:47:21 -0700

    Added config.yaml.

--- examples/new.py
+++ examples/new.py
@@ -10,11 +10,6 @@ sys.path.append('local/server')
 from streamlet import Notebook, Chart
 
 with Notebook() as print:
-    print('Hello world.')
-    # print('Starting the loop')
-    # for i in range(100):
-    #     print('i', i)
-    # print('Finished the loop')
-
-    # an_array = np.random.randn(200, 200)
-    # print(an_array)
+    print('hello world')
+    an_array = np.random.randn(200, 200)
+    print(an_array)
