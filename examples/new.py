115f469f9642d687a2450cd919304ad08fab4aa8
Thiago Teixeira <thiagot@gmail.com>
2018-03-20 00:43:28 -0700

    Fix new.py since it got modified when merging

--- examples/new.py
+++ examples/new.py
@@ -9,18 +9,7 @@ import random
 from streamlit import Notebook, Chart
 
 with Notebook() as print:
-<<<<<<< HEAD
     print('hello world')
     an_array = np.random.randn(200, 200)
     print(an_array)
     print(an_array.shape)
-=======
-    print('Hello world.')
-    # print('Starting the loop')
-    # for i in range(100):
-    #     print('i', i)
-    # print('Finished the loop')
-    #
-    # an_array = np.random.randn(200, 200)
-    # print(an_array)
->>>>>>> About to remove switchboard from Notebook.
