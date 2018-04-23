ffc0d49f00db3c3de06e6ea3ca62053e1c962d7d
Thiago Teixeira <thiagot@gmail.com>
2018-04-22 23:42:00 -0700

    Fix bug where indices were not being shown for bar charts. The underlying issue was that Chart.js did not support rangeIndex, but that was due to a single word missing in the supported_indices array :)

--- shared/client/src/elements/Chart.js
+++ shared/client/src/elements/Chart.js
@@ -81,8 +81,9 @@ const INDEX_COLUMN_DESIGNATOR = '::index';
 
 /** Types of dataframe-indices that are supported as x axes. */
 const SUPPORTED_INDEX_TYPES = new Set([
-  'int_64Index', 'uint_64Index', 'float_64Index', 'datetimeIndex', 'timedeltaIndex',
-  // TODO(tvst): 'range_index', etc.
+  'int_64Index', 'uint_64Index', 'float_64Index', 'datetimeIndex',
+  'timedeltaIndex', 'rangeIndex',
+  // TODO(tvst): Support Other index types
 ]);
 
 class Chart extends PureComponent {
