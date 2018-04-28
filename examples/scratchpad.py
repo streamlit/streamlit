aa520eae88c866c516cfb52ee295ab3c52d5e28d
Adrien Treuille <adrien.g.treuille@gmail.com>
2018-04-28 11:34:04 -0700

    Looking for interesting taxi data.

--- examples/scratchpad.py
+++ examples/scratchpad.py
@@ -1,34 +1,26 @@
 #!./streamlit_run
 
 from streamlit import io, cache
-
-import json
 import numpy as np
-import pandas as pd
-import string
-import sys
 import time
+import pandas as pd
+# import shapefile
 
-@cache
-def open_json(from_line):
-    with open('swift-gnss-20180405-180845.sbp.expanded.json') as file:
-        return [json.loads(line) for line in file.readlines()]
 
-timings = pd.DataFrame.from_records([
-    {'method': 'no_cache', 'time': 123.0},
-    {'method': 'cache',    'time': 345.0},
-]).set_index('method')
+# Uber data_key
+data = pd.read_csv("labels_crowdai.csv")
+io.write(data)
 
-io.write(timings)
-io.bar_chart(timings)
+### NYC green taxi data
 
-# timings.append(
-# io.write(timings)
-
-# io.json(json_data)
-# data = pd.DataFrame()
-# data['labels'] = list(string.ascii_lowercase[:10])
-# data['frequencies'] = list(range(10))
-# data.set_index('labels', inplace=True)
-# io.write(data)
-# io.bar_chart(data)
+# @cache
+# def load_taxi_data(n_rows):
+#     return pd.read_csv('taxi_data/2017_Green_Taxi_Trip_Data.csv')[:n_rows]
+#
+# taxi_data = load_taxi_data(10000)
+# io.write('read the file', len(taxi_data))
+# io.write(taxi_data)
+# counts, distances = np.histogram(taxi_data['trip_distance'], bins=10, range=(0,10.0))
+# counts = pd.DataFrame.from_dict({'counts': counts, 'distances': distances[:-1]})
+# io.bar_chart(counts.set_index('distances'), height=300)
+# io.help(np.histogram)
