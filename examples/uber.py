d44b37cfd6ad77916a65579baaf60e68be169c38
Adrien Treuille <adrien.g.treuille@gmail.com>
2018-05-01 17:11:40 -0700

    Added uber.py

---
+++ examples/uber.py
@@ -0,0 +1,48 @@
+#!./streamlit_run
+
+from streamlit import io, cache
+import pandas as pd
+import numpy as np
+
+io.title('Uber Example')
+
+DATE_TIME = 'Date/Time'
+
+@cache
+def load_data(nrows):
+    data = pd.read_csv('uber-raw-data-sep14.csv', nrows=nrows)
+    data[DATE_TIME] = pd.to_datetime(data[DATE_TIME])
+    return data
+
+nrows = 100000
+io.write('Loaded', nrows, 'rows.')
+data = load_data(nrows)
+
+io.header('Raw Data')
+data['hour'] = data['Date/Time'].dt.hour
+data['day'] = data['Date/Time'].dt.dayofweek
+io.write('About to write data')
+io.write(data)
+io.write('Wrote raw data')
+
+raise RuntimeError('Nothing.')
+
+# io.binned_scatter_chart(data[['hour', 'day']].set_index('hour'))
+# io.header('Pick Up Times')
+# freq, hours = np.histogram(data[DATE_TIME].dt.hour, bins=24, range=(0,24))
+# io.write(freq)
+# io.bar_chart(freq)
+#
+# io.geo_chart(data[['Lat', 'Lon']].set_index('Lat'))
+
+
+
+
+# io.write(hours)
+
+
+# freq, lats = np.histogram(data['Lat'], bins=20, range=(40.6, 40.8))
+# hist_data = pd.DataFrame({'freq': freq, 'lats': lats[:-1]}).set_index('lats')
+# io.bar_chart(hist_data, height=300)
+# # io.bar_chart()
+# io.help(np.histogram)
