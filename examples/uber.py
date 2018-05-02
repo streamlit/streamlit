6872ff8e29052417b43b77258f5e2431504e983b
Adrien Treuille <adrien.g.treuille@gmail.com>
2018-05-02 11:08:37 -0700

    uber.py

--- examples/uber.py
+++ examples/uber.py
@@ -18,31 +18,31 @@ nrows = 100000
 io.write('Loaded', nrows, 'rows.')
 data = load_data(nrows)
 
-io.header('Raw Data')
-data['hour'] = data['Date/Time'].dt.hour
-data['day'] = data['Date/Time'].dt.dayofweek
-io.write('About to write data')
-io.write(data)
-io.write('Wrote raw data')
-
-raise RuntimeError('Nothing.')
+with io.echo():
+    io.header('Raw Data')
+    data['hour'] = data['Date/Time'].dt.hour
+    data['day'] = data['Date/Time'].dt.dayofweek
+    io.write('About to write data')
+    io.write(data)
+    io.write('Wrote raw data')
 
 # io.binned_scatter_chart(data[['hour', 'day']].set_index('hour'))
+# # io.write(data[['hour', 'day']].set_index('hour'))
 # io.header('Pick Up Times')
 # freq, hours = np.histogram(data[DATE_TIME].dt.hour, bins=24, range=(0,24))
 # io.write(freq)
 # io.bar_chart(freq)
 #
 # io.geo_chart(data[['Lat', 'Lon']].set_index('Lat'))
-
-
-
-
-# io.write(hours)
-
-
-# freq, lats = np.histogram(data['Lat'], bins=20, range=(40.6, 40.8))
-# hist_data = pd.DataFrame({'freq': freq, 'lats': lats[:-1]}).set_index('lats')
-# io.bar_chart(hist_data, height=300)
-# # io.bar_chart()
-# io.help(np.histogram)
+#
+#
+#
+#
+# # io.write(hours)
+#
+#
+# # freq, lats = np.histogram(data['Lat'], bins=20, range=(40.6, 40.8))
+# # hist_data = pd.DataFrame({'freq': freq, 'lats': lats[:-1]}).set_index('lats')
+# # io.bar_chart(hist_data, height=300)
+# # # io.bar_chart()
+# # io.help(np.histogram)
