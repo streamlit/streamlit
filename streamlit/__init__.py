d789604ba4a835e1667864cbd505efcd661bcebc
armando atienza <armando@playground.global>
2018-05-08 09:52:44 -0700

    Move python local and shared code to top level for simplification.

---
+++ streamlit/__init__.py
@@ -0,0 +1,4 @@
+# Import some files directly from this module
+from .local.Chart import *
+from .local.caching import cache
+from .local import io
--- streamlit/local/server/streamlit/__init__.py
+++
@@ -1,4 +0,0 @@
-# Import some files directly from this module
-from .local.Chart import *
-from .local.caching import cache
-from .local import io
