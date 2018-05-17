f75371351f67c6d236ce0ca63105135e218790c7
Adrien Treuille <adrien.g.treuille@gmail.com>
2018-05-16 18:04:13 -0700

    Switched to stateless functional components.

--- frontend/src/elements/Table.js
+++ frontend/src/elements/Table.js
@@ -56,8 +56,12 @@ class Table extends PureComponent {
           <div>
             <div>Real Table:</div>
             <ReactTable>
-              <thead> { htmlRows({df, header: true}) } </thead>
-              <tbody> { htmlRows({df, header: false}) } </tbody>
+              <thead>
+                <TableRows df={df} header={true}/>
+              </thead>
+              <tbody>
+                <TableRows df={df} header={false}/>
+              </tbody>
             </ReactTable>
           </div>
         </div>
@@ -126,9 +130,15 @@ class Table extends PureComponent {
  * df     - The dataFrame to display.
  * header - Whether to display the header.
  */
-function htmlRows({df, header}) {
-  const { headerRows, headerCols, dataRows, dataCols, cols, rows } =
-    dataFrameGetDimensions(df);
+function TableRows({df, header}) {
+  // const { headerRows, headerCols, dataRows, dataCols, cols, rows } =
+  //   dataFrameGetDimensions(df);
+  // let rows = [];
+  // const startRow = header ? 0 : headerRows;
+  // const endRow = header ? headerRows : rows;
+  // for (var rowIdx = startRow ; rowIdx < endRow ; rowIdx++) {
+  //
+  // }
   return <div>Printing Rows: {`${header}`}</div>;
 }
 
