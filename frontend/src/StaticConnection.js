8d55dd8d1e4469f84884ec8c85f381fd05c5721c
Adrien Treuille <adrien.g.treuille@gmail.com>
2018-05-22 11:26:57 -0700

    Starting to work on StaticConnection class.

---
+++ frontend/src/StaticConnection.js
@@ -0,0 +1,25 @@
+/**
+ * This class is the "brother" of ProtobufWebsocket. The class implements
+ * loading deltas over an HTTP connection (as opposed to with websockets).
+ * Like ProtobufWebsocket it also implements:
+ *
+ *   get_status() - returns information to display status in the GUI
+ *   connected_to_proxy() - always returns false
+ *   send_to_proxy() - raises an exception because there's no proxy connection
+ */
+
+
+/**
+* This class is the "brother" of ProtobufWebsocket. The class implements
+* loading deltas over an HTTP connection (as opposed to with websockets).
+* Like ProtobufWebsocket it also implements:
+*
+*   get_status() - returns information to display status in the GUI
+*   connected_to_proxy() - always returns false
+*   send_to_proxy() - raises an exception because there's no proxy connection
+*/
+class StaticConnection {
+
+};
+
+export default StaticConnection;
