7490ac315dcca1bcc4e6d66e6e6b9e94d0735f21
Adrien Treuille <adrien.g.treuille@gmail.com>
2018-07-04 12:46:32 -0700

    Got rid of vestigial code: streamlit_msg_iter

--- lib/streamlit/streamlit_msg_proto.py
+++ lib/streamlit/streamlit_msg_proto.py
@@ -25,26 +25,3 @@ def new_report_msg(report_id, ws):
     msg = protobuf.ForwardMsg()
     msg.new_report = str(report_id)
     yield ws.write_message(msg.SerializeToString(), binary=True)
-
-def streamlit_msg_iter(ws):
-    pass
-'''
-async def streamlit_msg_iter(ws):
-    """Takes a websocket and yields a series of DeltaLists."""
-    async for binary_msg in ws:
-        if binary_msg.type == aiohttp.WSMsgType.BINARY:
-            streamlit_msg = protobuf.ForwardMsg()
-            try:
-                streamlit_msg.ParseFromString(binary_msg.data)
-            except Exception as e:
-                print(f'Cannot parse binary message: {e}')
-                continue
-            yield streamlit_msg
-        elif binary_msg.type == aiohttp.WSMsgType.CLOSE:
-            ws.close()
-        elif binary_msg.type == aiohttp.aiohttp.WSMsgType.ERROR:
-            print('ws connection closed with exception %s' %
-                  ws.exception())
-        else:
-            print(f'Received incorrect message type {binary_msg.type.name}.')
-'''
