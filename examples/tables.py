66df2b6166e695cd48dbd74c142346021b4295c2
Thiago Teixeira <thiagot@gmail.com>
2018-07-04 15:17:14 -0700

    Make ultra-narrow dataframes not stretch to the entire container width.

---
+++ examples/tables.py
@@ -0,0 +1,15 @@
+"""The crypt of top secret undocumented Streamlit API calls."""
+
+import streamlit as st
+import numpy as np
+import pandas as pd
+from datetime import datetime
+
+st.title('Testing')
+
+for c in [1, 2, 5, 20]:
+    for r in [1, 2, 3, 4, 5, 6, 7, 8, 20]:
+        df = pd.DataFrame(np.random.randn(c, r), index=range(c),
+            columns=range(r))
+
+        st.write(df)
