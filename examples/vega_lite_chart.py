d7dcc814af4f87e884128af76d69339231c7a7fe
Thiago Teixeira <thiagot@gmail.com>
2018-08-10 02:20:24 +0100

    Add more Vega Lite examples.

--- examples/vega_lite_chart.py
+++ examples/vega_lite_chart.py
@@ -22,11 +22,18 @@ st.vega_lite.scatter_chart(df)
 
 st.header('Custom charts')
 
-df = pd.DataFrame([['A', 'B', 'C', 'D'], [28, 55, 43, 91]], index=['a', 'b']).T
+st.subheader('Override basic chart')
+
+df = pd.DataFrame({'a': np.random.randn(50), 'b': np.random.randn(50)})
+
+st.vega_lite.line_chart(df,
+    x_axis_title='My X title!')
 
 # -
 
 st.subheader('Bar chart')
+
+df = pd.DataFrame([['A', 'B', 'C', 'D'], [28, 55, 43, 91]], index=['a', 'b']).T
 
 st.write('This...')
 
@@ -63,6 +70,81 @@ st.vega_lite_chart(df, {
     }
   })
 
+# -
+
+st.subheader('Box plot')
+
+st.write(
+    'This example is straight from the [Vega Lite docs]'
+    '(https://vega.github.io/vega-lite/examples/interactive_query_widgets.html)'
+    )
+
+st.vega_lite_chart(None,
+    {
+      "$schema": "https://vega.github.io/schema/vega-lite/v2.json",
+      "description": "A vertical 2D box plot showing median, min, and max in the US population distribution of age groups in 2000.",
+      "data": {"url": "https://vega.github.io/vega-lite/data/population.json"},
+      "mark": {
+        "type": "boxplot",
+        "extent": "min-max"
+      },
+      "encoding": {
+        "x": {"field": "age","type": "ordinal"},
+        "y": {
+          "field": "people",
+          "type": "quantitative",
+          "axis": {"title": "population"}
+        }
+      }
+    })
+
+# -
+
+st.subheader('Query widgets')
+
+st.write(
+    'This example is straight from the [Vega Lite docs]'
+    '(https://vega.github.io/vega-lite/examples/interactive_query_widgets.html)'
+    )
+
+st.vega_lite_chart(None,
+    {
+      "$schema": "https://vega.github.io/schema/vega-lite/v2.json",
+      "description": "Drag the sliders to highlight points.",
+      "data": {"url": "https://vega.github.io/vega-lite/data/cars.json"},
+      "transform": [{"calculate": "year(datum.Year)", "as": "Year"}],
+      "layer": [{
+        "selection": {
+          "CylYr": {
+            "type": "single", "fields": ["Cylinders", "Year"],
+            "bind": {
+              "Cylinders": {"input": "range", "min": 3, "max": 8, "step": 1},
+              "Year": {"input": "range", "min": 1969, "max": 1981, "step": 1}
+            }
+          }
+        },
+        "mark": "circle",
+        "encoding": {
+          "x": {"field": "Horsepower", "type": "quantitative"},
+          "y": {"field": "Miles_per_Gallon", "type": "quantitative"},
+          "color": {
+            "condition": {"selection": "CylYr", "field": "Origin", "type": "nominal"},
+            "value": "grey"
+          }
+        }
+      }, {
+        "transform": [{"filter": {"selection": "CylYr"}}],
+        "mark": "circle",
+        "encoding": {
+          "x": {"field": "Horsepower", "type": "quantitative"},
+          "y": {"field": "Miles_per_Gallon", "type": "quantitative"},
+          "color": {"field": "Origin", "type": "nominal"},
+          "size": {"value": 100}
+        }
+      }]
+    })
+
+# -
 
 st.subheader('Geo chart (WIP)')
 
