c74fab292f27c320e892a98707ba51ab52039295
Thiago Teixeira <thiagot@gmail.com>
2018-09-18 13:42:02 -0700

    Fix compilation warnigns

--- frontend/src/MainMenu.js
+++ frontend/src/MainMenu.js
@@ -4,7 +4,6 @@
  */
 
 import React, {Component} from 'react';
-import ConnectionState from './ConnectionState';
 import {
   Dropdown,
   DropdownItem,
--- frontend/src/baseconsts.js
+++ frontend/src/baseconsts.js
@@ -6,4 +6,4 @@ export const PROXY_PORT = 8501;
 /**
  * True when in development mode.
  */
-export const IS_DEV_ENV = window.location.port != PROXY_PORT;
+export const IS_DEV_ENV = window.location.port !== PROXY_PORT;
