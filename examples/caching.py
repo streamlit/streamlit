6a086b22d0c0e840b8e1842ee62b900047f785aa
Thiago Teixeira <thiagot@gmail.com>
2018-11-01 18:55:46 -0700

    Clean up examples/caching.py and add test that you can turn caching back on

--- examples/caching.py
+++ examples/caching.py
@@ -24,32 +24,52 @@ check_if_cached()
 if scope['need_to_clear_cache']:
     st.warning('You must clear your cache before you run this script!')
 else:
-    st.subheader('Test 1')
+    st.subheader('Test that basic caching works')
+    before = scope['num_executions']
     v1 = my_func(1, 2, dont_care=10)
     v2 = my_func(1, 2, dont_care=10)
-    if v1 == v2 and scope['num_executions'] == 1:
+    after = scope['num_executions']
+    if after == before + 1:
         st.write('OK')
     else:
         st.write('Fail')
 
-    st.subheader('Test 2')
+    st.subheader('Test that when you change arguments it\'s a cache miss')
+    before = scope['num_executions']
     v = my_func(10, 2, dont_care=10)
-    if v != v1 and scope['num_executions'] == 2:
+    after = scope['num_executions']
+    if after == before + 1:
         st.write('OK')
     else:
         st.write('Fail')
 
-    st.subheader('Test 3')
+    st.subheader('Test that when you change **kwargs it\'s a cache miss')
+    before = scope['num_executions']
     v = my_func(10, 2, dont_care=100)
-    if v != v1 and scope['num_executions'] == 3:
+    after = scope['num_executions']
+    if after == before + 1:
         st.write('OK')
     else:
         st.write('Fail')
 
-    st.subheader('Test 4')
+    st.subheader('Test that you can turn off caching')
+    before = scope['num_executions']
     st.set_config(client_caching=False)
     v = my_func(1, 2, dont_care=10)
-    if v != v1 and scope['num_executions'] == 4:
+    after = scope['num_executions']
+    if after == before + 1:
         st.write('OK')
     else:
         st.write('Fail')
+
+    st.subheader('Test that you can turn on caching')
+    before = scope['num_executions']
+    st.set_config(client_caching=True)
+    v1 = my_func(1, 2, dont_care=10)
+    v2 = my_func(1, 2, dont_care=10)
+    after = scope['num_executions']
+    if after == before:
+        st.write('OK')
+    else:
+        st.write('Fail')
+
