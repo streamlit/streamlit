85fb8a31ca23def1b29eb0b19ee132012a9855a4
Thiago Teixeira <thiagot@gmail.com>
2018-11-14 10:06:47 -0800

    Update comment in examples/disable_client.py

--- examples/disable_client.py
+++ examples/disable_client.py
@@ -6,7 +6,7 @@ a.text('This should show up first')
 
 st.warning('IMPORTANT: Make sure you get a streamlit.config.Error in the terminal!')
 
-st.set_config({'client.enabled': False})  # This causes an exception.
+st.set_config({'client.enabled': False})
 
 b.text('This should not appear')
 st.write('This should not appear')
@@ -16,4 +16,4 @@ with st.echo():
     foo = 'Nothing matters in here!'
     st.write('Nothing here will appear')
 
-st.set_config({'client.enabled': True})
+st.set_config({'client.enabled': True})  # This causes an exception.
