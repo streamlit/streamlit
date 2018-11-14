3bcf27defde7add6e31d5a8028429b484bb3722e
Thiago Teixeira <thiagot@gmail.com>
2018-11-14 15:15:35 -0800

    Update example for set_config({'client.enable': ...})

--- examples/disable_client.py
+++ examples/disable_client.py
@@ -1,14 +1,23 @@
 import streamlit as st
 
-a = st.text('This should not appear')
-b = st.text('This should show up second')
-a.text('This should show up first')
+# DISABLED
+st.set_config({'client.enabled': False})
+
+st.text('This should not appear')
+
+# ENABLED
+st.set_config({'client.enabled': True})
 
 st.warning('IMPORTANT: Make sure you get a streamlit.config.Error in the terminal!')
 
+a = st.text('This will be overwritten')
+b = st.text('This will be overwritten too')
+a.text('This should show up first')
+
+# DISABLED
 st.set_config({'client.enabled': False})
 
-b.text('This should not appear')
+b.text('This overwrites b, but should not appear')
 st.write('This should not appear')
 st.line_chart([1, 2, 3, 4])
 
@@ -16,4 +25,8 @@ with st.echo():
     foo = 'Nothing matters in here!'
     st.write('Nothing here will appear')
 
+# ENABLED
 st.set_config({'client.enabled': True})  # This causes an exception.
+
+st.write('This should appear last')
+b.text('This should appear second')
