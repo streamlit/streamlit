ded87d001f821fb2272ff1340cbbfe51c002186c
Adrien Treuille <adrien.g.treuille@gmail.com>
2018-11-18 15:35:55 -0800

    Created giant test case testing simple and complex ConfigOptions as well as get_option() and set_option().

--- lib/tests/streamlit/config_test.py
+++ lib/tests/streamlit/config_test.py
@@ -97,3 +97,52 @@ class ConfigTest(unittest.TestCase):
         """
         with self.assertRaises(AssertionError):
             config._create_option('_test.snake_case')
+
+    def test_get_set_and_complex_config_options(self):
+        """Creates a pair of config options, one of which depends on another
+        and verifies that changing one changes the other.
+
+        This also implicitly tests simple and complex ConfigOptions as well as
+        get_option() and set_option().
+        """
+        # Set up both options.
+        DUMMY_VAL_1, DUMMY_VAL_2, DUMMY_VAL_3 = 'Steven', 'Vincent', 'Buscemi'
+        config._create_option('_test.independentOption',
+            description = 'This option can change at will',
+            default_val = DUMMY_VAL_1)
+        @config._create_option('_test.dependentOption')
+        def _test_dependent_option():
+            """This depends on the value of _test.independentOption."""
+            return config.get_option('_test.independentOption')
+
+        # Check that the default values are good.
+        self.assertEqual(config.get_option('_test.independentOption'),
+            DUMMY_VAL_1)
+        self.assertEqual(config.get_option('_test.dependentOption'),
+            DUMMY_VAL_1)
+        self.assertEqual(config.get_where_defined('_test.independentOption'),
+            ConfigOption.DEFAULT_DEFINITION)
+        self.assertEqual(config.get_where_defined('_test.dependentOption'),
+            ConfigOption.DEFAULT_DEFINITION)
+
+        # Override the independent option. Both update!
+        config.set_option('_test.independentOption', DUMMY_VAL_2)
+        self.assertEqual(config.get_option('_test.independentOption'),
+            DUMMY_VAL_2)
+        self.assertEqual(config.get_option('_test.dependentOption'),
+            DUMMY_VAL_2)
+        self.assertEqual(config.get_where_defined('_test.independentOption'),
+            config._USER_DEFINED)
+        self.assertEqual(config.get_where_defined('_test.dependentOption'),
+            ConfigOption.DEFAULT_DEFINITION)
+
+        # Override the dependent option. Only that updates!
+        config.set_option('_test.dependentOption', DUMMY_VAL_3)
+        self.assertEqual(config.get_option('_test.independentOption'),
+            DUMMY_VAL_2)
+        self.assertEqual(config.get_option('_test.dependentOption'),
+            DUMMY_VAL_3)
+        self.assertEqual(config.get_where_defined('_test.independentOption'),
+            config._USER_DEFINED)
+        self.assertEqual(config.get_where_defined('_test.dependentOption'),
+            config._USER_DEFINED)
