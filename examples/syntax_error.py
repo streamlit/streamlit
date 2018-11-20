4b310c82b9ac51ee101067ff6775ac7ff1246d5f
Thiago Teixeira <thiagot@gmail.com>
2018-11-20 11:43:25 -0800

    Tweak examples/syntax_error.py

--- examples/syntax_error.py
+++ examples/syntax_error.py
@@ -12,11 +12,11 @@ st.info('''
 st.write('(Some top text)')
 
 # Uncomment this as a block:
-# st.write('You should see an inline exception below:')
+#st.write('You should see an inline exception below:')
 #a = not_a_real_variable
 
 # Uncomment this as a block:
-# st.write('You should see a fullscreen exception:')
+#st.write('You should see a fullscreen exception:')
 #if True  # missing semicolon
 
 st.write('(Some bottom text)')
