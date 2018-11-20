e12b9b3c090259df2dfda0d359416ab079680927
Adrien Treuille <adrien.g.treuille@gmail.com>
2018-11-20 15:59:01 -0800

    Added examples/close_and_rerun.py to test the rerun issue.

---
+++ examples/close_and_rerun.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+st.header('Warning!!!')
+
+st.warning('You must rerun this script to test it.')
+
+st.write("""
+Please **close this tab and rerun this script** again quickly by running
+```
+python examples/close_and_rerun.py
+```
+Then, please **verify that a new broser tab opened.**
+""")
+
+st.balloons()
