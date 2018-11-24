934268bea85c85844b59db5d007410f93c6c4104
Adrien Treuille <adrien.g.treuille@gmail.com>
2018-11-23 20:40:45 -0800

    Moved examples/disable_client.py to use the orthodox config.set_option(..)

--- examples/disable_client.py
+++ examples/disable_client.py
@@ -1,19 +1,20 @@
 import streamlit as st
+from streamlit import config
 
 # DISABLED
-st.set_config({'client.displayEnabled': False})
+config.set_option('client.displayEnabled', False)
 
 st.text('This should not appear')
 
 # ENABLED
-st.set_config({'client.displayEnabled': True})
+config.set_option('client.displayEnabled', True)
 
 a = st.text('This will be overwritten')
 b = st.text('This will be overwritten too')
 a.text('This should show up first')
 
 # DISABLED
-st.set_config({'client.displayEnabled': False})
+config.set_option('client.displayEnabled', False)
 
 b.text('This overwrites b, but should not appear')
 st.write('This should not appear')
@@ -24,7 +25,7 @@ with st.echo():
     st.write('Nothing here will appear')
 
 # ENABLED
-st.set_config({'client.displayEnabled': True})
+config.set_option('client.displayEnabled', True)
 
 st.write('This should appear last')
 b.text('This should appear second')
