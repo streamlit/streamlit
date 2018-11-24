624f34f942725a8e04390dc4b75b1d6dcff5e603
Adrien Treuille <adrien.g.treuille@gmail.com>
2018-11-23 21:03:50 -0800

    Got the test case working, but it still always creates the connection.

--- examples/disable_client.py
+++ examples/disable_client.py
@@ -3,29 +3,32 @@ from streamlit import config
 
 # DISABLED
 config.set_option('client.displayEnabled', False)
+print('doing nothing')
 
 st.text('This should not appear')
 
-# ENABLED
-config.set_option('client.displayEnabled', True)
+print('doing nothing - part deux')
 
-a = st.text('This will be overwritten')
-b = st.text('This will be overwritten too')
-a.text('This should show up first')
-
-# DISABLED
-config.set_option('client.displayEnabled', False)
-
-b.text('This overwrites b, but should not appear')
-st.write('This should not appear')
-st.line_chart([1, 2, 3, 4])
-
-with st.echo():
-    foo = 'Nothing matters in here!'
-    st.write('Nothing here will appear')
-
-# ENABLED
-config.set_option('client.displayEnabled', True)
-
-st.write('This should appear last')
-b.text('This should appear second')
+# # ENABLED
+# config.set_option('client.displayEnabled', True)
+#
+# a = st.text('This will be overwritten')
+# b = st.text('This will be overwritten too')
+# a.text('This should show up first')
+#
+# # DISABLED
+# config.set_option('client.displayEnabled', False)
+#
+# b.text('This overwrites b, but should not appear')
+# st.write('This should not appear')
+# st.line_chart([1, 2, 3, 4])
+#
+# with st.echo():
+#     foo = 'Nothing matters in here!'
+#     st.write('Nothing here will appear')
+#
+# # ENABLED
+# config.set_option('client.displayEnabled', True)
+#
+# st.write('This should appear last')
+# b.text('This should appear second')
