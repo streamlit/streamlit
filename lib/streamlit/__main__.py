03327c7893315f5e60018a6fd530b6eb599d27fc
Adrien Treuille <adrien.g.treuille@gmail.com>
2018-11-25 15:51:46 -0800

    Added 2/3 compatibility header to __main__.py

--- lib/streamlit/__main__.py
+++ lib/streamlit/__main__.py
@@ -1,22 +1,29 @@
-# Copyright 2018 Streamlit Inc. All rights reserved.
+# -*- coding: future_fstrings -*-
 
 """This is a script which is run when the Streamlit package is executed."""
+
+# Python 2/3 compatibility
+from __future__ import print_function, division, unicode_literals, absolute_import
+from streamlit.compatibility import setup_2_3_shims
+setup_2_3_shims(globals())
 
 import click
 
 
 def print_usage(args):
-    """Print a help message."""
-    USAGE = """
-        Where [MODE] is one of:
-          clear_cache - Clear the memoization cache.
-          help        - Show help in browser.
-          kill_proxy  - Kill proxy.
-          usage       - Print this help message.
-          version     - Print the version number.
-    """
-    import textwrap
-    print("\n" + textwrap.dedent(USAGE).strip())
+    """Print this help message."""
+    # USAGE = """
+    #
+    #       clear_cache - Clear the memoization cache.
+    #       help        - Show help in browser.
+    #       kill_proxy  - Kill proxy.
+    #       usage       - Print this help message.
+    #       version     - Print the version number.
+    # """
+    # import textwrap
+    print("\nWhere [MODE] is one of:")
+    # for command, handler in COMMAND_HANDLERS.items():
+    #     print('%.13s xcommand, handler)
 
 
 def clear_cache(args):
@@ -26,7 +33,7 @@ def clear_cache(args):
 
 
 def help(args):
-    """Show help."""
+    """Show help in browser."""
     print('Showing help page in browser...')
     import streamlit.reference
     streamlit.reference.display_reference()
@@ -47,7 +54,7 @@ def run(args):
 
 
 def kill_proxy(*args):
-    """Kill Streamlit Proxy."""
+    """Kill the Streamlit proxy."""
     import psutil
     import getpass
 
@@ -65,18 +72,22 @@ def kill_proxy(*args):
 
 
 def version(*args):
-    """Print Stremalit's version."""
+    """Print Streamlit's version number."""
     import streamlit
     print('Streamlit v' + streamlit.__version__)
 
+def show_config(*args):
+    """Nothing."""
+    pass
 
 COMMAND_HANDLERS = {
-    'usage': print_usage,
     'clear_cache': clear_cache,
     'help': help,
-    'run': run,
     'kill_proxy': kill_proxy,
-    'version': version
+    'run': run,
+    'show_config': show_config,
+    'usage': print_usage,
+    'version': version,
 }
 
 
