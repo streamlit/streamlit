a7f236587d1f228ed3da59f300fdc2ac7995aa18
aaj-st <44782696+aaj-st@users.noreply.github.com>
2019-01-22 13:08:36 -0800

    Add adrien_tests to python unittest framework (#228)
    ```
    $ make pycoverage | grep -i compat
    tests/streamlit/compatibility_test.py::CompatibilityTest::test_builtin_types PASSED [  3%]
    tests/streamlit/compatibility_test.py::CompatibilityTest::test_print_function PASSED [  7%]
    streamlit/compatibility.py                      30      3    90%   28-30
    ```
    
    For #203

---
+++ lib/tests/streamlit/compatibility_test.py
@@ -0,0 +1,33 @@
+# -*- coding: future_fstrings -*-
+
+# Copyright 2018 Streamlit Inc. All rights reserved.
+
+# Python 2/3 compatibility
+from __future__ import print_function, division, unicode_literals, absolute_import
+from streamlit.compatibility import setup_2_3_shims, running_py3
+setup_2_3_shims(globals())
+
+
+import unittest
+
+class CompatibilityTest(unittest.TestCase):
+    def test_print_function(self):
+        if running_py3:
+            return
+        from io import BytesIO
+        string_buffer = BytesIO()
+        print(1, 2, file=string_buffer)
+        self.assertEqual('1 2\n', string_buffer.getvalue())
+
+    def test_builtin_types(self):
+        if running_py3:
+            return
+        import abc, itertools, future.types
+        self.assertEqual(range, future.types.newrange)
+        self.assertEqual(map, itertools.imap)
+        self.assertEqual(str, future.types.newstr)
+        self.assertEqual(dict, future.types.newdict)
+        self.assertEqual(object, future.types.newobject)
+        self.assertEqual(zip, itertools.izip)
+        self.assertEqual(int, future.types.newint)
+        self.assertEqual(open.__module__, '_io')
