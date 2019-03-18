streamlit/streamlit

commit 63dabea1ab4108b510603755f392f05f5fda9c06

Make st.title/header/subheader use Markdown under the hood (#274)
* Make st.title/header/subheader use Markdown under the hood. This make sure there's no difference in how we render those vs. Markdown titles.

* Fix tests.

frontend/src/StreamlitApp.css
@@ -46,7 +46,8 @@ h1, .h1 {
   font-family: 'IBM Plex Sans', sans-serif;
   font-weight: 700;
   font-size: 2.5rem;
-  margin: 2.5rem 0 0 0;
+  margin: 2.5rem 0 1.5rem 0;
+  line-height: 1em;
 }
 
 h1:first-child, .h1:first-child {
@@ -57,29 +58,41 @@ h2, .h2 {
   font-family: 'IBM Plex Sans', sans-serif;
   font-weight: 300;
   font-size: 2.0rem;
-  margin: 3rem 0 0 0;
+  margin: 2.5rem 0 1rem 0;
+  line-height: 1em;
 }
 
 h3, .h3 {
   font-family: 'IBM Plex Sans', sans-serif;
   font-weight: 500;
   font-size: 1.25rem;
-  margin: 0.25rem 0 0 0;
+  margin: 1.5rem 0 1rem 0;
+  line-height: 1em;
+}
+
+h4, .h4 {
+  font-family: 'IBM Plex Sans', sans-serif;
+  font-weight: 700;
+  font-size: 1.0rem;
+  margin: 1.5rem 0 1rem 0;
+  line-height: 1em;
 }
 
 h5, .h5 {
   font-family: 'IBM Plex Sans', sans-serif;
-  font-weight: 300;
-  font-size: 1.25rem;
-  margin: 0.25rem 0 0 0;
+  font-weight: 700;
+  font-size: 0.8rem;
+  text-transform: uppercase;
+  margin: 1.5rem 0 0.5rem 0;
+  line-height: 1em;
 }
 
 h6, .h6 {
   font-family: 'IBM Plex Sans', sans-serif;
   font-weight: 300;
-  font-size: 1rem;
-  text-transform: uppercase;
-  margin: 0;
+  font-size: 0.8rem;
+  margin: 1.5rem 0 0.5rem 0;
+  line-height: 1em;
 }
 
 header {
@@ -174,6 +187,10 @@ button:focus {
 .streamlit-container .element-container {
   display: inline-block;
   margin-bottom: 1rem;
+}
+
+.streamlit-container hr {
+  border-color: #F0F3F9;
 }
 
 .streamlit-container .footer {

frontend/src/elements/Text.css
@@ -12,7 +12,6 @@
 
 .streamlit-container .markdown-text-container {
   font-family: 'IBM Plex Sans', sans-serif;
-  /* font-family: 'IBM Plex Serif', serif; */
   margin-bottom: -1rem;
 }
 

frontend/src/elements/Text.js
@@ -34,7 +34,7 @@ class Text extends PureComponent {
       case TextProto.Format.MARKDOWN:
         return (
           <div className="markdown-text-container" style={{width}}>
-            <ReactMarkdown source={body} />
+            <ReactMarkdown source={body} escapeHtml={false} />
           </div>
         );
 
@@ -71,20 +71,6 @@ class Text extends PureComponent {
               name={false}
               style={{font: ""}}  // Unset so we can style via a CSS file.
             />
-          </div>
-        );
-
-      case TextProto.Format.TITLE:
-      case TextProto.Format.HEADER:
-      case TextProto.Format.SUB_HEADER:
-        const className = {
-          [TextProto.Format.TITLE]: 'h1',
-          [TextProto.Format.HEADER]: 'h2',
-          [TextProto.Format.SUB_HEADER]: 'h3',
-        }[format]
-        return (
-          <div className={className} style={{width}}>
-            {body}
           </div>
         );
 

lib/streamlit/DeltaGenerator.py
@@ -162,45 +162,45 @@ class DeltaGenerator(object):
 
     @_export
     @_create_element
-    def title(self, element, string):
-        """Display string in title formatting.
+    def title(self, element, body):
+        """Display text in title formatting.
 
         Parameters
         ----------
-        string : str
-            The string to display.
+        body : str
+            The text to display.
 
         """
-        element.text.body = str(string)
-        element.text.format = protobuf.Text.TITLE
+        element.text.body = '# %s' % textwrap.dedent(body).strip()
+        element.text.format = protobuf.Text.MARKDOWN
 
     @_export
     @_create_element
-    def header(self, element, string):
-        """Display string in header formatting.
+    def header(self, element, body):
+        """Display text in header formatting.
 
         Parameters
         ----------
-        string : str
-            The string to display.
+        body : str
+            The text to display.
 
         """
-        element.text.body = str(string)
-        element.text.format = protobuf.Text.HEADER
+        element.text.body = '## %s' % textwrap.dedent(body).strip()
+        element.text.format = protobuf.Text.MARKDOWN
 
     @_export
     @_create_element
-    def subheader(self, element, string):
-        """Display string in subheader formatting.
+    def subheader(self, element, body):
+        """Display text in subheader formatting.
 
         Parameters
         ----------
-        string : str
-            The string to display.
+        body : str
+            The text to display.
 
         """
-        element.text.body = str(string)
-        element.text.format = protobuf.Text.SUB_HEADER
+        element.text.body = '### %s' % textwrap.dedent(body).strip()
+        element.text.format = protobuf.Text.MARKDOWN
 
     @_export
     @_create_element
@@ -364,7 +364,7 @@ class DeltaGenerator(object):
 
     @_export
     def dataframe(self, df):
-        """Display a dataframe.
+        """Display a dataframe as an interactive table.
 
         Parameters
         ----------

lib/streamlit/__init__.py
@@ -95,7 +95,7 @@ def write(*args):
         - write(data_frame) : Displays the DataFrame as a table.
         - write(error)      : Prints an exception specially.
         - write(func)       : Displays information about a function.
-        - write(module)     : Displays infomration about the module.
+        - write(module)     : Displays information about the module.
         - write(obj)        : The default is to print str(obj).
 
     Examples

lib/tests/streamlit/delta_generator_test.py
@@ -84,10 +84,7 @@ class DeltaGeneratorTextTest(unittest.TestCase):
     def test_generic_text(self):
         """Test protobuf.Text generic str(body) stuff."""
         test_data = {
-            'title': protobuf.Text.TITLE,
             'text': protobuf.Text.PLAIN,
-            'header': protobuf.Text.HEADER,
-            'subheader': protobuf.Text.SUB_HEADER,
             'error': protobuf.Text.ERROR,
             'warning': protobuf.Text.WARNING,
             'info': protobuf.Text.INFO,

protobuf/Text.proto
@@ -17,15 +17,6 @@ message Text {
     // A JSON object. Stored as a string.
     JSON = 2;
 
-    // Corresponds to the <h1> tag.
-    TITLE = 3;
-
-    // Corresponds to the <h2> tag.
-    HEADER = 4;
-
-    // Corresponds to the <h3> tag.
-    SUB_HEADER = 5;
-
     // Shows an error message.
     ERROR = 6;
 

commit 8632880e64eb61af4450e1d51bafc6108ea74915

Fix CSS regressions in header gradient, wide-mode header background, dialog titles (#474)
* Update robots.txt using Google's robot tested tool.

* Fix regression in dialog titles, introduced in 63dabea

* Fix regression in the header gradient CSS, introduced in 10bc978

* Fix regression in wide mode header background.

* Make `streamlit hello` progress bar reach end.

* Move "About" to bottom of main menu, to match spec at #362

frontend/src/MainMenu.jsx
@@ -88,13 +88,13 @@ class MainMenu extends Component {
           <DropdownItem divider/>
 
           <DropdownItem
-              onClick={() => this.props.aboutCallback()}>
-            About
+              onClick={() => window.open(ONLINE_DOCS_URL, '_blank')}>
+            Documentation
           </DropdownItem>
 
           <DropdownItem
-              onClick={() => window.open(ONLINE_DOCS_URL, '_blank')}>
-            Documentation
+              onClick={() => this.props.aboutCallback()}>
+            About
           </DropdownItem>
 
         </DropdownMenu>

frontend/src/SettingsDialog.jsx
@@ -54,7 +54,7 @@ class SettingsDialog extends PureComponent {
                 onChange={this.handleCheckboxChange}
                 />
             {" "}
-            Show reports in wide mode
+            Show report in wide mode
           </label>
         </ModalBody>
 

frontend/src/StreamlitApp.css
@@ -96,11 +96,6 @@ h6, .h6 {
 }
 
 header {
-  border-top: 2px solid #FFFD80;
-  border-image: linear-gradient(90deg, #F63366, #FFFD80) 1;
-  border-bottom: 0;
-  border-left: 0;
-  border-right: 0;
   position: fixed;
   top: 0;
   left: 0;
@@ -113,8 +108,17 @@ header {
   align-items: center;
 }
 
-.wide header {
+.streamlit-wide header {
   background-image: linear-gradient(180deg, #ffffff 25%, rgba(255, 255, 255, 0.5) 75%, transparent);
+}
+
+header .decoration {
+  position: absolute;
+  top: 0;
+  right: 0;
+  left: 0;
+  height: 2px;
+  background-image: linear-gradient(90deg, #F63366, #FFFD80);
 }
 
 #brand {
@@ -229,7 +233,7 @@ button:focus {
 
 @media (max-width: 768px) {
   header,
-  .wide header {
+  .streamlit-wide header {
     position: absolute;
     background: unset;
   }

frontend/src/StreamlitApp.js
@@ -393,6 +393,7 @@ class StreamlitApp extends PureComponent {
     return (
       <div className={outerDivClass}>
         <header>
+          <div className="decoration"></div>
           <div id="brand">
             <a href="//streamlit.io">Streamlit</a>
           </div>

frontend/src/StreamlitDialog.css
@@ -24,3 +24,12 @@
   font-family: 'IBM Plex Mono', monospace;
   font-size: 0.8rem;
 }
+
+h5.modal-title {
+  font-family: IBM Plex Sans,sans-serif;
+  font-size: 1.25rem;
+  margin: 0;
+  font-weight: 300;
+  line-height: 1.5;
+  text-transform: none;
+}

lib/streamlit/hello.py
@@ -31,7 +31,7 @@ def run():
     status_text = st.empty()
     chart = st.line_chart(np.random.randn(10, 1))
 
-    for i in range(100):
+    for i in range(1, 101):
         # Update progress bar.
         progress_bar.progress(i)
 
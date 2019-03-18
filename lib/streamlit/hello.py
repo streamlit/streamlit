Fix CSS regressions in header gradient, wide-mode header background, dialog titles (#474)
* Update robots.txt using Google's robot tested tool.

* Fix regression in dialog titles, introduced in 63dabea

* Fix regression in the header gradient CSS, introduced in 10bc978

* Fix regression in wide mode header background.

* Make `streamlit hello` progress bar reach end.

* Move "About" to bottom of main menu, to match spec at #362

lib/streamlit/hello.py
@@ -31,7 +31,7 @@ def run():
     status_text = st.empty()
     chart = st.line_chart(np.random.randn(10, 1))
 
-    for i in range(100):
+    for i in range(1, 101):
         # Update progress bar.
         progress_bar.progress(i)
 