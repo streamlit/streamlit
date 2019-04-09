fcd3999330f40919fd3c596e52a5ba8ef729327a
Tim Conkling <tconkling@gmail.com>
2019-04-09 14:25:47 -0700

    Fix status widget display (#550)
    Fixes https://github.com/streamlit/streamlit/issues/365
    
    (Also removes `PersistentWebsocket`, which is unused and seems to be a previous version of `ConnectionStatus`)

---
+++ frontend/src/ConnectionState.ts
@@ -0,0 +1,14 @@
+/**
+ * @license
+ * Copyright 2018 Streamlit Inc. All rights reserved.
+ */
+
+export enum ConnectionState {
+  INITIAL = 'initial',
+  CONNECTED = 'connected',
+  DISCONNECTED = 'disconnected',
+  ERROR = 'error',
+  STATIC = 'static',
+}
+
+export default ConnectionState;
