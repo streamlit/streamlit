streamlit/streamlit

commit d0f891b59ee8764d916a277afb0ca32964114b97

Plotly support! (#567)
* version 0.32.0

* Move element-marshalling files into the elements/ folder

* Plotly support excluding tests

* st.write support for Plotly

* Docs for Plotly

* Add support for Matplotlib figures in Plotly charts

* Add plotly to st.write's docstring

* Add a few unit tests for Plotly

* Add st.plotly tests

* Lint

* Add Plotly to Pipfile

* Update pipfiles for circleci

* Improved DeltaGenerator plotly docstring

* Removed compat stuff from plotly_chart.py and linted

* Better argument parsing for plotly_chart.marshall

* Fix plotly_chart tests.

* Try to fix plotly tests on CircleCI

* Add examples/plotly_example.py

frontend/src/StreamlitApp.js
@@ -18,21 +18,6 @@ import {
 import { fromJS } from 'immutable';
 import url from 'url';
 
-// Display Elements
-import Audio from './elements/Audio';
-import Balloons from './elements/Balloons';
-import Chart from './elements/Chart';
-import DataFrame from './elements/DataFrame';
-import DocString from './elements/DocString';
-import ExceptionElement from './elements/ExceptionElement';
-import ImageList from './elements/ImageList';
-import Map from './elements/Map';
-import DeckGlChart from './elements/DeckGlChart';
-import Table from './elements/Table';
-import { Text } from './elements/Text';
-import VegaLiteChart from './elements/VegaLiteChart';
-import Video from './elements/Video';
-
 // Other local imports.
 import LoginBox from './LoginBox';
 import MainMenu from './MainMenu';
@@ -43,6 +28,14 @@ import { ConnectionState } from './ConnectionState';
 import { ReportRunState } from './ReportRunState';
 import { StatusWidget } from './StatusWidget';
 import { ReportEventDispatcher } from './ReportEvent';
+
+// Load (non-lazy) core elements.
+import Chart from './elements/Chart';
+import DocString from './elements/DocString';
+import ExceptionElement from './elements/ExceptionElement';
+import Table from './elements/Table';
+import { Text } from './elements/Text';
+
 import { setStreamlitVersion } from './baseconsts';
 import { ForwardMsg, Text as TextProto } from './protobuf';
 import { addRows } from './dataFrameProto';
@@ -50,6 +43,17 @@ import { initRemoteTracker, trackEventRemotely } from './remotetracking';
 import { toImmutableProto, dispatchOneOf } from './immutableProto';
 
 import './StreamlitApp.css';
+
+// Lazy-load display elements.
+const Audio = React.lazy(() => import('./elements/Audio'));
+const Balloons = React.lazy(() => import('./elements/Balloons'));
+const DataFrame = React.lazy(() => import('./elements/DataFrame'));
+const ImageList = React.lazy(() => import('./elements/ImageList'));
+const Map = React.lazy(() => import('./elements/Map'));
+const DeckGlChart = React.lazy(() => import('./elements/DeckGlChart'));
+const PlotlyChart = React.lazy(() => import('./elements/PlotlyChart'));
+const VegaLiteChart = React.lazy(() => import('./elements/VegaLiteChart'));
+const Video = React.lazy(() => import('./elements/Video'));
 
 
 class StreamlitApp extends PureComponent {
@@ -59,13 +63,7 @@ class StreamlitApp extends PureComponent {
     this.state = {
       reportId: '<null>',
       reportName: null,
-      elements: fromJS([{
-        type: 'text',
-        text: {
-          format: TextProto.Format.INFO,
-          body: 'Connecting...',
-        },
-      }]),
+      elements: fromJS([makeElementWithInfoText('Connecting...')]),
       userSettings: {
         wideMode: false,
         runOnSave: true,
@@ -605,6 +603,7 @@ class StreamlitApp extends PureComponent {
           exception: exc => <ExceptionElement element={exc} width={width} />,
           imgs: imgs => <ImageList imgs={imgs} width={width} />,
           map: map => <Map map={map} width={width} />,
+          plotlyChart: el => <PlotlyChart element={el} width={width} />,
           progress: p => <Progress value={p.get('value')} style={{width}} />,
           table: df => <Table df={df} width={width} />,
           text: text => <Text element={text} width={width} />,
@@ -618,7 +617,18 @@ class StreamlitApp extends PureComponent {
       <div style={{ width }} className="footer" />
     ).flatMap((element, indx) => {
       if (element) {
-        return [<div className="element-container" key={indx}>{element}</div>];
+        return [
+          <React.Suspense
+            fallback={<Text
+              element={makeElementWithInfoText('Loading...').get('text')}
+              width={width}
+            />}
+            className="element-container"
+            key={indx}
+          >
+            {element}
+          </React.Suspense>,
+        ];
       } else {
         return [];
       }
@@ -667,6 +677,17 @@ function handleAddRowsMessage(element, namedDataSet) {
  */
 function isEmbeddedInIFrame() {
   return url.parse(window.location.href, true).query.embed === 'true';
+}
+
+
+function makeElementWithInfoText(text) {
+  return fromJS({
+    type: 'text',
+    text: {
+      format: TextProto.Format.INFO,
+      body: text,
+    },
+  });
 }
 
 

Makefile
@@ -1,6 +1,14 @@
 # Black magic to get module directories
 modules := $(foreach initpy, $(foreach dir, $(wildcard lib/*), $(wildcard $(dir)/__init__.py)), $(realpath $(dir $(initpy))))
 
+PY_VERSION := $(shell python -c 'import platform; print(platform.python_version())')
+ANACONDA_VERSION := $(shell ./scripts/anaconda_version.sh only)
+ifdef ANACONDA_VERSION
+PY_VERSION := $(ANACONDA_VERSION)
+else
+PY_VERSION := python-$(PY_VERSION)
+endif
+
 help:
 	@echo "Streamlit Make Commands:"
 	@echo " init         - Run once to install python and js dependencies."
@@ -25,14 +33,6 @@ build: react-build
 
 setup:
 	pip install pip-tools pipenv
-
-PY_VERSION := $(shell python -c 'import platform; print(platform.python_version())')
-ANACONDA_VERSION := $(shell ./scripts/anaconda_version.sh only)
-ifdef ANACONDA_VERSION
-PY_VERSION := $(ANACONDA_VERSION)
-else
-PY_VERSION := python-$(PY_VERSION)
-endif
 
 pipenv: lib/Pipfile
 # In CircleCI, dont generate Pipfile.lock This is only used for development.

docs/api/charts.plotly_chart.py
@@ -1,0 +1,21 @@
+import streamlit as st
+import plotly.plotly as py
+import plotly.figure_factory as ff
+import numpy as np
+
+# Add histogram data
+x1 = np.random.randn(200) - 2
+x2 = np.random.randn(200)
+x3 = np.random.randn(200) + 2
+
+# Group data together
+hist_data = [x1, x2, x3]
+
+group_labels = ['Group 1', 'Group 2', 'Group 3']
+
+# Create distplot with custom bin_size
+fig = ff.create_distplot(
+        hist_data, group_labels, bin_size=[.1, .25, .5])
+
+# Plot!
+st.plotly_chart(fig)

examples/plotly_example.py
@@ -1,0 +1,91 @@
+import streamlit as st
+import plotly.plotly as py
+import plotly.graph_objs as go
+import numpy as np
+
+st.title('Plotly examples')
+
+st.header('Chart with two lines')
+
+trace0 = go.Scatter(
+    x=[1, 2, 3, 4],
+    y=[10, 15, 13, 17]
+)
+trace1 = go.Scatter(
+    x=[1, 2, 3, 4],
+    y=[16, 5, 11, 9]
+)
+data = [trace0, trace1]
+st.write(data)
+
+
+###
+
+st.header('Matplotlib chart in Plotly')
+
+import matplotlib.pyplot as plt
+
+f = plt.figure()
+arr = np.random.normal(1, 1, size=100)
+plt.hist(arr, bins=20)
+
+st.plotly_chart(f)
+
+
+###
+
+st.header('3D plot')
+
+x, y, z = np.random.multivariate_normal(np.array([0,0,0]), np.eye(3), 400).transpose()
+
+trace1 = go.Scatter3d(
+    x=x,
+    y=y,
+    z=z,
+    mode='markers',
+    marker=dict(
+        size=12,
+        color=z,                # set color to an array/list of desired values
+        colorscale='Viridis',   # choose a colorscale
+        opacity=0.8
+    )
+)
+
+data = [trace1]
+layout = go.Layout(
+    margin=dict(
+        l=0,
+        r=0,
+        b=0,
+        t=0
+    )
+)
+fig = go.Figure(data=data, layout=layout)
+
+st.write(fig)
+
+
+###
+
+st.header('Fancy density plot')
+
+import plotly.plotly as py
+import plotly.figure_factory as ff
+
+import numpy as np
+
+# Add histogram data
+x1 = np.random.randn(200) - 2
+x2 = np.random.randn(200)
+x3 = np.random.randn(200) + 2
+
+# Group data together
+hist_data = [x1, x2, x3]
+
+group_labels = ['Group 1', 'Group 2', 'Group 3']
+
+# Create distplot with custom bin_size
+fig = ff.create_distplot(hist_data, group_labels, bin_size=[.1, .25, .5])
+
+# Plot!
+st.plotly_chart(fig)

frontend/src/StreamlitApp.css
@@ -114,6 +114,12 @@ header .decoration {
   color: #F63366;
 }
 
+iframe {
+  border: none;
+  padding: 0;
+  margin: 0;
+}
+
 .streamlit-regular .streamlit-container {
   margin-top: 80px;
 }

frontend/src/elements/PlotlyChart.tsx
@@ -1,0 +1,73 @@
+/**
+ * @license
+ * Copyright 2019 Streamlit Inc. All rights reserved.
+ */
+
+import * as React from 'react';
+import {Map as ImmutableMap} from 'immutable';
+import {dispatchOneOf} from '../immutableProto';
+import Plot from 'react-plotly.js';
+
+interface Props {
+  width: number;
+  element: ImmutableMap<string, any>;
+}
+
+const DEFAULT_HEIGHT = 500;
+
+class PlotlyChart extends React.PureComponent<Props> {
+  private chartNode = React.createRef<HTMLDivElement>();
+
+  public constructor(props: Props) {
+    super(props);
+  }
+
+  public render(): React.ReactNode {
+    const el = this.props.element;
+
+    const height: number =
+        el.get('height') > 0 ? el.get('height') : DEFAULT_HEIGHT;
+
+    const width: number =
+        el.get('width') > 0 ? el.get('width') : this.props.width;
+
+    return dispatchOneOf(el, 'chart', {
+      url: (url: string) => this.renderIFrame(url, width, height),
+      figure: (figure: ImmutableMap<string, any>) =>
+        this.renderFigure(figure, width, height),
+    });
+  }
+
+  private renderIFrame = (
+    url: string,
+    width: number,
+    height: number,
+  ): React.ReactNode => {
+    return (
+      <iframe
+        src={url}
+        style={{width, height}}
+      ></iframe>
+    );
+  }
+
+  private renderFigure = (
+    figure: ImmutableMap<string, any>,
+    width: number,
+    height: number,
+  ): React.ReactNode => {
+    const spec = JSON.parse(figure.get('spec'));
+    const config = JSON.parse(figure.get('config'));
+    return (
+      <Plot
+        data={spec.data}
+        layout={spec.layout}
+        config={config}
+        frames={spec.frames}
+        style={{width, height}}
+      />
+    );
+  }
+}
+
+export default PlotlyChart;

lib/Pipfile
@@ -20,6 +20,7 @@ recommonmark = "*"
 # Packages used in unit tests:
 altair = "*"
 matplotlib = {version="*", markers="python_version > '3.0'"}
+plotly = "*"
 
 # TODO(armando): Automate syncing of requirements to conda/streamlit/meta.yaml
 # When you update the packages here, you have to update

commit 5250d7616745b90ea649d5aedca26cd6ba6410ee

Unbreak CSS of elements in reports (broken by #567) (#598)
* version 0.32.0

* Fix ReportObserver, FileObserver tests

pytest will only run tests in files whose name ends in '_test'

* Unbreak CSS of elements in reports (broken by #567)

frontend/src/StreamlitApp.js
@@ -618,16 +618,19 @@ class StreamlitApp extends PureComponent {
     ).flatMap((element, indx) => {
       if (element) {
         return [
-          <React.Suspense
-            fallback={<Text
-              element={makeElementWithInfoText('Loading...').get('text')}
-              width={width}
-            />}
+          <div
             className="element-container"
             key={indx}
-          >
-            {element}
-          </React.Suspense>,
+            >
+            <React.Suspense
+              fallback={<Text
+                element={makeElementWithInfoText('Loading...').get('text')}
+                width={width}
+              />}
+            >
+              {element}
+            </React.Suspense>
+          </div>,
         ];
       } else {
         return [];