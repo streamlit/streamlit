c42891faab6b0e9320256345218b1d23e8bb8bb8
armando atienza <44782696+aaj-st@users.noreply.github.com>
2019-05-07 11:40:51 -0700

    Refactor st.image to support many more things.
    For #631

--- lib/tests/streamlit/image_proto_test.py
+++ lib/tests/streamlit/image_proto_test.py
@@ -1,98 +1,65 @@
-# Copyright 2018 Streamlit Inc. All rights reserved.
-
 """Unit test for image_proto."""
-
-import io
+__copyright__ = 'Copyright 2019 Streamlit Inc. All rights reserved.'
 import unittest
-
-import streamlit.elements.image_proto as image_proto
-import PIL.Image
 
 
 class ImageProtoTest(unittest.TestCase):
     """Test streamlit.image_proto."""
 
+    def test_PIL_to_png_bytes(self):
+        """Test streamlit.image_proto.PIL_to_png_bytes."""
+        pass
+
+    def test_BytesIO_to_bytes(self):
+        """Test streamlit.image_proto.BytesIO_to_bytes."""
+        pass
+
+    def test_np_array_to_png_bytes(self):
+        """Test streamlit.image_proto.np_array_to_png_bytes."""
+        pass
+
+    def test_verify_np_shape(self):
+        """Test streamlit.image_proto.verify_np_shape.
+
+        Need to test the following:
+        * check shape not (2, 3)
+        * check shape 3 but dims 1, 3, 4
+        * if only one channel convert to just 2 dimensions.
+        """
+        pass
+
+    def test_bytes_to_b64(self):
+        """Test streamlit.image_proto.bytes_to_b64.
+
+        Need to test the following:
+        * if width is greater then requested then shrink
+        """
+        pass
+
+    def test_clip_image(self):
+        """Test streamlit.image_proto.clip_image.
+
+        Need to test the following:
+        * float
+        * int
+        * float with clipping
+        * int  with clipping
+        """
+        pass
+
     def test_marshall_images(self):
         """Test streamlit.image_proto.marshall_images.
 
         Need to test the following:
-        * images
-        * urls
+        * if list
+        * if not list
+        * if captions is not list but image is
+        * if captions length doesnt match image length
+        * if the caption is set.
+        * PIL Images
+        * BytesIO
+        * Url
+        * Path
+        * Bytes
         """
         pass
-
-    def test_clamp_range(self):
-        """Test streamlit.image_proto.convert_to_uint8.clamp_range.
-
-        Need to test:
-        * If clamp is set
-        * If img min/max is within set min/max
-        * If img min/max is outside min/max
-        * If img max is greater than 200 but max is 1.0, ie normalize.
-        * If none of the following happened.
-        """
-        pass
-
-    def test_convert_to_uint8(self):
-        """Test streamlit.image_proto.convert_to_uint8.
-
-        Need to test:
-        * If img is floating point ie [0.0, 1.0]
-        * If img is integer ie [0, 255)
-        * Neither
-        """
-        pass
-
-    def test_convert_to_4_color_channels(self):
-        """Test streamlit.image_proto.convert_to_4_color_channels.
-
-        Need to test:
-        * Image with 4 channels (RGB + A)
-        * Image with 3 channels
-        * Image with 1 channel (recursion)
-        * Image with 0 channels ie pass in MxN
-        """
-        pass
-
-    def test_convert_imgs_to_list(self):
-        """Test streamlit.image_proto.convert_imgs_to_list.
-
-        Need to test:
-        * Shape is 3
-        * Shape is 4
-        * Shape is neither
-        """
-        pass
-
-    def test_convert_captions_to_list(self):
-        """Test streamlit.image_proto.convert_captions_to_list.
-
-        Need to test:
-        * No caption
-        * caption is string type
-        * caption is not a string type
-        * len(captions) != len(images)
-        """
-        pass
-
-    def test_get_imagelist_and_type(self):
-        """Test streamlit.image_proto._get_imagelist_and_type."""
-        # Test single URL
-        ret = image_proto._get_imagelist_and_type('some_url', None)
-        self.assertEqual(ret, (['some_url'], 'image_type_url'))
-
-        # Test list of URLs
-        urls = ['url0', 'url1', 'url2']
-        ret = image_proto._get_imagelist_and_type(urls, None)
-        self.assertEqual(ret, (urls, 'image_type_url'))
-
-        # Test with single BytesIO image.
-        img = PIL.Image.new('RGB', (64, 64), color = 'red')
-        with io.BytesIO() as output:
-            img.save(output, format="png")
-            ([pil_image], image_type) = image_proto._get_imagelist_and_type(output, None)
-
-            self.assertEqual(image_type, 'image_type_bytes')
-            self.assertEqual(type(pil_image).__name__, 'PngImageFile')
-
-        # TODO(armando): Test with single and array of PIL Images
