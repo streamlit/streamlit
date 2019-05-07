4da7aebed4a96aed6e68820964d15da5d72a1f76
Tim Conkling <tconkling@gmail.com>
2019-05-07 15:45:16 -0700

    WebsocketConnection, StaticConnection: typescript (#656)
    Refactored `WebsocketConnection` and `StaticConnection` to Typescript.

--- frontend/src/WebsocketConnection.ts
+++ frontend/src/WebsocketConnection.ts
@@ -3,16 +3,14 @@
  * Copyright 2018 Streamlit Inc. All rights reserved.
  */
 
-import { ForwardMsg, BackMsg } from './protobuf';
-import { ConnectionState } from './ConnectionState';
-import { logError } from './log';
-
+import {ConnectionState} from './ConnectionState';
+import {logError} from './log';
+import {BackMsg, ForwardMsg, IBackMsg} from './protobuf';
 
 /**
  * Number of times to try to connect to websocket.
  */
 const MAX_RETRIES = 3;
-
 
 /**
  * Timeout for the WebSocket connection attempt, in millis. This grows by N
@@ -20,69 +18,73 @@ const MAX_RETRIES = 3;
  */
 const CONNECTION_TIMEOUT_MS = 2000;
 
+interface Props {
+  uriList: string[];
+
+  /** Function called when we receive a new message. */
+  onMessage: (message: any) => void;
+
+  /**
+   * Function called when our ConnectionState changes.
+   * If the new ConnectionState is ERROR, errMsg will be defined.
+   */
+  setConnectionState: (connectionState: ConnectionState, errMsg?: string) => void;
+}
 
 /**
-* This class is the "brother" of StaticConnection. The class  connects to the
-* proxy and gets deltas over a websocket connection. It also implements:
-*
-*   getStatus() - returns information to display status in the GUI
-*   connectedToProxy() - returns true when proxy connection is open
-*   sendToProxy() - sends a message to the proxy
-*/
-class WebsocketConnection {
+ * This class is the "brother" of StaticConnection. The class connects to the
+ * proxy and gets deltas over a websocket connection.
+ */
+export class WebsocketConnection {
+  private readonly props: Props;
+
   /**
-   * Constructor.
+   * To guarantee packet transmission order, this is the index of the last
+   * dispatched incoming message.
    */
-  constructor(props) {
+  private lastDispatchedMessageIndex: number = -1;
+
+  /**
+   * And this is the index of the next message we recieve.
+   */
+  private nextMessageIndex: number = 0;
+
+  /**
+   * This dictionary stores recieved messages that we haven't sent out yet
+   * (because we're still decoding previous messages)
+   */
+  private messageQueue: { [index: number]: any } = {};
+
+  /**
+   * Keep track of how many times we tried to connect.
+   */
+  private attemptNumber: number = 0;
+
+  private websocket?: WebSocket;
+
+  public constructor(props: Props) {
     this.props = props;
-
-    /**
-     * To guarantee packet transmission order, this is the index of the last
-     * dispatched incoming message.
-     */
-    this.lastDispatchedMessageIndex = -1;
-
-    /**
-     * And this is the index of the next message we recieve.
-     */
-    this.nextMessageIndex = 0;
-
-    /**
-     * This dictionary stores recieved messages that we haven't sent out yet
-     * (because we're still decoding previous messages)
-     */
-    this.messageQueue = {};
-
-    this.state = ConnectionState.DISCONNECTED;
-    this.websocket = null;
-
-    /**
-     * Keep track of how many times we tried to connect.
-     */
-    this.attemptNumber = 0;
-
     this.connect(0);
   }
 
-  connect(uriIndex) {
-    const { uriList, setConnectionState, onMessage } = this.props;
+  private connect(uriIndex: number): void {
+    const {uriList, setConnectionState, onMessage} = this.props;
 
     if (uriIndex >= uriList.length) {
       this.attemptNumber += 1;
       if (this.attemptNumber < MAX_RETRIES) {
         uriIndex = 0;
       } else {
-        setConnectionState({
-          connectionState: ConnectionState.ERROR,
-          errMsg: 'The connection is down. Please rerun your Python script.',
-        });
+        setConnectionState(
+          ConnectionState.ERROR,
+          'The connection is down. Please rerun your Python script.');
         return;
       }
     }
 
     let tryingNext = false;
 
-    const tryNext = () => {
+    const tryNext = (): void => {
       if (!tryingNext) {
         this.connect(uriIndex + 1);
         tryingNext = true;
@@ -90,7 +92,7 @@ class WebsocketConnection {
     };
 
     const timeoutId = setTimeout(() => {
-      if (this.websocket.readyState === 0) {
+      if (this.websocket && this.websocket.readyState === 0) {
         logError(
           `Websocket connection to ${uriList[uriIndex]} timed out`);
         this.websocket.close();
@@ -101,22 +103,18 @@ class WebsocketConnection {
     const uri = uriList[uriIndex];
     this.websocket = new WebSocket(uri);
 
-    this.websocket.onmessage = ({ data }) => {
+    this.websocket.onmessage = ({data}) => {
       this.handleMessage(data, onMessage);
     };
 
     this.websocket.onopen = () => {
       clearTimeout(timeoutId);
-      setConnectionState({
-        connectionState: ConnectionState.CONNECTED,
-      });
+      setConnectionState(ConnectionState.CONNECTED);
     };
 
     this.websocket.onclose = () => {
       clearTimeout(timeoutId);
-      setConnectionState({
-        connectionState: ConnectionState.DISCONNECTED,
-      });
+      setConnectionState(ConnectionState.DISCONNECTED);
     };
 
     this.websocket.onerror = () => {
@@ -129,14 +127,16 @@ class WebsocketConnection {
    * Encodes the message with the outgoingMessageType and sends it over the
    * wire.
    */
-  sendMessage(obj) {
-    if (!this.websocket) { return; }
+  public sendMessage(obj: IBackMsg): void {
+    if (!this.websocket) {
+      return;
+    }
     const msg = BackMsg.create(obj);
     const buffer = BackMsg.encode(msg).finish();
     this.websocket.send(buffer);
   }
 
-  handleMessage(data, onMessage) {
+  private handleMessage(data: any, onMessage: (data: any) => void): void {
     // Assign this message an index.
     const messageIndex = this.nextMessageIndex;
     this.nextMessageIndex += 1;
@@ -150,7 +150,13 @@ class WebsocketConnection {
         return;
       }
 
-      const resultArray = new Uint8Array(reader.result);
+      const result = reader.result;
+      if (result == null || typeof result === 'string') {
+        logError(`Unexpected result from FileReader: ${result}.`);
+        return;
+      }
+
+      const resultArray = new Uint8Array(result);
       this.messageQueue[messageIndex] = ForwardMsg.decode(resultArray);
       while ((this.lastDispatchedMessageIndex + 1) in this.messageQueue) {
         const dispatchMessageIndex = this.lastDispatchedMessageIndex + 1;
@@ -161,5 +167,3 @@ class WebsocketConnection {
     };
   }
 }
-
-export default WebsocketConnection;
