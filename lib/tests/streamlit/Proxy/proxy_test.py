streamlit/streamlit

commit 63dabea1ab4108b510603755f392f05f5fda9c06

Make st.title/header/subheader use Markdown under the hood (#274)
* Make st.title/header/subheader use Markdown under the hood. This make sure there's no difference in how we render those vs. Markdown titles.

* Fix tests.

lib/streamlit/__init__.py
@@ -95,7 +95,7 @@ def write(*args):
         - write(data_frame) : Displays the DataFrame as a table.
         - write(error)      : Prints an exception specially.
         - write(func)       : Displays information about a function.
-        - write(module)     : Displays infomration about the module.
+        - write(module)     : Displays information about the module.
         - write(obj)        : The default is to print str(obj).
 
     Examples

frontend/src/StreamlitApp.css
@@ -46,7 +46,8 @@ h1, .h1 {
   font-family: 'IBM Plex Sans', sans-serif;
   font-weight: 700;
   font-size: 2.5rem;
-  margin: 2.5rem 0 0 0;
+  margin: 2.5rem 0 1.5rem 0;
+  line-height: 1em;
 }
 
 h1:first-child, .h1:first-child {
@@ -57,29 +58,41 @@ h2, .h2 {
   font-family: 'IBM Plex Sans', sans-serif;
   font-weight: 300;
   font-size: 2.0rem;
-  margin: 3rem 0 0 0;
+  margin: 2.5rem 0 1rem 0;
+  line-height: 1em;
 }
 
 h3, .h3 {
   font-family: 'IBM Plex Sans', sans-serif;
   font-weight: 500;
   font-size: 1.25rem;
-  margin: 0.25rem 0 0 0;
+  margin: 1.5rem 0 1rem 0;
+  line-height: 1em;
+}
+
+h4, .h4 {
+  font-family: 'IBM Plex Sans', sans-serif;
+  font-weight: 700;
+  font-size: 1.0rem;
+  margin: 1.5rem 0 1rem 0;
+  line-height: 1em;
 }
 
 h5, .h5 {
   font-family: 'IBM Plex Sans', sans-serif;
-  font-weight: 300;
-  font-size: 1.25rem;
-  margin: 0.25rem 0 0 0;
+  font-weight: 700;
+  font-size: 0.8rem;
+  text-transform: uppercase;
+  margin: 1.5rem 0 0.5rem 0;
+  line-height: 1em;
 }
 
 h6, .h6 {
   font-family: 'IBM Plex Sans', sans-serif;
   font-weight: 300;
-  font-size: 1rem;
-  text-transform: uppercase;
-  margin: 0;
+  font-size: 0.8rem;
+  margin: 1.5rem 0 0.5rem 0;
+  line-height: 1em;
 }
 
 header {
@@ -174,6 +187,10 @@ button:focus {
 .streamlit-container .element-container {
   display: inline-block;
   margin-bottom: 1rem;
+}
+
+.streamlit-container hr {
+  border-color: #F0F3F9;
 }
 
 .streamlit-container .footer {

frontend/src/elements/Text.css
@@ -12,7 +12,6 @@
 
 .streamlit-container .markdown-text-container {
   font-family: 'IBM Plex Sans', sans-serif;
-  /* font-family: 'IBM Plex Serif', serif; */
   margin-bottom: -1rem;
 }
 

frontend/src/elements/Text.js
@@ -34,7 +34,7 @@ class Text extends PureComponent {
       case TextProto.Format.MARKDOWN:
         return (
           <div className="markdown-text-container" style={{width}}>
-            <ReactMarkdown source={body} />
+            <ReactMarkdown source={body} escapeHtml={false} />
           </div>
         );
 
@@ -71,20 +71,6 @@ class Text extends PureComponent {
               name={false}
               style={{font: ""}}  // Unset so we can style via a CSS file.
             />
-          </div>
-        );
-
-      case TextProto.Format.TITLE:
-      case TextProto.Format.HEADER:
-      case TextProto.Format.SUB_HEADER:
-        const className = {
-          [TextProto.Format.TITLE]: 'h1',
-          [TextProto.Format.HEADER]: 'h2',
-          [TextProto.Format.SUB_HEADER]: 'h3',
-        }[format]
-        return (
-          <div className={className} style={{width}}>
-            {body}
           </div>
         );
 

lib/streamlit/DeltaGenerator.py
@@ -162,45 +162,45 @@ class DeltaGenerator(object):
 
     @_export
     @_create_element
-    def title(self, element, string):
-        """Display string in title formatting.
+    def title(self, element, body):
+        """Display text in title formatting.
 
         Parameters
         ----------
-        string : str
-            The string to display.
+        body : str
+            The text to display.
 
         """
-        element.text.body = str(string)
-        element.text.format = protobuf.Text.TITLE
+        element.text.body = '# %s' % textwrap.dedent(body).strip()
+        element.text.format = protobuf.Text.MARKDOWN
 
     @_export
     @_create_element
-    def header(self, element, string):
-        """Display string in header formatting.
+    def header(self, element, body):
+        """Display text in header formatting.
 
         Parameters
         ----------
-        string : str
-            The string to display.
+        body : str
+            The text to display.
 
         """
-        element.text.body = str(string)
-        element.text.format = protobuf.Text.HEADER
+        element.text.body = '## %s' % textwrap.dedent(body).strip()
+        element.text.format = protobuf.Text.MARKDOWN
 
     @_export
     @_create_element
-    def subheader(self, element, string):
-        """Display string in subheader formatting.
+    def subheader(self, element, body):
+        """Display text in subheader formatting.
 
         Parameters
         ----------
-        string : str
-            The string to display.
+        body : str
+            The text to display.
 
         """
-        element.text.body = str(string)
-        element.text.format = protobuf.Text.SUB_HEADER
+        element.text.body = '### %s' % textwrap.dedent(body).strip()
+        element.text.format = protobuf.Text.MARKDOWN
 
     @_export
     @_create_element
@@ -364,7 +364,7 @@ class DeltaGenerator(object):
 
     @_export
     def dataframe(self, df):
-        """Display a dataframe.
+        """Display a dataframe as an interactive table.
 
         Parameters
         ----------

lib/tests/streamlit/delta_generator_test.py
@@ -84,10 +84,7 @@ class DeltaGeneratorTextTest(unittest.TestCase):
     def test_generic_text(self):
         """Test protobuf.Text generic str(body) stuff."""
         test_data = {
-            'title': protobuf.Text.TITLE,
             'text': protobuf.Text.PLAIN,
-            'header': protobuf.Text.HEADER,
-            'subheader': protobuf.Text.SUB_HEADER,
             'error': protobuf.Text.ERROR,
             'warning': protobuf.Text.WARNING,
             'info': protobuf.Text.INFO,

protobuf/Text.proto
@@ -17,15 +17,6 @@ message Text {
     // A JSON object. Stored as a string.
     JSON = 2;
 
-    // Corresponds to the <h1> tag.
-    TITLE = 3;
-
-    // Corresponds to the <h2> tag.
-    HEADER = 4;
-
-    // Corresponds to the <h3> tag.
-    SUB_HEADER = 5;
-
     // Shows an error message.
     ERROR = 6;
 

commit 204521f19fd12fe4baa1add413a8728b66f7594a

Release 0.37.0 (#683)
* Fix kill_proxy by looking whether 'python'/'Python' is a **substring** of the process name. (#255)

* Fix test deprecation warning of assertEquals

sed -i -e 's/assertEquals/assertEqual/g'

* Add user-tests directory and setup some basic scaffolding for non unit tests.

I know there's an examples and admin already.  This uses the pytest
framework which isn't being heavily used in this example but will be
important later on for the ability to easily reuse mocks, etc.

While this test is trivial, it isn't a unit test.  It actually goes and
uses the internet to check for an ip address which is an integration
test.  We will eventually test more user specific things.

* Use HTTPs throughout Streamlit codebase and site. (#349)

* Use HTTPs throughout Streamlit codebase and site.

* Make inline docs use https

* Remove entrypoints as a dependency in Pipfile now that conda is sorted.

I figured out how conda finally works and this is not necessary.
Also generate the lockfiles for python-*, no need to do it for anaconda
because we won't be using pip install for *conda* in the future.

* [docs] Add "first things to try" troubleshooting info. (#342)

* Rename wrapper annotation in DeltaGenerator, for clarity. (#346)

* Rename wrapper annotation in DeltaGenerator, for clarity.

* Update docstring.

* Fix st.add_rows and st.table. Poorly resolved merge conflicts broke them.

* Setup fixes; troubleshooting section

* Makefile: add 'pytest' help row

* Fix deck_gl_chart by passing correct proto.

* increase test_file_watch_and_callback timeout

I can run this test from the commandline no problem, but
it fails in PyCharm (presumably because of slowness from
additional magic that PyCharm inserts when running tests).

Increasing the multiplier to 3 worked for me, but I've set it
to 4 here for extra breathing room on slower machines.

* Make vega_lite_chart support inline 'data' and 'dataset' (#347)

* Fix st.add_rows and st.table. Poorly resolved merge conflicts broke them.

* Fix deck_gl_chart by passing correct proto.

* Make vega_lite_chart support inline 'data' and 'dataset'

* Add vega lite unit test

* Deprecate data_transforms in VegaLiteChart.proto

* Clarify vega_lite_example.py

* Clear cache (#359)

* frontend: "Clear cache" menu option, dialog, and message sending
* Remove BackMsg.rerun.clear_cache and associated frontend UI
* BackMsg: un-deprecate rerun_script; deprecrate rerun
* eslint: add env.browser=true; some eslint cleanup

* clear_cache hotkeys

- 'C' brings up the clear_cache dialog
- enter confirms the dialog

* Add Altair support! (#348)

* Fix st.add_rows and st.table. Poorly resolved merge conflicts broke them.

* Fix deck_gl_chart by passing correct proto.

* Add st.altair_chart() and make st.write() accept Pyplot Figures and Altair Charts.

* Add altair to docs and examples

* Add st.altair unit test

* Add altair to pipfile

* Setup fixes; troubleshooting section

* Makefile: add 'pytest' help row

* increase test_file_watch_and_callback timeout

I can run this test from the commandline no problem, but
it fails in PyCharm (presumably because of slowness from
additional magic that PyCharm inserts when running tests).

Increasing the multiplier to 3 worked for me, but I've set it
to 4 here for extra breathing room on slower machines.

* Clear cache (#359)

* frontend: "Clear cache" menu option, dialog, and message sending
* Remove BackMsg.rerun.clear_cache and associated frontend UI
* BackMsg: un-deprecate rerun_script; deprecrate rerun
* eslint: add env.browser=true; some eslint cleanup

* Fix docs to point to help@streamlit.io and correct Gradient link.

* [circleci] Dont run miniconda or python3.5

These only run on the develop branch.  I removed them and will get added
back when I get to work more on circle ci.

* version 0.27.0

* Update robots.txt using Google's robot tested tool.

* Update issue templates

* Include empty styling data for unstyled DataFrames

This fixes concatenation of unstyled + styled DataFrames

More optimal, but also more complex, would be to detect missing style
data in a DataFrame when doing this concatenation, and add it then.
This would need to be done in both Python and JS.

* Fix regression in dialog titles, introduced in 63dabea

conda/streamlit/meta.yaml
@@ -1,5 +1,5 @@
 {% set name = "streamlit" %}
-{% set version = "0.36.0" %}
+{% set version = "0.37.0" %}
 
 package:
   name: "{{ name|lower }}"

lib/Pipfile.locks/python-3.6.6
@@ -48,19 +48,19 @@
         },
         "boto3": {
             "hashes": [
-                "sha256:882cc4869b47b51dae4b4a900769e72171ff00e0b6bca644b2d7a7ad7378f324",
-                "sha256:cd503a7e7a04f1c14d2801f9727159dfa88c393b4004e98940fa4aa205d920c8"
+                "sha256:bc6635529f85cf130813fda973a2a5240f54532c7e284b04f215dcee9721bbb8",
+                "sha256:d60329ff211a496016c334dbecf7ab70500b19aee013d2435b4f30d9e0a84ca1"
             ],
             "index": "pypi",
-            "version": "==1.9.137"
+            "version": "==1.9.146"
         },
         "botocore": {
             "hashes": [
-                "sha256:0d95794f6b1239c75e2c5f966221bcd4b68020fddb5676f757531eedbb612ed8",
-                "sha256:3213cf48cf2ceee10fc3b93221f2cd1c38521cca7584f547d5c086213cc60f35"
+                "sha256:1517c52eaa3056d0e81f9a81b580d7f28440e7e1523d10a8acc8160c56be7113",
+                "sha256:19d9d56fcf4f16ffea8a929bbf3c72db3458b6c1f306c04031f3166759cd62ac"
             ],
             "index": "pypi",
-            "version": "==1.12.137"
+            "version": "==1.12.146"
         },
         "click": {
             "hashes": [
@@ -316,11 +316,11 @@
         },
         "urllib3": {
             "hashes": [
-                "sha256:4c291ca23bbb55c76518905869ef34bdd5f0e46af7afe6861e8375643ffee1a0",
-                "sha256:9a247273df709c4fedb38c711e44292304f73f39ab01beda9f6b9fc375669ac3"
+                "sha256:2393a695cd12afedd0dcb26fe5d50d0cf248e5a66f75dbd89a3d4eb333a61af4",
+                "sha256:a637e5fae88995b256e3409dc4d52c2e2e0ba32c42a6365fee8bbd2238de3cfb"
             ],
             "markers": "python_version >= '3.4'",
-            "version": "==1.24.2"
+            "version": "==1.24.3"
         },
         "watchdog": {
             "hashes": [
@@ -340,11 +340,11 @@
         },
         "altair": {
             "hashes": [
-                "sha256:0c19c3351f6afe7f7e419c340401533b4b886f2e55f826d02b9e3e2f9e637624",
-                "sha256:446cc19c55e321a5779196e6d19d304ac4c8a81c00350e1bb2541b157ba89f74"
+                "sha256:63934563a7a7b7186335858206a0b9be6043163b8b54a26cd3b3299a9e5e391f",
+                "sha256:65e243afa6da5b746c411890fd7dfd0f187c0f8e581cf3c34b07339712cf6627"
             ],
             "index": "pypi",
-            "version": "==3.0.0"
+            "version": "==3.0.1"
         },
         "atomicwrites": {
             "hashes": [
@@ -397,10 +397,10 @@
         },
         "commonmark": {
             "hashes": [
-                "sha256:9f6dda7876b2bb88dd784440166f4bc8e56cb2b2551264051123bacb0b6c1d8a",
-                "sha256:abcbc854e0eae5deaf52ae5e328501b78b4a0758bf98ac8bb792fce993006084"
+                "sha256:14c3df31e8c9c463377e287b2a1eefaa6019ab97b22dad36e2f32be59d61d68d",
+                "sha256:867fc5db078ede373ab811e16b6789e9d033b15ccd7296f370ca52d1ee792ce0"
             ],
-            "version": "==0.8.1"
+            "version": "==0.9.0"
         },
         "coverage": {
             "hashes": [
@@ -629,11 +629,11 @@
         },
         "mock": {
             "hashes": [
-                "sha256:5ce3c71c5545b472da17b72268978914d0252980348636840bd34a00b5cc96c1",
-                "sha256:b158b6df76edd239b8208d481dc46b6afd45a846b7812ff0ce58971cf5bc8bba"
+                "sha256:83657d894c90d5681d62155c82bda9c1187827525880eda8ff5df4ec813437c3",
+                "sha256:d157e52d4e5b938c550f39eb2fd15610db062441a9c2747d3dbfa9298211d0f8"
             ],
             "index": "pypi",
-            "version": "==2.0.0"
+            "version": "==3.0.5"
         },
         "more-itertools": {
             "hashes": [
@@ -712,13 +712,6 @@
             "index": "pypi",
             "version": "==0.24.2"
         },
-        "pbr": {
-            "hashes": [
-                "sha256:6901995b9b686cb90cceba67a0f6d4d14ae003cd59bc12beb61549bdfbe3bc89",
-                "sha256:d950c64aeea5456bbd147468382a5bb77fe692c13c9f00f0219814ce5b642755"
-            ],
-            "version": "==5.2.0"
-        },
         "pillow": {
             "hashes": [
                 "sha256:15c056bfa284c30a7f265a41ac4cbbc93bdbfc0dfe0613b9cb8a8581b51a9e55",
@@ -769,18 +762,18 @@
         },
         "plotly": {
             "hashes": [
-                "sha256:04708425517a7bdf4855976b8f91b647282104b3419bd097ff42655238793a4f",
-                "sha256:51eb88e82dc2221cf3cf84f5bf5a348597358e57285e436436baaee7e0a7f436"
+                "sha256:498c35a2a482f7c7937fc2f3681fec653a0191dd21e40e754a6b774234cd167e",
+                "sha256:545e01f400345d9a3914f3e8ede033908cdb19525559782e46449be84b8f9a56"
             ],
             "index": "pypi",
-            "version": "==3.8.1"
+            "version": "==3.9.0"
         },
         "pluggy": {
             "hashes": [
-                "sha256:19ecf9ce9db2fce065a7a0586e07cfb4ac8614fe96edf628a264b1c70116cf8f",
-                "sha256:84d306a647cc805219916e62aab89caa97a33a1dd8c342e87a37f91073cd4746"
+                "sha256:25a1bc1d148c9a640211872b4ff859878d422bccb59c9965e04eed468a0aa180",
+                "sha256:964cedd2b27c492fbf0b7f58b3284a09cf7f99b0f715941fb24a439b3af1bd1a"
             ],
-            "version": "==0.9.0"
+            "version": "==0.11.0"
         },
         "py": {
             "hashes": [
@@ -805,10 +798,10 @@
         },
         "pygments": {
             "hashes": [
-                "sha256:5ffada19f6203563680669ee7f53b64dabbeb100eb51b61996085e99c03b284a",
-                "sha256:e8218dd399a61674745138520d0d4cf2621d7e032439341bc3f647bff125818d"
+                "sha256:31cba6ffb739f099a85e243eff8cb717089fdd3c7300767d9fc34cb8e1b065f5",
+                "sha256:5ad302949b3c98dd73f8d9fcdc7e9cb592f120e32a18e23efd7f3dc51194472b"
             ],
-            "version": "==2.3.1"
+            "version": "==2.4.0"
         },
         "pyparsing": {
             "hashes": [
@@ -825,19 +818,19 @@
         },
         "pytest": {
             "hashes": [
-                "sha256:3773f4c235918987d51daf1db66d51c99fac654c81d6f2f709a046ab446d5e5d",
-                "sha256:b7802283b70ca24d7119b32915efa7c409982f59913c1a6c0640aacf118b95f5"
+                "sha256:136632a40451162cdfc18fe4d7ecc5d169b558a3d4bbb1603d4005308a42fd03",
+                "sha256:62b129bf8368554ca7a942cbdb57ea26aafef46cc65bc317cdac3967e54483a3"
             ],
             "index": "pypi",
-            "version": "==4.4.1"
+            "version": "==4.4.2"
         },
         "pytest-cov": {
             "hashes": [
-                "sha256:0ab664b25c6aa9716cbf203b17ddb301932383046082c081b9848a0edf5add33",
-                "sha256:230ef817450ab0699c6cc3c9c8f7a829c34674456f2ed8df1fe1d39780f7c87f"
+                "sha256:2b097cde81a302e1047331b48cadacf23577e431b61e9c6f49a1170bbe3d3da6",
+                "sha256:e00ea4fdde970725482f1f35630d12f074e121a23801aabf2ae154ec6bdd343a"
             ],
             "index": "pypi",
-            "version": "==2.6.1"
+            "version": "==2.7.1"
         },
         "python-dateutil": {
             "hashes": [
@@ -979,11 +972,11 @@
         },
         "testfixtures": {
             "hashes": [
-                "sha256:6b79324f35852eaacd4f5f440b20ee1b98df23001ff88db8b8bde3746f753f0a",
-                "sha256:c0028d2acd45e6604d359b806132af05815d64e882cf995fa45e6814ddaade13"
+                "sha256:819e9090bf3cce5dfaf56817f55ede76b3ae490ad0e9ffe3060a6618d11894c3",
+                "sha256:a6e95634a80bd6e5adc7e861729f4abd84433e91b4215d48a2098a096c0261b7"
             ],
             "index": "pypi",
-            "version": "==6.7.0"
+            "version": "==6.8.2"
         },
         "toolz": {
             "hashes": [
@@ -1028,11 +1021,11 @@
         },
         "urllib3": {
             "hashes": [
-                "sha256:4c291ca23bbb55c76518905869ef34bdd5f0e46af7afe6861e8375643ffee1a0",
-                "sha256:9a247273df709c4fedb38c711e44292304f73f39ab01beda9f6b9fc375669ac3"
+                "sha256:2393a695cd12afedd0dcb26fe5d50d0cf248e5a66f75dbd89a3d4eb333a61af4",
+                "sha256:a637e5fae88995b256e3409dc4d52c2e2e0ba32c42a6365fee8bbd2238de3cfb"
             ],
             "markers": "python_version >= '3.4'",
-            "version": "==1.24.2"
+            "version": "==1.24.3"
         },
         "virtualenv": {
             "hashes": [
@@ -1057,11 +1050,11 @@
         },
         "wheel": {
             "hashes": [
-                "sha256:66a8fd76f28977bb664b098372daef2b27f60dc4d1688cfab7b37a09448f0e9d",
-                "sha256:8eb4a788b3aec8abf5ff68d4165441bc57420c9f64ca5f471f58c3969fe08668"
+                "sha256:20c39930e770523bbfd73078306206cb4a69ef72ff2b67f93bcd7d30d5d794df",
+                "sha256:bd330c9e074b361ad68b8b6f8771f6ba81b6506e21dc77391056199bf01e7d9f"
             ],
             "index": "pypi",
-            "version": "==0.33.1"
+            "version": "==0.33.3"
         }
     }
 }

lib/setup.py
@@ -12,7 +12,7 @@ def readme():
 
 setuptools.setup(
     name='streamlit',
-    version='0.36.0',  # PEP-440
+    version='0.37.0',  # PEP-440
     description='Streaming Data Science',
     long_description=readme(),
     url='https://streamlit.io',

lib/streamlit/elements/data_frame_proto.py
@@ -60,9 +60,7 @@ def convert_anything_to_df(df):
     if _is_pandas_styler(df):
         return df.data
 
-    import pandas
-    global pd
-    pd = pandas
+    import pandas as pd
 
     if util.is_type(df, 'numpy.ndarray') and len(df.shape) == 0:
         return pd.DataFrame([])
@@ -429,8 +427,8 @@ def _get_data_frame(delta, name=None):
 
         # Some element types don't support named datasets.
         if name and element_type in ('data_frame', 'table', 'chart'):
-            raise ValueError('Dataset names not supported for st.%s' %
-                             element_type)
+            raise ValueError(
+                'Dataset names not supported for st.%s' % element_type)
 
         if element_type in 'data_frame':
             return delta.new_element.data_frame

lib/streamlit/proxy/BrowserWebSocket.py
@@ -15,10 +15,10 @@ from tornado.websocket import WebSocketClosedError
 from tornado.websocket import WebSocketHandler
 
 from streamlit import caching
-from streamlit import forward_msg_proto
 from streamlit import config
-from streamlit import protobuf
+from streamlit import forward_msg_proto
 from streamlit import process_runner
+from streamlit import protobuf
 from streamlit.proxy import Proxy
 from streamlit.proxy import proxy_util
 

lib/streamlit/proxy/ClientWebSocket.py
@@ -16,8 +16,9 @@ from tornado.websocket import WebSocketHandler
 from streamlit import config
 from streamlit import protobuf
 from streamlit.logger import get_logger
-from streamlit.proxy import Proxy, ClientConnection
+from streamlit.proxy import Proxy
 from streamlit.proxy import proxy_util
+from streamlit.proxy.ClientConnection import ClientConnection
 
 LOGGER = get_logger(__name__)
 

lib/streamlit/proxy/Proxy.py
@@ -103,8 +103,8 @@ class Proxy(object):
     def _set_up_server(self):
         # We have to import this in here to break a circular import reference
         # issue in Python 2.7.
-        from streamlit.proxy import ClientWebSocket
-        from streamlit.proxy import BrowserWebSocket
+        from streamlit.proxy.ClientWebSocket import ClientWebSocket
+        from streamlit.proxy.BrowserWebSocket import BrowserWebSocket
 
         routes = [
             ('/new/(.*)', ClientWebSocket, dict(proxy=self)),
@@ -673,4 +673,8 @@ def _print_urls(connection, wait_secs):
             EXTERNAL REPORT URL: %(external_url)s
             INTERNAL REPORT URL: %(internal_url)s
             ════════════════════════════════════════════════════════════
-        '''), {'external_url': external_url, 'internal_url': internal_url})
+        '''), {
+            'external_url': external_url,
+            'internal_url': internal_url,
+            'timeout_msg': timeout_msg,
+        })

lib/streamlit/proxy/__init__.py
@@ -1,16 +1,0 @@
-# Copyright 2018 Streamlit Inc. All rights reserved.
-
-"""The package which includes all files related to the Streamlit proxy
-server."""
-
-# Python 2/3 compatibility
-from __future__ import print_function, division, unicode_literals, absolute_import
-from streamlit.compatibility import setup_2_3_shims
-setup_2_3_shims(globals())
-
-from .ClientConnection import ClientConnection
-from .BrowserWebSocket import BrowserWebSocket
-from .ClientWebSocket import ClientWebSocket
-from .Proxy import Proxy
-
-import streamlit.proxy.proxy_util

lib/streamlit/proxy/__main__.py
@@ -10,7 +10,7 @@ setup_2_3_shims(globals())
 from streamlit.logger import get_logger
 LOGGER = get_logger(__name__)
 
-from streamlit.proxy import Proxy
+from streamlit.proxy.Proxy import Proxy
 
 
 def main():

lib/tests/streamlit/Proxy/ReportSession_test.py
@@ -11,7 +11,7 @@ import unittest
 import mock
 
 from streamlit.proxy.ReportSession import ReportSession
-from streamlit.proxy import ClientConnection
+from streamlit.proxy.ClientConnection import ClientConnection
 from streamlit import forward_msg_proto
 
 # An arbitrary UUID constant

lib/tests/streamlit/Proxy/proxy_test.py
@@ -1,20 +1,125 @@
-"""Proxy Unittest."""
-#from tornado.testing import AsyncHTTPTestCase
+import unittest
+from mock import patch
 
-# Disabled all of this because it was failing.
-# Made a ticket to fix this: https://trello.com/c/HpEeZGBy
+from streamlit import config
+from streamlit.proxy import Proxy
+from streamlit.proxy.ClientConnection import ClientConnection
+import streamlit.forward_msg_proto as forward_msg_proto
 
-# from streamlit.Proxy import Proxy
-#
-# class ProxyTest(AsyncHTTPTestCase):
-#     """Proxy unittest class."""
-#
-#     def get_app(self):
-#         """Get app."""
-#         proxy = Proxy()
-#         return proxy._app
-#
-#     def test_Proxy_class(self):
-#         """Test streamlit.Proxy.Proxy class."""
-#         # For now do nothing, just setup the scaffolding.
-#         self.assertTrue(True)
+msg = forward_msg_proto.new_report_msg(
+    'report_id', 'cwd', ['command_line'], 'source_file_path')
+
+connection = ClientConnection(msg.new_report, 'report_name')
+
+class StreamlitTest(unittest.TestCase):
+    """Test Streamlit.proxy.Proxy"""
+
+    @patch('streamlit.proxy.Proxy.LOGGER.info')
+    def test_print_urls1(self, mock_logger):
+        orig_get_option = config.get_option
+        orig_is_manually_set = config.is_manually_set
+
+        def mock_get_option(arg):
+            if arg == 'browser.proxyAddress':
+                return 'the-address'
+            return orig_get_option(arg)
+
+        def mock_is_manually_set(arg):
+            if arg == 'browser.proxyAddress':
+                return True
+            return orig_is_manually_set(arg)
+
+        with patch.object(config, 'get_option', new=mock_get_option):
+            with patch.object(
+                config, 'is_manually_set', new=mock_is_manually_set):
+
+                Proxy._print_urls(connection, float('inf'))
+
+        string = mock_logger.call_args[0][0]
+        dikt = mock_logger.call_args[0][1]
+        out = string % dikt
+        self.assertTrue('REPORT URL: http://the-address' in out)
+        self.assertTrue('within' not in out)
+
+    @patch('streamlit.proxy.Proxy.LOGGER.info')
+    def test_print_urls2(self, mock_logger):
+        orig_get_option = config.get_option
+        orig_is_manually_set = config.is_manually_set
+
+        def mock_get_option(arg):
+            if arg == 'browser.proxyAddress':
+                return 'the-address'
+            return orig_get_option(arg)
+
+        def mock_is_manually_set(arg):
+            if arg == 'browser.proxyAddress':
+                return True
+            return orig_is_manually_set(arg)
+
+        with patch.object(config, 'get_option', new=mock_get_option):
+            with patch.object(
+                config, 'is_manually_set', new=mock_is_manually_set):
+
+                Proxy._print_urls(connection, 10)
+
+        string = mock_logger.call_args[0][0]
+        dikt = mock_logger.call_args[0][1]
+        out = string % dikt
+        self.assertTrue('REPORT URL: http://the-address' in out)
+        self.assertTrue('within 10 seconds' in out)
+
+    @patch('streamlit.proxy.Proxy.LOGGER.info')
+    @patch('streamlit.proxy.Proxy.util.get_external_ip')
+    @patch('streamlit.proxy.Proxy.util.get_internal_ip')
+    def test_print_urls3(
+        self, mock_get_internal_ip, mock_get_external_ip, mock_logger):
+
+        orig_is_manually_set = config.is_manually_set
+
+        def mock_is_manually_set(arg):
+            if arg == 'browser.proxyAddress':
+                return False
+            return orig_is_manually_set(arg)
+
+        mock_get_internal_ip.return_value = 'internal-ip'
+        mock_get_external_ip.return_value = 'external-ip'
+
+        with patch.object(
+            config, 'is_manually_set', new=mock_is_manually_set):
+
+            Proxy._print_urls(connection, float('inf'))
+
+        string = mock_logger.call_args[0][0]
+        dikt = mock_logger.call_args[0][1]
+        out = string % dikt
+        self.assertTrue('INTERNAL REPORT URL: http://internal-ip' in out)
+        self.assertTrue('EXTERNAL REPORT URL: http://external-ip' in out)
+        self.assertTrue('within' not in out)
+
+    @patch('streamlit.proxy.Proxy.LOGGER.info')
+    @patch('streamlit.proxy.Proxy.util.get_external_ip')
+    @patch('streamlit.proxy.Proxy.util.get_internal_ip')
+    def test_print_urls4(
+        self, mock_get_internal_ip, mock_get_external_ip, mock_logger):
+
+        orig_is_manually_set = config.is_manually_set
+
+        def mock_is_manually_set(arg):
+            if arg == 'browser.proxyAddress':
+                return False
+            return orig_is_manually_set(arg)
+
+        mock_get_internal_ip.return_value = 'internal-ip'
+        mock_get_external_ip.return_value = 'external-ip'
+
+        with patch.object(
+            config, 'is_manually_set', new=mock_is_manually_set):
+
+            Proxy._print_urls(connection, 10)
+
+        string = mock_logger.call_args[0][0]
+        dikt = mock_logger.call_args[0][1]
+        out = string % dikt
+        self.assertTrue('INTERNAL REPORT URL: http://internal-ip' in out)
+        self.assertTrue('EXTERNAL REPORT URL: http://external-ip' in out)
+        self.assertTrue('within 10 seconds' in out)