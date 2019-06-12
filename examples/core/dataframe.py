7cc8b3bd6b4e248dd9d056de56b4a9764ba4786c
kantuni <henrikh.kantuni@gmail.com>
2019-06-11 21:22:55 -0400

    Table, Dataframe, Balloons

--- examples/core/dataframe.py
+++ examples/core/dataframe.py
@@ -2,6 +2,12 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+
+def highlight_first(value):
+    color = 'yellow' if value == 0 else 'white'
+    return 'background-color: %s' % color
+
+
 grid = np.arange(0, 100, 1).reshape(10, 10)
 df = pd.DataFrame(grid)
-st.dataframe(df)
+st.dataframe(df.style.applymap(highlight_first))
