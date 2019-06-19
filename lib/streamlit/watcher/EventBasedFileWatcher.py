05cc738561806e7befb25e02f427f3a6bf711322
Tim Conkling <tconkling@gmail.com>
2019-06-19 12:56:35 -0700

    EventBasedFileWatcher.close: use a lock

--- lib/streamlit/watcher/EventBasedFileWatcher.py
+++ lib/streamlit/watcher/EventBasedFileWatcher.py
@@ -173,17 +173,18 @@ class _MultiFileWatcher(object):
                 del self._folder_handlers[folder_path]
 
     def close(self):
-        """Close this _MultiFileWatcher object forever."""
-        if len(self._folder_handlers) != 0:
-            self._folder_handlers = {}
-            LOGGER.debug(
-                'Stopping observer thread even though there is a non-zero '
-                'number of event observers!')
-        else:
-            LOGGER.debug('Stopping observer thread')
+        with self._lock:
+            """Close this _MultiFileWatcher object forever."""
+            if len(self._folder_handlers) != 0:
+                self._folder_handlers = {}
+                LOGGER.debug(
+                    'Stopping observer thread even though there is a non-zero '
+                    'number of event observers!')
+            else:
+                LOGGER.debug('Stopping observer thread')
 
-        self._observer.stop()
-        self._observer.join(timeout=5)
+            self._observer.stop()
+            self._observer.join(timeout=5)
 
 
 class WatchedFile(object):
