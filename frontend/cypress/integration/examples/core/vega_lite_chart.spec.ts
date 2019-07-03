30d5f557521cb2b00860d9722bc6efa16e8685db
Tim Conkling <tconkling@gmail.com>
2019-07-03 14:26:33 -0700

    Update the vega_lite_chart e2e test with snapshots (#1011)
    It turns out there was already a bare-bones vega_lite e2e test, so I didn't create one from scratch.
    
    Instead, I merged `examples/vega_lite_example.py` into `core/vega_lite_chart.py`, and deleted the former file. 
    
    `vega_lite_chart.spec.ts` now tests snapshots for the various chart types
    
    Closes #882

--- frontend/cypress/integration/examples/core/vega_lite_chart.spec.ts
+++ frontend/cypress/integration/examples/core/vega_lite_chart.spec.ts
@@ -1,14 +1,18 @@
 /// <reference types="cypress" />
 
 describe('st.vega_lite_chart', () => {
-  beforeEach(() => {
+  before(() => {
+    // http://gs.statcounter.com/screen-resolution-stats/desktop/worldwide
+    cy.viewport(1366, 768)
     cy.visit('http://localhost:3000/')
+
+    // Force our header to scroll with the page, rather than
+    // remaining fixed. This prevents us from occasionally getting
+    // the little multi-colored ribbon at the top of our screenshots.
+    cy.get('.stApp > header').invoke('css', 'position', 'absolute')
   })
 
   it('displays charts on the DOM', () => {
-    cy.get('.element-container .stVegaLiteChart')
-      .should('have.length', 4)
-
     cy.get('.element-container .stVegaLiteChart')
       .find('canvas')
       .should('have.class', 'marks')
@@ -26,5 +30,13 @@ describe('st.vega_lite_chart', () => {
 
     cy.get('.stVegaLiteChart canvas')
       .eq(3).should('have.css', 'width', '500px')
+  })
+
+  it('supports different ways to get the same plot', () => {
+    cy.get('.stVegaLiteChart')
+      .filter(idx => idx >= 4 && idx <= 7)
+      .each(el => {
+        cy.wrap(el).matchImageSnapshot()
+      })
   })
 })
