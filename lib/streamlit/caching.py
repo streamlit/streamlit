0e27c1efe84b1f8023143184632ff96a0b667832
Dominik Moritz <domoritz@gmail.com>
2019-07-24 00:23:11 +0200

    Add names of non-local names to function hash (#1135)

--- lib/streamlit/caching.py
+++ lib/streamlit/caching.py
@@ -166,8 +166,12 @@ def cache(func=None, on_disk=False):
             LOGGER.debug('Hashing %i bytes. %s',
                 len(arg_string), func.__name__)
             hasher.update(arg_string)
+            # hash the bytecode
             hasher.update(func.__code__.co_code)
+            # hash constants that are referenced by the bytecode
             hasher.update(pickle.dumps(func.__code__.co_consts, pickle.HIGHEST_PROTOCOL))
+            # hash non-local names referenced by the bytecode
+            hasher.update(pickle.dumps(func.__code__.co_names, pickle.HIGHEST_PROTOCOL))
             key = hasher.hexdigest()
             LOGGER.debug('Cache key: %s', key)
 
