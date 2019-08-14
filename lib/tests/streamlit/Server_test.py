ab78e57bc472e2dd236758718190c970b407f699
Tim Conkling <tconkling@gmail.com>
2019-08-14 13:58:29 -0700

    MetricsHandlerTest

--- lib/tests/streamlit/Server_test.py
+++ lib/tests/streamlit/Server_test.py
@@ -7,8 +7,10 @@ import tornado.web
 from mock import patch
 from tornado.testing import AsyncHTTPTestCase
 
+from streamlit import config
 from streamlit.server import Server
 from streamlit.server.routes import HealthHandler
+from streamlit.server.routes import MetricsHandler
 
 
 class ServerUtilsTest(unittest.TestCase):
@@ -62,7 +64,6 @@ class HealthHandlerTest(AsyncHTTPTestCase):
         ])
 
     def test_healthz(self):
-        """Test that a healthy server returns OK"""
         response = self.fetch('/healthz')
         self.assertEqual(200, response.code)
         self.assertEqual(b'ok', response.body)
@@ -70,3 +71,20 @@ class HealthHandlerTest(AsyncHTTPTestCase):
         self._is_healthy = False
         response = self.fetch('/healthz')
         self.assertEqual(503, response.code)
+
+
+class MetricsHandlerTest(AsyncHTTPTestCase):
+    """Tests the /metrics endpoint"""
+    def get_app(self):
+        return tornado.web.Application([
+            (r'/metrics', MetricsHandler),
+        ])
+
+    def test_metrics(self):
+        config.set_option('global.metrics', False)
+        response = self.fetch('/metrics')
+        self.assertEqual(404, response.code)
+
+        config.set_option('global.metrics', True)
+        response = self.fetch('/metrics')
+        self.assertEqual(200, response.code)
