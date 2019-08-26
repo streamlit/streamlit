d2cba40e097922073e356e0dc9bc2037e2f171e9
Matteo Monchiero <matteo@streamlit.io>
2019-08-26 11:32:30 -0700

    Adding unit tests for magic

---
+++ lib/tests/streamlit/magic_test.py
@@ -0,0 +1,107 @@
+# Copyright 2019 Streamlit Inc. All rights reserved.
+
+"""Magic unit test."""
+
+import unittest
+import ast
+
+import streamlit.magic as magic
+
+CODE_SIMPLE_STATEMENTS = '''
+a = 1
+b = 10
+a
+b
+'''
+
+CODE_IF_STATEMENT = '''
+a = 1
+if True:
+    a
+    if False:
+        a
+    elif False:
+        a
+    else:
+        a
+else:
+    a
+
+'''
+
+CODE_FOR_STATEMENT = '''
+a = 1
+for i in range(10):
+    for j in range(2):
+        a
+
+'''
+
+CODE_TRY_STATEMENT = '''
+try:
+    a = 10
+    a
+except Exception:
+    try:
+        a
+    finally:
+        a
+finally:
+    a
+'''
+
+CODE_FUNCTION_CALL = '''
+def myfunc(a):
+    a
+a =10
+myfunc(a)
+'''
+
+CODE_WITH_STATEMENT = '''
+a = 10
+with None:
+    a
+'''
+
+
+class MagicTest(unittest.TestCase):
+    """Test for Magic.
+    The test count the number of substitutions that magic.add_code do for
+    a few code snippets. The test passes if the expected number of
+    substitutions have been made.
+    """
+
+    def _testCode(self, code, expected_count):
+        tree = magic.add_magic(code, './')
+        count = 0
+        for node in ast.walk(tree):
+            # count the nodes where a substitution has been made, i.e.
+            # look for 'calls' to a 'streamlit' function
+            if type(node) is ast.Call and 'streamlit' in ast.dump(node.func):
+                count += 1
+        self.assertEqual(count, expected_count,
+                         'There must be at least two streamlit nodes')
+
+    def test_simple_statement(self):
+        """Test simple statements"""
+        self._testCode(CODE_SIMPLE_STATEMENTS, 2)
+
+    def test_if_statement(self):
+        """Test if statements"""
+        self._testCode(CODE_IF_STATEMENT, 5)
+
+    def test_for_statement(self):
+        """Test for statements"""
+        self._testCode(CODE_FOR_STATEMENT, 1)
+
+    def test_try_statement(self):
+        """Test try statements"""
+        self._testCode(CODE_FOR_STATEMENT, 1)
+
+    def test_function_call_statement(self):
+        """Test with function calls"""
+        self._testCode(CODE_FUNCTION_CALL, 1)
+
+    def test_with_statement(self):
+        """Test with 'with' statements"""
+        self._testCode(CODE_WITH_STATEMENT, 1)
