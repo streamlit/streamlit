158420a2f3e820e69ab806afef075898bb525cc7
Jonathan Rhone <rhone.j@gmail.com>
2019-08-27 19:11:34 +0200

    fix reuse label test, disabled test, and audo test

--- examples/core/reuse_label.py
+++ examples/core/reuse_label.py
@@ -14,19 +14,26 @@
 # limitations under the License.
 
 import streamlit as st
+from streamlit import config
 
 if not hasattr(st, 'test_run_count'):
     st.test_run_count = 0
 else:
     st.test_run_count += 1
 
-# Starting the report takes up the first run.
-# When run the test will see the slider first.
-if not st.test_run_count:
-    w1 = st.button('label')
-elif st.test_run_count <= 2:
-    w1 = st.slider('label', 25, 0, 100, 1)
+if config.get_option('server.headless'):
+    if not st.test_run_count:
+        w1 = st.slider('label', 0, 100, 25, 1)
+    else:
+        w1 = st.selectbox('label', ('m', 'f'), 1)
 else:
-    w1 = st.selectbox('label', ('m', 'f'), 1)
+    # Starting the report takes up the first run.
+    # When run the test will see the slider first.
+    if not st.test_run_count:
+        w1 = st.button('label')
+    elif st.test_run_count == 1:
+        w1 = st.slider('label', 0, 100, 25, 1)
+    else:
+        w1 = st.selectbox('label', ('m', 'f'), 1)
 
 st.write('value 1:', w1)
