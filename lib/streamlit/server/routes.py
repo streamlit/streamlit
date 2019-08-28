6da1a3b0e0e0776b8fe08a2ca1d49fdbd45b4e2a
Tim Conkling <tconkling@gmail.com>
2019-08-28 12:42:40 -0700

    CORS /OPTIONS docs

--- lib/streamlit/server/routes.py
+++ lib/streamlit/server/routes.py
@@ -62,6 +62,18 @@ class _SpecialRequestHandler(tornado.web.RequestHandler):
     def options(self):
         """/OPTIONS handler for preflight CORS checks.
 
+        When a browser is making a CORS request, it may sometimes first
+        send an OPTIONS request, to check whether the server understands the
+        CORS protocol. This is optional, and doesn't happen for every request
+        or in every browser. If an OPTIONS request does get sent, and is not
+        then handled by the server, the browser will fail the underlying
+        request.
+
+        The proper way to handle this is to send a 204 response ("no content")
+        with the CORS headers attached. (These headers are automatically added
+        to every outgoing response, including OPTIONS responses,
+        via set_default_headers().)
+
         See https://developer.mozilla.org/en-US/docs/Glossary/Preflight_request
         """
         self.set_status(204)
@@ -156,9 +168,6 @@ class MessageCacheHandler(tornado.web.RequestHandler):
         self.set_status(200)
 
     def options(self):
-        """/OPTIONS handler for preflight CORS checks.
-
-        See https://developer.mozilla.org/en-US/docs/Glossary/Preflight_request
-        """
+        """/OPTIONS handler for preflight CORS checks."""
         self.set_status(204)
         self.finish()
