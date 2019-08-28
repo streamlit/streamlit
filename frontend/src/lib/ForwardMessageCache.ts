0b316d3af7a5c8ad90355dbd970fea927f6fe31e
Tim Conkling <tconkling@gmail.com>
2019-08-28 10:34:23 -0700

    Header fix

--- frontend/src/lib/ForwardMessageCache.ts
+++ frontend/src/lib/ForwardMessageCache.ts
@@ -1,14 +1,27 @@
 /**
  * @license
- * Copyright 2019 Streamlit Inc. All rights reserved.
+ * Copyright 2018-2019 Streamlit Inc.
  *
- * @fileoverview Handles message caching for WebsocketConnection.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 import {ForwardMsg, ForwardMsgMetadata} from 'autogen/proto'
 import {logMessage} from 'lib/log'
 import {BaseUriParts, buildHttpUri} from 'lib/ServerUtil'
 
+/**
+ * Handles ForwardMsg caching for WebsocketConnection.
+ */
 export class ForwardMsgCache {
   private readonly messages = new Map<string, ForwardMsg>()
 
