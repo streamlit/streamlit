500d7a575aa91441409b047a187c89c313ae6cdf
Matteo Monchiero <matteo@streamlit.io>
2019-08-30 15:13:18 -0700

    e2e test

---
+++ frontend/cypress/integration/examples/core/dataframe_dimension_spec.spec.ts
@@ -0,0 +1,43 @@
+/**
+ * @license
+ * Copyright 2018-2019 Streamlit Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/// <reference types="cypress" />
+
+describe('Dataframes with different sizes', () => {
+  const expected = [
+    { width: '727px', height: '300px' },
+    { width: '247px', height: '150px' },
+    { width: '247px', height: '300px' },
+    { width: '727px', height: '150px' },
+
+  ]
+  before(() => {
+    // http://gs.statcounter.com/screen-resolution-stats/desktop/worldwide
+    cy.viewport(1366, 768)
+    cy.visit('http://localhost:3000/')
+  })
+
+  it('should shows as expected', () => {
+    cy.get('.element-container .stDataFrame')
+      .should('have.length', 4)
+      .each((el, idx) => {
+        cy.wrap(el)
+          .should('have.css', 'width', expected[idx].width)
+          .should('have.css', 'height', expected[idx].height)
+      })
+  })
+})
