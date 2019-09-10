557a83364d67f2d8f2eca2eb8705bfa906d622d0
kantuni <henrikh.kantuni@gmail.com>
2019-09-10 15:07:49 -0400

    Make pytest happy

--- lib/tests/streamlit/streamlit_test.py
+++ lib/tests/streamlit/streamlit_test.py
@@ -40,7 +40,7 @@ def get_version():
     """Get version by parsing out setup.py."""
     dirname = os.path.dirname(__file__)
     base_dir = os.path.abspath(os.path.join(dirname, "../.."))
-    pattern = re.compile(r"(?:.*version=\')(?P<version>.*)(?:\',  # PEP-440$)")
+    pattern = re.compile(r"(?:.*version=\")(?P<version>.*)(?:\",  # PEP-440$)")
     for line in open(os.path.join(base_dir, "setup.py")).readlines():
         m = pattern.match(line)
         if m:
