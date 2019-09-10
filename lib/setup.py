047fe072d7f0bd52447539e2f3d0273c417109a3
kantuni <henrikh.kantuni@gmail.com>
2019-09-10 14:26:31 -0400

    Pre-commit hook test

--- lib/setup.py
+++ lib/setup.py
@@ -13,22 +13,22 @@ def readme():
 
 
 setuptools.setup(
-    name="streamlit",
-    version="0.45.0",  # PEP-440
-    description="Magical developer tool for machine learning engineers",
-    long_description=readme(),
-    url="https://streamlit.io",
-    author="Streamlit Inc",
-    author_email="hello@streamlit.io",
-    license="Apache 2",
-    packages=setuptools.find_packages(exclude=["tests", "tests.*"]),
-    # Requirements
-    install_requires=requirements,
-    zip_safe=False,  # install source files not egg
-    include_package_data=True,  # copy html and friends
-    entry_points={"console_scripts": ["streamlit = streamlit.cli:main"]},
-    # For Windows so that streamlit * commands work ie.
-    # - streamlit version
-    # - streamlit hello
-    scripts=["bin/streamlit.cmd"],
+                    name="streamlit",
+                    version="0.45.0",  # PEP-440
+                    description="Magical developer tool for machine learning engineers",
+                    long_description=readme(),
+                    url="https://streamlit.io",
+                    author="Streamlit Inc",
+                    author_email="hello@streamlit.io",
+                    license="Apache 2",
+                    packages=setuptools.find_packages(exclude=["tests", "tests.*"]),
+                    # Requirements
+                    install_requires=requirements,
+                    zip_safe=False,  # install source files not egg
+                    include_package_data=True,  # copy html and friends
+                    entry_points={"console_scripts": ["streamlit = streamlit.cli:main"]},
+                    # For Windows so that streamlit * commands work ie.
+                    # - streamlit version
+                    # - streamlit hello
+                    scripts=["bin/streamlit.cmd"],
 )
