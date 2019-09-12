22be6e169a75046f9f50d4b98911038fc54e4d44
Matteo Monchiero <matteo@streamlit.io>
2019-09-12 14:37:22 -0700

    moving cli name attribute to the tests

--- lib/tests/streamlit/cli_test.py
+++ lib/tests/streamlit/cli_test.py
@@ -27,6 +27,10 @@ from streamlit import cli
 class CliTest(unittest.TestCase):
     """Unit tests for the cli."""
 
+    def setUp(self):
+        cli.name = 'test'
+        self.runner = CliRunner()
+
     def test_help(self):
         """streamlit help should show the expected help text"""
         expected_help = """Usage: _jb_pytest_runner.py [OPTIONS] COMMAND [ARGS]...
@@ -54,68 +58,56 @@ Commands:
   run       Run a Python script, piping stderr to Streamlit.
   version   Print Streamlit's version number.
 """
-        runner = CliRunner()
-        result = runner.invoke(cli, ['help'])
+        result = self.runner.invoke(cli, ['help'])
         self.assertEqual(0, result.exit_code)
         self.assertEqual(expected_help, result.output)
 
     def test_run_no_arguments(self):
         """streamlit run should fail if run with no arguments"""
-        runner = CliRunner()
-        result = runner.invoke(cli, ['run'])
+        result = self.runner.invoke(cli, ['run'])
         self.assertNotEqual(0, result.exit_code)
 
     def test_run_existing_file_argument(self):
         """streamlit run succeeds if an existing file is passed"""
-        runner = CliRunner()
-
         # Mocking _main_run
         cli._main_run = MagicMock()
 
         with tempfile.NamedTemporaryFile() as file:
-            result = runner.invoke(cli, ['run', file.name])
+            result = self.runner.invoke(cli, ['run', file.name])
         self.assertEqual(0, result.exit_code)
 
     def test_run_non_existing_file_argument(self):
         """streamlit run should fail if a non existing file is passed"""
-        runner = CliRunner()
-
         # Mocking _main_run
         cli._main_run = MagicMock()
 
-        result = runner.invoke(cli, ['run', '/non-existing-streamlit-script'])
+        result = self.runner.invoke(cli, ['run', '/non-existing-streamlit-script'])
         self.assertNotEqual(0, result.exit_code)
 
     def test_run_valid_url(self):
         """streamlit run succeeds if an existing url is passed"""
-        runner = CliRunner()
-
         # Mocking _main_run
         cli._main_run = MagicMock()
 
-        result = runner.invoke(cli, ['run', 'http://www.cnn.com'])
+        result = self.runner.invoke(cli, ['run', 'http://www.cnn.com'])
         self.assertEqual(0, result.exit_code)
 
     def test_run_non_valid_url(self):
         """streamlit run should fail if a non valid url is passed"""
-        runner = CliRunner()
-
         # Mocking _main_run
         cli._main_run = MagicMock()
 
-        result = runner.invoke(cli, ['run', 'odd_protocol://www.cnn.com'])
+        result = self.runner.invoke(cli, ['run', 'odd_protocol://www.cnn.com'])
         self.assertNotEqual(0, result.exit_code)
 
     def test_run_non_existing_url(self):
         """streamlit run should fail if a non existing but valid
          url is passed
          """
-        runner = CliRunner()
-
         # Mocking _main_run
         cli._main_run = MagicMock()
 
-        result = runner.invoke(cli, [
+        result = self.runner.invoke(cli, [
             'run',
             'http://www.cnn.com/some-cnn-streamlit-script'
         ])
