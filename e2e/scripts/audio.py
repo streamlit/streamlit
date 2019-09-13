82b4cb99f176157e793aba80f424e83166d10a60
Tim Conkling <tconkling@gmail.com>
2019-09-13 15:35:54 -0700

    pylint e2e/scripts

--- e2e/scripts/audio.py
+++ e2e/scripts/audio.py
@@ -16,6 +16,6 @@
 import streamlit as st
 import requests
 
-url = 'https://www.w3schools.com/html/horse.ogg'
+url = "https://www.w3schools.com/html/horse.ogg"
 file = requests.get(url).content
 st.audio(file)
--- e2e/scripts/video.py
+++ e2e/scripts/video.py
@@ -16,6 +16,6 @@
 import streamlit as st
 import requests
 
-url = 'https://www.w3schools.com/html/mov_bbb.mp4'
+url = "https://www.w3schools.com/html/mov_bbb.mp4"
 file = requests.get(url).content
 st.video(file)
