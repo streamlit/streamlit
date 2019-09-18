90838273f9377a40ca34e78474434349713fa438
Tim Conkling <tconkling@gmail.com>
2019-09-18 10:59:27 -0700

    Capitalize

--- lib/tests/streamlit/cli_test.py
+++ lib/tests/streamlit/cli_test.py
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Unit tests for the streamlit CLI."""
+"""Unit tests for the Streamlit CLI."""
 
 import unittest
 
