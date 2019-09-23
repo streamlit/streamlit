b21990b535184cd600917df198bfc49164a6687c
Tim Conkling <tconkling@gmail.com>
2019-09-23 09:53:53 -0700

    e2e test for magic

---
+++ e2e/scripts/magic.py
@@ -0,0 +1,93 @@
+import asyncio
+import contextlib
+
+async_loop = asyncio.new_event_loop()
+
+
+"no block"
+
+if True:
+    "IF"
+
+if False:
+    pass
+elif True:
+    "ELIF"
+
+if False:
+    pass
+else:
+    "ELSE"
+
+
+for ii in range(1):
+    "FOR"
+
+while True:
+    "WHILE"
+    break
+
+
+@contextlib.contextmanager
+def context_mgr():
+    try:
+        yield
+    finally:
+        pass
+
+
+with context_mgr():
+    "WITH"
+
+try:
+    "TRY"
+except:
+    raise
+
+try:
+    raise RuntimeError("shenanigans!")
+except RuntimeError:
+    "EXCEPT"
+
+try:
+    pass
+finally:
+    "FINALLY"
+
+def func(value):
+    value
+
+func("FUNCTION")
+
+
+async def async_func(value):
+    value
+
+
+async_loop.run_until_complete(async_func("ASYNC FUNCTION"))
+
+
+async def async_for():
+    async def async_iter():
+        yield
+
+    async for _ in async_iter():
+        "ASYNC FOR"
+
+
+async_loop.run_until_complete(async_for())
+
+
+async def async_with():
+    @contextlib.asynccontextmanager
+    async def async_context_mgr():
+        try:
+            yield
+        finally:
+            pass
+
+    async with async_context_mgr():
+        "ASYNC WITH"
+
+
+async_loop.run_until_complete(async_with())
---
+++ e2e/specs/magic.spec.ts
@@ -0,0 +1,49 @@
+/**
+ * @license
+ * Copyright 2018-2019 Streamlit Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/// <reference types="cypress" />
+
+describe("streamlit magic", () => {
+  before(() => {
+    cy.visit("http://localhost:3000/");
+  });
+
+  it("displays markdown", () => {
+    const expected = [
+      "no block",
+      "IF",
+      "ELIF",
+      "ELSE",
+      "FOR",
+      "WHILE",
+      "WITH",
+      "TRY",
+      "EXCEPT",
+      "FINALLY",
+      "FUNCTION",
+      "ASYNC FUNCTION",
+      "ASYNC FOR",
+      "ASYNC WITH"
+    ];
+
+    expected.forEach((text, index) => {
+      cy.get(".element-container > .stText > p")
+        .eq(index)
+        .contains(text);
+    });
+  });
+});
