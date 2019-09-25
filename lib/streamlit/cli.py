c8cfd41812df854d47d747ac4b3494b235e5ca30
Matteo Monchiero <matteo@streamlit.io>
2019-09-25 09:19:23 -0700

    python2 fix

--- lib/streamlit/cli.py
+++ lib/streamlit/cli.py
@@ -128,15 +128,13 @@ def main_run(file_or_url, args=None):
     temporary file and runs this file.
     """
     import subprocess
-
-    command_line = subprocess.list2cmdline(
-        [
+    cmd_line_as_list = [
             click.get_current_context().parent.command_path,
             click.get_current_context().command.name,
             file_or_url,
-            *args,
         ]
-    )
+    cmd_line_as_list.extend(args)
+    command_line = subprocess.list2cmdline(cmd_line_as_list)
 
     from validators import url
 
