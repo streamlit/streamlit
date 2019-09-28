72fa705365eec0c76829497c71976b32a2053554
Matteo Monchiero <matteo@streamlit.io>
2019-09-28 10:48:57 -0700

    moving the endcoding hack fix out of a cached function since Python37 didn't like that.

--- lib/streamlit/hello.py
+++ lib/streamlit/hello.py
@@ -164,12 +164,12 @@ def data_frame_demo():
     """
     import sys
     import pandas as pd
+    if sys.version_info[0] < 3:
+        reload(sys)
+        sys.setdefaultencoding("utf-8")
 
     @st.cache
     def get_UN_data():
-        if sys.version_info[0] < 3:
-            reload(sys)
-            sys.setdefaultencoding("utf-8")
         AWS_BUCKET_URL = "https://streamlit-demo-data.s3-us-west-2.amazonaws.com"
         df = pd.read_csv(AWS_BUCKET_URL + "/agri.csv.gz")
         return df.set_index("Region")
