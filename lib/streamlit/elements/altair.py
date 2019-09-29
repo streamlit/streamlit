1ff90f2027202917e99822f6f9111a6692beb557
Thiago Teixeira <thiagot@gmail.com>
2019-09-28 22:18:26 -0700

    Hide legend from native charts when data has only 1 column.

--- lib/streamlit/elements/altair.py
+++ lib/streamlit/elements/altair.py
@@ -32,14 +32,20 @@ def generate_chart(chart_type, data):
     if not isinstance(data, pd.DataFrame):
         data = convert_anything_to_df(data)
 
+    n_cols = len(data.columns)
     data = pd.melt(data.reset_index(), id_vars=['index'])
+
+    if chart_type == 'area':
+        opacity = {'value': 0.7}
+    else:
+        opacity = {'value': 1.0}
 
     chart = getattr(alt.Chart(data), 'mark_' + chart_type)().encode(
         alt.X('index', title=''),
         alt.Y('value', title=''),
-        alt.Color('variable', title=''),
-        alt.Tooltip(['index', 'value', 'variable'])).interactive()
-
+        alt.Color('variable', title='', type='nominal'),
+        alt.Tooltip(['index', 'value', 'variable']),
+        opacity=opacity).interactive()
     return chart
 
 
