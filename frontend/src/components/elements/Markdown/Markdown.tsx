0d272d845c32423fb50fafebc2a18cab99a640d2
Naomi <pnaomi@gmail.com>
2019-10-21 14:26:17 -0700

    added Markdown element in frontend (untested)

---
+++ frontend/src/components/elements/Markdown/Markdown.tsx
@@ -0,0 +1,107 @@
+/**
+ * @license
+ * Copyright 2018-2019 Streamlit Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import React, { ReactNode, ReactElement } from "react"
+
+import classNames from "classnames"
+import ReactJson from "react-json-view"
+import ReactMarkdown from "react-markdown"
+import { Map as ImmutableMap } from "immutable"
+import { Markdown as MarkdownProto } from "autogen/proto"
+
+// Ignoring typeScript for this module as it has no ts support
+// eslint-disable-next-line @typescript-eslint/ban-ts-ignore
+// @ts-ignore
+import htmlParser from "react-markdown/plugins/html-parser"
+
+import CodeBlock from "../CodeBlock"
+
+import "./Markdown.scss"
+
+interface LinkProps {
+  href: string
+  children: ReactElement
+}
+
+interface LinkReferenceProps {
+  href: string
+  children: [ReactElement]
+}
+
+// Using target="_blank" without rel="noopener noreferrer" is a security risk:
+// see https://mathiasbynens.github.io/rel-noopener
+const linkWithTargetBlank = (props: LinkProps): ReactElement => (
+  <a href={props.href} target="_blank" rel="noopener noreferrer">
+    {props.children}
+  </a>
+)
+
+// Handle rendering a link through a reference, ex [text](href)
+// Don't convert to a link if only `[text]` and missing `(href)`
+const linkReferenceHasParens = (props: LinkReferenceProps): any => {
+  const { href, children } = props
+
+  if (!href) {
+    return children.length ? `[${children[0].props.children}]` : ""
+  }
+
+  return (
+    <a href={href} target="_blank" rel="noopener noreferrer">
+      {children}
+    </a>
+  )
+}
+
+interface Props {
+  width: number
+  element: ImmutableMap<string, any>
+}
+
+/**
+ * Functional element representing formatted text.
+ */
+class Markdown extends React.PureComponent<Props> {
+  public render(): ReactNode {
+    const { element, width } = this.props
+    const body = element.get("body")
+    const format = element.get("format")
+    const renderers = {
+      code: CodeBlock,
+      link: linkWithTargetBlank,
+      linkReference: linkReferenceHasParens,
+    }
+    const styleProp = { width }
+
+    const allowHTML = element.get("allowHtml")
+    const astPlugins = allowHTML ? [htmlParser()] : []
+
+    return (
+      <div className="markdown-text-container stText" style={styleProp}>
+        <ReactMarkdown
+          source={body}
+          escapeHtml={!allowHTML}
+          astPlugins={astPlugins}
+          renderers={renderers}
+        />
+      </div>
+    )
+  }
+
+  }
+}
+
+export default Markdown
