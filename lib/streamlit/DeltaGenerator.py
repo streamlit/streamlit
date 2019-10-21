44fdce828b4157c5dd4847bde7f215a70c898f48
Tim Conkling <tconkling@gmail.com>
2019-10-21 10:10:02 -0700

    Markdown formatting in duplicate widget error message

--- lib/streamlit/DeltaGenerator.py
+++ lib/streamlit/DeltaGenerator.py
@@ -129,25 +129,25 @@ def _build_duplicate_widget_message(widget_type, user_key=None):
     if user_key is not None:
         message = textwrap.dedent(
             """
-            There are multiple identical st.{widget_type} widgets that use the
-             '{user_key}' key.
+            **There are multiple identical `st.{widget_type}` widgets with
+             `key='{user_key}'`.**
             
-            To fix this, please make sure that the 'key' argument is unique for 
-            each st.{widget_type} you create.
+            To fix this, please make sure that the `key` argument is unique for
+            each `st.{widget_type}` you create.
             """
         )
     else:
         message = textwrap.dedent(
             """
-            There are multiple identical st.{widget_type} widgets with the 
-            same generated key.
+            **There are multiple identical `st.{widget_type}` widgets with the
+            same generated key.**
             
             (When a widget is created, it's assigned an internal key based on
             its structure. Multiple widgets with an identical structure will
             result in the same internal key, which causes this error.)
             
-            To fix this, please pass a unique 'key' argument to 
-            st.{widget_type}().
+            To fix this, please pass a unique `key` argument to
+            `st.{widget_type}`.
             """
         )
 
