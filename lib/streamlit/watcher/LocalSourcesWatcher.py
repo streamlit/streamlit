c4660abe9df09f5fd4e366e96a05c61dd5b9e41b
Karl Taylor <k4r1@users.noreply.github.com>
2019-10-28 20:09:30 +0800

    Add test for nested module unloading

--- lib/streamlit/watcher/LocalSourcesWatcher.py
+++ lib/streamlit/watcher/LocalSourcesWatcher.py
@@ -98,9 +98,9 @@ class LocalSourcesWatcher(object):
         # Delete all watched modules so we can guarantee changes to the
         # updated module are reflected on reload.
         #
-        # In principle, for reloading a given module, we only need to unload 
+        # In principle, for reloading a given module, we only need to unload
         # the module itself and all of the modules which import it (directly
-        # or indirectly) such that when we exec the application code, the 
+        # or indirectly) such that when we exec the application code, the
         # changes are reloaded and reflected in the running application.
         #
         # However, determining all import paths for a given loaded module is
@@ -126,7 +126,8 @@ class LocalSourcesWatcher(object):
 
         try:
             wm = WatchedModule(
-                watcher=FileWatcher(filepath, self.on_file_changed), module_name=module_name
+                watcher=FileWatcher(filepath, self.on_file_changed),
+                module_name=module_name,
             )
         except ErrorType:
             # If you don't have permission to read this file, don't even add it
