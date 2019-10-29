cb2fd1f87089274b9a841e6a03f96a175b29781b
Jonathan Rhone <rhone.j@gmail.com>
2019-10-29 12:37:14 +0300

    black

--- e2e/scripts/vega_update.py
+++ e2e/scripts/vega_update.py
@@ -6,6 +6,6 @@ arr = np.sin(np.arange(0, 100, 0.1))
 chart = st.line_chart([0.0])
 
 N = 100
-for i in range(0, len(arr), N):     # Chunk the data
+for i in range(0, len(arr), N):  # Chunk the data
     chart.add_rows(arr[i : i + N])  # Append chunks
-    time.sleep(0.1)                 # Fake computation, so we can see the add_rows animate :)
+    time.sleep(0.1)  # Fake computation, so we can see the add_rows animate :)
