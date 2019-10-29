b49bf5e9939d3703506c7547dee2cc8852428bd8
Tim Conkling <tconkling@gmail.com>
2019-10-29 12:14:34 -0700

    De-bold exception messages

--- lib/streamlit/DeltaGenerator.py
+++ lib/streamlit/DeltaGenerator.py
@@ -128,8 +128,8 @@ def _build_duplicate_widget_message(widget_type, user_key=None):
     if user_key is not None:
         message = textwrap.dedent(
             """
-            **There are multiple identical `st.{widget_type}` widgets with
-             `key='{user_key}'`.**
+            There are multiple identical `st.{widget_type}` widgets with 
+            `key='{user_key}'`.
             
             To fix this, please make sure that the `key` argument is unique for
             each `st.{widget_type}` you create.
@@ -138,8 +138,8 @@ def _build_duplicate_widget_message(widget_type, user_key=None):
     else:
         message = textwrap.dedent(
             """
-            **There are multiple identical `st.{widget_type}` widgets with the
-            same generated key.**
+            There are multiple identical `st.{widget_type}` widgets with the
+            same generated key.
             
             (When a widget is created, it's assigned an internal key based on
             its structure. Multiple widgets with an identical structure will
