09d5735d39517b6cc09c47f355ad1634e273e9b0
arraydude <arraydude@gmail.com>
2019-11-22 17:20:45 -0300

    Adding emoji to unit test

--- e2e/specs/markdown.spec.ts
+++ e2e/specs/markdown.spec.ts
@@ -25,7 +25,7 @@ describe("st.markdown", () => {
   it("displays markdown", () => {
     cy.get(".element-container .stMarkdown").should("have.length", 8);
     cy.get(".element-container .stMarkdown").then(els => {
-      expect(els[0].textContent).to.eq("This markdown is awesome!");
+      expect(els[0].textContent).to.eq("This markdown is awesome! 😎");
       expect(els[1].textContent).to.eq("This <b>HTML tag</b> is escaped!");
       expect(els[2].textContent).to.eq("This HTML tag is not escaped!");
       expect(els[3].textContent).to.eq("[text]");
