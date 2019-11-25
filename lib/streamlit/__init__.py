860d9cf78276cc06d49d225732d9bc732ccc6b79
Jonathan Rhone <rhone.j@gmail.com>
2019-11-25 20:29:17 +0100

    black

--- lib/streamlit/__init__.py
+++ lib/streamlit/__init__.py
@@ -223,7 +223,9 @@ def set_option(key, value):
         return
 
     raise StreamlitAPIException(
-        "{key} cannot be set on the fly. Set as command line option, e.g. streamlit run script.py --{key}, or in config.toml instead.".format(key=key)
+        "{key} cannot be set on the fly. Set as command line option, e.g. streamlit run script.py --{key}, or in config.toml instead.".format(
+            key=key
+        )
     )
 
 
