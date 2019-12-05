d8d2adbc6afee30d36e3b93968dda1b70884e50c
nthmost <naomi@nthmost.com>
2019-12-05 11:47:01 -0800

    internal vars only, 1/2: don't add external vars to consts

--- cachetest.py
+++ cachetest.py
@@ -1,18 +1,41 @@
 import streamlit as st
 
-things = [1,2,3]
+
+st.title("Cache test: adding `10` to `number`")
+
+    
+
+with st.echo():
+    things = [1,2,3]
+    number = 5
+
+def called_by_cached_function():
+    return number + 2
+
+@st.cache
+def add(num):
+    resultd = {"dkey_added": number + num}
+    anothervar = num + 2
+    resultd["dkey_second_add"] = anothervar
+    resultd["dkey_number_from_called_function"] = called_by_cached_function()
+    return resultd
 
 @st.cache
 def hash_dicts(inp):
-    outd = {'things': things,
-            'a': inp}
+    outd = {"dkey_things": things,
+            "dkey_inp": inp}
     return outd
 
-st.header("iteration 1")
-st.write(hash_dicts(5))
+st.header("iteration 1: `number = 5`")
+with st.echo():
+    st.write(hash_dicts(5))
+    st.write(add(10))
 
-st.header("iteration 2: mutated `things`")
-things = [4,5,6]
+st.header("iteration 2: change `number` to `3`. Mutated `things`")
 
-st.write(hash_dicts(5))
+with st.echo():
+    things = [4,5,6]
+    number = 3
+    st.write(add(10))
+    st.write(hash_dicts(5))
 
