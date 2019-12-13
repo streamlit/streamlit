53c9567801eaf759ec32ec78f444b81c83989949
nthmost <naomi@nthmost.com>
2019-12-13 10:48:04 -0800

    removed spurious test script

--- chatbot.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-placeholder = st.empty()
-inp = st.text_input(label="chat", value="")
-
-if inp:
-    user_says = user_says + inp
-    placeholder.button(label=user_says)            # text_area(user_says)
-
