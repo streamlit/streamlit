streamlit/streamlit

commit ed62e0a37da29c17a94f9183a82ed0bda9bcda8b

Bugfix/ticker saturation2 (#785)
* improve old solution to don't lose first data

* return only for line_chart, bar_chart and area_chart

lib/streamlit/DeltaGenerator.py
@@ -116,7 +116,7 @@ def _with_element(method):
         if delta_type in DELTAS_TYPES_THAT_MELT_DATAFRAMES and len(args) > 0:
             data = args[0]
             if isinstance(data, pd.DataFrame):
-                last_index = data.index[-1] if data.index.size > 0 else 0
+                last_index = data.index[-1] if data.index.size > 0 else -1
 
         def marshall_element(element):
             return method(dg, element, *args, **kwargs)
@@ -2557,6 +2557,18 @@ class DeltaGenerator(object):
                 "Method requires exactly one dataset"
             )
 
+        # Regenerate chart with data
+        if self._last_index == -1:
+            if self._delta_type == 'line_chart':
+                self.line_chart(data)
+                return
+            elif self._delta_type == 'bar_chart':
+                self.bar_chart(data)
+                return
+            elif self._delta_type == 'area_chart':
+                self.area_chart(data)
+                return
+  
         data, self._last_index = _maybe_melt_data_for_add_rows(
             data, self._delta_type, self._last_index
         )

commit 63f6028f404e793781f500a690dd67acb47c2492

Fix hello.py add_rows issue, introduced in #785

lib/streamlit/DeltaGenerator.py
@@ -26,7 +26,6 @@ import functools
 import json
 import random
 import textwrap
-import pandas as pd
 from datetime import datetime
 from datetime import date
 from datetime import time
@@ -34,6 +33,7 @@ from datetime import time
 from streamlit import caching
 from streamlit import config
 from streamlit import metrics
+from streamlit import type_util
 from streamlit.ReportThread import get_report_ctx
 from streamlit.UploadedFileManager import UploadedFileManager
 from streamlit.errors import DuplicateWidgetID
@@ -117,12 +117,17 @@ def _with_element(method):
         caching.maybe_show_cached_st_function_warning(dg)
 
         delta_type = method.__name__
-        last_index = -1
+        last_index = None
 
         if delta_type in DELTAS_TYPES_THAT_MELT_DATAFRAMES and len(args) > 0:
             data = args[0]
-            if isinstance(data, pd.DataFrame):
-                last_index = data.index[-1] if data.index.size > 0 else -1
+            if type_util.is_dataframe_compatible(data):
+                data = type_util.convert_anything_to_df(data)
+
+                if data.index.size > 0:
+                    last_index = data.index[-1]
+                else:
+                    last_index = None
 
         def marshall_element(element):
             return method(dg, element, *args, **kwargs)
@@ -566,9 +571,7 @@ class DeltaGenerator(object):
            height: 75px
 
         """
-        from streamlit.type_util import is_sympy_expession
-
-        if is_sympy_expession(body):
+        if type_util.is_sympy_expession(body):
             import sympy
 
             body = sympy.latex(body)
@@ -2657,17 +2660,19 @@ class DeltaGenerator(object):
                 "Method requires exactly one dataset"
             )
 
-        # Regenerate chart with data
-        if self._last_index == -1:
-            if self._delta_type == "line_chart":
-                self.line_chart(data)
-                return
-            elif self._delta_type == "bar_chart":
-                self.bar_chart(data)
-                return
-            elif self._delta_type == "area_chart":
-                self.area_chart(data)
-                return
+        # When doing add_rows on an element that does not already have data
+        # (for example, st.line_chart() without any args), call the original
+        # st.foo() element with new data instead of doing an add_rows().
+        if (
+            self._delta_type in DELTAS_TYPES_THAT_MELT_DATAFRAMES
+            and self._last_index is None
+        ):
+            # IMPORTANT: This assumes delta types and st method names always
+            # match!
+            st_method_name = self._delta_type
+            st_method = getattr(self, st_method_name)
+            st_method(data, **kwargs)
+            return
 
         data, self._last_index = _maybe_melt_data_for_add_rows(
             data, self._delta_type, self._last_index
@@ -2700,7 +2705,7 @@ def _maybe_melt_data_for_add_rows(data, delta_type, last_index):
     # by vega_lite will be different and it will throw an error.
     if delta_type in DELTAS_TYPES_THAT_MELT_DATAFRAMES:
         if not isinstance(data, pd.DataFrame):
-            data = data_frame_proto.convert_anything_to_df(data)
+            data = type_util.convert_anything_to_df(data)
 
         if type(data.index) is pd.RangeIndex:
             old_step = _get_pandas_index_attr(data, "step")

lib/streamlit/__init__.py
@@ -253,14 +253,6 @@ def set_option(key, value):
 
 # Special methods:
 
-_DATAFRAME_LIKE_TYPES = (
-    "DataFrame",  # pandas.core.frame.DataFrame
-    "Index",  # pandas.core.indexes.base.Index
-    "Series",  # pandas.core.series.Series
-    "Styler",  # pandas.io.formats.style.Styler
-    "ndarray",  # numpy.ndarray
-)
-
 _HELP_TYPES = (
     _types.BuiltinFunctionType,
     _types.BuiltinMethodType,
@@ -409,7 +401,7 @@ def write(*args, **kwargs):
             # Order matters!
             if isinstance(arg, string_types):  # noqa: F821
                 string_buffer.append(arg)
-            elif type(arg).__name__ in _DATAFRAME_LIKE_TYPES:
+            elif _type_util.is_dataframe_like(arg):
                 flush_buffer()
                 if len(_np.shape(arg)) > 2:
                     text(arg)

lib/streamlit/elements/altair.py
@@ -20,7 +20,7 @@ from __future__ import absolute_import
 
 from datetime import date
 
-from streamlit.elements.data_frame_proto import convert_anything_to_df
+from streamlit import type_util
 import streamlit.elements.vega_lite as vega_lite
 import altair as alt
 import pandas as pd
@@ -56,7 +56,7 @@ def generate_chart(chart_type, data):
         data = {"": []}
 
     if not isinstance(data, pd.DataFrame):
-        data = convert_anything_to_df(data)
+        data = type_util.convert_anything_to_df(data)
 
     index_name = data.index.name
     if index_name is None:

lib/streamlit/elements/data_frame_proto.py
@@ -39,7 +39,7 @@ def marshall_data_frame(data, proto_df):
     proto_df : proto.DataFrame
         Output. The protobuf for a Streamlit DataFrame proto.
     """
-    df = convert_anything_to_df(data)
+    df = type_util.convert_anything_to_df(data)
 
     # Convert df into an iterable of columns (each of type Series).
     df_data = (df.iloc[:, col] for col in range(len(df.columns)))
@@ -51,40 +51,8 @@ def marshall_data_frame(data, proto_df):
     _marshall_index(df.columns, proto_df.columns)
     _marshall_index(df.index, proto_df.index)
 
-    styler = data if _is_pandas_styler(data) else None
+    styler = data if type_util.is_pandas_styler(data) else None
     _marshall_styles(proto_df.style, df, styler)
-
-
-def convert_anything_to_df(df):
-    """Try to convert different formats to a Pandas Dataframe.
-
-    Parameters
-    ----------
-    df : ndarray, Iterable, dict, DataFrame, Styler, None, or any
-
-    Returns
-    -------
-    pandas.DataFrame
-
-    """
-    if type_util.is_type(df, "pandas.core.frame.DataFrame"):
-        return df
-
-    if _is_pandas_styler(df):
-        return df.data
-
-    import pandas as pd
-
-    if type_util.is_type(df, "numpy.ndarray") and len(df.shape) == 0:
-        return pd.DataFrame([])
-
-    # Try to convert to pandas.DataFrame. This will raise an error is df is not
-    # compatible with the pandas.DataFrame constructor.
-    return pd.DataFrame(df)
-
-
-def _is_pandas_styler(obj):
-    return type_util.is_type(obj, "pandas.io.formats.style.Styler")
 
 
 def _marshall_styles(proto_table_style, df, styler=None):

lib/streamlit/type_util.py
@@ -23,6 +23,8 @@ setup_2_3_shims(globals())
 
 import re
 
+from streamlit import errors
+
 
 def is_type(obj, fqn_type_pattern):
     """Check type without importing expensive modules.
@@ -54,6 +56,36 @@ def is_type(obj, fqn_type_pattern):
         return fqn_type_pattern == actual_fqn
     else:
         return fqn_type_pattern.match(actual_fqn) is not None
+
+
+_PANDAS_DF_TYPE_STR = "pandas.core.frame.DataFrame"
+_PANDAS_INDEX_TYPE_STR = "pandas.core.indexes.base.Index"
+_PANDAS_SERIES_TYPE_STR = "pandas.core.series.Series"
+_PANDAS_STYLER_TYPE_STR = "pandas.io.formats.style.Styler"
+_NUMPY_ARRAY_TYPE_STR = "numpy.ndarray"
+
+_DATAFRAME_LIKE_TYPES = (
+    _PANDAS_DF_TYPE_STR,
+    _PANDAS_INDEX_TYPE_STR,
+    _PANDAS_SERIES_TYPE_STR,
+    _PANDAS_STYLER_TYPE_STR,
+    _NUMPY_ARRAY_TYPE_STR,
+)
+
+_DATAFRAME_COMPATIBLE_TYPES = (
+    dict,
+    list,
+    type(None),
+)
+
+
+def is_dataframe_like(obj):
+    return any(is_type(obj, t) for t in _DATAFRAME_LIKE_TYPES)
+
+
+def is_dataframe_compatible(obj):
+    """True if type that can be passed to convert_anything_to_df."""
+    return is_dataframe_like(obj) or type(obj) in _DATAFRAME_COMPATIBLE_TYPES
 
 
 _SYMPY_RE = re.compile(r"^sympy.*$")
@@ -155,3 +187,48 @@ def is_namedtuple(x):
     if not isinstance(f, tuple):
         return False
     return all(type(n).__name__ == "str" for n in f)
+
+
+def is_pandas_styler(obj):
+    return is_type(obj, _PANDAS_STYLER_TYPE_STR)
+
+
+def convert_anything_to_df(df):
+    """Try to convert different formats to a Pandas Dataframe.
+
+    Parameters
+    ----------
+    df : ndarray, Iterable, dict, DataFrame, Styler, None, dict, list, or any
+
+    Returns
+    -------
+    pandas.DataFrame
+
+    """
+    if is_type(df, _PANDAS_DF_TYPE_STR):
+        return df
+
+    if is_pandas_styler(df):
+        return df.data
+
+    import pandas as pd
+
+    if is_type(df, "numpy.ndarray") and len(df.shape) == 0:
+        return pd.DataFrame([])
+
+    # Try to convert to pandas.DataFrame. This will raise an error is df is not
+    # compatible with the pandas.DataFrame constructor.
+    try:
+        return pd.DataFrame(df)
+
+    except ValueError:
+        raise errors.StreamlitAPIException(
+            """
+Unable to convert object of type `%(type)s` to `pandas.DataFrame`.
+
+Offending object:
+```py
+%(object)s
+```"""
+            % {"type": type(df), "object": df,}
+        )

lib/tests/streamlit/data_frame_proto_test.py
@@ -54,17 +54,6 @@ class DataFrameProtoTest(unittest.TestCase):
         """
         pass
 
-    def test_convert_anything_to_df(self):
-        """Test streamlit.data_frame_proto.convert_anything_to_df.
-
-        Need to test dataframe is:
-        * pandas.core.frame.DataFrame
-        * pandas_styler
-        * numpy.ndaray and has a shape
-        * default
-        """
-        pass
-
     def test_is_pandas_styler(self):
         """Test streamlit.data_frame_proto._is_pandas_styler.
 

lib/tests/streamlit/dataframe_coercion_test.py
@@ -19,7 +19,7 @@
 from __future__ import print_function, division, unicode_literals, absolute_import
 from streamlit.compatibility import setup_2_3_shims
 
-from streamlit.elements.data_frame_proto import convert_anything_to_df
+from streamlit import type_util
 
 setup_2_3_shims(globals())
 
@@ -34,7 +34,7 @@ class DataFrameCoercionTest(unittest.TestCase):
         of equal-length lists
         """
         d = {"a": [1], "b": [2], "c": [3]}
-        df = convert_anything_to_df(d)
+        df = type_util.convert_anything_to_df(d)
         self.assertEqual(type(df), pd.DataFrame)
         self.assertEqual(df.shape, (1, 3))
 
@@ -43,7 +43,7 @@ class DataFrameCoercionTest(unittest.TestCase):
         from an empty numpy array.
         """
         arr = np.array([])
-        df = convert_anything_to_df(arr)
+        df = type_util.convert_anything_to_df(arr)
         self.assertEqual(type(df), pd.DataFrame)
         self.assertEqual(df.shape, (0, 1))
 
@@ -51,6 +51,6 @@ class DataFrameCoercionTest(unittest.TestCase):
         """Test that a DataFrame can be constructed from a pandas.Styler"""
         d = {"a": [1], "b": [2], "c": [3]}
         styler = pd.DataFrame(d).style.format("{:.2%}")
-        df = convert_anything_to_df(styler)
+        df = type_util.convert_anything_to_df(styler)
         self.assertEqual(type(df), pd.DataFrame)
         self.assertEqual(df.shape, (1, 3))

lib/tests/streamlit/write_test.py
@@ -25,6 +25,7 @@ import numpy as np
 import pandas as pd
 
 import streamlit as st
+from streamlit import type_util
 
 
 class StreamlitWriteTest(unittest.TestCase):
@@ -52,15 +53,19 @@ class StreamlitWriteTest(unittest.TestCase):
     def test_dataframe(self):
         """Test st.write with dataframe."""
         data = {
-            "DataFrame": pd.DataFrame([[20, 30, 50]], columns=["a", "b", "c"]),
-            "Series": pd.Series(np.array(["a", "b", "c"])),
-            "Index": pd.Index(list("abc")),
-            "ndarray": np.array(["a", "b", "c"]),
-            "Styler": pd.DataFrame({"a": [1], "b": [2]}).style.format("{:.2%}"),
+            type_util._PANDAS_DF_TYPE_STR: pd.DataFrame(
+                [[20, 30, 50]], columns=["a", "b", "c"]
+            ),
+            type_util._PANDAS_SERIES_TYPE_STR: pd.Series(np.array(["a", "b", "c"])),
+            type_util._PANDAS_INDEX_TYPE_STR: pd.Index(list("abc")),
+            type_util._PANDAS_STYLER_TYPE_STR: pd.DataFrame(
+                {"a": [1], "b": [2]}
+            ).style.format("{:.2%}"),
+            type_util._NUMPY_ARRAY_TYPE_STR: np.array(["a", "b", "c"]),
         }
 
         # Make sure we have test cases for all _DATAFRAME_LIKE_TYPES
-        self.assertEqual(sorted(data.keys()), sorted(st._DATAFRAME_LIKE_TYPES))
+        self.assertEqual(sorted(data.keys()), sorted(type_util._DATAFRAME_LIKE_TYPES))
 
         for df in data.values():
             with patch("streamlit.dataframe") as p:
@@ -92,7 +97,7 @@ class StreamlitWriteTest(unittest.TestCase):
     @patch("streamlit.type_util.is_type")
     def test_altair_chart(self, is_type):
         """Test st.write with altair_chart."""
-        is_type.return_value = True
+        is_type.side_effect = make_is_type_mock(type_util._ALTAIR_RE)
 
         class FakeChart(object):
             pass
@@ -105,7 +110,7 @@ class StreamlitWriteTest(unittest.TestCase):
     @patch("streamlit.type_util.is_type")
     def test_pyplot(self, is_type):
         """Test st.write with matplotlib."""
-        is_type.side_effect = [False, True]
+        is_type.side_effect = make_is_type_mock("matplotlib.figure.Figure")
 
         class FakePyplot(object):
             pass
@@ -194,3 +199,29 @@ class StreamlitWriteTest(unittest.TestCase):
             with st.spinner("some message"):
                 time.sleep(0.15)
             e.assert_called_once_with()
+
+
+def make_is_type_mock(true_type_matchers):
+    """Return a function that mocks is_type.
+
+    When you do this:
+    mock_is_type.side_effect = make_is_type_mock("foo.bar.Baz")
+
+    ...then when you call mock_is_type(my_type, "foo.bar.Baz") it will return
+    True (and False otherwise).
+
+    You can also pass in a tuple.
+    """
+    if type(true_type_matchers) is not tuple:
+        true_type_matchers = (true_type_matchers,)
+
+    def new_is_type(obj, type_matchers):
+        if type(type_matchers) is not tuple:
+            type_matchers = (type_matchers,)
+
+        for type_matcher in type_matchers:
+            if type_matcher in true_type_matchers:
+                return True
+        return False
+
+    return new_is_type