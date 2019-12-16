6ece1a6f14e7820509f68f5bf1b9330b85353270
nthmost <naomi@nthmost.com>
2019-12-16 14:25:44 -0800

    cosmetic

--- lib/tests/streamlit/caching_test.py
+++ lib/tests/streamlit/caching_test.py
@@ -72,7 +72,6 @@ class CacheTest(unittest.TestCase):
         import random
 
         possible_genes = ["ACVRL1", "BRCA1", "BRCA2", "ENG", "FANCA"]
-
         def random_gene():
             return random.choice(possible_genes)
 
