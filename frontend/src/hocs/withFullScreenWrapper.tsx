1a58b30786e84f4e7d8165159bbfec5b6fd69081
arraydude <arraydude@gmail.com>
2019-12-17 15:50:01 -0300

    withFullScreenWrapper hoc

---
+++ frontend/src/hocs/withFullScreenWrapper.tsx
@@ -0,0 +1,36 @@
+import React, { PureComponent, ComponentType } from "react"
+import { Map as ImmutableMap } from "immutable"
+
+import FullScreenWrapper from "components/shared/FullScreenWrapper"
+
+interface ReportElementProps {
+  width: number
+  element: ImmutableMap<string, any>
+  height?: number
+  index?: number
+}
+
+function withFullScreenWrapper(WrappedComponent: ComponentType<any>) {
+  return class ComponentWithPagination extends PureComponent<
+    ReportElementProps
+  > {
+    render() {
+      const { element, index, width } = this.props
+
+      return (
+        <FullScreenWrapper width={width}>
+          {({ width, height }) => (
+            <WrappedComponent
+              element={element}
+              index={index}
+              width={width}
+              height={height}
+            />
+          )}
+        </FullScreenWrapper>
+      )
+    }
+  }
+}
+
+export default withFullScreenWrapper
