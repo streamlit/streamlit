cfd6e310bff46c1825667709c0432201e8065853
Jonathan Rhone <rhone.j@gmail.com>
2019-12-18 17:59:08 -0500

    tests for hashing dict, iterable, user object

--- lib/tests/streamlit/hashing_test.py
+++ lib/tests/streamlit/hashing_test.py
@@ -20,6 +20,7 @@ import os
 import sys
 import tempfile
 import time
+import types
 import unittest
 
 import altair as alt
@@ -66,6 +67,42 @@ class HashTest(unittest.TestCase):
         self.assertNotEqual(get_hash((1, 2)), get_hash((2, 2)))
         self.assertNotEqual(get_hash((1,)), get_hash(1))
         self.assertNotEqual(get_hash((1,)), get_hash([1]))
+
+    def test_dict(self):
+        dict_gen = {1: (x for x in range(1))}
+
+        self.assertEqual(get_hash({1: 1}), get_hash({1: 1}))
+        self.assertNotEqual(get_hash({1: 1}), get_hash({1: 2}))
+        self.assertNotEqual(get_hash({1: 1}), get_hash([(1, 1)]))
+
+        with self.assertRaises(UnhashableType):
+            get_hash(dict_gen)
+        get_hash(dict_gen, hash_funcs={types.GeneratorType: id})
+
+    def test_reduce_(self):
+        class A(object):
+          def __init__(self):
+            self.x = [1, 2, 3]
+
+        class B(object):
+          def __init__(self):
+            self.x = [1, 2, 3]
+
+        class C(object):
+          def __init__(self):
+            self.x = (x for x in range(1))
+
+        self.assertEqual(get_hash(A()), get_hash(A()))
+        self.assertNotEqual(get_hash(A()), get_hash(B()))
+        self.assertNotEqual(get_hash(A()), get_hash(A().__reduce__()))
+
+        with self.assertRaises(UnhashableType):
+            get_hash(C())
+        get_hash(C(), hash_funcs={types.GeneratorType: id})
+
+    def test_generator(self):
+        with self.assertRaises(UnhashableType):
+            get_hash((x for x in range(1)))
 
     def test_float(self):
         self.assertEqual(get_hash(0.1), get_hash(0.1))
