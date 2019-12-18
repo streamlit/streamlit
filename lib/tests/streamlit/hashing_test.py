8e803c070bbbfe5113e6877708b41605115835d3
Jonathan Rhone <rhone.j@gmail.com>
2019-12-18 17:59:26 -0500

    black

--- lib/tests/streamlit/hashing_test.py
+++ lib/tests/streamlit/hashing_test.py
@@ -81,16 +81,16 @@ class HashTest(unittest.TestCase):
 
     def test_reduce_(self):
         class A(object):
-          def __init__(self):
-            self.x = [1, 2, 3]
+            def __init__(self):
+                self.x = [1, 2, 3]
 
         class B(object):
-          def __init__(self):
-            self.x = [1, 2, 3]
+            def __init__(self):
+                self.x = [1, 2, 3]
 
         class C(object):
-          def __init__(self):
-            self.x = (x for x in range(1))
+            def __init__(self):
+                self.x = (x for x in range(1))
 
         self.assertEqual(get_hash(A()), get_hash(A()))
         self.assertNotEqual(get_hash(A()), get_hash(B()))
