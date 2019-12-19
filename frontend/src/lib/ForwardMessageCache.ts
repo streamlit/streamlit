ca8580338d38aadb125c23e9ef38242a54f14ccb
Thiago Teixeira <thiago@streamlit.io>
2019-12-18 22:00:19 -0800

    Upgrade typescript and improve type annotations

--- frontend/src/lib/ForwardMessageCache.ts
+++ frontend/src/lib/ForwardMessageCache.ts
@@ -107,6 +107,9 @@ export class ForwardMsgCache {
     }
 
     // Copy the metadata from the refMsg into our new message
+    if (!msg.metadata) {
+      throw new Error("ForwardMsg has no metadata")
+    }
     newMsg.metadata = ForwardMsgMetadata.create(msg.metadata)
     return newMsg
   }
@@ -159,7 +162,7 @@ export class ForwardMsgCache {
       return
     }
 
-    if (!msg.metadata.cacheable) {
+    if (!msg.metadata || !msg.metadata.cacheable) {
       // Don't cache messages that the server hasn't marked as cacheable.
       return
     }
