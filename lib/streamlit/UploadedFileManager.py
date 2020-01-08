4865086bd96108bb92f74021db3d8e836882415a
arraydude <arraydude@gmail.com>
2020-01-08 15:50:47 -0300

    make headers

--- lib/streamlit/UploadedFileManager.py
+++ lib/streamlit/UploadedFileManager.py
@@ -13,7 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
 class File(object):
     """Queue that smartly accumulates the report's messages."""
 
--- lib/tests/streamlit/scriptrunner/test_data/good_script_no_encoding.py
+++ lib/tests/streamlit/scriptrunner/test_data/good_script_no_encoding.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # Copyright 2018-2020 Streamlit Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
