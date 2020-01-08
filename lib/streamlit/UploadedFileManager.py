29109660f54fc297fd2d0583c1a978a75e9a4eed
arraydude <arraydude@gmail.com>
2020-01-08 18:50:30 -0300

    make format

--- lib/streamlit/UploadedFileManager.py
+++ lib/streamlit/UploadedFileManager.py
@@ -13,6 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+
 class File(object):
     """Queue that smartly accumulates the report's messages."""
 
