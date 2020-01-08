5fcb9d4d3af2467de66e2f82d10732742854a028
arraydude <arraydude@gmail.com>
2020-01-08 15:47:23 -0300

    Simplifying import

---
+++ frontend/src/hocs/withScreencast/components/index.tsx
@@ -0,0 +1,6 @@
+// Dialogs
+export { default as ScreencastDialog } from "./ScreencastDialog"
+export { default as VideoRecordedDialog } from "./VideoRecordedDialog"
+export {
+  default as UnsupportedBrowserDialog,
+} from "./UnsupportedBrowserDialog"
