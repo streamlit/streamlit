a74d0dcd8b8843f62acf1358f279b6335c144a84
Tim Conkling <tconkling@gmail.com>
2020-01-13 11:27:23 -0800

    test

--- examples/test.py
+++ examples/test.py
@@ -1,32 +1,14 @@
-import sys
-
 import streamlit as st
+from streamlit.js_number import JSNumber
 
-MAX_INT64 = int((1 << 63) - 1)
-MIN_INT64 = int(-MAX_INT64)
+value = st.slider("Slider: int54", min_value=JSNumber.MIN_SAFE_INTEGER, max_value=JSNumber.MAX_SAFE_INTEGER)
+st.write(f"[value={value}")
 
-MAX_INT54 = int((1 << 53) - 1)
-MIN_INT54 = int(-MAX_INT54)
+value = st.slider("Slider: float", min_value=JSNumber.MIN_NEGATIVE_VALUE, max_value=JSNumber.MAX_VALUE)
+st.write(f"[value={value}")
 
-MAX_FLOAT = sys.float_info.max
-# MAX_FLOAT = 9e+296
-MIN_FLOAT = -MAX_FLOAT
-# MAX_FLOAT = float(MAX_INT)
+value = st.number_input("Input: int54", min_value=JSNumber.MIN_SAFE_INTEGER, max_value=JSNumber.MAX_SAFE_INTEGER)
+st.write(f"[value={value}")
 
-value = st.slider("Slider: int54", min_value=MIN_INT54, max_value=MAX_INT54)
-st.write(f"[value={value}, min={MIN_INT54}, max={MAX_INT54}]")
-
-value = st.slider("Slider: int64", min_value=MIN_INT64, max_value=MAX_INT64)
-st.write(f"[value={value}, min={MIN_INT64}, max={MAX_INT64}]")
-
-value = st.slider("Slider: float", min_value=MIN_FLOAT, max_value=MAX_FLOAT)
-st.write(f"[value={value}, min={MIN_FLOAT}, max={MAX_FLOAT}]")
-
-value = st.number_input("Input: int54", min_value=MIN_INT54, max_value=MAX_INT54 + 10)
-st.write(f"[value={value}, min={MIN_INT54}, max={MAX_INT54}]")
-
-value = st.number_input("Input: int64", min_value=MIN_INT64, max_value=MAX_INT64)
-st.write(f"[value={value}, min={MIN_INT64}, max={MAX_INT64}]")
-
-value = st.number_input("Input: float", min_value=MIN_FLOAT, max_value=MAX_FLOAT)
-st.write(f"[value={value}, min={MIN_FLOAT}, max={MAX_FLOAT}]")
+value = st.number_input("Input: float", min_value=JSNumber.MIN_NEGATIVE_VALUE, max_value=JSNumber.MAX_VALUE)
+st.write(f"[value={value}")
