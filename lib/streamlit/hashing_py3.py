527422cbd471ff4a6af4d784feedcb157b05466c
Jonathan Rhone <rhone.j@gmail.com>
2020-02-06 16:23:01 -0300

    wip add filename and lineno to user hash error message

--- lib/streamlit/hashing_py3.py
+++ lib/streamlit/hashing_py3.py
@@ -16,10 +16,13 @@
 """A hashing utility for code that uses Python 3 specific code so it needs to be
 conditionally imported."""
 
+import ast
 import dis
 import importlib
 import re
 import textwrap
+
+import astor
 
 from streamlit import source_util as _source_util
 from streamlit.errors import UserHashError
@@ -34,26 +37,46 @@ def _clean_text(text):
     return textwrap.dedent(str(text)).strip()
 
 
-# TODO update message with https://github.com/streamlit/streamlit/pull/839/files
-def _hashing_user_error_message(exc, lines):
+def _hashing_user_error_message(exc, lines, filename, lineno):
     return textwrap.dedent(
         """
         %(exception)s
 
-        ```%(lines)s```
+        Error in `%(filename)s` near line `%(lineno)s`:
 
-        Usually this means there is an error in your code.
+        ```
+        %(lines)s
+        ```
 
         If you think this is actually a Streamlit bug, please [file a bug report here.]
         (https://github.com/streamlit/streamlit/issues/new/choose)
     """
-        % {"exception": str(exc), "lines": _clean_text(lines)}
+        % {"exception": str(exc), "lines": _clean_text(lines), "filename": filename, "lineno": lineno}
     ).strip("\n")
+
+
+def _get_failing_lines(code, lineno):
+    lines = []
+
+    with _source_util.open_python_file(code.co_filename) as source_file:
+        source_lines = source_file.readlines()
+        lines.append(source_lines[lineno-1])
+        initial_spaces = SPACES_RE.match(lines[0]).end()
+
+        # Get the lines below the start line where the indent
+        # is >= to the start line. Do not allow new lines.
+        for line in source_lines[lineno:]:
+            indentation = SPACES_RE.match(line).end()
+
+            if indentation < initial_spaces:
+                break
+            lines.append(line)
+    return lines
 
 
 def get_referenced_objects(code, context):
     tos = None  # top of the stack
-
+    lineno = None
     refs = []
 
     def set_tos(t):
@@ -63,6 +86,45 @@ def get_referenced_objects(code, context):
             refs.append(tos)
         tos = t
 
+    def handle_operation(op):
+        nonlocal tos
+        nonlocal lineno
+        if op.starts_line is not None:
+            lineno = op.starts_line
+
+        if op.opname in ["LOAD_GLOBAL", "LOAD_NAME"]:
+            if op.argval in context.globals:
+                set_tos(context.globals[op.argval])
+            else:
+                set_tos(op.argval)
+        elif op.opname in ["LOAD_DEREF", "LOAD_CLOSURE"]:
+            set_tos(context.cells[op.argval])
+        elif op.opname == "IMPORT_NAME":
+            try:
+                set_tos(importlib.import_module(op.argval))
+            except ImportError:
+                set_tos(op.argval)
+        elif op.opname in ["LOAD_METHOD", "LOAD_ATTR", "IMPORT_FROM"]:
+            if tos is None:
+                refs.append(op.argval)
+            elif isinstance(tos, str):
+                tos += "." + op.argval
+            else:
+                tos = getattr(tos, op.argval)
+        elif op.opname == "DELETE_FAST" and tos:
+            del context.varnames[op.argval]
+            tos = None
+        elif op.opname == "STORE_FAST" and tos:
+            context.varnames[op.argval] = tos
+            tos = None
+        elif op.opname == "LOAD_FAST" and op.argval in context.varnames:
+            set_tos(context.varnames[op.argval])
+        else:
+            # For all other instructions, hash the current TOS.
+            if tos is not None:
+                refs.append(tos)
+                tos = None
+
     # Our goal is to find referenced objects. The problem is that co_names
     # does not have full qualified names in it. So if you access `foo.bar`,
     # co_names has `foo` and `bar` in it but it doesn't tell us that the
@@ -70,76 +132,20 @@ def get_referenced_objects(code, context):
     # from which object an attribute is requested.
     # Read more about bytecode at https://docs.python.org/3/library/dis.html
 
-    lineno = None
-
     for op in dis.get_instructions(code):
         try:
-            if op.starts_line is not None:
-                lineno = op.starts_line
-
-            if op.opname in ["LOAD_GLOBAL", "LOAD_NAME"]:
-                if op.argval in context.globals:
-                    set_tos(context.globals[op.argval])
-                else:
-                    set_tos(op.argval)
-            elif op.opname in ["LOAD_DEREF", "LOAD_CLOSURE"]:
-                set_tos(context.cells[op.argval])
-            elif op.opname == "IMPORT_NAME":
-                try:
-                    set_tos(importlib.import_module(op.argval))
-                except ImportError:
-                    set_tos(op.argval)
-            elif op.opname in ["LOAD_METHOD", "LOAD_ATTR", "IMPORT_FROM"]:
-                if tos is None:
-                    refs.append(op.argval)
-                elif isinstance(tos, str):
-                    tos += "." + op.argval
-                else:
-                    tos = getattr(tos, op.argval)
-            elif op.opname == "DELETE_FAST" and tos:
-                del context.varnames[op.argval]
-                tos = None
-            elif op.opname == "STORE_FAST" and tos:
-                context.varnames[op.argval] = tos
-                tos = None
-            elif op.opname == "LOAD_FAST" and op.argval in context.varnames:
-                set_tos(context.varnames[op.argval])
-            else:
-                # For all other instructions, hash the current TOS.
-                if tos is not None:
-                    refs.append(tos)
-                    tos = None
+            handle_operation(op)
         except Exception as e:
-            import ast
-            import astor
-
-            filename = code.co_filename
-            start_line = lineno - 1
-            end_line = lineno
-            lines = []
-
-            with _source_util.open_python_file(filename) as source_file:
-                source_lines = source_file.readlines()
-                lines.extend(source_lines[start_line:end_line])
-                initial_spaces = SPACES_RE.match(lines[0]).end()
-
-                # Get the lines below the start line where the indent
-                # is >= to the start line. Do not allow new lines
-                for line in source_lines[end_line:]:
-                    indentation = SPACES_RE.match(line).end()
-
-                    if indentation < initial_spaces:
-                        break
-                    lines.append(line)
+            lines = _get_failing_lines(code, lineno)
 
             try:
                 parsed_context = ast.parse("".join(lines).lstrip())
-                copy_code = astor.to_source(parsed_context)
+                broken_code = astor.to_source(parsed_context)
             except:
                 LOGGER.debug("AST could not parse user code: %s" % lines)
-                copy_code = "Could not parse code"
+                broken_code = "Could not parse code"
 
-            msg = _hashing_user_error_message(e, copy_code)
+            msg = _hashing_user_error_message(e, broken_code, code.co_filename, lineno)
             raise UserHashError(msg).with_traceback(e.__traceback__)
 
     return refs
