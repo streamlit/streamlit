15f4870d740558e9f42cd12ef0da59fa2e945816
Nate White <nate@streamlit.io>
2020-02-10 11:11:52 -0800

    Fix an incomplete copy for a dict that's then modified.
    New test fails before the change, and passes afterwards.

--- lib/tests/streamlit/map_test.py
+++ lib/tests/streamlit/map_test.py
@@ -52,6 +52,13 @@ class StMapTest(testutil.DeltaGeneratorTestCase):
         self.assertEqual(c.get("initialViewState").get("pitch"), 0)
         self.assertEqual(c.get("layers")[0].get("@@type"), "ScatterplotLayer")
 
+    def test_default_map_copy(self):
+        """Test that _DEFAULT_MAP is not modified as other work occurs."""
+        self.assertEqual(_DEFAULT_MAP["initialViewState"]["latitude"], 0)
+
+        st.map(df1)
+        self.assertEqual(_DEFAULT_MAP["initialViewState"]["latitude"], 0)
+
     def test_map_leak(self):
         """Test that maps don't stay in memory when you create a new blank one.
 
