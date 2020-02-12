df27eaa965c6465fc4f5150602a22ab09a8aa97a
Jonathan Rhone <rhone.j@gmail.com>
2020-02-11 22:47:02 -0300

    get rid of side effecty function and move it inside the try block

--- lib/streamlit/hashing_py3.py
+++ lib/streamlit/hashing_py3.py
@@ -78,49 +78,6 @@ def get_referenced_objects(code, context):
             refs.append(tos)
         tos = t
 
-    def handle_operation(op):
-        nonlocal tos
-        nonlocal lineno
-
-        # Sometimes starts_line is None, in which case let's just remember the
-        # previous start_line (if any). This way when there's an exception we at
-        # least can point users somewhat near the line where the error stems from.
-        if op.starts_line is not None:
-            lineno = op.starts_line
-
-        if op.opname in ["LOAD_GLOBAL", "LOAD_NAME"]:
-            if op.argval in context.globals:
-                set_tos(context.globals[op.argval])
-            else:
-                set_tos(op.argval)
-        elif op.opname in ["LOAD_DEREF", "LOAD_CLOSURE"]:
-            set_tos(context.cells[op.argval])
-        elif op.opname == "IMPORT_NAME":
-            try:
-                set_tos(importlib.import_module(op.argval))
-            except ImportError:
-                set_tos(op.argval)
-        elif op.opname in ["LOAD_METHOD", "LOAD_ATTR", "IMPORT_FROM"]:
-            if tos is None:
-                refs.append(op.argval)
-            elif isinstance(tos, str):
-                tos += "." + op.argval
-            else:
-                tos = getattr(tos, op.argval)
-        elif op.opname == "DELETE_FAST" and tos:
-            del context.varnames[op.argval]
-            tos = None
-        elif op.opname == "STORE_FAST" and tos:
-            context.varnames[op.argval] = tos
-            tos = None
-        elif op.opname == "LOAD_FAST" and op.argval in context.varnames:
-            set_tos(context.varnames[op.argval])
-        else:
-            # For all other instructions, hash the current TOS.
-            if tos is not None:
-                refs.append(tos)
-                tos = None
-
     # Our goal is to find referenced objects. The problem is that co_names
     # does not have full qualified names in it. So if you access `foo.bar`,
     # co_names has `foo` and `bar` in it but it doesn't tell us that the
@@ -130,7 +87,44 @@ def get_referenced_objects(code, context):
 
     for op in dis.get_instructions(code):
         try:
-            handle_operation(op)
+            # Sometimes starts_line is None, in which case let's just remember the
+            # previous start_line (if any). This way when there's an exception we at
+            # least can point users somewhat near the line where the error stems from.
+            if op.starts_line is not None:
+                lineno = op.starts_line
+
+            if op.opname in ["LOAD_GLOBAL", "LOAD_NAME"]:
+                if op.argval in context.globals:
+                    set_tos(context.globals[op.argval])
+                else:
+                    set_tos(op.argval)
+            elif op.opname in ["LOAD_DEREF", "LOAD_CLOSURE"]:
+                set_tos(context.cells[op.argval])
+            elif op.opname == "IMPORT_NAME":
+                try:
+                    set_tos(importlib.import_module(op.argval))
+                except ImportError:
+                    set_tos(op.argval)
+            elif op.opname in ["LOAD_METHOD", "LOAD_ATTR", "IMPORT_FROM"]:
+                if tos is None:
+                    refs.append(op.argval)
+                elif isinstance(tos, str):
+                    tos += "." + op.argval
+                else:
+                    tos = getattr(tos, op.argval)
+            elif op.opname == "DELETE_FAST" and tos:
+                del context.varnames[op.argval]
+                tos = None
+            elif op.opname == "STORE_FAST" and tos:
+                context.varnames[op.argval] = tos
+                tos = None
+            elif op.opname == "LOAD_FAST" and op.argval in context.varnames:
+                set_tos(context.varnames[op.argval])
+            else:
+                # For all other instructions, hash the current TOS.
+                if tos is not None:
+                    refs.append(tos)
+                    tos = None
         except Exception as e:
             lines = _get_failing_lines(code, lineno)
 
