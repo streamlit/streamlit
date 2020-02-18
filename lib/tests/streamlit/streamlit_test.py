51e29e51b2901fc284ef360904485332378fd3a3
nthmost <naomi@nthmost.com>
2020-02-18 15:54:29 -0800

    linting

--- lib/tests/streamlit/streamlit_test.py
+++ lib/tests/streamlit/streamlit_test.py
@@ -389,12 +389,11 @@ class StreamlitAPITest(testutil.DeltaGeneratorTestCase):
         el = self.get_delta_from_queue().new_element
         self.assertEqual(el.json.body, '{"some": "json"}')
 
-        # Test that an object containing non-json-friendly keys can still 
+        # Test that an object containing non-json-friendly keys can still
         # be displayed.  Resultant json body will be missing those keys.
 
         n = np.array([1, 2, 3, 4, 5])
-        data = {n[0]: "this key will not render as JSON",
-                "array": n}
+        data = {n[0]: "this key will not render as JSON", "array": n}
         st.json(data)
 
         el = self.get_delta_from_queue().new_element
