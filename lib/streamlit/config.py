b162787ebd9b9a725d188849d69db656cf0207ed
ohdearaugustin <ohdearaugustin@users.noreply.github.com>
2020-02-22 01:48:57 +0100

    Ability to bind to a server address with server.address config option (#1107)
    * Fix server address bind
    
    * Fix missing commma
    
    * fix docstrings for server.address and server.port
    
    * Fix URL auto-opened by browser.
    
    Co-authored-by: Thiago Teixeira <thiagot@gmail.com>

--- lib/streamlit/config.py
+++ lib/streamlit/config.py
@@ -440,9 +440,21 @@ def _server_run_on_save():
     return False
 
 
+@_create_option("server.address")
+def _server_address():
+    """The address where the server will listen for client and browser
+    connections. Use this if you want to bind the server to a specific address.
+    If set, the server will only be accessible from this address, and not from
+    any aliases (like localhost).
+
+    Default: (unset)
+    """
+    return None
+
+
 @_create_option("server.port", type_=int)
 def _server_port():
-    """The port where the server will listen for client and browser
+    """The port where the server will listen for browser
     connections.
 
     Default: 8501
