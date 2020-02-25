0169edb0a49f53258dc75e9d35d6ea82f644304b
arraydude <arraydude@gmail.com>
2020-02-25 14:02:42 -0300

    Table test WIP

---
+++ frontend/src/components/elements/Table/mock.ts
@@ -0,0 +1,392 @@
+export default {
+  data: {
+    cols: [
+      {
+        int64s: { data: [0, 10, 20, 30, 40, 50, 60, 70, 80, 90] },
+        type: "int64s",
+      },
+      {
+        int64s: { data: [1, 11, 21, 31, 41, 51, 61, 71, 81, 91] },
+        type: "int64s",
+      },
+      {
+        int64s: { data: [2, 12, 22, 32, 42, 52, 62, 72, 82, 92] },
+        type: "int64s",
+      },
+      {
+        int64s: { data: [3, 13, 23, 33, 43, 53, 63, 73, 83, 93] },
+        type: "int64s",
+      },
+      {
+        int64s: { data: [4, 14, 24, 34, 44, 54, 64, 74, 84, 94] },
+        type: "int64s",
+      },
+      {
+        int64s: { data: [5, 15, 25, 35, 45, 55, 65, 75, 85, 95] },
+        type: "int64s",
+      },
+      {
+        int64s: { data: [6, 16, 26, 36, 46, 56, 66, 76, 86, 96] },
+        type: "int64s",
+      },
+      {
+        int64s: { data: [7, 17, 27, 37, 47, 57, 67, 77, 87, 97] },
+        type: "int64s",
+      },
+      {
+        int64s: { data: [8, 18, 28, 38, 48, 58, 68, 78, 88, 98] },
+        type: "int64s",
+      },
+      {
+        int64s: { data: [9, 19, 29, 39, 49, 59, 69, 79, 89, 99] },
+        type: "int64s",
+      },
+    ],
+  },
+  index: { rangeIndex: { start: 0, stop: 10 }, type: "rangeIndex" },
+  columns: { rangeIndex: { start: 0, stop: 10 }, type: "rangeIndex" },
+  style: {
+    cols: [
+      {
+        styles: [
+          { css: [], displayValue: "", hasDisplayValue: false },
+          {
+            css: [],
+            displayValue: "",
+            hasDisplayValue: false,
+          },
+          { css: [], displayValue: "", hasDisplayValue: false },
+          {
+            css: [],
+            displayValue: "",
+            hasDisplayValue: false,
+          },
+          { css: [], displayValue: "", hasDisplayValue: false },
+          {
+            css: [],
+            displayValue: "",
+            hasDisplayValue: false,
+          },
+          { css: [], displayValue: "", hasDisplayValue: false },
+          {
+            css: [],
+            displayValue: "",
+            hasDisplayValue: false,
+          },
+          { css: [], displayValue: "", hasDisplayValue: false },
+          {
+            css: [],
+            displayValue: "",
+            hasDisplayValue: false,
+          },
+        ],
+      },
+      {
+        styles: [
+          { css: [], displayValue: "", hasDisplayValue: false },
+          {
+            css: [],
+            displayValue: "",
+            hasDisplayValue: false,
+          },
+          { css: [], displayValue: "", hasDisplayValue: false },
+          {
+            css: [],
+            displayValue: "",
+            hasDisplayValue: false,
+          },
+          { css: [], displayValue: "", hasDisplayValue: false },
+          {
+            css: [],
+            displayValue: "",
+            hasDisplayValue: false,
+          },
+          { css: [], displayValue: "", hasDisplayValue: false },
+          {
+            css: [],
+            displayValue: "",
+            hasDisplayValue: false,
+          },
+          { css: [], displayValue: "", hasDisplayValue: false },
+          {
+            css: [],
+            displayValue: "",
+            hasDisplayValue: false,
+          },
+        ],
+      },
+      {
+        styles: [
+          { css: [], displayValue: "", hasDisplayValue: false },
+          {
+            css: [],
+            displayValue: "",
+            hasDisplayValue: false,
+          },
+          { css: [], displayValue: "", hasDisplayValue: false },
+          {
+            css: [],
+            displayValue: "",
+            hasDisplayValue: false,
+          },
+          { css: [], displayValue: "", hasDisplayValue: false },
+          {
+            css: [],
+            displayValue: "",
+            hasDisplayValue: false,
+          },
+          { css: [], displayValue: "", hasDisplayValue: false },
+          {
+            css: [],
+            displayValue: "",
+            hasDisplayValue: false,
+          },
+          { css: [], displayValue: "", hasDisplayValue: false },
+          {
+            css: [],
+            displayValue: "",
+            hasDisplayValue: false,
+          },
+        ],
+      },
+      {
+        styles: [
+          { css: [], displayValue: "", hasDisplayValue: false },
+          {
+            css: [],
+            displayValue: "",
+            hasDisplayValue: false,
+          },
+          { css: [], displayValue: "", hasDisplayValue: false },
+          {
+            css: [],
+            displayValue: "",
+            hasDisplayValue: false,
+          },
+          { css: [], displayValue: "", hasDisplayValue: false },
+          {
+            css: [],
+            displayValue: "",
+            hasDisplayValue: false,
+          },
+          { css: [], displayValue: "", hasDisplayValue: false },
+          {
+            css: [],
+            displayValue: "",
+            hasDisplayValue: false,
+          },
+          { css: [], displayValue: "", hasDisplayValue: false },
+          {
+            css: [],
+            displayValue: "",
+            hasDisplayValue: false,
+          },
+        ],
+      },
+      {
+        styles: [
+          { css: [], displayValue: "", hasDisplayValue: false },
+          {
+            css: [],
+            displayValue: "",
+            hasDisplayValue: false,
+          },
+          { css: [], displayValue: "", hasDisplayValue: false },
+          {
+            css: [],
+            displayValue: "",
+            hasDisplayValue: false,
+          },
+          { css: [], displayValue: "", hasDisplayValue: false },
+          {
+            css: [],
+            displayValue: "",
+            hasDisplayValue: false,
+          },
+          { css: [], displayValue: "", hasDisplayValue: false },
+          {
+            css: [],
+            displayValue: "",
+            hasDisplayValue: false,
+          },
+          { css: [], displayValue: "", hasDisplayValue: false },
+          {
+            css: [],
+            displayValue: "",
+            hasDisplayValue: false,
+          },
+        ],
+      },
+      {
+        styles: [
+          { css: [], displayValue: "", hasDisplayValue: false },
+          {
+            css: [],
+            displayValue: "",
+            hasDisplayValue: false,
+          },
+          { css: [], displayValue: "", hasDisplayValue: false },
+          {
+            css: [],
+            displayValue: "",
+            hasDisplayValue: false,
+          },
+          { css: [], displayValue: "", hasDisplayValue: false },
+          {
+            css: [],
+            displayValue: "",
+            hasDisplayValue: false,
+          },
+          { css: [], displayValue: "", hasDisplayValue: false },
+          {
+            css: [],
+            displayValue: "",
+            hasDisplayValue: false,
+          },
+          { css: [], displayValue: "", hasDisplayValue: false },
+          {
+            css: [],
+            displayValue: "",
+            hasDisplayValue: false,
+          },
+        ],
+      },
+      {
+        styles: [
+          { css: [], displayValue: "", hasDisplayValue: false },
+          {
+            css: [],
+            displayValue: "",
+            hasDisplayValue: false,
+          },
+          { css: [], displayValue: "", hasDisplayValue: false },
+          {
+            css: [],
+            displayValue: "",
+            hasDisplayValue: false,
+          },
+          { css: [], displayValue: "", hasDisplayValue: false },
+          {
+            css: [],
+            displayValue: "",
+            hasDisplayValue: false,
+          },
+          { css: [], displayValue: "", hasDisplayValue: false },
+          {
+            css: [],
+            displayValue: "",
+            hasDisplayValue: false,
+          },
+          { css: [], displayValue: "", hasDisplayValue: false },
+          {
+            css: [],
+            displayValue: "",
+            hasDisplayValue: false,
+          },
+        ],
+      },
+      {
+        styles: [
+          { css: [], displayValue: "", hasDisplayValue: false },
+          {
+            css: [],
+            displayValue: "",
+            hasDisplayValue: false,
+          },
+          { css: [], displayValue: "", hasDisplayValue: false },
+          {
+            css: [],
+            displayValue: "",
+            hasDisplayValue: false,
+          },
+          { css: [], displayValue: "", hasDisplayValue: false },
+          {
+            css: [],
+            displayValue: "",
+            hasDisplayValue: false,
+          },
+          { css: [], displayValue: "", hasDisplayValue: false },
+          {
+            css: [],
+            displayValue: "",
+            hasDisplayValue: false,
+          },
+          { css: [], displayValue: "", hasDisplayValue: false },
+          {
+            css: [],
+            displayValue: "",
+            hasDisplayValue: false,
+          },
+        ],
+      },
+      {
+        styles: [
+          { css: [], displayValue: "", hasDisplayValue: false },
+          {
+            css: [],
+            displayValue: "",
+            hasDisplayValue: false,
+          },
+          { css: [], displayValue: "", hasDisplayValue: false },
+          {
+            css: [],
+            displayValue: "",
+            hasDisplayValue: false,
+          },
+          { css: [], displayValue: "", hasDisplayValue: false },
+          {
+            css: [],
+            displayValue: "",
+            hasDisplayValue: false,
+          },
+          { css: [], displayValue: "", hasDisplayValue: false },
+          {
+            css: [],
+            displayValue: "",
+            hasDisplayValue: false,
+          },
+          { css: [], displayValue: "", hasDisplayValue: false },
+          {
+            css: [],
+            displayValue: "",
+            hasDisplayValue: false,
+          },
+        ],
+      },
+      {
+        styles: [
+          { css: [], displayValue: "", hasDisplayValue: false },
+          {
+            css: [],
+            displayValue: "",
+            hasDisplayValue: false,
+          },
+          { css: [], displayValue: "", hasDisplayValue: false },
+          {
+            css: [],
+            displayValue: "",
+            hasDisplayValue: false,
+          },
+          { css: [], displayValue: "", hasDisplayValue: false },
+          {
+            css: [],
+            displayValue: "",
+            hasDisplayValue: false,
+          },
+          { css: [], displayValue: "", hasDisplayValue: false },
+          {
+            css: [],
+            displayValue: "",
+            hasDisplayValue: false,
+          },
+          { css: [], displayValue: "", hasDisplayValue: false },
+          {
+            css: [],
+            displayValue: "",
+            hasDisplayValue: false,
+          },
+        ],
+      },
+    ],
+  },
+}
