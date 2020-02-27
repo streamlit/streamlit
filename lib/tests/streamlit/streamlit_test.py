66e316b58d4782d5c919c33bfd71e84dcdd075b1
Jonathan Rhone <rhone.j@gmail.com>
2020-02-27 09:53:26 -0300

    fix package version tests and comparisons

--- lib/tests/streamlit/streamlit_test.py
+++ lib/tests/streamlit/streamlit_test.py
@@ -57,7 +57,7 @@ class StreamlitTest(unittest.TestCase):
 
     # TODO re-enable after fixing version bug
     # AssertionError: '0.56.1' != '0.56.2.dev20200225'
-    def _test_streamlit_version(self):
+    def test_streamlit_version(self):
         """Test streamlit.__version__."""
         self.assertEqual(__version__, get_version())
 
