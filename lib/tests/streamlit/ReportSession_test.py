7c927289ea6b5124f7e07062e66762d52c24a79c
nthmost <naomi@nthmost.com>
2020-02-27 15:39:08 -0800

    all tests pass in python 3.7

--- lib/tests/streamlit/ReportSession_test.py
+++ lib/tests/streamlit/ReportSession_test.py
@@ -148,7 +148,7 @@ class ReportSessionSerializationTest(tornado.testing.AsyncTestCase):
         rs._report.report_id = "TestReportID"
 
         orig_ctx = get_report_ctx()
-        ctx = ReportContext(rs._report.enqueue, None, None, None, 0)
+        ctx = ReportContext("test session", rs._report.enqueue, None, None, None)
         add_report_ctx(ctx=ctx)
 
         rs._scriptrunner = MagicMock()
