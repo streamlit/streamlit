fbe1d2be10c74872fd5076fc7f4dd4a1697f3632
Jonathan Rhone <rhone.j@gmail.com>
2020-03-02 14:02:30 -0300

    Pypi nightly (#1173)

--- frontend/craco.config.js
+++ frontend/craco.config.js
@@ -9,9 +9,7 @@ module.exports = {
       // HardSourceWebpackPlugin adds aggressive build caching
       // to speed up our slow builds.
       // https://github.com/mzgoddard/hard-source-webpack-plugin
-      // TODO permanently disable this on circleci builds?
-      // TODO try with it enabled after memory issues are resolved
-      // webpackConfig.plugins.unshift(new HardSourceWebpackPlugin())
+      webpackConfig.plugins.unshift(new HardSourceWebpackPlugin())
 
       return webpackConfig
     },
