streamlit/streamlit

commit 202cd80bad8c8eb51d3e75d3923b225b0092c882

st.cache expiration (#1152)
Adds `ttl` and `max_entries` params to `@st.cache`

- Each `@st.cache`d function now has its own in-memory cache (previously, all functions shared a single cache).
- `cachetools.LRUCache` and `cachetools.TTLCache` replace raw dictionaries as the cache implementations.

This closes #364. (Well, partly: per discussions with @tvst and @treuille, the other bits of that ticket (`global` and `finalizer_func` options) are going to wait for more discussion about caching changes.)

lib/streamlit/caching.py
@@ -19,15 +19,18 @@ import ast
 import contextlib
 import hashlib
 import inspect
+import math
 import os
 import pickle
 import shutil
 import struct
 import textwrap
 import threading
-import types
+import time
 from collections import namedtuple
-from typing import Any, Dict, List
+from typing import Any, Dict
+
+from cachetools import LRUCache, TTLCache
 
 import streamlit as st
 from streamlit.util import functools_wraps
@@ -53,6 +56,12 @@ to suppress the warning.
 
 LOGGER = get_logger(__name__)
 
+# The timer function we use with TTLCache.
+TTLCACHE_TIMER = time.monotonic
+
+# A list of all mem-caches we've created.
+_all_mem_caches = []
+
 
 class CacheError(Exception):
     pass
@@ -69,10 +78,6 @@ class CachedObjectWasMutatedError(ValueError):
 
 CacheEntry = namedtuple("CacheEntry", ["value", "hash"])
 DiskCacheEntry = namedtuple("DiskCacheEntry", ["value"])
-
-
-# The in memory cache.
-_mem_cache = {}  # type: Dict[str, CacheEntry]
 
 
 # A thread-local counter that's incremented when we enter @st.cache
@@ -201,9 +206,37 @@ def _get_mutated_output_error_message():
     return message
 
 
-def _read_from_mem_cache(key, allow_output_mutation, hash_funcs):
-    if key in _mem_cache:
-        entry = _mem_cache[key]
+def _create_mem_cache(max_entries, ttl):
+    """Create an in-memory cache object with the given parameters."""
+    if max_entries is None and ttl is None:
+        # If we have no max_entries or TTL, we can just use a regular dict.
+        mem_cache = {}
+    else:
+        if max_entries is None:
+            max_entries = math.inf
+        elif not isinstance(max_entries, int):
+            raise Exception("`max_entries` must be an int or None")
+
+        if not isinstance(ttl, (float, int)) and ttl is not None:
+            raise Exception("`ttl` must be a float or None")
+
+        # If ttl is none, just create an LRUCache. (TTLCache is simply an
+        # LRUCache that adds a ttl option.)
+        if ttl is None:
+            mem_cache = LRUCache(maxsize=max_entries)
+        else:
+            mem_cache = TTLCache(maxsize=max_entries, ttl=ttl, timer=TTLCACHE_TIMER)
+
+    # Stick the new cache in our global list
+    global _all_mem_caches
+    _all_mem_caches.append(mem_cache)
+
+    return mem_cache
+
+
+def _read_from_mem_cache(mem_cache, key, allow_output_mutation, hash_funcs):
+    if key in mem_cache:
+        entry = mem_cache[key]
 
         if (
             allow_output_mutation
@@ -219,13 +252,13 @@ def _read_from_mem_cache(key, allow_output_mutation, hash_funcs):
         raise CacheKeyNotFoundError("Key not found in mem cache")
 
 
-def _write_to_mem_cache(key, value, allow_output_mutation, hash_funcs):
+def _write_to_mem_cache(mem_cache, key, value, allow_output_mutation, hash_funcs):
     if allow_output_mutation:
         hash = None
     else:
         hash = get_hash(value, hash_funcs=hash_funcs)
 
-    _mem_cache[key] = CacheEntry(value=value, hash=hash)
+    mem_cache[key] = CacheEntry(value=value, hash=hash)
 
 
 def _read_from_disk_cache(key):
@@ -263,9 +296,7 @@ def _write_to_disk_cache(key, value):
         raise CacheError("Unable to write to cache: %s" % e)
 
 
-def _read_from_cache(
-    key, persisted, allow_output_mutation, func_or_code, hash_funcs=None
-):
+def _read_from_cache(mem_cache, key, persisted, allow_output_mutation, hash_funcs=None):
     """
     Read the value from the cache. Our goal is to read from memory
     if possible. If the data was mutated (hash changed), we show a
@@ -273,20 +304,24 @@ def _read_from_cache(
     or rerun the code.
     """
     try:
-        return _read_from_mem_cache(key, allow_output_mutation, hash_funcs)
+        return _read_from_mem_cache(mem_cache, key, allow_output_mutation, hash_funcs)
     except CachedObjectWasMutatedError as e:
         st.warning(_get_mutated_output_error_message())
         return e.cached_value
     except CacheKeyNotFoundError as e:
         if persisted:
             value = _read_from_disk_cache(key)
-            _write_to_mem_cache(key, value, allow_output_mutation, hash_funcs)
+            _write_to_mem_cache(
+                mem_cache, key, value, allow_output_mutation, hash_funcs
+            )
             return value
         raise e
 
 
-def _write_to_cache(key, value, persist, allow_output_mutation, hash_funcs=None):
-    _write_to_mem_cache(key, value, allow_output_mutation, hash_funcs)
+def _write_to_cache(
+    mem_cache, key, value, persist, allow_output_mutation, hash_funcs=None
+):
+    _write_to_mem_cache(mem_cache, key, value, allow_output_mutation, hash_funcs)
     if persist:
         _write_to_disk_cache(key, value)
 
@@ -299,6 +334,8 @@ def cache(
     suppress_st_warning=False,
     hash_funcs=None,
     ignore_hash=False,
+    max_entries=None,
+    ttl=None,
 ):
     """Function decorator to memoize function executions.
 
@@ -331,6 +368,15 @@ def cache(
         inside Streamlit's caching mechanism: when the hasher encounters an object, it will first
         check to see if its type matches a key in this dict and, if so, will use the provided
         function to generate a hash for it. See below for an example of how this can be used.
+
+    max_entries : int or None
+        The maximum number of entries to keep in the cache, or None
+        for an unbounded cache. (When a new entry is added to a full cache,
+        the oldest cached entry will be removed.) The default is None.
+
+    ttl : float or None
+        The maximum number of seconds to keep an entry in the cache, or
+        None if cache entries should not expire. The default is None.
 
     ignore_hash : boolean
         DEPRECATED. Please use allow_output_mutation instead.
@@ -395,7 +441,12 @@ def cache(
             show_spinner=show_spinner,
             suppress_st_warning=suppress_st_warning,
             hash_funcs=hash_funcs,
+            max_entries=max_entries,
+            ttl=ttl,
         )
+
+    # Create the function's in-memory cache.
+    mem_cache = _create_mem_cache(max_entries, ttl)
 
     @functools_wraps(func)
     def wrapped_func(*args, **kwargs):
@@ -414,7 +465,7 @@ def cache(
         else:
             message = "Running %s(...)." % name
 
-        def get_or_set_cache():
+        def get_or_create_cached_value():
             hasher = hashlib.new("md5")
 
             args_hasher = CodeHasher("md5", hasher, hash_funcs)
@@ -430,7 +481,11 @@ def cache(
 
             try:
                 return_value = _read_from_cache(
-                    key, persist, allow_output_mutation, func, hash_funcs
+                    mem_cache=mem_cache,
+                    key=key,
+                    persisted=persist,
+                    allow_output_mutation=allow_output_mutation,
+                    hash_funcs=hash_funcs,
                 )
                 LOGGER.debug("Cache hit: %s", func)
             except CacheKeyNotFoundError:
@@ -444,6 +499,7 @@ def cache(
                         return_value = func(*args, **kwargs)
 
                 _write_to_cache(
+                    mem_cache=mem_cache,
                     key=key,
                     value=return_value,
                     persist=persist,
@@ -455,9 +511,9 @@ def cache(
 
         if show_spinner:
             with st.spinner(message):
-                return get_or_set_cache()
+                return get_or_create_cached_value()
         else:
-            return get_or_set_cache()
+            return get_or_create_cached_value()
 
     # Make this a well-behaved decorator by preserving important function
     # attributes.
@@ -500,6 +556,7 @@ class Cache(Dict[Any, Any]):
     def __init__(self, persist=False, allow_output_mutation=False):
         self._persist = persist
         self._allow_output_mutation = allow_output_mutation
+        self._mem_cache = _create_mem_cache(None, None)
 
         dict.__init__(self)
 
@@ -554,7 +611,10 @@ class Cache(Dict[Any, Any]):
 
         try:
             value, _ = _read_from_cache(
-                key, self._persist, self._allow_output_mutation, code, None
+                mem_cache=self._mem_cache,
+                key=key,
+                persisted=self._persist,
+                allow_output_mutation=self._allow_output_mutation,
             )
             self.update(value)
         except CacheKeyNotFoundError:
@@ -562,12 +622,17 @@ class Cache(Dict[Any, Any]):
                 # If we don't hash the results, we don't need to use exec and just return True.
                 # This way line numbers will be correct.
                 _write_to_cache(
-                    key=key, value=self, persist=False, allow_output_mutation=True
+                    mem_cache=self._mem_cache,
+                    key=key,
+                    value=self,
+                    persist=False,
+                    allow_output_mutation=True,
                 )
                 return True
 
             exec(code, caller_frame.f_globals, caller_frame.f_locals)
             _write_to_cache(
+                mem_cache=self._mem_cache,
                 key=key,
                 value=self,
                 persist=self._persist,
@@ -621,5 +686,7 @@ def _clear_disk_cache():
 
 
 def _clear_mem_cache():
-    global _mem_cache
-    _mem_cache = {}
+    global _all_mem_caches
+    # Copy _all_mem_caches to guard against threading errors
+    for mem_cache in list(_all_mem_caches):
+        mem_cache.clear()

lib/Pipfile
@@ -42,6 +42,7 @@ base58 = "*"
 blinker = "*"
 boto3 = "*"
 botocore = "*"
+cachetools = ">=4.0"
 click = ">=7.0"
 # Pin python-dateutil to work around a botocore issue: https://github.com/boto/botocore/issues/1872
 # TODO: Remove this pin when the above issue is fixed.

lib/mypy.ini
@@ -31,5 +31,5 @@ ignore_errors = True
 [mypy-streamlit.proto.*]
 ignore_errors = True
 
-[mypy-altair,base58,blinker,bokeh.embed,botocore,boto3,chart_studio.*,cPickle,flake8.main,future.*,matplotlib,matplotlib.pyplot,numpy,pandas,PIL,pipenv.*,plotly.*,prometheus_client,pydeck,pyflakes,pyflakes.checker,setuptools,sympy,tensorflow.*,tornado.*,tzlocal,validators,watchdog,watchdog.observers]
+[mypy-altair,base58,blinker,bokeh.embed,botocore,boto3,cachetools.*,chart_studio.*,cPickle,flake8.main,future.*,matplotlib,matplotlib.pyplot,numpy,pandas,PIL,pipenv.*,plotly.*,prometheus_client,pydeck,pyflakes,pyflakes.checker,setuptools,sympy,tensorflow.*,tornado.*,tzlocal,validators,watchdog,watchdog.observers]
 ignore_missing_imports = true

lib/tests/streamlit/caching_test.py
@@ -209,6 +209,146 @@ class CacheTest(testutil.DeltaGeneratorTestCase):
         # The other thread should not have modified the main thread
         self.assertEqual(1, get_counter())
 
+    def test_max_size(self):
+        """The oldest object should be evicted when maxsize is reached."""
+        # Create 2 cached functions to test that they don't interfere
+        # with each other.
+
+        foo_vals = []
+
+        @st.cache(max_entries=2)
+        def foo(x):
+            foo_vals.append(x)
+            return x
+
+        bar_vals = []
+
+        @st.cache(max_entries=3)
+        def bar(x):
+            bar_vals.append(x)
+            return x
+
+        self.assertEqual([], foo_vals)
+        self.assertEqual([], bar_vals)
+
+        # Stick two items in both caches. foo will be filled.
+        foo(0), foo(1)
+        bar(0), bar(1)
+        self.assertEqual([0, 1], foo_vals)
+        self.assertEqual([0, 1], bar_vals)
+
+        # 0, 1 are already cached, so called_values shouldn't change.
+        foo(0), foo(1)
+        bar(0), bar(1)
+        self.assertEqual([0, 1], foo_vals)
+        self.assertEqual([0, 1], bar_vals)
+
+        # Add a new item to the cache.
+        # foo: 0 should be evicted; 1 and 2 should be present.
+        # bar: 0, 1, 2 present.
+        foo(2)
+        bar(2)
+
+        # foo(0) again should cause 0 to be added again, since it was
+        # previously evicted. Nothing will have been evicted from bar.
+        foo(1), foo(0)
+        bar(1), bar(0)
+        self.assertEqual([0, 1, 2, 0], foo_vals)
+        self.assertEqual([0, 1, 2], bar_vals)
+
+    # Reduce the huge amount of logspam we get from hashing/caching
+    @patch("streamlit.hashing.LOGGER.debug")
+    @patch("streamlit.caching.LOGGER.debug")
+    def test_no_max_size(self, _1, _2):
+        """If max_size is None, the cache is unbounded."""
+        called_values = []
+
+        @st.cache(max_entries=None)
+        def f(x):
+            called_values.append(x)
+            return x
+
+        # Stick a bunch of items in the cache.
+        for ii in range(256):
+            f(ii)
+
+        # Clear called_values, and test that accessing the same bunch of
+        # items doesn't result in f() being called.
+        called_values = []
+        for ii in range(256):
+            f(ii)
+        self.assertEqual([], called_values)
+
+    @patch("streamlit.caching.TTLCACHE_TIMER")
+    def test_ttl(self, timer_patch):
+        """Entries should expire after the given ttl."""
+        # Create 2 cached functions to test that they don't interfere
+        # with each other.
+        foo_vals = []
+
+        @st.cache(ttl=1)
+        def foo(x):
+            foo_vals.append(x)
+            return x
+
+        bar_vals = []
+
+        @st.cache(ttl=5)
+        def bar(x):
+            bar_vals.append(x)
+            return x
+
+        # Store a value at time 0
+        timer_patch.return_value = 0
+        foo(0)
+        bar(0)
+        self.assertEqual([0], foo_vals)
+        self.assertEqual([0], bar_vals)
+
+        # Advance our timer, but not enough to expire our value.
+        timer_patch.return_value = 0.5
+        foo(0)
+        bar(0)
+        self.assertEqual([0], foo_vals)
+        self.assertEqual([0], bar_vals)
+
+        # Advance our timer enough to expire foo, but not bar.
+        timer_patch.return_value = 1.5
+        foo(0)
+        bar(0)
+        self.assertEqual([0, 0], foo_vals)
+        self.assertEqual([0], bar_vals)
+
+    def test_clear_cache(self):
+        """Clear cache should do its thing."""
+        foo_vals = []
+
+        @st.cache
+        def foo(x):
+            foo_vals.append(x)
+            return x
+
+        bar_vals = []
+
+        @st.cache
+        def bar(x):
+            bar_vals.append(x)
+            return x
+
+        foo(0), foo(1), foo(2)
+        bar(0), bar(1), bar(2)
+        self.assertEqual([0, 1, 2], foo_vals)
+        self.assertEqual([0, 1, 2], bar_vals)
+
+        # Clear the cache and access our original values again. They
+        # should be recomputed.
+        caching.clear_cache()
+
+        foo(0), foo(1), foo(2)
+        bar(0), bar(1), bar(2)
+        self.assertEqual([0, 1, 2, 0, 1, 2], foo_vals)
+        self.assertEqual([0, 1, 2, 0, 1, 2], bar_vals)
+
 
 # Temporarily turn off these tests since there's no Cache object in __init__
 # right now.

lib/tests/streamlit/help_test.py
@@ -108,8 +108,15 @@ class StHelpTest(testutil.DeltaGeneratorTestCase):
             self.assertEqual(
                 ds.signature,
                 (
-                    "(func=None, persist=False, "
-                    "allow_output_mutation=False, show_spinner=True, suppress_st_warning=False, hash_funcs=None, ignore_hash=False)"
+                    "(func=None, "
+                    "persist=False, "
+                    "allow_output_mutation=False, "
+                    "show_spinner=True, "
+                    "suppress_st_warning=False, "
+                    "hash_funcs=None, "
+                    "ignore_hash=False, "
+                    "max_entries=None, "
+                    "ttl=None)"
                 ),
             )
             self.assertTrue(ds.doc_string.startswith("Function decorator to"))

commit 931e0616b463ce06b8da1880374662baaca40ff9

Fix st.cache (#1208)
This fixes breakage that I introduced in #1152 

Rather than creating the cache as a local variable in the st.cache wrapper, we instead manage all caches in a global `_MemCaches` class. Each cache is keyed off its wrapped function's fully qualified name, and its contents.

There's a new `ScriptRunner` test that makes sure that caches are reused across multiple runs of the same script.

lib/streamlit/caching.py
@@ -28,9 +28,9 @@ import textwrap
 import threading
 import time
 from collections import namedtuple
-from typing import Any, Dict
+from typing import Any, Dict, Optional
 
-from cachetools import LRUCache, TTLCache
+from cachetools import TTLCache
 
 import streamlit as st
 from streamlit.util import functools_wraps
@@ -56,11 +56,9 @@ to suppress the warning.
 
 LOGGER = get_logger(__name__)
 
-# The timer function we use with TTLCache.
+# The timer function we use with TTLCache. This is the default timer func, but
+# is exposed here as a constant so that it can be patched in unit tests.
 TTLCACHE_TIMER = time.monotonic
-
-# A list of all mem-caches we've created.
-_all_mem_caches = []
 
 
 class CacheError(Exception):
@@ -78,6 +76,64 @@ class CachedObjectWasMutatedError(ValueError):
 
 CacheEntry = namedtuple("CacheEntry", ["value", "hash"])
 DiskCacheEntry = namedtuple("DiskCacheEntry", ["value"])
+
+
+class _MemCaches(object):
+    """Manages all in-memory st.cache caches"""
+
+    def __init__(self):
+        # Contains a cache object for each st.cache'd function
+        self._lock = threading.RLock()
+        self._function_caches = {}  # type: Dict[str, TTLCache]
+
+    def get_cache(
+        self, key: str, max_entries: Optional[float], ttl: Optional[float]
+    ) -> TTLCache:
+        """Return the mem cache for the given key.
+
+        If it doesn't exist, create a new one with the given params.
+        """
+
+        if max_entries is None:
+            max_entries = math.inf
+        if ttl is None:
+            ttl = math.inf
+
+        if not isinstance(max_entries, (int, float)):
+            raise RuntimeError("max_entries must be an int")
+        if not isinstance(ttl, (int, float)):
+            raise RuntimeError("ttl must be a float")
+
+        # Get the existing cache, if it exists, and validate that its params
+        # haven't changed.
+        with self._lock:
+            mem_cache = self._function_caches.get(key)
+            if (
+                mem_cache is not None
+                and mem_cache.ttl == ttl
+                and mem_cache.maxsize == max_entries
+            ):
+                return mem_cache
+
+            # Create a new cache object and put it in our dict
+            LOGGER.debug(
+                "Creating new mem_cache (key=%s, max_entries=%s, ttl=%s)",
+                key,
+                max_entries,
+                ttl,
+            )
+            mem_cache = TTLCache(maxsize=max_entries, ttl=ttl, timer=TTLCACHE_TIMER)
+            self._function_caches[key] = mem_cache
+            return mem_cache
+
+    def clear(self) -> None:
+        """Clear all caches"""
+        with self._lock:
+            self._function_caches = {}
+
+
+# Our singleton _MemCaches instance
+_mem_caches = _MemCaches()
 
 
 # A thread-local counter that's incremented when we enter @st.cache
@@ -204,34 +260,6 @@ def _get_mutated_output_error_message():
     ).strip("\n")
 
     return message
-
-
-def _create_mem_cache(max_entries, ttl):
-    """Create an in-memory cache object with the given parameters."""
-    if max_entries is None and ttl is None:
-        # If we have no max_entries or TTL, we can just use a regular dict.
-        mem_cache = {}
-    else:
-        if max_entries is None:
-            max_entries = math.inf
-        elif not isinstance(max_entries, int):
-            raise Exception("`max_entries` must be an int or None")
-
-        if not isinstance(ttl, (float, int)) and ttl is not None:
-            raise Exception("`ttl` must be a float or None")
-
-        # If ttl is none, just create an LRUCache. (TTLCache is simply an
-        # LRUCache that adds a ttl option.)
-        if ttl is None:
-            mem_cache = LRUCache(maxsize=max_entries)
-        else:
-            mem_cache = TTLCache(maxsize=max_entries, ttl=ttl, timer=TTLCACHE_TIMER)
-
-    # Stick the new cache in our global list
-    global _all_mem_caches
-    _all_mem_caches.append(mem_cache)
-
-    return mem_cache
 
 
 def _read_from_mem_cache(mem_cache, key, allow_output_mutation, hash_funcs):
@@ -445,8 +473,36 @@ def cache(
             ttl=ttl,
         )
 
-    # Create the function's in-memory cache.
-    mem_cache = _create_mem_cache(max_entries, ttl)
+    # Create the unique key for this function's cache. The cache will be
+    # retrieved from inside the wrapped function.
+    #
+    # A naive implementation would involve simply creating the cache object
+    # right here in the wrapper, which in a normal Python script would be
+    # executed only once. But in Streamlit, we reload all modules related to a
+    # user's app when the app is re-run, which means that - among other
+    # things - all function decorators in the app will be re-run, and so any
+    # decorator-local objects will be recreated.
+    #
+    # Furthermore, our caches can be destroyed and recreated (in response
+    # to cache clearing, for example), which means that retrieving the
+    # function's cache here (so that the wrapped function can save a lookup)
+    # is incorrect: the cache itself may be recreated between
+    # decorator-evaluation time and decorated-function-execution time. So
+    # we must retrieve the cache object *and* perform the cached-value lookup
+    # inside the decorated function.
+
+    func_hasher = CodeHasher("md5", None, hash_funcs)
+    # Include the function's module and qualified name in the hash.
+    # This means that two identical functions in different modules
+    # will not share a hash; it also means that two identical *nested*
+    # functions in the same module will not share a hash.
+    func_hasher.update(func.__module__)
+    func_hasher.update(func.__qualname__)
+    func_hasher.update(func)
+    cache_key = func_hasher.hexdigest()
+    LOGGER.debug(
+        "mem_cache key for %s.%s: %s", func.__module__, func.__qualname__, cache_key
+    )
 
     @functools_wraps(func)
     def wrapped_func(*args, **kwargs):
@@ -458,7 +514,7 @@ def cache(
             LOGGER.debug("Purposefully skipping cache")
             return func(*args, **kwargs)
 
-        name = func.__name__
+        name = func.__qualname__
 
         if len(args) == 0 and len(kwargs) == 0:
             message = "Running %s()." % name
@@ -466,23 +522,34 @@ def cache(
             message = "Running %s(...)." % name
 
         def get_or_create_cached_value():
-            hasher = hashlib.new("md5")
+            # First, get the cache that's attached to this function.
+            # This cache's key is generated (above) from the function's code.
+            global _mem_caches
+            mem_cache = _mem_caches.get_cache(cache_key, max_entries, ttl)
 
-            args_hasher = CodeHasher("md5", hasher, hash_funcs)
+            # Next, calculate the key for the value we'll be searching for
+            # within that cache. This key is generated from both the function's
+            # code and the arguments that are passed into it. (Even though this
+            # key is used to index into a per-function cache, it must be
+            # globally unique, because it is *also* used for a global on-disk
+            # cache that is *not* per-function.)
+            value_hasher = hashlib.new("md5")
+
+            args_hasher = CodeHasher("md5", value_hasher, hash_funcs)
             args_hasher.update([args, kwargs])
             LOGGER.debug("Hashing arguments to %s of %i bytes.", name, args_hasher.size)
 
-            code_hasher = CodeHasher("md5", hasher, hash_funcs)
+            code_hasher = CodeHasher("md5", value_hasher, hash_funcs)
             code_hasher.update(func)
             LOGGER.debug("Hashing function %s in %i bytes.", name, code_hasher.size)
 
-            key = hasher.hexdigest()
-            LOGGER.debug("Cache key: %s", key)
+            value_key = value_hasher.hexdigest()
+            LOGGER.debug("Cache key: %s", value_key)
 
             try:
                 return_value = _read_from_cache(
                     mem_cache=mem_cache,
-                    key=key,
+                    key=value_key,
                     persisted=persist,
                     allow_output_mutation=allow_output_mutation,
                     hash_funcs=hash_funcs,
@@ -500,7 +567,7 @@ def cache(
 
                 _write_to_cache(
                     mem_cache=mem_cache,
-                    key=key,
+                    key=value_key,
                     value=return_value,
                     persist=persist,
                     allow_output_mutation=allow_output_mutation,
@@ -556,7 +623,7 @@ class Cache(Dict[Any, Any]):
     def __init__(self, persist=False, allow_output_mutation=False):
         self._persist = persist
         self._allow_output_mutation = allow_output_mutation
-        self._mem_cache = _create_mem_cache(None, None)
+        self._mem_cache = {}
 
         dict.__init__(self)
 
@@ -686,7 +753,5 @@ def _clear_disk_cache():
 
 
 def _clear_mem_cache():
-    global _all_mem_caches
-    # Copy _all_mem_caches to guard against threading errors
-    for mem_cache in list(_all_mem_caches):
-        mem_cache.clear()
+    global _mem_caches
+    _mem_caches.clear()

lib/tests/streamlit/scriptrunner/ScriptRunner_test.py
@@ -314,6 +314,36 @@ class ScriptRunnerTest(unittest.TestCase):
                 ],
             )
 
+    def test_rerun_caching(self):
+        """Test that st.caches are maintained across script runs."""
+
+        # Run st_cache_script.
+        runner = TestScriptRunner("st_cache_script.py")
+        runner.enqueue_rerun()
+        runner.start()
+        runner.join()
+
+        # The script has 4 cached functions, each of which writes out
+        # the same text.
+        self._assert_text_deltas(
+            runner,
+            [
+                "cached function called",
+                "cached function called",
+                "cached function called",
+                "cached function called",
+            ],
+        )
+
+        # Re-run the script on a second runner.
+        runner = TestScriptRunner("st_cache_script.py")
+        runner.enqueue_rerun()
+        runner.start()
+        runner.join()
+
+        # The cached functions should not have been called on this second run
+        self._assert_text_deltas(runner, [])
+
     def _assert_no_exceptions(self, scriptrunner):
         """Asserts that no uncaught exceptions were thrown in the
         scriptrunner's run thread.

lib/tests/streamlit/scriptrunner/test_data/st_cache_script.py
@@ -1,0 +1,60 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018-2020 Streamlit Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""A script for ScriptRunnerTest that uses st.cache"""
+
+import streamlit as st
+
+# Except for their names, these functions all intentionally have the same
+# bodies - so their ASTs should hash to the same values. However, they should
+# *not* share the same caches, because we include a function's qualified
+# name in its cache key. To test that this is true, the associated ScriptRunner
+# test should run the script twice:
+# - On the first run, "cached function called" should be produced 4 times
+# - On the second run, "cached function called" should not be produced
+
+
+@st.cache(suppress_st_warning=True)
+def cached1():
+    st.text("cached function called")
+    return "cached value"
+
+
+@st.cache(suppress_st_warning=True)
+def cached2():
+    st.text("cached function called")
+    return "cached value"
+
+
+def outer_func():
+    # These closures share the names and bodies of the functions in the outer
+    # scope, but they should have their own independent caches.
+    @st.cache(suppress_st_warning=True)
+    def cached1():
+        st.text("cached function called")
+        return "cached value"
+
+    @st.cache(suppress_st_warning=True)
+    def cached2():
+        st.text("cached function called")
+        return "cached value"
+
+    cached1()
+    cached2()
+
+
+cached1()
+cached2()
+outer_func()