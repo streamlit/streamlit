fe0280acc95273afc6f0e94aecda0cba9e067d2d
Jonathan Rhone <rhone.j@gmail.com>
2020-03-26 10:06:41 -0300

    no op

--- lib/tests/streamlit/hashing_test.py
+++ lib/tests/streamlit/hashing_test.py
@@ -210,8 +210,8 @@ class HashTest(unittest.TestCase):
         self.assertNotEqual(get_hash(abs), get_hash(type))
 
     def test_regex(self):
-        p1 = re.compile(".*")
         p2 = re.compile(".*")
+        p1 = re.compile(".*")
         p3 = re.compile(".*", re.I)
         self.assertEqual(get_hash(p1), get_hash(p2))
         self.assertNotEqual(get_hash(p1), get_hash(p3))
