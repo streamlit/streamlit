95f47595fa8696fa671fbe7ac3b31dc12b3c5d59
Amie Chen <chinghc@gmail.com>
2020-04-09 10:03:06 +0800

    change defualt color

--- lib/streamlit/DeltaGenerator.py
+++ lib/streamlit/DeltaGenerator.py
@@ -2096,7 +2096,7 @@ class DeltaGenerator(object):
         return file_datas if accept_multiple_files else file_datas[0]
 
     @_with_element
-    def color_picker(self, element, label, value="", key=None):
+    def color_picker(self, element, label, value="#00f900", key=None):
         """Display a color picker widget.
 
         Parameters
@@ -2105,7 +2105,8 @@ class DeltaGenerator(object):
             A short label explaining to the user what this input is for.
         value : any
             The hex value of this widget when it first renders. This will be
-            cast to str internally. Internally prepend a hash to the string if it doesn't have it.
+            cast to str internally. Internally prepend a hash to the string
+            if the hash symbol is absent.
         key : str
             An optional string to use as the unique key for the widget.
             If this is omitted, a key will be generated for the widget
@@ -2119,12 +2120,12 @@ class DeltaGenerator(object):
 
         Example
         -------
-        >>> color = st.color_picker('Pick A Color', '#414141')
+        >>> color = st.color_picker('Pick A Color', '#00f900')
         >>> st.write('The current color is', color)
 
         """
         element.color_picker.label = label
-        element.color_picker.default = "#000000"
+        element.color_picker.default = str(value)
 
         ui_value = _get_widget_ui_value("color_picker", element, user_key=key)
         current_value = ui_value if ui_value is not None else value
