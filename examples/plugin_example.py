dfdc3f41b2011448fc06180851339fb3eb84c6e1
Tim Conkling <tconkling@gmail.com>
2020-04-14 16:30:09 -0700

    Add some docs, and set background: transparent

--- examples/plugin_example.py
+++ examples/plugin_example.py
@@ -1,10 +1,19 @@
 import streamlit as st
 
+# Register "my_plugin" as a Streamlit component.
 st.plugin("my_plugin", "plugin_template/build")
 
-result = st.my_plugin(
-    label="How many bats?",
-    options=["one bat", "TWO bats", "THREE bats", "FOUR BATS! ah ah ah!"],
-    default="one bat",
-)
-st.write("This many: %s" % result)
+# Create an instance of the component. Arguments we pass here
+# will be available in an "args" dictionary in the component.
+# "Default" is a special argument that specifies the initial return value of
+# my_plugin, before the user has interacted with it.
+num_clicks = st.my_plugin(name="Streamlit", default=0)
+st.markdown("You clicked %s times!" % int(num_clicks))
+
+# We can create multiple instances of the component
+num_clicks = st.my_plugin(name="World", default=0)
+st.markdown("You clicked %s times!" % int(num_clicks))
+
+# It can live in the sidebar
+num_clicks = st.sidebar.my_plugin(name="Sidebar", default=0)
+st.sidebar.markdown("You clicked %s times!" % int(num_clicks))
