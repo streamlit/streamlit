3ac3c9d044bd5515fe685eb1d9120fc826f769f9
Tim Conkling <tconkling@gmail.com>
2020-04-14 09:20:19 -0700

    Remove unnecessary files

--- plugin_template/README.md
+++
@@ -1,4 +0,0 @@
-## Steps to create the plugin
-
-- `npx create-react-app my-app --template typescript`
-- add `"homepage": ".",` to `package.json`, to allow serving from anywhere (https://create-react-app.dev/docs/deployment)
--- plugin_template/src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import Plugin from "./App";
-
-test("renders learn react link", () => {
-  const { getByText } = render(<Plugin />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
