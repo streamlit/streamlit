738736c1d7138eac3a8c6605bf7829d26aee0b53
erikhopf <erik.hopf@gmail.com>
2020-04-14 01:21:25 -0700

    Add sphinx-markdown-tables (#1343)
    * Adding markdown support.
    
    * Adding sphinx-markdown-tables to the pip file.

--- docs/conf.py
+++ docs/conf.py
@@ -49,6 +49,7 @@ extensions = [
     "sphinx.ext.napoleon",
     "recommonmark",
     "stoutput",
+    'sphinx_markdown_tables'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
