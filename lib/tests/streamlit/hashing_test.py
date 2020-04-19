d5323aea69a056545f26d1b480f877bd56ef99b0
Jonathan Rhone <rhone.j@gmail.com>
2020-04-19 12:47:05 -0300

    black

--- lib/tests/streamlit/hashing_test.py
+++ lib/tests/streamlit/hashing_test.py
@@ -108,14 +108,14 @@ class HashTest(unittest.TestCase):
         self.assertNotEqual(get_hash(a), get_hash(c))
 
     def test_getset_descriptor(self):
-        class A():
+        class A:
             x = 1
 
-        class B():
+        class B:
             x = 1
 
-        a = A.__dict__['__dict__']
-        b = B.__dict__['__dict__']
+        a = A.__dict__["__dict__"]
+        b = B.__dict__["__dict__"]
         assert is_type(a, "builtins.getset_descriptor")
 
         self.assertEqual(get_hash(a), get_hash(a))
