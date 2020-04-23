a20f92b67ee1f4cfe2b16108dfb60917e138a505
nthmost <naomi@nthmost.com>
2020-04-22 17:39:52 -0700

    doc fixes

--- docs/conf.py
+++ docs/conf.py
@@ -99,7 +99,7 @@ html_favicon = "favicon.png"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static"]
+# html_static_path = ["_static"]
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
--- lib/streamlit/DeltaGenerator.py
+++ lib/streamlit/DeltaGenerator.py
@@ -1338,10 +1338,12 @@ class DeltaGenerator(object):
             the usual case, this function will render the global plot.
 
         clear_figure : bool
-            If True, the figure will be cleared after being rendered. 
+            If True, the figure will be cleared after being rendered.
             If False, the figure will not be cleared after being rendered.
             If left unspecified, we pick a default based on the value of `fig`.
+
             * If `fig` is set, defaults to `False`.
+
             * If `fig` is not set, defaults to `True`. This simulates Jupyter's
               approach to matplotlib rendering.
 
