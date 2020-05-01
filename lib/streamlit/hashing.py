2a0e6e516633e63da69593a26e8df309716daffe
Jonathan Rhone <rhone.j@gmail.com>
2020-05-01 19:18:43 -0400

    cleanup

--- lib/streamlit/hashing.py
+++ lib/streamlit/hashing.py
@@ -491,14 +491,6 @@ class _CodeHasher:
         elif type_util.is_type(obj, "tensorflow.python.client.session.Session"):
             return self.to_bytes(id(obj))
 
-        elif type_util.is_type(obj, "torch._C._TensorBase") or type_util.is_type(
-            obj, "torch.Tensor"
-        ):
-            # TODO handle `detach` here since TensorBase PR doesn't use it
-            # RuntimeError: Can't call numpy() on Variable that requires grad.
-            # Use var.detach().numpy() instead.
-            return self.to_bytes(obj.detach().numpy())
-
         elif type_util.is_type(obj, "builtins.PyCapsule"):
             ctypes.pythonapi.PyCapsule_GetPointer.restype = ctypes.c_void_p
             ctypes.pythonapi.PyCapsule_GetPointer.argtypes = [
