31d9805560806d0c79ec9ba0f32655ec32e32871
Jonathan Rhone <rhone.j@gmail.com>
2020-05-07 22:43:32 -0400

    Torch Tensorbase hash func (#1394)

--- lib/tests/streamlit/hashing_test.py
+++ lib/tests/streamlit/hashing_test.py
@@ -34,6 +34,7 @@ import numpy as np
 import pandas as pd
 import pytest
 import sqlalchemy as db
+import torch
 from mock import patch, MagicMock
 from parameterized import parameterized
 
@@ -378,6 +379,19 @@ class HashTest(unittest.TestCase):
 
         tf_session2 = tf.compat.v1.Session(config=tf_config)
         self.assertNotEqual(get_hash(tf_session), get_hash(tf_session2))
+
+    def test_torch_c_tensorbase(self):
+        a = torch.ones([1, 1]).__reduce__()[1][2]
+        b = torch.ones([1, 1], requires_grad=True).__reduce__()[1][2]
+        c = torch.ones([1, 2]).__reduce__()[1][2]
+
+        assert is_type(a, "torch._C._TensorBase")
+        self.assertEqual(get_hash(a), get_hash(b))
+        self.assertNotEqual(get_hash(a), get_hash(c))
+
+        b.mean().backward()
+        # Calling backward on a tensorbase doesn't seem to affect the gradient
+        self.assertEqual(get_hash(a), get_hash(b))
 
     def test_torch_tensor(self):
         a = torch.ones([1, 1])
