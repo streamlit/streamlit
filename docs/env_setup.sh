2896d889d30d44b3c55b940eac30d4dca5ce5f51
Randy Zwitch <randy@streamlit.io>
2020-05-08 16:57:41 -0400

    Add sphinx tables lib

--- docs/env_setup.sh
+++ docs/env_setup.sh
@@ -1,6 +1,5 @@
 #whole script assumes you start in docs/
 
-
 #get protoc pre-compiled binary, unzip
 wget https://github.com/protocolbuffers/protobuf/releases/download/v3.11.4/protoc-3.11.4-linux-x86_64.zip
 unzip protoc-3.11.4-linux-x86_64.zip
@@ -12,7 +11,6 @@ unzip protoc-3.11.4-linux-x86_64.zip
     --mypy_out=../lib \
 	../proto/streamlit/proto/*.proto
 
-#ls -ltrh ../lib/streamlit/proto
-
-#re-run build process to make protobuf available
+#re-run setup.py build process to make protobuf available
+#this is tremendously fragile, as ../lib is hardcoded in here
 /home/docs/checkouts/readthedocs.org/user_builds/streamlit-streamlit/envs/latest/bin/python -m pip install --upgrade --upgrade-strategy eager --no-cache-dir ../lib
