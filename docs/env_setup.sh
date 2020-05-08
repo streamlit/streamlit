585f888bd9caa62f67e21b549ff66d9dd178ebbc
Randy Zwitch <randy@streamlit.io>
2020-05-08 16:17:07 -0400

    Try to generate pb

--- docs/env_setup.sh
+++ docs/env_setup.sh
@@ -2,4 +2,10 @@
 wget https://github.com/protocolbuffers/protobuf/releases/download/v3.11.4/protoc-3.11.4-linux-x86_64.zip
 unzip protoc-3.11.4-linux-x86_64.zip
 
-./bin.protoc
+#assumes you are in docs/
+./bin/protoc \
+    --proto_path=../proto \
+	--python_out=../lib \
+	../proto/streamlit/proto/*.proto
+
+ls ../lib/streamlit/proto
