9997ffc3123e5951e5dd98f376ccd50176bb3847
Randy Zwitch <randy@streamlit.io>
2020-05-08 16:42:19 -0400

    Add proper path back to protoc

--- docs/env_setup.sh
+++ docs/env_setup.sh
@@ -4,7 +4,7 @@ unzip protoc-3.11.4-linux-x86_64.zip
 
 #build protobufs
 #assumes you are in docs/
-protoc \
+./bin/protoc \
     --proto_path=../proto \
 	--python_out=../lib \
     --mypy_out=../lib \
