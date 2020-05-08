c74ceaca12ee21987a4b1a7069ca3fb6adce1116
Randy Zwitch <randy@streamlit.io>
2020-05-08 15:33:51 -0400

    More path figuring

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,9 @@ import subprocess
 subprocess.run(["wget", "https://github.com/protocolbuffers/protobuf/releases/download/v3.11.4/protoc-3.11.4-linux-x86_64.zip"])
 subprocess.run(["unzip", "protoc-3.11.4-linux-x86_64.zip"])
 #/home/docs/checkouts/readthedocs.org/user_builds/streamlit-streamlit/checkouts/latest/docs
-subprocess.run(["./bin/protoc --version"])
+subprocess.run(["ls", "-ltrh"])
+
+#subprocess.run(["./bin/protoc --version"])
 
 # -- Project information -----------------------------------------------------
 
