51c09281da0e8b63c522b5e242ce7f419eedb061
Randy Zwitch <randy@streamlit.io>
2020-05-08 16:37:53 -0400

    Run as root

--- docs/env_setup.sh
+++ docs/env_setup.sh
@@ -2,7 +2,7 @@
 #wget https://github.com/protocolbuffers/protobuf/releases/download/v3.11.4/protoc-3.11.4-linux-x86_64.zip
 #unzip protoc-3.11.4-linux-x86_64.zip
 
-apt install -y protobuf-compiler 
+sudo apt install -y protobuf-compiler 
 
 #build protobufs
 #assumes you are in docs/
