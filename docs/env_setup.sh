b65005602669299ad330aa4d2a283eda6183566e
Randy Zwitch <randy@streamlit.io>
2020-05-08 16:52:41 -0400

    Fix path issue

--- docs/env_setup.sh
+++ docs/env_setup.sh
@@ -1,16 +1,18 @@
+#whole script assumes you start in docs/
+
+
 #get protoc pre-compiled binary, unzip
 wget https://github.com/protocolbuffers/protobuf/releases/download/v3.11.4/protoc-3.11.4-linux-x86_64.zip
 unzip protoc-3.11.4-linux-x86_64.zip
 
 #build protobufs
-#assumes you are in docs/
 ./bin/protoc \
     --proto_path=../proto \
 	--python_out=../lib \
     --mypy_out=../lib \
 	../proto/streamlit/proto/*.proto
 
-ls -ltrh ../lib/streamlit/proto
+#ls -ltrh ../lib/streamlit/proto
 
 #re-run build process to make protobuf available
-/home/docs/checkouts/readthedocs.org/user_builds/streamlit-streamlit/envs/latest/bin/python -m pip install --upgrade --upgrade-strategy eager --no-cache-dir ./lib
+/home/docs/checkouts/readthedocs.org/user_builds/streamlit-streamlit/envs/latest/bin/python -m pip install --upgrade --upgrade-strategy eager --no-cache-dir ../lib
