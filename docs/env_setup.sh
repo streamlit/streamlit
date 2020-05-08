64843795dcc150fd5ddfb29fb687364e34bead4e
Randy Zwitch <randy@streamlit.io>
2020-05-08 16:24:01 -0400

    Check full directory

--- docs/env_setup.sh
+++ docs/env_setup.sh
@@ -2,10 +2,11 @@
 wget https://github.com/protocolbuffers/protobuf/releases/download/v3.11.4/protoc-3.11.4-linux-x86_64.zip
 unzip protoc-3.11.4-linux-x86_64.zip
 
+#build protobufs
 #assumes you are in docs/
 ./bin/protoc \
     --proto_path=../proto \
 	--python_out=../lib \
 	../proto/streamlit/proto/*.proto
 
-ls ../lib/streamlit/proto
+ls -ltrhd ../lib/streamlit/proto
