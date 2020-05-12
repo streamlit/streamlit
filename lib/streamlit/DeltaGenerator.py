2c31ce36ba7925d38e4b0dfc92ea775a5035907c
Ian Calvert <ianjcalvert@gmail.com>
2020-05-12 15:19:28 +0100

    Fix multiselect docstring example
    Add in ellipsis for line continuation

--- lib/streamlit/DeltaGenerator.py
+++ lib/streamlit/DeltaGenerator.py
@@ -1708,7 +1708,7 @@ class DeltaGenerator(object):
         -------
         >>> options = st.multiselect(
         ...     'What are your favorite colors',
-                ('Green', 'Yellow', 'Red', 'Blue'),
+        ...     ('Green', 'Yellow', 'Red', 'Blue'),
         ...     ['Yellow', 'Red'])
         >>>
         >>> st.write('You selected:', options)
