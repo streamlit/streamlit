1f2ec2ef1ba7f9766244f2f73934e2756f28b040
Randy Zwitch <randy@streamlit.io>
2020-05-12 13:08:58 -0400

    Add comment about code duplication

--- docs/env_setup.sh
+++ docs/env_setup.sh
@@ -1,5 +1,9 @@
-#whole script assumes you start in docs/
+#RZ, 20200512: This file duplicates some of the functionality from the core `make all-devel`
+#This is because we cannot assume we have sudo capaibilities or modify the RTD Docker container
+#https://github.com/streamlit/streamlit/pull/1435#discussion_r423826910
 
+
+#whole script assumes you start in docs/
 #get protoc pre-compiled binary, unzip
 wget https://github.com/protocolbuffers/protobuf/releases/download/v3.11.4/protoc-3.11.4-linux-x86_64.zip
 unzip protoc-3.11.4-linux-x86_64.zip
