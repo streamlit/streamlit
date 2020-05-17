ee708afe2adf2da432bb2cea12fdc461d022952e
kantuni <henrikh.kantuni@gmail.com>
2020-05-17 22:40:27 +0400

    Example PyPI package

--- components_beta/template/example.py
+++ components_beta/template/example.py
@@ -1,21 +1,5 @@
 import streamlit as st
-
-# Declare a Streamlit component.
-# It will be served by the local webpack dev server that you can
-# run via `npm run start`.
-MyComponent = st.declare_component(url="http://localhost:3001")
-
-# Alternately, if you've built a production version of the component,
-# you can register the component's static files via the `path` param:
-# MyComponent = st.declare_component(path="component_template/build")
-
-# Add a wrapper function to the component.
-# This is an optional step that enables you to customize your component's
-# API, pre-process its input args, and post-process its output value.
-@MyComponent
-def create_instance(f, name, key=None):
-    return f(name=name, key=key, default=0)
-
+from package import MyComponent
 
 # Register the component. This assigns it a name within the Streamlit
 # namespace. "Declaration" and "registration" are separate steps:
