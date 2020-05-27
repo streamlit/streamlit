ee8b650c11b16ebe80698f21269b0e352fe89978
Tim Conkling <tconkling@gmail.com>
2020-05-27 11:07:35 -0700

    ComponentRegistry.test.tsx

--- frontend/src/components/widgets/CustomComponent/ComponentRegistry.test.tsx
+++ frontend/src/components/widgets/CustomComponent/ComponentRegistry.test.tsx
@@ -46,12 +46,72 @@ describe("ComponentRegistry", () => {
       "http://streamlit.mock:80/component/foo/index.html"
     )
 
-    // "Disconnect" from the server, and call getComponentURL again; it should not error.
+    // "Disconnect" from the server, and call getComponentURL again;
+    // it should return a URL constructed from the cached server URI.
     serverURI = undefined
     expect(registry.getComponentURL("bar", "index.html")).toEqual(
       "http://streamlit.mock:80/component/bar/index.html"
     )
   })
 
-  test("Handles listener registration", () => {})
+  test("Dispatches messages to listeners", () => {
+    const registry = new ComponentRegistry(() => MOCK_SERVER_URI)
+    const onMessageEvent = registry["onMessageEvent"]
+
+    // Create some mocks
+    const msgSource1: any = {}
+    const msgSource2: any = {}
+    const msgListener1 = jest.fn()
+    const msgListener2 = jest.fn()
+
+    // This should not error (and will not be handled).
+    onMessageEvent(new MessageEvent("message", { source: msgSource1 }))
+
+    // Register a listener for message events from the window.
+    registry.registerListener(msgSource1, msgListener1)
+
+    // Send a message to the registry. It should be re-dispatched
+    // to our listener.
+    const messageData = {
+      isStreamlitMessage: true,
+      type: "setComponentValue",
+    }
+    onMessageEvent(
+      new MessageEvent("message", { source: msgSource1, data: messageData })
+    )
+    expect(msgListener1).toBeCalledWith(messageData.type, messageData)
+
+    // Send a message that's missing data. It should *not* be re-dispatched.
+    msgListener1.mockReset()
+    onMessageEvent(new MessageEvent("message", { source: msgSource1 }))
+    expect(msgListener1).not.toBeCalled()
+
+    // De-register our listener. Messages should not be re-dispatched.
+    msgListener1.mockReset()
+    registry.deregisterListener(msgSource1)
+    onMessageEvent(
+      new MessageEvent("message", { source: msgSource1, data: messageData })
+    )
+    expect(msgListener1).not.toBeCalled()
+
+    // Ensure that listeners only receive messages for their own source.
+    registry.registerListener(msgSource1, msgListener1)
+    registry.registerListener(msgSource2, msgListener2)
+
+    msgListener1.mockReset()
+    msgListener2.mockReset()
+    onMessageEvent(
+      new MessageEvent("message", { source: msgSource1, data: messageData })
+    )
+    expect(msgListener1).toBeCalledWith(messageData.type, messageData)
+    expect(msgListener2).not.toBeCalled()
+
+    msgListener1.mockReset()
+    msgListener2.mockReset()
+    onMessageEvent(
+      new MessageEvent("message", { source: msgSource2, data: messageData })
+    )
+    expect(msgListener1).not.toBeCalled()
+    expect(msgListener2).toBeCalledWith(messageData.type, messageData)
+  })
 })
