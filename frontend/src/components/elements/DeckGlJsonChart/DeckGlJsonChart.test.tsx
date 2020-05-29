30eefeed242a9e413e2487b22ecccf2edef2aeec
Jonathan Rhone <rhone.j@gmail.com>
2020-05-29 13:19:33 -0400

    jest tests

--- frontend/src/components/elements/DeckGlJsonChart/DeckGlJsonChart.test.tsx
+++ frontend/src/components/elements/DeckGlJsonChart/DeckGlJsonChart.test.tsx
@@ -22,15 +22,53 @@ import { fromJS } from "immutable"
 
 import { DeckGlJsonChart, PropsWithHeight } from "./DeckGlJsonChart"
 
-const getProps = (elementProps: object = {}): PropsWithHeight => ({
-  element: fromJS({
-    json: `{"initialViewState": {"bearing": -27.36, "latitude": 52.2323, "longitude": -1.415, "maxZoom": 15, "minZoom": 5, "pitch": 40.5, "zoom": 6}, "layers": [{"@@type": "HexagonLayer", "autoHighlight": true, "coverage": 1, "data": "https://raw.githubusercontent.com/uber-common/deck.gl-data/master/examples/3d-heatmap/heatmap-data.csv", "elevationRange": [0, 3000], "elevationScale": 50, "extruded": true, "getPosition": "@@=[lng, lat]", "id": "0533490f-fcf9-4dc0-8c94-ae4fbd42eb6f", "pickable": true}], "mapStyle": "mapbox://styles/mapbox/light-v9", "views": [{"@@type": "MapView", "controller": true}]}`,
-    ...elementProps,
-  }),
-  width: 0,
-  mapboxToken: "mapboxToken",
-  height: undefined,
-})
+const getProps = (
+  elementProps: object = {},
+  initialViewStateProps: object = {}
+): PropsWithHeight => {
+  let json = {
+    initialViewState: {
+      bearing: -27.36,
+      latitude: 52.2323,
+      longitude: -1.415,
+      maxZoom: 15,
+      minZoom: 5,
+      pitch: 40.5,
+      zoom: 6,
+    },
+    layers: [
+      {
+        "@@type": "HexagonLayer",
+        autoHighlight: true,
+        coverage: 1,
+        data:
+          "https://raw.githubusercontent.com/uber-common/deck.gl-data/master/examples/3d-heatmap/heatmap-data.csv",
+        elevationRange: [0, 3000],
+        elevationScale: 50,
+        extruded: true,
+        getPosition: "@@=[lng, lat]",
+        id: "0533490f-fcf9-4dc0-8c94-ae4fbd42eb6f",
+        pickable: true,
+      },
+    ],
+    mapStyle: "mapbox://styles/mapbox/light-v9",
+    views: [{ "@@type": "MapView", controller: true }],
+  }
+  json.initialViewState = {
+    ...json.initialViewState,
+    ...initialViewStateProps,
+  }
+
+  return {
+    element: fromJS({
+      json: JSON.stringify(json),
+      ...elementProps,
+    }),
+    width: 0,
+    mapboxToken: "mapboxToken",
+    height: undefined,
+  }
+}
 
 describe("DeckGlJsonChart element", () => {
   it("renders without crashing", () => {
@@ -38,6 +76,49 @@ describe("DeckGlJsonChart element", () => {
     const wrapper = shallow(<DeckGlJsonChart {...props} />)
 
     expect(wrapper.find(DeckGL).length).toBe(1)
+  })
+
+  it("merges client and server changes", () => {
+    const props = getProps()
+    const wrapper = shallow(<DeckGlJsonChart {...props} />)
+    const DeckGL = wrapper.find("DeckGL")
+
+    expect(DeckGL.length).toBe(1)
+    expect(DeckGL.prop("onViewStateChange")).toBeDefined()
+
+    // @ts-ignore
+    DeckGL.prop("onViewStateChange")({
+      viewState: {
+        bearing: 5,
+        latitude: 5,
+        longitude: 5,
+        maxZoom: 5,
+        minZoom: 5,
+        pitch: 5,
+        zoom: 5,
+      },
+    })
+
+    wrapper.setProps(
+      getProps(
+        {},
+        {
+          pitch: 40.5,
+          zoom: 10,
+        }
+      )
+    )
+
+    // @ts-ignore
+    expect(wrapper.state("viewState")).toStrictEqual({
+      bearing: 5,
+      latitude: 5,
+      longitude: 5,
+      maxZoom: 5,
+      minZoom: 5,
+      pitch: 5,
+      zoom: 10,
+    })
   })
 
   it("should render tooltip", () => {
