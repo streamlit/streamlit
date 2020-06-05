5cf8de3713f2dd99c86e3ac7950d12af615afb9d
Randy Zwitch <rzwitch@gmail.com>
2020-06-05 15:05:00 -0400

    Make CSS variables

--- docs/conf.py
+++ docs/conf.py
@@ -105,7 +105,7 @@ html_theme = "sphinx_rtd_theme"
 html_theme_options = {
     "analytics_id": "UA-122023594-3",
     "logo_only": True,
-    "style_nav_header_background": "#F3F6FF",
+    "style_nav_header_background": "#f3f6f6",
 }
 
 # Add any paths that contain custom static files (such as style sheets) here,
