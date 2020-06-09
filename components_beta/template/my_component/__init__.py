475b4fd70d00a54c15729d3eba1ad26ba876a128
Tim Conkling <tconkling@gmail.com>
2020-06-09 12:26:22 -0700

    template: use os.path

--- components_beta/template/my_component/__init__.py
+++ components_beta/template/my_component/__init__.py
@@ -1,16 +1,16 @@
+import os
 import streamlit as st
-from pathlib import Path
 
-# Declare a Streamlit component.
-# It will be served by the local webpack dev server that you can
-# run via `npm run start`.
+# Declare a Streamlit component. It will be served by the local dev server
+# that you run via `npm run start`.
 _my_component = st.declare_component(url="http://localhost:3001")
 
 # When you're ready to distribute a production version of the component,
-# register the component's static files via the `path` param:
-# parent_folder = Path(__file__).parent.absolute()
-# frontend = str(parent_folder) + "/frontend/build"
-# _my_component = st.declare_component(path=frontend)
+# replace the `url` param with `path`, and point it to to the component's
+# build directory:
+# parent_dir = os.path.dirname(os.path.abspath(__file__))
+# build_dir = os.path.join(parent_dir, "frontend/build")
+# _my_component = st.declare_component(path=build_dir)
 
 # Add a wrapper function to the component.
 # This is an optional step that lets you customize your component's
