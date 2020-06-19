4c09dab62478936bc570493c89b935e1719f4df5
karrie <info.karrie@gmail.com>
2020-06-19 17:46:06 -0400

    Update lib/streamlit/server/routes.py
    Co-authored-by: Amey Deshpande <10554902+Amey-D@users.noreply.github.com>

--- lib/streamlit/server/routes.py
+++ lib/streamlit/server/routes.py
@@ -136,7 +136,7 @@ class HealthHandler(_SpecialRequestHandler):
             # Tornado will set the _xsrf cookie automatically for the page on
             # request for the document. However, if the server is reset and
             # server.enableCSRF is updated, the browser does not reload the document.
-            # Manually setting the cooking on /healthz since it is pinged when the
+            # Manually setting the cookie on /healthz since it is pinged when the
             # browser is disconnected from the server.
             if config.get_option("server.enableCSRF"):
                 self.set_cookie("_xsrf", self.xsrf_token)
