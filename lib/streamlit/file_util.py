fbcbcbe6b171ab19a58a8f5ed2eda87b71be3888
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2020-06-26 01:56:54 +0400

    Public assets

--- lib/streamlit/file_util.py
+++ lib/streamlit/file_util.py
@@ -121,6 +121,12 @@ def get_static_dir():
     return os.path.normpath(os.path.join(dirname, "static"))
 
 
+def get_assets_dir():
+    """Get the folder where static assets live."""
+    dirname = os.path.dirname(os.path.normpath(__file__))
+    return os.path.normpath(os.path.join(dirname, "static/assets"))
+
+
 def get_streamlit_file_path(*filepath):
     """Return the full path to a file in ~/.streamlit.
 
