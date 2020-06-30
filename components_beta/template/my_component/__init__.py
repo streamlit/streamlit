2f9d7675563174947988c72bdac43cf9cb57fe55
Tim Conkling <tconkling@gmail.com>
2020-06-30 16:24:10 -0700

    namespace custom components into `streamlit.components.v1`

--- components_beta/template/my_component/__init__.py
+++ components_beta/template/my_component/__init__.py
@@ -1,5 +1,5 @@
 import os
-import streamlit as st
+from streamlit.components.v1 import declare_component
 
 # Create a _RELEASE constant. We'll set this to False while we're developing
 # the component, and True when we're ready to package and distribute it.
@@ -7,19 +7,19 @@ import streamlit as st
 # release process.)
 _RELEASE = False
 
-# Declare a Streamlit component. `st.declare_component` returns a function
+# Declare a Streamlit component. `declare_component` returns a function
 # that is used to create instances of the component. We're naming this
 # function "_component_func", with an underscore prefix, because we don't want
 # to expose it directly to users. Instead, we will create a custom wrapper
 # function, below, that will serve as our component's public API.
 
-# It's worth noting that this call to `st.declare_component` is the
+# It's worth noting that this call to `declare_component` is the
 # *only thing* you need to do to create the binding between Streamlit and
 # your component frontend. Everything else we do in this file is simply a
 # best practice.
 
 if not _RELEASE:
-    _component_func = st.declare_component(
+    _component_func = declare_component(
         # We give the component a simple, descriptive name ("my_component"
         # does not fit this bill, so please choose something better for your
         # own component :)
@@ -35,12 +35,12 @@ else:
     # build directory:
     parent_dir = os.path.dirname(os.path.abspath(__file__))
     build_dir = os.path.join(parent_dir, "frontend/build")
-    _component_func = st.declare_component("my_component", path=build_dir)
+    _component_func = declare_component("my_component", path=build_dir)
 
 
 # Create a wrapper function for the component. This is an optional
 # best practice - we could simply expose the component function returned by
-# `st.declare_component` and call it done. The wrapper allows us to customize
+# `declare_component` and call it done. The wrapper allows us to customize
 # our component's API: we can pre-process its input args, post-process its
 # output value, and add a docstring for users.
 def my_component(name, key=None):
@@ -81,6 +81,8 @@ def my_component(name, key=None):
 # During development, we can run this just as we would any other Streamlit
 # app: `$ streamlit run my_component/__init__.py`
 if not _RELEASE:
+    import streamlit as st
+
     st.subheader("Component with constant args")
 
     # Create an instance of our component with a constant `name` arg, and
