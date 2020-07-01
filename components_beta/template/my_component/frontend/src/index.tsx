15eb21c3178af6850e1ba6dd1466aff269af3307
Tim Conkling <tconkling@gmail.com>
2020-06-30 17:00:01 -0700

    Streamlit.loadStreamlitCSS is a public, opt-in function

--- components_beta/template/my_component/frontend/src/index.tsx
+++ components_beta/template/my_component/frontend/src/index.tsx
@@ -1,6 +1,10 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import MyComponent from "./MyComponent"
+import { Streamlit } from "./streamlit"
+
+// Load the default Streamlit CSS
+Streamlit.loadStreamlitCSS()
 
 ReactDOM.render(
   <React.StrictMode>
