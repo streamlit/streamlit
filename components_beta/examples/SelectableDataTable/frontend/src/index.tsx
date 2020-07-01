457006417b976b5995336f0085616d6d9df6b72e
Tim Conkling <tconkling@gmail.com>
2020-07-01 12:44:05 -0700

    Remove Streamlit.loadStreamlitCSS

--- components_beta/examples/SelectableDataTable/frontend/src/index.tsx
+++ components_beta/examples/SelectableDataTable/frontend/src/index.tsx
@@ -3,7 +3,4 @@ import ReactDOM from "react-dom"
 import SelectableDataTable from "./SelectableDataTable"
 import { Streamlit } from "./streamlit"
 
-// Load the default Streamlit CSS
-Streamlit.loadStreamlitCSS()
-
 ReactDOM.render(<SelectableDataTable />, document.getElementById("root"))
