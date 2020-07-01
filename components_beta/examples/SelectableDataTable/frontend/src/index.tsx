cf62eba83521f003a28b2f8bd8c1c4d02414be2a
Tim Conkling <tconkling@gmail.com>
2020-07-01 14:21:10 -0700

    Remove unused import

--- components_beta/examples/SelectableDataTable/frontend/src/index.tsx
+++ components_beta/examples/SelectableDataTable/frontend/src/index.tsx
@@ -1,6 +1,5 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import SelectableDataTable from "./SelectableDataTable"
-import { Streamlit } from "./streamlit"
 
 ReactDOM.render(<SelectableDataTable />, document.getElementById("root"))
