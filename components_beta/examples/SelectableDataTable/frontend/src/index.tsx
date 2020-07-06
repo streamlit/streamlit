d837a6ddaf76dfd2f23968ff28d4fb4d41093079
Tim Conkling <tconkling@gmail.com>
2020-07-06 14:18:16 -0700

    Remove the components_beta directory
    Component templates + examples are now in https://github.com/streamlit/component-template

--- components_beta/examples/SelectableDataTable/frontend/src/index.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react"
-import ReactDOM from "react-dom"
-import SelectableDataTable from "./SelectableDataTable"
-
-ReactDOM.render(<SelectableDataTable />, document.getElementById("root"))
