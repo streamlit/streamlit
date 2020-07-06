504dbed5ef704c275faf28fd2a7ebd986382032f
Randy Zwitch <rzwitch@gmail.com>
2020-07-06 15:53:17 -0400

    Add icon for external links

--- docs/conf.py
+++ docs/conf.py
@@ -108,6 +108,7 @@ html_theme_options = {
     "analytics_id": "UA-122023594-3",
     "logo_only": True,
     "style_nav_header_background": "#ffffff",
+    "style_external_links": True,
 }
 
 # Add any paths that contain custom static files (such as style sheets) here,
