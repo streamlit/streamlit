66b6c75574f4e1e5c69a31b198ad4db900e516a7
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2020-07-08 23:44:55 +0400

    E2E tests for custom components

--- e2e/scripts/components_declare_component.py
+++ e2e/scripts/components_declare_component.py
@@ -15,7 +15,6 @@
 import streamlit.components.v1 as components
 
 url = "http://not.a.real.url"
-test_component = components.declare_component(
-    "test_component", url=url)
+test_component = components.declare_component("test_component", url=url)
 
 test_component()
