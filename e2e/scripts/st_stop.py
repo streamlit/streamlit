91876ae8ce08201859b3a019aa1b94d725216184
Ken McGrady <ken.mcgrady@gmail.com>
2020-07-28 16:41:10 -0700

    Fixed Integration Test

--- e2e/scripts/st_stop.py
+++ e2e/scripts/st_stop.py
@@ -16,6 +16,9 @@ import streamlit as st
 
 st.text("Text before stop")
 
-st.stop()
+# Since st.stop() throws an intentional exception, we want this to run
+# only in streamlit
+if st._is_running_with_streamlit:
+    st.stop()
 
 st.text("Text after stop")
