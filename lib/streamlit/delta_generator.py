9074936ad9faa13c71213a2f6792dcc9f28bf373
Austin Chen <akrolsmir@gmail.com>
2020-07-29 04:21:52 -0700

    Extract audio and video

--- lib/streamlit/delta_generator.py
+++ lib/streamlit/delta_generator.py
@@ -63,6 +63,7 @@ from streamlit.elements.deck_gl import DeckGlMixin
 from streamlit.elements.deck_gl_json_chart import PydeckMixin
 from streamlit.elements.map import MapMixin
 from streamlit.elements.iframe_proto import IframeMixin
+from streamlit.elements.media_proto import MediaMixin
 
 LOGGER = get_logger(__name__)
 
@@ -219,6 +220,7 @@ class DeltaGenerator(
     IframeMixin,
     MarkdownMixin,
     MapMixin,
+    MediaMixin,
     PlotlyMixin,
     PydeckMixin,
     JsonMixin,
@@ -691,84 +693,6 @@ class DeltaGenerator(
         width = -1  # Always use full width for favicons
         element.favicon.url = image_proto.image_to_url(
             image, width, clamp, channels, format, image_id="favicon", allow_emoji=True
-        )
-
-    @_with_element
-    def audio(self, element, data, format="audio/wav", start_time=0):
-        """Display an audio player.
-
-        Parameters
-        ----------
-        data : str, bytes, BytesIO, numpy.ndarray, or file opened with
-                io.open().
-            Raw audio data, filename, or a URL pointing to the file to load.
-            Numpy arrays and raw data formats must include all necessary file
-            headers to match specified file format.
-        start_time: int
-            The time from which this element should start playing.
-        format : str
-            The mime type for the audio file. Defaults to 'audio/wav'.
-            See https://tools.ietf.org/html/rfc4281 for more info.
-
-        Example
-        -------
-        >>> audio_file = open('myaudio.ogg', 'rb')
-        >>> audio_bytes = audio_file.read()
-        >>>
-        >>> st.audio(audio_bytes, format='audio/ogg')
-
-        .. output::
-           https://share.streamlit.io/0.25.0-2JkNY/index.html?id=Dv3M9sA7Cg8gwusgnVNTHb
-           height: 400px
-
-        """
-        from .elements import media_proto
-
-        media_proto.marshall_audio(
-            self._get_coordinates(), element.audio, data, format, start_time
-        )
-
-    @_with_element
-    def video(self, element, data, format="video/mp4", start_time=0):
-        """Display a video player.
-
-        Parameters
-        ----------
-        data : str, bytes, BytesIO, numpy.ndarray, or file opened with
-                io.open().
-            Raw video data, filename, or URL pointing to a video to load.
-            Includes support for YouTube URLs.
-            Numpy arrays and raw data formats must include all necessary file
-            headers to match specified file format.
-        format : str
-            The mime type for the video file. Defaults to 'video/mp4'.
-            See https://tools.ietf.org/html/rfc4281 for more info.
-        start_time: int
-            The time from which this element should start playing.
-
-        Example
-        -------
-        >>> video_file = open('myvideo.mp4', 'rb')
-        >>> video_bytes = video_file.read()
-        >>>
-        >>> st.video(video_bytes)
-
-        .. output::
-           https://share.streamlit.io/0.61.0-yRE1/index.html?id=LZLtVFFTf1s41yfPExzRu8
-           height: 600px
-
-        .. note::
-           Some videos may not display if they are encoded using MP4V (which is an export option in OpenCV), as this codec is
-           not widely supported by browsers. Converting your video to H.264 will allow the video to be displayed in Streamlit.
-           See this `StackOverflow post <https://stackoverflow.com/a/49535220/2394542>`_ or this
-           `Streamlit forum post <https://discuss.streamlit.io/t/st-video-doesnt-show-opencv-generated-mp4/3193/2>`_
-           for more information.
-
-        """
-        from .elements import media_proto
-
-        media_proto.marshall_video(
-            self._get_coordinates(), element.video, data, format, start_time
         )
 
     @_with_element
