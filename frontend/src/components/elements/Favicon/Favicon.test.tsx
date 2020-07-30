6c6dfab0719712cf33a5e58b66ce87e23c2703d2
Austin Chen <akrolsmir@gmail.com>
2020-07-30 11:23:48 -0700

    Update favicon unit tests

--- frontend/src/components/elements/Favicon/Favicon.test.tsx
+++ frontend/src/components/elements/Favicon/Favicon.test.tsx
@@ -15,38 +15,7 @@
  * limitations under the License.
  */
 
-import React from "react"
-import { mount } from "enzyme"
-import { fromJS } from "immutable"
-
-import { Favicon, Props } from "./Favicon"
-
-const getProps = (elementProps: Record<string, unknown> = {}): Props => ({
-  element: fromJS({
-    url: "https://streamlit.io/path/to/favicon.png",
-    ...elementProps,
-  }),
-})
-
-// Overwrite bounding box code for testing (https://stackoverflow.com/a/52146902)
-const mockedRect = {
-  x: 0,
-  y: 0,
-  bottom: 0,
-  left: 0,
-  right: 0,
-  top: 0,
-  width: 10,
-  height: 10,
-  toJSON: () => "",
-}
-const originalGetBBox = SVGElement.prototype.getBBox
-beforeEach(() => {
-  SVGElement.prototype.getBBox = () => mockedRect
-})
-afterEach(() => {
-  SVGElement.prototype.getBBox = originalGetBBox
-})
+import { handleFavicon } from "./Favicon"
 
 function getFaviconHref(): string {
   const faviconElement: HTMLLinkElement | null = document.querySelector(
@@ -57,61 +26,36 @@ function getFaviconHref(): string {
 
 document.head.innerHTML = `<link rel="shortcut icon" href="default.png">`
 
+const PIZZA_TWEMOJI_URL = "https://twemoji.maxcdn.com/2/72x72/1f355.png"
+
 test("is set up with the default favicon", () => {
   expect(getFaviconHref()).toBe("http://localhost/default.png")
 })
 
 describe("Favicon element", () => {
-  it("renders without crashing", () => {
-    const props = getProps()
-    const wrapper = mount(<Favicon {...props} />)
-
-    expect(wrapper.find(".stHidden")).toBeTruthy()
-  })
-
   it("should set the favicon in the DOM", () => {
-    const props = getProps()
-    mount(<Favicon {...props} />)
-
+    handleFavicon("https://streamlit.io/path/to/favicon.png")
     expect(getFaviconHref()).toBe("https://streamlit.io/path/to/favicon.png")
   })
 
   it("should accept /media urls from backend", () => {
-    const props = getProps({ url: "/media/1234567890.png" })
-    mount(<Favicon {...props} />)
-
+    handleFavicon("/media/1234567890.png")
     expect(getFaviconHref()).toBe("http://localhost/media/1234567890.png")
   })
 
-  const PIZZA_EMOJI_SVG = `data:image/svg+xml,
-  <svg version="1.2" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
-    <text
-      style="transform: translate(50%, 50%) scale(10)"
-      dominant-baseline="central"
-      text-anchor="middle">
-      %F0%9F%8D%95
-    </text>
-  </svg>`.replace(/\n/g, "")
-
   it("should accept emojis directly", () => {
-    const props = getProps({ url: "🍕" })
-    mount(<Favicon {...props} />)
-
-    expect(getFaviconHref()).toBe(PIZZA_EMOJI_SVG)
+    handleFavicon("🍕")
+    expect(getFaviconHref()).toBe(PIZZA_TWEMOJI_URL)
   })
 
   it("should accept emoji shortcodes", () => {
-    const props = getProps({ url: ":pizza:" })
-    mount(<Favicon {...props} />)
-
-    expect(getFaviconHref()).toBe(PIZZA_EMOJI_SVG)
+    handleFavicon(":pizza:")
+    expect(getFaviconHref()).toBe(PIZZA_TWEMOJI_URL)
   })
 
-  it("should change the favicon when the props change", () => {
-    const props = getProps()
-    const wrapper = mount(<Favicon {...props} />)
-    wrapper.setProps(getProps({ url: "🍕" }))
-
-    expect(getFaviconHref()).toBe(PIZZA_EMOJI_SVG)
+  it("should update the favicon when it changes", () => {
+    handleFavicon("/media/1234567890.png")
+    handleFavicon(":pizza:")
+    expect(getFaviconHref()).toBe(PIZZA_TWEMOJI_URL)
   })
 })
