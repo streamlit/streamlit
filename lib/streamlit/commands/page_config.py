9ae71cf23fec029147e2713752fe4fb7b199fc48
Austin Chen <akrolsmir@gmail.com>
2020-07-31 11:46:27 -0700

    Write docs for set_page_config

--- lib/streamlit/commands/page_config.py
+++ lib/streamlit/commands/page_config.py
@@ -23,29 +23,43 @@ def set_page_config(
     page_title=None, page_icon=None, layout="centered", initial_sidebar_state="auto"
 ):
     """
-    TODO docstring
+    Configures the default settings of the page.
 
-    Set the page favicon to the specified image or emoji.
-    You can use the same image parameters as `st.image`, like URLs or numpy arrays.
-    You can also use an emoji as a favicon, either directly or with a shortcode [1].
-    Favicon emojis are loaded from a CDN, courtesy of Twitter and MaxCDN [2].
-    [1] https://www.webfx.com/tools/emoji-cheat-sheet/
-    [2] https://twemoji.twitter.com/
-    Note: This is a beta feature. See
-    https://docs.streamlit.io/en/latest/pre_release_features.html for more
-    information.
+    .. note::
+        This must be the first Streamlit command used in your app, and must only
+        be set once.
+
+        This is a beta feature. See
+        https://docs.streamlit.io/en/latest/pre_release_features.html for more
+        information.
+
     Parameters
     ----------
-    image : numpy.ndarray, [numpy.ndarray], BytesIO, str, or [str]
-        Monochrome image of shape (w,h) or (w,h,1)
-        OR a color image of shape (w,h,3)
-        OR an RGBA image of shape (w,h,4)
-        OR a URL to fetch the image from
-        OR an emoji like '🦈'
-        OR an emoji shortcode like ':shark:'
+    page_title: str or None
+        The page title, shown in the browser tab.
+        Strings get appended with "• Streamlit".
+    page_icon : Anything supported by st.image or str or None
+        The page favicon.
+        Besides the types supported by `st.image` (like URLs or numpy arrays),
+        you can pass in an emoji as a string ("🦈") or a shortcode (":shark:").
+        Emoji icons are courtesy of Twemoji and loaded from MaxCDN.
+    layout: "centered" or "wide"
+        How the page content should be laid out.
+        "centered" constrains the elements into a centered column of fixed width;
+        "wide" uses the entire screen.
+    initial_sidebar_state: "auto" or "expanded" or "collapsed"
+        How the sidebar should start out.
+        "auto" hides the sidebar on mobile-sized devices, and shows on desktop.
+        "expanded" shows the sidebar initially; "collapsed" hides it.
+
     Example
     -------
-    >>> st.beta_set_favicon('https://docs.streamlit.io/en/latest/_static/logomark_website.png')
+    >>> st.beta_set_page_config(
+    ...     page_title="Ex-stream-ly Cool App",
+    ...     page_icon="🧊",
+    ...     layout="wide",
+    ...     initial_sidebar_state="expanded",
+    ... )
     """
 
     msg = ForwardMsg_pb2.ForwardMsg()
