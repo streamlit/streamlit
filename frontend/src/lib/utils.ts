bbc3f6f586865c228f0136548a6f27816431c481
wusker <s.bergellio@gmail.com>
2020-08-12 18:29:54 -0600

    Add functionality for heading anchors (#824)

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -160,8 +160,8 @@ export function slugify(text: string | undefined): string | undefined {
     .toString()
     .toLowerCase()
     .replace(/\s+/g, "-") // Replace spaces with -
-    .replace(/[^\w\-]+/g, "") // Remove all non-word chars
-    .replace(/\-\-+/g, "-") // Replace multiple - with single -
+    .replace(/[^\w-]+/g, "") // Remove all non-word chars
+    .replace(/--+/g, "-") // Replace multiple - with single -
     .replace(/^-+/, "") // Trim - from start of text
     .replace(/-+$/, "") // Trim - from end of text
 }
