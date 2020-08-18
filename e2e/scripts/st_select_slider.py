9f826d1b633d42d0535f76b6dd59f8480992cfa0
Ken McGrady <ken@streamlit.io>
2020-08-18 13:36:35 -0700

    Introduce Select Slider which creates a slider over discrete values (#1849)

---
+++ e2e/scripts/st_select_slider.py
@@ -0,0 +1,22 @@
+# Copyright 2018-2020 Streamlit Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import streamlit as st
+
+w1 = st.select_slider(
+    "Label 1",
+    value=("orange", "blue"),
+    options=["red", "orange", "yellow", "green", "blue", "indigo", "violet"],
+)
+st.write("Value 1:", w1)
