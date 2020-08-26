7bbe2daeb61c77f26dfd92ec03457520522d713d
karrie <karrie@streamlit.io>
2020-08-26 13:36:45 -0400

    Reformat with latest black version (#1925)

--- e2e/scripts/st_experimental_set_query_params.py
+++ e2e/scripts/st_experimental_set_query_params.py
@@ -18,5 +18,7 @@ set_query_params = st.button("Set current query params")
 
 if set_query_params:
     st.experimental_set_query_params(
-        show_map=True, number_of_countries=2, selected=["asia", "america"],
+        show_map=True,
+        number_of_countries=2,
+        selected=["asia", "america"],
     )
