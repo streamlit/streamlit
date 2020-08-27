5f27305062a3a309c95c97c3a300430f073c54c5
masa3141 <masahiro@kazama.tv>
2020-08-27 21:52:24 +0900

    use VirtualDropdown

--- frontend/src/components/widgets/Multiselect/Multiselect.tsx
+++ frontend/src/components/widgets/Multiselect/Multiselect.tsx
@@ -20,7 +20,20 @@ import without from "lodash/without"
 import { Map as ImmutableMap } from "immutable"
 import { multiSelectOverrides } from "lib/widgetTheme"
 import { WidgetStateManager, Source } from "lib/WidgetStateManager"
-import { TYPE, Select as UISelect, OnChangeParams } from "baseui/select"
+import {
+  TYPE,
+  Select as UISelect,
+  OnChangeParams,
+  StyledDropdownListItem,
+} from "baseui/select"
+import { withStyle } from "baseui"
+import { StyledList, StyledEmptyState, OptionListProps } from "baseui/menu"
+import { FixedSizeList } from "react-window"
+
+const LIST_ITEM_HEIGHT = 40
+const EMPTY_LIST_HEIGHT = 80
+const MAX_LIST_HEIGHT = 300
+const PADDING = 16
 
 export interface Props {
   disabled: boolean
@@ -40,6 +53,73 @@ interface MultiselectOption {
   label: string
   value: string
 }
+
+const ListItem = withStyle(StyledDropdownListItem, {
+  paddingTop: 0,
+  paddingBottom: 0,
+  display: "flex",
+  alignItems: "center",
+})
+
+const FixedSizeListItem = ({
+  data,
+  index,
+  style,
+}: {
+  data: { props: OptionListProps }[]
+  index: number
+  style: React.CSSProperties
+}) => {
+  const { item, overrides, ...restChildProps } = data[index].props
+  return (
+    <ListItem
+      key={item.value}
+      style={{
+        boxSizing: "border-box",
+        ...style,
+      }}
+      {...restChildProps}
+    >
+      {item.label}
+    </ListItem>
+  )
+}
+
+const VirtualDropdown = React.forwardRef((props: any, ref) => {
+  const children = React.Children.toArray(
+    props.children
+  ) as React.ReactElement[]
+
+  if (!children[0] || !children[0].props.item) {
+    return (
+      <StyledList $style={{ height: EMPTY_LIST_HEIGHT + "px" }} ref={ref}>
+        <StyledEmptyState {...children[0].props} />
+      </StyledList>
+    )
+  }
+
+  const height = Math.min(
+    MAX_LIST_HEIGHT,
+    children.length * LIST_ITEM_HEIGHT + PADDING
+  )
+
+  return (
+    <StyledList $style={{ height: height + "px" }} ref={ref}>
+      <FixedSizeList
+        width="100%"
+        height={height}
+        itemCount={children.length}
+        itemData={children}
+        itemKey={(index: number, data: { props: OptionListProps }[]) =>
+          data[index].props.item.value
+        }
+        itemSize={LIST_ITEM_HEIGHT}
+      >
+        {FixedSizeListItem}
+      </FixedSizeList>
+    </StyledList>
+  )
+})
 
 class Multiselect extends React.PureComponent<Props, State> {
   public state: State = {
@@ -120,7 +200,9 @@ class Multiselect extends React.PureComponent<Props, State> {
           value={this.valueFromState}
           disabled={disabled}
           size={"compact"}
-          overrides={multiSelectOverrides}
+          overrides={Object.assign(multiSelectOverrides, {
+            Dropdown: { component: VirtualDropdown },
+          })}
         />
       </div>
     )
