ee739f7d03989ae07860a861241b9ee03f8fdb29
Tim Conkling <tconkling@gmail.com>
2020-09-08 16:38:36 -0700

    Remove bytes_test project

--- bytes_test/test.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-import streamlit.components.v1 as components
-
-
-bytes_test = components.declare_component("bytes_test", url="http://localhost:3001")
-
-return_value = bytes_test(name="Test", bytes="qwert".encode())
-st.write(return_value)
