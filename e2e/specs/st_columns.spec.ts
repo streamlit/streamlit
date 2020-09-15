e7929753e95b0bd9c67d2487c54c95b4b3e3e586
Austin Chen <akrolsmir@gmail.com>
2020-09-15 14:38:33 -0700

    Add a e2e test for columns

---
+++ e2e/specs/st_columns.spec.ts
@@ -0,0 +1,51 @@
+/**
+ * @license
+ * Copyright 2018-2020 Streamlit Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/// <reference types="cypress" />
+
+describe("st.column", () => {
+  before(() => {
+    cy.visit("http://localhost:3000/");
+  });
+
+  it("creates 3 equal-width columns", () => {
+    cy.get(".stBlock-horiz stBlock")
+      .eq(0)
+      .should("have.attr", "flex", "1");
+    cy.get(".stBlock-horiz stBlock")
+      .eq(1)
+      .should("have.attr", "flex", "1");
+    cy.get(".stBlock-horiz stBlock")
+      .eq(2)
+      .should("have.attr", "flex", "1");
+  });
+
+  it("creates 4 variable-width columns", () => {
+    cy.get(".stBlock-horiz stBlock")
+      .eq(3)
+      .should("have.attr", "flex", "1");
+    cy.get(".stBlock-horiz stBlock")
+      .eq(4)
+      .should("have.attr", "flex", "2");
+    cy.get(".stBlock-horiz stBlock")
+      .eq(5)
+      .should("have.attr", "flex", "4");
+    cy.get(".stBlock-horiz stBlock")
+      .eq(6)
+      .should("have.attr", "flex", "8");
+  });
+});
