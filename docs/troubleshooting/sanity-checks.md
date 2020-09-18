97932aeb85d564de0b07adb1cadff25f14ba2468
CircleCI <jonathan@streamlit.io>
2020-09-18 06:39:40 +0000

    Update version and project name in files

--- docs/troubleshooting/sanity-checks.md
+++ docs/troubleshooting/sanity-checks.md
@@ -26,7 +26,7 @@ $ pip install --upgrade streamlit
 $ streamlit version
 ```
 
-...and then verify that the version number printed is `0.67.0`.
+...and then verify that the version number printed is `0.67.1.dev20200917`.
 
 **Try reproducing the issue now.** If not fixed, keep reading on.
 
--- lib/setup.py
+++ lib/setup.py
@@ -8,9 +8,9 @@ from pipenv.project import Project
 from pipenv.utils import convert_deps_to_pip
 from setuptools.command.install import install
 
-VERSION = "0.67.0"  # PEP-440
+VERSION = "0.67.1.dev20200917"  # PEP-440
 
-NAME = "streamlit"
+NAME = "streamlit-nightly"
 
 DESCRIPTION = "The fastest way to build data apps in Python"
 
