4a55a43c1ad75b280f25bfd4a96657a39193236f
Austin Chen <akrolsmir@gmail.com>
2020-10-10 13:29:59 -0700

    Fix formatting of st.file_uploader docstring (#2141)
    Before:
    ![](https://i.imgur.com/O2nxkls.png)
    
    After:
    ![](https://i.imgur.com/5USjCF8.png)
    
    (incidentally, I hate everything about python's docstring code formatting)

--- lib/streamlit/elements/file_uploader.py
+++ lib/streamlit/elements/file_uploader.py
@@ -56,15 +56,15 @@ class FileUploaderMixin:
         ...     # To read file as bytes:
         ...     bytes_data = uploaded_file.read()
         ...     st.write(bytes_data)
-
+        >>>
         ...     # To convert to a string based IO:
         ...     stringio = StringIO(uploaded_file.decode("utf-8"))
         ...     st.write(stringio)
-
+        >>>
         ...     # To read file as string:
         ...     string_data = stringio.read()
         ...     st.write(string_data)
-
+        >>>
         ...     # Can be used wherever a "file-like" object is accepted:
         ...     dataframe = pd.read_csv(uploaded_file)
         ...     st.write(dataframe)
