d550029f62da5bb0e582aabfafaddd472444e872
Tim Conkling <tconkling@gmail.com>
2020-10-12 17:56:53 -0700

    ReportNode.test.ts

--- frontend/src/lib/ReportNode.test.ts
+++ frontend/src/lib/ReportNode.test.ts
@@ -15,7 +15,91 @@
  * limitations under the License.
  */
 
-import { ElementNode, ReportRoot } from "./ReportNode"
+import {
+  Block as BlockProto,
+  BlockPath,
+  DataFrame as DataFrameProto,
+  Delta as DeltaProto,
+  Element,
+  ForwardMsgMetadata,
+} from "autogen/proto"
+import mockDataFrameData from "components/elements/DataFrame/mock"
+import { Writer } from "protobufjs"
+import { toImmutableProto } from "./immutableProto"
+import { BlockNode, ElementNode, ReportNode, ReportRoot } from "./ReportNode"
+
+const NO_REPORT_ID = "NO_REPORT_ID"
+
+// prettier-ignore
+const BLOCK = block([
+  text("1"),
+  block([
+    text("2"),
+  ]),
+])
+
+const ROOT = new ReportRoot(BLOCK, BlockNode.empty())
+
+describe("ReportNode.getIn", () => {
+  it("handles shallow paths", () => {
+    const node = BLOCK.getIn([0])
+    expect(node).toBeTextNode("1")
+  })
+
+  it("handles deep paths", () => {
+    const node = BLOCK.getIn([1, 0])
+    expect(node).toBeTextNode("2")
+  })
+
+  it("returns undefined for invalid paths", () => {
+    const node = BLOCK.getIn([2, 3, 4])
+    expect(node).toBeUndefined()
+  })
+})
+
+describe("ReportNode.setIn", () => {
+  it("handles shallow paths", () => {
+    const newBlock = BLOCK.setIn([0], text("new"), NO_REPORT_ID)
+    expect(newBlock.getIn([0])).toBeTextNode("new")
+
+    // Check BLOCK..newBlock diff is as expected.
+    expect(newBlock).not.toStrictEqual(BLOCK)
+    expect(newBlock.getIn([1])).toStrictEqual(BLOCK.getIn([1]))
+  })
+
+  it("handles deep paths", () => {
+    const newBlock = BLOCK.setIn([1, 1], text("new"), NO_REPORT_ID)
+    expect(newBlock.getIn([1, 1])).toBeTextNode("new")
+
+    // Check BLOCK..newBlock diff is as expected
+    expect(newBlock).not.toStrictEqual(BLOCK)
+    expect(newBlock.getIn([0])).toStrictEqual(BLOCK.getIn([0]))
+    expect(newBlock.getIn([1])).not.toStrictEqual(BLOCK.getIn([1]))
+    expect(newBlock.getIn([1, 0])).toStrictEqual(BLOCK.getIn([1, 0]))
+    expect(newBlock.getIn([1, 1])).not.toStrictEqual(BLOCK.getIn([1, 1]))
+  })
+
+  it("throws an error for invalid paths", () => {
+    expect(() => BLOCK.setIn([1, 2], text("new"), NO_REPORT_ID)).toThrow(
+      "Bad 'setIn' index 2 (should be between [0, 1])"
+    )
+  })
+})
+
+describe("ElementNode.immutableElement", () => {
+  it("returns an immutableJS element", () => {
+    const node = text("ahoy!")
+    expect(node.immutableElement).toEqual(
+      toImmutableProto(Element, new Element({ text: { body: "ahoy!" } }))
+    )
+  })
+
+  it("does not recompute its value", () => {
+    // accessing `immutableElement` twice should return the same instance.
+    const node = text("ahoy!")
+    expect(node.immutableElement).toStrictEqual(node.immutableElement)
+  })
+})
 
 describe("ReportRoot.empty", () => {
   it("creates an empty tree", () => {
@@ -35,4 +119,223 @@ describe("ReportRoot.empty", () => {
   })
 })
 
-describe("ReportRoot.applyDelta", () => {})
+describe("ReportRoot.applyDelta", () => {
+  it("handles 'newElement' deltas", () => {
+    const delta = makeProto(DeltaProto, {
+      newElement: { text: { body: "newElement!" } },
+    })
+    const newRoot = ROOT.applyDelta(
+      "new_report_id",
+      delta,
+      forwardMsgMetadata(BlockPath.Container.MAIN, [1, 1])
+    )
+
+    const newNode = newRoot.main.getIn([1, 1]) as ElementNode
+    expect(newNode).toBeTextNode("newElement!")
+
+    // Check that our new reportID has been set only on the touched nodes
+    expect(newRoot.main.reportId).toBe("new_report_id")
+    expect(newRoot.main.getIn([0])?.reportId).toBe(NO_REPORT_ID)
+    expect(newRoot.main.getIn([1])?.reportId).toBe("new_report_id")
+    expect(newRoot.main.getIn([1, 0])?.reportId).toBe(NO_REPORT_ID)
+    expect(newRoot.main.getIn([1, 1])?.reportId).toBe("new_report_id")
+    expect(newRoot.sidebar.reportId).toBe(NO_REPORT_ID)
+  })
+
+  it("handles 'addBlock' deltas", () => {
+    const delta = makeProto(DeltaProto, { addBlock: {} })
+    const newRoot = ROOT.applyDelta(
+      "new_report_id",
+      delta,
+      forwardMsgMetadata(BlockPath.Container.MAIN, [1, 1])
+    )
+
+    const newNode = newRoot.main.getIn([1, 1]) as BlockNode
+    expect(newNode).toBeDefined()
+
+    // Check that our new reportID has been set only on the touched nodes
+    expect(newRoot.main.reportId).toBe("new_report_id")
+    expect(newRoot.main.getIn([0])?.reportId).toBe(NO_REPORT_ID)
+    expect(newRoot.main.getIn([1])?.reportId).toBe("new_report_id")
+    expect(newRoot.main.getIn([1, 0])?.reportId).toBe(NO_REPORT_ID)
+    expect(newRoot.main.getIn([1, 1])?.reportId).toBe("new_report_id")
+    expect(newRoot.sidebar.reportId).toBe(NO_REPORT_ID)
+  })
+
+  it("handles 'addRows' deltas", () => {
+    // Create a report with a dataframe node
+    const mockDataFrameProto = makeProto(DataFrameProto, mockDataFrameData)
+
+    const root = ReportRoot.empty().applyDelta(
+      "preAddRows",
+      makeProto(DeltaProto, { newElement: { dataFrame: mockDataFrameProto } }),
+      forwardMsgMetadata(BlockPath.Container.MAIN, [0])
+    )
+
+    const preAddRowsElement = root.main.getIn([0]) as ElementNode
+    expect(preAddRowsElement.reportId).toBe("preAddRows")
+
+    // Sanity check - ensure we have a dataframe.
+    expect(preAddRowsElement.immutableElement.get("dataFrame")).toEqual(
+      toImmutableProto(DataFrameProto, mockDataFrameProto)
+    )
+
+    // Add rows
+    const newRoot = root.applyDelta(
+      "postAddRows",
+      makeProto(DeltaProto, { addRows: { data: mockDataFrameProto } }),
+      forwardMsgMetadata(BlockPath.Container.MAIN, [0])
+    )
+
+    const addRowsElement = newRoot.main.getIn([0]) as ElementNode
+    expect(addRowsElement.reportId).toBe("postAddRows")
+
+    // Our new element should look like the result of calling `addRows`
+    // with the same dataframe data twice.
+    // const immutableElement = toImmutableProto(
+    //   Element,
+    //   makeProto(Element, { dataFrame: mockDataFrameProto })
+    // )
+    // const expectedData = addRows(immutableElement, immutableElement)
+    // expect(addRowsElement.immutableElement.get("dataFrame")).toEqual(
+    //   expectedData
+    // )
+    // TODO!
+  })
+})
+
+describe("ReportRoot.clearStaleNodes", () => {
+  it("clears stale nodes", () => {
+    // Add a new element and clear stale nodes
+    const delta = makeProto(DeltaProto, {
+      newElement: { text: { body: "newElement!" } },
+    })
+    const newRoot = ROOT.applyDelta(
+      "new_report_id",
+      delta,
+      forwardMsgMetadata(BlockPath.Container.MAIN, [1, 1])
+    ).clearStaleNodes("new_report_id")
+
+    // We should now only have a single element, inside a single block
+    expect(newRoot.main.getIn([0, 0])).toBeTextNode("newElement!")
+    expect(newRoot.getElements().size).toBe(1)
+  })
+
+  it("handles `allowEmpty` blocks correctly", () => {
+    // Create a tree with two blocks, one with allowEmpty: true, and the other
+    // with allowEmpty: false
+    const newRoot = ReportRoot.empty()
+      .applyDelta(
+        "new_report_id",
+        makeProto(DeltaProto, { addBlock: { allowEmpty: true } }),
+        forwardMsgMetadata(BlockPath.Container.MAIN, [0])
+      )
+      .applyDelta(
+        "new_report_id",
+        makeProto(DeltaProto, { addBlock: { allowEmpty: false } }),
+        forwardMsgMetadata(BlockPath.Container.MAIN, [1])
+      )
+
+    expect(newRoot.main.getIn([0])).toBeInstanceOf(BlockNode)
+    expect(newRoot.main.getIn([1])).toBeInstanceOf(BlockNode)
+
+    // Prune nodes. Only the `allowEmpty` node should remain.
+    const pruned = newRoot.clearStaleNodes("new_report_id")
+    expect(pruned.main.getIn([0])).toBeInstanceOf(BlockNode)
+    expect(pruned.main.getIn([1])).not.toBeDefined()
+  })
+})
+
+describe("ReportRoot.getElements", () => {
+  it("returns all elements", () => {
+    // We have elements at main.[0] and main.[1, 0]
+    expect(ROOT.getElements()).toEqual(
+      new Set([
+        (ROOT.main.getIn([0]) as ElementNode).element,
+        (ROOT.main.getIn([1, 0]) as ElementNode).element,
+      ])
+    )
+  })
+})
+
+/** Create a `Text` element node with the given properties. */
+function text(text: string, reportId = NO_REPORT_ID): ElementNode {
+  const element = makeProto(Element, { text: { body: text } })
+  return new ElementNode(element, ForwardMsgMetadata.create(), reportId)
+}
+
+/** Create a BlockNode with the given properties. */
+function block(
+  children: ReportNode[] = [],
+  reportId = NO_REPORT_ID
+): BlockNode {
+  return new BlockNode(children, makeProto(BlockProto, {}), reportId)
+}
+
+/** Create a ForwardMsgMetadata with the given container and path */
+function forwardMsgMetadata(
+  container: BlockPath.Container,
+  path: number[]
+): ForwardMsgMetadata {
+  return makeProto(ForwardMsgMetadata, {
+    deltaId: path[path.length - 1],
+    parentBlock: { container, path: path.slice(1) },
+  })
+}
+
+/**
+ * Make a "fully concrete" instance of a protobuf message.
+ * This function constructs a message and then encodes and decodes it as
+ * if it had arrived on the wire. This ensures that that it has all its
+ * 'oneOfs' and 'defaults' set.
+ */
+function makeProto<Type, Props>(
+  MessageType: {
+    new (props: Props): Type
+    encode: (message: Type, writer: Writer) => Writer
+    decode: (bytes: Uint8Array) => Type
+  },
+  properties: Props
+): Type {
+  const message = new MessageType(properties)
+  const bytes = MessageType.encode(message, Writer.create()).finish()
+  return MessageType.decode(bytes)
+}
+
+// Custom Jest matchers for dealing with ReportNodes
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace jest {
+    interface Matchers<R> {
+      toBeTextNode(text: string): R
+    }
+  }
+}
+
+expect.extend({
+  toBeTextNode(received, text): jest.CustomMatcherResult {
+    const elementNode = received as ElementNode
+    if (elementNode == null) {
+      return {
+        message: () => `expected ${received} to be an instance of ElementNode`,
+        pass: false,
+      }
+    }
+
+    const { type } = elementNode.element
+    if (type !== "text") {
+      return {
+        message: () =>
+          `expected ${received}.element.type to be 'text', but it was ${type}`,
+        pass: false,
+      }
+    }
+
+    const textBody = elementNode.element.text?.body
+    return {
+      message: () =>
+        `expected ${received}.element.text.body to be "${text}", but it was "${textBody}"`,
+      pass: textBody === text,
+    }
+  },
+})
