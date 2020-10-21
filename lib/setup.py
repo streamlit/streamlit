3e99658410033785994ba176690d8a072a3d7a12
CircleCI <jonathan@streamlit.io>
2020-10-21 06:33:12 +0000

    Update version and project name in files

--- lib/setup.py
+++ lib/setup.py
@@ -8,9 +8,9 @@ from pipenv.project import Project
 from pipenv.utils import convert_deps_to_pip
 from setuptools.command.install import install
 
-VERSION = "0.69.2"  # PEP-440
+VERSION = "0.69.3.dev20201020"  # PEP-440
 
-NAME = "streamlit"
+NAME = "streamlit-nightly"
 
 DESCRIPTION = "The fastest way to build data apps in Python"
 
