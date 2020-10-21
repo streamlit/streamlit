889400a6b9b6775b01d92c3ffe8bf715e3b25c4d
Tim Conkling <tconkling@gmail.com>
2020-10-21 16:26:08 -0700

    Simplify!

--- lib/streamlit/delta_generator.py
+++ lib/streamlit/delta_generator.py
@@ -243,10 +243,14 @@ class DeltaGenerator(
         return wrapper
 
     @property
-    def _parent_block_types(self) -> Iterable[Optional[str]]:
+    def _parent_block_types(self) -> Iterable[str]:
+        """Iterate all the block types used by this DeltaGenerator and all
+        its ancestor DeltaGenerators.
+        """
         current_dg: Optional[DeltaGenerator] = self
         while current_dg is not None:
-            yield current_dg._block_type
+            if current_dg._block_type is not None:
+                yield current_dg._block_type
             current_dg = current_dg._parent
 
     @property
