7a92fb324be7433901c693832eae7a629e56f880
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2020-10-25 23:50:28 -0400

    Close #1921

--- e2e/specs/st_alert.spec.js
+++ e2e/specs/st_alert.spec.js
@@ -18,24 +18,39 @@
 describe("st.error and friends", () => {
   before(() => {
     cy.visit("http://localhost:3000/");
+
+    // Wait for the "Please, wait" alert to disappear.
+    cy.get(".element-container .stAlert .markdown-text-container").should(
+      "have.length",
+      4
+    );
   });
 
-  it("displays correctly", () => {
+  it("displays an error message correctly", () => {
     cy.get(".element-container .stAlert .markdown-text-container")
       .eq(0)
       .contains("This is an error");
+  });
+
+  it("displays a warning message correctly", () => {
     cy.get(".element-container .stAlert .markdown-text-container")
       .eq(1)
       .contains("This is a warning");
+  });
+
+  it("displays an info message correctly", () => {
     cy.get(".element-container .stAlert .markdown-text-container")
       .eq(2)
       .contains("This is an info message");
+  });
+
+  it("displays a success message correctly", () => {
     cy.get(".element-container .stAlert .markdown-text-container")
       .eq(3)
       .contains("This is a success message");
   });
 
-  it("displays correctly", () => {
+  it("matches snapshot", () => {
     cy.get(".main > .block-container").matchImageSnapshot("alerts");
   });
 });
