d5abe95bae43c0d3e118d3053292d68100d7006f
SimonBiggs <me@simonbiggs.net>
2020-11-17 21:13:53 +1100

    add pylint disable

--- scripts/test_data/print_command_line.py
+++ scripts/test_data/print_command_line.py
@@ -15,6 +15,8 @@
 import streamlit as st
 
 server = st.server.server.Server.get_current()
-print(f'{{"server._command_line": "{server._command_line}"}}')
+print(
+    f'{{"server._command_line": "{server._command_line}"}}'  # pylint: disable = protected-access
+)
 
 server.stop()
