6906b468391c41d32afa15f6b03c4021c74757ab
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2020-12-09 02:36:43 +0400

    Bump React to 17.0.1

--- frontend/src/setupTests.js
+++ frontend/src/setupTests.js
@@ -18,6 +18,6 @@
 // This file is only used in tests, so these imports can be in devDependencies
 /* eslint-disable import/no-extraneous-dependencies */
 import { configure } from "enzyme"
-import Adapter from "enzyme-adapter-react-16"
+import Adapter from "@wojtekmaj/enzyme-adapter-react-17"
 
 configure({ adapter: new Adapter() })
