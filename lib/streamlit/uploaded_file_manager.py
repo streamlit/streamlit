35747569913ae1af13bf80b555be724c56efe9dc
Tim Conkling <tconkling@gmail.com>
2020-12-08 11:28:58 -0500

    Add a more detailed comment about BytesIO's copy-on-write optimization

--- lib/streamlit/uploaded_file_manager.py
+++ lib/streamlit/uploaded_file_manager.py
@@ -30,11 +30,15 @@ class UploadedFileRec(NamedTuple):
 class UploadedFile(io.BytesIO):
     """A mutable uploaded file.
 
-    This class extends BytesIO, which has copy-on-write semantics and does
-    not copy the underlying bytes until there's a mutation.
+    This class extends BytesIO, which has copy-on-write semantics when
+    initialized with `bytes`.
     """
 
     def __init__(self, record: UploadedFileRec):
+        # BytesIO's copy-on-write semantics doesn't seem to be mentioned in
+        # the Python docs - possibly because it's a CPython-only optimization
+        # and not guaranteed to be in other Python runtimes. But it's detailed
+        # here: https://hg.python.org/cpython/rev/79a5fbe2c78f
         super(UploadedFile, self).__init__(record.data)
         self.id = record.id
         self.name = record.name
