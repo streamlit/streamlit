2edbe3c7bc3a109c9b5fcc6ae50e8c35bd3fe4e3
Emiliano Rosso <arraydude@gmail.com>
2020-12-15 19:00:12 -0300

    Using whitelist check cloud/core communication (#2455)
    * Removing origin check s4a/core communication
    
    * Using whitelist
    
    * removing console.log
    
    * origin
    
    * origin
    
    * origin
    
    * tests
    
    * tests

--- frontend/src/urls.ts
+++ frontend/src/urls.ts
@@ -27,3 +27,9 @@ export const BUG_URL =
 /** The URL we direct people to for troubleshooting components. */
 export const COMPONENT_DEVELOPER_URL =
   "https://docs.streamlit.io/en/stable/develop_streamlit_components.html"
+export const CLOUD_COMM_WHITELIST = [
+  "devel.streamlit.test",
+  "share.streamlit.io",
+  "share-head.streamlit.io",
+  "share-staging.streamlit.io",
+]
