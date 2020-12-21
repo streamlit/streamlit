f1e1b8cedf2baac701ec19d1b84f50c885245bfa
Tim Conkling <tconkling@gmail.com>
2020-12-21 14:30:34 -0500

    Update the test app

--- test.py
+++ test.py
@@ -1,34 +1,31 @@
 import streamlit as st
 
-st.checkbox("outside")
+st.title("st.form")
 
 with st.beta_form():
     checkbox_val = st.checkbox("checkbox")
-    text_val = st.text_input("text_input")
+    color_val = st.color_picker("color")
+    # file = st.file_uploader("file")
+    multiselect_val = st.multiselect("multiselect", ["foo", "bar", "baz"])
+    number_val = st.number_input("number")
+    radio_val = st.radio("radio", ["floo", "blar", "blaz"])
+    selectslider_val = st.select_slider("select_slider", ["red", "yeller", "blue"])
+    selectbox_val = st.selectbox("selectbox", ["choco", "vanilli", "berry"])
+    slider_val = st.slider("slider")
+    textinput_val = st.text_input("text_input")
+    textarea_val = st.text_area("text_area")
 
-st.write("checkbox:", checkbox_val, "text:", text_val)
+st.markdown("---")
 
-# with st.beta_expander("st.expander"):
-#     st.code("Inside beta_exapnder")
-#     st.button("submit")
-#
-# st.markdown("---")
-#
-# with st.beta_container():
-#     st.code("Inside beta_container")
-#     st.button("submit", key="asdf")
-#
-# st.markdown("---")
-#
-# cols = st.beta_columns(2)
-# with cols[0]:
-#     st.code("Inside beta_columns (1)")
-#     st.button("submit", key="columns1")
-# with cols[1]:
-#     st.code("Inside beta_columns (2)")
-#     st.button("submit", key="columns2")
-#
-# st.markdown("---")
-#
-# st.code("Not inside a container")
-# st.button("submit", key="qwert")
+st.header("Submitted values:")
+
+st.write("checkbox:", checkbox_val)
+st.write("color:", color_val)
+st.write("multiselect:", ",".join(multiselect_val))
+st.write("number:", number_val)
+st.write("radio:", radio_val)
+st.write("select_slider:", selectslider_val)
+st.write("selectbox:", selectbox_val)
+st.write("slider:", slider_val)
+st.write("text_input:", textinput_val)
+st.write("text_area:", textarea_val)
