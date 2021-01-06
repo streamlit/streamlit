b0148526418e9da21ecead81b5bb7d15b52c8004
Brandon Hsiao <bh@streamlit.io>
2021-01-06 15:42:40 -0800

    add upcoming e2e test

---
+++ e2e/scripts/st_tooltips.py
@@ -0,0 +1,31 @@
+# Copyright 2018-2020 Streamlit Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import streamlit as st
+
+# TODO: fill in parameters
+st.button(help='tooltip')
+st.checkbox(help='tooltip')
+st.markdown(help='tooltip')
+st.number_input(help='tooltip')
+st.radio(help='tooltip')
+st.selectbox(help='tooltip')
+st.text_input(help='tooltip')
+st.write(help='tooltip')
+st.markdown(help='tooltip')
+st.header(help='tooltip')
+st.subheader(help='tooltip')
+st.code(help='tooltip')
+st.title(help='tooltip')
+st.latex(help='tooltip')
