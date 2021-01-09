8c3d893159a887c334fcb0e93e208cca8f4eafce
Brandon Hsiao <bh@streamlit.io>
2021-01-09 10:37:54 -0800

    get e2e test rendering with all help keywords passed in

--- e2e/scripts/st_tooltips.py
+++ e2e/scripts/st_tooltips.py
@@ -13,19 +13,43 @@
 # limitations under the License.
 
 import streamlit as st
+from datetime import datetime
+from datetime import time
+import numpy as np
+from numpy.random import randn
+from plotly import figure_factory
+import pandas as pd
 
-# TODO: fill in parameters
-st.button(help="tooltip")
-st.checkbox(help="tooltip")
-st.markdown(help="tooltip")
-st.number_input(help="tooltip")
-st.radio(help="tooltip")
-st.selectbox(help="tooltip")
-st.text_input(help="tooltip")
-st.write(help="tooltip")
-st.markdown(help="tooltip")
-st.header(help="tooltip")
-st.subheader(help="tooltip")
-st.code(help="tooltip")
-st.title(help="tooltip")
-st.latex(help="tooltip")
+
+def get_plotly_chart():
+    # Add histogram data
+    x1 = randn(200) - 2
+    x2 = randn(200)
+    x3 = randn(200) + 2
+
+    # Group data together
+    hist_data = [x1, x2, x3]
+    group_labels = ["Group 1", "Group 2", "Group 3"]
+    bin_size = [0.1, 0.25, 0.5]
+
+    # Create distribution plot with custom bin_size
+    return figure_factory.create_distplot(hist_data, group_labels, bin_size)
+
+
+st.button("some button", help="tooltip")
+st.checkbox("some checkbox", help="tooltip")
+st.number_input("number input", value=1, help="tooltip")
+st.radio("some radio", ("a", "b", "c"), 0, help="tooltip")
+st.selectbox("selectbox", ("a", "b", "c"), 0, help="tooltip")
+st.text_input("some input text", "default text", help="tooltip")
+st.time_input("time", datetime(2019, 7, 6, 21, 15), help="tooltip")
+st.date_input("date", datetime(2019, 7, 6, 21, 15), help="tooltip")
+st.write("here is some text", help="tooltip")
+st.markdown("here is some text", help="tooltip")
+st.header("some header", help="tooltip")
+st.subheader("some subheader", help="tooltip")
+st.code("import streamlit as st", language="python", help="tooltip")
+st.latex(r"\LaTeX", help="tooltip")
+st.image(np.repeat(0, 10000).reshape(100, 100), help="tooltip")
+st.line_chart(pd.DataFrame(randn(20, 3), columns=["a", "b", "c"]), help="tooltip")
+st.plotly_chart(get_plotly_chart())
