7f79479d37a5293536f8a8e0aa679df33021b431
karrie <karrie@streamlit.io>
2021-01-12 14:02:27 -0500

    Rename snapshot folders (#2553)
    * Rename snapshot folders
    
    * Update valid snapshot changes
    
    * Fix markup styles
    
    * Fix main menu changes
    
    * Fix file uploader changes
    
    * Fix multiselect styles
    
    * Update disconnect snapshot
    
    * Update dataframe snapshot
    
    * Fix checkbox
    
    * Fix disabled slider
    
    * Update snapshot for empty tables
    
    * Fix multiselect. Update snapshot to incorrect one in order to pass tests
    
    * Update table corner cell styling
    
    * add dummy snapshots
    
    * Update snapshots
    
    * Update component snapshot to pass
    
    * update table snapshot
    
    * Get new snapshots
    
    * Add snapshots
    
    * Fix number input. Reset main menu
    
    * Revert back to class for stale-element

--- frontend/src/components/widgets/Checkbox/Checkbox.tsx
+++ frontend/src/components/widgets/Checkbox/Checkbox.tsx
@@ -113,6 +113,7 @@ class Checkbox extends React.PureComponent<Props, State> {
             Label: {
               style: {
                 color: colors.bodyText,
+                marginBottom: ".4rem",
               },
             },
           }}
