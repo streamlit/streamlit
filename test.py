456f3d96a31a7d440eeec0f05986bb31a057af5e
Tim Conkling <tconkling@gmail.com>
2021-01-12 16:14:10 -0800

    Test forms-in-columns and columns-in-forms

--- test.py
+++ test.py
@@ -2,8 +2,8 @@ import streamlit as st
 
 st.title("st.form")
 
+"## Basic"
 with st.beta_form():
-    checkbox_val = st.checkbox("checkbox")
     # color_val = st.color_picker("color")
     # file = st.file_uploader("file")
     # multiselect_val = st.multiselect("multiselect", ["foo", "bar", "baz"])
@@ -14,6 +14,32 @@ with st.beta_form():
     slider_val = st.slider("slider")
     # textinput_val = st.text_input("text_input")
     # textarea_val = st.text_area("text_area")
+st.write("slider:", slider_val)
+
+"---"
+
+"## Forms-in-columns"
+col1, col2 = st.beta_columns(2)
+with col1:
+    with st.beta_form(key="form.col1"):
+        slider_val = st.slider("slider", key="slider.col1")
+    st.write("slider:", slider_val)
+with col2:
+    with st.beta_form(key="form.col2"):
+        slider_val = st.slider("slider", key="slider.col2")
+    st.write("slider:", slider_val)
+
+"---"
+
+"## Columns-in-forms"
+with st.beta_form(key="columns-in-forms"):
+    col1, col2 = st.beta_columns(2)
+    with col1:
+        slider_val1 = st.slider("slider", key="slider.column-in-form1")
+    with col2:
+        slider_val2 = st.slider("slider", key="slider.column-in-form2")
+    st.write("sliders:", slider_val1, slider_val2)
+
 
 # st.markdown("---")
 #
