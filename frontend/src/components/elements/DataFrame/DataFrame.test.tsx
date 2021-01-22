b682bd5084f61dd27a37d0ddf00baf984cf9fc8d
Vincent Donato <vincent@streamlit.io>
2021-01-22 15:24:41 -0800

    Add unit tests and update snapshots

--- frontend/src/components/elements/DataFrame/DataFrame.test.tsx
+++ frontend/src/components/elements/DataFrame/DataFrame.test.tsx
@@ -18,10 +18,16 @@
 import React from "react"
 import { shallow } from "enzyme"
 import { fromJS } from "immutable"
+import { random, times } from "lodash"
 
 import mockDataFrame from "./mock"
 import { DataFrame, DataFrameProps } from "./DataFrame"
 import { MIN_CELL_WIDTH_PX } from "./DataFrameUtil"
+
+const SCROLLBAR_SIZE = 10
+jest.mock("vendor/dom-helpers", () => ({
+  scrollbarSize: () => SCROLLBAR_SIZE,
+}))
 
 const getProps = (
   elementProps: Record<string, unknown> = {}
@@ -32,6 +38,20 @@ const getProps = (
   }),
   width: 400,
   height: 400,
+})
+
+const fakeData = (
+  numRows: number,
+  numCols: number
+): Partial<typeof mockDataFrame> => ({
+  data: {
+    cols: times(numCols, () => ({
+      int64s: { data: times(numRows, () => random(0, 9)) },
+      type: "int64s",
+    })),
+  },
+  index: { rangeIndex: { start: 0, stop: numRows }, type: "rangeIndex" },
+  columns: { rangeIndex: { start: 0, stop: numCols }, type: "rangeIndex" },
 })
 
 describe("DataFrame Element", () => {
@@ -56,10 +76,12 @@ describe("DataFrame Element", () => {
     expect(multiGridProps.columnCount).toBe(11)
     expect(multiGridProps).toHaveProperty("enableFixedColumnScroll")
     expect(multiGridProps).toHaveProperty("enableFixedRowScroll")
-    expect(multiGridProps.height).toBe(275)
+    // 275px for the dataframe itself + 10px for the horizontal scrollbar
+    expect(multiGridProps.height).toBe(285)
     expect(multiGridProps.rowHeight).toBe(25)
     expect(multiGridProps.rowCount).toBe(11)
-    expect(multiGridProps.width).toBe(398)
+    // 400px full container width - 12px for border and vertical scrollbar
+    expect(multiGridProps.width).toBe(388)
   })
 
   it("should render as empty if there's no data", () => {
@@ -80,5 +102,31 @@ describe("DataFrame Element", () => {
     expect(multiGridProps.rowHeight).toBe(MIN_CELL_WIDTH_PX)
     expect(multiGridProps.rowCount).toBe(1)
     expect(multiGridProps.width).toBe(60)
+  })
+
+  it("adds extra height for horizontal scrollbar when wide but not tall", () => {
+    let props = getProps({ ...fakeData(1, 1) })
+    let wrapper = shallow(<DataFrame {...props} />)
+    const normalHeight = wrapper.find("MultiGrid").props().height
+
+    props = getProps({ ...fakeData(1, 20) })
+    wrapper = shallow(<DataFrame {...props} />)
+    const heightWithScrollbar = wrapper.find("MultiGrid").props().height
+
+    expect(heightWithScrollbar).toBe(normalHeight + SCROLLBAR_SIZE)
+  })
+
+  it("adds extra width for vertical scrollbar when tall but not wide", () => {
+    // Be careful to ensure that the number of digits needed to display the
+    // largest row number is the same for the two DataFrames.
+    let props = getProps({ ...fakeData(11, 1) })
+    let wrapper = shallow(<DataFrame {...props} />)
+    const normalWidth = wrapper.find("MultiGrid").props().width
+
+    props = getProps({ ...fakeData(99, 1) })
+    wrapper = shallow(<DataFrame {...props} />)
+    const widthWithScrollbar = wrapper.find("MultiGrid").props().width
+
+    expect(widthWithScrollbar).toBe(normalWidth + SCROLLBAR_SIZE)
   })
 })
