fd59adc35aee5e10a1802a121b5bc9f1134b086a
Austin Chen <akrolsmir@gmail.com>
2021-01-22 10:22:03 -0500

    Refactor: remove "_proto" from "media_proto.py"
    See #2136

--- lib/streamlit/delta_generator.py
+++ lib/streamlit/delta_generator.py
@@ -45,7 +45,7 @@ from streamlit.elements.vega_lite import VegaLiteMixin
 from streamlit.elements.deck_gl_json_chart import PydeckMixin
 from streamlit.elements.map import MapMixin
 from streamlit.elements.iframe_proto import IframeMixin
-from streamlit.elements.media_proto import MediaMixin
+from streamlit.elements.media import MediaMixin
 from streamlit.elements.checkbox import CheckboxMixin
 from streamlit.elements.multiselect import MultiSelectMixin
 from streamlit.elements.radio import RadioMixin
