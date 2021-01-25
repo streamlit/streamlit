14f62b6907baec8277e1cd2a9b863f9a03547815
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2021-01-25 13:57:54 +0400

    Bump year to 2021 in license headers

--- lib/streamlit/source_util.py
+++ lib/streamlit/source_util.py
@@ -1,4 +1,4 @@
-# Copyright 2018-2020 Streamlit Inc.
+# Copyright 2018-2021 Streamlit Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,7 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
 
 def open_python_file(filename):
     """Open a read-only Python file taking proper care of its encoding.
