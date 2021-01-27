b0c932cd48e138fddb512b3f1a9961f91d587f09
Ken McGrady <ken@streamlit.io>
2021-01-27 14:02:55 -0800

    Add additional test

--- lib/tests/streamlit/delta_generator_test.py
+++ lib/tests/streamlit/delta_generator_test.py
@@ -645,6 +645,25 @@ class WidgetIdText(testutil.DeltaGeneratorTestCase):
 
         self.assertNotEqual(element1.text_input.id, element2.text_input.id)
 
+    def test_ids_are_equal_when_values_are_diff_but_keys_are_equal(self):
+        text_input1 = TextInput()
+        text_input1.label = "Label #1"
+        text_input1.default = "Value #1"
+
+        text_input2 = TextInput()
+        text_input2.label = "Label #2"
+        text_input2.default = "Value #2"
+
+        element1 = Element()
+        element1.text_input.CopyFrom(text_input1)
+
+        element2 = Element()
+        element2.text_input.CopyFrom(text_input2)
+
+        register_widget("text_input", element1.text_input, user_key="some_key")
+        with self.assertRaises(DuplicateWidgetID):
+            register_widget("text_input", element2.text_input, user_key="some_key")
+
 
 class DeltaGeneratorImageTest(testutil.DeltaGeneratorTestCase):
     """Test DeltaGenerator Images"""
