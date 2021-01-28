731d55b95be552d0db5804ef00dcf8134c0f6748
Amanda Walker <amanda@amandawalker.io>
2021-01-27 16:15:46 -0800

    Add svg test to st.image e2e tests
    Mostly so CI will run it and give me a snapshot diff.

--- e2e/specs/st_image.spec.js
+++ e2e/specs/st_image.spec.js
@@ -91,4 +91,10 @@ describe("st.image", () => {
         .matchImageSnapshot("black-square-column");
     }
   });
+
+  it("displays SVG images that load external images", () => {
+    cy.get("[data-testid='stImage'] svg").matchImageSnapshot(
+      "karriebear-avatar"
+    );
+  });
 });
