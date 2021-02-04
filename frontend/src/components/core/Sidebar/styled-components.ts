3bbec7a85b2f94fdab52b8d23a8ec1dde9f5bb7f
karrie <karrie@streamlit.io>
2021-02-04 16:16:52 -0500

    Fix sidebar gradiant

--- frontend/src/components/core/Sidebar/styled-components.ts
+++ frontend/src/components/core/Sidebar/styled-components.ts
@@ -57,7 +57,7 @@ export interface StyledSidebarContentProps {
 }
 export const StyledSidebarContent = styled.div<StyledSidebarContentProps>(
   ({ isCollapsed, theme }) => ({
-    backgroundColor: theme.colors.secondaryBg,
+    backgroundColor: theme.colors.bgColor,
     backgroundImage: `linear-gradient(to bottom, ${theme.colors.bgColor}, ${theme.colors.secondaryBg})`,
     backgroundAttachment: "fixed",
     flexShrink: 0,
