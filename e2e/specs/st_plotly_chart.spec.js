996d331e77d7db52dcda36216577091058ee771a
Vincent Donato <vincent@streamlit.io>
2021-02-05 20:01:06 -0800

    Set plotly chart background to theme secondaryBg color (#2748)
    * Set plotly chart background to theme secondaryBg color
    
    Now that the sidebar background color has been renamed/repurposed to be
    a secondaryBg color, it seems appropriate to use it for this. This'll
    probably need a product review down the line, but this now looks pretty
    reasonable in both light and dark mode compared to before.
    
    * Add snapshot test
    
    * Set RNG seed
    
    * Update snapshot

--- e2e/specs/st_plotly_chart.spec.js
+++ e2e/specs/st_plotly_chart.spec.js
@@ -29,4 +29,10 @@ describe("st.plotly_chart", () => {
       .find(".modebar-btn--logo")
       .should("have.attr", "data-title", "Produced with Plotly");
   });
+
+  it("has consistent visuals", () => {
+    cy.get(".element-container .stPlotlyChart")
+      .first()
+      .matchImageSnapshot("st_plotly_chart");
+  });
 });
