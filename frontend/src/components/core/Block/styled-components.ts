5db090db855238fe8a14e0fd24874182edff1bcd
Brandon Hsiao <bh@streamlit.io>
2021-02-08 15:20:35 -0800

    hide empty columns on mobile

--- frontend/src/components/core/Block/styled-components.ts
+++ frontend/src/components/core/Block/styled-components.ts
@@ -56,12 +56,13 @@ export const StyledElementContainer = styled.div<StyledElementContainerProps>(
 )
 
 export interface StyledColumnProps {
+  isEmpty: boolean
   weight: number
   width: number
   withLeftPadding: boolean
 }
 export const StyledColumn = styled.div<StyledColumnProps>(
-  ({ weight, width, withLeftPadding, theme }) => {
+  ({ isEmpty, weight, width, withLeftPadding, theme }) => {
     // The minimal viewport width used to determine the minimal
     // fixed column width while accounting for column proportions.
     // Randomly selected based on visual experimentation.
@@ -76,9 +77,25 @@ export const StyledColumn = styled.div<StyledColumnProps>(
       width,
       paddingLeft: withLeftPadding ? theme.spacing.sm : theme.spacing.none,
       [`@media (max-width: ${theme.breakpoints.columns})`]: {
+        display: isEmpty ? "none" : undefined,
         minWidth: `${columnPercentage > 0.5 ? "min" : "max"}(
           ${columnPercentage * 100}% - ${theme.spacing.twoXL},
           ${columnPercentage * parseInt(theme.breakpoints.columns, 10)}px)`,
+      },
+    }
+  }
+)
+
+export interface StyledBlockProps {
+  isEmpty: boolean
+  width: number
+}
+export const StyledBlock = styled.div<StyledBlockProps>(
+  ({ isEmpty, width, theme }) => {
+    return {
+      width,
+      [`@media (max-width: ${theme.breakpoints.columns})`]: {
+        display: isEmpty ? "none" : undefined,
       },
     }
   }
