8814a4d965757b1820ce302b33d773b21af5cebe
karrie <karrie@streamlit.io>
2021-02-08 18:01:44 -0500

    Theming OS change (#2755)
    * Add event listener for OS theme change
    
    * Doing too much reorg, only update auto theme if auto was last selected

--- frontend/src/ThemedApp.test.tsx
+++ frontend/src/ThemedApp.test.tsx
@@ -23,6 +23,24 @@ import ThemedApp from "./ThemedApp"
 import AppWithScreencast from "./App"
 
 describe("ThemedApp", () => {
+  beforeEach(() => {
+    // sourced from:
+    // https://jestjs.io/docs/en/manual-mocks#mocking-methods-which-are-not-implemented-in-jsdom
+    Object.defineProperty(window, "matchMedia", {
+      writable: true,
+      value: jest.fn().mockImplementation(query => ({
+        matches: false,
+        media: query,
+        onchange: null,
+        addListener: jest.fn(), // deprecated
+        removeListener: jest.fn(), // deprecated
+        addEventListener: jest.fn(),
+        removeEventListener: jest.fn(),
+        dispatchEvent: jest.fn(),
+      })),
+    })
+  })
+
   afterEach(() => {
     window.localStorage.clear()
   })
