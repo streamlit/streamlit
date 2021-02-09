49529b6ffac155a11dad7530ed937558c63854c1
Vincent Donato <vincent@streamlit.io>
2021-02-08 16:36:14 -0800

    Add customTheme.setAsDefault config var and pass to frontend

--- lib/streamlit/config.py
+++ lib/streamlit/config.py
@@ -762,6 +762,16 @@ _create_option(
 )
 
 _create_option(
+    "customTheme.setAsDefault",
+    description="""
+        Whether to set the custom theme to be the default theme for this
+        streamlit app if one is defined.
+        """,
+    default_val=True,
+    type_=bool,
+)
+
+_create_option(
     "customTheme.primary",
     description="""
         Used to style primary interface elements. It's the color displayed
