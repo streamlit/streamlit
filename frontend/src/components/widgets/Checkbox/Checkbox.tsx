737abc2e13323b2d9cc8d647924b9ae0018e4662
karrie <karrie@streamlit.io>
2021-02-11 16:16:35 -0500

    merge

--- frontend/src/components/widgets/Checkbox/Checkbox.tsx
+++ frontend/src/components/widgets/Checkbox/Checkbox.tsx
@@ -113,7 +113,6 @@ class Checkbox extends React.PureComponent<Props, State> {
             Label: {
               style: {
                 color: colors.bodyText,
-                marginBottom: ".4rem",
               },
             },
           }}
