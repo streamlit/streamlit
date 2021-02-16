4ecb76ebbce9b9c45b38a58cd2dbf1e9d1b289c5
Tomasz Kontusz <tomasz.kontusz@gmail.com>
2021-02-16 21:45:23 +0100

    Generate the function's cache key lazily
    This way globals that are only created after the cached function
    can actually be included in the hash. Before this change only their
    names were included, which could lead to stale cache being used.

--- lib/streamlit/caching.py
+++ lib/streamlit/caching.py
@@ -460,52 +460,7 @@ def cache(
             ttl=ttl,
         )
 
-    # Create the unique key for this function's cache. The cache will be
-    # retrieved from inside the wrapped function.
-    #
-    # A naive implementation would involve simply creating the cache object
-    # right here in the wrapper, which in a normal Python script would be
-    # executed only once. But in Streamlit, we reload all modules related to a
-    # user's app when the app is re-run, which means that - among other
-    # things - all function decorators in the app will be re-run, and so any
-    # decorator-local objects will be recreated.
-    #
-    # Furthermore, our caches can be destroyed and recreated (in response
-    # to cache clearing, for example), which means that retrieving the
-    # function's cache here (so that the wrapped function can save a lookup)
-    # is incorrect: the cache itself may be recreated between
-    # decorator-evaluation time and decorated-function-execution time. So
-    # we must retrieve the cache object *and* perform the cached-value lookup
-    # inside the decorated function.
-
-    func_hasher = hashlib.new("md5")
-
-    # Include the function's __module__ and __qualname__ strings in the hash.
-    # This means that two identical functions in different modules
-    # will not share a hash; it also means that two identical *nested*
-    # functions in the same module will not share a hash.
-    # We do not pass `hash_funcs` here, because we don't want our function's
-    # name to get an unexpected hash.
-    update_hash(
-        (func.__module__, func.__qualname__),
-        hasher=func_hasher,
-        hash_funcs=None,
-        hash_reason=HashReason.CACHING_FUNC_BODY,
-        hash_source=func,
-    )
-
-    # Include the function's body in the hash. We *do* pass hash_funcs here,
-    # because this step will be hashing any objects referenced in the function
-    # body.
-    update_hash(
-        func,
-        hasher=func_hasher,
-        hash_funcs=hash_funcs,
-        hash_reason=HashReason.CACHING_FUNC_BODY,
-        hash_source=func,
-    )
-
-    cache_key = func_hasher.hexdigest()
+    cache_key = None
     _LOGGER.debug(
         "mem_cache key for %s.%s: %s", func.__module__, func.__qualname__, cache_key
     )
@@ -528,6 +483,13 @@ def cache(
             message = "Running `%s(...)`." % name
 
         def get_or_create_cached_value():
+            nonlocal cache_key
+            if cache_key is None:
+                # Wait with generating the cache key until the first call.
+                # This way we can see complete globals, including functions
+                # defined after this one.
+                cache_key = _hash_func(func, hash_funcs)
+
             # First, get the cache that's attached to this function.
             # This cache's key is generated (above) from the function's code.
             mem_cache = _mem_caches.get_cache(cache_key, max_entries, ttl)
@@ -613,6 +575,54 @@ def cache(
         pass
 
     return wrapped_func
+
+
+def _hash_func(func, hash_funcs) -> str:
+    # Create the unique key for a function's cache. The cache will be retrieved
+    # from inside the wrapped function.
+    #
+    # A naive implementation would involve simply creating the cache object
+    # right in the wrapper, which in a normal Python script would be executed
+    # only once. But in Streamlit, we reload all modules related to a user's
+    # app when the app is re-run, which means that - among other things - all
+    # function decorators in the app will be re-run, and so any decorator-local
+    # objects will be recreated.
+    #
+    # Furthermore, our caches can be destroyed and recreated (in response to
+    # cache clearing, for example), which means that retrieving the function's
+    # cache in the decorator (so that the wrapped function can save a lookup)
+    # is incorrect: the cache itself may be recreated between
+    # decorator-evaluation time and decorated-function-execution time. So we
+    # must retrieve the cache object *and* perform the cached-value lookup
+    # inside the decorated function.
+    func_hasher = hashlib.new("md5")
+
+    # Include the function's __module__ and __qualname__ strings in the hash.
+    # This means that two identical functions in different modules
+    # will not share a hash; it also means that two identical *nested*
+    # functions in the same module will not share a hash.
+    # We do not pass `hash_funcs` here, because we don't want our function's
+    # name to get an unexpected hash.
+    update_hash(
+        (func.__module__, func.__qualname__),
+        hasher=func_hasher,
+        hash_funcs=None,
+        hash_reason=HashReason.CACHING_FUNC_BODY,
+        hash_source=func,
+    )
+
+    # Include the function's body in the hash. We *do* pass hash_funcs here,
+    # because this step will be hashing any objects referenced in the function
+    # body.
+    update_hash(
+        func,
+        hasher=func_hasher,
+        hash_funcs=hash_funcs,
+        hash_reason=HashReason.CACHING_FUNC_BODY,
+        hash_source=func,
+    )
+    cache_key = func_hasher.hexdigest()
+    return cache_key
 
 
 class Cache(Dict[Any, Any]):
