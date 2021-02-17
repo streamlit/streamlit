a6afae940a3d6488aa568e92f011eccab0e1f68c
Brandon Hsiao <bh@streamlit.io>
2021-02-17 12:11:25 -0800

    fix quotes

--- e2e/scripts/st_datetimes_dataframe.py
+++ e2e/scripts/st_datetimes_dataframe.py
@@ -15,7 +15,7 @@
 import streamlit as st
 import pandas as pd
 
-df = pd.DataFrame({'str': ['2020-04-14 00:00:00']})
-df['notz'] = pd.to_datetime(df['str'])
-df['yaytz'] = pd.to_datetime(df['str']).dt.tz_localize('Europe/Moscow')
+df = pd.DataFrame({"str": ["2020-04-14 00:00:00"]})
+df["notz"] = pd.to_datetime(df["str"])
+df["yaytz"] = pd.to_datetime(df["str"]).dt.tz_localize("Europe/Moscow")
 st.dataframe(df)
