5e7e8e3e30bbd834edd49cd22ed30c0523fa3ab5
Brandon Hsiao <bh@streamlit.io>
2021-02-17 12:07:48 -0800

    add e2e test

---
+++ e2e/specs/st_datetimes_dataframe.spec.js
@@ -0,0 +1,35 @@
+/**
+ * @license
+ * Copyright 2018-2021 Streamlit Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+describe("st.dataframe", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000/");
+    cy.get(".element-container .stDataFrame")
+      .find(".ReactVirtualized__Grid__innerScrollContainer")
+      .find("[data-testid='StyledDataFrameDataCell']")
+      .as("cells");
+  });
+
+  it("displays datetimes correctly", () => {
+    cy.get("@cells")
+      .eq(1)
+      .should("have.text", "2020-04-14T00:00:00-07:00");
+    cy.get("@cells")
+      .eq(2)
+      .should("have.text", "2020-04-14T00:00:00+03:00");
+  });
+});
