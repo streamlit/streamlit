a4ac0f9bab34eddfa9238fe24b073d6f0f36627f
Brandon Hsiao <bh@streamlit.io>
2021-02-17 13:31:54 -0800

    update test

--- e2e/specs/st_datetimes_dataframe.spec.js
+++ e2e/specs/st_datetimes_dataframe.spec.js
@@ -25,11 +25,30 @@ describe("st.dataframe", () => {
   });
 
   it("displays datetimes correctly", () => {
+    const datetimeString = "2020-04-14T00:00:00";
+
+    // Assert that notz and yaytz render as desired.
+    //
+    // We can't just assert "have.text" against a hardcoded string, because the
+    // timezone we want displayed depends on whatever timezone the test is
+    // being run in.  So we use moment to dynamically generate the correct
+    // string.
+    //
+    // This feels a little like "tautologically copying the code we're trying
+    // to test into the test itself," but we're still contingently testing the
+    // actual important thing, the notz/yaytz logic.
+
+    // notz column should show datetime in current timezone
     cy.get("@cells")
       .eq(1)
-      .should("have.text", "2020-04-14T00:00:00-07:00");
+      .should("have.text", Cypress.moment(datetimeString).format());
+
+    // yaytz column should show datetime in provided timezone
     cy.get("@cells")
       .eq(2)
-      .should("have.text", "2020-04-14T00:00:00+03:00");
+      .should(
+        "have.text",
+        Cypress.moment.parseZone(`${datetimeString}+03:00`).format()
+      );
   });
 });
