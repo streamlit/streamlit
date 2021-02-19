c6b4f30162cb440649dc33782fee488f6d86a784
Brandon Hsiao <bh@streamlit.io>
2021-02-19 09:06:08 -0800

    add test

--- frontend/src/components/elements/CodeBlock/CodeBlock.test.tsx
+++ frontend/src/components/elements/CodeBlock/CodeBlock.test.tsx
@@ -16,9 +16,10 @@
  */
 
 import React from "react"
-import { shallow } from "lib/test_util"
+import { mount, shallow } from "lib/test_util"
 import { logWarning } from "lib/log"
 import CodeBlock, { CodeBlockProps } from "./CodeBlock"
+import CopyButton from "./CopyButton"
 
 jest.mock("lib/log", () => ({
   logWarning: jest.fn(),
@@ -51,6 +52,23 @@ describe("CodeBlock Element", () => {
 
     expect(wrapper.find("StyledCodeBlock").length).toBe(1)
     expect(wrapper.find("code").prop("className")).toBe("language-python")
+  })
+
+  it("should render copy button when code block has content", () => {
+    const props = getProps({
+      value: "i am not empty",
+      language: null,
+    })
+    const wrapper = mount(<CodeBlock {...props} />)
+    expect(wrapper.find(CopyButton)).toHaveLength(1)
+  })
+
+  it("should not render copy button when code block is empty", () => {
+    const props = getProps({
+      value: "",
+    })
+    const wrapper = mount(<CodeBlock {...props} />)
+    expect(wrapper.find(CopyButton)).toHaveLength(0)
   })
 
   it("should warn if there's no highlight available", () => {
