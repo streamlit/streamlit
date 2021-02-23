4811a49859c483b44b08b475d55704e0c6bcdb74
Tim Conkling <tconkling@gmail.com>
2021-02-23 12:15:29 -0800

    reload secrets.toml when it changes

--- lib/streamlit/secrets.py
+++ lib/streamlit/secrets.py
@@ -19,7 +19,10 @@ from typing import Any, Optional, Mapping
 import toml
 
 import streamlit as st
+from streamlit.logger import get_logger
+from streamlit.watcher.file_watcher import watch_file
 
+LOGGER = get_logger(__name__)
 SECRETS_FILE_LOC = os.path.abspath(os.path.join(".", ".streamlit", "secrets.toml"))
 
 
@@ -34,11 +37,12 @@ class Secrets(Mapping[str, Any]):
     Parses secrets.toml on-demand. Cannot be externally mutated.
     """
 
-    def __init__(self):
+    def __init__(self, file_path: str):
         # Our secrets dict.
         self._secrets: Optional[Mapping[str, Any]] = None
         self._lock = threading.RLock()
         self._file_watcher_installed = False
+        self._file_path = file_path
 
     def load_if_toml_exists(self) -> None:
         """Load secrets.toml from disk if it exists. If it doesn't exist,
@@ -72,16 +76,22 @@ class Secrets(Mapping[str, Any]):
             Raised if secrets.toml doesn't exist.
 
         """
+        # Avoid taking a lock for the common case where secrets are already
+        # loaded.
+        secrets = self._secrets
+        if secrets is not None:
+            return secrets
+
         with self._lock:
             if self._secrets is not None:
                 return self._secrets
 
             try:
-                with open(SECRETS_FILE_LOC) as f:
+                with open(self._file_path) as f:
                     secrets_file_str = f.read()
             except FileNotFoundError:
                 if print_exceptions:
-                    st.error(f"Secrets file not found. Expected at: {SECRETS_FILE_LOC}")
+                    st.error(f"Secrets file not found. Expected at: {self._file_path}")
                 raise
 
             try:
@@ -95,7 +105,26 @@ class Secrets(Mapping[str, Any]):
                 _maybe_set_environment_variable(k, v)
 
             self._secrets = secrets
+            self._maybe_install_file_watcher()
+
             return self._secrets
+
+    def _maybe_install_file_watcher(self) -> None:
+        with self._lock:
+            if self._file_watcher_installed:
+                return
+
+            watch_file(self._file_path, self._on_secrets_file_changed)
+
+            # We set file_watcher_installed to True even if watch_file
+            # returns False to avoid repeatedly trying to install it.
+            self._file_watcher_installed = True
+
+    def _on_secrets_file_changed(self, _) -> None:
+        with self._lock:
+            LOGGER.debug(f"Secrets file {self._file_path} changed, reloading")
+            self._reset()
+            self._parse(print_exceptions=True)
 
     def __getitem__(self, key):
         return self._parse(True)[key]
