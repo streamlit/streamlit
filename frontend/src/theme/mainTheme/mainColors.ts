cc05f39519f1a97c5e93d4525684ac98885bb7ef
BEAST GLATISANT <26509147+g147@users.noreply.github.com>
2021-02-28 18:23:25 +0530

    g7

--- frontend/src/theme/mainTheme/mainColors.ts
+++ frontend/src/theme/mainTheme/mainColors.ts
@@ -25,7 +25,7 @@ const genericColors = {
   info: "#1e6777",
   success: "#176c36",
   warning: "#947c2d",
-  primary: "#f63366",
+  primary: "#4d4dff",
   disabled: colors.gray30,
   secondary: colors.gray60,
   lightestGray: colors.gray20,
