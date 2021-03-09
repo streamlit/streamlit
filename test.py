f5884a1dfaef9c2af5bb3d961edec593d52885dd
Tim Conkling <tconkling@gmail.com>
2021-03-08 16:08:26 -0800

    test.py

---
+++ test.py
@@ -0,0 +1,14 @@
+from typing import List, Optional
+
+import streamlit as st
+from streamlit.uploaded_file_manager import UploadedFile
+
+files: Optional[List[UploadedFile]] = st.file_uploader(
+    "files", accept_multiple_files=True
+)
+
+if files is None or len(files) == 0:
+    st.write("Upload and press submit!")
+else:
+    for f in files:
+        st.video(f)
