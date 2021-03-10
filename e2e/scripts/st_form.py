55118c64b40e3c33d8e788df59f6874915b77303
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2021-03-10 17:37:22 +0400

    st.form E2E tests (#2913)
    * st.form E2E tests

---
+++ e2e/scripts/st_form.py
@@ -0,0 +1,29 @@
+import streamlit as st
+from datetime import date, time
+
+with st.beta_form():
+    checkbox = st.checkbox("Checkbox", False)
+    color_picker = st.color_picker("Color Picker")
+    date_input = st.date_input("Date Input", date(2019, 7, 6))
+    multiselect = st.multiselect("Multiselect", ["foo", "bar"], default=["foo"])
+    number_input = st.number_input("Number Input")
+    radio = st.radio("Radio", ["foo", "bar", "baz"])
+    selectbox = st.selectbox("Selectbox", ["foo", "bar", "baz"])
+    select_slider = st.select_slider("Select Slider", ["foo", "bar", "baz"])
+    slider = st.slider("Slider")
+    text_area = st.text_area("Text Area", value="foo")
+    text_input = st.text_input("Text Input", value="foo")
+    time_input = st.time_input("Time Input", time(8, 45))
+
+"Checkbox:", checkbox
+"Color Picker:", color_picker
+"Date Input:", date_input
+"Multiselect:", ", ".join(multiselect)
+"Number Input:", number_input
+"Radio:", radio
+"Selectbox:", selectbox
+"Select Slider:", select_slider
+"Slider:", slider
+"Text Area:", text_area
+"Text Input:", text_input
+"Time Input:", time_input
