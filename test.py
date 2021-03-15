6d9750bbf6a52822557023053dda3a64be235545
Tim Conkling <tconkling@gmail.com>
2021-03-15 11:34:14 -0700

    Whoops, remove stray test file

--- test.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-files = st.file_uploader("files", accept_multiple_files=True)
-
-if files is None or len(files) == 0:
-    st.write("Upload and press submit!")
-else:
-    for f in files:
-        st.video(f)
