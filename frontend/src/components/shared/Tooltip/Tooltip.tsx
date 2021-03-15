f34d0bae3b7f2e23857ae7793624c2c75e33e64d
bh-streamlit <76131205+bh-streamlit@users.noreply.github.com>
2021-03-15 12:47:46 -0700

    Tooltips + theming design changes (#2956)
    * save work
    
    * fix padding
    
    * use linkText
    
    * just remove link style

--- frontend/src/components/shared/Tooltip/Tooltip.tsx
+++ frontend/src/components/shared/Tooltip/Tooltip.tsx
@@ -78,6 +78,7 @@ function Tooltip({
             borderBottomRightRadius: "0.25rem",
             border: `1px solid ${colors.fadedText10}`,
             backgroundColor: colors.fadedText10,
+            padding: "0 !important",
           },
         },
         Inner: {
@@ -86,6 +87,7 @@ function Tooltip({
             color: colors.bodyText,
             fontSize: "0.875rem",
             fontWeight: "normal",
+            padding: "0 !important",
           },
         },
       }}
--- frontend/src/components/shared/TooltipIcon/styled-components.ts
+++ frontend/src/components/shared/TooltipIcon/styled-components.ts
@@ -1,4 +1,5 @@
 import styled from "@emotion/styled"
+import { transparentize } from "color2k"
 
 export const StyledTooltipIconWrapper = styled.div(({ theme }) => ({
   svg: {
@@ -18,14 +19,15 @@ export const StyledTooltipContentWrapper = styled.div(({ theme }) => ({
   maxWidth: `calc(${theme.sizes.contentMaxWidth} - 4rem)`,
   maxHeight: "300px",
   overflow: "auto",
+  padding: "14px 16px", // make vertical padding slightly smaller to account for line height
 
   [`@media (max-width: ${theme.breakpoints.sm})`]: {
     maxWidth: `calc(100% - 2rem)`,
   },
-  a: {
-    color: theme.colors.blue50,
-  },
   img: {
     maxWidth: "100%",
+  },
+  code: {
+    background: transparentize(theme.colors.darkenedBgMix60, 0.8),
   },
 }))
