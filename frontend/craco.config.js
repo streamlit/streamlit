6aa4ccda6cd9cf2b6e273ea7effae11af12fb57a
Brandon Hsiao <bh@streamlit.io>
2021-03-29 19:21:12 -0700

    save

--- frontend/craco.config.js
+++ frontend/craco.config.js
@@ -41,27 +41,17 @@ module.exports = {
     configure: webpackConfig => {
       webpackConfig.resolve.mainFields = ["main", "module"]
 
-      // hardsource appears to make React app server start more slowly in our
-      // end-to-end tests, as well as our production builds, so adding an
-      // environment variable to disable it
-      if (!process.env.BUILD_AS_FAST_AS_POSSIBLE) {
-        // HardSourceWebpackPlugin adds aggressive build caching
-        // to speed up our slow builds.
-        // https://github.com/mzgoddard/hard-source-webpack-plugin
-        webpackConfig.plugins.unshift(new HardSourceWebpackPlugin())
-      }
-
-      // handle terser plugin
+      // find terser plugin
       const minimizerPlugins = webpackConfig.optimization.minimizer
-      const isTerserPlugin = item => item.options.terserOptions
-      const terserPluginIndex = minimizerPlugins.findIndex(isTerserPlugin)
+      const terserPluginIndex = minimizerPlugins.findIndex(
+        item => item.options.terserOptions
+      )
 
       if (process.env.BUILD_AS_FAST_AS_POSSIBLE) {
         // remove terser
         minimizerPlugins.splice(terserPluginIndex, 1)
 
-        // When we're running E2E tests or building for PR preview, we can just
-        // skip type checking and linting; these are handled in separate tests.
+        // skip type checking and linting
         const pluginsToRemove = [
           "ForkTsCheckerWebpackPlugin",
           "ESLintWebpackPlugin",
@@ -75,6 +65,15 @@ module.exports = {
       } else {
         const parallel = process.env.CIRCLECI ? false : true
         minimizerPlugins[terserPluginIndex].options.parallel = parallel
+
+        // HardSourceWebpackPlugin adds aggressive build caching.
+        // More info: https://github.com/mzgoddard/hard-source-webpack-plugin
+        //
+        // This speeds up builds for local development.  Empirically, however, it
+        // seems to slow down one-time production builds, so we are making it
+        // possible to turn it off via setting BUILD_AS_FAST_AS_POSSIBLE=1.  This
+        // is useful in CircleCI, when doing releases, etc.
+        webpackConfig.plugins.unshift(new HardSourceWebpackPlugin())
       }
 
       return webpackConfig
