1f48dc075e47d52c777939ffc038cd09f7f76edc
Amanda Walker <amanda@amandawalker.io>
2021-04-02 10:54:22 -0700

    Make modal close check idiomatic

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -222,7 +222,7 @@ export class App extends PureComponent<Props, State> {
     ) {
       this.sendRerunBackMsg()
     }
-    if (this.props.s4aCommunication.currentState.forcedModalClose === true) {
+    if (this.props.s4aCommunication.currentState.forcedModalClose) {
       this.closeDialog()
     }
   }
