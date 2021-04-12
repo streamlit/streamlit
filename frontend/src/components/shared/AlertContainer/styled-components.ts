1779b53005724425258d4f0a769f0387abc4d8ca
Thiago Teixeira <thiago@streamlit.io>
2021-04-12 16:53:42 -0700

    Remove background from pre/code inside alert elements

--- frontend/src/components/shared/AlertContainer/styled-components.ts
+++ frontend/src/components/shared/AlertContainer/styled-components.ts
@@ -19,8 +19,8 @@ import styled from "@emotion/styled"
 
 export const StyledAlertContent = styled.div(({ theme }) => ({
   pre: {
-    // Default color for syntax highlighting.
-    backgroundColor: "#ffffff44",
+    backgroundColor: theme.colors.transparent,
+    padding: theme.spacing.none,
 
     "pre, code": {
       backgroundColor: theme.colors.transparent,
