98615a3f6401365ea61eb1c14ac3efe975387474
Tim Conkling <tconkling@gmail.com>
2021-04-26 17:28:51 -0700

    Temporarily remove example code from st.form API docs (#3182)
    I'm not able to upload sample apps to our S3 sharing bucket (our AWS credentials may have changed?), so we're going to ship without this sample code. We'll add them back in the next release!

--- lib/streamlit/elements/form.py
+++ lib/streamlit/elements/form.py
@@ -133,37 +133,6 @@ class FormMixin:
             A string that identifies the form. Each form must have its own
             key. (This key is not displayed to the user in the interface.)
 
-        Examples
-        --------
-
-        Inserting elements using "with" notation:
-
-        >>> with st.form("my_form"):
-        ...    st.write("Inside the form")
-        ...    slider_val = st.slider("Form slider")
-        ...    checkbox_val = st.checkbox("Form checkbox")
-        ...
-        ...    # Every form must have a submit button.
-        ...    submitted = st.form_submit_button("Submit")
-        ...    if submitted:
-        ...        st.write("slider", slider_val, "checkbox", checkbox_val)
-        ...
-        >>> st.write("Outside the form")
-
-        .. output ::
-            https://TODO
-
-        Inserting elements out of order:
-
-        >>> form = st.form()
-        >>> form.slider("Inside the form")
-        >>> st.slider("Outside the form")
-        >>>
-        >>> # Now add a submit button to the form:
-        >>> form.form_submit_button("Submit")
-
-        .. output ::
-            https://TODO
         """
 
         if is_in_form(self.dg):
