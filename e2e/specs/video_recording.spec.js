a96970a76c9a7e3e9642a2c7fadca883d0ce74ce
kajarenc <kajarenc@gmail.com>
2021-05-07 19:16:42 +0400

    Quote fix

--- e2e/specs/video_recording.spec.js
+++ e2e/specs/video_recording.spec.js
@@ -7,7 +7,7 @@ describe("video recording", () => {
 
   it("displays recording indicator during recording", () => {
     cy.get("#MainMenu > button").click();
-    cy.get('[data-testid="main-menu-list"]')
+    cy.get("[data-testid='main-menu-list']")
       .contains("Record a screencast")
       .click();
 
@@ -16,17 +16,17 @@ describe("video recording", () => {
       .click();
 
     // Wait for the countdown before starting the video recording.
-    cy.get("[data-testid='recordingIndicator']", { timeout: 6000 }).should(
+    cy.get("[data-testid='recordingIndicator']", { timeout: 5000 }).should(
       "exist"
     );
 
     cy.get("#MainMenu > button").click();
 
-    cy.get('[data-testid="main-menu-list"]')
+    cy.get("[data-testid='main-menu-list']")
       .contains("Stop recording")
       .click();
 
-    cy.get('[data-baseweb="modal"] [aria-label="Close"]').click();
+    cy.get("[data-baseweb='modal'] [aria-label='Close']").click();
 
     cy.get("[data-testid='recordingIndicator']").should("not.exist");
   });
