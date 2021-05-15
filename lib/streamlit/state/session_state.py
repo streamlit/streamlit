4dcc7746e78586bf25717926fb614fe5d3819240
Vincent Donato <vincent@streamlit.io>
2021-05-14 17:31:53 -0700

    Implement _get_session_state (#3267)
    * Implement _get_session_state
    
    _get_session_state fetches the SessionState object for the session it is called
    from.
    
    We're once again mostly able to copy the implementation in the prototype for
    this PR. The only notable change was to account for the fact that the
    st.session_state API no longer allows users to initialize default values
    for SessionState objects on creation (more precisely, users can no
    longer directly call the SessionState constructor), so we can create a
    SessionState object when we initialize a ReportSession.
    
    We also had to
    
    * Create an __init__.py file in the new state directory. We didn't
      notice that this hadn't been done before because mypy didn't start
      complaining until we started importing streamlit.state.session_state
      in other files.
    * Move some test util code out of server_test.py and into
      server_test_case.py.
    
    * Instantiate SessionState in ReportSession.__init__
    
    * Add another _get_session_state test

--- lib/streamlit/state/session_state.py
+++ lib/streamlit/state/session_state.py
@@ -126,3 +126,28 @@ class SessionState(MutableMapping[str, Any]):
             del self[key]
         except KeyError:
             raise AttributeError(key)
+
+
+def _get_session_state() -> SessionState:
+    """Get the SessionState object for the current session.
+
+    Note that in streamlit scripts, this function should not be called
+    directly. Instead, SessionState objects should be accessed via
+    st.session_state.
+    """
+    # Getting the session id easily comes from the report context, which is
+    # a little weird, but a precedent that has been set.
+    ctx = get_report_ctx()
+    this_session: Optional["ReportSession"] = None
+
+    if ctx is not None:
+        this_session = Server.get_current().get_session_by_id(ctx.session_id)
+
+    if this_session is None:
+        raise RuntimeError(
+            "We were unable to retrieve your Streamlit session."
+            " Is your application utilizing threads? It's possible that could"
+            " be conflicting with our system."
+        )
+
+    return this_session.session_state
