b63749883c9677619d1e70b15dc3a0d49d06e606
Vincent Donato <vincent@streamlit.io>
2021-05-14 17:31:53 -0700

    Implement _get_session_state (#3267)
    * Implement _get_session_state
    
    _get_session_state fetches the SessionState object for the session it is called
    from.
    
    We're once again mostly able to copy the implementation in the prototype for
    this PR. The only notable change was to account for the fact that the
    st.session_state API no longer allows users to initialize default values
    for SessionState objects on creation (more precisely, users can no
    longer directly call the SessionState constructor), so we can create a
    SessionState object when we initialize a ReportSession.
    
    We also had to
    
    * Create an __init__.py file in the new state directory. We didn't
      notice that this hadn't been done before because mypy didn't start
      complaining until we started importing streamlit.state.session_state
      in other files.
    * Move some test util code out of server_test.py and into
      server_test_case.py.
    
    * Instantiate SessionState in ReportSession.__init__
    
    * Add another _get_session_state test

--- lib/streamlit/report_session.py
+++ lib/streamlit/report_session.py
@@ -15,6 +15,7 @@
 import sys
 import uuid
 from enum import Enum
+from typing import Optional, TYPE_CHECKING
 
 import tornado.gen
 import tornado.ioloop
@@ -46,6 +47,8 @@ from streamlit.uploaded_file_manager import UploadedFileManager
 from streamlit.watcher.local_sources_watcher import LocalSourcesWatcher
 
 LOGGER = get_logger(__name__)
+if TYPE_CHECKING:
+    from streamlit.state.session_state import SessionState
 
 
 class ReportSessionState(Enum):
@@ -112,6 +115,13 @@ class ReportSession(object):
         self._script_request_queue = ScriptRequestQueue()
 
         self._scriptrunner = None
+
+        # This needs to be lazily imported to avoid a dependency cycle.
+        from streamlit.state.session_state import SessionState
+
+        self._session_state = SessionState()
+
+        # TODO: Also initialize the yet-to-be-implemented WidgetStateManager.
 
         LOGGER.debug("ReportSession initialized (id=%s)", self.id)
 
@@ -229,6 +239,10 @@ class ReportSession(object):
         self._enqueue_script_request(ScriptRequest.RERUN, rerun_data)
         self._set_page_config_allowed = True
 
+    @property
+    def session_state(self) -> "SessionState":
+        return self._session_state
+
     def _on_source_file_changed(self):
         """One of our source files changed. Schedule a rerun if appropriate."""
         if self._run_on_save:
@@ -304,11 +318,8 @@ class ReportSession(object):
                     self._local_sources_watcher.update_watched_modules
                 )
             else:
-                # When a script fails to compile, we send along the exception.
-                import streamlit.elements.exception as exception_utils
-
                 msg = ForwardMsg()
-                exception_utils.marshall(
+                exception.marshall(
                     msg.session_event.script_compilation_exception, exception
                 )
                 self.enqueue(msg)
