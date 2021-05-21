8a26fdc780ba25a0586d7f667a7c8f9aaf00462a
snehankekre <snehan.minerva@gmail.com>
2021-05-21 15:39:58 +0530

    Test Spanish translations on RTD

--- docs/conf.py
+++ docs/conf.py
@@ -81,6 +81,9 @@ master_doc = "index"
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
 language = None
+locale_dirs = ['locale/']
+gettext_uuid = True
+gettext_compact = False
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
