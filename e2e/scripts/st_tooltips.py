3c3288b78f5edc31b2af0fce2d5d5812223486c9
RISHABH BUDHIRAJA <rishabh.budhiraja@gmail.com>
2021-05-22 22:50:01 +0530

    Fix: Tooltip should de-indent strings

--- e2e/scripts/st_tooltips.py
+++ e2e/scripts/st_tooltips.py
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import textwrap
+
 import streamlit as st
 from datetime import datetime
 
@@ -26,17 +28,17 @@ sapien eget diam euismod eleifend. Nulla purus enim, finibus ut velit eu,
 malesuada dictum nulla. In non arcu et risus maximus fermentum eget nec ante.
 """.strip()
 
-st.text_input("some input text", "default text", help=default_tooltip)
-st.number_input("number input", value=1, help=default_tooltip)
-st.checkbox("some checkbox", help=default_tooltip)
-st.radio("best animal", ("tiger", "giraffe", "bear"), 0, help=default_tooltip)
-st.button("some button", help=default_tooltip)
-st.selectbox("selectbox", ("a", "b", "c"), 0, help=default_tooltip)
-st.time_input("time", datetime(2019, 7, 6, 21, 15), help=default_tooltip)
-st.date_input("date", datetime(2019, 7, 6, 21, 15), help=default_tooltip)
-st.slider("slider", 0, 100, 50, help=default_tooltip)
-st.color_picker("color picker", help=default_tooltip)
-st.file_uploader("file uploader", help=default_tooltip)
-st.multiselect("multiselect", ["a", "b", "c"], ["a", "b"], help=default_tooltip)
-st.text_area("textarea", help=default_tooltip)
-st.select_slider("selectslider", options=["a", "b", "c"], help=default_tooltip)
+st.text_input("some input text", "default text", help=textwrap.dedent(default_tooltip))
+st.number_input("number input", value=1, help=textwrap.dedent(default_tooltip))
+st.checkbox("some checkbox", help=textwrap.dedent(default_tooltip))
+st.radio("best animal", ("tiger", "giraffe", "bear"), 0, help=textwrap.dedent(default_tooltip))
+st.button("some button", help=textwrap.dedent(default_tooltip))
+st.selectbox("selectbox", ("a", "b", "c"), 0, help=textwrap.dedent(default_tooltip))
+st.time_input("time", datetime(2019, 7, 6, 21, 15), help=textwrap.dedent(default_tooltip))
+st.date_input("date", datetime(2019, 7, 6, 21, 15), help=textwrap.dedent(default_tooltip))
+st.slider("slider", 0, 100, 50, help=textwrap.dedent(default_tooltip))
+st.color_picker("color picker", help=textwrap.dedent(default_tooltip))
+st.file_uploader("file uploader", help=textwrap.dedent(default_tooltip))
+st.multiselect("multiselect", ["a", "b", "c"], ["a", "b"], help=textwrap.dedent(default_tooltip))
+st.text_area("textarea", help=textwrap.dedent(default_tooltip))
+st.select_slider("selectslider", options=["a", "b", "c"], help=textwrap.dedent(default_tooltip))
