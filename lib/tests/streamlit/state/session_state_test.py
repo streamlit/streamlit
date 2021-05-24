cd87b6605d7e7ffd2d407513473ad8b4dda10fea
Vincent Donato <vincent@streamlit.io>
2021-05-24 16:41:59 -0700

    Start session state and widget state unification (#3309)
    * Allow widget values to be accessible from st.session_state
    
    * Add helpers for flagging incorrect session_state API usage
    
    * Address PR feedback
    
    * Fix test to support both python 3.6 and 3.8
    
    Co-authored-by: Amanda Walker <amanda@amandawalker.io>

--- lib/tests/streamlit/state/session_state_test.py
+++ lib/tests/streamlit/state/session_state_test.py
@@ -22,9 +22,11 @@ from streamlit.errors import StreamlitAPIException
 from streamlit.report_session import ReportSession
 from streamlit.report_thread import _StringSet
 from streamlit.state.session_state import (
-    _get_session_state,
+    get_session_state,
+    _get_widget_mgr,
     SessionState,
 )
+from streamlit.state.widgets import WidgetManager
 from streamlit.uploaded_file_manager import UploadedFileManager
 from tests.server_test_case import ServerTestCase
 
@@ -37,6 +39,14 @@ class SessionStateTests(unittest.TestCase):
             old_state={"foo": "bar"},
             new_state={"baz": "qux"},
         )
+
+        patcher = patch("streamlit.state.session_state._get_widget_mgr")
+        self.addCleanup(patcher.stop)
+
+        mock_get_widget_mgr = patcher.start()
+        mock_widget_mgr = MagicMock()
+        mock_widget_mgr.get_keyed_widget_values.return_value = {"foo2": "bar2"}
+        mock_get_widget_mgr.return_value = mock_widget_mgr
 
     def test_mapping_invariants(self):
         s = self.session_state
@@ -51,37 +61,47 @@ class SessionStateTests(unittest.TestCase):
         assert self.session_state._old_state == {"foo": "bar", "baz": "qux"}
 
     def test_merged_state(self):
-        assert self.session_state._merged_state == {"foo": "bar", "baz": "qux"}
+        assert self.session_state._merged_state == {
+            "foo": "bar",
+            "foo2": "bar2",
+            "baz": "qux",
+        }
 
     def test_merged_state_new_state_priority(self):
         session_state = SessionState(
-            old_state={"foo": "bar"},
+            old_state={"foo": "bar", "foo2": "bar1"},
             new_state={"foo": "baz"},
         )
-        assert session_state._merged_state == {"foo": "baz"}
+        assert session_state._merged_state == {"foo": "baz", "foo2": "bar2"}
+
+        session_state = SessionState(old_state={}, new_state={"foo2": "baz"})
+        assert session_state._merged_state == {"foo2": "baz"}
 
     def test_is_new_value(self):
         assert not self.session_state.is_new_value("foo")
+        assert not self.session_state.is_new_value("foo2")
         assert self.session_state.is_new_value("baz")
 
     def test_contains(self):
         assert "corge" not in self.session_state  # does not contain key
         assert "foo" in self.session_state  # key in new state
+        assert "foo2" in self.session_state  # key in widget manager state
         assert "baz" in self.session_state  # key in old state
 
     def test_iter(self):
         state_iter = iter(self.session_state)
         assert next(state_iter) == "foo"
+        assert next(state_iter) == "foo2"
         assert next(state_iter) == "baz"
 
         with pytest.raises(StopIteration):
             next(state_iter)
 
     def test_len(self):
-        assert len(self.session_state) == 2
+        assert len(self.session_state) == 3
 
     def test_str(self):
-        assert str(self.session_state) == "{'foo': 'bar', 'baz': 'qux'}"
+        assert str(self.session_state) == "{'foo': 'bar', 'foo2': 'bar2', 'baz': 'qux'}"
 
     def test_getitem(self):
         assert self.session_state["foo"] == "bar"
@@ -116,7 +136,7 @@ class SessionStateTests(unittest.TestCase):
 
     def test_delitem(self):
         del self.session_state["foo"]
-        assert self.session_state._merged_state == {"baz": "qux"}
+        assert self.session_state._merged_state == {"baz": "qux", "foo2": "bar2"}
 
     def test_delitem_error(self):
         with pytest.raises(KeyError) as e:
@@ -156,7 +176,7 @@ class SessionStateTests(unittest.TestCase):
 
     def test_delattr(self):
         del self.session_state.foo
-        assert self.session_state._merged_state == {"baz": "qux"}
+        assert self.session_state._merged_state == {"baz": "qux", "foo2": "bar2"}
 
     def test_delattr_error(self):
         with pytest.raises(AttributeError) as e:
@@ -168,7 +188,7 @@ class SessionStateTests(unittest.TestCase):
 # ReportSession instantiating one below are suppressed.
 @patch("streamlit.report_session.LocalSourcesWatcher")
 @patch("streamlit.state.session_state.get_report_ctx")
-class GetSessionStateTests(ServerTestCase):
+class SessionStateHelperTests(ServerTestCase):
     @tornado.testing.gen_test
     def test_get_session_state(self, patched_get_report_ctx, _):
         yield self.start_server_loop()
@@ -180,13 +200,14 @@ class GetSessionStateTests(ServerTestCase):
         mock_ctx.session_id = session.id
         patched_get_report_ctx.return_value = mock_ctx
 
-        assert isinstance(_get_session_state(), SessionState)
+        assert isinstance(get_session_state(), SessionState)
+        assert isinstance(_get_widget_mgr(), WidgetManager)
 
     def test_get_session_state_error_if_no_ctx(self, patched_get_report_ctx, _):
         patched_get_report_ctx.return_value = None
 
         with pytest.raises(RuntimeError) as e:
-            _get_session_state()
+            get_session_state()
         assert "We were unable to retrieve your Streamlit session." in str(e.value)
 
     @tornado.testing.gen_test
@@ -198,5 +219,5 @@ class GetSessionStateTests(ServerTestCase):
         patched_get_report_ctx.return_value = mock_ctx
 
         with pytest.raises(RuntimeError) as e:
-            _get_session_state()
+            get_session_state()
         assert "We were unable to retrieve your Streamlit session." in str(e.value)
