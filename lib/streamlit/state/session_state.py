a352071f843e1fcccd9f38081c80500d6c6679de
Amanda Walker <amanda@amandawalker.io>
2021-06-09 16:03:59 -0700

    Add comment about session state method implementation

--- lib/streamlit/state/session_state.py
+++ lib/streamlit/state/session_state.py
@@ -106,6 +106,9 @@ class WStates(MutableMapping[str, Any]):
         return len(self.states)
 
     def __iter__(self):
+        # For this and many other methods, we can't simply delegate to the
+        # states field, because we need to invoke `__getitem__` for any
+        # values, to handle deserialization and unwrapping of values.
         for key in self.states:
             yield key
 
