db5f750ccd87b61708b14cc4a1ce606c7d5c6940
Vincent Donato <vincent@streamlit.io>
2021-06-09 13:29:02 -0700

    Add forms with multiple submit buttons fix and tests (#3376)
    * Add forms with multiple submit buttons fix and tests
    
    * Address PR feedback
    
    Co-authored-by: Amanda Walker <amanda@amandawalker.io>

---
+++ e2e/specs/form_multiple_buttons.spec.js
@@ -0,0 +1,26 @@
+/**
+ * @license
+ * Copyright 2018-2021 Streamlit Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+describe("st.form", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000/");
+  });
+
+  it("displays both buttons", () => {
+    cy.get("[data-testid=stFormSubmitButton]").should("have.length", 2);
+  });
+});
