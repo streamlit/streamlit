a28fb9790a85ab335357f75cc0053287dcd1d2ab
kajarenc <kajarenc@gmail.com>
2021-06-15 13:03:52 +0400

    * Add unittests for date_input, time_input, slider, text_input and textarea widgets inside columns

--- lib/tests/streamlit/text_area_test.py
+++ lib/tests/streamlit/text_area_test.py
@@ -75,6 +75,21 @@ class TextAreaTest(testutil.DeltaGeneratorTestCase):
         text_area_proto = self.get_delta_from_queue(1).new_element.text_area
         self.assertEqual(text_area_proto.form_id, form_proto.form.form_id)
 
+    def test_inside_column(self):
+        """Test that it works correctly inside of a column."""
+        col1, col2, col3 = st.beta_columns([2.5, 1.5, 8.3])
+
+        with col1:
+            st.text_area("foo")
+
+        all_deltas = self.get_all_deltas_from_queue()
+
+        # 5 elements will be created: 1 horizontal block, 3 columns, 1 widget
+        self.assertEqual(len(all_deltas), 5)
+        text_area_proto = self.get_delta_from_queue().new_element.text_area
+
+        self.assertEqual(text_area_proto.label, "foo")
+
 
 class SomeObj(object):
     pass
