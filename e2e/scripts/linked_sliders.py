b71c6f43e761eafc29c0ad6318c05306331c9ceb
Amanda Walker <amanda@amandawalker.io>
2021-06-22 15:13:39 -0700

    Wrap e2e test in check for streamlit running
    Otherwise the script will crash when the integration tests run it bare.

--- e2e/scripts/linked_sliders.py
+++ e2e/scripts/linked_sliders.py
@@ -14,42 +14,42 @@
 
 import streamlit as st
 
-to_celsius = lambda fahrenheit: (fahrenheit - 32) * 5.0 / 9.0
-to_fahrenheit = lambda celsius: 9.0 / 5.0 * celsius + 32
+if st._is_running_with_streamlit:
 
-MIN_CELSIUS, MAX_CELSIUS = -100.0, 100.0
+    to_celsius = lambda fahrenheit: (fahrenheit - 32) * 5.0 / 9.0
+    to_fahrenheit = lambda celsius: 9.0 / 5.0 * celsius + 32
 
-state = st.session_state
+    MIN_CELSIUS, MAX_CELSIUS = -100.0, 100.0
 
-if "celsius" not in st.session_state:
-    state.celsius = MIN_CELSIUS
+    state = st.session_state
 
-# Callbacks if something changes
-def celsius_changed():
-    state.fahrenheit = to_fahrenheit(state.celsius)
+    if "celsius" not in st.session_state:
+        state.celsius = MIN_CELSIUS
 
+    # Callbacks if something changes
+    def celsius_changed():
+        state.fahrenheit = to_fahrenheit(state.celsius)
 
-def fahrenheit_changed():
-    state.celsius = to_celsius(state.fahrenheit)
+    def fahrenheit_changed():
+        state.celsius = to_celsius(state.fahrenheit)
 
+    # Display the sliders.
+    st.slider(
+        "Celsius",
+        min_value=MIN_CELSIUS,
+        max_value=MAX_CELSIUS,
+        on_change=celsius_changed,
+        key="celsius",
+    )
 
-# Display the sliders.
-st.slider(
-    "Celsius",
-    min_value=MIN_CELSIUS,
-    max_value=MAX_CELSIUS,
-    on_change=celsius_changed,
-    key="celsius",
-)
+    st.slider(
+        "Fahrenheit",
+        min_value=to_fahrenheit(MIN_CELSIUS),
+        max_value=to_fahrenheit(MAX_CELSIUS),
+        value=to_fahrenheit(state.celsius),
+        on_change=fahrenheit_changed,
+        key="fahrenheit",
+    )
 
-st.slider(
-    "Fahrenheit",
-    min_value=to_fahrenheit(MIN_CELSIUS),
-    max_value=to_fahrenheit(MAX_CELSIUS),
-    value=to_fahrenheit(state.celsius),
-    on_change=fahrenheit_changed,
-    key="fahrenheit",
-)
-
-st.write("Celsius", state.celsius)
-st.write("Fahrenheit", state.fahrenheit)
+    st.write("Celsius", state.celsius)
+    st.write("Fahrenheit", state.fahrenheit)
