d75e316c90ac26279327ab9a08907dd0454dd255
Ken McGrady <ken@streamlit.io>
2021-07-14 15:14:44 -0700

    Allow set_page_config after callbacks (#3550)

--- e2e/scripts/st_set_page_config.py
+++ e2e/scripts/st_set_page_config.py
@@ -22,3 +22,41 @@ st.set_page_config(
 )
 st.sidebar.button("Sidebar!")
 st.markdown("Main!")
+
+
+def show_balloons():
+    st.balloons()
+
+
+st.button("Balloons", on_click=show_balloons)
+
+
+def double_set_page_config():
+    st.set_page_config(
+        page_title="Change 1",
+        page_icon=":shark:",
+        layout="wide",
+        initial_sidebar_state="collapsed",
+    )
+
+    st.set_page_config(
+        page_title="Change 2",
+        page_icon=":shark:",
+        layout="wide",
+        initial_sidebar_state="collapsed",
+    )
+
+
+st.button("Double Set Page Config", on_click=double_set_page_config)
+
+
+def single_set_page_config():
+    st.set_page_config(
+        page_title="Change 3",
+        page_icon=":shark:",
+        layout="wide",
+        initial_sidebar_state="collapsed",
+    )
+
+
+st.button("Single Set Page Config", on_click=single_set_page_config)
