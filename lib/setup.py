274b4395784f684db49ed81b15966041a1664398
CircleCI <jonathan@streamlit.io>
2021-07-17 06:32:22 +0000

    Update version and project name in files

--- lib/setup.py
+++ lib/setup.py
@@ -27,9 +27,9 @@ except:
     )
     sys.exit(exit_msg)
 
-VERSION = "0.84.0"  # PEP-440
+VERSION = "0.84.2.dev20210716"  # PEP-440
 
-NAME = "streamlit"
+NAME = "streamlit-nightly"
 
 DESCRIPTION = "The fastest way to build data apps in Python"
 
