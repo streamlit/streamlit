cef14bdb3b38509292ada834f1f0642c4a6e6cf9
Tim Conkling <tconkling@gmail.com>
2021-07-20 15:07:32 -0700

    Don't hash function referenced objects

--- lib/streamlit/memoization/memo_hashing.py
+++ lib/streamlit/memoization/memo_hashing.py
@@ -556,18 +556,22 @@ class _CodeHasher:
         # Hash the bytecode.
         self.update(h, code.co_code)
 
-        # Hash constants that are referenced by the bytecode but ignore names of lambdas.
-        consts = [
-            n
-            for n in code.co_consts
-            if not isinstance(n, str) or not n.endswith(".<lambda>")
-        ]
-        self.update(h, consts, context)
+        # We are deliberately _only_ hashing a function's body.
+        # Not following references, not hashing closed-over objects.
+        # TODO: remove all this!
 
-        context.cells.push(code, func=func)
-        for ref in get_referenced_objects(code, context):
-            self.update(h, ref, context)
-        context.cells.pop()
+        # # Hash constants that are referenced by the bytecode but ignore names of lambdas.
+        # consts = [
+        #     n
+        #     for n in code.co_consts
+        #     if not isinstance(n, str) or not n.endswith(".<lambda>")
+        # ]
+        # self.update(h, consts, context)
+        #
+        # context.cells.push(code, func=func)
+        # for ref in get_referenced_objects(code, context):
+        #     self.update(h, ref, context)
+        # context.cells.pop()
 
         return h.digest()
 
