bebcd2cc862cf21a52a52af9292536d0d649c00e
willhuang1997 <willhuang1997@gmail.com>
2021-08-03 16:04:10 -0700

    Reformatting

--- lib/tests/streamlit/image_test.py
+++ lib/tests/streamlit/image_test.py
@@ -281,17 +281,8 @@ class ImageProtoTest(testutil.DeltaGeneratorTestCase):
         """
         pass
 
-    @parameterized.expand(
-        [
-            ("P", True),
-            ("RGBA", True),
-            ("LA", True),
-            ("RGB", False)
-        ]
-    )
+    @parameterized.expand([("P", True), ("RGBA", True), ("LA", True), ("RGB", False)])
     def test_image_may_have_alpha_channel(self, format: str, expected_alpha: bool):
-        img = Image.new(format,(1,1))
+        img = Image.new(format, (1, 1))
         self.assertTrue(img.mode == format)
         self.assertEqual(image._image_may_have_alpha_channel(img), expected_alpha)
-
-
