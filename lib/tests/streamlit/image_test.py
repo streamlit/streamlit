b88c95d9a69b9a82bfb9e3d51772af50e29dcab2
willhuang1997 <willhuang1997@gmail.com>
2021-08-03 16:03:56 -0700

    Changing logic to may have alpha channel with unit tests

--- lib/tests/streamlit/image_test.py
+++ lib/tests/streamlit/image_test.py
@@ -280,3 +280,18 @@ class ImageProtoTest(testutil.DeltaGeneratorTestCase):
         * int  with clipping
         """
         pass
+
+    @parameterized.expand(
+        [
+            ("P", True),
+            ("RGBA", True),
+            ("LA", True),
+            ("RGB", False)
+        ]
+    )
+    def test_image_may_have_alpha_channel(self, format: str, expected_alpha: bool):
+        img = Image.new(format,(1,1))
+        self.assertTrue(img.mode == format)
+        self.assertEqual(image._image_may_have_alpha_channel(img), expected_alpha)
+
+
