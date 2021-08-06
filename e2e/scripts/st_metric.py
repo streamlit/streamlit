20997e3a98ecdc77e8564152d627149247287c73
Thiago Teixeira <thiago@streamlit.io>
2021-08-06 15:55:19 -0700

    Design pass on st.metric. Plus improve visual test.

--- e2e/scripts/st_metric.py
+++ e2e/scripts/st_metric.py
@@ -17,8 +17,19 @@ import streamlit as st
 col1, col2, col3 = st.beta_columns(3)
 
 with col1:
-    st.metric("Test 1", 123, 123, "normal")
+    st.metric("User growth", 123, 123, "normal")
+with col2:
+    st.metric("S&P 500", -4.56, 0)
+with col3:
+    st.metric("Apples I've eaten", "23k", " -20", "off")
+
+" "
+
+col1, col2, col3 = st.beta_columns(3)
+
+with col1:
+    st.selectbox("Pick one", [])
 with col2:
     st.metric("Test 2", -4.56, 1.23, "inverse")
 with col3:
-    st.metric("Test 3", "23k", " -20", "off")
+    st.slider("Pick another")
