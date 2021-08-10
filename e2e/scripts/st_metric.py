d2c426ede2ff85ff9946f6dc44993d11d437b159
willhuang1997 <willhuang1997@gmail.com>
2021-08-10 10:47:04 -0700

    Run e2e tests

--- e2e/scripts/st_metric.py
+++ e2e/scripts/st_metric.py
@@ -19,7 +19,7 @@ col1, col2, col3 = st.beta_columns(3)
 with col1:
     st.metric("User growth", 123, 123, "normal")
 with col2:
-    st.metric("S&P 500", -4.56, 0)
+    st.metric("S&P 500", -4.56, -50)
 with col3:
     st.metric("Apples I've eaten", "23k", " -20", "off")
 
