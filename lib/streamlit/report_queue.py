bcef063e5da8cb2285d50702465aeff828ecbd4f
Tim Conkling <tconkling@gmail.com>
2021-08-09 18:19:01 -0700

    don't need to lock inside a constructor

--- lib/streamlit/report_queue.py
+++ lib/streamlit/report_queue.py
@@ -36,16 +36,14 @@ class ReportQueue:
     def __init__(self):
         """Constructor."""
         self._lock = threading.Lock()
+        self._queue: List[ForwardMsg] = []
 
-        with self._lock:
-            self._queue: List[ForwardMsg] = []
-
-            # A mapping of (delta_path -> _queue.indexof(msg)) for each
-            # Delta message in the queue. We use this for coalescing
-            # redundant outgoing Deltas (where a newer Delta supercedes
-            # an older Delta, with the same delta_path, that's still in the
-            # queue).
-            self._delta_index_map: Dict[Tuple[int, ...], int] = dict()
+        # A mapping of (delta_path -> _queue.indexof(msg)) for each
+        # Delta message in the queue. We use this for coalescing
+        # redundant outgoing Deltas (where a newer Delta supercedes
+        # an older Delta, with the same delta_path, that's still in the
+        # queue).
+        self._delta_index_map: Dict[Tuple[int, ...], int] = dict()
 
     def __repr__(self) -> str:
         return util.repr_(self)
