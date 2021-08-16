eb6dcb226e5d6b6820f04977680455207685e376
kajarenc <kajarenc@gmail.com>
2021-08-16 21:30:43 +0400

    add unittests

--- lib/tests/streamlit/download_button_test.py
+++ lib/tests/streamlit/download_button_test.py
@@ -15,15 +15,25 @@
 """download_button unit test."""
 
 import streamlit as st
+
+from parameterized import parameterized
 from tests import testutil
 
 
 class DownloadButtonTest(testutil.DeltaGeneratorTestCase):
     """Test ability to marshall download_button protos."""
 
-    def test_just_label(self):
-        """Test that it can be called with label and string data."""
-        st.download_button("the label", data="hello world")
+    @parameterized.expand([("hello world",), (b"byteshere",)])
+    def test_just_label(self, data):
+        """Test that it can be called with label and string or bytes data."""
+        st.download_button("the label", data=data)
 
         c = self.get_delta_from_queue().new_element.download_button
         self.assertEqual(c.label, "the label")
+
+    def test_url_exist(self):
+        """Test that file url exist in proto."""
+        st.download_button("the label", data="juststring")
+
+        c = self.get_delta_from_queue().new_element.download_button
+        self.assertTrue("/media/", c.url)
