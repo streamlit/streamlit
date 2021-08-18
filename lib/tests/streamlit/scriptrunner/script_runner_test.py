0cb93cde3176e1ed970f17201c8273704becf4ba
willhuang1997 <willhuang1997@gmail.com>
2021-08-18 01:48:09 -0700

    Reformatting changes

--- lib/tests/streamlit/scriptrunner/script_runner_test.py
+++ lib/tests/streamlit/scriptrunner/script_runner_test.py
@@ -277,7 +277,9 @@ class ScriptRunnerTest(AsyncTestCase):
                 self.assertEqual(elts[1].WhichOneof("type"), "exception")
                 exc_msg = elts[1].exception.message
                 self.assertTrue(_GENERIC_UNCAUGHT_EXCEPTION_TEXT in exc_msg)
-                self.assertTrue("Exception Type: <class 'AttributeError'>..." in exc_msg)
+                self.assertTrue(
+                    "Exception Type: <class 'AttributeError'>..." in exc_msg
+                )
                 self.assertTrue("bad.do_a_thing()" in exc_msg)
 
     def test_stop_script(self):
