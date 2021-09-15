54e57161f9eae7c7b5c82222f6b64f80b9492a39
Amanda Walker <amanda@amandawalker.io>
2021-09-15 16:52:49 -0700

    Add comment for SessionState item lookup

--- lib/streamlit/state/session_state.py
+++ lib/streamlit/state/session_state.py
@@ -363,7 +363,9 @@ class SessionState(MutableMapping[str, Any]):
     def __getitem__(self, key: str) -> Any:
         wid_key_map = self.reverse_key_wid_map
         widget_id = self._get_widget_id(key)
+
         if widget_id in wid_key_map and widget_id == key:
+            # the "key" is a raw widget id, so get its associated user key for lookup
             key = wid_key_map[widget_id]
         try:
             return self._getitem(widget_id, key)
