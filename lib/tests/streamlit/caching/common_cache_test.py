5a91298b5872d30aa466292d6b1ac42b67d5908b
Tim Conkling <tconkling@gmail.com>
2021-09-22 14:56:33 -0700

    Pull common cache tests into "common_cache_test"

--- lib/tests/streamlit/caching/common_cache_test.py
+++ lib/tests/streamlit/caching/common_cache_test.py
@@ -13,18 +13,156 @@
 # limitations under the License.
 
 """Tests that are common to both st.memo and st.singleton"""
+import threading
 import unittest
+from unittest.mock import patch
 
 from parameterized import parameterized
+
 import streamlit as st
+from streamlit.caching import MEMO_CALL_STACK, SINGLETON_CALL_STACK
+
+memo = st.experimental_memo
+singleton = st.experimental_singleton
 
 
 class CommonCacheTest(unittest.TestCase):
-    @parameterized.expand([(st.experimental_memo, ), (st.experimental_singleton,)])
-    def test_simple(self, cache_decorator):
+    def tearDown(self):
+        # Some of these tests reach directly into CALL_STACK data and twiddle it.
+        # Reset default values on teardown.
+        MEMO_CALL_STACK._cached_func_stack = []
+        MEMO_CALL_STACK._suppress_st_function_warning = 0
+        SINGLETON_CALL_STACK._cached_func_stack = []
+        SINGLETON_CALL_STACK._suppress_st_function_warning = 0
+        super().tearDown()
+
+    @parameterized.expand([("memo", memo), ("singleton", singleton)])
+    def test_simple(self, _, cache_decorator):
         @cache_decorator
         def foo():
             return 42
 
         self.assertEqual(foo(), 42)
         self.assertEqual(foo(), 42)
+
+    @parameterized.expand([("memo", memo), ("singleton", singleton)])
+    def test_multiple_int_like_floats(self, _, cache_decorator):
+        @cache_decorator
+        def foo(x):
+            return x
+
+        self.assertEqual(foo(1.0), 1.0)
+        self.assertEqual(foo(3.0), 3.0)
+
+    @parameterized.expand([("memo", memo), ("singleton", singleton)])
+    def test_return_cached_object(self, _, cache_decorator):
+        """If data has been cached, the cache function shouldn't be called."""
+        with patch.object(st, "exception") as mock_exception:
+            called = [False]
+
+            @cache_decorator
+            def f(x):
+                called[0] = True
+                return x
+
+            self.assertFalse(called[0])
+            f(0)
+
+            self.assertTrue(called[0])
+
+            called = [False]  # Reset called
+
+            f(0)
+            self.assertFalse(called[0])
+
+            f(1)
+            self.assertTrue(called[0])
+
+            mock_exception.assert_not_called()
+
+    @parameterized.expand([("memo", memo), ("singleton", singleton)])
+    def test_mutate_args(self, _, cache_decorator):
+        """Mutating an argument inside a memoized function doesn't throw
+        an error (but it's probably not a great idea)."""
+        with patch.object(st, "exception") as mock_exception:
+
+            @cache_decorator
+            def foo(d):
+                d["answer"] += 1
+                return d["answer"]
+
+            d = {"answer": 0}
+
+            self.assertEqual(foo(d), 1)
+            self.assertEqual(foo(d), 2)
+
+            mock_exception.assert_not_called()
+
+    @parameterized.expand([("memo", memo), ("singleton", singleton)])
+    def test_ignored_args(self, _, cache_decorator):
+        """Args prefixed with _ are not used as part of the cache key."""
+        call_count = [0]
+
+        @cache_decorator
+        def foo(arg1, _arg2, *args, kwarg1, _kwarg2=None, **kwargs):
+            call_count[0] += 1
+
+        foo(1, 2, 3, kwarg1=4, _kwarg2=5, kwarg3=6, _kwarg4=7)
+        self.assertEqual([1], call_count)
+
+        # Call foo again, but change the values for _arg2, _kwarg2, and _kwarg4.
+        # The call count shouldn't change, because these args will not be part
+        # of the hash.
+        foo(1, None, 3, kwarg1=4, _kwarg2=None, kwarg3=6, _kwarg4=None)
+        self.assertEqual([1], call_count)
+
+        # Changing the value of any other argument will increase the call
+        # count. We test each argument type:
+
+        # arg1 (POSITIONAL_OR_KEYWORD)
+        foo(None, 2, 3, kwarg1=4, _kwarg2=5, kwarg3=6, _kwarg4=7)
+        self.assertEqual([2], call_count)
+
+        # *arg (VAR_POSITIONAL)
+        foo(1, 2, None, kwarg1=4, _kwarg2=5, kwarg3=6, _kwarg4=7)
+        self.assertEqual([3], call_count)
+
+        # kwarg1 (KEYWORD_ONLY)
+        foo(1, 2, 3, kwarg1=None, _kwarg2=5, kwarg3=6, _kwarg4=7)
+        self.assertEqual([4], call_count)
+
+        # **kwarg (VAR_KEYWORD)
+        foo(1, 2, 3, kwarg1=4, _kwarg2=5, kwarg3=None, _kwarg4=7)
+        self.assertEqual([5], call_count)
+
+    @parameterized.expand(
+        [("memo", MEMO_CALL_STACK), ("singleton", SINGLETON_CALL_STACK)]
+    )
+    def test_multithreaded_call_stack(self, _, call_stack):
+        """Test that cached_func_stack behaves properly in multiple threads."""
+
+        def get_counter():
+            return len(call_stack._cached_func_stack)
+
+        def set_counter(val):
+            call_stack._cached_func_stack = ["foo"] * val
+
+        self.assertEqual(0, get_counter())
+        set_counter(1)
+        self.assertEqual(1, get_counter())
+
+        values_in_thread = []
+
+        def thread_test():
+            values_in_thread.append(get_counter())
+            set_counter(55)
+            values_in_thread.append(get_counter())
+
+        thread = threading.Thread(target=thread_test)
+        thread.start()
+        thread.join()
+
+        self.assertEqual([0, 55], values_in_thread)
+
+        # The other thread should not have modified the main thread
+        self.assertEqual(1, get_counter())
--- lib/tests/streamlit/caching/singleton_test.py
+++ lib/tests/streamlit/caching/singleton_test.py
@@ -30,42 +30,9 @@ class SingletonTest(unittest.TestCase):
         singleton_decorator.SINGLETON_CALL_STACK._suppress_st_function_warning = 0
         super().tearDown()
 
-    def test_multiple_int_like_floats(self):
-        @st.experimental_singleton
-        def foo(x):
-            return x
-
-        self.assertEqual(foo(1.0), 1.0)
-        self.assertEqual(foo(3.0), 3.0)
-
-    @patch.object(st, "exception")
-    def test_return_memoized_object(self, exception):
-        """If data has been cached, the memoized function shouldn't be called."""
-        called = [False]
-
-        @st.experimental_singleton
-        def f(x):
-            called[0] = True
-            return x
-
-        self.assertFalse(called[0])
-        f(0)
-
-        self.assertTrue(called[0])
-
-        called = [False]  # Reset called
-
-        f(0)
-        self.assertFalse(called[0])
-
-        f(1)
-        self.assertTrue(called[0])
-
-        exception.assert_not_called()
-
     @patch.object(st, "exception")
     def test_mutate_return(self, exception):
-        """Mutating a memoized return value is legal, and will affect
+        """Mutating a singleton return value is legal, and *will* affect
         future accessors of the data."""
 
         @st.experimental_singleton
@@ -82,23 +49,6 @@ class SingletonTest(unittest.TestCase):
 
         self.assertEqual(r1, [1, 1])
         self.assertEqual(r2, [1, 1])
-
-    @patch.object(st, "exception")
-    def test_mutate_args(self, exception):
-        """Mutating an argument inside a memoized function doesn't throw
-        an error (but it's probably not a great idea)."""
-
-        @st.experimental_singleton
-        def foo(d):
-            d["answer"] += 1
-            return d["answer"]
-
-        d = {"answer": 0}
-
-        self.assertEqual(foo(d), 1)
-        self.assertEqual(foo(d), 2)
-
-        exception.assert_not_called()
 
     @patch(
         "streamlit.caching.singleton_decorator.SINGLETON_CALL_STACK._show_cached_st_function_warning"
@@ -173,68 +123,3 @@ class SingletonTest(unittest.TestCase):
         # Make sure everything got reset properly
         st.text("foo")
         warning.assert_not_called()
-
-    def test_multithread_stack(self):
-        """Test that cached_func_stack behaves properly in multiple threads."""
-
-        def get_counter():
-            return len(singleton_decorator.SINGLETON_CALL_STACK._cached_func_stack)
-
-        def set_counter(val):
-            singleton_decorator.SINGLETON_CALL_STACK._cached_func_stack = ["foo"] * val
-
-        self.assertEqual(0, get_counter())
-        set_counter(1)
-        self.assertEqual(1, get_counter())
-
-        values_in_thread = []
-
-        def thread_test():
-            values_in_thread.append(get_counter())
-            set_counter(55)
-            values_in_thread.append(get_counter())
-
-        thread = threading.Thread(target=thread_test)
-        thread.start()
-        thread.join()
-
-        self.assertEqual([0, 55], values_in_thread)
-
-        # The other thread should not have modified the main thread
-        self.assertEqual(1, get_counter())
-
-    def test_ignored_args(self):
-        """Args prefixed with _ are not used as part of the cache key."""
-        call_count = [0]
-
-        @st.experimental_singleton
-        def foo(arg1, _arg2, *args, kwarg1, _kwarg2=None, **kwargs):
-            call_count[0] += 1
-
-        foo(1, 2, 3, kwarg1=4, _kwarg2=5, kwarg3=6, _kwarg4=7)
-        self.assertEqual([1], call_count)
-
-        # Call foo again, but change the values for _arg2, _kwarg2, and _kwarg4.
-        # The call count shouldn't change, because these args will not be part
-        # of the hash.
-        foo(1, None, 3, kwarg1=4, _kwarg2=None, kwarg3=6, _kwarg4=None)
-        self.assertEqual([1], call_count)
-
-        # Changing the value of any other argument will increase the call
-        # count. We test each argument type:
-
-        # arg1 (POSITIONAL_OR_KEYWORD)
-        foo(None, 2, 3, kwarg1=4, _kwarg2=5, kwarg3=6, _kwarg4=7)
-        self.assertEqual([2], call_count)
-
-        # *arg (VAR_POSITIONAL)
-        foo(1, 2, None, kwarg1=4, _kwarg2=5, kwarg3=6, _kwarg4=7)
-        self.assertEqual([3], call_count)
-
-        # kwarg1 (KEYWORD_ONLY)
-        foo(1, 2, 3, kwarg1=None, _kwarg2=5, kwarg3=6, _kwarg4=7)
-        self.assertEqual([4], call_count)
-
-        # **kwarg (VAR_KEYWORD)
-        foo(1, 2, 3, kwarg1=4, _kwarg2=5, kwarg3=None, _kwarg4=7)
-        self.assertEqual([5], call_count)
