66f7f993bb5dfe943f996a6df9ece12eff36b5b6
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2021-10-05 21:53:44 -0400

    [Arrow] Immutable add rows with `immer` (#3840)
    * Immutable add rows with Immer
    
    * Fix ReportNode unit tests
    
    * Add E2E tests
    
    * Add E2E snapshots
    
    * addRows doesn't mutate the original dataframe
    
    * Add comment explaining why the test is needed

---
+++ e2e/scripts/st_arrow_line_chart_add_rows_special.py
@@ -0,0 +1,40 @@
+# Copyright 2018-2021 Streamlit Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+This example was failing due to an issue (#3653) in st._arrow_add_rows.
+In the previos implementation of Quiver, we were mutating the Quiver element
+in the addRows function, which prevented re-rendering of the line chart.
+This example reproduces the issue, so that we don't repeat the same mistake
+in the future.
+"""
+
+import time
+
+import pandas as pd
+import streamlit as st
+
+current_time = pd.to_datetime("08:00:00 2021-01-01", utc=True)
+simulation_step = pd.Timedelta(seconds=10)
+
+df1 = pd.DataFrame(data=[[current_time, 1]], columns=["t", "y"]).set_index("t")
+line_chart = st._arrow_line_chart(df1, use_container_width=True)
+
+for count in range(5):
+    current_time += simulation_step
+
+    df2 = pd.DataFrame(data=[[current_time, count]], columns=["t", "y"]).set_index("t")
+    # This issue is Arrow specific, that's why st._legacy_add_rows doesn't have a similar test.
+    line_chart._arrow_add_rows(df2)
+    time.sleep(1)
