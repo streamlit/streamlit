cf01284197bbe1251e63c89b288e89722e5def58
Amanda Walker <amanda@amandawalker.io>
2021-10-07 16:37:01 -0700

    Wrap e2e in check for integration tests

--- e2e/scripts/3873.py
+++ e2e/scripts/3873.py
@@ -1,15 +1,18 @@
 import streamlit as st
 
-if "checkbox1" not in st.session_state:
-    st.session_state.checkbox1 = True
+if st._is_running_with_streamlit:
+    if "checkbox1" not in st.session_state:
+        st.session_state.checkbox1 = True
 
+    def on_checkbox_change(changed_checkbox_number):
+        if changed_checkbox_number == 1:
+            st.session_state.checkbox2 = False
+        elif changed_checkbox_number == 2:
+            st.session_state.checkbox1 = False
 
-def on_checkbox_change(changed_checkbox_number):
-    if changed_checkbox_number == 1:
-        st.session_state.checkbox2 = False
-    elif changed_checkbox_number == 2:
-        st.session_state.checkbox1 = False
-
-
-st.checkbox(label="Checkbox1", key="checkbox1", on_change=on_checkbox_change, args=(1,))
-st.checkbox(label="Checkbox2", key="checkbox2", on_change=on_checkbox_change, args=(2,))
+    st.checkbox(
+        label="Checkbox1", key="checkbox1", on_change=on_checkbox_change, args=(1,)
+    )
+    st.checkbox(
+        label="Checkbox2", key="checkbox2", on_change=on_checkbox_change, args=(2,)
+    )
