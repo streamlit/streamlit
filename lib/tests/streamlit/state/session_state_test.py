adf217839a179e36e2129bf86f538fc451b96ae9
Amanda Walker <amanda@amandawalker.io>
2021-10-11 11:13:28 -0700

    Fix `SessionState.keys()` to handle new widget edgecase (#3837)
    * Fix `SessionState.keys()` to handle new widget edgecase
    
    It didn't properly combine an old session state key with the widget id
    of a new widget that uses that key. Likely some other methods also make
    incorrect assumptions that this never happens.
    
    * Explicit None checking in SessionState get method

--- lib/tests/streamlit/state/session_state_test.py
+++ lib/tests/streamlit/state/session_state_test.py
@@ -718,3 +718,33 @@ def test_key_wid_lookup_equiv(state):
     k_wid_map = state._key_id_mapping
     for k, wid in k_wid_map.items():
         assert state[k] == state[wid]
+
+
+def test_map_set_del_3837_regression():
+    """A regression test for `test_map_set_del` that involves too much setup
+    to conveniently use the hypothesis `example` decorator."""
+
+    meta1 = stst.mock_metadata(
+        "$$GENERATED_WIDGET_KEY-e3e70682-c209-4cac-629f-6fbed82c07cd-None", 0
+    )
+    meta2 = stst.mock_metadata(
+        "$$GENERATED_WIDGET_KEY-f728b4fa-4248-5e3a-0a5d-2f346baa9455-0", 0
+    )
+    m = SessionState()
+    m["0"] = 0
+    m.set_unkeyed_widget(
+        meta1, "$$GENERATED_WIDGET_KEY-e3e70682-c209-4cac-629f-6fbed82c07cd-None"
+    )
+    m.compact_state()
+
+    m.set_keyed_widget(
+        meta2, "$$GENERATED_WIDGET_KEY-f728b4fa-4248-5e3a-0a5d-2f346baa9455-0", "0"
+    )
+    key = "0"
+    value1 = 0
+
+    m[key] = value1
+    l1 = len(m)
+    del m[key]
+    assert key not in m
+    assert len(m) == l1 - 1
