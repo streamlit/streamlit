fab5d1fcd320bd360515f52af7b206b74414f9f3
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2021-10-15 12:24:41 -0400

    Add comment explaining why `_on_secrets_file_changed` was created

--- lib/streamlit/report_session.py
+++ lib/streamlit/report_session.py
@@ -257,7 +257,13 @@ class ReportSession(object):
             self._enqueue_file_change_message()
 
     def _on_secrets_file_changed(self, _):
-        """Is called when secrets._file_change_listener emits a Signal."""
+        """Called when `secrets._file_change_listener` emits a Signal."""
+
+        # NOTE: At the time of writing, this function only calls `_on_source_file_changed`.
+        # The reason behind creating this function instead of just passing `_on_source_file_changed`
+        # to `connect` / `disconnect` directly is that every function that is passed to `connect` / `disconnect`
+        # must have at least one argument for `sender` (in this case we don't really care about it, thus `_`),
+        # and introducing an unnecessary argument to `_on_source_file_changed` just for this purpose sounded finicky.
         self._on_source_file_changed()
 
     def _clear_queue(self):
