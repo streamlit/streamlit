52a7ab1ce9e52e1dace4c417f0b7e53e84ac0173
Randy Zwitch <rzwitch@gmail.com>
2021-10-18 14:04:39 -0400

    Delete docs folder and update Makefile

--- docs/api-examples-source/text.write3.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-import pandas as pd
-
-data_frame = pd.DataFrame(
-    {"first column": [1, 2, 3, 4], "second column": [10, 20, 30, 40]}
-)
-
-st.write("1 + 1 = ", 2)
-st.write("Below is a DataFrame:", data_frame, "Above is a dataframe.")
--- docs/api-examples-source/widget.selectbox.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-
-option = st.selectbox(
-    "How would you like to be contacted?", ("Email", "Home phone", "Mobile phone")
-)
-
-st.write("You selected:", option)
