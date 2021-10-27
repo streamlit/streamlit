c16eccccf2099cde132030fb33c51fd5cf02d3a3
mayagbarnes <maya8617@gmail.com>
2021-10-27 11:28:31 -0700

    Initial draft fix

--- frontend/src/components/core/StatusWidget/styled-components.ts
+++ frontend/src/components/core/StatusWidget/styled-components.ts
@@ -16,7 +16,7 @@
  */
 
 import styled, { CSSObject } from "@emotion/styled"
-import { Theme } from "src/theme"
+import { darkTheme, Theme } from "src/theme"
 
 /*
   "ConnectionStatus" styles are used for displaying
@@ -118,12 +118,17 @@ export const StyledReportButtonContainer = styled.span<
   ...(isMinimized ? minimizedStyles(theme) : {}),
 }))
 
-export const StyledReportRunningIcon = styled.img(({ theme }) => ({
-  opacity: 0.4,
-  width: "1.6rem",
-  height: "1.6rem",
-  marginRight: `-${theme.spacing.sm}`,
-}))
+export const StyledReportRunningIcon = styled.img(({ theme }) => {
+  // Best way to check dark or light theme?
+  const darkMode = darkTheme.emotion.colors === theme.colors
+  return {
+    opacity: 0.4,
+    width: "1.6rem",
+    height: "1.6rem",
+    marginRight: `-${theme.spacing.sm}`,
+    filter: darkMode ? "invert(1)" : "invert(0)",
+  }
+})
 
 export const StyledStatusWidget = styled.div(({ theme }) => ({
   "&.StatusWidget-appear": {
