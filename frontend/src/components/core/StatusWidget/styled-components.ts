71be943092cfa185c630e50f8025cc81c697a084
mayagbarnes <maya8617@gmail.com>
2021-10-28 15:20:55 -0700

    Reverse change in styled components

--- frontend/src/components/core/StatusWidget/styled-components.ts
+++ frontend/src/components/core/StatusWidget/styled-components.ts
@@ -16,7 +16,7 @@
  */
 
 import styled, { CSSObject } from "@emotion/styled"
-import { darkTheme, Theme } from "src/theme"
+import { Theme } from "src/theme"
 
 /*
   "ConnectionStatus" styles are used for displaying
@@ -118,14 +118,12 @@ export const StyledReportButtonContainer = styled.span<
   ...(isMinimized ? minimizedStyles(theme) : {}),
 }))
 
-export const StyledReportRunningIcon = styled.img(({ theme }) => {
-  return {
-    opacity: 0.4,
-    width: "1.6rem",
-    height: "1.6rem",
-    marginRight: `-${theme.spacing.sm}`,
-  }
-})
+export const StyledReportRunningIcon = styled.img(({ theme }) => ({
+  opacity: 0.4,
+  width: "1.6rem",
+  height: "1.6rem",
+  marginRight: `-${theme.spacing.sm}`,
+}))
 
 export const StyledStatusWidget = styled.div(({ theme }) => ({
   "&.StatusWidget-appear": {
