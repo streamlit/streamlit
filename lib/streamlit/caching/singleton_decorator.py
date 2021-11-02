4da9f8a41004d2dad8a20e435fb87c28639ada00
Tim Conkling <tconkling@gmail.com>
2021-11-02 15:00:55 -0700

    SingletonStatsProviderTest

--- lib/streamlit/caching/singleton_decorator.py
+++ lib/streamlit/caching/singleton_decorator.py
@@ -233,7 +233,7 @@ class SingletonCache(Cache):
         for item_key, item_value in mem_cache.items():
             stats.append(
                 CacheStat(
-                    category_name="st.singleton",
+                    category_name="st_singleton",
                     cache_name=self.display_name,
                     byte_length=asizeof.asizeof(item_value),
                 )
