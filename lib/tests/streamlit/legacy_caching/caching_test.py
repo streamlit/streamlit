9a71b9165eda2a132503f1924559f71f33ae5bd7
Amanda Walker <amanda@amandawalker.io>
2021-11-02 13:06:23 -0700

    Add length and entry checking for cache stats test

--- lib/tests/streamlit/legacy_caching/caching_test.py
+++ lib/tests/streamlit/legacy_caching/caching_test.py
@@ -652,6 +652,7 @@ def test_cache_stats_provider():
 
     init_size = sum(stat.byte_length for stat in caches.get_stats())
     assert init_size == 0
+    assert len(caches.get_stats()) == 0
 
     @st.cache
     def foo():
@@ -660,15 +661,22 @@ def test_cache_stats_provider():
     foo()
     new_size = sum(stat.byte_length for stat in caches.get_stats())
     assert new_size > 0
+    assert len(caches.get_stats()) == 1
 
     foo()
     new_size_2 = sum(stat.byte_length for stat in caches.get_stats())
     assert new_size_2 == new_size
 
     @st.cache
-    def bar():
+    def bar(i):
         return 0
 
-    bar()
+    bar(0)
     new_size_3 = sum(stat.byte_length for stat in caches.get_stats())
     assert new_size_3 > new_size_2
+    assert len(caches.get_stats()) == 2
+
+    bar(1)
+    new_size_4 = sum(stat.byte_length for stat in caches.get_stats())
+    assert new_size_4 > new_size_3
+    assert len(caches.get_stats()) == 3
