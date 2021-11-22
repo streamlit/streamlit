9d88721e713ee3a72c81ae3503512b84a3f3d24c
Maya Barnes <63436329+mayagbarnes@users.noreply.github.com>
2021-11-22 10:37:11 -0800

    Fix cutoff/overlap of long values in st.slider (#4076)
    * Revise Slider.tsx to fix thumb value
    
    * Refactor helper & add/update e2e tests
    
    * Fix tests
    
    * Clean-up

--- e2e/specs/st_slider.spec.js
+++ e2e/specs/st_slider.spec.js
@@ -18,6 +18,8 @@
 describe("st.slider", () => {
   beforeEach(() => {
     cy.visit("http://localhost:3000/");
+    // Open sidebar expander
+    cy.get(".streamlit-expanderHeader").click();
   });
 
   it("looks right", () => {
@@ -25,14 +27,16 @@ describe("st.slider", () => {
     cy.get("[data-testid='stDecoration']").invoke("css", "display", "none");
 
     cy.get(".stSlider")
-      .first()
+      .eq(2)
       .matchThemedSnapshots("slider");
   });
 
   it("shows labels", () => {
     cy.get(".stSlider label").should(
       "have.text",
-      "Label 1" +
+      "Label A" +
+        "Label B" +
+        "Label 1" +
         "Label 2" +
         "Label 3 - This is a very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very long label" +
         "Label 4"
@@ -41,14 +45,28 @@ describe("st.slider", () => {
 
   it("shows full label when the label is long", () => {
     cy.get(".stSlider")
-      .eq(2)
+      .eq(4)
       .matchThemedSnapshots("slider_with_long_label");
+  });
+
+  it("shows full thumb value when the value is long", () => {
+    cy.get(".stSlider")
+      .eq(0)
+      .matchThemedSnapshots("long_thumb_value");
+  });
+
+  it("does not overlap expander container when thumb value is long", () => {
+    cy.get(".stSlider")
+      .eq(1)
+      .matchThemedSnapshots("expander_thumb_value");
   });
 
   it("has correct values", () => {
     cy.get(".stMarkdown").should(
       "have.text",
-      "Value 1: 25" +
+      "Value A: 12345678" +
+        "Value B: 10000" +
+        "Value 1: 25" +
         "Value 2: (25.0, 75.0)" +
         "Value 3: 1" +
         "Value 4: 25" +
@@ -62,40 +80,40 @@ describe("st.slider", () => {
 
     // trigger click in the center of the slider
     cy.get('.stSlider [role="slider"]')
-      .first()
+      .eq(2)
       .parent()
       .click();
 
     cy.get(".stMarkdown")
-      .first()
+      .eq(2)
       .should("have.text", "Value 1: 50");
   });
 
   it("increments the value on right arrow key press", () => {
     cy.get('.stSlider [role="slider"]')
-      .first()
+      .eq(2)
       .click()
       .type("{rightarrow}", { force: true });
 
     cy.get(".stMarkdown")
-      .first()
+      .eq(2)
       .should("have.text", "Value 1: 26");
   });
 
   it("decrements the value on left arrow key press", () => {
     cy.get('.stSlider [role="slider"]')
-      .first()
+      .eq(2)
       .click()
       .type("{leftarrow}", { force: true });
 
     cy.get(".stMarkdown")
-      .first()
+      .eq(2)
       .should("have.text", "Value 1: 24");
   });
 
   it("maintains its state on rerun", () => {
     cy.get('.stSlider [role="slider"]')
-      .first()
+      .eq(2)
       .click()
       .type("{leftarrow}", { force: true });
 
@@ -106,7 +124,7 @@ describe("st.slider", () => {
     });
 
     cy.get(".stMarkdown")
-      .first()
+      .eq(2)
       .should("have.text", "Value 1: 24");
   });
 
