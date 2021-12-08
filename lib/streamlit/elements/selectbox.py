c3229ade7457eaaf265cfc7ca275f8e318b32233
Maya Barnes <63436329+mayagbarnes@users.noreply.github.com>
2021-12-07 17:15:07 -0800

    Implement & test disabled - selectbox widget

--- lib/streamlit/elements/selectbox.py
+++ lib/streamlit/elements/selectbox.py
@@ -42,6 +42,8 @@ class SelectboxMixin:
         on_change: Optional[WidgetCallback] = None,
         args: Optional[WidgetArgs] = None,
         kwargs: Optional[WidgetKwargs] = None,
+        *,  # keyword-only arguments:
+        disabled: bool = False,
     ) -> Any:
         """Display a select widget.
 
@@ -70,6 +72,9 @@ class SelectboxMixin:
             An optional tuple of args to pass to the callback.
         kwargs : dict
             An optional dict of kwargs to pass to the callback.
+        disabled : bool
+            An optional boolean, which disables the selectbox if set to True.
+            The default is False. This is a keyword only argument.
 
         Returns
         -------
@@ -106,6 +111,7 @@ class SelectboxMixin:
         selectbox_proto.default = index
         selectbox_proto.options[:] = [str(format_func(option)) for option in opt]
         selectbox_proto.form_id = current_form_id(self.dg)
+        selectbox_proto.disabled = disabled
         if help is not None:
             selectbox_proto.help = dedent(help)
 
