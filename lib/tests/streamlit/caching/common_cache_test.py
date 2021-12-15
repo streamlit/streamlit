e47b734e51b3d37a93eb4fc3675e90916492a296
Tim Conkling <tconkling@gmail.com>
2021-12-15 14:17:20 -0800

    test_clear_single_cache

--- lib/tests/streamlit/caching/common_cache_test.py
+++ lib/tests/streamlit/caching/common_cache_test.py
@@ -302,3 +302,33 @@ class CommonCacheTest(unittest.TestCase):
         bar(0), bar(1), bar(2)
         self.assertEqual([0, 1, 2, 0, 1, 2], foo_vals)
         self.assertEqual([0, 1, 2, 0, 1, 2], bar_vals)
+
+    @parameterized.expand([("memo", memo), ("singleton", singleton)])
+    def test_clear_single_cache(self, _, cache_decorator):
+        foo_call_count = [0]
+
+        @cache_decorator
+        def foo():
+            foo_call_count[0] += 1
+
+        bar_call_count = [0]
+
+        @cache_decorator
+        def bar():
+            bar_call_count[0] += 1
+
+        foo(), foo(), foo()
+        bar(), bar(), bar()
+        self.assertEqual(1, foo_call_count[0])
+        self.assertEqual(1, bar_call_count[0])
+
+        # Clear just foo's cache, and call the functions again.
+        foo.clear_cache()
+
+        foo(), foo(), foo()
+        bar(), bar(), bar()
+
+        # Foo will have been called a second time, and bar will still
+        # have been called just once.
+        self.assertEqual(2, foo_call_count[0])
+        self.assertEqual(1, bar_call_count[0])
