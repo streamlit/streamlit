a404398c04b07e4822208784300e4bce5719107b
Ken McGrady <ken@streamlit.io>
2021-12-27 15:02:24 -0800

    Allow columns to be rendered to create spacing (#4217)

--- e2e/scripts/st_columns_layout.py
+++ e2e/scripts/st_columns_layout.py
@@ -19,3 +19,9 @@ c1, c2, c3 = st.columns(3)
 c1.write("Foo")
 c2.write("Bar")
 c3.write("Baz")
+
+c1, c2, c3 = st.columns(3)
+
+# We use longer text here because movement should
+# be considered a large change in the screenshot comparison
+c3.write("Some long text to write")
--- frontend/src/components/core/Block/Block.tsx
+++ frontend/src/components/core/Block/Block.tsx
@@ -53,7 +53,8 @@ interface BlockPropsWithWidth extends BaseBlockProps {
 const BlockNodeRenderer = (props: BlockPropsWithWidth): ReactElement => {
   const { node } = props
 
-  if (node.isEmpty) {
+  // Allow columns to create the specified space regardless of empty state
+  if (node.isEmpty && !node.deltaBlock.column) {
     return <></>
   }
 
