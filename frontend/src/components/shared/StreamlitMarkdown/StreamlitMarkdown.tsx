bbda8fd30cd3d26d01ec1651263b7aa8f2078505
Ken McGrady <ken@streamlit.io>
2021-12-27 19:14:27 -0800

    Upgrade react-markdown to version 7
    This resolves some issues with markdown. The upgrades introduced
    some breaking changes, so the changes have been made

--- frontend/src/components/shared/StreamlitMarkdown/StreamlitMarkdown.tsx
+++ frontend/src/components/shared/StreamlitMarkdown/StreamlitMarkdown.tsx
@@ -18,24 +18,23 @@
 import React, {
   ReactElement,
   ReactNode,
-  Fragment,
   PureComponent,
   CSSProperties,
   HTMLProps,
+  FunctionComponent,
 } from "react"
-import ReactMarkdown from "react-markdown"
+import ReactMarkdown, { Components } from "react-markdown"
+import { PluggableList } from "react-markdown/lib/react-markdown"
+import { ReactMarkdownProps } from "react-markdown/lib/complex-types"
 import { once } from "lodash"
-// @ts-ignore
-import htmlParser from "react-markdown/plugins/html-parser"
-// @ts-ignore
-import Tex from "@matejmazur/react-katex"
-// @ts-ignore
-import RemarkMathPlugin from "remark-math"
+import remarkMathPlugin from "remark-math"
+import rehypeRaw from "rehype-raw"
+import rehypeKatex from "rehype-katex"
 import { Link as LinkIcon } from "react-feather"
-// @ts-ignore
-import RemarkEmoji from "remark-emoji"
+import remarkEmoji from "remark-emoji"
+import remarkGfm from "remark-gfm"
 import PageLayoutContext from "src/components/core/PageLayoutContext"
-import CodeBlock from "src/components/elements/CodeBlock/"
+import CodeBlock, { CodeTag } from "src/components/elements/CodeBlock/"
 import IsSidebarContext from "src/components/core/Sidebar/IsSidebarContext"
 import {
   StyledStreamlitMarkdown,
@@ -82,32 +81,16 @@ const scrollNodeIntoView = once((node: HTMLElement): void => {
 interface HeadingWithAnchorProps {
   tag: string
   anchor?: string
-  children: [ReactElement]
+  children: ReactNode[]
   tagProps?: HTMLProps<HTMLHeadingElement>
 }
 
-interface CustomHeadingProps {
-  level: string | number
-  children: [ReactElement]
-}
-
-interface CustomParsedHtmlProps {
-  type: ReactElement
-  element: {
-    type: string
-    props: {
-      "data-anchor": string
-      children: [ReactElement]
-    }
-  }
-}
-
-export function HeadingWithAnchor({
+export const HeadingWithAnchor: FunctionComponent<HeadingWithAnchorProps> = ({
   tag,
   anchor: propsAnchor,
   children,
   tagProps,
-}: HeadingWithAnchorProps): ReactElement {
+}) => {
   const isSidebar = React.useContext(IsSidebarContext)
   const [elementId, setElementId] = React.useState(propsAnchor)
   const [target, setTarget] = React.useState<HTMLElement | null>(null)
@@ -166,27 +149,17 @@ export function HeadingWithAnchor({
   )
 }
 
-function CustomHeading({ level, children }: CustomHeadingProps): ReactElement {
-  return <HeadingWithAnchor tag={`h${level}`}>{children}</HeadingWithAnchor>
-}
+type HeadingProps = JSX.IntrinsicElements["h1"] &
+  ReactMarkdownProps & { level: number; "data-anchor"?: string }
 
-function CustomParsedHtml(props: CustomParsedHtmlProps): ReactElement {
-  const {
-    element: { type, props: elementProps },
-  } = props
-
-  const isSidebar = React.useContext(IsSidebarContext)
-
-  const headingElements = ["h1", "h2", "h3", "h4", "h5", "h6"]
-  if (isSidebar || !headingElements.includes(type)) {
-    // casting to any because ReactMarkdown's types are funky
-    // but this just means "call the original renderer provided by ReactMarkdown"
-    return (ReactMarkdown.renderers.parsedHtml as any)(props)
-  }
-
-  const { "data-anchor": anchor, children, ...rest } = elementProps
+export const CustomHeading: FunctionComponent<HeadingProps> = ({
+  node,
+  children,
+  ...rest
+}) => {
+  const anchor = rest["data-anchor"]
   return (
-    <HeadingWithAnchor tag={type} anchor={anchor} tagProps={rest}>
+    <HeadingWithAnchor tag={node.tagName} anchor={anchor}>
       {children}
     </HeadingWithAnchor>
   )
@@ -210,31 +183,32 @@ class StreamlitMarkdown extends PureComponent<Props> {
   public render = (): ReactNode => {
     const { source, allowHTML, style, isCaption } = this.props
 
-    const renderers = {
-      code: CodeBlock,
-      link: linkWithTargetBlank,
-      linkReference: linkReferenceHasParens,
-      inlineMath: (props: { value: string }): ReactElement => (
-        <Tex>{props.value}</Tex>
-      ),
-      math: (props: { value: string }): ReactElement => (
-        <Tex block>{props.value}</Tex>
-      ),
-      heading: CustomHeading,
-      parsedHtml: CustomParsedHtml,
+    const renderers: Components = {
+      pre: CodeBlock,
+      code: CodeTag,
+      a: LinkWithTargetBlank,
+      h1: CustomHeading,
+      h2: CustomHeading,
+      h3: CustomHeading,
+      h4: CustomHeading,
+      h5: CustomHeading,
+      h6: CustomHeading,
     }
 
-    const plugins = [RemarkMathPlugin, RemarkEmoji]
-    const astPlugins = allowHTML ? [htmlParser()] : []
+    const plugins = [remarkMathPlugin, remarkEmoji, remarkGfm]
+    const rehypePlugins: PluggableList = [rehypeKatex]
+    if (allowHTML) {
+      rehypePlugins.push(rehypeRaw)
+    }
 
     const renderMarkdown = (): ReactElement => (
       <ReactMarkdown
-        source={source}
-        escapeHtml={!allowHTML}
-        astPlugins={astPlugins}
-        plugins={plugins}
-        renderers={renderers}
-      />
+        remarkPlugins={plugins}
+        rehypePlugins={rehypePlugins}
+        components={renderers}
+      >
+        {source}
+      </ReactMarkdown>
     )
 
     return (
@@ -246,47 +220,23 @@ class StreamlitMarkdown extends PureComponent<Props> {
 }
 
 interface LinkProps {
-  children: ReactElement
-  href: string
-  title?: string
-}
-
-interface LinkReferenceProps {
-  children: [ReactElement]
-  href: string
+  node: any
+  children: ReactNode[]
+  href?: string
   title?: string
 }
 
 // Using target="_blank" without rel="noopener noreferrer" is a security risk:
 // see https://mathiasbynens.github.io/rel-noopener
-export function linkWithTargetBlank(props: LinkProps): ReactElement {
+export function LinkWithTargetBlank(props: LinkProps): ReactElement {
   // if it's a #hash link, don't open in new tab
-  if (props.href.startsWith("#")) {
-    const { children, ...rest } = props
+  const { href } = props
+  if (href && href.startsWith("#")) {
+    const { children, node, ...rest } = props
     return <a {...rest}>{children}</a>
   }
 
-  const { href, title, children } = props
-  return (
-    <a href={href} title={title} target="_blank" rel="noopener noreferrer">
-      {children}
-    </a>
-  )
-}
-
-// Handle rendering a link through a reference, ex [text](href)
-// Don't convert to a link if only `[text]` and missing `(href)`
-export function linkReferenceHasParens(
-  props: LinkReferenceProps
-): ReactElement | null {
-  const { href, title, children } = props
-
-  if (!href) {
-    return children.length ? (
-      <Fragment>[{children[0].props.children}]</Fragment>
-    ) : null
-  }
-
+  const { title, children } = props
   return (
     <a href={href} title={title} target="_blank" rel="noopener noreferrer">
       {children}
