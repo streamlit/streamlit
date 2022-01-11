dfc3686ad82197e2224af8074224601d8655ea88
Amanda Walker <amanda@amandawalker.io>
2022-01-11 11:41:46 -0800

    Remove context fallback in register_widget

--- lib/streamlit/state/widgets.py
+++ lib/streamlit/state/widgets.py
@@ -20,7 +20,7 @@ from typing import Any, Callable, cast, Dict, Optional, Set, Tuple, Union
 
 import attr
 
-from streamlit.script_run_context import get_script_run_ctx, ScriptRunContext
+from streamlit.script_run_context import ScriptRunContext
 from streamlit import util
 from streamlit.errors import DuplicateWidgetID
 from streamlit.proto.Button_pb2 import Button
@@ -137,8 +137,6 @@ def register_widget(
     widget_id = _get_widget_id(element_type, element_proto, user_key)
     element_proto.id = widget_id
 
-    if ctx is None:
-        ctx = get_script_run_ctx()
     if ctx is None:
         # Early-out if we're not running inside a ReportThread (which
         # probably means we're running as a "bare" Python script, and
