0dce3884971bc2cf4c8a06f85fdf79ae6119b818
Vincent Donato <vincent@streamlit.io>
2022-01-20 16:40:43 -0800

    Run help instead of version in the CLI smoke tests (to fix the nightly) (#4305)

--- lib/streamlit/cli.py
+++ lib/streamlit/cli.py
@@ -111,6 +111,10 @@ def help(ctx):
     # Pretend user typed 'streamlit --help' instead of 'streamlit help'.
     import sys
 
+    # We use _get_command_line_as_string to run some assertions but don't do
+    # anything with its return value.
+    _get_command_line_as_string()
+
     assert len(sys.argv) == 2  # This is always true, but let's assert anyway.
     sys.argv[1] = "--help"
     main(prog_name="streamlit")
