aaa123fce397317601a70815954f9d010d33abe8
kajarenc <kajarenc@gmail.com>
2022-01-25 01:01:38 +0400

    remove tests, check for test

--- e2e/scripts/st_file_uploader.py
+++ e2e/scripts/st_file_uploader.py
@@ -56,20 +56,3 @@ with st.form("foo"):
         st.text("No upload")
     else:
         st.text(form_file.read())
-
-if not st.session_state.get("counter"):
-    st.session_state.counter = 0
-
-
-def file_uploader_on_change():
-    st.session_state.counter += 1
-
-
-st.file_uploader(
-    "Drop a file:",
-    type=["txt"],
-    key="on_change_example",
-    on_change=file_uploader_on_change,
-)
-
-st.text(st.session_state.counter)
