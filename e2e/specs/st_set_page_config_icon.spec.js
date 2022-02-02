ebdbb0b3a7a5c178cdbfac29fd435d94be1307d9
William Huang <whuang@streamlit.io>
2022-02-02 15:54:28 -0800

    Addressing PR comments

--- e2e/specs/st_set_page_config_icon.spec.js
+++ e2e/specs/st_set_page_config_icon.spec.js
@@ -23,6 +23,9 @@ describe("st.set_page_config", () => {
   it("sets the page favicon with ico file", () => {
     cy.get("link[rel='shortcut icon']")
       .should("have.attr", "href")
-      .should("contain", "png");
+      .should(
+        "contain",
+        "92018b2805266c4cb9a98e90c849ce5b5e7ba6d1af423bd7b7c345da.png"
+      );
   });
 });
