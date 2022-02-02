a2d718d503b5dc0c4d863515d13ddb78bfb09719
William Huang <whuang@streamlit.io>
2022-02-02 12:14:33 -0800

    Reformat

--- e2e/scripts/st_set_page_config_icon.py
+++ e2e/scripts/st_set_page_config_icon.py
@@ -2,9 +2,11 @@ import streamlit as st
 
 import os, fnmatch
 
+
 def isAssetsFaviconPath(path):
     if "assets/favicon.ico" in path:
         return path
+
 
 def find(pattern, path):
     result = []
@@ -14,11 +16,14 @@ def find(pattern, path):
                 result.append(os.path.join(root, name))
     return result
 
-paths = find('favicon.ico', '.')
+
+paths = find("favicon.ico", ".")
 filtered_paths = list(filter(None, list(map(isAssetsFaviconPath, paths))))
 
 if len(filtered_paths) != 1:
-    print("Can't seem to find assets/favicon.ico in the directory you're in. Maybe go back to the Streamlit root directory and running 'streamlit run e2e/scripts/st_set_page_config_icon.py'")
+    print(
+        "Can't seem to find assets/favicon.ico in the directory you're in. Maybe go back to the Streamlit root directory and running 'streamlit run e2e/scripts/st_set_page_config_icon.py'"
+    )
 else:
     st.set_page_config(
         page_icon=filtered_paths[0],
