7fc96dd2ff9e87f7789e7685e51608cb95e8144f
Tim Conkling <tconkling@gmail.com>
2022-02-08 15:44:44 -0800

    Replace AppSessionTest.test_enqueue_without_tracer/test_enqueue_with_tracer with ScriptRunnerTest.test_enqueue

--- lib/tests/streamlit/scriptrunner/script_runner_test.py
+++ lib/tests/streamlit/scriptrunner/script_runner_test.py
@@ -17,8 +17,8 @@
 import os
 import sys
 import time
-from typing import List
-from unittest.mock import patch
+from typing import List, Any
+from unittest.mock import MagicMock, patch
 
 import pytest
 from parameterized import parameterized
@@ -37,6 +37,7 @@ from streamlit.script_runner import ScriptRunner, ScriptRunnerEvent
 from streamlit.state.session_state import SessionState
 from streamlit.uploaded_file_manager import UploadedFileManager
 from tests import testutil
+from testutil import patch_config_options
 
 text_utf = "complete! 👨‍🎤"
 text_no_encoding = text_utf
@@ -70,6 +71,50 @@ class ScriptRunnerTest(AsyncTestCase):
         self._assert_no_exceptions(scriptrunner)
         self._assert_events(scriptrunner, [ScriptRunnerEvent.SHUTDOWN])
         self._assert_text_deltas(scriptrunner, [])
+
+    @parameterized.expand(
+        [
+            ("installTracer=False", False),
+            ("installTracer=True", True),
+        ]
+    )
+    def test_enqueue(self, _, install_tracer):
+        """Make sure we try to handle execution control requests whenever
+        our _enqueue function is called, unless "runner.installTracer" is set.
+        """
+        with patch_config_options({"runner.installTracer": install_tracer}):
+            # Create a TestScriptRunner. We won't actually be starting its
+            # script thread - instead, we'll manually call _enqueue on it, and
+            # pretend we're in the script thread.
+            runner = TestScriptRunner("not_a_script.py")
+            runner._is_in_script_thread = MagicMock(return_value=True)
+
+            maybe_handle_execution_control_request_mock = MagicMock()
+            runner._maybe_handle_execution_control_request = (
+                maybe_handle_execution_control_request_mock
+            )
+
+            enqueue_forward_msg_mock = MagicMock()
+            runner._enqueue_forward_msg = enqueue_forward_msg_mock
+
+            # Enqueue a message on the runner
+            mock_msg = MagicMock()
+            runner._enqueue(mock_msg)
+
+            # The message should have been passed up to
+            enqueue_forward_msg_mock.assert_called_once_with(mock_msg)
+
+            # If "install_tracer" is true, maybe_handle_execution_control_request
+            # should not be called by the enqueue function. (In reality, it will
+            # still be called once in the tracing callback But in this test
+            # we're not actually installing a tracer - the script is not being
+            # run.) If "install_tracer" is false, the function should be called
+            # once.
+            expected_call_count = 0 if install_tracer else 1
+            self.assertEqual(
+                expected_call_count,
+                maybe_handle_execution_control_request_mock.call_count,
+            )
 
     @parameterized.expand(
         [
@@ -623,17 +668,13 @@ class TestScriptRunner(ScriptRunner):
         # DeltaGenerator deltas will be enqueued into self.forward_msg_queue.
         self.forward_msg_queue = ForwardMsgQueue()
 
-        def enqueue_fn(msg):
-            self.forward_msg_queue.enqueue(msg)
-            self._maybe_handle_execution_control_request()
-
         self.script_request_queue = ScriptRequestQueue()
         script_path = os.path.join(os.path.dirname(__file__), "test_data", script_name)
 
         super(TestScriptRunner, self).__init__(
             session_id="test session id",
             session_data=SessionData(script_path, "test command line"),
-            enqueue_forward_msg=enqueue_fn,
+            enqueue_forward_msg=self.forward_msg_queue.enqueue,
             client_state=ClientState(),
             session_state=SessionState(),
             request_queue=self.script_request_queue,
@@ -644,8 +685,8 @@ class TestScriptRunner(ScriptRunner):
         self.script_thread_exceptions = []
 
         # Accumulates all ScriptRunnerEvents emitted by us.
-        self.events = []
-        self.event_data = []
+        self.events: List[ScriptRunnerEvent] = []
+        self.event_data: List[Any] = []
 
         def record_event(event, **kwargs):
             self.events.append(event)
