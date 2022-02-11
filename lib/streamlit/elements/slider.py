1d16f6fc8358212d81f9b9cd8d4efe4350a4ad4a
Lukas Masuch <Lukas.Masuch@gmail.com>
2022-02-11 16:02:16 +0100

    Update slider.py

--- lib/streamlit/elements/slider.py
+++ lib/streamlit/elements/slider.py
@@ -421,7 +421,7 @@ class SliderMixin:
 
             def _micros_to_datetime(micros):
                 utc_dt = UTC_EPOCH + timedelta(microseconds=micros)
-                # Add the original timezone. No conversation is required here,
+                # Add the original timezone. No conversion is required here,
                 # since in the serialization, we also just replace the timestamp with UTC.
                 return utc_dt.replace(tzinfo=orig_tz)
 
