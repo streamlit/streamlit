afb33e3dac2a308c6ce26c7c04bd49eb80f0bb7c
Emily Morehouse <emily@cuttlesoft.com>
2022-02-15 11:24:29 -0700

    [STLT-17] 🔨 (Release Pipeline) Refactor regression test to add better comments, improve test organization, and improve error messages

--- scripts/cli_regression_tests.py
+++ scripts/cli_regression_tests.py
@@ -20,22 +20,30 @@ import time
 
 import pytest
 
-# Before running, ensure that you have:
-# - An isolated environment with Streamlit installed in production mode (not development) as well
-#   pytest. This can include the current version, nightly, or local build/wheel, like one of the
-#   following:
-#         pip install streamlit-nightly=[nightly tag]
-#         pip install lib/dist/<WHEEL_FILE>
-#         pip install streamlit
-# - The STREAMLIT_RELEASE_VERSION environment variable must be set, such as:
-#        export STREAMLIT_RELEASE_VERSION=1.5.1
-#
-# You can then run the tests from the root of the Streamlit repository using one of the following:
-#         pytest scripts/cli_regression_tests.py
-#         make cli-regression-tests
-
 
 class TestCLIRegressions:
+    """Suite of CLI regression tests to be run against a release build of the Streamlit library.
+
+    Before running, ensure that you have:
+        - An isolated environment with Streamlit installed in production mode (not development) as
+          well as pytest. This can include the current version, nightly, or local build/wheel, like
+          one of the following:
+                pip install streamlit-nightly=[nightly tag]
+                pip install lib/dist/<WHEEL_FILE>
+                pip install streamlit
+        - The STREAMLIT_RELEASE_VERSION environment variable must be set, such as:
+                export STREAMLIT_RELEASE_VERSION=1.5.1
+
+    You can then run the tests from the root of the Streamlit repository using one of the following:
+            pytest scripts/cli_regression_tests.py
+            make cli-regression-tests
+
+    This test suite makes use of Python's built-in assert statement. Note that assertions in the
+    form of `assert <expression>` use Pytest's assertion introspection. In some cases, a more clear
+    error message is specified manually by using `assert <expression>, <message>`. See
+    https://docs.pytest.org/en/7.0.x/how-to/assert.html#assert-details for more details.
+    """
+
     @pytest.fixture(scope="module", autouse=True)
     def setup(self):
         # ---- Initialization
@@ -73,24 +81,6 @@ class TestCLIRegressions:
     def run_command(self, command):
         return subprocess.check_output(self.parameterize(command)).decode("UTF-8")
 
-    def test_streamlit_version(self):
-        assert (
-            STREAMLIT_RELEASE_VERSION != None
-        ), "You must set the $STREAMLIT_RELEASE_VERSION env variable"
-        assert STREAMLIT_RELEASE_VERSION in self.run_command("streamlit version")
-
-    def test_streamlit_activate(self):
-        process = subprocess.Popen(
-            "streamlit activate", stdin=subprocess.PIPE, shell=True
-        )
-        process.stdin.write(b"regressiontest@streamlit.io\n")
-        process.stdin.flush()
-        process.communicate()
-
-        with open(CREDENTIALS_FILE_PATH) as f:
-            assert "regressiontest@streamlit.io" in f.read()
-
-    # ---------------------------------------------------------------------------------------------
     def run_single_proc(self, command, wait_in_seconds=2):
         proc = subprocess.Popen(
             command,
@@ -147,6 +137,27 @@ class TestCLIRegressions:
         except subprocess.TimeoutExpired:
             assert False, "Subprocess timed out"
 
+    def test_streamlit_version(self):
+        assert (
+            STREAMLIT_RELEASE_VERSION != None
+        ), "You must set the $STREAMLIT_RELEASE_VERSION env variable"
+        assert STREAMLIT_RELEASE_VERSION in self.run_command(
+            "streamlit version"
+        ), f"Package version does not match the desired version of {STREAMLIT_RELEASE_VERSION}"
+
+    def test_streamlit_activate(self):
+        process = subprocess.Popen(
+            "streamlit activate", stdin=subprocess.PIPE, shell=True
+        )
+        process.stdin.write(b"regressiontest@streamlit.io\n")
+        process.stdin.flush()
+        process.communicate()
+
+        with open(CREDENTIALS_FILE_PATH) as f:
+            assert (
+                "regressiontest@streamlit.io" in f.read()
+            ), "Email address was not found in the credentials file"
+
     def test_port_reassigned(self):
         """When starting a new Streamlit session, it will run on port 8501 by default. If 8501 is
         not available, it will use the next available port.
@@ -157,8 +168,8 @@ class TestCLIRegressions:
             f"streamlit run --server.headless=true {REPO_ROOT}/examples/file_uploader.py",
         )
 
-        assert ":8501" in out_one, out_one
-        assert ":8502" in out_two, out_two
+        assert ":8501" in out_one, f"Incorrect port. See output:\n{out_one}"
+        assert ":8502" in out_two, f"Incorrect port. See output:\n{out_two}"
 
     def test_conflicting_port(self):
         out_one, out_two = self.run_double_proc(
@@ -166,15 +177,17 @@ class TestCLIRegressions:
             f"streamlit run --server.headless=true --server.port=8501 {REPO_ROOT}/examples/file_uploader.py",
         )
 
-        assert ":8501" in out_one, out_one
-        assert "Port 8501 is already in use" in out_two, out_two
+        assert ":8501" in out_one, f"Incorrect port. See output:\n{out_one}"
+        assert (
+            "Port 8501 is already in use" in out_two
+        ), f"Incorrect conflict. See output:\n{out_one}"
 
     def test_cli_defined_port(self):
         out = self.run_single_proc(
             f"streamlit run --server.headless=true --server.port=9999 {REPO_ROOT}/examples/file_uploader.py",
         )
 
-        assert ":9999" in out, out
+        assert ":9999" in out, f"Incorrect port. See output:\n{out}"
 
     def test_config_toml_defined_port(self):
         with open(CONFIG_FILE_PATH, "w") as file:
@@ -184,4 +197,4 @@ class TestCLIRegressions:
             f"streamlit run --server.headless=true {REPO_ROOT}/examples/file_uploader.py",
         )
 
-        assert ":8888" in out, out
+        assert ":8888" in out, f"Incorrect port. See output:\n{out}"
