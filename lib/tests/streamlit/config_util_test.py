41c83dab3f6d43da776aafa26973431a15df2993
Vincent Donato <vincent@streamlit.io>
2022-02-28 14:49:26 -0800

    Hide `ui.hideTopBar` and the entire `ui` section (#4447)

--- lib/tests/streamlit/config_util_test.py
+++ lib/tests/streamlit/config_util_test.py
@@ -87,6 +87,20 @@ class ConfigUtilTest(unittest.TestCase):
         assert 'address = "example.com"' in lines
         assert "port = 8501" in lines
 
+    @patch("streamlit.config_util.click.echo")
+    def test_ui_section_hidden(self, patched_echo):
+        config_options = create_config_options({})
+
+        config_util.show_config(CONFIG_SECTION_DESCRIPTIONS, config_options)
+
+        [(args, _)] = patched_echo.call_args_list
+        # Remove the ascii escape sequences used to color terminal output.
+        output = re.compile(r"\x1b[^m]*m").sub("", args[0])
+        lines = set(output.split("\n"))
+
+        assert "[ui]" not in lines
+        assert "# hideTopBar = false" not in lines
+
     @parameterized.expand(
         [
             # Nothing changed.
