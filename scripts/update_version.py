335a5155e9ff821bd70c409be1c3dc6cbf1dc898
Emily Morehouse <emily@cuttlesoft.com>
2022-02-28 10:47:32 -0700

    [STLT-13] 🚑 (Scripts - Update Version) Allow PEP-440 version to be used as the version arg as well as semver (#4451)

--- scripts/update_version.py
+++ scripts/update_version.py
@@ -115,10 +115,19 @@ def main():
         raise (e)
 
     # We need two flavors of the version - one that's semver-compliant for Node, one that's
-    # PEP440-compliant for Python. We expect the incoming version to be semver-compliant; `verify_pep440`
-    # will convert it to PEP440-compliant.
+    # PEP440-compliant for Python. We allow for the incoming version to be either semver-compliant
+    # PEP440-compliant.
+    # - `verify_pep440` automatically converts semver to PEP440-compliant
     pep440_version = verify_pep440(sys.argv[1])
-    semver_version = verify_semver(sys.argv[1])
+
+    # - Attempt to convert to semver-compliant. If a failure occurs, manually attempt to convert.
+    semver_version = None
+    try:
+        semver_version = verify_semver(sys.argv[1])
+    except ValueError:
+        semver_version = verify_semver(
+            sys.argv[1].replace("rc", "-rc.").replace(".dev", "-dev")
+        )
 
     update_files(PYTHON, pep440_version)
     update_files(NODE, semver_version)
