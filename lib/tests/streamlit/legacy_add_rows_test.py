b8a0bdf42726f7db69d225fff6850f2441a7c624
Tim Conkling <tconkling@gmail.com>
2022-03-08 14:00:23 -0800

    test_legacy_add_rows_suceeds_when_wrong_shape

--- lib/tests/streamlit/legacy_add_rows_test.py
+++ lib/tests/streamlit/legacy_add_rows_test.py
@@ -273,17 +273,19 @@ class DeltaGeneratorAddRowsTest(testutil.DeltaGeneratorTestCase):
             get_script_run_ctx().reset()
             self.forward_msg_queue.clear()
 
-    def test_legacy_add_rows_fails_when_wrong_shape(self):
-        """Test that _legacy_add_rows raises error when input has wrong shape."""
+    def test_legacy_add_rows_suceeds_when_wrong_shape(self):
+        """_legacy_add_rows doesn't raise an error even if its input has the
+         wrong shape. Instead, it's up to the frontend to catch and raise
+         this error.
+         """
         all_methods = self._get_unnamed_data_methods() + self._get_named_data_methods()
 
         for method in all_methods:
             # Create a new data-carrying element (e.g. st._legacy_dataframe)
             el = method(DATAFRAME)
 
-            with self.assertRaises(ValueError):
-                # This is what we're testing:
-                el._legacy_add_rows(NEW_ROWS_WRONG_SHAPE)
+            # This is what we're testing:
+            el._legacy_add_rows(NEW_ROWS_WRONG_SHAPE)
 
             # Clear the queue so the next loop is like a brand new test.
             get_script_run_ctx().reset()
