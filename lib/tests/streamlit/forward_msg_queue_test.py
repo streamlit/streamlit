e42213be227fc18ef40dfa0f169c7c7086ee3faa
Tim Conkling <tconkling@gmail.com>
2022-03-08 13:34:04 -0800

    Fix test_multiple_containers

--- lib/tests/streamlit/forward_msg_queue_test.py
+++ lib/tests/streamlit/forward_msg_queue_test.py
@@ -276,7 +276,7 @@ class ForwardMsgQueueTest(unittest.TestCase):
 
         rq.enqueue(NEW_SESSION_MSG)
 
-        def enqueue_deltas(container: RootContainer, path: Tuple[int, ...]):
+        def enqueue_deltas(container: int, path: Tuple[int, ...]):
             # We deep-copy the protos because we mutate each one
             # multiple times.
             msg = copy.deepcopy(TEXT_DELTA_MSG1)
@@ -294,22 +294,33 @@ class ForwardMsgQueueTest(unittest.TestCase):
         enqueue_deltas(RootContainer.MAIN, ())
         enqueue_deltas(RootContainer.SIDEBAR, (0, 0, 1))
 
-        def assert_deltas(container: RootContainer, path: Tuple[int, ...], idx: int):
+        def assert_deltas(container: int, path: Tuple[int, ...], idx: int):
+            # Text delta
             self.assertEqual(
                 make_delta_path(container, path, 0), queue[idx].metadata.delta_path
             )
             self.assertEqual("text1", queue[idx].delta.new_element.text.body)
 
+            # Dataframe delta
             self.assertEqual(
                 make_delta_path(container, path, 1), queue[idx + 1].metadata.delta_path
             )
             col0 = queue[idx + 1].delta.new_element.data_frame.data.cols[0].int64s.data
             col1 = queue[idx + 1].delta.new_element.data_frame.data.cols[1].int64s.data
-            self.assertEqual([0, 1, 2, 3, 4, 5], col0)
-            self.assertEqual([10, 11, 12, 13, 14, 15], col1)
+            self.assertEqual([0, 1, 2], col0)
+            self.assertEqual([10, 11, 12], col1)
+
+            # add_rows delta
+            self.assertEqual(
+                make_delta_path(container, path, 1), queue[idx + 2].metadata.delta_path
+            )
+            ar_col0 = queue[idx + 2].delta.add_rows.data.data.cols[0].int64s.data
+            ar_col1 = queue[idx + 2].delta.add_rows.data.data.cols[1].int64s.data
+            self.assertEqual([3, 4, 5], ar_col0)
+            self.assertEqual([13, 14, 15], ar_col1)
 
         queue = rq.flush()
-        self.assertEqual(5, len(queue))
+        self.assertEqual(7, len(queue))
 
         assert_deltas(RootContainer.MAIN, (), 1)
-        assert_deltas(RootContainer.SIDEBAR, (0, 0, 1), 3)
+        assert_deltas(RootContainer.SIDEBAR, (0, 0, 1), 4)
