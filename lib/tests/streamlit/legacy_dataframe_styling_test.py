9c32a03afe0c3a4754e2d7469c4ed4b0f1112d2b
Tim Conkling <tconkling@gmail.com>
2022-03-08 16:00:45 -0800

    fix test_add_styled_rows

--- lib/tests/streamlit/legacy_dataframe_styling_test.py
+++ lib/tests/streamlit/legacy_dataframe_styling_test.py
@@ -132,23 +132,18 @@ class LegacyDataFrameStylingTest(testutil.DeltaGeneratorTestCase):
         [(st._legacy_dataframe, _get_df_proto), (st._legacy_table, _get_table_proto)]
     )
     def test_add_styled_rows(self, st_element, get_proto):
-        """Add rows should preserve existing styles and append new styles"""
+        """Add rows messages should include styling data if supplied."""
         df1 = pd.DataFrame([5, 6])
         df2 = pd.DataFrame([7, 8])
 
-        css_values = [
-            {css_s("color", "red")},
-            {css_s("color", "red")},
-            {css_s("color", "black")},
-            {css_s("color", "black")},
-        ]
-
         x = st_element(df1.style.applymap(lambda val: "color: red"))
+        proto_df = get_proto(self.get_delta_from_queue().new_element)
+        self._assert_column_css_styles(proto_df, 0, [{css_s("color", "red")}, {css_s("color", "red")}])
 
         x._legacy_add_rows(df2.style.applymap(lambda val: "color: black"))
 
-        proto_df = get_proto(self._get_element())
-        self._assert_column_css_styles(proto_df, 0, css_values)
+        proto_df = self.get_delta_from_queue().add_rows.data
+        self._assert_column_css_styles(proto_df, 0, [{css_s("color", "black")}, {css_s("color", "black")}])
 
     @parameterized.expand(
         [(st._legacy_dataframe, _get_df_proto), (st._legacy_table, _get_table_proto)]
