6828b58ec5c5585bdeb1fb7fd06caeadebc59c66
Vincent Donato <vincent@streamlit.io>
2022-03-17 23:35:26 -0700

    Add useIsOverflowing hook

---
+++ frontend/src/lib/Hooks.test.ts
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react"
+
+import { useIsOverflowing } from "./Hooks"
+
+const stateSetters = []
+
+jest.mock("react", () => ({
+  __esModule: true,
+  ...jest.requireActual("react"),
+  useEffect: jest.fn().mockImplementation(cb => cb()),
+  useState: jest.fn().mockImplementation(() => {
+    const setValue = jest.fn()
+    stateSetters.push(setValue)
+
+    return [false, setValue]
+  }),
+}))
+
+// NOTE: We can't test the return value of useIsOverflowing directly because
+// it won't have changed in a single run of the function. This is why we just
+// check that the state setter was called.
+describe("useIsOverflowing", () => {
+  it("sets state to true if the element is overflowing", () => {
+    const ref = { current: { scrollHeight: 1, clientHeight: 0 } }
+    useIsOverflowing(ref)
+
+    const setIsOverflowing = stateSetters.pop()
+    expect(setIsOverflowing).toHaveBeenCalledWith(true)
+  })
+
+  it("sets state to false if the element is not overflowing", () => {
+    const ref = { current: { scrollHeight: 1, clientHeight: 1 } }
+    useIsOverflowing(ref)
+
+    const setIsOverflowing = stateSetters.pop()
+    expect(setIsOverflowing).toHaveBeenCalledWith(false)
+  })
+})
