698f777ada37d4ec55a38b3d7cd19e1641623431
Lukas Masuch <Lukas.Masuch@gmail.com>
2022-03-20 18:59:34 +0100

    Add theming and styling configuration for interactive tables (#4524)

--- frontend/src/components/widgets/DataGrid/DataGrid.tsx
+++ frontend/src/components/widgets/DataGrid/DataGrid.tsx
@@ -23,6 +23,7 @@ import {
   GridCell,
   GridCellKind,
 } from "@glideapps/glide-data-grid"
+import { transparentize } from "color2k"
 
 import withFullScreenWrapper from "src/hocs/withFullScreenWrapper"
 import { Quiver } from "src/lib/Quiver"
@@ -43,19 +44,48 @@ function DataGrid({
 }: DataGridProps): ReactElement {
   const theme: Theme = useTheme()
 
+  const dataGridTheme = {
+    // Explanations: https://github.com/glideapps/glide-data-grid/blob/main/packages/core/API.md#theme
+    accentColor: theme.colors.primary,
+    accentFg: theme.colors.white, // TODO(lukasmasuch): Do we need a different color here?
+    accentLight: transparentize(theme.colors.primary, 0.9),
+    borderColor: theme.colors.fadedText05,
+    fontFamily: theme.fonts.sansSerif,
+    bgSearchResult: transparentize(theme.colors.primary, 0.9),
+    // Header styling:
+    bgIconHeader: theme.colors.fadedText60,
+    fgIconHeader: theme.colors.white,
+    bgHeader: theme.colors.bgMix,
+    bgHeaderHasFocus: theme.colors.secondaryBg,
+    bgHeaderHovered: theme.colors.bgMix, // no hovering color change
+    textHeader: theme.colors.fadedText60,
+    textHeaderSelected: theme.colors.white,
+    headerFontStyle: `bold ${theme.fontSizes.sm}`,
+    // Cell styling
+    baseFontStyle: theme.fontSizes.sm,
+    editorFontSize: theme.fontSizes.sm,
+    textDark: theme.colors.bodyText,
+    textMedium: theme.colors.fadedText60,
+    textLight: theme.colors.fadedText40,
+    textBubble: theme.colors.fadedText60,
+    bgCell: theme.colors.bgColor,
+    bgCellMedium: theme.colors.bgColor, // TODO(lukasmasuch): Do we need a different color here?
+    cellHorizontalPadding: 8,
+    cellVerticalPadding: 3,
+    // Special cells:
+    bgBubble: theme.colors.secondaryBg,
+    bgBubbleSelected: theme.colors.secondaryBg,
+    linkColor: theme.colors.linkText,
+    drilldownBorder: "rgba(0, 0, 0, 0)", // TODO(lukasmasuch): Do we need a different color here?
+  }
+
   // TODO(lukasmasuch): Apply automatic height calculation instead of the default value in later PRs
   const height = propHeight || 300
 
   return (
     // This is a styled-components theme provider (not emotion!).
     // It is required by glide-data-grid to customize the theming.
-    <ThemeProvider
-      theme={
-        {
-          /* TODO(lukasmasuch): Theme will be added in a later PR */
-        }
-      }
-    >
+    <ThemeProvider theme={dataGridTheme}>
       <DataGridContainer
         className="stDataGrid"
         width={width}
