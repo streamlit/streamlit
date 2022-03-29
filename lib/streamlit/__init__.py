403afa50b51fbcdf48aa36554f1156b14226a39b
Toubi <antoine@toubhans.org>
2022-03-30 00:43:12 +0200

    Fix code example in experimental_show docstring (#4563)

--- lib/streamlit/__init__.py
+++ lib/streamlit/__init__.py
@@ -261,7 +261,7 @@ def experimental_show(*args):
     >>> dataframe = pd.DataFrame({
     ...     'first column': [1, 2, 3, 4],
     ...     'second column': [10, 20, 30, 40],
-    ... }))
+    ... })
     >>> st.experimental_show(dataframe)
 
     Notes
