cf2af9c6119126939f07ff6ad1811acdcc5b32d5
Ken McGrady <ken.mcgrady@gmail.com>
2022-04-19 12:25:09 -0700

    Address PR comments

--- frontend/src/theme/emotion.d.ts
+++ frontend/src/theme/emotion.d.ts
@@ -16,7 +16,11 @@
  */
 import { Theme as StreamlitTheme } from "src/theme"
 
-// By default
+// Outside imports make declarations not ambient, so we separate out from
+// the ambient declarations.d.ts
+//
+// This declaration allows us to extend our type declarations for emotion's
+// theme (an empty object) to be our type
 declare module "@emotion/react" {
   export interface Theme extends StreamlitTheme {}
 }
