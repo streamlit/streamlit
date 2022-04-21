7ec27675c58e02406d223a8ac7cb6ea4de7b30e0
Amanda Walker <amanda@amandawalker.io>
2022-04-21 11:26:58 -0700

    Cache LocalSourcesWatcher modules (#4578)
    * Cache LocalSourcesWatcher modules
    
    The modules that have been imported by the python interpreter will
    usually not change between script runs, and the filesystem lookups to
    get the modules' paths are expensive, so we want to skip this when
    possible.
    
    We use the set of module names (the keys of sys.modules) to determine if
    we need to regenerate the paths. This could miss an already imported
    module being replaced by another version of itself with a different file
    path, but that's quite an edge case, and it would only miss out on us
    being able to see changes to the module's file to prompt the user to
    rerun the script, which seems fine.

--- lib/streamlit/watcher/local_sources_watcher.py
+++ lib/streamlit/watcher/local_sources_watcher.py
@@ -43,6 +43,7 @@ class LocalSourcesWatcher:
         self._session_data = session_data
         self._on_file_changed: List[Callable[[], None]] = []
         self._is_closed = False
+        self._cached_sys_modules: Set[str] = set()
 
         # Blacklist for folders that should not be watched
         self._folder_black_list = FolderBlackList(
@@ -134,12 +135,13 @@ class LocalSourcesWatcher:
         if self._is_closed:
             return
 
-        modules_paths = {
-            name: self._exclude_blacklisted_paths(get_module_paths(module))
-            for name, module in dict(sys.modules).items()
-        }
-
-        self._register_necessary_watchers(modules_paths)
+        if set(sys.modules) != self._cached_sys_modules:
+            modules_paths = {
+                name: self._exclude_blacklisted_paths(get_module_paths(module))
+                for name, module in dict(sys.modules).items()
+            }
+            self._cached_sys_modules = set(sys.modules)
+            self._register_necessary_watchers(modules_paths)
 
     def _register_necessary_watchers(self, module_paths: Dict[str, Set[str]]) -> None:
         for name, paths in module_paths.items():
