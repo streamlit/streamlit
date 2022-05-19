da5aba829dd9de97c00d91fdff434e59310ed3be
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2022-05-19 13:00:21 +0900

    Add @stlite/kernel to the frontend to replace ConnectionManager and FileUploadClient for stlite

--- frontend/app/src/App.tsx
+++ frontend/app/src/App.tsx
@@ -20,6 +20,8 @@ import { HotKeys, KeyMap } from "react-hotkeys"
 import { enableAllPlugins as enableImmerPlugins } from "immer"
 import classNames from "classnames"
 
+import { ConnectionManager, StliteKernelContext, FileUploadClient } from "@stlite/kernel"
+
 // Other local imports.
 import { AppContext } from "@streamlit/app/src/components/AppContext"
 import AppView from "@streamlit/app/src/components/AppView"
@@ -33,7 +35,6 @@ import {
   DialogType,
   StreamlitDialog,
 } from "@streamlit/app/src/components/StreamlitDialog"
-import { ConnectionManager } from "@streamlit/app/src/connection/ConnectionManager"
 import { ConnectionState } from "@streamlit/app/src/connection/ConnectionState"
 import { SessionEventDispatcher } from "@streamlit/app/src/SessionEventDispatcher"
 import {
@@ -57,7 +58,6 @@ import {
   BaseUriParts,
   RERUN_PROMPT_MODAL_DIALOG,
   SessionInfo,
-  FileUploadClient,
   logError,
   logMessage,
   AppRoot,
@@ -227,6 +227,8 @@ export class App extends PureComponent<Props, State> {
 
   private readonly embeddingId: string = generateUID()
 
+  static contextType = StliteKernelContext
+
   public constructor(props: Props) {
     super(props)
 
@@ -371,6 +373,7 @@ export class App extends PureComponent<Props, State> {
     // Initialize connection manager here, to avoid
     // "Can't call setState on a component that is not yet mounted." error.
     this.connectionManager = new ConnectionManager({
+      kernel: this.context.kernel,
       sessionInfo: this.sessionInfo,
       endpoints: this.endpoints,
       onMessage: this.handleMessage,
@@ -380,6 +383,7 @@ export class App extends PureComponent<Props, State> {
       resetHostAuthToken: this.hostCommunicationMgr.resetAuthToken,
       setAllowedOriginsResp: this.hostCommunicationMgr.setAllowedOriginsResp,
     })
+    this.uploadClient.setKernel(this.context.kernel)
 
     if (isScrollingHidden()) {
       document.body.classList.add("embedded")
