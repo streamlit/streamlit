660f591e1e75a50afad56683bef8864a842a9917
Karen Javadyan <kajarenc@gmail.com>
2022-05-25 20:07:09 +0400

    Fix typos in tests

--- lib/tests/streamlit/radio_test.py
+++ lib/tests/streamlit/radio_test.py
@@ -53,10 +53,10 @@ class RadioTest(testutil.DeltaGeneratorTestCase):
 
     def test_horizontal_default_value(self):
         """Test that it can called with horizontal param value False by default."""
-        st.radio("the label", ("m", "f"), horizontal=True)
+        st.radio("the label", ("m", "f"))
 
         c = self.get_delta_from_queue().new_element.radio
-        self.assertEqual(c.horizontal, True)
+        self.assertEqual(c.horizontal, False)
 
     def test_valid_value(self):
         """Test that valid value is an int."""
