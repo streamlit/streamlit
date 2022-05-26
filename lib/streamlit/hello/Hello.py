3aaf792aba559bd7f7ee505eed0b4c248ef1e8b4
Vincent Donato <vincent@streamlit.io>
2022-05-25 17:33:02 -0700

    Move call to set_page_config into `run` function

--- lib/streamlit/hello/Hello.py
+++ lib/streamlit/hello/Hello.py
@@ -15,15 +15,14 @@
 import streamlit as st
 from streamlit.logger import get_logger
 
-st.set_page_config(
-    page_title="Hello",
-    page_icon="👋",
-)
-
 LOGGER = get_logger(__name__)
 
 
 def run():
+    st.set_page_config(
+        page_title="Hello",
+        page_icon="👋",
+    )
 
     st.write("# Welcome to Streamlit! 👋")
 
