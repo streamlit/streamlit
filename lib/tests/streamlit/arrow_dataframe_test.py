eff78d7efb9627bef7bef11639916a62a50a098a
lukasmasuch <lukas.masuch@gmail.com>
2022-05-26 17:34:06 -0700

    Further cleanup

--- lib/tests/streamlit/arrow_dataframe_test.py
+++ lib/tests/streamlit/arrow_dataframe_test.py
@@ -44,40 +44,6 @@ def mock_data_frame():
     )
 
 
-class ArrowDataFrameDimensionsTest(testutil.DeltaGeneratorTestCase):
-    """Test the interactive table component."""
-
-    def test_no_dimensions(self):
-        """When no dimension parameters are passed"""
-        self._do_test(lambda fn, df: fn(df), 0, 0)
-
-    def test_with_dimensions(self):
-        """When dimension parameter are passed"""
-        self._do_test(lambda fn, df: fn(df, 10, 20), 10, 20)
-
-    def test_with_height_only(self):
-        """When only height parameter is passed"""
-        self._do_test(lambda fn, df: fn(df, height=20), 0, 20)
-
-    def test_with_width_only(self):
-        """When only width parameter is passed"""
-        self._do_test(lambda fn, df: fn(df, width=20), 20, 0)
-
-    def _do_test(self, fn, expectedWidth, expectedHeight):
-        df = pd.DataFrame({"A": [1, 2, 3, 4, 5]})
-
-        fn(st._arrow_dataframe, df)
-        metadata = self._get_metadata()
-        self.assertEqual(metadata.element_dimension_spec.width, expectedWidth)
-        self.assertEqual(metadata.element_dimension_spec.height, expectedHeight)
-
-    def _get_metadata(self):
-        """Returns the metadata for the most recent element in the
-        DeltaGenerator queue
-        """
-        return self.forward_msg_queue._queue[-1].metadata
-
-
 class ArrowDataFrameProtoTest(testutil.DeltaGeneratorTestCase):
     """Test ability to marshall arrow protos."""
 
