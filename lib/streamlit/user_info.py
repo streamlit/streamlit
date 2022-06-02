af2381a4b4d1aaa46dbba155c8ba616709debfef
Harald Husum <harald.husum@intelecy.com>
2022-06-02 12:12:49 +0200

    Fix typing issues

--- lib/streamlit/user_info.py
+++ lib/streamlit/user_info.py
@@ -5,26 +5,27 @@ from streamlit.scriptrunner import get_script_run_ctx as _get_script_run_ctx
 from streamlit.scriptrunner.script_run_context import UserInfo
 
 
+def get_user_info() -> UserInfo:
+    ctx = _get_script_run_ctx()
+    if ctx is None:
+        # TODO: Add appropriate warnings when ctx is missing
+        return {}
+    return ctx.user_info
+
+
 class UserInfoProxy(Mapping[str, Optional[str]]):
     """
     A dict like proxy object for accessing information about current user.
     """
 
     def __getitem__(self, key: str) -> Optional[str]:
-        ctx = _get_script_run_ctx()
-        if ctx is not None:
-            user_info = ctx.user_info
-            return user_info[key]
+        return get_user_info()[key]
 
     def __getattr__(self, key: str) -> Optional[str]:
-        ctx = _get_script_run_ctx()
-        if ctx is not None:
-            user_info = ctx.user_info
-
-            try:
-                return user_info[key]
-            except KeyError:
-                raise AttributeError
+        try:
+            return get_user_info()[key]
+        except KeyError:
+            raise AttributeError
 
     def __setattr__(self, name: str, value: Optional[str]) -> NoReturn:
         raise StreamlitAPIException("st.experimental_user cannot be modified")
@@ -33,17 +34,10 @@ class UserInfoProxy(Mapping[str, Optional[str]]):
         raise StreamlitAPIException("st.experimental_user cannot be modified")
 
     def __iter__(self) -> Iterator[str]:
-        ctx = _get_script_run_ctx()
-        if ctx is not None:
-            return iter(ctx.user_info)
+        return iter(get_user_info())
 
     def __len__(self) -> int:
-        ctx = _get_script_run_ctx()
-        if ctx is not None:
-            return len(ctx.user_info)
-        return 0
+        return len(get_user_info())
 
     def to_dict(self) -> UserInfo:
-        ctx = _get_script_run_ctx()
-        if ctx is not None:
-            return ctx.user_info
+        return get_user_info()
