3da0b0842fa9fc258a287593def4fb7a44f83ef5
Juan Garcia <jugarcia@c7rqqmwxhr.home>
2022-06-09 14:47:01 -0300

    Improve disabled states for Multiselect

--- frontend/src/theme/utils.ts
+++ frontend/src/theme/utils.ts
@@ -190,6 +190,8 @@ export const createThemeOverrides = (theme: Theme): Record<string, any> => {
       borderOpaque: colors.darkenedBgMix25,
       accent: transparentize(colors.primary, 0.5),
       tagPrimarySolidBackground: colors.primary,
+      tagPrimaryFontDisabled: colors.fadedText10,
+      tagPrimaryOutlinedDisabled: colors.transparent,
       borderFocus: colors.primary,
       contentPrimary: colors.bodyText,
       inputPlaceholder: colors.fadedText60,
