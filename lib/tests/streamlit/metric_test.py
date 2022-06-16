d02e0ac07db0ddebcf178dcd9534213f17f1e4ac
William Huang <willhuang1997@gmail.com>
2022-06-16 14:53:15 -0700

    Fix tests

--- lib/tests/streamlit/metric_test.py
+++ lib/tests/streamlit/metric_test.py
@@ -189,6 +189,6 @@ class MetricTest(testutil.DeltaGeneratorTestCase):
         )
 
     def test_help(self):
-        st.metric("label_test", help="   help text")
+        st.metric("label_test", value="500", help="   help text")
         c = self.get_delta_from_queue().new_element.metric
         self.assertEqual(c.help, "help text")
