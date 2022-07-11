5947a504513cc2f48a6f926634bc683b7e7640d9
Tim Conkling <tconkling@gmail.com>
2022-07-11 16:05:29 -0400

    unused import

--- lib/tests/streamlit/web/bootstrap_test.py
+++ lib/tests/streamlit/web/bootstrap_test.py
@@ -24,7 +24,6 @@ from streamlit import config
 from streamlit.session_data import SessionData
 from streamlit.web import bootstrap
 from streamlit.web.bootstrap import NEW_VERSION_TEXT
-from streamlit.web.server.server import Server
 from tests import testutil
 from tests.isolated_asyncio_test_case import IsolatedAsyncioTestCase
 
