22ea8bb1f64819da2183cec5f1e9ec865b05737e
Tim Conkling <tconkling@gmail.com>
2022-07-11 12:31:02 -0400

    pyformat

--- lib/streamlit/components/v1/components.py
+++ lib/streamlit/components/v1/components.py
@@ -24,8 +24,7 @@ from streamlit import util
 from streamlit.elements.form import current_form_id
 from streamlit.errors import StreamlitAPIException
 from streamlit.logger import get_logger
-from streamlit.proto.Components_pb2 import SpecialArg, \
-    ArrowTable as ArrowTableProto
+from streamlit.proto.Components_pb2 import SpecialArg, ArrowTable as ArrowTableProto
 from streamlit.proto.Element_pb2 import Element
 from streamlit.scriptrunner import get_script_run_ctx
 from streamlit.state import NoValue, register_widget
--- lib/tests/streamlit/stats_test.py
+++ lib/tests/streamlit/stats_test.py
@@ -66,7 +66,13 @@ class StatsHandlerTest(tornado.testing.AsyncHTTPTestCase):
         mock_stats_manager = MagicMock()
         mock_stats_manager.get_stats = MagicMock(side_effect=lambda: self.mock_stats)
         return tornado.web.Application(
-            [(r"/st-metrics", StatsRequestHandler, dict(stats_manager=mock_stats_manager))]
+            [
+                (
+                    r"/st-metrics",
+                    StatsRequestHandler,
+                    dict(stats_manager=mock_stats_manager),
+                )
+            ]
         )
 
     def test_no_stats(self):
