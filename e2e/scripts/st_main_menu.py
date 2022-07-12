b5c28a64e1bd083378025a9b3f74b8c0081e34b1
Harald Husum <harald.husum@intelecy.com>
2022-07-13 01:15:25 +0200

    Type check e2e scripts (#4932)
    * Expand mypy scope
    
    * Remove unused imports
    
    * Add explanatory comment
    
    * Fix typo
    
    * Formatting

--- e2e/scripts/st_main_menu.py
+++ e2e/scripts/st_main_menu.py
@@ -17,5 +17,7 @@ import streamlit as st
 # Not possible to test the urls in the menu as they are hidden behind
 # the click handler of the button
 # https://github.com/cypress-io/cypress-example-recipes/blob/master/examples/testing-dom__tab-handling-links/cypress/integration/tab_handling_anchor_links_spec.js
-menu_items = {"about": "_*This can be markdown!*_"}
+from streamlit.commands.page_config import MenuItems
+
+menu_items: MenuItems = {"about": "_*This can be markdown!*_"}
 st.set_page_config(menu_items=menu_items)
