29a6e69684d7b58e7ea5c3936141357d37ac5ded
Amanda Walker <amanda@amandawalker.io>
2022-07-13 17:49:48 -0700

    Improve name and docs of DG id selector function

--- lib/streamlit/caching/cache_utils.py
+++ lib/streamlit/caching/cache_utils.py
@@ -384,7 +384,7 @@ class CacheMessagesCallStack(threading.local):
         executing cached functions, so they can be replayed any time the function's
         execution is skipped because they're in the cache.
         """
-        id_to_save = self.get_dg_to_save_as_invoked(invoked_dg_id, used_dg_id)
+        id_to_save = self.select_dg_to_save(invoked_dg_id, used_dg_id)
         for msgs in self._cached_message_stack:
             msgs.append(
                 ElementMsgData(delta_type, element_proto, id_to_save, returned_dg_id)
@@ -399,13 +399,22 @@ class CacheMessagesCallStack(threading.local):
         used_dg_id: str,
         returned_dg_id: str,
     ) -> None:
-        id_to_save = self.get_dg_to_save_as_invoked(invoked_dg_id, used_dg_id)
+        id_to_save = self.select_dg_to_save(invoked_dg_id, used_dg_id)
         for msgs in self._cached_message_stack:
             msgs.append(BlockMsgData(block_proto, id_to_save, returned_dg_id))
         for s in self._seen_dg_stack:
             s.add(returned_dg_id)
 
-    def get_dg_to_save_as_invoked(self, invoked_id: str, acting_on_id: str) -> str:
+    def select_dg_to_save(self, invoked_id: str, acting_on_id: str) -> str:
+        """Select the id of the DG that this message should be invoked on
+        during message replay.
+
+        See Note [DeltaGenerator method invocation]
+
+        invoked_id is the DG the st function was called on, usually `st._main`
+        acting_on_id is the DG the st function ultimately runs on, which may be different
+        if the invoked DG delegated to another one because it was in a `with` block.
+        """
         if len(self._seen_dg_stack) > 0 and acting_on_id in self._seen_dg_stack[-1]:
             return acting_on_id
         else:
