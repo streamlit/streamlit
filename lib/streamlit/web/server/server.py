f361caed96878ffa5ed913f20050ccefc14b72c7
Tim Conkling <tconkling@gmail.com>
2022-07-14 13:58:26 -0400

    actually let's not fiddle with this at all

--- lib/streamlit/web/server/server.py
+++ lib/streamlit/web/server/server.py
@@ -727,18 +727,23 @@ class _BrowserWebSocketHandler(WebSocketHandler, SessionClient):
 
     def open(self, *args, **kwargs) -> Optional[Awaitable[None]]:
         # Extract user info from the X-Streamlit-User header
-        email: Optional[str] = None
+        is_public_cloud_app = False
+
         try:
             header_content = self.request.headers["X-Streamlit-User"]
             payload = base64.b64decode(header_content)
             user_obj = json.loads(payload)
+            email = user_obj["email"]
             is_public_cloud_app = user_obj["isPublicCloudApp"]
-            if not is_public_cloud_app:
-                email = user_obj["email"]
         except (KeyError, binascii.Error, json.decoder.JSONDecodeError):
-            email = None
+            email = "test@localhost.com"
 
-        user_info = {"email": email}
+        user_info: Dict[str, Optional[str]] = dict()
+        if is_public_cloud_app:
+            user_info["email"] = None
+        else:
+            user_info["email"] = email
+
         self._session = self._server._create_app_session(self, user_info)
         return None
 
