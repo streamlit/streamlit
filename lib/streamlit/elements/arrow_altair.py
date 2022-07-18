b3bb7c10a2808dfd28d6ac69e09a8d5a2bcf1113
lukasmasuch <lukas.masuch@gmail.com>
2022-07-18 15:23:44 +0200

    Add comments for theme activation logic

--- lib/streamlit/elements/arrow_altair.py
+++ lib/streamlit/elements/arrow_altair.py
@@ -41,8 +41,10 @@ if TYPE_CHECKING:
 # Create and enable streamlit theme
 STREAMLIT_THEME = {"embedOptions": {"theme": "streamlit"}}
 
+# This allows to use alt.themes.enable("streamlit") to activate Streamlit theme.
 alt.themes.register("streamlit", lambda: {"usermeta": STREAMLIT_THEME})
-# We don't want to activate the streamlit theme for Altair as default for now:
+# We don't want to activate the Streamlit theme for all Altair as default for now.
+# However, the Streamlit theme will be activated as default for our built-in charts.
 alt.themes.enable("none")
 
 
@@ -328,6 +330,7 @@ def _generate_chart(
 
     chart = (
         getattr(
+            # Built-in charts use the streamlit theme as default. So, we set this explicitly here.
             alt.Chart(data, width=width, height=height, usermeta=STREAMLIT_THEME),
             "mark_" + chart_type.value,
         )()
