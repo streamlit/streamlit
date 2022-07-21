cc6114403f802249a102eb12baf8be2cf58a2cd5
Tim Conkling <tconkling@gmail.com>
2022-07-21 15:58:00 -0700

    Another import fix

--- lib/streamlit/web/server/session_client.py
+++ lib/streamlit/web/server/session_client.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from typing import Protocol
+from typing_extensions import Protocol
 
 from streamlit.proto.ForwardMsg_pb2 import ForwardMsg
 
