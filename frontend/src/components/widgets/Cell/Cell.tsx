d4b4010e0f42ed475d62cff3b9826022ba166767
Thiago Teixeira <thiago@streamlit.io>
2022-07-22 23:52:16 -0700

    Improve CM6 theme (still kinda bad!).

--- frontend/src/components/widgets/Cell/Cell.tsx
+++ frontend/src/components/widgets/Cell/Cell.tsx
@@ -17,6 +17,8 @@
 
 import React, { useContext, useCallback, useRef } from "react"
 import CodeMirror, { ReactCodeMirrorRef } from "@uiw/react-codemirror"
+import { createTheme } from "@uiw/codemirror-themes"
+import { tags as t } from "@lezer/highlight"
 import { ViewUpdate } from "@codemirror/view"
 import { defaultKeymap } from "@codemirror/commands"
 import { python } from "@codemirror/lang-python"
@@ -30,6 +32,37 @@ import CallbacksContext from "src/components/core/CallbacksContext"
 import Icon from "src/components/shared/Icon"
 
 import { StyledCloseButton } from "./styled-components"
+
+const myTheme = createTheme({
+  theme: "light",
+  settings: {
+    background: "#fafafa",
+    foreground: "#31333f",
+    caret: "#262730",
+    selection: "#ffc7c7",
+    selectionMatch: "#e6eaf1",
+    lineHighlight: "#9af8ff",
+    gutterBackground: "#fafafa",
+    gutterForeground: "#808495",
+  },
+  styles: [
+    { tag: [t.comment, t.bracket], color: "#6a737d" },
+    { tag: [t.className, t.propertyName], color: "#6f42c1" },
+    {
+      tag: [t.variableName, t.attributeName, t.number, t.operator],
+      color: "#005cc5",
+    },
+    {
+      tag: [t.keyword, t.typeName, t.typeOperator, t.typeName],
+      color: "#d73a49",
+    },
+    { tag: [t.string, t.meta, t.regexp], color: "#032f62" },
+    { tag: [t.name, t.quote], color: "#22863a" },
+    { tag: [t.heading], color: "#24292e", fontWeight: "bold" },
+    { tag: [t.emphasis], color: "#24292e", fontStyle: "italic" },
+    { tag: [t.deleted], color: "#b31d28", backgroundColor: "ffeef0" },
+  ],
+})
 
 export interface Props {
   element: CellProto
@@ -148,7 +181,7 @@ function Cell({ element, cellIndex }: Props) {
         value={cellModel.body}
         extensions={[python(), keymap.of(KEYMAPS)]}
         placeholder="# Type Python here!"
-        theme={undefined /*dracula*/}
+        theme={myTheme /*dracula*/}
         basicSetup={{
           foldGutter: false,
           highlightActiveLine: false,
@@ -156,12 +189,15 @@ function Cell({ element, cellIndex }: Props) {
           defaultKeymap: false,
         }}
         style={{
-          padding: "0 0 0 1rem",
-          margin: "0 0 0 -2px",
+          background: "#fafafa",
           overflow: "hidden",
-          fontSize: "1rem",
+          fontFamily: "Source Code Pro, monospace",
+          fontSize: "14px",
           border: "none",
-          borderLeft: "2px solid #ddd",
+          borderRadius: "0 0.25rem 0.25rem 0",
+          padding: "1rem 1rem 1rem calc(1rem + 2px)",
+          margin: "0 0 0 -3px",
+          borderLeft: "3px solid #e6eaf1",
         }}
         onChange={onChange}
       />
