de82dd6fd4a2616713b2a5ba6821b9f4aa283ff7
Tim Conkling <tconkling@gmail.com>
2022-07-25 10:12:46 -0700

    More fixes

--- lib/tests/streamlit/web/server/server_test.py
+++ lib/tests/streamlit/web/server/server_test.py
@@ -85,30 +85,28 @@ class ServerTest(ServerTestCase):
     @tornado.testing.gen_test
     async def test_start_stop(self):
         """Test that we can start and stop the server."""
-        with patch(
-            "streamlit.web.server.server.LocalSourcesWatcher"
-        ), self._patch_app_session():
+        with patch("streamlit.runtime.LocalSourcesWatcher"), self._patch_app_session():
             await self.start_server_loop()
-            self.assertEqual(RuntimeState.WAITING_FOR_FIRST_SESSION, self.server._state)
+            self.assertEqual(
+                RuntimeState.WAITING_FOR_FIRST_SESSION, self.server._runtime._state
+            )
 
             await self.ws_connect()
             self.assertEqual(
-                RuntimeState.ONE_OR_MORE_SESSIONS_CONNECTED, self.server._state
+                RuntimeState.ONE_OR_MORE_SESSIONS_CONNECTED, self.server._runtime._state
             )
 
             self.server.stop()
-            self.assertEqual(RuntimeState.STOPPING, self.server._state)
+            self.assertEqual(RuntimeState.STOPPING, self.server._runtime._state)
 
             await asyncio.sleep(0.1)
-            self.assertEqual(RuntimeState.STOPPED, self.server._state)
+            self.assertEqual(RuntimeState.STOPPED, self.server._runtime._state)
 
     @tornado.testing.gen_test
     async def test_websocket_connect(self):
         """Test that we can connect to the server via websocket."""
 
-        with patch(
-            "streamlit.web.server.server.LocalSourcesWatcher"
-        ), self._patch_app_session():
+        with patch("streamlit.runtime.LocalSourcesWatcher"), self._patch_app_session():
             await self.start_server_loop()
 
             self.assertFalse(self.server.browser_is_connected)
@@ -118,8 +116,8 @@ class ServerTest(ServerTestCase):
             self.assertTrue(self.server.browser_is_connected)
 
             # Get this client's SessionInfo object
-            self.assertEqual(1, len(self.server._session_info_by_id))
-            session_info = list(self.server._session_info_by_id.values())[0]
+            self.assertEqual(1, len(self.server._runtime._session_info_by_id))
+            session_info = list(self.server._runtime._session_info_by_id.values())[0]
 
             # Close the connection
             ws_client.close()
@@ -129,15 +127,13 @@ class ServerTest(ServerTestCase):
             # Ensure AppSession.shutdown() was called, and that our
             # SessionInfo was cleared.
             session_info.session.shutdown.assert_called_once()
-            self.assertEqual(0, len(self.server._session_info_by_id))
+            self.assertEqual(0, len(self.server._runtime._session_info_by_id))
 
     @tornado.testing.gen_test
     async def test_multiple_connections(self):
         """Test multiple websockets can connect simultaneously."""
 
-        with patch(
-            "streamlit.web.server.server.LocalSourcesWatcher"
-        ), self._patch_app_session():
+        with patch("streamlit.runtime.LocalSourcesWatcher"), self._patch_app_session():
             await self.start_server_loop()
 
             self.assertFalse(self.server.browser_is_connected)
@@ -151,7 +147,7 @@ class ServerTest(ServerTestCase):
             self.assertTrue(self.server.browser_is_connected)
 
             # Assert that our session_infos are sane
-            session_infos = list(self.server._session_info_by_id.values())
+            session_infos = list(self.server._runtime._session_info_by_id.values())
             self.assertEqual(2, len(session_infos))
             self.assertNotEqual(
                 session_infos[0].session.id,
@@ -170,9 +166,7 @@ class ServerTest(ServerTestCase):
 
     @tornado.testing.gen_test
     async def test_websocket_compression(self):
-        with patch(
-            "streamlit.web.server.server.LocalSourcesWatcher"
-        ), self._patch_app_session():
+        with patch("streamlit.runtime.LocalSourcesWatcher"), self._patch_app_session():
             config._set_option("server.enableWebsocketCompression", True, "test")
             await self.start_server_loop()
 
@@ -188,9 +182,7 @@ class ServerTest(ServerTestCase):
 
     @tornado.testing.gen_test
     async def test_websocket_compression_disabled(self):
-        with patch(
-            "streamlit.web.server.server.LocalSourcesWatcher"
-        ), self._patch_app_session():
+        with patch("streamlit.runtime.LocalSourcesWatcher"), self._patch_app_session():
             config._set_option("server.enableWebsocketCompression", False, "test")
             await self.start_server_loop()
 
@@ -206,21 +198,19 @@ class ServerTest(ServerTestCase):
     @tornado.testing.gen_test
     async def test_forwardmsg_hashing(self):
         """Test that outgoing ForwardMsgs contain hashes."""
-        with patch(
-            "streamlit.web.server.server.LocalSourcesWatcher"
-        ), self._patch_app_session():
+        with patch("streamlit.runtime.LocalSourcesWatcher"), self._patch_app_session():
             await self.start_server_loop()
 
             ws_client = await self.ws_connect()
 
             # Get the server's socket and session for this client
-            session_info = list(self.server._session_info_by_id.values())[0]
+            session_info = list(self.server._runtime._session_info_by_id.values())[0]
 
             # Create a message and ensure its hash is unset; we're testing
             # that _send_message adds the hash before it goes out.
             msg = _create_dataframe_msg([1, 2, 3])
             msg.ClearField("hash")
-            self.server._send_message(session_info, msg)
+            self.server._runtime._send_message(session_info, msg)
 
             received = await self.read_forward_msg(ws_client)
             self.assertEqual(populate_hash_if_needed(msg), received.hash)
@@ -229,26 +219,24 @@ class ServerTest(ServerTestCase):
     async def test_forwardmsg_cacheable_flag(self):
         """Test that the metadata.cacheable flag is set properly on outgoing
         ForwardMsgs."""
-        with patch(
-            "streamlit.web.server.server.LocalSourcesWatcher"
-        ), self._patch_app_session():
+        with patch("streamlit.runtime.LocalSourcesWatcher"), self._patch_app_session():
             await self.start_server_loop()
 
             ws_client = await self.ws_connect()
 
             # Get the server's socket and session for this client
-            session_info = list(self.server._session_info_by_id.values())[0]
+            session_info = list(self.server._runtime._session_info_by_id.values())[0]
 
             config._set_option("global.minCachedMessageSize", 0, "test")
             cacheable_msg = _create_dataframe_msg([1, 2, 3])
-            self.server._send_message(session_info, cacheable_msg)
+            self.server._runtime._send_message(session_info, cacheable_msg)
             received = await self.read_forward_msg(ws_client)
             self.assertTrue(cacheable_msg.metadata.cacheable)
             self.assertTrue(received.metadata.cacheable)
 
             config._set_option("global.minCachedMessageSize", 1000, "test")
             cacheable_msg = _create_dataframe_msg([4, 5, 6])
-            self.server._send_message(session_info, cacheable_msg)
+            self.server._runtime._send_message(session_info, cacheable_msg)
             received = await self.read_forward_msg(ws_client)
             self.assertFalse(cacheable_msg.metadata.cacheable)
             self.assertFalse(received.metadata.cacheable)
@@ -256,21 +244,19 @@ class ServerTest(ServerTestCase):
     @tornado.testing.gen_test
     async def test_duplicate_forwardmsg_caching(self):
         """Test that duplicate ForwardMsgs are sent only once."""
-        with patch(
-            "streamlit.web.server.server.LocalSourcesWatcher"
-        ), self._patch_app_session():
+        with patch("streamlit.runtime.LocalSourcesWatcher"), self._patch_app_session():
             config._set_option("global.minCachedMessageSize", 0, "test")
 
             await self.start_server_loop()
             ws_client = await self.ws_connect()
 
             # Get the server's socket and session for this client
-            session_info = list(self.server._session_info_by_id.values())[0]
+            session_info = list(self.server._runtime._session_info_by_id.values())[0]
 
             msg1 = _create_dataframe_msg([1, 2, 3], 1)
 
             # Send the message, and read it back. It will not have been cached.
-            self.server._send_message(session_info, msg1)
+            self.server._runtime._send_message(session_info, msg1)
             uncached = await self.read_forward_msg(ws_client)
             self.assertEqual("delta", uncached.WhichOneof("type"))
 
@@ -278,7 +264,7 @@ class ServerTest(ServerTestCase):
 
             # Send an equivalent message. This time, it should be cached,
             # and a "hash_reference" message should be received instead.
-            self.server._send_message(session_info, msg2)
+            self.server._runtime._send_message(session_info, msg2)
             cached = await self.read_forward_msg(ws_client)
             self.assertEqual("ref_hash", cached.WhichOneof("type"))
             # We should have the *hash* of msg1 and msg2:
@@ -292,16 +278,14 @@ class ServerTest(ServerTestCase):
         """Test that report_run_count is incremented when a report
         finishes running.
         """
-        with patch(
-            "streamlit.web.server.server.LocalSourcesWatcher"
-        ), self._patch_app_session():
+        with patch("streamlit.runtime.LocalSourcesWatcher"), self._patch_app_session():
             config._set_option("global.minCachedMessageSize", 0, "test")
             config._set_option("global.maxCachedMessageAge", 1, "test")
 
             await self.start_server_loop()
             await self.ws_connect()
 
-            session = list(self.server._session_info_by_id.values())[0]
+            session = list(self.server._runtime._session_info_by_id.values())[0]
 
             data_msg = _create_dataframe_msg([1, 2, 3])
 
@@ -312,13 +296,16 @@ class ServerTest(ServerTestCase):
                     else ForwardMsg.FINISHED_WITH_COMPILE_ERROR
                 )
                 finish_msg = _create_script_finished_msg(status)
-                self.server._send_message(session, finish_msg)
+                self.server._runtime._send_message(session, finish_msg)
 
             def is_data_msg_cached():
-                return self.server._message_cache.get_message(data_msg.hash) is not None
+                return (
+                    self.server._runtime._message_cache.get_message(data_msg.hash)
+                    is not None
+                )
 
             def send_data_msg():
-                self.server._send_message(session, data_msg)
+                self.server._runtime._send_message(session, data_msg)
 
             # Send a cacheable message. It should be cached.
             send_data_msg()
@@ -352,21 +339,19 @@ class ServerTest(ServerTestCase):
     async def test_orphaned_upload_file_deletion(self):
         """An uploaded file with no associated AppSession should be
         deleted."""
-        with patch(
-            "streamlit.web.server.server.LocalSourcesWatcher"
-        ), self._patch_app_session():
+        with patch("streamlit.runtime.LocalSourcesWatcher"), self._patch_app_session():
             await self.start_server_loop()
             await self.ws_connect()
 
             # "Upload a file" for a session that doesn't exist
-            self.server._uploaded_file_mgr.add_file(
+            self.server._runtime._uploaded_file_mgr.add_file(
                 session_id="no_such_session",
                 widget_id="widget_id",
                 file=UploadedFileRec(0, "file.txt", "type", b"123"),
             )
 
             self.assertEqual(
-                self.server._uploaded_file_mgr.get_all_files(
+                self.server._runtime._uploaded_file_mgr.get_all_files(
                     "no_such_session", "widget_id"
                 ),
                 [],
@@ -377,14 +362,12 @@ class ServerTest(ServerTestCase):
         """Sending a message to a disconnected SessionClient raises an error.
         We should gracefully handle the error by cleaning up the session.
         """
-        with patch(
-            "streamlit.web.server.server.LocalSourcesWatcher"
-        ), self._patch_app_session():
+        with patch("streamlit.runtime.LocalSourcesWatcher"), self._patch_app_session():
             await self.start_server_loop()
             await self.ws_connect()
 
             # Get the server's socket and session for this client
-            session_info = list(self.server._session_info_by_id.values())[0]
+            session_info = list(self.server._runtime._session_info_by_id.values())[0]
 
             with patch.object(
                 session_info.session, "flush_browser_queue"
@@ -402,7 +385,7 @@ class ServerTest(ServerTestCase):
                 # and the Websocket client's write_message will be called,
                 # raising our WebSocketClosedError.
                 while not flush_browser_queue.called:
-                    self.server._need_send_data.set()
+                    self.server._runtime._need_send_data.set()
                     await asyncio.sleep(0)
 
                 flush_browser_queue.assert_called_once()
@@ -411,7 +394,7 @@ class ServerTest(ServerTestCase):
                 # Our session should have been removed from the server as
                 # a result of the WebSocketClosedError.
                 self.assertIsNone(
-                    self.server._get_session_info(session_info.session.id)
+                    self.server._runtime._get_session_info(session_info.session.id)
                 )
 
     @tornado.testing.gen_test
@@ -422,10 +405,12 @@ class ServerTest(ServerTestCase):
             await self.ws_connect()
 
             # Get our connected BrowserWebSocketHandler
-            session_info = list(self.server._session_info_by_id.values())[0]
+            session_info = list(self.server._runtime._session_info_by_id.values())[0]
 
-            self.assertFalse(self.server.is_active_session("not_a_session_id"))
-            self.assertTrue(self.server.is_active_session(session_info.session.id))
+            self.assertFalse(self.server._runtime.is_active_session("not_a_session_id"))
+            self.assertTrue(
+                self.server._runtime.is_active_session(session_info.session.id)
+            )
 
     @tornado.testing.gen_test
     async def test_get_eventloop(self):
@@ -435,11 +420,11 @@ class ServerTest(ServerTestCase):
         with self._patch_app_session():
             with self.assertRaises(RuntimeError):
                 # Server hasn't started yet: error!
-                _ = self.server._get_eventloop()
+                _ = self.server._runtime._get_eventloop()
 
             # Server has started: no error
             await self.start_server_loop()
-            eventloop = self.server._get_eventloop()
+            eventloop = self.server._runtime._get_eventloop()
             self.assertIsInstance(eventloop, asyncio.AbstractEventLoop)
 
 
@@ -741,7 +726,7 @@ class ScriptCheckTest(tornado.testing.AsyncTestCase):
         with os.fdopen(self._fd, "w") as tmp:
             tmp.write(script)
 
-        ok, msg = await self._server.does_script_run_without_error()
+        ok, msg = await self._server._runtime.does_script_run_without_error()
         event_based_path_watcher._MultiPathWatcher.get_singleton().close()
         event_based_path_watcher._MultiPathWatcher._singleton = None
         self.assertEqual(expected_loads, ok)
