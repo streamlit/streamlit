f283576f8879d5729f494a809e259d6bad89dc7f
lukasmasuch <lukas.masuch@gmail.com>
2022-07-25 18:31:11 +0200

    Fix unit tests

--- lib/tests/streamlit/arrow_altair_test.py
+++ lib/tests/streamlit/arrow_altair_test.py
@@ -113,23 +113,21 @@ class ArrowChartsTest(testutil.DeltaGeneratorTestCase):
             EXPECTED_DATAFRAME,
         )
 
-    # def test_arrow_line_chart_with_x_y(self):
-    #     """Test st._arrow_line_chart."""
-    #     df = pd.DataFrame([[20, 30, 50]], columns=["a", "b", "c"])
-    #     EXPECTED_DATAFRAME = pd.DataFrame(
-    #         [[0, "a", 20], [0, "b", 30], [0, "c", 50]],
-    #         index=[0, 1, 2],
-    #     )
+    def test_arrow_line_chart_with_x_y(self):
+        """Test st._arrow_line_chart."""
+        df = pd.DataFrame([[20, 30, 50]], columns=["a", "b", "c"])
+        EXPECTED_DATAFRAME = pd.DataFrame([[20, 30, 50]], columns=["a", "b"])
 
-    #     st._arrow_line_chart(df, x="a", y="b")
+        st._arrow_line_chart(df, x="a", y="b")
 
-    #     proto = self.get_delta_from_queue().new_element.arrow_vega_lite_chart
-    #     chart_spec = json.loads(proto.spec)
-    #     self.assertEqual(chart_spec["mark"], "line")
-    #     pd.testing.assert_frame_equal(
-    #         bytes_to_data_frame(proto.datasets[0].data.data),
-    #         EXPECTED_DATAFRAME,
-    #     )
+        proto = self.get_delta_from_queue().new_element.arrow_vega_lite_chart
+        chart_spec = json.loads(proto.spec)
+        self.assertEqual(chart_spec["mark"], "line")
+        print(chart_spec)
+        pd.testing.assert_frame_equal(
+            bytes_to_data_frame(proto.datasets[0].data.data),
+            EXPECTED_DATAFRAME,
+        )
 
     def test_arrow_line_chart_with_generic_index(self):
         """Test st._arrow_line_chart with a generic index."""
@@ -138,7 +136,7 @@ class ArrowChartsTest(testutil.DeltaGeneratorTestCase):
         EXPECTED_DATAFRAME = pd.DataFrame(
             [[20, "b", 30], [20, "c", 50]],
             index=[0, 1],
-            columns=["a", "variable", "value"],
+            columns=["a", "value", "variable"],
         )
 
         st._arrow_line_chart(df)
@@ -157,7 +155,7 @@ class ArrowChartsTest(testutil.DeltaGeneratorTestCase):
         EXPECTED_DATAFRAME = pd.DataFrame(
             [[0, "a", 20], [0, "b", 30], [0, "c", 50]],
             index=[0, 1, 2],
-            columns=["index", "variable", "value"],
+            columns=["index", "value", "variable"],
         )
 
         st._arrow_area_chart(df)
@@ -176,7 +174,7 @@ class ArrowChartsTest(testutil.DeltaGeneratorTestCase):
         EXPECTED_DATAFRAME = pd.DataFrame(
             [[0, "a", 20], [0, "b", 30], [0, "c", 50]],
             index=[0, 1, 2],
-            columns=["index", "variable", "value"],
+            columns=["index", "value", "variable"],
         )
 
         st._arrow_bar_chart(df)
