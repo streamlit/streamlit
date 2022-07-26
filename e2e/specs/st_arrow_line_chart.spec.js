fb22b24cbc5ab8994235d08de1315d40ea5ead8e
lukasmasuch <lukas.masuch@gmail.com>
2022-07-26 16:27:29 +0200

    Add e2e test

--- e2e/specs/st_arrow_line_chart.spec.js
+++ e2e/specs/st_arrow_line_chart.spec.js
@@ -23,6 +23,20 @@ describe("st._arrow_line_chart", () => {
   it("displays a line chart", () => {
     cy.get(".element-container [data-testid='stArrowVegaLiteChart']")
       .find("canvas")
+      .first()
       .should("have.css", "height", "350px");
+  });
+
+  it("displays correctly", () => {
+    cy.get(".element-container [data-testid='stArrowVegaLiteChart']").should(
+      "have.length",
+      7
+    );
+
+    cy.get(".element-container [data-testid='stArrowVegaLiteChart']").each(
+      (el, idx) => {
+        return cy.wrap(el).matchThemedSnapshots("arrow_line_chart" + idx);
+      }
+    );
   });
 });
