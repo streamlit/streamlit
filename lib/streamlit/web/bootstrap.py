b7cbe03cc901dad9a98a3d97e577866268d411bc
Karen Javadyan <kajarenc@gmail.com>
2022-07-28 17:04:48 +0400

    Add debug prints

--- lib/streamlit/web/bootstrap.py
+++ lib/streamlit/web/bootstrap.py
@@ -330,15 +330,19 @@ def _install_pages_watcher(main_script_path_str: str) -> None:
     def _on_pages_changed(_path: str) -> None:
         invalidate_pages_cache()
 
-    main_script_path = Path(main_script_path_str)
-    pages_dir = main_script_path.parent / "pages"
+    try:
+        main_script_path = Path(main_script_path_str)
+        pages_dir = main_script_path.parent / "pages"
 
-    watch_dir(
-        str(pages_dir),
-        _on_pages_changed,
-        glob_pattern="*.py",
-        allow_nonexistent=True,
-    )
+        watch_dir(
+            str(pages_dir),
+            _on_pages_changed,
+            glob_pattern="*.py",
+            allow_nonexistent=True,
+        )
+    except Exception as e:
+        print("OOOOOOOOOOOOOOOO MAMA")
+        print(e)
 
 
 def run(
