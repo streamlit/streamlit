79678a80e2dae26949db847fc9bcf4b55334f6ce
Juan Martín García <me@jmg.land>
2022-08-04 20:01:56 -0300

    Update callout colors and functionality (#4964)
    * Update notification colors
    
    * Remove snapshots
    
    * Update snapshots
    
    * Add back missing snapshots
    
    * Fix linting errors
    
    * Update tests removing unused border check
    
    * Remove unused import
    
    * WIP: Basic support for icon parameter
    
    * Add emoji validation to icon parameter
    
    * Better regex validation
    
    * Use EmojiIcon component instead of markdown
    
    * Move regex pattern to string_utils file
    
    * Use None as the default value for icon
    
    * Update streamlitException layout
    
    * Add pythong tests
    
    * Add python tests
    
    * Add JS tests
    
    * Update e2e scripts
    
    * Add newline after function
    
    * Run linter
    
    * Delete alert snapshots
    
    * Add back alert snapshots
    
    * Fix linting errors
    
    * Refactor lib files to comply with type checking
    
    * Add back hello snapshots
    
    * Remove e2e tests for icon alerts as emojis arent rendering as expected
    
    * Fix linting errors
    
    * Update js tests
    
    * Move new colors to lightTheme instead of overriding the base ones
    
    * Move Format definition above in proto
    
    * Move import above type checking
    
    * Replace shortcodes in docstring with emojis
    
    * Order imports alphabetically
    
    * Remove check_emoji function and merge it with validate_emoji
    
    * Use new validation method for emoji check
    
    * Check emoji in separate function
    
    * Remove hello snapshots
    
    * Fix linting
    
    * Remove old function call and add optional type checking
    
    * Update warning color to enhance accessibility
    
    * Remove light mode snapshot
    
    * Update tests to use valid emojis
    
    * Add some snapshots, remove others
    
    * Update tests
    
    * Add back snapshots
    
    * Remove unnecessary imports
    
    * Add more tests for validate_emoji and make some small cleanup changes
    
    Co-authored-by: Juan Martin Garcia <juan.garcia@snowflake.com>
    Co-authored-by: Vincent Donato <vincent@streamlit.io>

--- e2e/scripts/st_alert.py
+++ e2e/scripts/st_alert.py
@@ -19,6 +19,11 @@ st.warning("This is a warning")
 st.info("This is an info message")
 st.success("This is a success message")
 
-# This is here so we can test the distance between the success message and the
+st.error("This is an error", icon="🚨")
+st.warning("This is a warning", icon="⚠️")
+st.info("This is an info message", icon="👉🏻")
+st.success("This is a success message", icon="✅")
+
+# This is here so we can test the distance beween the success message and the
 # "Done!" text below.
 st.write("Done!")
