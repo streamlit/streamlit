f955f480942a39bc7cdde512794281799fefeaa8
Vincent Donato <vincent@streamlit.io>
2022-08-04 15:35:26 -0700

    Remove unnecessary imports

--- lib/streamlit/elements/alert.py
+++ lib/streamlit/elements/alert.py
@@ -12,8 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import re
-
 from typing import cast, Optional, TYPE_CHECKING
 
 from streamlit.errors import StreamlitAPIException
