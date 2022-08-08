9bae704c846213dbbfa4d4700a29de3bee2657ba
Tim Conkling <tconkling@gmail.com>
2022-08-08 13:17:41 -0700

    pyformat

--- lib/tests/streamlit/runtime/runtime_test_case.py
+++ lib/tests/streamlit/runtime/runtime_test_case.py
@@ -60,9 +60,7 @@ class RuntimeTestCase(IsolatedAsyncioTestCase):
     def _create_mock_app_session(cls, *args, **kwargs):
         """Create a mock AppSession. Each mocked instance will have
         its own unique ID."""
-        mock_id = mock.PropertyMock(
-            return_value=f"mock_id:{cls._next_session_id}"
-        )
+        mock_id = mock.PropertyMock(return_value=f"mock_id:{cls._next_session_id}")
         cls._next_session_id += 1
 
         mock_session = mock.MagicMock(AppSession, autospec=True, *args, **kwargs)
