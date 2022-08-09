96c5c0fee9ee14c9d65af5f3af3b7922578dc38a
Tim Conkling <tconkling@gmail.com>
2022-08-09 12:12:46 -0700

    test_handle_session_client_disconnected

--- lib/tests/streamlit/runtime/runtime_test.py
+++ lib/tests/streamlit/runtime/runtime_test.py
@@ -195,6 +195,32 @@ class RuntimeTest(RuntimeTestCase):
         await self.start_runtime_loop()
         self.assertTrue(streamlit._is_running_with_streamlit)
 
+    async def test_handle_session_client_disconnected(self):
+        """Runtime should gracefully handle `SessionClient.write_forward_msg`
+        raising a `SessionClientDisconnectedError`.
+        """
+        await self.start_runtime_loop()
+
+        client = MagicMock(spec=SessionClient)
+        session_id = self.runtime.create_session(client, MagicMock())
+
+        # Send the client a message. All should be well.
+        self.enqueue_forward_msg(session_id, create_dataframe_msg([1, 2, 3]))
+        await self.tick_runtime_loop()
+
+        client.write_forward_msg.assert_called_once()
+        self.assertTrue(self.runtime.is_active_session(session_id))
+
+        # Send another message - but this time the client will raise an error.
+        raise_disconnected_error = MagicMock(side_effect=SessionClientDisconnectedError)
+        client.write_forward_msg = raise_disconnected_error
+        self.enqueue_forward_msg(session_id, create_dataframe_msg([1, 2, 3]))
+        await self.tick_runtime_loop()
+
+        # Assert that our error was raised, and that our session was disconnected.
+        raise_disconnected_error.assert_called_once()
+        self.assertFalse(self.runtime.is_active_session(session_id))
+
     async def test_forwardmsg_hashing(self):
         """Test that outgoing ForwardMsgs contain hashes."""
         await self.start_runtime_loop()
