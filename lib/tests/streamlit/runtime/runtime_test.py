6a130b1da8bc199188bb3adbdf01d5346a1b2d49
Tim Conkling <tconkling@gmail.com>
2022-08-10 12:21:32 -0700

    test_shutdown_appsessions_on_stop

--- lib/tests/streamlit/runtime/runtime_test.py
+++ lib/tests/streamlit/runtime/runtime_test.py
@@ -149,6 +149,33 @@ class RuntimeTest(RuntimeTestCase):
         self.runtime.close_session(session_id)
         self.assertFalse(self.runtime.is_active_session(session_id))
 
+    async def test_shutdown_appsessions_on_stop(self):
+        """When the Runtime stops, it should shut down open AppSessions."""
+        with self.patch_app_session():
+            await self.start_runtime_loop()
+
+            # Create a few sessions
+            app_sessions = []
+            for _ in range(3):
+                session_id = self.runtime.create_session(
+                    MockSessionClient(), MagicMock()
+                )
+                app_session = self.runtime._get_session_info(session_id).session
+                app_sessions.append(app_session)
+
+            # Sanity check
+            for app_session in app_sessions:
+                app_session.shutdown.assert_not_called()
+
+            # Stop the Runtime
+            self.runtime.stop()
+            await self.runtime.stopped
+
+            # All sessions should be shut down
+            self.assertEqual(RuntimeState.STOPPED, self.runtime.state)
+            for app_session in app_sessions:
+                app_session.shutdown.assert_called_once()
+
     async def test_handle_backmsg(self):
         """BackMsgs should be delivered to the appropriate AppSession."""
         with self.patch_app_session():
