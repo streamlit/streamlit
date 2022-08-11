b17065662c9a682d2010f0e932e1bb3b2845006a
Tomasz Szerszeń <tomasz.szerszen@snowflake.com>
2022-08-11 08:57:29 +0200

    rename setStartTime to onLoadedMetadata

--- frontend/src/components/elements/Video/Video.tsx
+++ frontend/src/components/elements/Video/Video.tsx
@@ -36,9 +36,11 @@ export default function Video({ element, width }: VideoProps): ReactElement {
   useEffect(() => {
     const videoNode = videoRef.current
 
-    const setStartTime: () => void = () => {
+    const onLoadedMetadata: () => void = () => {
       if (videoNode) {
+        // setStartTime
         videoNode.currentTime = element.startTime
+
         /* height of HTML video element needs to be set to avoid scrolling issue: https://github.com/streamlit/streamlit/issues/5069
            initially HTML video has height 0 and when tabs are switched page auto scrolls up
            because of "lack of content" on the page, setting height of HTML video element fixes it
@@ -48,12 +50,12 @@ export default function Video({ element, width }: VideoProps): ReactElement {
     }
 
     if (videoNode) {
-      videoNode.addEventListener("loadedmetadata", setStartTime)
+      videoNode.addEventListener("loadedmetadata", onLoadedMetadata)
     }
 
     return () => {
       if (videoNode) {
-        videoNode.removeEventListener("loadedmetadata", setStartTime)
+        videoNode.removeEventListener("loadedmetadata", onLoadedMetadata)
       }
     }
   }, [element])
