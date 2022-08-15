0a886cddc32c1dc59a7dd1f50c0768f0785c666b
Tim Conkling <tconkling@gmail.com>
2022-08-15 12:31:13 -0700

    sanity check

--- lib/tests/streamlit/runtime/runtime_threading_test.py
+++ lib/tests/streamlit/runtime/runtime_threading_test.py
@@ -35,6 +35,11 @@ class RuntimeThreadingTest(IsolatedAsyncioTestCase):
 
         def create_runtime_on_another_thread():
             try:
+                # This function should be called in another thread, which
+                # should not already have an asyncio loop.
+                with self.assertRaises(BaseException):
+                    asyncio.get_running_loop()
+
                 # Create a Runtime instance and put it in the (thread-safe) queue,
                 # so that the main thread can retrieve it safely. If Runtime
                 # creation fails, we'll stick an Exception in the queue instead.
