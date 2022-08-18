f044720d78dfa625bdcfe9d2fd4a3b2a2f27b4b0
Tim Conkling <tconkling@gmail.com>
2022-08-18 12:53:30 -0700

    comments

--- lib/tests/streamlit/runtime/app_session_test.py
+++ lib/tests/streamlit/runtime/app_session_test.py
@@ -238,6 +238,8 @@ class AppSessionTest(unittest.TestCase):
         session = _create_test_session()
         session._create_scriptrunner(initial_rerun_data=RerunData())
 
+        # Our test AppSession is created with a mock EventLoop, so
+        # we pretend that this function is called on that same mock EventLoop.
         with patch(
             "streamlit.runtime.app_session.asyncio.get_running_loop",
             return_value=session._event_loop,
@@ -455,7 +457,7 @@ class AppSessionScriptEventTest(IsolatedAsyncioTestCase):
 
     async def test_event_handler_asserts_if_called_off_event_loop(self):
         """AppSession._handle_scriptrunner_event_on_event_loop will assert
-        if it's called from another thread.
+        if it's called from another event loop (or no event loop).
         """
         event_loop = asyncio.get_running_loop()
         session = _create_test_session(event_loop)
