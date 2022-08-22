8dc5c6d9d2d61c589a8a1d38d8fbfe37cc0c14df
Tim Conkling <tconkling@gmail.com>
2022-08-22 12:10:59 -0700

    type anno

--- lib/streamlit/web/server/routes.py
+++ lib/streamlit/web/server/routes.py
@@ -108,7 +108,7 @@ class AddSlashHandler(tornado.web.RequestHandler):
 
 
 class MediaFileHandler(tornado.web.StaticFileHandler):
-    def set_default_headers(self):
+    def set_default_headers(self) -> None:
         if allow_cross_origin_requests():
             self.set_header("Access-Control-Allow-Origin", "*")
 
