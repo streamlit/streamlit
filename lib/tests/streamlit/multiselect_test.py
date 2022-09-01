c65293b14d5dc704f77be09a9510c9c1cd988a42
Karen Javadyan <kajarenc@gmail.com>
2022-09-01 18:53:24 +0400

    Add LabelVisibility proto for multiselect

--- lib/tests/streamlit/multiselect_test.py
+++ lib/tests/streamlit/multiselect_test.py
@@ -21,6 +21,7 @@ from parameterized import parameterized
 
 import streamlit as st
 from streamlit.errors import StreamlitAPIException
+from streamlit.proto.LabelVisibilityMessage_pb2 import LabelVisibilityMessage
 from tests import testutil
 
 
@@ -33,7 +34,9 @@ class Multiselectbox(testutil.DeltaGeneratorTestCase):
 
         c = self.get_delta_from_queue().new_element.multiselect
         self.assertEqual(c.label, "the label")
-        self.assertEqual(c.label_visibility, "visible")
+        self.assertEqual(
+            c.label_visibility.value, LabelVisibilityMessage.LabelVisibilityEnum.VISIBLE
+        )
         self.assertListEqual(c.default[:], [])
         self.assertEqual(c.disabled, False)
 
@@ -245,9 +248,9 @@ class Multiselectbox(testutil.DeltaGeneratorTestCase):
 
     @parameterized.expand(
         [
-            ("visible", "visible"),
-            ("hidden", "hidden"),
-            ("collapsed", "collapsed"),
+            ("visible", LabelVisibilityMessage.LabelVisibilityEnum.VISIBLE),
+            ("hidden", LabelVisibilityMessage.LabelVisibilityEnum.HIDDEN),
+            ("collapsed", LabelVisibilityMessage.LabelVisibilityEnum.COLLAPSED),
         ]
     )
     def test_label_visibility(self, label_visibility_value, proto_value):
@@ -255,7 +258,7 @@ class Multiselectbox(testutil.DeltaGeneratorTestCase):
         st.multiselect("the label", ("m", "f"), label_visibility=label_visibility_value)
 
         c = self.get_delta_from_queue().new_element.multiselect
-        self.assertEqual(c.label_visibility, proto_value)
+        self.assertEqual(c.label_visibility.value, proto_value)
 
     def test_label_visibility_wrong_value(self):
         with self.assertRaises(StreamlitAPIException) as e:
