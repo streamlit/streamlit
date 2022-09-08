f0655e40e9f22e033c663843760454a2c76d93c0
Tim Conkling <tconkling@gmail.com>
2022-09-08 15:34:04 -0700

    test_cache_stats

--- lib/tests/streamlit/web/server/memory_media_file_storage_test.py
+++ lib/tests/streamlit/web/server/memory_media_file_storage_test.py
@@ -144,3 +144,28 @@ class MemoryMediaFileStorageTest(unittest.TestCase):
     def test_delete_invalid_file_is_a_noop(self):
         """deleting a file that doesn't exist doesn't raise an error."""
         self.storage.delete_file("mock_file_id")
+
+    def test_cache_stats(self):
+        """Test our CacheStatsProvider implementation."""
+        self.assertEqual(0, len(self.storage.get_stats()))
+
+        # Add several files to storage. We'll unique-ify them by filename.
+        mock_data = b"some random mock binary data"
+        num_files = 5
+        for ii in range(num_files):
+            self.storage.load_and_get_id(
+                mock_data, mimetype="video/mp4", filename=f"{ii}.mp4"
+            )
+
+        stats = self.storage.get_stats()
+        self.assertEqual(num_files, len(stats))
+        self.assertEqual("st_media_file_manager", stats[0].category_name)
+        self.assertEqual(
+            len(mock_data) * num_files, sum(stat.byte_length for stat in stats)
+        )
+
+        # Remove files, and ensure our cache doesn't report they still exist
+        for file_id in list(self.storage._files_by_id.keys()):
+            self.storage.delete_file(file_id)
+
+        self.assertEqual(0, len(self.storage.get_stats()))
