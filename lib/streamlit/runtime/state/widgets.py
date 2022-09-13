33b2362060e1f6de171755a355952270918fa097
Amanda Walker <amanda@amandawalker.io>
2022-09-12 17:59:00 -0700

    Clarify `get_widget_user_key` behavior

--- lib/streamlit/runtime/state/widgets.py
+++ lib/streamlit/runtime/state/widgets.py
@@ -205,8 +205,8 @@ def user_key_from_widget_id(wid: str) -> Optional[str]:
     Returns `None` instead of `"None"` if there was no user key,
     for compatibility with the rest of the codebase, which represents it that way.
 
-    TODO This will not work correctly if the user actually provides "None"
-    as a key, but we can't avoid this kind of problem while storing the
+    TODO This will incorrectly indicate no user key if the user actually provides
+    "None" as a key, but we can't avoid this kind of problem while storing the
     string representation of the no-user-key sentinel as part of the widget id.
     """
     user_key = wid.split("-", maxsplit=2)[-1]
--- lib/tests/streamlit/runtime/state/widgets_test.py
+++ lib/tests/streamlit/runtime/state/widgets_test.py
@@ -336,3 +336,11 @@ class WidgetUserKeyTests(testutil.DeltaGeneratorTestCase):
 
         k = list(state._keys())[0]
         assert user_key_from_widget_id(k) == "my-slider"
+
+    def test_get_widget_user_key_incorrect_none(self):
+        state = get_script_run_ctx().session_state._state
+        st.checkbox("checkbox", key="None")
+
+        k = list(state._keys())[0]
+        # Incorrectly inidcates no user key
+        assert user_key_from_widget_id(k) == None
