2b7357f8131add53874f3187e3bb1bbede907efa
Kamil Bregula <kamil.bregula@snowflake.com>
2022-09-13 13:59:44 +0200

    (Generated) Add license headers by pre-commit hook

--- scripts/get_latest_vizjs.sh
+++ scripts/get_latest_vizjs.sh
@@ -1,3 +1,17 @@
+# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # Download the latest version of viz.js from npm.
 # We don't manage viz via node_modules, so we should run this script
 # from time to time to update it.
