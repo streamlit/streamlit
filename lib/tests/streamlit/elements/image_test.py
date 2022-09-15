e8771cc195f024dd82489569c70a8e1db88f0564
Tim Conkling <tconkling@gmail.com>
2022-09-15 12:52:15 -0700

    Better header

--- lib/tests/streamlit/elements/image_test.py
+++ lib/tests/streamlit/elements/image_test.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""st.image unit tests."""
+"""Unit tests for st.image and other image.py utility code."""
 
 import PIL.Image as Image
 import cv2
