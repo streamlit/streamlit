f60af943eabc18aece21a36a1e5d206b4268bd74
Amanda Walker <amanda@amandawalker.io>
2022-09-15 14:10:40 -0700

    Update comment about widget vs nonwidget for cache warning callstack

--- lib/streamlit/runtime/caching/cache_utils.py
+++ lib/streamlit/runtime/caching/cache_utils.py
@@ -413,10 +413,11 @@ class CacheWarningCallStack(threading.local):
             The name of the Streamlit function that was called.
 
         """
-        # TODO does this leave anything unsupported? can we remove the warning callstack?
+        # There are some elements not in either list, which we still want to warn about.
+        # Ideally we will fix this by either updating the lists or creating a better
+        # way of categorizing elements.
         if st_func_name in NONWIDGET_ELEMENTS or st_func_name in WIDGETS:
             return
-        print("found something that is neither a widget nor nonwidget")
         if len(self._cached_func_stack) > 0 and self._suppress_st_function_warning <= 0:
             cached_func = self._cached_func_stack[-1]
             self._show_cached_st_function_warning(dg, st_func_name, cached_func)
