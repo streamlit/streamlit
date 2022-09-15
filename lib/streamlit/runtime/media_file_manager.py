ac2eb872076b591abc91563ea4b9ab08f6064ff0
Tim Conkling <tconkling@gmail.com>
2022-09-15 16:51:44 -0700

    Create MediaFileManager on the fly when running in raw mode

--- lib/streamlit/runtime/media_file_manager.py
+++ lib/streamlit/runtime/media_file_manager.py
@@ -21,6 +21,7 @@ from typing import Dict, Set, Optional, Union
 import streamlit
 from streamlit.logger import get_logger
 from .media_file_storage import MediaFileStorage, MediaFileKind
+from .memory_media_file_storage import MemoryMediaFileStorage
 
 LOGGER = get_logger(__name__)
 
@@ -251,12 +252,17 @@ def get_media_file_manager() -> MediaFileManager:
     """Return the singleton MediaFileManager instance. Raise an error
     if it hasn't been instantiated yet.
     """
+    global _media_file_manager
+
     if _media_file_manager is None:
         if not streamlit._is_running_with_streamlit:
             # If we're running in "raw" mode, we have no Runtime to
             # create our media_file_manager. So we create one on the
             # fly instead.
             LOGGER.info("Creating in-memory MediaFileManager")
+            storage = MemoryMediaFileStorage("/media")
+            _media_file_manager = MediaFileManager(storage)
+        else:
+            raise RuntimeError("MediaFileManager hasn't been created!")
 
-        raise RuntimeError("MediaFileManager hasn't been created!")
     return _media_file_manager
