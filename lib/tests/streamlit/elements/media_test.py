067ec44d32877bb4b489aecc2e676d384754b8f0
Tim Conkling <tconkling@gmail.com>
2022-09-15 14:53:01 -0700

    fix media_test.py

--- lib/tests/streamlit/elements/media_test.py
+++ lib/tests/streamlit/elements/media_test.py
@@ -23,6 +23,9 @@ import streamlit as st
 from streamlit import RootContainer
 from streamlit.cursor import make_delta_path
 from streamlit.elements.media import MediaData
+from streamlit.runtime import media_file_manager
+from streamlit.runtime.media_file_manager import MediaFileManager
+from streamlit.web.server.memory_media_file_storage import MemoryMediaFileStorage
 from tests import testutil
 
 
@@ -32,6 +35,16 @@ class TestMediaKind(Enum):
 
 
 class MediaTest(testutil.DeltaGeneratorTestCase):
+    def setUp(self, override_root=True):
+        super().setUp(override_root)
+        media_file_manager._media_file_manager = MediaFileManager(
+            MemoryMediaFileStorage("/mock/media")
+        )
+
+    def tearDown(self):
+        media_file_manager._media_file_manager = None
+        super().tearDown()
+
     @parameterized.expand(
         [
             ("foo.wav", "audio/wav", TestMediaKind.AUDIO, False),
@@ -55,7 +68,7 @@ class MediaTest(testutil.DeltaGeneratorTestCase):
         MediaFileManager. URL-based media does not go through the MediaFileManager.
         """
         with mock.patch(
-            "streamlit.elements.media.media_file_manager.add"
+            "streamlit.runtime.media_file_manager.MediaFileManager.add"
         ) as mock_mfm_add:
             mock_mfm_add.return_value = "https://mockoutputurl.com"
 
