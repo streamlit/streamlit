f57beb2a1ea656b1706c34fd1d8f16171eef218d
Tim Conkling <tconkling@gmail.com>
2022-09-20 09:56:43 -0700

    clarifying comment

--- lib/tests/streamlit/runtime/runtime_test.py
+++ lib/tests/streamlit/runtime/runtime_test.py
@@ -484,6 +484,9 @@ class ScriptCheckTest(RuntimeTestCase):
         super().setUp()
 
     async def asyncSetUp(self):
+        # We don't call super().asyncSetUp() here. (Our superclass creates
+        # its own Runtime instance with a mock script_path, but we want
+        # to specify a non-mocked path.)
         config = RuntimeConfig(
             script_path=self._path,
             command_line="mock command line",
