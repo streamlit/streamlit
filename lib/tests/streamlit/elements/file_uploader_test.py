d90014e1cdb875c5fd2928b26335b0ff36f9cf5a
Tim Conkling <tconkling@gmail.com>
2022-09-21 16:24:54 -0700

    PREFERRED_MIMETYPE_EXTENSION_MAP: - Remove "image/jpeg" mapping - Add a comment about "audio/wav" - Remove get_extension_for_mimetype Python 3.6 comment - Add an explicit `test_get_extension_for_mimetype` unit test

--- lib/tests/streamlit/elements/file_uploader_test.py
+++ lib/tests/streamlit/elements/file_uploader_test.py
@@ -55,10 +55,10 @@ class FileUploaderTest(testutil.DeltaGeneratorTestCase):
 
     def test_multiple_types(self):
         """Test that it can be called using an array for type parameter."""
-        st.file_uploader("the label", type=["png", ".svg", "jpeg"])
+        st.file_uploader("the label", type=["png", ".svg", "jpg"])
 
         c = self.get_delta_from_queue().new_element.file_uploader
-        self.assertEqual(c.type, [".png", ".svg", ".jpeg"])
+        self.assertEqual(c.type, [".png", ".svg", ".jpg"])
 
     @patch("streamlit.elements.file_uploader._get_file_recs")
     def test_multiple_files(self, get_file_recs_patch):
