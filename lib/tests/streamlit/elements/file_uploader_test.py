c20b67c72c15addee5fd0af4d4e565d50a3aefb1
Tim Conkling <tconkling@gmail.com>
2022-09-21 16:33:22 -0700

    revert unrelated change

--- lib/tests/streamlit/elements/file_uploader_test.py
+++ lib/tests/streamlit/elements/file_uploader_test.py
@@ -55,10 +55,10 @@ class FileUploaderTest(testutil.DeltaGeneratorTestCase):
 
     def test_multiple_types(self):
         """Test that it can be called using an array for type parameter."""
-        st.file_uploader("the label", type=["png", ".svg", "jpg"])
+        st.file_uploader("the label", type=["png", ".svg", "jpeg"])
 
         c = self.get_delta_from_queue().new_element.file_uploader
-        self.assertEqual(c.type, [".png", ".svg", ".jpg"])
+        self.assertEqual(c.type, [".png", ".svg", ".jpeg"])
 
     @patch("streamlit.elements.file_uploader._get_file_recs")
     def test_multiple_files(self, get_file_recs_patch):
