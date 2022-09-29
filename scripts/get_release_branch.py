710b9f8d1867b6416fcd2f879559b21937d98192
Maya Barnes <maya.barnes@snowflake.com>
2022-09-28 21:40:48 -0700

    Fix nits & refactor PR branch lookup to script

---
+++ scripts/get_release_branch.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Retrieve the branch name from the release PR."""
+import json
+import requests
+
+
+def check_for_release_pr(pull):
+    label = pull["head"]["label"]
+
+    if label.find("release/") != -1:
+        return pull["head"]["ref"]
+    else:
+        pass
+
+
+def get_release_branch():
+    """Retrieve the release branch from the release PR"""
+
+    url = "https://api.github.com/repos/streamlit/streamlit/pulls"
+    response = requests.get(url).text
+    result = json.loads(response)
+
+    # Response is in an array, must map over each pull (dict)
+    for pull in result:
+        ref = check_for_release_pr(pull)
+        if ref != None:
+            return ref
+
+
+def main():
+    """Run main loop."""
+
+    release_branch = get_release_branch()
+    print(release_branch)
+
+
+if __name__ == "__main__":
+    main()
