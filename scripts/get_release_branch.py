d7dca7a4cb2b2ea0c3514a51ceef56509b9eeed9
Maya Barnes <maya.barnes@snowflake.com>
2022-09-30 02:45:22 -0700

    Clean up

--- scripts/get_release_branch.py
+++ scripts/get_release_branch.py
@@ -32,7 +32,7 @@ def get_release_branch():
     response = requests.get(url).json()
 
     # Response is in an array, must map over each pull (dict)
-    for pull in result:
+    for pull in response:
         ref = check_for_release_pr(pull)
         if ref != None:
             return ref
