4588f68cbecb2496027558a24485187df33b87bd
Maya Barnes <maya.barnes@snowflake.com>
2022-09-30 02:00:30 -0700

    Address nits

--- scripts/slack_notifications.py
+++ scripts/slack_notifications.py
@@ -42,26 +42,21 @@ def send_notification():
 
     if workflow == "nightly":
         failure = nightly_slack_messages[message_key]
-        message = {"text": ":blobonfire: Nightly build failed %s" % failure}
+        payload = {"text": f":blobonfire: Nightly build failed {failure}"}
 
     if workflow == "candidate":
         if message_key == "success":
-            message = {"text": ":rocket: Release Candidate was successful!"}
+            payload = {"text": ":rocket: Release Candidate was successful!"}
         else:
-            message = {"text": ":blobonfire: Release Candidate failed"}
+            payload = {"text": ":blobonfire: Release Candidate failed"}
 
-    payload = json.dumps(message)
-
-    response = requests.post(webhook, payload)
+    response = requests.post(webhook, json=payload)
 
     if response.status_code != 200:
-        raise Exception(
-            "Unable to send slack message, HTTP response: %s" % response.text
-        )
+        raise Exception(f"Unable to send slack message, HTTP response: {response.text}")
 
 
 def main():
-    """Run main loop."""
 
     send_notification()
 
