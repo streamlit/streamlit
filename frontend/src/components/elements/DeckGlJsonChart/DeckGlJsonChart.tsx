385cab104ba63af8fff78f1fbef790838ca593a2
Juan Martin Garcia <juan.garcia@snowflake.com>
2022-10-04 17:05:10 -0300

    Move the mapTheme variable outside the if condition

--- frontend/src/components/elements/DeckGlJsonChart/DeckGlJsonChart.tsx
+++ frontend/src/components/elements/DeckGlJsonChart/DeckGlJsonChart.tsx
@@ -146,11 +146,14 @@ export class DeckGlJsonChart extends PureComponent<PropsWithHeight, State> {
     const { element, width, height, theme } = props
     const json = JSON.parse(element.json)
 
+    // To determine the correct theme for both the map style (if unset),
+    // and the zoom buttons
+    const mapTheme = hasLightBackgroundColor(theme) ? "light" : "dark"
+    json.mapTheme = mapTheme
+
     // If unset, use either the Mapbox light or dark style based on Streamlit's theme
     // For Mapbox styles, see https://docs.mapbox.com/api/maps/styles/#mapbox-styles
     if (!notNullOrUndefined(json.mapStyle)) {
-      const mapTheme = hasLightBackgroundColor(theme) ? "light" : "dark"
-      json.mapTheme = mapTheme
       json.mapStyle = `mapbox://styles/mapbox/${mapTheme}-v9`
     }
 
