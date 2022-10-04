f47a908b57d54fda176f6d91f0715f763c2cda4a
lukasmasuch <lukas.masuch@gmail.com>
2022-10-04 19:04:43 +0200

    Add additional unit tests

--- lib/tests/streamlit/type_util_test.py
+++ lib/tests/streamlit/type_util_test.py
@@ -135,3 +135,23 @@ float            float64
 string            object
 dtype: object""",
         )
+
+    def test_data_frame_with_unsupported_column_types(self):
+        df = pd.DataFrame(
+            {
+                "mixed-integer": [1, "foo", 3],
+                "mixed": [1.0, "foo", 3],
+                "complex": [1 + 2j, 3 + 4j, 5 + 6 * 1j],
+                "integer": [1, 2, 3],
+                "float": [1.0, 2.1, 3.2],
+                "string": ["foo", "bar", None],
+            }
+        )
+
+        try:
+            data_frame_to_bytes(df)
+        except Exception as ex:
+            self.fail(
+                "No exception should have been thrown here. "
+                f"Unsupported types of this dataframe should have been automatically fixed: {ex}"
+            )
