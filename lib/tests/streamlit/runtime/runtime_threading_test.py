e619c29ca5664686e0909bf17755264670dee40e
Vincent Donato <vincent@streamlit.io>
2022-10-13 17:06:49 -0400

    Refactor current session behavior into WebsocketSessionManager (#5451)
    * Implement a barebones WebsocketSessionManager
    
    * Shut down all sessions on runtime shutdown

--- lib/tests/streamlit/runtime/runtime_threading_test.py
+++ lib/tests/streamlit/runtime/runtime_threading_test.py
@@ -45,7 +45,10 @@ class RuntimeThreadingTest(IsolatedAsyncioTestCase):
                 # so that the main thread can retrieve it safely. If Runtime
                 # creation fails, we'll stick an Exception in the queue instead.
                 config = RuntimeConfig(
-                    "mock/script/path.py", "", media_file_storage=MagicMock()
+                    "mock/script/path.py",
+                    "",
+                    media_file_storage=MagicMock(),
+                    session_manager_class=MagicMock(),
                 )
                 queue.put(Runtime(config))
             except BaseException as e:
