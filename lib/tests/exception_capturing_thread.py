50e4698cde3faa24f5ec5ec56b4cda3d04ad85ce
Tim Conkling <tconkling@gmail.com>
2022-10-13 12:01:12 -0700

    ExceptionCapturingThread

---
+++ lib/tests/exception_capturing_thread.py
@@ -0,0 +1,51 @@
+# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import threading
+from typing import Optional
+
+
+class ExceptionCapturingThread(threading.Thread):
+    """Thread subclass that captures unhandled exceptions."""
+
+    def __init__(
+        self, group=None, target=None, name=None, args=(), kwargs=None, *, daemon=None
+    ):
+        super().__init__(
+            group=group,
+            target=target,
+            name=name,
+            args=args,
+            kwargs=kwargs,
+            daemon=daemon,
+        )
+        self._unhandled_exception: Optional[BaseException] = None
+
+    @property
+    def unhandled_exception(self) -> Optional[BaseException]:
+        """The unhandled exception raised by the thread's target, if it raised one."""
+        return self._unhandled_exception
+
+    def assert_no_unhandled_exception(self):
+        """If the thread target raised an unhandled exception, re-raise it.
+        Otherwise no-op.
+        """
+        if self._unhandled_exception is not None:
+            raise self._unhandled_exception
+
+    def run(self) -> None:
+        try:
+            super().run()
+        except BaseException as e:
+            self._unhandled_exception = e
