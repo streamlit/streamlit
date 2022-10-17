0f6313dc315821a456df6d55d0537ffc2e159473
Tim Conkling <tconkling@gmail.com>
2022-10-17 12:38:56 -0700

    UploadedFileManagerTHreadingTests

--- lib/tests/streamlit/runtime/uploaded_file_manager_test.py
+++ lib/tests/streamlit/runtime/uploaded_file_manager_test.py
@@ -178,12 +178,12 @@ class UploadedFileManagerThreadingTest(unittest.TestCase):
         """`add_file` is thread-safe."""
         added_files = []
 
-        def add_file(ii: int) -> None:
+        def add_file(index: int) -> None:
             # Invent a new file and add it to the mgr
             file = UploadedFileRec(
-                id=0, name=f"file_{ii}", type="type", data=bytes(f"{ii}", "utf-8")
+                id=0, name=f"file_{index}", type="type", data=bytes(f"{index}", "utf-8")
             )
-            added_files.append(self.mgr.add_file("session", f"widget_{ii}", file))
+            added_files.append(self.mgr.add_file("session", f"widget_{index}", file))
 
         call_on_threads(add_file, num_threads=self.NUM_THREADS)
         for ii in range(self.NUM_THREADS):
@@ -225,6 +225,31 @@ class UploadedFileManagerThreadingTest(unittest.TestCase):
         call_on_threads(remove_file, self.NUM_THREADS)
 
         self.assertEqual(0, len(self.mgr.get_all_files("session", "widget")))
+
+    def test_remove_session_files(self):
+        """`remove_session_files` is thread-safe."""
+        # Add a bunch of files, each to a different session to a single widget
+        file_ids = []
+        for ii in range(self.NUM_THREADS):
+            file = UploadedFileRec(id=0, name=f"file_{ii}", type="type", data=b"123")
+            file_ids.append(self.mgr.add_file(f"session_{ii}", "widget", file).id)
+
+        # Have each thread remove its session's file
+        def remove_session_files(index: int) -> None:
+            session_id = f"session_{index}"
+            # Our file should exist
+            session_files = self.mgr.get_all_files(session_id, "widget")
+            self.assertEqual(1, len(session_files))
+            self.assertEqual(file_ids[index], session_files[0].id)
+
+            # Remove session files
+            self.mgr.remove_session_files(session_id)
+
+            # Our file should no longer exist
+            session_files = self.mgr.get_all_files(session_id, "widget")
+            self.assertEqual(0, len(session_files))
+
+        call_on_threads(remove_session_files, num_threads=self.NUM_THREADS)
 
     def test_remove_orphaned_files(self):
         """`remove_orphaned_files` is thread-safe."""
