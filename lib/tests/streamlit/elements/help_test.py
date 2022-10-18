77b8aa4505854ea79972049d983000632fccb87c
Karen Javadyan <kajarenc@gmail.com>
2022-10-18 18:48:38 +0400

    * Fix function signature test for python 3.7

--- lib/tests/streamlit/elements/help_test.py
+++ lib/tests/streamlit/elements/help_test.py
@@ -64,13 +64,25 @@ class StHelpTest(testutil.DeltaGeneratorTestCase):
         self.assertEqual("audio", ds.name)
         self.assertEqual("streamlit", ds.module)
         self.assertEqual("<class 'method'>", ds.type)
-        self.assertEqual(
-            "(data: Union[str, bytes, _io.BytesIO, io.RawIOBase, "
-            "_io.BufferedReader, ForwardRef('npt.NDArray[Any]'), NoneType], "
-            "format: str = 'audio/wav', start_time: int = 0, *, "
-            "sample_rate: Optional[int] = None) -> 'DeltaGenerator'",
-            ds.signature,
-        )
+
+        if sys.version_info < (3, 9):
+            # Python < 3.9 represents the signature slightly differently
+            self.assertEqual(
+                "(data: Union[str, bytes, _io.BytesIO, io.RawIOBase, "
+                "_io.BufferedReader, ForwardRef('npt.NDArray[Any]'), NoneType], "
+                "format: str = 'audio/wav', start_time: int = 0, *, "
+                "sample_rate: Union[int, NoneType] = None) -> 'DeltaGenerator'",
+                ds.signature,
+            )
+        else:
+            self.assertEqual(
+                "(data: Union[str, bytes, _io.BytesIO, io.RawIOBase, "
+                "_io.BufferedReader, ForwardRef('npt.NDArray[Any]'), NoneType], "
+                "format: str = 'audio/wav', start_time: int = 0, *, "
+                "sample_rate: Optional[int] = None) -> 'DeltaGenerator'",
+                ds.signature,
+            )
+
         self.assertTrue(ds.doc_string.startswith("Display an audio player"))
 
     def test_unwrapped_deltagenerator_func(self):
