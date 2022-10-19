f775ba92e379aeec58f4e7a095b9250463e94c2d
Karen Javadyan <kajarenc@gmail.com>
2022-10-19 15:07:41 +0400

    move assert calls outside of `with assertRaises` block, because it doesn't execute after captured exception in block

--- lib/tests/streamlit/elements/slider_test.py
+++ lib/tests/streamlit/elements/slider_test.py
@@ -262,8 +262,8 @@ class SliderTest(DeltaGeneratorTestCase):
     def test_label_visibility_wrong_value(self):
         with self.assertRaises(StreamlitAPIException) as e:
             st.slider("the label", label_visibility="wrong_value")
-            self.assertEquals(
-                str(e),
-                "Unsupported label_visibility option 'wrong_value'. Valid values are "
-                "'visible', 'hidden' or 'collapsed'.",
-            )
+        self.assertEquals(
+            str(e.exception),
+            "Unsupported label_visibility option 'wrong_value'. Valid values are "
+            "'visible', 'hidden' or 'collapsed'.",
+        )
