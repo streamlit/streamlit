395b5a60ab01e982f5c1425bacd20ba8b57f0166
lukasmasuch <lukas.masuch@gmail.com>
2022-10-20 21:05:30 +0200

    Fix codeql issues

--- lib/streamlit/web/server/routes.py
+++ lib/streamlit/web/server/routes.py
@@ -20,7 +20,7 @@ from streamlit import config, file_util
 from streamlit.logger import get_logger
 from streamlit.runtime.runtime_util import serialize_forward_msg
 
-LOGGER = get_logger(__name__)
+_LOGGER = get_logger(__name__)
 
 
 def allow_cross_origin_requests():
@@ -181,7 +181,7 @@ class MessageCacheHandler(tornado.web.RequestHandler):
         msg_hash = self.get_argument("hash", None)
         if msg_hash is None:
             # Hash is missing! This is a malformed request.
-            LOGGER.error(
+            _LOGGER.error(
                 "HTTP request for cached message is missing the hash attribute."
             )
             self.set_status(404)
@@ -190,14 +190,14 @@ class MessageCacheHandler(tornado.web.RequestHandler):
         message = self._cache.get_message(msg_hash)
         if message is None:
             # Message not in our cache.
-            LOGGER.error(
+            _LOGGER.error(
                 "HTTP request for cached message could not be fulfilled. "
-                f"No such message: {msg_hash}"
+                "No such message"
             )
             self.set_status(404)
             raise tornado.web.Finish()
 
-        LOGGER.debug(f"MessageCache HIT [hash={msg_hash}]")
+        _LOGGER.debug("MessageCache HIT")
         msg_str = serialize_forward_msg(message)
         self.set_header("Content-Type", "application/octet-stream")
         self.write(msg_str)
