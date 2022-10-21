1633e283ffabd394b1ba2b09564a64639f03e12b
willhuang1997 <willhuang1997@gmail.com>
2022-10-21 15:52:22 -0700

    Update e2e script based on test matrix for all customTheme methods

--- e2e/scripts/st_plotly_chart.py
+++ e2e/scripts/st_plotly_chart.py
@@ -17,8 +17,8 @@ from datetime import datetime
 import numpy as np
 import pandas as pd
 import plotly.express as px
+import plotly.figure_factory as ff
 import plotly.graph_objects as go
-from plotly import figure_factory
 
 import streamlit as st
 
@@ -36,24 +36,19 @@ group_labels = ["Group 1", "Group 2", "Group 3"]
 bin_size = [0.1, 0.25, 0.5]
 
 # Create distribution plot with custom bin_size
-chart = figure_factory.create_distplot(hist_data, group_labels, bin_size)
+chart = ff.create_distplot(hist_data, group_labels, bin_size)
 
 # Plot!
 st.plotly_chart(chart)
 
-# Histogram Plot
-df_2d_hist = px.data.tips()
-fig_2d_hist = px.density_heatmap(df_2d_hist, x="total_bill", y="tip")
-st.plotly_chart(fig_2d_hist, theme="streamlit")
-
-# Bar Cart
+# Bar Chart
 long_df_bar = px.data.medals_long()
 fig_bar = px.bar(
     long_df_bar, x="nation", y="count", color="medal", title="Long-Form Input"
 )
 st.plotly_chart(fig_bar, theme="streamlit")
 
-# Bubble Cart
+# Bubble Chart
 df_bubble = px.data.gapminder()
 fig_bubble = px.scatter(
     df_bubble.query("year==2007"),
@@ -92,35 +87,16 @@ fig_candlestick = go.Figure(
 )
 st.plotly_chart(fig_candlestick, theme="streamlit")
 
-# Dot Plot
-df_dot = px.data.medals_long()
-fig_dot = px.scatter(df_dot, y="nation", x="count", color="medal", symbol="medal")
-fig_dot.update_traces(marker_size=10)
-st.plotly_chart(fig_dot, theme="streamlit")
-
-# Filled Area Chart
-df_filled_area = px.data.gapminder()
-fig_filled_area = px.area(
-    df_filled_area, x="year", y="pop", color="continent", line_group="country"
-)
-st.plotly_chart(fig_filled_area, theme="streamlit")
-
-# Pie Chart
-df_pie = px.data.tips()
-fig_pie = px.pie(df_pie, values="tip", names="day")
-st.plotly_chart(fig_pie, theme="streamlit")
-
 # Sunburst Chart
-data_sunburst = dict(
-    character=["Eve", "Cain", "Seth", "Enos", "Noam", "Abel", "Awan", "Enoch", "Azura"],
-    parent=["", "Eve", "Eve", "Seth", "Seth", "Eve", "Eve", "Awan", "Eve"],
-    value=[10, 14, 12, 10, 2, 6, 6, 4, 4],
-)
+df = px.data.gapminder().query("year == 2007")
 fig_sunburst = px.sunburst(
-    data_sunburst,
-    names="character",
-    parents="parent",
-    values="value",
+    df,
+    path=["continent", "country"],
+    values="pop",
+    # setting color here to a string sets customdata attribute
+    color="lifeExp",
+    hover_data=["iso_alpha"],
+    color_continuous_midpoint=np.average(df["lifeExp"], weights=df["pop"]),
 )
 st.plotly_chart(fig_sunburst, theme="streamlit")
 
@@ -138,12 +114,7 @@ fig_contour = go.Figure(
 )
 st.plotly_chart(fig_contour, theme="streamlit")
 
-import numpy as np
-
 # Dist plot
-import plotly.figure_factory as ff
-
-# Add histogram data
 x1 = np.random.randn(200) - 2
 x2 = np.random.randn(200)
 x3 = np.random.randn(200) + 2
@@ -157,122 +128,6 @@ group_labels = ["Group 1", "Group 2", "Group 3", "Group 4"]
 # Create distplot with custom bin_size
 fig_dist = ff.create_distplot(hist_data, group_labels, bin_size=0.2)
 st.plotly_chart(fig_dist, theme="streamlit")
-
-# Funnel Chart
-fig_funnel = go.Figure()
-
-fig_funnel.add_trace(
-    go.Funnel(
-        name="Montreal",
-        y=["Website visit", "Downloads", "Potential customers", "Requested price"],
-        x=[120, 60, 30, 20],
-        textinfo="value+percent initial",
-    )
-)
-
-fig_funnel.add_trace(
-    go.Funnel(
-        name="Toronto",
-        orientation="h",
-        y=[
-            "Website visit",
-            "Downloads",
-            "Potential customers",
-            "Requested price",
-            "invoice sent",
-        ],
-        x=[100, 60, 40, 30, 20],
-        textposition="inside",
-        textinfo="value+percent previous",
-    )
-)
-
-fig_funnel.add_trace(
-    go.Funnel(
-        name="Vancouver",
-        orientation="h",
-        y=[
-            "Website visit",
-            "Downloads",
-            "Potential customers",
-            "Requested price",
-            "invoice sent",
-            "Finalized",
-        ],
-        x=[90, 70, 50, 30, 10, 5],
-        textposition="outside",
-        textinfo="value+percent total",
-    )
-)
-
-st.plotly_chart(fig_funnel, theme="streamlit")
-
-# Heatmap
-fig_heatmap = go.Figure(
-    data=go.Heatmap(
-        z=[[1, None, 30, 50, 1], [20, 1, 60, 80, 30], [30, 60, 1, -10, 20]],
-        x=["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"],
-        y=["Morning", "Afternoon", "Evening"],
-        hoverongaps=False,
-    )
-)
-st.plotly_chart(fig_heatmap, theme="streamlit")
-
-# Icicle Chart
-fig_icicle = go.Figure(
-    go.Icicle(
-        labels=[
-            "Eve",
-            "Cain",
-            "Seth",
-            "Enos",
-            "Noam",
-            "Abel",
-            "Awan",
-            "Enoch",
-            "Azura",
-        ],
-        parents=["", "Eve", "Eve", "Seth", "Seth", "Eve", "Eve", "Awan", "Eve"],
-        values=[65, 14, 12, 10, 2, 6, 6, 4, 4],
-        branchvalues="total",
-        root_color="lightgrey",
-    )
-)
-
-# Treemaps Chart
-vendors = ["A", "B", "C", "D", None, "E", "F", "G", "H", None]
-sectors = [
-    "Tech",
-    "Tech",
-    "Finance",
-    "Finance",
-    "Other",
-    "Tech",
-    "Tech",
-    "Finance",
-    "Finance",
-    "Other",
-]
-regions = [
-    "North",
-    "North",
-    "North",
-    "North",
-    "North",
-    "South",
-    "South",
-    "South",
-    "South",
-    "South",
-]
-sales = [1, 3, 2, 4, 1, 2, 2, 1, 4, 1]
-df = pd.DataFrame(dict(vendors=vendors, sectors=sectors, regions=regions, sales=sales))
-df["all"] = "all"  # in order to have a single root node
-fig_icicle = px.treemap(
-    df, path=["all", "regions", "sectors", "vendors"], values="sales"
-)
-
-st.plotly_chart(fig_icicle, theme="streamlit")
 
 # Waterfall Chart
 fig_waterfall = go.Figure(
