70eeab965f3b83263f29377169423eb38b7e3a02
Karen Javadyan <kajarenc@gmail.com>
2022-10-24 17:50:06 +0400

    * add explanatory comment for local wave import

--- lib/streamlit/elements/media.py
+++ lib/streamlit/elements/media.py
@@ -341,6 +341,8 @@ def _make_wav(data: "npt.NDArray[Any]", sample_rate: int) -> bytes:
     We use code from IPython display module to convert numpy array to wave bytes
     https://github.com/ipython/ipython/blob/1015c392f3d50cf4ff3e9f29beede8c1abfdcb2a/IPython/lib/display.py#L146
     """
+    # we import wave here locally to import it only when needed (when numpy array given
+    # to st.audio data)
     import wave
 
     scaled, nchan = _validate_and_normalize(data)
