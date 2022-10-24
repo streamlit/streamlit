cb0fe62f1949a0aeb1f5500d9b188d97cfce8f35
Karen Javadyan <kajarenc@gmail.com>
2022-10-24 17:58:08 +0400

    add docstring for _maybe_convert_to_wav_bytes function

--- lib/streamlit/elements/media.py
+++ lib/streamlit/elements/media.py
@@ -359,6 +359,7 @@ def _make_wav(data: "npt.NDArray[Any]", sample_rate: int) -> bytes:
 def _maybe_convert_to_wav_bytes(
     data: MediaData, sample_rate: Optional[int]
 ) -> MediaData:
+    """Convert data to wav bytes if the data type is numpy array."""
     if type_util.is_type(data, "numpy.ndarray") and sample_rate is not None:
         data = _make_wav(cast("npt.NDArray[Any]", data), sample_rate)
     return data
