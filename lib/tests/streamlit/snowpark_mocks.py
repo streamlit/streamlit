082a7bae43410d751efeb0ced6832b2782bea4b2
Tomasz Szerszen <tomasz.szerszen@snowflake.com>
2022-10-25 15:28:15 +0200

    Snowpark integration: Add support for uncollected dataframes in st.map and improve st.table data collection (#5590)

--- lib/tests/streamlit/snowpark_mocks.py
+++ lib/tests/streamlit/snowpark_mocks.py
@@ -16,16 +16,16 @@
 import random
 from typing import List
 
+import numpy as np
+import pandas as pd
 
-class DataFrame:
-    """This is dummy DataFrame class,
-    which imitates snowflake.snowpark.dataframe.DataFrame class
-    for testing purposes."""
 
-    __module__ = "snowflake.snowpark.dataframe"
-
-    def __init__(self, num_of_rows: int = 50000, num_of_cols: int = 4):
+class _SnowparkDataLikeBaseClass:
+    def __init__(
+        self, is_map: bool = False, num_of_rows: int = 50000, num_of_cols: int = 4
+    ):
         self._data = None
+        self._is_map = is_map
         self._num_of_rows = num_of_rows
         self._num_of_cols = num_of_cols
 
@@ -33,22 +33,31 @@ class DataFrame:
         return self._num_of_rows
 
     def take(self, n: int):
-        """Returns n element of fake DataFrame, which imitates take of snowflake.snowpark.dataframe.DataFrame"""
+        """Returns n element of fake Data like, which imitates take of snowflake.snowpark.dataframe.DataFrame"""
         self._lazy_evaluation()
         if n > self._num_of_rows:
             n = self._num_of_rows
         return self._data[:n]
 
     def collect(self) -> List[List[int]]:
-        """Returns fake DataFrame, which imitates collection of snowflake.snowpark.dataframe.DataFrame"""
+        """Returns fake Data like, which imitates collection of snowflake.snowpark.dataframe.DataFrame"""
         self._lazy_evaluation()
         return self._data
 
     def _lazy_evaluation(self):
-        """Sometimes we don't need data inside DataFrame class, so we populate it once and only when necessary"""
+        """Sometimes we don't need data inside Data like class, so we populate it once and only when necessary"""
         if self._data is None:
-            random.seed(0)
-            self._data = self._random_data()
+            if self._is_map:
+                self._data = pd.DataFrame(
+                    (
+                        np.random.randn(self._num_of_rows, 2) / [50, 50]
+                        + [37.76, -122.4]
+                    ),
+                    columns=["lat", "lon"],
+                )
+            else:
+                random.seed(0)
+                self._data = self._random_data()
 
     def _random_data(self) -> List[List[int]]:
         data: List[List[int]] = []
@@ -61,6 +70,36 @@ class DataFrame:
         for _ in range(0, self._num_of_cols):
             row.append(random.randint(1, 1000000))
         return row
+
+
+class DataFrame(_SnowparkDataLikeBaseClass):
+    """This is dummy DataFrame class,
+    which imitates snowflake.snowpark.dataframe.DataFrame class
+    for testing purposes."""
+
+    __module__ = "snowflake.snowpark.dataframe"
+
+    def __init__(
+        self, is_map: bool = False, num_of_rows: int = 50000, num_of_cols: int = 4
+    ):
+        super(DataFrame, self).__init__(
+            is_map=is_map, num_of_rows=num_of_rows, num_of_cols=num_of_cols
+        )
+
+
+class Table(_SnowparkDataLikeBaseClass):
+    """This is dummy Table class,
+    which imitates snowflake.snowpark.dataframe.DataFrame class
+    for testing purposes."""
+
+    __module__ = "snowflake.snowpark.table"
+
+    def __init__(
+        self, is_map: bool = False, num_of_rows: int = 50000, num_of_cols: int = 4
+    ):
+        super(Table, self).__init__(
+            is_map=is_map, num_of_rows=num_of_rows, num_of_cols=num_of_cols
+        )
 
 
 class Row:
