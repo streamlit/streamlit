10867cb2b893879ee8c56c38121b1d7a9ffec88b
lukasmasuch <lukas.masuch@gmail.com>
2022-10-25 16:09:37 +0200

    Fix typing issue

--- lib/streamlit/elements/utils.py
+++ lib/streamlit/elements/utils.py
@@ -79,7 +79,7 @@ def check_session_state_rules(
 
 def get_label_visibility_proto_value(
     label_visibility_string: type_util.LabelVisibility,
-) -> LabelVisibilityMessage.LabelVisibilityOptions:
+) -> LabelVisibilityMessage.LabelVisibilityOptions.ValueType:
     """Returns one of LabelVisibilityMessage enum constants based on string value."""
 
     if label_visibility_string == "visible":
