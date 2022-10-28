1c75f7bf183fe5d5978c597657fb303ae562f798
Maya Barnes <maya8617@gmail.com>
2022-10-28 11:01:26 -0700

    Add timeout > 10000 for graphviz

--- e2e/specs/st_graphviz_chart.spec.js
+++ e2e/specs/st_graphviz_chart.spec.js
@@ -18,8 +18,9 @@ describe("st.graphviz_chart", () => {
   before(() => {
     cy.loadApp("http://localhost:3000/");
 
+    // Running status widget often concludes before charts loaded
     // Wait until charts are no longer loading
-    cy.get('.stAlert', { timeout: 10000 }).should('not.exist');
+    cy.get('.stAlert', { timeout: 15000 }).should('not.exist');
 
     cy.prepForElementSnapshots();
   });
