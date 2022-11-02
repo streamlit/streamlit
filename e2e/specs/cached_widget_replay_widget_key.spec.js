a0b61871b4b5b9e3fbb7a4d9879048ed6ad20a75
Amanda Walker <amanda@amandawalker.io>
2022-11-01 17:20:29 -0700

    More specific button selection

--- e2e/specs/cached_widget_replay_widget_key.spec.js
+++ e2e/specs/cached_widget_replay_widget_key.spec.js
@@ -22,7 +22,7 @@ describe("widget replay cache selection", () => {
   it("runs cached function with new widget values", () => {
     cy.get(".stRadio").should("have.length", 1);
     cy.get("[data-testid='stText']").should("have.text", "['function ran']");
-    cy.get(".stButton").first().click();
+    cy.get(".stButton button").first().click();
     cy.get("[data-testid='stStatusWidget']", { timeout: 10000 }).should(
       "exist"
     );
@@ -35,7 +35,7 @@ describe("widget replay cache selection", () => {
     cy.get(".stRadio").first().find("input").last().click({ force: true });
     cy.get(".stRadio").should("have.length", 1);
     cy.get("[data-testid='stText']").should("have.text", "['function ran']");
-    cy.get(".stButton").first().click();
+    cy.get(".stButton button").first().click();
     cy.get("[data-testid='stStatusWidget']", { timeout: 10000 }).should(
       "exist"
     );
