4a9f7c0953aa557e18c6d965caa97ad0900dd796
Vincent Donato <vincent@streamlit.io>
2022-11-04 11:18:02 -0700

    Replace assertEquals->assertEqual to silence deprecation warnings (#5648)

--- lib/tests/streamlit/elements/text_input_test.py
+++ lib/tests/streamlit/elements/text_input_test.py
@@ -163,7 +163,7 @@ class TextInputTest(DeltaGeneratorTestCase):
     def test_label_visibility_wrong_value(self):
         with self.assertRaises(StreamlitAPIException) as e:
             st.text_input("the label", label_visibility="wrong_value")
-        self.assertEquals(
+        self.assertEqual(
             str(e.exception),
             "Unsupported label_visibility option 'wrong_value'. Valid values are "
             "'visible', 'hidden' or 'collapsed'.",
