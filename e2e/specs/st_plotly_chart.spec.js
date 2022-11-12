b133944be1bb1e19d6ccdd0b5d632595e4b6ece1
willhuang1997 <willhuang1997@gmail.com>
2022-11-11 16:18:48 -0800

    Fix cypress test since I added a new test

--- e2e/specs/st_plotly_chart.spec.js
+++ e2e/specs/st_plotly_chart.spec.js
@@ -20,7 +20,7 @@ describe("st.plotly_chart", () => {
   });
 
   beforeEach(() => {
-    cy.get(".element-container").should("have.length", 12);
+    cy.get(".element-container").should("have.length", 13);
   });
 
   it("displays a plotly chart", () => {
