fc6b838cf4e9ad67c4bb1bf9334d960f18a84562
willhuang1997 <willhuang1997@gmail.com>
2022-11-14 22:48:13 -0800

    Delete snapshots and change tests

--- e2e/scripts/st_arrow_vega_lite_chart.py
+++ e2e/scripts/st_arrow_vega_lite_chart.py
@@ -76,10 +76,10 @@ interactive_spec = {
     "config": {"scale": {"bandPaddingInner": 0.2}},
 }
 
-st._arrow_vega_lite_chart(df, spec, use_container_width=True)
-st._arrow_vega_lite_chart(df, spec, use_container_width=True)
-st._arrow_vega_lite_chart(df, spec)
-st._arrow_vega_lite_chart(df, spec_with_width)
+st._arrow_vega_lite_chart(df, spec, use_container_width=True, theme="streamlit")
+st._arrow_vega_lite_chart(df, spec, use_container_width=True, theme="streamlit")
+st._arrow_vega_lite_chart(df, spec, theme="streamlit")
+st._arrow_vega_lite_chart(df, spec_with_width, theme="streamlit")
 st._arrow_vega_lite_chart(interactive_spec, None)
 
 # Screenshot comparison
@@ -100,6 +100,7 @@ st._arrow_vega_lite_chart(
         },
     },
     use_container_width=True,
+    theme="streamlit",
 )
 
 st.write("Using a top-level `df` and keywords as a spec:")
@@ -112,6 +113,7 @@ st._arrow_vega_lite_chart(
     y_field="b",
     y_type="quantitative",
     use_container_width=True,
+    theme="streamlit",
 )
 
 st.write("Putting the `df` inside the spec, as a `dataset`:")
@@ -127,6 +129,7 @@ st._arrow_vega_lite_chart(
         },
     },
     use_container_width=True,
+    theme="streamlit",
 )
 
 st.write("Putting the `df` inside the spec, as inline `data`:")
@@ -141,6 +144,7 @@ st._arrow_vega_lite_chart(
         },
     },
     use_container_width=True,
+    theme="streamlit",
 )
 
 st.write("Putting the `df` inside the spec, as inline `data` (different notation):")
@@ -153,7 +157,8 @@ st._arrow_vega_lite_chart(
             "x": {"field": "a", "type": "ordinal"},
             "y": {"field": "b", "type": "quantitative"},
         },
-    }
+    },
+    theme="streamlit",
 )
 
 df = pd.DataFrame(data, columns=["a", "b", "c"])
