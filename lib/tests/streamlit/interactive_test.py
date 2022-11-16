6041f5d9925c26b2c6741e4e440d04bf4e981d4f
Amanda Walker <amanda@amandawalker.io>
2022-11-16 11:15:27 -0800

    Remove unused annotations import

--- lib/tests/streamlit/interactive_test.py
+++ lib/tests/streamlit/interactive_test.py
@@ -11,8 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from __future__ import annotations
-
 import unittest
 from unittest.mock import MagicMock, patch
 
