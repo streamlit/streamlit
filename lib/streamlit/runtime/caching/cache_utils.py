9fc9961756bc8169ce4d1a1a403eadaf1d99a85d
Amanda Walker <amanda@amandawalker.io>
2022-11-18 11:58:24 -0800

    Hoist element message data creation out of loop
    We only want to reset the callstack's local data after we are done
    adding the widget/media element to the message list for each cached
    function we are running in. Moving the creation of the message data
    class out of the loop, and saving it to each of the lists, accomplishes
    this with the bonus of saving some memory use.

--- lib/streamlit/runtime/caching/cache_utils.py
+++ lib/streamlit/runtime/caching/cache_utils.py
@@ -599,39 +599,39 @@ class CacheMessagesCallStack(threading.local):
             return
 
         id_to_save = self.select_dg_to_save(invoked_dg_id, used_dg_id)
+        if isinstance(element_proto, Widget):
+            wid = element_proto.id
+            # TODO replace `Message` with a more precise type
+            if not self._registered_metadata:
+                _LOGGER.error(
+                    "Trying to save widget message that wasn't registered. This should not be possible."
+                )
+                raise AttributeError
+            widget_meta = WidgetMsgMetadata(
+                wid, None, metadata=self._registered_metadata
+            )
+        else:
+            widget_meta = None
+
+        media_data = self._media_data
+
+        element_msg_data = ElementMsgData(
+            delta_type,
+            element_proto,
+            id_to_save,
+            returned_dg_id,
+            widget_meta,
+            media_data,
+        )
         for msgs in self._cached_message_stack:
-            if isinstance(element_proto, Widget):
-                wid = element_proto.id
-                # TODO replace `Message` with a more precise type
-                if not self._registered_metadata:
-                    _LOGGER.error(
-                        "Trying to save widget message that wasn't registered. This should not be possible."
-                    )
-                    raise AttributeError
-                widget_meta = WidgetMsgMetadata(
-                    wid, None, metadata=self._registered_metadata
-                )
-            else:
-                widget_meta = None
-
-            media_data = self._media_data
-
             if self._allow_widgets or widget_meta is None:
-                msgs.append(
-                    ElementMsgData(
-                        delta_type,
-                        element_proto,
-                        id_to_save,
-                        returned_dg_id,
-                        widget_meta,
-                        media_data,
-                    )
-                )
+                msgs.append(element_msg_data)
 
-            # Reset instance state, now that it has been used for the
-            # associated element.
-            self._media_data = []
-            self._registered_metadata = None
+        # Reset instance state, now that it has been used for the
+        # associated element.
+        self._media_data = []
+        self._registered_metadata = None
+
         for s in self._seen_dg_stack:
             s.add(returned_dg_id)
 
