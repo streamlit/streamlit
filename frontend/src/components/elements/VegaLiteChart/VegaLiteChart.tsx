435630259bcab0356e95ce3949146fe70941ac92
willhuang1997 <willhuang1997@gmail.com>
2022-11-28 22:31:11 -0800

    Revert "Add vega-interpreter to Vega Lite Charts (#5462)"
    This reverts commit 9e9c4bc48001080b693decb9a41c0db610497265.

--- frontend/src/components/elements/VegaLiteChart/VegaLiteChart.tsx
+++ frontend/src/components/elements/VegaLiteChart/VegaLiteChart.tsx
@@ -29,7 +29,6 @@ import { ensureError } from "src/lib/ErrorHandling"
 import { Theme } from "src/theme"
 import embed from "vega-embed"
 import * as vega from "vega"
-import { expressionInterpreter } from "vega-interpreter"
 import { StyledVegaLiteChartContainer } from "./styled-components"
 
 const MagicFields = {
@@ -284,13 +283,7 @@ export class VegaLiteChart extends PureComponent<PropsWithHeight, State> {
 
     const el = this.props.element
     const spec = this.generateSpec()
-    const options = {
-      // Adds interpreter support for Vega expressions that is compliant with CSP
-      ast: true,
-      expr: expressionInterpreter,
-    }
-
-    const { vgSpec, view, finalize } = await embed(this.element, spec, options)
+    const { vgSpec, view, finalize } = await embed(this.element, spec)
 
     this.vegaView = view
     this.vegaFinalizer = finalize
