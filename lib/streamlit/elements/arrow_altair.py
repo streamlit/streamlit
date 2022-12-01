d633bd786592d556f9a4a880ef24c9c59f6119b7
willhuang1997 <willhuang1997@gmail.com>
2022-11-30 22:24:57 -0800

    Turn on streamlit theme for altair and plotly

--- lib/streamlit/elements/arrow_altair.py
+++ lib/streamlit/elements/arrow_altair.py
@@ -273,7 +273,7 @@ class ArrowAltairMixin:
         self,
         altair_chart: Chart,
         use_container_width: bool = False,
-        theme: Union[None, Literal["streamlit"]] = None,
+        theme: Union[None, Literal["streamlit"]] = "streamlit",
     ) -> "DeltaGenerator":
         """Display a chart using the Altair library.
 
@@ -567,7 +567,7 @@ def marshall(
     vega_lite_chart: ArrowVegaLiteChartProto,
     altair_chart: Chart,
     use_container_width: bool = False,
-    theme: Union[None, Literal["streamlit"]] = None,
+    theme: Union[None, Literal["streamlit"]] = "streamlit",
     **kwargs: Any,
 ) -> None:
     """Marshall chart's data into proto."""
