17fe8921838d2ea9274e6e4c31b7ebb65110dd7d
lukasmasuch <lukas.masuch@gmail.com>
2023-01-14 00:51:56 +0100

    Don't convert value to string in error

--- frontend/src/components/widgets/DataFrame/columns/TextColumn.ts
+++ frontend/src/components/widgets/DataFrame/columns/TextColumn.ts
@@ -56,8 +56,9 @@ function TextColumn(props: BaseColumnProps): BaseColumn {
           displayData,
         } as TextCell
       } catch (error) {
+        // This should never happen, but if it does, we want to show an error
         return getErrorCell(
-          toSafeString(data),
+          "Incompatible value",
           `The value cannot be interpreted as string. Error: ${error}`
         )
       }
