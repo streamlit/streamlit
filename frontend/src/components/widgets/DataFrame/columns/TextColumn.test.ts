bc5624a1d8052baffd67a09474e0e7cb45b97794
lukasmasuch <lukas.masuch@gmail.com>
2023-01-14 00:52:14 +0100

    Add tests for boolean and text column

---
+++ frontend/src/components/widgets/DataFrame/columns/TextColumn.test.ts
@@ -0,0 +1,71 @@
+/**
+ * Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { GridCellKind } from "@glideapps/glide-data-grid"
+
+import TextColumn from "./TextColumn"
+import { TextCell } from "@glideapps/glide-data-grid"
+
+const MOCK_TEXT_COLUMN_PROPS = {
+  id: "1",
+  title: "Text column",
+  indexNumber: 0,
+  isEditable: false,
+  isHidden: false,
+  isIndex: false,
+  isStretched: false,
+  arrowType: {
+    pandas_type: "unicode",
+    numpy_type: "object",
+  },
+}
+
+class SomeObject {}
+
+describe("TextColumn", () => {
+  it("creates a valid column instance", () => {
+    const mockColumn = TextColumn(MOCK_TEXT_COLUMN_PROPS)
+    expect(mockColumn.title).toEqual(MOCK_TEXT_COLUMN_PROPS.title)
+    expect(mockColumn.id).toEqual(MOCK_TEXT_COLUMN_PROPS.id)
+
+    const mockCell = mockColumn.getCell("foo")
+    expect(mockCell.kind).toEqual(GridCellKind.Text)
+    expect((mockCell as TextCell).data).toEqual("foo")
+  })
+
+  it.each([
+    ["foo", "foo"],
+    ["abc def 1234 $", "abc def 1234 $"],
+    [1, "1"],
+    [0, "0"],
+    [0.123, "0.123"],
+    ["", ""],
+    [[], ""],
+    [["foo", "bar"], "foo,bar"],
+    [[1, 2, 0.1231], "1,2,0.1231"],
+    [true, "true"],
+    [new SomeObject(), "[object Object]"],
+    [null, null],
+    [undefined, null],
+  ])(
+    "supports string-compatible value (%p parsed as %p)",
+    (input: any, value: string | null) => {
+      const mockColumn = TextColumn(MOCK_TEXT_COLUMN_PROPS)
+      const cell = mockColumn.getCell(input)
+      expect(mockColumn.getCellValue(cell)).toEqual(value)
+    }
+  )
+})
