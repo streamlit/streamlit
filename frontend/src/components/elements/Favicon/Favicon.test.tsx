bb7c895c2621b235bb862bafe37ad70c873804e4
Zachary Blackwood <zachary@streamlit.io>
2023-01-20 18:27:13 -0500

    Switch to new functioning twemoji url (#5943)
    * Switch to new functioning twemoji url
    
    * Linting
    
    * Remove space for prettier

--- frontend/src/components/elements/Favicon/Favicon.test.tsx
+++ frontend/src/components/elements/Favicon/Favicon.test.tsx
@@ -25,10 +25,12 @@ function getFaviconHref(): string {
 
 document.head.innerHTML = `<link rel="shortcut icon" href="default.png">`
 
-const PIZZA_TWEMOJI_URL = "https://twemoji.maxcdn.com/2/72x72/1f355.png"
-const SATELLITE_TWEMOJI_URL = "https://twemoji.maxcdn.com/2/72x72/1f6f0.png"
+const PIZZA_TWEMOJI_URL =
+  "https://cdn.jsdelivr.net/gh/twitter/twemoji@14.0.2/assets/72x72/1f355.png"
+const SATELLITE_TWEMOJI_URL =
+  "https://cdn.jsdelivr.net/gh/twitter/twemoji@14.0.2/assets/72x72/1f6f0.png"
 const CRESCENT_MOON_TWEMOJI_URL =
-  "https://twemoji.maxcdn.com/2/72x72/1f319.png"
+  "https://cdn.jsdelivr.net/gh/twitter/twemoji@14.0.2/assets/72x72/1f319.png"
 
 test("is set up with the default favicon", () => {
   expect(getFaviconHref()).toBe("http://localhost/default.png")
