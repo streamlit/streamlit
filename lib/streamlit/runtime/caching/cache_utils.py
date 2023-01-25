060694e35ba7201f7bde1ec094e4ba06954e5c87
Tim Conkling <tconkling@gmail.com>
2023-01-25 13:35:19 -0800

    fix a docstring

--- lib/streamlit/runtime/caching/cache_utils.py
+++ lib/streamlit/runtime/caching/cache_utils.py
@@ -12,7 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Common cache logic shared by st.memo and st.singleton."""
+"""Common cache logic shared by st.cache_data and st.cache_resource."""
+
 from __future__ import annotations
 
 import functools
