d665382ac387b0f5b444e317533e34c81f0fd82d
lukasmasuch <lukas.masuch@gmail.com>
2023-01-30 20:39:40 +0100

    Remove default timeout for loading

--- e2e/specs/st_arrow_dataframe_column_types.js
+++ e2e/specs/st_arrow_dataframe_column_types.js
@@ -16,10 +16,6 @@
 
 describe("st.dataframe and st.data_editor support a variety of column types", () => {
   before(() => {
-    // Increasing timeout since we're waiting for
-    // dataframe tables to be rendered.
-    Cypress.config("defaultCommandTimeout", 15000);
-
     cy.loadApp("http://localhost:3000/");
     cy.prepForElementSnapshots();
   });
