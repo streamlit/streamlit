3606c8c68876b40cb15167fb11c85ccac0dc151c
Kamil Bregula <kamil.bregula@snowflake.com>
2023-02-06 13:49:32 +0100

    fixup! Allow control of st.pyplot() width

--- lib/tests/streamlit/elements/pyplot_test.py
+++ lib/tests/streamlit/elements/pyplot_test.py
@@ -91,7 +91,7 @@ class PyplotTest(DeltaGeneratorTestCase):
             else:
                 fig_clf.assert_not_called()
 
-    @parameterized.expand([(True, image.ORIGINAL_WIDTH), (False, image.COLUMN_WIDTH)])
+    @parameterized.expand([(True, image.COLUMN_WIDTH), (False, image.ORIGINAL_WIDTH)])
     def test_st_pyplot_use_container_width(
         self, use_container_width: bool, image_width: int
     ):
