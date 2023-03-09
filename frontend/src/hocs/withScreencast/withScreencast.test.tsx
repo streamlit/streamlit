71e7e42a56e3d4fd1d9d26f56b83b99e1f7da33b
Tim Conkling <tconkling@gmail.com>
2023-03-09 15:26:47 -0800

    withScreencast: types

--- frontend/src/hocs/withScreencast/withScreencast.test.tsx
+++ frontend/src/hocs/withScreencast/withScreencast.test.tsx
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-import React, { ComponentType } from "react"
+import React, { PureComponent, ReactElement } from "react"
 import ScreenCastRecorder from "src/lib/ScreenCastRecorder"
 import { shallow } from "src/lib/test_util"
 
@@ -28,43 +28,54 @@ import {
 
 jest.mock("src/lib/ScreenCastRecorder")
 
-const testComponent: ComponentType = () => <div>test</div>
+interface TestProps {
+  screenCast: ScreenCastHOC
+  label: string
+}
+
+class TestComponent extends PureComponent<TestProps> {
+  public render = (): ReactElement => <div>test</div>
+}
+
+const WrappedTestComponent = withScreencast(TestComponent)
 
 describe("withScreencast HOC", () => {
   it("renders without crashing", () => {
-    const WithHoc = withScreencast(testComponent)
-    const wrapper = shallow(<WithHoc />)
-
+    const wrapper = shallow(<WrappedTestComponent label={"mockLabel"} />)
     expect(wrapper.html()).not.toBeNull()
   })
 
   it("wrapped component should have screenCast prop", () => {
-    const WithHoc = withScreencast(testComponent)
-    const wrapper = shallow(<WithHoc />)
+    const wrapper = shallow(<WrappedTestComponent label={"mockLabel"} />)
+    expect(wrapper.find(TestComponent).props().screenCast).toBeDefined()
+  })
 
-    // @ts-ignore
-    expect(wrapper.find(testComponent).props().screenCast).toBeDefined()
+  it("passes other props to wrapped component", () => {
+    const wrapper = shallow(<WrappedTestComponent label={"mockLabel"} />)
+    expect(wrapper.find(TestComponent).props().label).toBe("mockLabel")
+  })
+
+  it("defines displayName", () => {
+    expect(WrappedTestComponent.displayName).toBe(
+      "withScreencast(TestComponent)"
+    )
   })
 
   describe("Steps", () => {
-    const WithHoc = withScreencast(testComponent)
-    const wrapper = shallow(<WithHoc />)
+    const wrapper = shallow(<WrappedTestComponent label={"mockLabel"} />)
 
-    // @ts-ignore
     ScreenCastRecorder.isSupportedBrowser = () => true
 
-    // @ts-ignore
     wrapper
-      .find(testComponent)
+      .find(TestComponent)
       .props()
-      // @ts-ignore
       .screenCast.startRecording("screencast-filename")
 
-    it("should show a configuration dialog before start recording", () => {
+    it("shows a configuration dialog before start recording", () => {
       expect(wrapper.find(ScreencastDialog).length).toBe(1)
     })
 
-    it("should show a countdown after setup", async () => {
+    it("shows a countdown after setup", async () => {
       await wrapper.find(ScreencastDialog).props().startRecording()
 
       const countdownWrapper = wrapper.find(Countdown)
@@ -72,27 +83,22 @@ describe("withScreencast HOC", () => {
       expect(countdownWrapper.length).toBe(1)
     })
 
-    it("should be on recording state after countdown", async () => {
+    it("is in recording state after countdown", async () => {
       const countdownWrapper = wrapper.find(Countdown)
 
-      // @ts-ignore
+      // @ts-expect-error
       wrapper.instance().recorder.start = jest.fn().mockReturnValue(true)
 
       await countdownWrapper.props().endCallback()
 
-      const wrappedComponentProps: {
-        screenCast: ScreenCastHOC
-      } = wrapper.find(testComponent).props() as any
-
+      const wrappedComponentProps = wrapper.find(TestComponent).props()
       expect(wrappedComponentProps.screenCast.currentState).toBe("RECORDING")
     })
 
-    it("should show recorded dialog after recording", async () => {
-      const wrappedComponentProps: {
-        screenCast: ScreenCastHOC
-      } = wrapper.find(testComponent).props() as any
+    it("shows recorded dialog after recording", async () => {
+      const wrappedComponentProps = wrapper.find(TestComponent).props()
 
-      // @ts-ignore
+      // @ts-expect-error
       wrapper.instance().recorder.stop = jest
         .fn()
         .mockReturnValue(new Blob([]))
@@ -104,37 +110,30 @@ describe("withScreencast HOC", () => {
     })
   })
 
-  it("should show an unsupported dialog when it's an unsupported browser", () => {
-    const WithHoc = withScreencast(testComponent)
-    const wrapper = shallow(<WithHoc />)
+  it("shows an unsupported dialog when it's an unsupported browser", () => {
+    const wrapper = shallow(<WrappedTestComponent label={"mockLabel"} />)
 
-    // @ts-ignore
     ScreenCastRecorder.isSupportedBrowser = () => false
 
-    // @ts-ignore
     wrapper
-      .find(testComponent)
+      .find(TestComponent)
       .props()
-      // @ts-ignore
       .screenCast.startRecording("screencast-filename")
 
     expect(wrapper.find(UnsupportedBrowserDialog).length).toBe(1)
   })
 
-  it("should show an unsupported dialog when it doesn't have a mediaDevices support", () => {
-    const WithHoc = withScreencast(testComponent)
-    const wrapper = shallow(<WithHoc />)
+  it("shows an unsupported dialog when it doesn't have a mediaDevices support", () => {
+    const wrapper = shallow(<WrappedTestComponent label={"mockLabel"} />)
 
     Object.defineProperty(window.navigator, "mediaDevices", {
       value: undefined,
       configurable: true,
     })
 
-    // @ts-ignore
     wrapper
-      .find(testComponent)
+      .find(TestComponent)
       .props()
-      // @ts-ignore
       .screenCast.startRecording("screencast-filename")
 
     expect(wrapper.find(UnsupportedBrowserDialog).length).toBe(1)
