125fb720ee3ebe9365c677c38517706c40e49c0e
Maya Barnes <63436329+mayagbarnes@users.noreply.github.com>
2023-03-14 17:32:32 -0700

    Add react-dropzone prop to address disabled file type behavior (#6315)
    Updating to react-dropzone v12 caused st.file_uploader's File Selection Dialog to show all file types as available for selection, despite the extensions not being included in the type param. This overrides default use of File System Access API for react-dropzone. Additionally, .showOpenFilePicker() is still experimental and is not compatible with Firefox & Safari.

--- frontend/src/components/widgets/FileUploader/FileDropzone.tsx
+++ frontend/src/components/widgets/FileUploader/FileDropzone.tsx
@@ -44,6 +44,9 @@ const FileDropzone = ({
     accept={acceptedExtensions.length ? acceptedExtensions : undefined}
     maxSize={maxSizeBytes}
     disabled={disabled}
+    // react-dropzone v12+ uses the File System Access API by default,
+    // causing the bug described in https://github.com/streamlit/streamlit/issues/6176.
+    useFsAccessApi={false}
   >
     {({ getRootProps, getInputProps }) => (
       <StyledFileDropzoneSection
