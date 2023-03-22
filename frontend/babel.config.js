ad3764ac4057ecff60be5592ba1e41f00fa2213a
willhuang1997 <willhuang1997@gmail.com>
2023-03-22 14:28:37 -0700

    Remove unnecessary file

--- frontend/babel.config.js
+++
@@ -1,8 +0,0 @@
-console.log("Using custom babel file to preserve modules and other things!")
-module.exports = {
-  presets: [
-    ['./scripts/babel-preset-dev-env.js'],
-  ],
-  plugins: ['@emotion'],
-  ignore: ['./src/autogen/**', '**/*.test.ts', '**/*.test.tsx', '**/*.d.ts'],
-}
