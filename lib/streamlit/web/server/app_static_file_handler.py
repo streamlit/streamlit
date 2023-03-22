37f0600a9276e81f3fa174cb6ac6dec2ad2b3ffe
Karen Javadyan <kajarenc@gmail.com>
2023-03-22 20:01:21 +0400

    Add `.webp` to list of safe static file extensions (#6331)
    Add `.webp` to list of SAFE_APP_STATIC_FILE_EXTENSIONS

--- lib/streamlit/web/server/app_static_file_handler.py
+++ lib/streamlit/web/server/app_static_file_handler.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import mimetypes
 import os
 from pathlib import Path
 from typing import Optional
@@ -30,10 +31,14 @@ _LOGGER = get_logger(__name__)
 MAX_APP_STATIC_FILE_SIZE = 200 * 1024 * 1024  # 200 MB
 # The list of file extensions that we serve with the corresponding Content-Type header.
 # All files with other extensions will be served with Content-Type: text/plain
-SAFE_APP_STATIC_FILE_EXTENSIONS = (".jpg", ".jpeg", ".png", ".gif")
+SAFE_APP_STATIC_FILE_EXTENSIONS = (".jpg", ".jpeg", ".png", ".gif", ".webp")
 
 
 class AppStaticFileHandler(AssetsFileHandler):
+    def initialize(self, path: str, default_filename: Optional[str] = None) -> None:
+        super().initialize(path, default_filename)
+        mimetypes.add_type("image/webp", ".webp")
+
     def validate_absolute_path(self, root: str, absolute_path: str) -> Optional[str]:
         full_path = os.path.realpath(absolute_path)
 
