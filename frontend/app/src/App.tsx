b9cd7456c892dfb728bde752d6548e4c57eb4ff8
willhuang1997 <willhuang1997@gmail.com>
2023-03-30 13:55:40 -0700

    Fix

--- frontend/app/src/App.tsx
+++ frontend/app/src/App.tsx
@@ -705,9 +705,10 @@ export class App extends PureComponent<Props, State> {
       RERUN_PROMPT_MODAL_DIALOG &&
       sessionEvent.type === "scriptChangedOnDisk"
     ) {
+      // @ts-expect-error
       const newDialog: DialogProps = {
         type: DialogType.SCRIPT_CHANGED,
-        rerunCallback: this.rerunScript,
+        onRerun: this.rerunScript,
         onClose: () => {},
         allowRunOnSave: this.state.allowRunOnSave,
       }
