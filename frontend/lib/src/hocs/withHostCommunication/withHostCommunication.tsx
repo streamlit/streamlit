67c07ebaf2fe7d1aa3556279af4e8ce67966eb62
willhuang1997 <willhuang1997@gmail.com>
2023-03-31 16:51:29 -0700

    update snapshots

--- frontend/lib/src/hocs/withHostCommunication/withHostCommunication.tsx
+++ frontend/lib/src/hocs/withHostCommunication/withHostCommunication.tsx
@@ -229,7 +229,7 @@ function withHostCommunication<P extends InjectedProps>(
       return () => {
         window.removeEventListener("message", receiveMessage)
       }
-    }, [allowedOriginsResp, deferredAuthToken])
+    }, [allowedOriginsResp, deferredAuthToken, props.theme])
 
     return (
       <WrappedComponent
