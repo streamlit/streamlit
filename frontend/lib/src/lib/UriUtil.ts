4f59e84688da18f2635de0e218d951ca8aefdf89
willhuang1997 <willhuang1997@gmail.com>
2023-04-03 15:38:55 -0700

    Add an extra null check in UriUtil

--- frontend/lib/src/lib/UriUtil.ts
+++ frontend/lib/src/lib/UriUtil.ts
@@ -135,7 +135,9 @@ function makePath(basePath: string, subPath: string): string {
  * True if we're connected to the host via HTTPS.
  */
 function isHttps(): boolean {
-  return window.location.href.startsWith("https://")
+  return (
+    window.location !== null && window.location.href.startsWith("https://")
+  )
 }
 
 /**
