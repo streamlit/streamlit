44e0d3124cc52ab98f158d29c85973197fc1bd4b
willhuang1997 <willhuang1997@gmail.com>
2023-04-04 09:51:29 -0700

    Remove location prep code

--- frontend/app/src/App.test.tsx
+++ frontend/app/src/App.test.tsx
@@ -123,19 +123,9 @@ jest.mock("moment", () =>
 )
 
 describe("App", () => {
-  const { location: originalLocation } = window
   beforeEach(() => {
     // @ts-expect-error
     window.prerenderReady = false
-    const { location: originalLocation } = window
-    // Replace window.location with a mutable object
-    // @ts-expect-error
-    delete window.location
-    window.location = { ...originalLocation }
-  })
-
-  afterEach(() => {
-    window.location = originalLocation
   })
 
   it("renders without crashing", () => {
