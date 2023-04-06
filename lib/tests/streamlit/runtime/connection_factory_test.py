9c0d626e57c9de17a22e73301ddc45b41961e017
Vincent Donato <vincent@streamlit.io>
2023-04-06 14:55:02 -0700

    Rework connection factory function API (#6436)
    * Remove the notion of default connection names
    
    * Rework connection factory API
    
    * Name type variable more descriptively
    
    * Change from Union[str, None] to Optional[str]

--- lib/tests/streamlit/runtime/connection_factory_test.py
+++ lib/tests/streamlit/runtime/connection_factory_test.py
@@ -12,18 +12,24 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import os
 import sys
 import threading
 import unittest
-from unittest.mock import MagicMock, patch
+from unittest.mock import MagicMock, mock_open, patch
 
 import pytest
 from parameterized import parameterized
 
 from streamlit.connections import SQL, BaseConnection, Snowpark
 from streamlit.errors import StreamlitAPIException
-from streamlit.runtime.connection_factory import _create_connection, connection_factory
+from streamlit.runtime.connection_factory import (
+    _create_connection,
+    _get_first_party_connection,
+    connection_factory,
+)
 from streamlit.runtime.scriptrunner import add_script_run_ctx
+from streamlit.runtime.secrets import secrets_singleton
 from tests.testutil import create_mock_script_run_ctx
 
 
@@ -38,6 +44,10 @@ class ConnectionFactoryTest(unittest.TestCase):
     def setUp(self) -> None:
         super().setUp()
 
+        # st.secrets modifies os.environ, so we save it here and
+        # restore in tearDown.
+        self._prev_environ = dict(os.environ)
+
         # Caching functions rely on an active script run ctx
         add_script_run_ctx(threading.current_thread(), create_mock_script_run_ctx())
 
@@ -45,14 +55,123 @@ class ConnectionFactoryTest(unittest.TestCase):
         super().tearDown()
         _create_connection.clear()
 
-    def test_passes_name_and_args_to_class(self):
-        conn = connection_factory(MockConnection, name="nondefault", foo="bar")
-        assert conn._connection_name == "nondefault"
+        secrets_singleton._reset()
+
+        os.environ.clear()
+        os.environ.update(self._prev_environ)
+
+    def test_create_connection_helper_explodes_if_not_BaseConnection_subclass(self):
+        class NotABaseConnection:
+            pass
+
+        with pytest.raises(StreamlitAPIException) as e:
+            _create_connection("my_connection", NotABaseConnection)
+
+        assert "is not a subclass of BaseConnection" in str(e.value)
+
+    @parameterized.expand(
+        [
+            ("snowpark", Snowpark),
+            ("sql", SQL),
+        ]
+    )
+    def test_get_first_party_connection_helper(
+        self, connection_class_name, expected_connection_class
+    ):
+        assert (
+            _get_first_party_connection(connection_class_name)
+            == expected_connection_class
+        )
+
+    def test_get_first_party_connection_helper_errors_when_invalid(self):
+        with pytest.raises(StreamlitAPIException) as e:
+            _get_first_party_connection("not_a_first_party_connection")
+
+        assert "Invalid connection" in str(e.value)
+
+    @parameterized.expand(
+        [
+            # No connection_class is specified, and there's no config file to find one
+            # in.
+            (None, FileNotFoundError, "No secrets files found"),
+            # Nonexistent module.
+            (
+                "nonexistent.module.SomeConnection",
+                ModuleNotFoundError,
+                "No module named 'nonexistent'",
+            ),
+            # The module exists, but the connection_class doesn't.
+            (
+                "streamlit.connections.Nonexistent",
+                AttributeError,
+                "module 'streamlit.connections' has no attribute 'Nonexistent'",
+            ),
+            # Invalid first party connection name.
+            (
+                "not_a_first_party_connection",
+                StreamlitAPIException,
+                "Invalid connection 'not_a_first_party_connection'",
+            ),
+        ]
+    )
+    def test_connection_factory_errors(
+        self, connection_class, expected_error_class, expected_error_msg
+    ):
+        with pytest.raises(expected_error_class) as e:
+            connection_factory(
+                "nonexistsent_connection", connection_class=connection_class
+            )
+
+        assert expected_error_msg in str(e.value)
+
+    @patch("streamlit.runtime.connection_factory._create_connection")
+    def test_can_specify_class_with_full_name_in_kwargs(
+        self, patched_create_connection
+    ):
+        connection_factory(
+            "my_connection", connection_class="streamlit.connections.SQL"
+        )
+
+        patched_create_connection.assert_called_once_with("my_connection", SQL)
+
+    @patch("streamlit.runtime.connection_factory._create_connection")
+    def test_can_specify_first_party_class_in_kwargs(self, patched_create_connection):
+        connection_factory("my_connection", connection_class="sql")
+
+        patched_create_connection.assert_called_once_with("my_connection", SQL)
+
+    @patch("streamlit.runtime.connection_factory._create_connection")
+    def test_can_specify_class_with_full_name_in_config(
+        self, patched_create_connection
+    ):
+        mock_toml = """
+[connections.my_connection]
+connection_class="streamlit.connections.SQL"
+"""
+        with patch("builtins.open", new_callable=mock_open, read_data=mock_toml):
+            connection_factory("my_connection")
+
+        patched_create_connection.assert_called_once_with("my_connection", SQL)
+
+    @patch("streamlit.runtime.connection_factory._create_connection")
+    def test_can_specify_first_party_class_in_config(self, patched_create_connection):
+        mock_toml = """
+[connections.my_connection]
+connection_class="snowpark"
+"""
+        with patch("builtins.open", new_callable=mock_open, read_data=mock_toml):
+            connection_factory("my_connection")
+
+        patched_create_connection.assert_called_once_with("my_connection", Snowpark)
+
+    def test_can_pass_class_directly_to_factory_func(self):
+        conn = connection_factory("my_connection", MockConnection, foo="bar")
+        assert conn._connection_name == "my_connection"
         assert conn._kwargs == {"foo": "bar"}
 
     def test_caches_connection_instance(self):
-        conn = connection_factory(MockConnection)
-        assert connection_factory(MockConnection) is conn
+        conn = connection_factory("my_connection", MockConnection)
+        assert connection_factory("my_connection", MockConnection) is conn
 
     @parameterized.expand(
         [
@@ -75,7 +194,7 @@ class ConnectionFactoryTest(unittest.TestCase):
         )
 
         with pytest.raises(ModuleNotFoundError) as e:
-            connection_factory(MockConnection)
+            connection_factory("my_connection", MockConnection)
         assert str(e.value) == (
             f"No module named '{missing_module}'. "
             f"You need to install the '{pypi_package}' package to use this connection."
@@ -88,7 +207,7 @@ class ConnectionFactoryTest(unittest.TestCase):
     def test_generic_missing_dependency_error(self):
         """Test our generic error message when a ModuleNotFoundError is thrown."""
         with pytest.raises(ModuleNotFoundError) as e:
-            connection_factory(MockConnection)
+            connection_factory("my_connection", MockConnection)
         assert str(e.value) == (
             "No module named 'foo'. "
             "You may be missing a dependency required to use this connection."
@@ -109,17 +228,3 @@ class ConnectionFactoryTest(unittest.TestCase):
         for m in modules:
             for disallowed_import in DISALLOWED_IMPORTS:
                 assert disallowed_import not in m
-
-    def test_raises_exception_when_passed_invalid_class_string(self):
-        with pytest.raises(StreamlitAPIException) as e:
-            connection_factory("nonexistent")
-
-        assert "Invalid connection nonexistent" in str(e.value)
-
-    @patch("streamlit.runtime.connection_factory._create_connection")
-    def test_connection_string_shorthand(self, patched_create_connection):
-        connection_factory("sql")
-        patched_create_connection.assert_called_with(SQL, name="default")
-
-        connection_factory("snowpark")
-        patched_create_connection.assert_called_with(Snowpark, name="default")
