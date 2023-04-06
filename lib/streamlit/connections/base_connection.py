9fa93eaf6fc1ec3adb9661e88e370ce9eb5dfbd2
Vincent Donato <vincent@streamlit.io>
2023-04-06 14:55:02 -0700

    Rework connection factory function API (#6436)
    * Remove the notion of default connection names
    
    * Rework connection factory API
    
    * Name type variable more descriptively
    
    * Change from Union[str, None] to Optional[str]

--- lib/streamlit/connections/base_connection.py
+++ lib/streamlit/connections/base_connection.py
@@ -19,20 +19,17 @@ from typing import Generic, Optional, TypeVar
 from streamlit.runtime.secrets import AttrDict, secrets_singleton
 from streamlit.util import calc_md5
 
-T = TypeVar("T")
+RawConnectionT = TypeVar("RawConnectionT")
 
 
-class BaseConnection(ABC, Generic[T]):
+class BaseConnection(ABC, Generic[RawConnectionT]):
     """TODO(vdonato): docstrings for this class and all public methods."""
 
-    def __init__(self, connection_name: str = "default", **kwargs) -> None:
-        if connection_name == "default":
-            connection_name = self.default_connection_name()
-
+    def __init__(self, connection_name: str, **kwargs) -> None:
         self._connection_name = connection_name
         self._kwargs = kwargs
 
-        self._raw_instance: Optional[T] = self.connect(**kwargs)
+        self._raw_instance: Optional[RawConnectionT] = self.connect(**kwargs)
         secrets_dict = self.get_secrets().to_dict()
         self._config_section_hash = calc_md5(json.dumps(secrets_dict))
         secrets_singleton.file_change_listener.connect(self._on_secrets_changed)
@@ -42,7 +39,7 @@ class BaseConnection(ABC, Generic[T]):
 
     def _repr_html_(self) -> str:
         # TODO(vdonato): Change this to whatever we actually want the default to be.
-        return f"Hi, I am a {self.default_connection_name()} connection!"
+        return f"Hi, I am a {self._connection_name} connection!"
 
     # Methods with default implementations that we don't expect subclasses to want or
     # need to overwrite.
@@ -66,30 +63,18 @@ class BaseConnection(ABC, Generic[T]):
 
         return connections_section.get(self._connection_name, AttrDict({}))
 
-    @classmethod
-    def default_connection_name(cls) -> str:
-        name = cls._default_connection_name
-
-        if name is None:
-            raise NotImplementedError(
-                "Subclasses of BaseConnection must define a _default_connection_name attribute."
-            )
-        return name
-
     # TODO(vdonato): Finalize the name for this method. Should this be `invalidate`?
     def reset(self) -> None:
         self._raw_instance = None
 
     @property
-    def _instance(self) -> T:
+    def _instance(self) -> RawConnectionT:
         if self._raw_instance is None:
             self._raw_instance = self.connect(**self._kwargs)
 
         return self._raw_instance
 
     # Abstract fields/methods that subclasses of BaseConnection must implement
-    _default_connection_name: Optional[str] = None
-
     @abstractmethod
-    def connect(self, **kwargs) -> T:
+    def connect(self, **kwargs) -> RawConnectionT:
         raise NotImplementedError
