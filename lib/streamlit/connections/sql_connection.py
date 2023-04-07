b3116b5abd016f9ccf2e1bc9fc73ba9acec9769e
Vincent Donato <vincent@streamlit.io>
2023-04-07 16:06:50 -0700

    Plumb a few arguments through SQL.query explicitly

--- lib/streamlit/connections/sql_connection.py
+++ lib/streamlit/connections/sql_connection.py
@@ -14,7 +14,7 @@
 
 from contextlib import contextmanager
 from datetime import timedelta
-from typing import TYPE_CHECKING, Iterator, Optional, Union, cast
+from typing import TYPE_CHECKING, Iterator, List, Optional, Union, cast
 
 import pandas as pd
 
@@ -66,17 +66,40 @@ class SQL(ExperimentalBaseConnection["Engine"]):
     def query(
         self,
         sql: str,
+        *,  # keyword-only arguments:
         ttl: Optional[Union[float, int, timedelta]] = None,
+        index_col: Optional[Union[str, List[str]]] = None,
+        chunksize: Optional[int] = None,
+        params=None,
         **kwargs,
     ) -> pd.DataFrame:
         from sqlalchemy import text
 
         @cache_data(ttl=ttl)
-        def _query(sql: str, **kwargs) -> pd.DataFrame:
+        def _query(
+            sql: str,
+            index_col=None,
+            chunksize=None,
+            params=None,
+            **kwargs,
+        ) -> pd.DataFrame:
             instance = self._instance.connect()
-            return pd.read_sql(text(sql), instance, **kwargs)
+            return pd.read_sql(
+                text(sql),
+                instance,
+                index_col=index_col,
+                chunksize=chunksize,
+                params=params,
+                **kwargs,
+            )
 
-        return _query(sql, **kwargs)
+        return _query(
+            sql,
+            index_col=index_col,
+            chunksize=chunksize,
+            params=params,
+            **kwargs,
+        )
 
     @contextmanager
     def session(self) -> Iterator["Session"]:
