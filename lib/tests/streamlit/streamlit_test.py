2aa6cea972b5790e81c30c39846818d6596ec263
Kamil Bregula <kamil.bregula@snowflake.com>
2023-04-11 11:42:31 +0200

    Remove test_st_arrow_altair_chart test
    It is duplicated by lib/tests/streamlit/elements/arrow_altair_test.py line 44

--- lib/tests/streamlit/streamlit_test.py
+++ lib/tests/streamlit/streamlit_test.py
@@ -192,27 +192,6 @@ class StreamlitTest(unittest.TestCase):
 class StreamlitAPITest(DeltaGeneratorTestCase):
     """Test Public Streamlit Public APIs."""
 
-    def test_st_arrow_altair_chart(self):
-        """Test st._arrow_altair_chart."""
-        import altair as alt
-
-        df = pd.DataFrame(np.random.randn(3, 3), columns=["a", "b", "c"])
-
-        c = (
-            alt.Chart(df)
-            .mark_circle()
-            .encode(x="a", y="b", size="c", color="c")
-            .interactive()
-        )
-        st._arrow_altair_chart(c)
-
-        proto = self.get_delta_from_queue().new_element.arrow_vega_lite_chart
-        spec = json.loads(proto.spec)
-
-        # Checking Vega-Lite is a lot of work so rather than doing that, we
-        # just checked to see if the spec data name matches the dataset.
-        self.assertEqual(spec.get("data").get("name"), proto.datasets[0].name)
-
     def test_st_legacy_area_chart(self):
         """Test st._legacy_area_chart."""
         df = pd.DataFrame([[10, 20, 30]], columns=["a", "b", "c"])
