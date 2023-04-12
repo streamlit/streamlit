06edd28f15369dd2fbc68617a89f4d641d8ce0d6
Kamil Bregula <kamil.bregula@snowflake.com>
2023-04-12 10:06:50 +0200

    Remove test_st_arrow_dataframe test
    It is duplicated by lib/tests/streamlit/elements/arrow_dataframe_test.py line 51

--- lib/tests/streamlit/streamlit_test.py
+++ lib/tests/streamlit/streamlit_test.py
@@ -192,17 +192,6 @@ class StreamlitTest(unittest.TestCase):
 class StreamlitAPITest(DeltaGeneratorTestCase):
     """Test Public Streamlit Public APIs."""
 
-    def test_st_arrow_dataframe(self):
-        """Test st._arrow_dataframe."""
-        from streamlit.type_util import bytes_to_data_frame
-
-        df = pd.DataFrame({"one": [1, 2], "two": [11, 22]})
-
-        st._arrow_dataframe(df)
-
-        proto = self.get_delta_from_queue().new_element.arrow_data_frame
-        pd.testing.assert_frame_equal(bytes_to_data_frame(proto.data), df)
-
     def test_st_empty(self):
         """Test st.empty."""
         st.empty()
