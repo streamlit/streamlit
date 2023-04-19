e1552fce120b366b4e3a8176e4d7d93f40528881
willhuang1997 <willhuang1997@gmail.com>
2023-04-18 20:49:14 -0700

    Fix lint error

--- frontend/src/setupTests.js
+++ frontend/src/setupTests.js
@@ -21,6 +21,7 @@ import Adapter from "@wojtekmaj/enzyme-adapter-react-17"
 
 configure({ adapter: new Adapter() })
 
+/* eslint @typescript-eslint/no-var-requires: "off" */
 // needed for BokehChart.test.tsx as we import bokeh scripts that use message channel
 // https://github.com/jsdom/jsdom/issues/2448
 // need to add --experimental-worker in node options craco test cli command
