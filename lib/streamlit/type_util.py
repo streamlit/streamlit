06445501a3c7305a7c44fed5aa6281b63b08da4c
lukasmasuch <lukas.masuch@gmail.com>
2023-04-23 04:33:36 +0200

    Fix typing

--- lib/streamlit/type_util.py
+++ lib/streamlit/type_util.py
@@ -449,7 +449,7 @@ def is_namedtuple(x: object) -> TypeGuard[NamedTuple]:
     return all(type(n).__name__ == "str" for n in f)
 
 
-def is_pandas_styler(obj: object) -> TypeGuard[Styler]:
+def is_pandas_styler(obj: object) -> TypeGuard["Styler"]:
     return is_type(obj, _PANDAS_STYLER_TYPE_STR)
 
 
@@ -539,7 +539,6 @@ def convert_anything_to_df(
     # Try to convert to pandas.DataFrame. This will raise an error is df is not
     # compatible with the pandas.DataFrame constructor.
     try:
-
         return DataFrame(data)
 
     except ValueError as ex:
@@ -855,7 +854,7 @@ def convert_df_to_data_format(
     DataFrame,
     Series,
     Index,
-    Styler,
+    "Styler",
     pa.Table,
     np.ndarray[Any, np.dtype[Any]],
     Tuple[Any],
