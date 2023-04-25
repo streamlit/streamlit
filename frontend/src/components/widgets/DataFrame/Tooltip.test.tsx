dbad8efbfc9aa1d214bdde05c159c1f436710c08
lukasmasuch <lukas.masuch@gmail.com>
2023-04-25 15:11:52 +0200

    Try ot test the click to close behavior

--- frontend/src/components/widgets/DataFrame/Tooltip.test.tsx
+++ frontend/src/components/widgets/DataFrame/Tooltip.test.tsx
@@ -1,6 +1,9 @@
 import React from "react"
 
-import { screen } from "@testing-library/react"
+import { screen, fireEvent } from "@testing-library/react"
+import userEvent from "@testing-library/user-event"
+import { waitFor } from "@testing-library/react"
+
 import "@testing-library/jest-dom"
 
 import { render } from "src/lib/test_util"
@@ -12,6 +15,7 @@ describe("Dataframe Tooltip", () => {
     top: 100,
     left: 100,
     content: "**This is a tooltip.**",
+    clearTooltip: jest.fn(),
   }
 
   test("renders the tooltip with provided content", () => {
@@ -23,11 +27,38 @@ describe("Dataframe Tooltip", () => {
     expect(tooltipContent).toHaveStyle("font-weight: bold")
   })
 
+  test("calls the clearTooltip callback when escape key is pressed", async () => {
+    const user = userEvent.setup()
+    const clearTooltipMock = jest.fn()
+    const propsWithMockedClearTooltip: TooltipProps = {
+      ...defaultProps,
+      clearTooltip: clearTooltipMock,
+    }
+
+    render(<Tooltip {...propsWithMockedClearTooltip} />)
+
+    const tooltipContent = screen.getByText("This is a tooltip.")
+    expect(tooltipContent).toBeInTheDocument()
+
+    user.keyboard("{Escape}")
+    fireEvent.keyDown(document, { key: "Escape", code: "Escape" })
+    fireEvent.keyDown(tooltipContent, { key: "Escape", code: "Escape" })
+    fireEvent.keyDown(window, { key: "Escape", code: "Escape" })
+    fireEvent.mouseDown(document.body)
+    user.click(document.body)
+
+    // Wait for the event handler to process the keydown event
+    await waitFor(() => {
+      expect(clearTooltipMock).toHaveBeenCalledTimes(1)
+    })
+  })
+
   test("renders the tooltip at the correct position", () => {
     const customPositionProps: TooltipProps = {
       top: 200,
       left: 300,
       content: "Positioned tooltip.",
+      clearTooltip: jest.fn(),
     }
 
     render(<Tooltip {...customPositionProps} />)
