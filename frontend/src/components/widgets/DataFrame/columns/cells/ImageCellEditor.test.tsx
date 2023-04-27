725c43f78e132d6f58b7cfd635fd64f3a9aa4e76
lukasmasuch <lukas.masuch@gmail.com>
2023-04-27 14:03:33 +0200

    Update test

--- frontend/src/components/widgets/DataFrame/columns/cells/ImageCellEditor.test.tsx
+++ frontend/src/components/widgets/DataFrame/columns/cells/ImageCellEditor.test.tsx
@@ -15,8 +15,12 @@
  */
 
 import React from "react"
-import { render, screen } from "@testing-library/react"
-import "@testing-library/jest-dom/extend-expect"
+
+import { screen } from "@testing-library/react"
+import "@testing-library/jest-dom"
+
+import { render } from "src/lib/test_util"
+
 import { ImageCellEditor } from "./ImageCellEditor"
 
 describe("ImageCellEditor", () => {
