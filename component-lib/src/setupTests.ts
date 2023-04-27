53951a8408483bbb2363f3d4f08460c309decbd4
Kamil Bregula <kamil.bregula@snowflake.com>
2023-04-27 17:36:58 +0200

    Bump dependencies of component-lib

--- component-lib/src/setupTests.ts
+++ component-lib/src/setupTests.ts
@@ -14,18 +14,24 @@
  * limitations under the License.
  */
 
-import {Streamlit} from "./streamlit";
+import { TextEncoder, TextDecoder } from "util";
+
+global.TextEncoder = TextEncoder;
+// @ts-expect-error
+global.TextDecoder = TextDecoder;
 
 beforeEach(() => {
   // The Streamlit class defines several static fields. To avoid side effects,
   // we restore them to their original state.
+  const {Streamlit} = require('./streamlit')
+
   Object.defineProperty(Streamlit, 'registeredMessageListener', {
     value: false,
     configurable: true,
     writable: true
   })
   Object.defineProperty(Streamlit, 'lastFrameHeight', {
-    value: undefined,
+    value: null,
     configurable: true,
     writable: true
   })
