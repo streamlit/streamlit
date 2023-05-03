287309691876d4d5baef4dc0690fef1f780a7568
willhuang1997 <willhuang1997@gmail.com>
2023-05-03 11:25:21 -0700

    Add DeployedAppMetadata to minimal export list

--- frontend/app/src/app/SegmentMetricsManager.ts
+++ frontend/app/src/app/SegmentMetricsManager.ts
@@ -17,7 +17,7 @@
 import { pick } from "lodash"
 import { SessionInfo } from "src/lib/SessionInfo"
 import { initializeSegment } from "src/app/vendor/Segment"
-import { DeployedAppMetadata } from "src/lib/hocs/withHostCommunication/types"
+import { DeployedAppMetadata } from "@streamlit/lib"
 import Protobuf, { Delta, Element, ForwardMsgMetadata } from "src/lib/proto"
 import { IS_DEV_ENV } from "@streamlit/lib"
 import { logAlways } from "src/lib/util/log"
