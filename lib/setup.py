3758b809309765e62f751d8637ae8a6957baee26
Tim Conkling <tconkling@gmail.com>
2023-05-03 11:37:48 -0700

    Enforce minimum protoc version in our Makefile

--- lib/setup.py
+++ lib/setup.py
@@ -39,6 +39,11 @@ INSTALL_REQUIRES = [
     "packaging>=14.1",
     "pandas<3,>=0.25",
     "pillow>=6.2.0",
+    # Python protobuf 4.21 (the first 4.x version) is compatible with protobufs
+    # generated from `protoc` >= 3.19.0 (https://protobuf.dev/news/2022-05-06/#python-updates).
+    # (`protoc` is installed separately from the Python protobuf package, so this >= 3.19 pin
+    # doesn't actually enforce a `protoc` minimum version. `protoc` min version is enforced
+    # in our Makefile.)
     "protobuf>=3.19, <5",
     "pyarrow>=4.0",
     "pympler>=0.9",
