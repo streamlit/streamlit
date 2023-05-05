12ce0b6d94a428947ad3e387cb0bce5ea2593f3d
willhuang1997 <willhuang1997@gmail.com>
2023-05-05 14:05:53 -0700

    Add ignore for typechecking on globalStyles because it's breaking app for now

--- frontend/lib/src/theme/globalStyles.ts
+++ frontend/lib/src/theme/globalStyles.ts
@@ -16,9 +16,11 @@
 
 import { css } from "@emotion/react"
 import { darken, transparentize, readableColor } from "color2k"
-import { EmotionTheme } from "src/theme/index"
+// import { EmotionTheme } from "src/theme/index"
 
-export const globalStyles = (theme: EmotionTheme): any => css`
+// This theme should be emotionTheme but needs to be researched more why 
+// app doesn't pick up theme properly
+export const globalStyles = (theme: any): any => css`
   a,
   a:visited {
     color: ${theme.colors.primary};
