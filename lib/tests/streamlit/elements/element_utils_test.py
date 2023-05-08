05a14ad1490973dd5dadea770fef9655ae368649
Anton Kozyriev <sonara4mus@gmail.com>
2023-05-09 00:22:58 +0300

    test(streamlit.elements): scenario for checking session state if duplication is disabled

--- lib/tests/streamlit/elements/element_utils_test.py
+++ lib/tests/streamlit/elements/element_utils_test.py
@@ -17,6 +17,7 @@ from unittest.mock import MagicMock, patch
 
 import pytest
 
+from streamlit import config
 from streamlit.elements.utils import check_callback_rules, check_session_state_rules
 from streamlit.errors import StreamlitAPIException
 
@@ -90,6 +91,22 @@ class ElementUtilsTest(unittest.TestCase):
         args, kwargs = patched_st_warning.call_args
         warning_msg = args[0]
         assert 'The widget with key "the key"' in warning_msg
+
+    @patch("streamlit.runtime.Runtime.exists", MagicMock(return_value=True))
+    @patch("streamlit.elements.utils.get_session_state")
+    @patch("streamlit.warning")
+    def test_check_session_state_rules_hide_warning_if_state_duplication_disabled(
+        self, patched_st_warning, patched_get_session_state
+    ):
+        config._set_option("global.disableWidgetStateDuplicationWarning", True, "test")
+
+        mock_session_state = MagicMock()
+        mock_session_state.is_new_state_value.return_value = True
+        patched_get_session_state.return_value = mock_session_state
+
+        check_session_state_rules(5, key="the key")
+
+        patched_st_warning.assert_not_called()
 
     @patch("streamlit.runtime.Runtime.exists", MagicMock(return_value=True))
     @patch("streamlit.elements.utils.get_session_state")
