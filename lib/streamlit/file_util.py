849b4ef54862859d7b3fd3c8e7ad246b8b9955d8
Tim Conkling <tconkling@gmail.com>
2023-05-08 14:49:35 -0700

    Remove unused `AssetsFileHandler` endpoint and directory (#6648)
    `AssetsFileHandler` serves files from `static/assets`. The only file in this directory is `streamlit.css`, which is a vestige from our olden days and hasn't been used since we switched to using Emotion for our CSS. 
    
    (This PR removes `streamlit.css` as well.)

--- lib/streamlit/file_util.py
+++ lib/streamlit/file_util.py
@@ -129,12 +129,6 @@ def get_app_static_dir(main_script_path: str) -> str:
     return os.path.abspath(static_dir)
 
 
-def get_assets_dir():
-    """Get the folder where static assets live."""
-    dirname = os.path.dirname(os.path.normpath(__file__))
-    return os.path.normpath(os.path.join(dirname, "static/assets"))
-
-
 def get_streamlit_file_path(*filepath) -> str:
     """Return the full path to a file in ~/.streamlit.
 
