c0995d90ca39bf836417b51c064ea294db15b1a6
Harsh Pathak <harsh.pathak@snowflake.com>
2023-05-11 15:59:21 -0700

    Adding security policy (#6666)
    * Create SECURITY.md
    
    * Don't require license header on SECURITY.md
    
    * Autoformat SECURITY.md
    
    ---------
    
    Co-authored-by: Vincent Donato <vincent@streamlit.io>

---
+++ SECURITY.md
@@ -0,0 +1,13 @@
+# Security Policy
+
+## Supported Versions
+
+| Version   | Supported |
+| --------- | --------- |
+| >= 1.11.1 | ✅        |
+
+</br>
+
+Please refer to the Snowflake [HackerOne program](https://hackerone.com/snowflake?type=team) for our security policies and for reporting any security vulnerabilities.
+
+For other security related questions and concerns, please contact the Snowflake security team at security@snowflake.com
--- scripts/check_license_headers.py
+++ scripts/check_license_headers.py
@@ -36,7 +36,7 @@ IGNORE_PATTERN = re.compile(
     # Exclude images.
     r"|\.(?:png|jpg|jpeg|gif|ttf|woff|otf|eot|woff2|ico|svg)$"
     # Exclude files, because they make it obvious which product they relate to.
-    r"|(LICENSE|NOTICES|CODE_OF_CONDUCT\.md|README\.md|CONTRIBUTING\.md)$"
+    r"|(LICENSE|NOTICES|CODE_OF_CONDUCT\.md|README\.md|CONTRIBUTING\.md|SECURITY.md)$"
     # Exclude files, because they do not support comments
     r"|\.(json|prettierrc|nvmrc)$"
     # Exclude generated files, because they don't have any degree of creativity.
