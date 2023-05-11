53fc166e53c1a762551a7ffcfbb6eb7d96972420
willhuang1997 <willhuang1997@gmail.com>
2023-05-11 14:16:52 -0700

    Fix BaseColorPicker directory

--- frontend/lib/src/components/widgets/ColorPicker/ColorPicker.tsx
+++ frontend/lib/src/components/widgets/ColorPicker/ColorPicker.tsx
@@ -18,7 +18,7 @@ import React from "react"
 import { ColorPicker as ColorPickerProto } from "src/proto"
 import { FormClearHelper } from "src/components/widgets/Form"
 import { WidgetStateManager, Source } from "src/WidgetStateManager"
-import UIColorPicker from "src/components/shared/BaseColorPicker"
+import BaseColorPicker from "src/components/shared/BaseColorPicker"
 import { labelVisibilityProtoValueToEnum } from "src/util/utils"
 
 export interface Props {
@@ -121,7 +121,7 @@ class ColorPicker extends React.PureComponent<Props, State> {
     )
 
     return (
-      <UIColorPicker
+      <BaseColorPicker
         label={element.label}
         labelVisibility={labelVisibilityProtoValueToEnum(
           element.labelVisibility?.value
