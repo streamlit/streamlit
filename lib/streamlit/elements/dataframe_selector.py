be711b6d5af71d41cf0a913e0094bc52698cca35
lukasmasuch <lukas.masuch@gmail.com>
2023-05-19 20:51:11 +0200

    Add docstring example to `st.dataframe`

--- lib/streamlit/elements/dataframe_selector.py
+++ lib/streamlit/elements/dataframe_selector.py
@@ -143,6 +143,37 @@ class DataFrameSelectorMixin:
            https://doc-dataframe1.streamlitapp.com/
            height: 410px
 
+        Or you can customize the dataframe via ``column_config``, ``hide_index``, or ``column_order``:
+
+        >>> import random
+        >>> import pandas as pd
+        >>> import streamlit as st
+        >>>
+        >>> df = pd.DataFrame(
+        >>>     {
+        >>>         "name": ["Roadmap", "Extras", "Issues"],
+        >>>         "url": ["https://roadmap.streamlit.app", "https://extras.streamlit.app", "https://issues.streamlit.app"],
+        >>>         "stars": [random.randint(0, 1000) for _ in range(3)],
+        >>>         "views_history": [[random.randint(0, 5000) for _ in range(30)] for _ in range(3)],
+        >>>     }
+        >>> )
+        >>> st.dataframe(
+        >>>     df,
+        >>>     column_config={
+        >>>         "name": "App name",
+        >>>         "stars": st.column_config.NumberColumn(
+        >>>             "Github Stars",
+        >>>             help="Number of stars on GitHub",
+        >>>             format="%d ⭐",
+        >>>         ),
+        >>>         "url": st.column_config.LinkColumn("App URL"),
+        >>>         "views_history": st.column_config.LineChartColumn(
+        >>>             "Views (past 30 days)", y_min=0, y_max=5000
+        >>>         ),
+        >>>     },
+        >>>     hide_index=True,
+        >>> )
+
         """
         if _use_arrow():
             return self.dg._arrow_dataframe(
