db42069ec987930d1ee6f0cb0815d44bb5070128
lukasmasuch <lukas.masuch@gmail.com>
2023-05-19 17:44:32 +0200

    Add column example

--- lib/streamlit/elements/lib/column_types.py
+++ lib/streamlit/elements/lib/column_types.py
@@ -226,6 +226,29 @@ def Column(
     Examples
     --------
 
+    >>> import pandas as pd
+    >>> import streamlit as st
+    >>>
+    >>> data_df = pd.DataFrame(
+    >>>     {
+    >>>         "widgets": ["st.selectbox", "st.number_input", "st.text_area", "st.button"],
+    >>>     }
+    >>> )
+    >>>
+    >>> st.experimental_data_editor(
+    >>>     data_df,
+    >>>     column_config={
+    >>>         "widgets": st.column_config.Column(
+    >>>             "Streamlit Widgets",
+    >>>             help="Streamlit **widget** commands 🎈",
+    >>>             width="medium",
+    >>>             required=True,
+    >>>         )
+    >>>     },
+    >>>     hide_index=True,
+    >>>     num_rows="dynamic",
+    >>> )
+
     .. output::
         https://doc-column.streamlit.app/
         height: 350px
