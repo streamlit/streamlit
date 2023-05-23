1b4d233cfc7510188ed6f0e80de2e2acf76284db
snehankekre <snehan.minerva@gmail.com>
2023-05-23 17:40:17 +0530

    Fix reStructuredText formatting issues

--- lib/streamlit/runtime/caching/cache_resource_api.py
+++ lib/streamlit/runtime/caching/cache_resource_api.py
@@ -287,14 +287,16 @@ class CacheResourceAPI:
         ttl : float, timedelta, str, or None
             The maximum time to keep an entry in the cache. Can be one of:
 
-            * `None` if cache entries should never expire (default).
+            * ``None`` if cache entries should never expire (default).
             * A number specifying the time in seconds.
-            * A string specifying the time in a format supported by [Pandas's
-              Timedelta constructor](https://pandas.pydata.org/docs/reference/api/pandas.Timedelta.html)`,
-              e.g. `"1d"`, `"1.5 days"`, or `"1h23s"`.
-            * A `timedelta` object from
-              [Python's built-in datetime library](https://docs.python.org/3/library/datetime.html#timedelta-objects),
-              e.g. `timedelta(days=1)`.
+            * A string specifying the time in a format supported by `Pandas's
+              Timedelta constructor <https://pandas.pydata.org/docs/reference/api/pandas.Timedelta.html>`_,
+              e.g. ``"1d"``, ``"1.5 days"``, or ``"1h23s"``.
+            * A ``timedelta`` object from `Python's built-in datetime library
+              <https://docs.python.org/3/library/datetime.html#timedelta-objects>`_,
+              e.g. ``timedelta(days=1)``.
+
+            Note that ``ttl`` will be ignored if ``persist="disk"`` or ``persist=True``.
 
         max_entries : int or None
             The maximum number of entries to keep in the cache, or None
