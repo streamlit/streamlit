d452f446893fd73867e1e88177629425443301a0
lukasmasuch <lukas.masuch@gmail.com>
2023-05-25 23:05:37 +0200

    Mini change

--- lib/streamlit/elements/data_editor.py
+++ lib/streamlit/elements/data_editor.py
@@ -846,8 +846,7 @@ class DataEditorMixin:
         "experimental_data_editor",
         "2023-09-01",
         """
-**Breaking change:** The data editor's representation in `st.session_state` was changed.
-The `edited_rows` dictionary is now called `edited_rows` and uses a
+**Breaking change:** The data editor's representation in `st.session_state` was changed. The `edited_rows` dictionary is now called `edited_rows` and uses a
 different format (`{0: {"column name": "edited value"}}` instead of
 `{"0:1": "edited value"}`). You may need to adjust the code if your app uses
 `st.experimental_data_editor` in combination with `st.session_state`."
