6b489c77d36646d4a0dfddc1506b7ecf64a4b54a
lukasmasuch <lukas.masuch@gmail.com>
2023-05-25 21:47:37 +0200

    Update warnings

--- lib/streamlit/elements/data_editor.py
+++ lib/streamlit/elements/data_editor.py
@@ -548,9 +548,10 @@ class DataEditorMixin:
         .. warning::
             When going from ``st.experimental_data_editor`` to ``st.data_editor`` in
             1.23.0, the format of the data editor's representation in ``st.session_state``
-            was changed so that the first column is denoted by 0, not 1. The index column
-            is now denoted by -1. You may need to adjust the code if your app uses
-            ``st.experimental_data_editor`` in combination with ``st.session_state``.
+            was changed. The ``edited_cells`` dictionary now uses the row number and
+            column name as keys (e.g. ``{0: {"col1": ...} }``) instead of the row and column
+            numbers (e.g. ``{"0:1": ...}``). If your app uses ``st.experimental_data_editor``
+            in combination with ``st.session_state``, you may need to adjust the code.
 
         Parameters
         ----------
@@ -846,9 +847,12 @@ class DataEditorMixin:
         gather_metrics("experimental_data_editor", data_editor),
         "experimental_data_editor",
         "2023-09-01",
-        "**Breaking change:** The format of the data editor's representation in "
-        + "`st.session_state` was changed so that the first column is denoted by 0,"
-        + "not 1. The index column is now denoted by -1. You may need to adjust "
-        + "the code if your app uses `st.experimental_data_editor` in combination "
-        + "with `st.session_state`.",
+        """
+**Breaking change:** The format of the data editor's representation in
+`st.session_state` was changed. The `edited_cells` dictionary now uses the
+row number and column name as keys (e.g. `{0: {"col1": ...} }`) instead of
+the row and column numbers (e.g. `{"0:1": ...}`). If your app uses
+`st.experimental_data_editor` in combination with `st.session_state`,
+you may need to adjust the code.
+""",
     )
