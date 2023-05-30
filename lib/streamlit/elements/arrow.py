ab2d4b7a2f1ab86fe88cdd572bb84efbc9dcdb06
Snehan Kekre <snehan.minerva@gmail.com>
2023-05-30 23:06:23 +0530

    Replace curly with straightquotes in docstring examples (#6757)

--- lib/streamlit/elements/arrow.py
+++ lib/streamlit/elements/arrow.py
@@ -112,7 +112,7 @@ class ArrowMixin:
             * A string to set the display label of the column.
 
             * One of the column types defined under ``st.column_config``, e.g.
-              ``st.column_config.NumberColumn(”Dollar values”, format=”$ %d”)`` to show
+              ``st.column_config.NumberColumn("Dollar values”, format=”$ %d")`` to show
               a column as dollar amounts. See more info on the available column types
               and config options `here <https://docs.streamlit.io/library/api-reference/data/st.column_config>`_.
 
--- lib/streamlit/elements/markdown.py
+++ lib/streamlit/elements/markdown.py
@@ -77,7 +77,7 @@ class MarkdownMixin:
         >>> import streamlit as st
         >>>
         >>> st.markdown('Streamlit is **_really_ cool**.')
-        >>> st.markdown(”This text is :red[colored red], and this is **:blue[colored]** and bold.”)
+        >>> st.markdown("This text is :red[colored red], and this is **:blue[colored]** and bold.")
         >>> st.markdown(":green[$\sqrt{x^2+y^2}=1$] is a Pythagorean identity. :pencil:")
 
         """
