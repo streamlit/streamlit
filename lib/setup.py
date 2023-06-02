162d8a4a85384bc1d5fea37e826345dbb40f5e1a
Karen Javadyan <kajarenc@gmail.com>
2023-06-02 23:33:37 +0400

    Add python 3.11 to classifiers list. (#6786)

--- lib/setup.py
+++ lib/setup.py
@@ -135,6 +135,7 @@ setuptools.setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "Topic :: Database :: Front-Ends",
         "Topic :: Office/Business :: Financial :: Spreadsheet",
         "Topic :: Scientific/Engineering :: Information Analysis",
