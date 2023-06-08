b5eee308b11e4dbaeaf0100745336479a1c259b0
Vincent Donato <vincent@streamlit.io>
2023-06-08 15:52:26 -0700

    Allow file uploader URLs to be fetched via websocket (#6791)
    * Add new proto definitions for file upload URL requests/responses
    
    * Add server websocket handler for file_urls_request BackMsgs
    
    * Rename FileUrls* -> FileURLs*
    
    * Install uuid types
    
    * Mark resolver fields as readonly
    
    * Allow file URLs to be requested via websocket
    
    * Add temporary  # type: ignore
    
    * Remove HTTP endpoints for fetching file URLs
    
    * Remove unused variable to appease eslint

--- frontend/app/src/App.test.tsx
+++ frontend/app/src/App.test.tsx
@@ -1229,6 +1229,54 @@ describe("App.handleDeltaMessage", () => {
   })
 })
 
+describe("App.requestFileURLs", () => {
+  let wrapper: ShallowWrapper
+  let instance: App
+
+  beforeEach(() => {
+    wrapper = shallow(<App {...getProps()} />)
+    instance = wrapper.instance() as App
+
+    // @ts-expect-error
+    instance.sendBackMsg = jest.fn()
+
+    // @ts-expect-error
+    instance.sessionInfo.setCurrent(mockSessionInfoProps())
+  })
+
+  it("properly constructs fileUrlsRequest BackMsg", () => {
+    instance.isServerConnected = jest.fn().mockReturnValue(true)
+
+    instance.requestFileURLs(
+      "myRequestId",
+      // @ts-expect-error
+      [{ name: "file1.txt" }, { name: "file2.txt" }, { name: "file3.txt" }]
+    )
+
+    // @ts-expect-error
+    expect(instance.sendBackMsg).toHaveBeenCalledWith({
+      fileUrlsRequest: {
+        fileNames: ["file1.txt", "file2.txt", "file3.txt"],
+        requestId: "myRequestId",
+        sessionId: "mockSessionId",
+      },
+    })
+  })
+
+  it("does nothing if server is disconnected", () => {
+    instance.isServerConnected = jest.fn().mockReturnValue(false)
+
+    instance.requestFileURLs(
+      "myRequestId",
+      // @ts-expect-error
+      [{ name: "file1.txt" }, { name: "file2.txt" }, { name: "file3.txt" }]
+    )
+
+    // @ts-expect-error
+    expect(instance.sendBackMsg).not.toHaveBeenCalled()
+  })
+})
+
 describe("Test Main Menu shortcut functionality", () => {
   let prevWindowLocation: Location
   beforeEach(() => {
