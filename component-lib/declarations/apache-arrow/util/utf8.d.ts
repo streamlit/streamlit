21e7beeae90f7136a53870c98f9705868d1e7be7
Kamil Breguła <kamil.bregula@snowflake.com>
2023-06-14 09:08:09 +0200

    Bump dependencies of component-lib (#6830)

--- component-lib/declarations/apache-arrow/util/utf8.d.ts
+++
@@ -1,6 +0,0 @@
-/** @ignore */
-export declare const decodeUtf8: (
-  input: ArrayBuffer | SharedArrayBuffer | ArrayBufferView
-) => string;
-/** @ignore */
-export declare const encodeUtf8: (value?: string | undefined) => Uint8Array;
