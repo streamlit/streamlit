c3788290fbe07d220ba57771a98ff3933a50f721
Vincent Donato <vincent@streamlit.io>
2023-06-16 14:36:25 -0700

    Change some types from Promise<number> to Promise<void>

--- frontend/src/lib/StreamlitLib.test.tsx
+++ frontend/src/lib/StreamlitLib.test.tsx
@@ -73,7 +73,7 @@ class Endpoints implements StreamlitEndpoints {
     sessionId: string,
     onUploadProgress?: (progressEvent: any) => void,
     cancelToken?: CancelToken
-  ): Promise<number> {
+  ): Promise<void> {
     return Promise.reject(new Error("Unimplemented"))
   }
 
