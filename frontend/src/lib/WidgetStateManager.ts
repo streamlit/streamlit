64d995ef392ddc6ad3f93561789224576362a9f9
Ken McGrady <ken.mcgrady@gmail.com>
2023-06-17 18:54:23 -0700

    Setup String Trigger Values

--- frontend/src/lib/WidgetStateManager.ts
+++ frontend/src/lib/WidgetStateManager.ts
@@ -23,6 +23,7 @@ import {
   IFileUploaderState,
   SInt64Array,
   StringArray,
+  StringTriggerValue,
   WidgetState,
   WidgetStates,
 } from "src/lib/proto"
@@ -36,7 +37,7 @@ export interface Source {
 /** Common widget protobuf fields that are used by the WidgetStateManager. */
 export interface WidgetInfo {
   id: string
-  formId: string
+  formId?: string
 }
 
 /**
@@ -239,6 +240,21 @@ export class WidgetStateManager {
     if (form.clearOnSubmit) {
       form.formCleared.emit()
     }
+  }
+
+  /**
+   * Sets the trigger value for the given widget ID to true, sends a rerunScript message
+   * to the server, and then immediately unsets the trigger value.
+   */
+  public setStringTriggerValue(
+    widget: WidgetInfo,
+    value: string,
+    source: Source
+  ): void {
+    this.createWidgetState(widget, source).stringTriggerValue =
+      new StringTriggerValue({ data: value })
+    this.onWidgetValueChanged(widget.formId, source)
+    this.deleteWidgetState(widget.id)
   }
 
   /**
@@ -527,7 +543,7 @@ export class WidgetStateManager {
   private createWidgetState(widget: WidgetInfo, source: Source): WidgetState {
     const addToForm = isValidFormId(widget.formId) && source.fromUi
     const widgetStateDict = addToForm
-      ? this.getOrCreateFormState(widget.formId).widgetStates
+      ? this.getOrCreateFormState(widget.formId as string).widgetStates
       : this.widgetStates
 
     return widgetStateDict.createState(widget.id)
