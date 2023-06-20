33d6a26ae2a1a7090ffb670cf0af1350ae7e4ec6
Lukas Masuch <Lukas.Masuch@gmail.com>
2023-06-20 18:46:22 +0200

    Fix stale element issue caused by spinners (#6859)
    * Fix stale element issue caused by spinners
    
    * Add unit test
    
    * Fix unit test
    
    * Fix tests
    
    * Fix tests
    
    * Fix correct caching command
    
    * Update comment
    
    * Update comment
    
    * Add comment to spinner test

--- lib/tests/streamlit/write_test.py
+++ lib/tests/streamlit/write_test.py
@@ -327,7 +327,7 @@ class StreamlitWriteTest(unittest.TestCase):
         """Test st.spinner."""
         # TODO(armando): Test that the message is actually passed to
         # message.warning
-        with patch("streamlit.delta_generator.DeltaGenerator.empty") as e:
+        with patch("streamlit.delta_generator.DeltaGenerator.container") as e:
             with st.spinner("some message"):
                 time.sleep(0.15)
             e.assert_called_once_with()
