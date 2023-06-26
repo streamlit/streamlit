dece0f4e13387a33cf18842eaaf8ce127878b12c
Amanda Walker <amanda@amandawalker.io>
2023-06-26 12:54:05 -0700

    Fix altair version check

--- e2e/scripts/st_arrow_altair_chart.py
+++ e2e/scripts/st_arrow_altair_chart.py
@@ -60,7 +60,7 @@ st._arrow_altair_chart(chart.configure_mark(color="black"), theme="streamlit")
 # mark_arc was added in 4.2, but we have to support altair 4.0-4.1, so we
 # have to skip this part of the test when testing min versions.
 major, minor, patch = alt.__version__.split(".")
-if not (major == 4 and minor < 2):
+if not (major == '4' and minor < '2'):
 
     source = pd.DataFrame(
         {"category": [1, 2, 3, 4, 5, 6], "value": [4, 6, 10, 3, 7, 8]}
