216c9f1dadf5060629778a356bcf29df131e4ec2
Hiroki Fujioka <hiroki976@gmail.com>
2023-06-27 11:51:46 +0900

    destructure placeholder and placeholderNoOptions

--- frontend/src/lib/components/widgets/Multiselect/Multiselect.tsx
+++ frontend/src/lib/components/widgets/Multiselect/Multiselect.tsx
@@ -215,9 +215,7 @@ class Multiselect extends React.PureComponent<Props, State> {
     const { options } = element
     const disabled = options.length === 0 ? true : this.props.disabled
     const placeholder =
-      options.length === 0
-        ? this.props.element.placeholderNoOptions
-        : this.props.element.placeholder
+      options.length === 0 ? element.placeholderNoOptions : element.placeholder
     const selectOptions: MultiselectOption[] = options.map(
       (option: string, idx: number) => {
         return {
