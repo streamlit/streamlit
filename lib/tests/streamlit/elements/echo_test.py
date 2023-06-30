dbc89bdb717593aa2669cde92ca4661ffc374d26
Brian Hess <brian.hess@snowflake.com>
2023-06-30 10:27:22 -0400

    modify st.echo to use ast to handle indenting properly (#5375)
    st.echo() has a simplistic approach to figuring out what to consider inside the echo(). It actually has issues with some use cases entirely, such as:
    
    with st.echo(
        location="below"
    ): 
        st.markdown("# Hiya")
    Instead, we should use ast to properly parse the file and output the proper full with st_echo() block.

--- lib/tests/streamlit/elements/echo_test.py
+++ lib/tests/streamlit/elements/echo_test.py
@@ -29,7 +29,6 @@ class EchoTest(DeltaGeneratorTestCase):
     def test_echo(self, _, echo, echo_index, output_index):
         # The empty lines below are part of the test. Do not remove them.
         with echo():
-
             st.write("Hello")
 
             "hi"
@@ -70,8 +69,69 @@ class MyClass(object):
 
         self.clear_queue()
 
+    @parameterized.expand(
+        [
+            ("code_location default", {}, 0, 1),
+            ("code_location above", {"code_location": "above"}, 0, 1),
+            ("code_location below", {"code_location": "below"}, 1, 0),
+        ]
+    )
+    def test_echo_unindent(
+        self,
+        _,
+        echo_kwargs_very_long_name_very_long_very_very_very_very_very_very_long,
+        echo_index,
+        output_index,
+    ):
+        with st.echo(
+            **echo_kwargs_very_long_name_very_long_very_very_very_very_very_very_long
+        ):
+            st.write("Hello")
+            "hi"
+
+            def foo(x):
+                y = x + 10
+
+                print(y)
+
+            class MyClass(object):
+                def do_x(self):
+                    pass
+
+                def do_y(self):
+                    pass
+
+        echo_str = """st.write("Hello")
+"hi"
+
+def foo(x):
+    y = x + 10
+
+    print(y)
+
+class MyClass(object):
+    def do_x(self):
+        pass
+
+    def do_y(self):
+        pass"""
+
+        element = self.get_delta_from_queue(echo_index).new_element
+        self.assertEqual(echo_str, element.code.code_text)
+        element = self.get_delta_from_queue(output_index).new_element
+        self.assertEqual("Hello", element.markdown.body)
+        self.clear_queue()
+
     def test_root_level_echo(self):
         import tests.streamlit.echo_test_data.root_level_echo
+
+        echo_str = "a = 123"
+
+        element = self.get_delta_from_queue(0).new_element
+        self.assertEqual(echo_str, element.code.code_text)
+
+    def test_echo_multiline_param(self):
+        import tests.streamlit.echo_test_data.multiline_param_echo
 
         echo_str = "a = 123"
 
