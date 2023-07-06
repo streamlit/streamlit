d84820bff4886a2783148e6f8925f229a15a1c68
willhuang1997 <willhuang1997@gmail.com>
2023-07-06 16:57:50 -0700

    Fix relative imports for lib/src/util

--- frontend/lib/src/util/FileHelper.ts
+++ frontend/lib/src/util/FileHelper.ts
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-import { isFromWindows } from "src/util/utils"
+import { isFromWindows } from "../util/utils"
 
 export enum FileSize {
   Gigabyte = "gb",
--- frontend/lib/src/util/utils.ts
+++ frontend/lib/src/util/utils.ts
@@ -18,7 +18,7 @@ import {
   Alert as AlertProto,
   LabelVisibilityMessage as LabelVisibilityMessageProto,
   Element,
-} from "src/proto"
+} from "../proto"
 import _ from "lodash"
 import xxhash from "xxhashjs"
 
