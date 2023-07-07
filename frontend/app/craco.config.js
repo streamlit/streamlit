5f934f98b12413702b48c1fa2be15f24bc9091cf
willhuang1997 <willhuang1997@gmail.com>
2023-07-06 17:01:49 -0700

    Remove semicolons from craco.config.js

--- frontend/app/craco.config.js
+++ frontend/app/craco.config.js
@@ -46,7 +46,7 @@ module.exports = {
   webpack: {
     configure: webpackConfig => {
       // this file overrides the default CRA configurations (webpack, eslint, babel, etc)
-      const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
+      const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin")
       // ignore webpack warnings by source-map-loader https://github.com/facebook/create-react-app/pull/11752
       webpackConfig.ignoreWarnings = [/Failed to parse source map from/]
       webpackConfig.resolve.mainFields = ["module", "main"]
@@ -91,19 +91,19 @@ module.exports = {
 
       // Remove ModuleScopePlugin which throws when we try to import something
       // outside of src/.
-      webpackConfig.resolve.plugins.pop();
+      webpackConfig.resolve.plugins.pop()
 
       // Resolve the path aliases.
-      webpackConfig.resolve.plugins.push(new TsconfigPathsPlugin());
+      webpackConfig.resolve.plugins.push(new TsconfigPathsPlugin())
 
       // Let Babel compile outside of src/.
-      const oneOfRule = webpackConfig.module.rules.find((rule) => rule.oneOf);
-      const tsRule = oneOfRule.oneOf.find((rule) =>
+      const oneOfRule = webpackConfig.module.rules.find(rule => rule.oneOf)
+      const tsRule = oneOfRule.oneOf.find(rule =>
         rule.test.toString().includes("ts|tsx")
-      );
+      )
 
-      tsRule.include = undefined;
-      tsRule.exclude = /node_modules/;
+      tsRule.include = undefined
+      tsRule.exclude = /node_modules/
 
       return webpackConfig
     },
