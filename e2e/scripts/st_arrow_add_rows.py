b7d2bf339d3732d7bf78c80b71b8064e137a66cd
Thiago Teixeira <thiago.teixeira@snowflake.com>
2023-07-18 22:55:39 +0200

    Revert st_arrow_add_rows test to avoid too many changes in this PR. Will land in another PR.

--- e2e/scripts/st_arrow_add_rows.py
+++ e2e/scripts/st_arrow_add_rows.py
@@ -18,45 +18,16 @@ import pandas as pd
 
 import streamlit as st
 
-df = pd.DataFrame({"a": [10, 11], "b": [20, 21], "c": [30, 31]})
-df_add_rows = pd.DataFrame({"a": [12, 13], "b": [22, 23], "c": [32, 33]})
+df = pd.DataFrame({"a": [1, 2], "b": [3, 4], "c": [5, 6]})
 
-df2 = pd.DataFrame({"a": [10, 11], "b": [1, 3], "c": [2, 4]})
-df2_add_rows = pd.DataFrame({"a": [12, 13], "b": [2, 4], "c": [3, 5]})
-
-
-"""
-# Tabular tests
-
-Should show index=[0, 1, 2, 3], a=[10, 11, 12, 13], b=[20, 21, 22, 23],
-c=[30, 31, 32, 33].
-"""
 table_element = st._arrow_table(df)
 dataframe_element = st._arrow_dataframe(df)
-
-"""
-# Empty chart test
-
-Should show 3 lines going up. One line should be quite far from the others.
-"""
 chart_element_1 = st._arrow_line_chart()
+chart_element_2 = st._arrow_line_chart(df)
 
-"""
-# Non-empty chart test
-
-Should show 2 lines zig-zagging, and a farther one just going up.
-"""
-chart_element_2 = st._arrow_line_chart(df2)
-
-"""
-# Chart encoding tests
-
-These are 4 identical charts, built in different ways using Altair and Vega-Lite directly.
-
-Each should show 1 line zig-zagging.
-"""
+# 4 identical charts, built in different ways.
 vega_element_1 = st._arrow_vega_lite_chart(
-    df2,
+    df,
     {
         "mark": {"type": "line", "point": True},
         "encoding": {
@@ -68,7 +39,7 @@ vega_element_1 = st._arrow_vega_lite_chart(
 )
 vega_element_2 = st._arrow_vega_lite_chart(
     {
-        "datasets": {"foo": df2},
+        "datasets": {"foo": df},
         "data": {"name": "foo"},
         "mark": {"type": "line", "point": True},
         "encoding": {
@@ -80,7 +51,7 @@ vega_element_2 = st._arrow_vega_lite_chart(
 )
 vega_element_3 = st._arrow_vega_lite_chart(
     {
-        "datasets": {"foo": df2},
+        "datasets": {"foo": df},
         "data": {"name": "foo"},
         "mark": {"type": "line", "point": True},
         "encoding": {
@@ -91,26 +62,20 @@ vega_element_3 = st._arrow_vega_lite_chart(
     use_container_width=True,
 )
 altair_element = st._arrow_altair_chart(
-    alt.Chart(df2).mark_line(point=True).encode(x="a", y="b").interactive(),
+    alt.Chart(df).mark_line(point=True).encode(x="a", y="b").interactive(),
     use_container_width=True,
 )
 
-table_element._arrow_add_rows(df_add_rows)
-dataframe_element._arrow_add_rows(df_add_rows)
-chart_element_1._arrow_add_rows(df2_add_rows)
-chart_element_2._arrow_add_rows(df2_add_rows)
-vega_element_1._arrow_add_rows(df2_add_rows)
-vega_element_2._arrow_add_rows(df2_add_rows)
-vega_element_3._arrow_add_rows(foo=df2_add_rows)
-altair_element._arrow_add_rows(df2_add_rows)
+table_element._arrow_add_rows(df)
+dataframe_element._arrow_add_rows(df)
+chart_element_1._arrow_add_rows(df)
+chart_element_2._arrow_add_rows(df)
+vega_element_1._arrow_add_rows(df)
+vega_element_2._arrow_add_rows(df)
+vega_element_3._arrow_add_rows(foo=df)
+altair_element._arrow_add_rows(df)
 
-"""
-# Mismatched dimensions
-
-Test that `_arrow_add_rows` errors out when the dataframe dimensions don't
-match.
-
-This should show an error.
-"""
+# Test that `_arrow_add_rows` errors out when the dataframe dimensions don't match.
+# This should show an error!
 dataframe_element = st._arrow_dataframe(df)
 dataframe_element._arrow_add_rows(np.abs(np.random.randn(1, 6)))
