6783dea1c7f5071fbb45d1d7ef1b0e9b691475b9
Debbie Matthews <debbie.matthews@snowflake.com>
2023-07-21 04:05:53 -0700

    Update docstrings for st.form, st.toast, st.date_input (#7043)
    * Update forms docstring
    
    Update forms docstring to include embedded apps and link to new forms guide
    
    * Update st.form docstring
    
    Removed the link to a guide in the doc (link published to the page from the docs repo now). Added limitation about not using callbacks on widgets within forms, except for st.form_submit_button
    
    * Add cache warning to st.toast
    
    Update st.toast docstring to warn of incompatibility with caching.
    
    * Update st.date_input docstring
    
    Added another example and embedded app to st.date_input. This demonstrates date format as well as date range selection.
    
    * Increase height on date input apps and lint code blocks
    
    * Display result
    
    ---------
    
    Co-authored-by: snehankekre <snehan.minerva@gmail.com>

--- lib/streamlit/elements/time_widgets.py
+++ lib/streamlit/elements/time_widgets.py
@@ -488,19 +488,38 @@ class TimeWidgetsMixin:
         datetime.date or a tuple with 0-2 dates
             The current value of the date input widget.
 
-        Example
-        -------
+        Examples
+        --------
         >>> import datetime
         >>> import streamlit as st
         >>>
-        >>> d = st.date_input(
-        ...     "When\'s your birthday",
-        ...     datetime.date(2019, 7, 6))
+        >>> d = st.date_input("When's your birthday", datetime.date(2019, 7, 6))
         >>> st.write('Your birthday is:', d)
 
         .. output::
            https://doc-date-input.streamlit.app/
-           height: 260px
+           height: 380px
+
+        >>> import datetime
+        >>> import streamlit as st
+        >>>
+        >>> today = datetime.datetime.now()
+        >>> next_year = today.year + 1
+        >>> jan_1 = datetime.date(next_year, 1, 1)
+        >>> dec_31 = datetime.date(next_year, 12, 31)
+        >>>
+        >>> d = st.date_input(
+        ...     "Select your vacation for next year",
+        ...     (jan_1, datetime.date(next_year, 1, 7)),
+        ...     jan_1,
+        ...     dec_31,
+        ...     format="MM.DD.YYYY",
+        ... )
+        >>> d
+
+        .. output::
+           https://doc-date-input1.streamlit.app/
+           height: 380px
 
         """
         ctx = get_script_run_ctx()
