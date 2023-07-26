4fcb62ccd9c6e2ee4c48cc1119e61943e79fba46
Lukas Masuch <Lukas.Masuch@gmail.com>
2023-07-26 20:21:39 +0200

    Implement playwright e2e tests prototype (#7001)
    * Initial investigation
    
    * Update python playwright support
    
    * Cleanup
    
    * Add github actions flow
    
    * Fix playwright workflow
    
    * Update playwright
    
    * Add snapshots
    
    * Remove e2e script
    
    * Fix linting issue
    
    * Add additional time
    
    * Add copyright header
    
    * Update snapshots
    
    * Move into main gitignore
    
    * Fix wait for logic
    
    * Add debug results to workflow
    
    * Improve gitignore
    
    * Improved conftest
    
    * Add additional tests
    
    * Use locator everywhere
    
    * Fix wrong upload dir
    
    * Add snapshots
    
    * Update e2e/playwright/conftest.py
    
    Co-authored-by: Kamil Breguła <kamil.bregula@snowflake.com>
    
    * Use healthcheck
    
    Co-authored-by: Kamil Breguła <kamil.bregula@snowflake.com>
    
    * Get random available port from operating system
    
    Co-authored-by: Kamil Breguła <kamil.bregula@snowflake.com>
    
    * Fix issues
    
    * Add comment
    
    * Remove unneeded comment
    
    * Add make command and other updates
    
    * Add some docstring comments
    
    * Some more updates
    
    * Add checkbox test
    
    * Migrate multiselect test
    
    * Add snapshots
    
    * Improve e2e tests
    
    * Update tests
    
    * Change e2e screenshot utils to work on element
    
    * Update tests
    
    * Improve tests
    
    * Improve app loader functionality
    
    * Change back Pipfile
    
    * Fix flaky test
    
    * Remove wait for pause
    
    * Update conftest
    
    * Apply review comments
    
    * Remove snapshots
    
    * Apply review comments
    
    * Lower wait time
    
    * Remove wait time
    
    * Add todo comment
    
    * Migrate two additional tests
    
    * Add new snapshots
    
    * Downgrade machine to 4 cores
    
    * Add file type docstring
    
    ---------
    
    Co-authored-by: Kamil Breguła <kamil.bregula@snowflake.com>

---
+++ e2e/playwright/conftest.py
@@ -0,0 +1,420 @@
+# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+Global pytest fixtures for e2e tests.
+This file is automatically run by pytest before tests are executed.
+"""
+from __future__ import annotations
+
+import contextlib
+import os
+import re
+import shlex
+import shutil
+import socket
+import subprocess
+import sys
+import time
+from io import BytesIO
+from pathlib import Path
+from tempfile import TemporaryFile
+from types import ModuleType
+from typing import Any, Generator, List, Literal, Protocol
+
+import pytest
+import requests
+from PIL import Image
+from playwright.sync_api import ElementHandle, Locator, Page
+from pytest import FixtureRequest
+
+
+class AsyncSubprocess:
+    """A context manager. Wraps subprocess. Popen to capture output safely."""
+
+    def __init__(self, args, cwd=None, env=None):
+        self.args = args
+        self.cwd = cwd
+        self.env = env
+        self._proc = None
+        self._stdout_file = None
+
+    def terminate(self):
+        """Terminate the process and return its stdout/stderr in a string."""
+        if self._proc is not None:
+            self._proc.terminate()
+            self._proc.wait()
+            self._proc = None
+
+        # Read the stdout file and close it
+        stdout = None
+        if self._stdout_file is not None:
+            self._stdout_file.seek(0)
+            stdout = self._stdout_file.read()
+            self._stdout_file.close()
+            self._stdout_file = None
+
+        return stdout
+
+    def __enter__(self):
+        self.start()
+        return self
+
+    def start(self):
+        # Start the process and capture its stdout/stderr output to a temp
+        # file. We do this instead of using subprocess.PIPE (which causes the
+        # Popen object to capture the output to its own internal buffer),
+        # because large amounts of output can cause it to deadlock.
+        self._stdout_file = TemporaryFile("w+")
+        print(f"Running: {shlex.join(self.args)}")
+        self._proc = subprocess.Popen(
+            self.args,
+            cwd=self.cwd,
+            stdout=self._stdout_file,
+            stderr=subprocess.STDOUT,
+            text=True,
+            env={**os.environ.copy(), **self.env} if self.env else None,
+        )
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if self._proc is not None:
+            self._proc.terminate()
+            self._proc = None
+        if self._stdout_file is not None:
+            self._stdout_file.close()
+            self._stdout_file = None
+
+
+def resolve_test_to_script(test_module: ModuleType) -> str:
+    """Resolve the test module to the corresponding test script filename."""
+    module_name = test_module.__name__
+    return module_name.replace("_test", ".py")
+
+
+def find_available_port(host: str = "localhost") -> int:
+    """Find an available port on the given host."""
+    with contextlib.closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as s:
+        s.bind((host, 0))  # 0 means that the OS chooses a random port
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        return int(s.getsockname()[1])  # [1] contains the randomly selected port number
+
+
+def is_app_server_running(port: int, host: str = "localhost") -> bool:
+    """Check if the app server is running."""
+    try:
+        return (
+            requests.get(f"http://{host}:{port}/_stcore/health", timeout=1).text == "ok"
+        )
+    except Exception:
+        return False
+
+
+def wait_for_app_server_to_start(port: int, timeout: int = 5) -> bool:
+    """Wait for the app server to start.
+
+    Parameters
+    ----------
+    port : int
+        The port on which the app server is running.
+
+    timeout : int
+        The number of minutes to wait for the app server to start.
+
+    Returns
+    -------
+    bool
+        True if the app server is started, False otherwise.
+    """
+
+    print(f"Waiting for app to start... {port}")
+    start_time = time.time()
+    while not is_app_server_running(port):
+        time.sleep(3)
+        if time.time() - start_time > 60 * timeout:
+            return False
+    return True
+
+
+def wait_for_app_run(page: Page):
+    """Wait for the given page to finish running."""
+    page.wait_for_selector(
+        "[data-testid='stStatusWidget']", timeout=20000, state="detached"
+    )
+    # Give the app a little more time to render everything
+    # TODO(lukasmasuch): Do we need this to reduce flakiness?
+    # page.wait_for_timeout(500)
+
+
+def wait_for_app_loaded(page: Page):
+    """Wait for the app to fully load."""
+    # Wait for the app view container to appear:
+    page.wait_for_selector(
+        "[data-testid='stAppViewContainer']", timeout=30000, state="attached"
+    )
+    # Wait for the main app container to appear:
+    page.wait_for_selector(
+        "[data-testid='block-container']", timeout=20000, state="attached"
+    )
+    # Wait for the main menu to appear:
+    page.wait_for_selector("#MainMenu", timeout=20000, state="attached")
+    wait_for_app_run(page)
+
+
+@pytest.fixture(scope="module")
+def app_port() -> int:
+    """Fixture that returns an available port on localhost."""
+    return find_available_port()
+
+
+@pytest.fixture(scope="module", autouse=True)
+def app_server(
+    app_port: int, request: FixtureRequest
+) -> Generator[AsyncSubprocess, None, None]:
+    """Fixture that starts and stops the Streamlit app server."""
+    streamlit_proc = AsyncSubprocess(
+        [
+            "streamlit",
+            "run",
+            resolve_test_to_script(request.module),
+            "--server.headless",
+            "true",
+            "--global.developmentMode",
+            "false",
+            "--server.port",
+            str(app_port),
+            "--browser.gatherUsageStats",
+            "false",
+        ],
+        cwd=".",
+    )
+    streamlit_proc.start()
+    if not wait_for_app_server_to_start(app_port):
+        streamlit_stdout = streamlit_proc.terminate()
+        print(streamlit_stdout)
+        raise RuntimeError("Unable to start Streamlit app")
+    yield streamlit_proc
+    streamlit_stdout = streamlit_proc.terminate()
+    print(streamlit_stdout)
+
+
+@pytest.fixture(scope="function")
+def app(page: Page, app_port: int) -> Page:
+    """Fixture that opens the app."""
+    page.goto(f"http://localhost:{app_port}/")
+    wait_for_app_loaded(page)
+    return page
+
+
+@pytest.fixture(scope="function", params=["light_theme", "dark_theme"])
+def app_theme(request) -> str:
+    """Fixture that returns the theme name."""
+    return str(request.param)
+
+
+@pytest.fixture(scope="function")
+def themed_app(page: Page, app_port: int, app_theme: str) -> Page:
+    """Fixture that opens the app with the given theme."""
+    page.goto(f"http://localhost:{app_port}/?embed_options={app_theme}")
+    wait_for_app_loaded(page)
+    return page
+
+
+class ImageCompareFunction(Protocol):
+    def __call__(
+        self,
+        element: ElementHandle | Locator,
+        *,
+        image_threshold: float = 0.001,
+        pixel_threshold: float = 0.05,
+        name: str | None = None,
+        fail_fast: bool = False,
+    ) -> None:
+        """Compare a screenshot with screenshot from a past run.
+
+        Parameters
+        ----------
+        element : ElementHandle or Locator
+            The element to take a screenshot of.
+        image_threshold : float, optional
+            The allowed percentage of different pixels in the image.
+        pixel_threshold : float, optional
+            The allowed percentage of difference for a single pixel.
+        name : str | None, optional
+            The name of the screenshot without an extension. If not provided, the name
+            of the test function will be used.
+        fail_fast : bool, optional
+            If True, the comparison will stop at the first pixel mismatch.
+        """
+
+
+@pytest.fixture(scope="session")
+def output_folder(pytestconfig: Any) -> Path:
+    """Fixture that returns the directory that is used for all test failures information.
+
+    This includes:
+    - snapshot-tests-failures: This directory contains all the snapshots that did not
+    match with the snapshots from past runs. The folder structure is based on the folder
+    structure used in the main snapshots folder.
+    - snapshot-updates: This directory contains all the snapshots that got updated in
+    the current run based on folder structure used in the main snapshots folder.
+    """
+    return Path(pytestconfig.getoption("--output")).resolve()
+
+
+@pytest.fixture(scope="function")
+def assert_snapshot(
+    request: FixtureRequest, output_folder: Path
+) -> Generator[ImageCompareFunction, None, None]:
+    """Fixture that compares a screenshot with screenshot from a past run."""
+    platform = str(sys.platform)
+    module_name = request.module.__name__
+    test_function_name = request.node.originalname
+    root_path = Path(request.node.fspath).parent.resolve()
+
+    snapshot_dir: Path = root_path / "snapshots" / platform / module_name
+
+    module_snapshot_failures_dir: Path = (
+        output_folder / "snapshot-tests-failures" / platform / module_name
+    )
+    module_snapshot_updates_dir: Path = (
+        output_folder / "snapshot-updates" / platform / module_name
+    )
+
+    snapshot_file_suffix = ""
+    # Extract the parameter ids if they exist
+    match = re.search(r"\[(.*?)\]", request.node.name)
+    if match:
+        snapshot_file_suffix = f"[{match.group(1)}]"
+
+    snapshot_default_file_name: str = test_function_name + snapshot_file_suffix
+
+    test_failure_messages: List[str] = []
+
+    def compare(
+        element: ElementHandle | Locator,
+        *,
+        image_threshold: float = 0.001,
+        pixel_threshold: float = 0.05,
+        name: str | None = None,
+        fail_fast: bool = False,
+        file_type: Literal["png", "jpg"] = "png",
+    ) -> None:
+        """Compare a screenshot with screenshot from a past run.
+
+        Parameters
+        ----------
+        element : ElementHandle or Locator
+            The element to take a screenshot of.
+        image_threshold : float, optional
+            The allowed percentage of different pixels in the image.
+        pixel_threshold : float, optional
+            The allowed percentage of difference for a single pixel to be considered
+            different.
+        name : str | None, optional
+            The name of the screenshot without an extension. If not provided, the name
+            of the test function will be used.
+        fail_fast : bool, optional
+            If True, the comparison will stop at the first pixel mismatch.
+        file_type: "png" or "jpg"
+            The file type of the screenshot. Defaults to "png".
+        """
+        nonlocal test_failure_messages
+        nonlocal snapshot_default_file_name
+        nonlocal module_snapshot_updates_dir
+        nonlocal module_snapshot_failures_dir
+        nonlocal snapshot_file_suffix
+
+        if file_type == "jpg":
+            file_extension = ".jpg"
+            img_bytes = element.screenshot(
+                type="jpeg", quality=90, animations="disabled"
+            )
+
+        else:
+            file_extension = ".png"
+            img_bytes = element.screenshot(type="png", animations="disabled")
+
+        snapshot_file_name: str = snapshot_default_file_name
+        if name:
+            snapshot_file_name = name + snapshot_file_suffix
+
+        snapshot_file_path: Path = (
+            snapshot_dir / f"{snapshot_file_name}{file_extension}"
+        )
+
+        snapshot_updates_file_path: Path = (
+            module_snapshot_updates_dir / f"{snapshot_file_name}{file_extension}"
+        )
+
+        snapshot_file_path.parent.mkdir(parents=True, exist_ok=True)
+
+        test_failures_dir = module_snapshot_failures_dir / snapshot_file_name
+        if test_failures_dir.exists():
+            # Remove the past runs failure dir for this specific screenshot
+            shutil.rmtree(test_failures_dir)
+
+        if not snapshot_file_path.exists():
+            snapshot_file_path.write_bytes(img_bytes)
+            # Update this in updates folder:
+            snapshot_updates_file_path.parent.mkdir(parents=True, exist_ok=True)
+            snapshot_updates_file_path.write_bytes(img_bytes)
+            # For missing snapshots, we don't want to directly fail in order to generate
+            # all missing snapshots in one run.
+            test_failure_messages.append(f"Missing snapshot for {snapshot_file_name}")
+            return
+
+        from pixelmatch.contrib.PIL import pixelmatch
+
+        # Compare the new screenshot with the screenshot from past runs:
+        img_a = Image.open(BytesIO(img_bytes))
+        img_b = Image.open(snapshot_file_path)
+        img_diff = Image.new("RGBA", img_a.size)
+        try:
+            mismatch = pixelmatch(
+                img_a,
+                img_b,
+                img_diff,
+                threshold=pixel_threshold,
+                fail_fast=fail_fast,
+                alpha=0,
+            )
+        except ValueError as ex:
+            # ValueError is thrown when the images have different sizes
+            # Update this in updates folder:
+            snapshot_updates_file_path.parent.mkdir(parents=True, exist_ok=True)
+            snapshot_updates_file_path.write_bytes(img_bytes)
+            pytest.fail(f"Snapshot matching failed: {ex}")
+        max_diff_pixels = int(image_threshold * img_a.size[0] * img_a.size[1])
+
+        if mismatch < max_diff_pixels:
+            return
+
+        # Update this in updates folder:
+        snapshot_updates_file_path.parent.mkdir(parents=True, exist_ok=True)
+        snapshot_updates_file_path.write_bytes(img_bytes)
+
+        # Create new failures folder for this test:
+        test_failures_dir.mkdir(parents=True, exist_ok=True)
+        img_diff.save(f"{test_failures_dir}/diff_{snapshot_file_name}{file_extension}")
+        img_a.save(f"{test_failures_dir}/actual_{snapshot_file_name}{file_extension}")
+        img_b.save(f"{test_failures_dir}/expected_{snapshot_file_name}{file_extension}")
+
+        pytest.fail(
+            f"Snapshot mismatch for {snapshot_file_name} ({mismatch} pixels difference)"
+        )
+
+    yield compare
+
+    if test_failure_messages:
+        pytest.fail("Snapshot test failed \n" + "\n".join(test_failure_messages))
---
+++ e2e/playwright/st_expander_test.py
@@ -0,0 +1,72 @@
+# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from playwright.sync_api import Page, expect
+
+from conftest import ImageCompareFunction
+
+EXPANDER_HEADER_IDENTIFIER = ".streamlit-expanderHeader"
+
+
+def test_displays_expander_and_regular_containers_properly(app: Page):
+    """Test that expanders and regular containers are displayed properly."""
+
+    main_expanders = app.locator(".main [data-testid='stExpander']")
+    expect(main_expanders).to_have_count(2)
+
+    for expander in main_expanders.all():
+        expect(expander.locator(EXPANDER_HEADER_IDENTIFIER)).to_be_visible()
+
+    sidebar_expander = app.locator(
+        "[data-testid='stSidebar'] [data-testid='stExpander']"
+    ).first
+    expect(sidebar_expander.locator(EXPANDER_HEADER_IDENTIFIER)).to_be_visible()
+
+
+def test_expander_displays_correctly(
+    themed_app: Page, assert_snapshot: ImageCompareFunction
+):
+    """Test that sidebar and main container expanders are displayed correctly."""
+    # Focus the button, then ensure it's not cut off
+    themed_app.locator(".stButton button").focus()
+    assert_snapshot(themed_app.locator(".main"), name="expanders-in-main")
+    assert_snapshot(
+        themed_app.locator("[data-testid='stSidebar']"),
+        name="expanders-in-sidebar",
+    )
+
+
+def test_expander_collapses_and_expands(app: Page):
+    """Test that an expander collapses and expands."""
+    main_expanders = app.locator(".main [data-testid='stExpander']")
+    expect(main_expanders).to_have_count(2)
+
+    expanders = main_expanders.all()
+    # Starts expanded
+    expander_header = expanders[0].locator(EXPANDER_HEADER_IDENTIFIER)
+    expect(expander_header).to_be_visible()
+    toggle = expander_header.locator("svg").first
+    expect(toggle).to_be_visible()
+    expander_header.click()
+    toggle = expander_header.locator("svg").first
+    expect(toggle).to_be_visible()
+
+    # Starts collapsed
+    expander_header = expanders[1].locator(EXPANDER_HEADER_IDENTIFIER)
+    expect(expander_header).to_be_visible()
+    toggle = expander_header.locator("svg").first
+    expect(toggle).to_be_visible()
+    expander_header.click()
+    toggle = expander_header.locator("svg").first
+    expect(toggle).to_be_visible()
