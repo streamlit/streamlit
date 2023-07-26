775b3edba9b6937dbf3f24e92fe865c090377a10
lukasmasuch <lukas.masuch@gmail.com>
2023-07-26 11:48:38 +0200

    Remove wait time

--- e2e/playwright/conftest.py
+++ e2e/playwright/conftest.py
@@ -151,8 +151,8 @@ def wait_for_app_run(page: Page):
     page.wait_for_selector(
         "[data-testid='stStatusWidget']", timeout=20000, state="detached"
     )
-    # Give the app a little more time to render everything
-    page.wait_for_timeout(250)
+    # # Give the app a little more time to render everything
+    # page.wait_for_timeout(250)
 
 
 def wait_for_app_loaded(page: Page):
