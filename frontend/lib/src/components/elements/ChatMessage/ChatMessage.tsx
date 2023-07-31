d0434bc8cda1453a90d6a84efc47d3bf8fadad62
lukasmasuch <lukas.masuch@gmail.com>
2023-07-31 12:16:11 +0200

    Also check for human for chat message background

--- frontend/lib/src/components/elements/ChatMessage/ChatMessage.tsx
+++ frontend/lib/src/components/elements/ChatMessage/ChatMessage.tsx
@@ -91,7 +91,7 @@ const ChatMessage: React.FC<ChatMessageProps> = ({
     <StyledChatMessageContainer
       className="stChatMessage"
       data-testid="stChatMessage"
-      background={name.toLowerCase() === "user"}
+      background={["name", "human"].includes(name.toLowerCase())}
     >
       <ChatMessageAvatar name={name} avatar={avatar} avatarType={avatarType} />
       <StyledMessageContent
