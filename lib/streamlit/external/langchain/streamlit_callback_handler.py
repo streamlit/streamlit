89cc355ad7245bb6b1e47c612f8cbe9a66754c4c
lukasmasuch <lukas.masuch@gmail.com>
2023-08-15 18:03:19 +0200

    Use smaller block

--- lib/streamlit/external/langchain/streamlit_callback_handler.py
+++ lib/streamlit/external/langchain/streamlit_callback_handler.py
@@ -174,7 +174,7 @@ class LLMThought:
         self._llm_token_stream += _convert_newlines(token)
         if self._llm_token_stream_placeholder is None:
             self._llm_token_stream_placeholder = self._container.empty()
-        self._llm_token_stream_placeholder.markdown(self._llm_token_stream + " ▌")
+        self._llm_token_stream_placeholder.markdown(self._llm_token_stream + " ▎")
 
     def on_llm_end(self, response: LLMResult, **kwargs: Any) -> None:
         # `response` is the concatenation of all the tokens received by the LLM.
