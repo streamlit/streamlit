fd7d483a749c7b14741da000ae4b02c0e0113e33
Maya Barnes <maya8617@gmail.com>
2023-08-17 14:05:55 -0700

    Fix missing import

--- e2e/playwright/st_headings_divider_test.py
+++ e2e/playwright/st_headings_divider_test.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
+import pytest
 from playwright.sync_api import Page, expect
 
 from conftest import ImageCompareFunction
