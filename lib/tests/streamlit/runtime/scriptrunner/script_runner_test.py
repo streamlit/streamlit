b94cd408a583ad2362855a44bcabbcf8448106d2
Amanda Walker <amanda@amandawalker.io>
2023-08-21 16:18:35 -0700

    Delete a flaky test
    It tests the deprecated `st.cache` behavior, which we aren't going to
    change just to fix a race condition, so we should at least stop being
    subject to flaky tests.

--- lib/tests/streamlit/runtime/scriptrunner/script_runner_test.py
+++ lib/tests/streamlit/runtime/scriptrunner/script_runner_test.py
@@ -760,39 +760,6 @@ class ScriptRunnerTest(AsyncTestCase):
                 ],
             )
 
-    def test_rerun_caching(self):
-        """Test that st.caches are maintained across script runs."""
-        # Make sure there are no caches from other tests.
-        caching._mem_caches.clear()
-
-        # Run st_cache_script.
-        runner = TestScriptRunner("st_cache_script.py")
-        runner.request_rerun(RerunData())
-        runner.start()
-        runner.join()
-
-        # The script has 5 cached functions, each of which writes out
-        # some text.
-        self._assert_text_deltas(
-            runner,
-            [
-                "cached function called",
-                "cached function called",
-                "cached function called",
-                "cached function called",
-                "cached_depending_on_not_yet_defined called",
-            ],
-        )
-
-        # Re-run the script on a second runner.
-        runner = TestScriptRunner("st_cache_script.py")
-        runner.request_rerun(RerunData())
-        runner.start()
-        runner.join()
-
-        # The cached functions should not have been called on this second run
-        self._assert_text_deltas(runner, [])
-
     def test_invalidating_cache(self):
         """Test that st.caches are cleared when a dependency changes."""
         # Make sure there are no caches from other tests.
