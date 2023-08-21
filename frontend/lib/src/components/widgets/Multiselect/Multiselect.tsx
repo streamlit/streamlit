6ca644be0389df2f795c8b29ece7af4a4359c274
Maya Barnes <63436329+mayagbarnes@users.noreply.github.com>
2023-08-21 11:07:18 -0700

    Fix: st.multiselect No Results vs. max selection message (#7205)
    Fix logic for whether st.multiselect renders "No Results" or max selection message

--- frontend/lib/src/components/widgets/Multiselect/Multiselect.tsx
+++ frontend/lib/src/components/widgets/Multiselect/Multiselect.tsx
@@ -78,12 +78,16 @@ class Multiselect extends React.PureComponent<Props, State> {
   }
 
   private getNoResultsMsg(): string {
-    if (this.props.element.maxSelections === 0) {
+    const { maxSelections } = this.props.element
+    const { value } = this.state
+
+    if (maxSelections === 0) {
       return "No results"
+    } else if (value.length === maxSelections) {
+      const option = maxSelections !== 1 ? "options" : "option"
+      return `You can only select up to ${maxSelections} ${option}. Remove an option first.`
     }
-    const option =
-      this.props.element.maxSelections !== 1 ? "options" : "option"
-    return `You can only select up to ${this.props.element.maxSelections} ${option}. Remove an option first.`
+    return "No results"
   }
 
   get initialValue(): number[] {
@@ -240,7 +244,11 @@ class Multiselect extends React.PureComponent<Props, State> {
     const showKeyboardOnMobile = options.length > 10
 
     return (
-      <div className="row-widget stMultiSelect" style={style}>
+      <div
+        className="row-widget stMultiSelect"
+        data-testid="stMultiSelect"
+        style={style}
+      >
         <WidgetLabel
           label={element.label}
           disabled={disabled}
