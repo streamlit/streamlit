d53c65251d8d0f618ccc48bed68878e4450fbaee
Karen Javadyan <kajarenc@gmail.com>
2023-08-23 19:47:51 +0400

    add st.link_button frontend

---
+++ frontend/lib/src/components/elements/LinkButton/LinkButton.tsx
@@ -0,0 +1,79 @@
+/**
+ * Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import React, { ReactElement } from "react"
+import { LinkButton as LinkButtonProto } from "@streamlit/lib/src/proto"
+import BaseButton, {
+  BaseButtonTooltip,
+  BaseButtonKind,
+  BaseButtonSize,
+} from "@streamlit/lib/src/components/shared/BaseButton"
+import StreamlitMarkdown from "@streamlit/lib/src/components/shared/StreamlitMarkdown"
+
+export interface Props {
+  disabled: boolean
+  element: LinkButtonProto
+  width: number
+}
+
+function LinkButton(props: Props): ReactElement {
+  const { disabled, element, width } = props
+  const style = { width }
+
+  const kind =
+    element.type === "primary"
+      ? BaseButtonKind.PRIMARY
+      : BaseButtonKind.SECONDARY
+
+  const handleLinkClick: () => void = () => {
+    const link = document.createElement("a")
+    link.setAttribute("href", element.url)
+    link.setAttribute("target", "_blank")
+    link.click()
+  }
+
+  // When useContainerWidth true & has help tooltip,
+  // we need to pass the container width down to the button
+  const fluidWidth = element.help ? width : true
+
+  return (
+    <div
+      className="row-widget stLinkButton"
+      data-testid="stLinkButton"
+      style={style}
+    >
+      <BaseButtonTooltip help={element.help}>
+        <BaseButton
+          kind={kind}
+          size={BaseButtonSize.SMALL}
+          disabled={disabled}
+          onClick={handleLinkClick}
+          fluidWidth={element.useContainerWidth ? fluidWidth : false}
+        >
+          <StreamlitMarkdown
+            source={element.label}
+            allowHTML={false}
+            isLabel
+            largerLabel
+            disableLinks
+          />
+        </BaseButton>
+      </BaseButtonTooltip>
+    </div>
+  )
+}
+
+export default LinkButton
