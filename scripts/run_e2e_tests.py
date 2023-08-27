8209c4ceee42834ed100219dbbc901f3a4126563
Kamil Breguła <kamil.bregula@snowflake.com>
2023-08-27 13:23:30 +0200

    Remove tests for component templates (#7239)

--- scripts/run_e2e_tests.py
+++ scripts/run_e2e_tests.py
@@ -31,12 +31,6 @@ import requests
 
 ROOT_DIR = dirname(dirname(abspath(__file__)))  # streamlit root directory
 FRONTEND_DIR = join(ROOT_DIR, "frontend")
-COMPONENT_TEMPLATE_DIRS = {
-    "template": join(ROOT_DIR, "component-template/template/my_component"),
-    "template-reactless": join(
-        ROOT_DIR, "component-template/template-reactless/my_component"
-    ),
-}
 
 CREDENTIALS_FILE = os.path.expanduser("~/.streamlit/credentials.toml")
 
@@ -305,41 +299,6 @@ def run_test(
     return result == SUCCESS
 
 
-def run_component_template_e2e_test(ctx: Context, template_dir: str, name: str) -> bool:
-    """Build a component template and run its e2e tests."""
-    frontend_dir = join(template_dir, "frontend")
-
-    # Install the template's npm dependencies into its node_modules.
-    subprocess.run(
-        ["yarn", "install"],
-        cwd=frontend_dir,
-        capture_output=True,
-        text=True,
-        check=True,
-    )
-
-    # Start the template's dev server.
-    with AsyncSubprocess(["yarn", "start"], cwd=frontend_dir) as webpack_proc:
-        # Run the test!
-        main_script_path = join(template_dir, "__init__.py")
-        spec_path = join(ROOT_DIR, "e2e/specs/component_template.spec.js")
-
-        ctx.cypress_env_vars["COMPONENT_TEMPLATE_TYPE"] = name
-        success = run_test(ctx, spec_path, ["streamlit", "run", main_script_path])
-        del ctx.cypress_env_vars["COMPONENT_TEMPLATE_TYPE"]
-
-        webpack_stdout = webpack_proc.terminate()
-
-    if not success:
-        click.echo(
-            f"{click.style('webpack output:', fg='yellow', bold=True)}"
-            f"\n{webpack_stdout}"
-            f"\n"
-        )
-
-    return success
-
-
 def is_app_server_alive():
     try:
         r = requests.get("http://localhost:3000/", timeout=3)
@@ -464,12 +423,6 @@ def run_e2e_tests(
                     ["streamlit", "hello", "--server.headless=true"],
                     show_output=verbose,
                 )
-
-            elif basename(spec_path) == "component_template.spec.js":
-                if flaky_tests:
-                    continue
-                for name, template_dir in COMPONENT_TEMPLATE_DIRS.items():
-                    run_component_template_e2e_test(ctx, template_dir, name)
 
             elif basename(spec_path) == "multipage_apps.spec.js":
                 test_name, _ = splitext(basename(spec_path))
