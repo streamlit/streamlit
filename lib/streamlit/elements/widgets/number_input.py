8cd45fdae3cafafe3ba64f29be5b6307390d253d
lukasmasuch <lukas.masuch@gmail.com>
2023-08-31 20:07:20 +0200

    Update docstring

--- lib/streamlit/elements/widgets/number_input.py
+++ lib/streamlit/elements/widgets/number_input.py
@@ -165,9 +165,9 @@ class NumberInputMixin:
             The maximum permitted value.
             If None, there will be no maximum.
         value : int, float, or None
-            The value of this widget when it first renders. If ``None``, the widget will be
-            initialized as empty and returns ``None`` as long as the viewer didn't provide
-            input. Defaults to min_value, or 0.0 if min_value is None.
+            The value of this widget when it first renders. If ``None``, the widget
+            will be  initialized as empty and returns ``None`` as long as the viewer
+            didn't provide input. Defaults to min_value, or 0.0 if min_value is None.
         step : int, float, or None
             The stepping interval.
             Defaults to 1 if the value is an int, 0.01 otherwise.
