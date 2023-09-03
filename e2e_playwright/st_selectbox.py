d10ecb498255971a4f277f4b26af66ba9723d840
lukasmasuch <lukas.masuch@gmail.com>
2023-09-03 11:19:57 +0200

    Mini changes

--- e2e_playwright/st_selectbox.py
+++ e2e_playwright/st_selectbox.py
@@ -22,7 +22,7 @@ st.write("value 1:", i1)
 i2 = st.selectbox("selectbox 2", options, 0, format_func=lambda x: x.capitalize())
 st.write("value 2:", i2)
 
-i3 = st.selectbox("selectbox 3", [])
+i3: None = st.selectbox("selectbox 3", [])
 st.write("value 3:", i3)
 
 more_options = [
