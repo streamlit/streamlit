6a3bc1f03bc12301ffebbde74f1d5f6c16b2a111
Maya Barnes <maya8617@gmail.com>
2023-09-11 15:38:56 -0700

    Add app load wait

--- e2e_playwright/st_tabs_selection_test.py
+++ e2e_playwright/st_tabs_selection_test.py
@@ -14,7 +14,7 @@
 
 from playwright.sync_api import Page, expect
 
-from e2e_playwright.conftest import ImageCompareFunction
+from e2e_playwright.conftest import ImageCompareFunction, wait_for_app_run
 
 EXPANDER_HEADER_IDENTIFIER = ".streamlit-expanderHeader"
 
@@ -34,6 +34,8 @@ def test_maintains_selection_when_other_tab_added(
     tab_buttons.nth(1).click()
     # Select Add Tab 3 button
     control_buttons.nth(0).click()
+    # Wait for tabs to properly load
+    wait_for_app_run(app)
     assert_snapshot(app.locator(".stTabs"), name="tabs-selection-add-tab")
 
 
@@ -44,6 +46,7 @@ def test_maintains_selection_when_other_tab_removed(
     control_buttons = app.locator(".stButton")
     # Reset Tabs
     control_buttons.nth(5).click()
+    wait_for_app_run(app)
     # Add Tab 3
     control_buttons.nth(0).click()
     # Select Tab 3
@@ -51,6 +54,8 @@ def test_maintains_selection_when_other_tab_removed(
     tab_buttons.nth(2).click()
     # Select Remove Tab 1 button
     control_buttons.nth(1).click()
+    # Wait for tabs to properly load
+    wait_for_app_run(app)
     assert_snapshot(app.locator(".stTabs"), name="tabs-selection-remove-tab")
 
 
@@ -61,11 +66,14 @@ def test_resets_selection_when_selected_tab_removed(
     control_buttons = app.locator(".stButton")
     # Reset Tabs
     control_buttons.nth(5).click()
+    wait_for_app_run(app)
     # Select Tab 2
     tab_buttons = app.locator(".stTabs button[role=tab]")
     tab_buttons.nth(1).click()
     # Select Remove Tab 2 button
     control_buttons.nth(2).click()
+    # Wait for tabs to properly load
+    wait_for_app_run(app)
     assert_snapshot(app.locator(".stTabs"), name="tabs-remove-selected")
 
 
@@ -76,10 +84,16 @@ def test_maintains_selection_when_same_name_exists(
     control_buttons = app.locator(".stButton")
     # Reset Tabs
     control_buttons.nth(5).click()
+    wait_for_app_run(app)
     # Add Tab 3
     control_buttons.nth(0).click()
+    # Select Tab 2
+    tab_buttons = app.locator(".stTabs button[role=tab]")
+    tab_buttons.nth(1).click()
     # Change Tab 1 & 3 Names
     control_buttons.nth(3).click()
+    # Wait for tabs to properly load
+    wait_for_app_run(app)
     assert_snapshot(app.locator(".stTabs"), name="tabs-change-some-names")
 
 
@@ -90,6 +104,12 @@ def test_resets_selection_when_tab_names_change(
     control_buttons = app.locator(".stButton")
     # Reset Tabs
     control_buttons.nth(5).click()
+    wait_for_app_run(app)
+    # Select Tab 2
+    tab_buttons = app.locator(".stTabs button[role=tab]")
+    tab_buttons.nth(1).click()
     # Change All Tab Names
     control_buttons.nth(4).click()
+    # Wait for tabs to properly load
+    wait_for_app_run(app)
     assert_snapshot(app.locator(".stTabs"), name="tabs-change-all-names")
