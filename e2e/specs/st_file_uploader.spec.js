47de44453fdb9f065fea7e4a42b353aafc314163
Maya Barnes <maya8617@gmail.com>
2023-09-13 14:29:16 -0700

    Try adding retry to individual flaky tests

--- e2e/specs/st_file_uploader.spec.js
+++ e2e/specs/st_file_uploader.spec.js
@@ -177,7 +177,7 @@ describe("st.file_uploader", () => {
     });
   });
 
-  it("uploads and deletes multiple files quickly", () => {
+  it("uploads and deletes multiple files quickly", { retries: { runMode: 1 } }, () => {
     const fileName1 = "file1.txt";
     const fileName2 = "file2.txt";
     const uploaderIndex = 2;
@@ -396,7 +396,7 @@ describe("st.file_uploader", () => {
   });
 
   // regression test for https://github.com/streamlit/streamlit/issues/4256 bug
-  it("does not call a callback when not changed", () => {
+  it("does not call a callback when not changed", { retries: { runMode: 1 } }, () => {
     const fileName1 = "file1.txt";
     const uploaderIndex = 6;
 
