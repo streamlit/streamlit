1d040d78fd6b039b1efb07bed9d9101a90b6119f
Debbie Matthews <debbie.matthews@snowflake.com>
2023-09-15 06:41:55 -0700

    Update st.text_area docstring example

--- lib/streamlit/elements/widgets/text_widgets.py
+++ lib/streamlit/elements/widgets/text_widgets.py
@@ -375,11 +375,13 @@ class TextWidgetsMixin:
         -------
         >>> import streamlit as st
         >>>
-        >>> txt = st.text_area('Text to analyze', 'It was the best of times, it was '\
-        ...     'the worst of times, it was the age of wisdom, it was the age of '\
-        ...     'foolishness, it was the epoch of belief, it was the epoch of '\
-        ...     'incredulity, it was the season of Light, it was the season of Darkness, '\
-        ...     'it was the spring of hope, it was the winter of despair, (...)'
+        >>> txt = st.text_area(
+        ...     "Text to analyze",
+        ...     "It was the best of times, it was the worst of times, it was the age of "
+        ...     "wisdom, it was the age of foolishness, it was the epoch of belief, it "
+        ...     "was the epoch of incredulity, it was the season of Light, it was the "
+        ...     "season of Darkness, it was the spring of hope, it was the winter of "
+        ...     "despair, (...)",
         ...     )
         >>>
         >>> st.write(f'You wrote {len(txt)} characters.')
