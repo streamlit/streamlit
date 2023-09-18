88aa92ab5df5aa20a41b568836a7257784c6791e
willhuang1997 <willhuang1997@gmail.com>
2023-09-18 10:56:12 -0700

    Fix super nit

--- frontend/lib/src/hocs/withPagination/Pagination.test.tsx
+++ frontend/lib/src/hocs/withPagination/Pagination.test.tsx
@@ -15,8 +15,8 @@
  */
 
 import React from "react"
-import { fireEvent, screen } from "@testing-library/react"
 import "@testing-library/jest-dom"
+import { fireEvent, screen } from "@testing-library/react"
 import { render } from "@streamlit/lib/src/test_util"
 import Pagination, { Props } from "./Pagination"
 
