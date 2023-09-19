91555211c440e40a9ae2b5c1222d7a8f8275025a
William Wei Huang <william.huang@snowflake.com>
2023-09-19 16:01:39 -0700

    Migrate FileDropzoneInstructions to RTL (#7379)
    * Migrate FileDropzoneInstructions to RTL
    
    * Fix lint
    
    * Fix comment
    
    ---------
    
    Co-authored-by: willhuang1997 <willhuang1997@gmail.com>

--- frontend/lib/src/components/widgets/FileUploader/FileDropzoneInstructions.tsx
+++ frontend/lib/src/components/widgets/FileUploader/FileDropzoneInstructions.tsx
@@ -38,7 +38,7 @@ const FileDropzoneInstructions = ({
   acceptedExtensions,
   maxSizeBytes,
 }: Props): React.ReactElement => (
-  <StyledFileDropzoneInstructions>
+  <StyledFileDropzoneInstructions data-testid="stFileDropzoneInstructions">
     <StyledFileDropzoneInstructionsFileUploaderIcon>
       <Icon content={CloudUpload} size="threeXL" />
     </StyledFileDropzoneInstructionsFileUploaderIcon>
