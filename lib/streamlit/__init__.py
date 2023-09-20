339467f9fd7db704300bbe31e4506f4c6315173f
Vincent Donato <vincent@streamlit.io>
2023-09-20 15:55:32 -0700

    Add name_override arg to deprecate_func_name

--- lib/streamlit/__init__.py
+++ lib/streamlit/__init__.py
@@ -227,5 +227,5 @@ experimental_set_query_params = _set_query_params
 experimental_rerun = _experimental_rerun
 experimental_data_editor = _main.experimental_data_editor
 experimental_connection = _deprecate_func_name(
-    connection, "experimental_connection", "connection", "2024-01-01"
+    connection, "experimental_connection", "2024-01-01", name_override="connection"
 )
