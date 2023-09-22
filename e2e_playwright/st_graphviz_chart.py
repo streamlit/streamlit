31b02f1b888ccb2a2393f20e320a33cd626fab24
snehankekre <snehan.minerva@gmail.com>
2023-09-22 20:59:13 +0530

    Migrate GraphVizChart e2e tests to playwright

---
+++ e2e_playwright/st_graphviz_chart.py
@@ -0,0 +1,87 @@
+# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import graphviz as graphviz
+
+import streamlit as st
+
+# basic graph
+hello = graphviz.Digraph("Hello World")
+hello.edge("Hello", "World")
+
+# styled graph
+styled = graphviz.Graph("G", filename="g_c_n.gv")
+styled.attr(bgcolor="purple:pink", label="agraph", fontcolor="white")
+
+with styled.subgraph(name="cluster1") as c:
+    c.attr(
+        fillcolor="blue:cyan",
+        label="acluster",
+        fontcolor="white",
+        style="filled",
+        gradientangle="270",
+    )
+    c.attr(
+        "node", shape="box", fillcolor="red:yellow", style="filled", gradientangle="90"
+    )
+    c.node("anode")
+
+# complex graph
+finite = graphviz.Digraph("finite_state_machine", filename="fsm.gv")
+finite.attr(rankdir="LR", size="8,5")
+
+finite.attr("node", shape="doublecircle")
+finite.node("LR_0")
+finite.node("LR_3")
+finite.node("LR_4")
+finite.node("LR_8")
+
+finite.attr("node", shape="circle")
+finite.edge("LR_0", "LR_2", label="SS(B)")
+finite.edge("LR_0", "LR_1", label="SS(S)")
+finite.edge("LR_1", "LR_3", label="S($end)")
+finite.edge("LR_2", "LR_6", label="SS(b)")
+finite.edge("LR_2", "LR_5", label="SS(a)")
+finite.edge("LR_2", "LR_4", label="S(A)")
+finite.edge("LR_5", "LR_7", label="S(b)")
+finite.edge("LR_5", "LR_5", label="S(a)")
+finite.edge("LR_6", "LR_6", label="S(b)")
+finite.edge("LR_6", "LR_5", label="S(a)")
+finite.edge("LR_7", "LR_8", label="S(b)")
+finite.edge("LR_7", "LR_5", label="S(a)")
+finite.edge("LR_8", "LR_6", label="S(b)")
+finite.edge("LR_8", "LR_5", label="S(a)")
+
+# draw graphs
+st.graphviz_chart(hello)
+
+st.graphviz_chart(styled)
+
+st.graphviz_chart(finite)
+
+# draw graphs in columns
+
+left_graph = graphviz.Digraph("Left")
+left_graph.edge("Left", "Graph")
+
+right_graph = graphviz.Digraph("Right")
+right_graph.edge("Right", "Graph")
+
+col1, col2 = st.columns([1, 1])
+
+with col1:
+    st.graphviz_chart(left_graph)
+
+with col2:
+    st.graphviz_chart(right_graph)
---
+++ e2e_playwright/st_graphviz_chart_test.py
@@ -0,0 +1,80 @@
+# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from playwright.sync_api import Page
+
+from e2e_playwright.conftest import ImageCompareFunction
+
+
+def get_first_graph_svg(app: Page):
+    return app.locator(".stGraphVizChart > svg").nth(0)
+
+
+def test_initial_setup(app: Page):
+    """Initial setup: ensure charts are loaded."""
+
+    title_count = len(
+        app.evaluate("document.querySelectorAll('.stGraphVizChart > svg > g > title')")
+    )
+    assert title_count == 5
+
+
+def test_shows_left_and_right_graph(app: Page):
+    """Test if it shows left and right graph."""
+    left_text = app.evaluate(
+        "document.querySelectorAll('.stGraphVizChart > svg > g > title')[3].textContent"
+    )
+
+    right_text = app.evaluate(
+        "document.querySelectorAll('.stGraphVizChart > svg > g > title')[4].textContent"
+    )
+    assert "Left" in left_text and "Right" in right_text
+
+
+def test_first_graph_dimensions(app: Page):
+    """Test the dimensions of the first graph."""
+    first_graph_svg = get_first_graph_svg(app)
+    assert first_graph_svg.get_attribute("width") == "79pt"
+    assert first_graph_svg.get_attribute("height") == "116pt"
+
+
+def test_first_graph_fullscreen(app: Page, assert_snapshot: ImageCompareFunction):
+    """Test if the first graph shows in fullscreen."""
+    # Hover over the parent div
+    app.locator(".stGraphVizChart").nth(0).hover()
+
+    # Enter fullscreen
+    app.locator('[data-testid="StyledFullScreenButton"]').nth(0).click()
+
+    first_graph_svg = get_first_graph_svg(app)
+    assert first_graph_svg.get_attribute("width") == "100%"
+    assert first_graph_svg.get_attribute("height") == "100%"
+    assert_snapshot(first_graph_svg, name="graphviz_fullscreen")
+
+
+def test_first_graph_after_exit_fullscreen(
+    app: Page, assert_snapshot: ImageCompareFunction
+):
+    """Test if the first graph has correct size after exiting fullscreen."""
+    # Hover over the parent div
+    app.locator(".stGraphVizChart").nth(0).hover()
+
+    # Enter and exit fullscreen
+    app.locator("[data-testid='StyledFullScreenButton']").nth(0).click()
+    app.locator("[data-testid='StyledFullScreenButton']").nth(0).click()
+
+    first_graph_svg = get_first_graph_svg(app)
+    assert first_graph_svg.get_attribute("width") == "79pt"
+    assert first_graph_svg.get_attribute("height") == "116pt"
+    assert_snapshot(first_graph_svg, name="graphviz_after_exit_fullscreen")
