a4ec9477bc42bb9e48888127112a9cdae7b24032
Lukas Masuch <Lukas.Masuch@gmail.com>
2023-09-28 19:41:53 +0100

    Remove legacy dataframe serialization (#7429)
    * Remove legacy serialization
    
    * Additional changes and fixes
    
    * Remove unused name
    
    * Remove react-virtualized
    
    * Remove immutable
    
    * Remove legacy prototypes from delta generator
    
    * Mini fix
    
    * Remove arrow from e2e playwright tests
    
    * Remove migrated arrow tests
    
    * Fix namings
    
    * Remove outdated prefix
    
    * Fix namings
    
    * Update expiration date

--- e2e/scripts/st_arrow_chart_utc_time.py
+++ e2e/scripts/st_arrow_chart_utc_time.py
@@ -32,11 +32,11 @@ df = pd.DataFrame(
 
 df.set_index("index", inplace=True)
 
-# st._arrow_area/_arrow_bar/_arrow_line_chart all use Altair/Vega-Lite under the hood.
+# st.area/bar/line_chart all use Altair/Vega-Lite under the hood.
 # By default, Vega-Lite displays time values in the browser's local
 # time zone. In `altair.generate_chart`, we explicitly set the time
 # display to UTC, so that our results are consistent. This test verifies
 # that change!
-st._arrow_area_chart(df)
-st._arrow_bar_chart(df)
-st._arrow_line_chart(df)
+st.area_chart(df)
+st.bar_chart(df)
+st.line_chart(df)
--- lib/tests/streamlit/runtime/forward_msg_cache_test.py
+++ lib/tests/streamlit/runtime/forward_msg_cache_test.py
@@ -18,7 +18,6 @@ import unittest
 from unittest.mock import MagicMock
 
 from streamlit import config
-from streamlit.elements import legacy_data_frame as data_frame
 from streamlit.proto.ForwardMsg_pb2 import ForwardMsg
 from streamlit.proto.RootContainer_pb2 import RootContainer
 from streamlit.runtime import app_session
@@ -28,13 +27,7 @@ from streamlit.runtime.forward_msg_cache import (
     populate_hash_if_needed,
 )
 from streamlit.runtime.stats import CacheStat
-
-
-def _create_dataframe_msg(df, id=1):
-    msg = ForwardMsg()
-    msg.metadata.delta_path[:] = [RootContainer.SIDEBAR, id]
-    data_frame.marshall_data_frame(df, msg.delta.new_element.data_frame)
-    return msg
+from tests.streamlit.message_mocks import create_dataframe_msg
 
 
 def _create_mock_session():
@@ -44,24 +37,24 @@ def _create_mock_session():
 class ForwardMsgCacheTest(unittest.TestCase):
     def test_msg_hash(self):
         """Test that ForwardMsg hash generation works as expected"""
-        msg1 = _create_dataframe_msg([1, 2, 3])
-        msg2 = _create_dataframe_msg([1, 2, 3])
+        msg1 = create_dataframe_msg([1, 2, 3])
+        msg2 = create_dataframe_msg([1, 2, 3])
         self.assertEqual(populate_hash_if_needed(msg1), populate_hash_if_needed(msg2))
 
-        msg3 = _create_dataframe_msg([2, 3, 4])
+        msg3 = create_dataframe_msg([2, 3, 4])
         self.assertNotEqual(
             populate_hash_if_needed(msg1), populate_hash_if_needed(msg3)
         )
 
     def test_delta_metadata(self):
         """Test that delta metadata doesn't change the hash"""
-        msg1 = _create_dataframe_msg([1, 2, 3], 1)
-        msg2 = _create_dataframe_msg([1, 2, 3], 2)
+        msg1 = create_dataframe_msg([1, 2, 3], 1)
+        msg2 = create_dataframe_msg([1, 2, 3], 2)
         self.assertEqual(populate_hash_if_needed(msg1), populate_hash_if_needed(msg2))
 
     def test_reference_msg(self):
         """Test creation of 'reference' ForwardMsgs"""
-        msg = _create_dataframe_msg([1, 2, 3], 34)
+        msg = create_dataframe_msg([1, 2, 3], 34)
         ref_msg = create_reference_msg(msg)
         self.assertEqual(populate_hash_if_needed(msg), ref_msg.ref_hash)
         self.assertEqual(msg.metadata, ref_msg.metadata)
@@ -70,7 +63,7 @@ class ForwardMsgCacheTest(unittest.TestCase):
         """Test MessageCache.add_message and has_message_reference"""
         cache = ForwardMsgCache()
         session = _create_mock_session()
-        msg = _create_dataframe_msg([1, 2, 3])
+        msg = create_dataframe_msg([1, 2, 3])
         cache.add_message(msg, session, 0)
 
         self.assertTrue(cache.has_message_reference(msg, session, 0))
@@ -80,7 +73,7 @@ class ForwardMsgCacheTest(unittest.TestCase):
         """Test MessageCache.get_message"""
         cache = ForwardMsgCache()
         session = _create_mock_session()
-        msg = _create_dataframe_msg([1, 2, 3])
+        msg = create_dataframe_msg([1, 2, 3])
 
         msg_hash = populate_hash_if_needed(msg)
 
@@ -92,7 +85,7 @@ class ForwardMsgCacheTest(unittest.TestCase):
         cache = ForwardMsgCache()
         session = _create_mock_session()
 
-        msg = _create_dataframe_msg([1, 2, 3])
+        msg = create_dataframe_msg([1, 2, 3])
         msg_hash = populate_hash_if_needed(msg)
 
         cache.add_message(msg, session, 0)
@@ -108,17 +101,17 @@ class ForwardMsgCacheTest(unittest.TestCase):
         session2 = _create_mock_session()
 
         # Only session1 has a ref to msg1.
-        msg1 = _create_dataframe_msg([1, 2, 3])
+        msg1 = create_dataframe_msg([1, 2, 3])
         populate_hash_if_needed(msg1)
         cache.add_message(msg1, session1, 0)
 
         # Only session2 has a ref to msg2.
-        msg2 = _create_dataframe_msg([1, 2, 3, 4])
+        msg2 = create_dataframe_msg([1, 2, 3, 4])
         populate_hash_if_needed(msg2)
         cache.add_message(msg2, session2, 0)
 
         # Both session1 and session2 have a ref to msg3.
-        msg3 = _create_dataframe_msg([1, 2, 3, 4, 5])
+        msg3 = create_dataframe_msg([1, 2, 3, 4, 5])
         populate_hash_if_needed(msg2)
         cache.add_message(msg3, session1, 0)
         cache.add_message(msg3, session2, 0)
@@ -145,7 +138,7 @@ class ForwardMsgCacheTest(unittest.TestCase):
         session1 = _create_mock_session()
         runcount1 = 0
 
-        msg = _create_dataframe_msg([1, 2, 3])
+        msg = create_dataframe_msg([1, 2, 3])
         msg_hash = populate_hash_if_needed(msg)
 
         cache.add_message(msg, session1, runcount1)
@@ -185,11 +178,11 @@ class ForwardMsgCacheTest(unittest.TestCase):
         # Test empty cache
         self.assertEqual([], cache.get_stats())
 
-        msg1 = _create_dataframe_msg([1, 2, 3])
+        msg1 = create_dataframe_msg([1, 2, 3])
         populate_hash_if_needed(msg1)
         cache.add_message(msg1, session, 0)
 
-        msg2 = _create_dataframe_msg([5, 4, 3, 2, 1, 0])
+        msg2 = create_dataframe_msg([5, 4, 3, 2, 1, 0])
         populate_hash_if_needed(msg2)
         cache.add_message(msg2, session, 0)
 
