d9133c423eb88220f51cc508772e1a419c0d7eef
kopp <kopp.michael@yahoo.de>
2023-09-29 20:31:59 +0200

    use Mapping instead of Dict in column_config_utils.py

--- lib/streamlit/elements/lib/column_config_utils.py
+++ lib/streamlit/elements/lib/column_config_utils.py
@@ -16,7 +16,7 @@ from __future__ import annotations
 
 import json
 from enum import Enum
-from typing import Dict, List, Optional, Union
+from typing import Dict, List, Mapping, Optional, Union
 
 import pandas as pd
 import pyarrow as pa
@@ -382,8 +382,8 @@ def determine_dataframe_schema(
 
 
 # A mapping of column names/IDs to column configs.
-ColumnConfigMapping: TypeAlias = Dict[Union[IndexIdentifierType, str], ColumnConfig]
-ColumnConfigMappingInput: TypeAlias = Dict[
+ColumnConfigMapping: TypeAlias = Mapping[Union[IndexIdentifierType, str], ColumnConfig]
+ColumnConfigMappingInput: TypeAlias = Mapping[
     Union[IndexIdentifierType, str],
     Union[ColumnConfig, None, str],
 ]
