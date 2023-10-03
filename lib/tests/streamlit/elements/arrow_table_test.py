b616d15b0ceade848fb2b752cbf1bd30c8c7f8be
lukasmasuch <lukas.masuch@gmail.com>
2023-10-03 15:12:17 +0200

    Remove outdated code

--- lib/tests/streamlit/elements/arrow_table_test.py
+++ lib/tests/streamlit/elements/arrow_table_test.py
@@ -116,19 +116,6 @@ class ArrowTest(DeltaGeneratorTestCase):
 
     @patch(
         "streamlit.type_util.is_pandas_version_less_than",
-        MagicMock(return_value=True),
-    )
-    @patch.object(Styler, "_translate")
-    def test_pandas_version_below_1_3_0(self, mock_styler_translate):
-        """Tests that `styler._translate` is called without arguments in Pandas < 1.3.0"""
-        df = mock_data_frame()
-        styler = df.style.set_uuid("FAKE_UUID")
-
-        st.table(styler)
-        mock_styler_translate.assert_called_once_with()
-
-    @patch(
-        "streamlit.type_util.is_pandas_version_less_than",
         MagicMock(return_value=False),
     )
     @patch.object(Styler, "_translate")
