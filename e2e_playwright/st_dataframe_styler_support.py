abafd67388f08b76bc1eb70ecf02548746b5a7ed
lukasmasuch <lukas.masuch@gmail.com>
2023-10-04 15:38:30 +0200

    Some fixed to e2e test

--- e2e_playwright/st_dataframe_styler_support.py
+++ e2e_playwright/st_dataframe_styler_support.py
@@ -36,7 +36,7 @@ def highlight_first(value):
 
 
 df = pd.DataFrame(np.arange(0, 100, 1).reshape(10, 10))
-st.dataframe(df.style.applymap(highlight_first))
+st.dataframe(df.style.map(highlight_first))
 
 st.header("Pandas Styler: Background and font styling")
 
@@ -52,7 +52,7 @@ def highlight_max(s, props=""):
 
 
 # Passing style values w/ all color formats to test css-style-string parsing robustness.
-styled_df = df.style.applymap(style_negative, props="color:#FF0000;").applymap(
+styled_df = df.style.map(style_negative, props="color:#FF0000;").map(
     lambda v: "opacity: 20%;" if (v < 0.3) and (v > -0.3) else None
 )
 
@@ -94,7 +94,7 @@ styled_df = weather_df.style.pipe(make_pretty)
 
 st.dataframe(styled_df)
 
-st.header("Styled link column")
+st.header("LinkColumn with display value:")
 st.dataframe(
     pd.DataFrame(
         {
