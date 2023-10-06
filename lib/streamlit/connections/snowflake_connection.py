8aabb00681391703e28e0c867ba5a799a39a4b93
Vincent Donato <vincent@streamlit.io>
2023-10-06 16:29:59 -0700

    Fix some type errors and typos

--- lib/streamlit/connections/snowflake_connection.py
+++ lib/streamlit/connections/snowflake_connection.py
@@ -12,21 +12,30 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# NOTE: We won't always be able to import from snowflake.{connector, snowpark}.* so need
+# the `type: ignore` comment below, but that comment will explode if `warn-unused-ignores`
+# is turned on when the package is available. Unfortunately, mypy doesn't provide a good
+# way to configure this at a per-line level :(
+# mypy: no-warn-unused-ignores
+
 from __future__ import annotations
 
 from datetime import timedelta
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Sequence, cast
 
 import pandas as pd
 
 from streamlit.connections import BaseConnection
 from streamlit.connections.util import load_from_snowsql_config_file, running_in_sis
 from streamlit.errors import StreamlitAPIException
+from streamlit.runtime.caching import cache_data
 
 if TYPE_CHECKING:
-    from snowflake.connector import DictCursor as Cursor
-    from snowflake.connector import SnowflakeConnection as InternalSnowflakeConnection
-    from snowflake.snowpark.session import Session
+    from snowflake.connector import (  # type: ignore # isort: skip
+        SnowflakeConnection as InternalSnowflakeConnection,
+    )
+    from snowflake.connector.cursor import SnowflakeCursor  # type: ignore
+    from snowflake.snowpark.session import Session  # type: ignore
 
 
 _REQUIRED_CONNECTION_PARAMS = {"account"}
@@ -40,7 +49,7 @@ class SnowflakeConnection(BaseConnection["InternalSnowflakeConnection"]):
     """
 
     def _connect(self, **kwargs) -> "InternalSnowflakeConnection":
-        import snowflake.connector
+        import snowflake.connector  # type: ignore
         from snowflake.snowpark.context import get_active_session  # type: ignore
 
         # If we're running in SiS, just call get_active_session() and retrieve the
@@ -81,11 +90,11 @@ class SnowflakeConnection(BaseConnection["InternalSnowflakeConnection"]):
         # If we have a version of the Snowflake Python Connector installed that predates
         # the ConfigManager, look for credentials in  ~/.snowsql/config as a last
         # resort.
-        snowsql_config = load_from_snowsql_config_file()
+        snowsql_config = load_from_snowsql_config_file(self._connection_name)
         conn_kwargs = {**snowsql_config, **kwargs}
 
         for p in _REQUIRED_CONNECTION_PARAMS:
-            if p not in conn_params:
+            if p not in conn_kwargs:
                 raise StreamlitAPIException(f"Missing Snowflake connection param: {p}")
 
         return snowflake.connector.connect(**conn_kwargs)
@@ -136,11 +145,29 @@ class SnowflakeConnection(BaseConnection["InternalSnowflakeConnection"]):
         self,
         df: pd.DataFrame,
         table_name: str,
-        database: str = None,
-        schema: str = None,
-        chunk_size: int = None,
+        database: str | None = None,
+        schema: str | None = None,
+        chunk_size: int | None = None,
         **kwargs,
-    ) -> (bool, int, int, str):
+    ) -> tuple[  # This return type is somewhat horrifying but is the return type of pandas_tools.write_pandas, so we keep it consistent here.
+        bool,
+        int,
+        int,
+        Sequence[
+            tuple[
+                str,
+                str,
+                int,
+                int,
+                int,
+                int,
+                str | None,
+                int | None,
+                int | None,
+                str | None,
+            ]
+        ],
+    ]:
         """Call snowflake.connector.pandas_tools.write_pandas with this connection.
 
         This convenience method is simply a thin wrapper around the ``write_pandas``
@@ -148,9 +175,9 @@ class SnowflakeConnection(BaseConnection["InternalSnowflakeConnection"]):
         information, see the `Snowflake Python Connector documentation
         <https://docs.snowflake.com/en/developer-guide/python-connector/python-connector-api#write_pandas>`_.
         """
-        from snowflake.connector.pandas_tools import write_pandas
+        from snowflake.connector.pandas_tools import write_pandas  # type: ignore
 
-        return write_pandas(
+        return write_pandas(  # type: ignore
             conn=self._instance,
             df=df,
             table_name=table_name,
@@ -160,13 +187,13 @@ class SnowflakeConnection(BaseConnection["InternalSnowflakeConnection"]):
             **kwargs,
         )
 
-    def cursor(self) -> "Cursor":
+    def cursor(self) -> "SnowflakeCursor":
         """Return a PEP 249-compliant cursor object.
 
         For more information, see the `Snowflake Python Connector documentation
         <https://docs.snowflake.com/en/developer-guide/python-connector/python-connector-api#object-cursor>`_.
         """
-        return conn._instance.cursor()
+        return self._instance.cursor()
 
     @property
     def raw_connection(self) -> "InternalSnowflakeConnection":
@@ -185,4 +212,6 @@ class SnowflakeConnection(BaseConnection["InternalSnowflakeConnection"]):
         """
         from snowflake.snowpark.session import Session
 
-        return Session.builder.configs({"connection": self._instance}).create()
+        return cast(
+            Session, Session.builder.configs({"connection": self._instance}).create()
+        )
