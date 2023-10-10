6db073eb985b37752301738235d1b2fe909dd9c3
Ken McGrady <ken.mcgrady@gmail.com>
2023-10-09 18:36:38 -0700

    Fix Chat Message container to align and manage the autosizer handling (#7504)
    * Fix Chat Message container to align and manage the autosizer handling
    
    * Remove chat message snapshots
    
    * Add Updated Snapshots
    
    * Attempt to make new playwright test
    
    * Remove async def as it's not supported
    
    * Add new snapshots
    
    * Minor cleanup
    
    ---------
    
    Co-authored-by: willhuang1997 <willhuang1997@gmail.com>

--- e2e_playwright/st_chat_message_test.py
+++ e2e_playwright/st_chat_message_test.py
@@ -14,7 +14,7 @@
 
 from playwright.sync_api import Page, expect
 
-from e2e_playwright.conftest import ImageCompareFunction
+from e2e_playwright.conftest import ImageCompareFunction, wait_for_app_run
 
 
 def test_renders_chat_messages_correctly_1(
@@ -23,7 +23,12 @@ def test_renders_chat_messages_correctly_1(
     """Test if the chat messages render correctly"""
     # Wait a bit more to allow all images to load:
     chat_message_elements = themed_app.locator(".stChatMessage")
-    expect(chat_message_elements).to_have_count(10)
+    expect(chat_message_elements).to_have_count(12)
+
+    # rerun to populate session state chat message
+    themed_app.keyboard.press("r")
+    wait_for_app_run(themed_app, wait_delay=1000)
+    expect(chat_message_elements).to_have_count(14)
     for i, element in enumerate(chat_message_elements.all()):
         element.scroll_into_view_if_needed()
         # Wait a bit more to allow the avatar images to load:
