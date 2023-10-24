95538edf9cb3c9d8196a4af048d289d40c8a31c9
Karen Javadyan <kajarenc@gmail.com>
2023-10-24 17:35:46 +0400

    Add explanatory comments

--- e2e_playwright/conftest.py
+++ e2e_playwright/conftest.py
@@ -250,6 +250,17 @@ def app(page: Page, app_port: int) -> Page:
 
 @pytest.fixture(scope="session")
 def browser_type_launch_args(browser_type_launch_args: Dict, browser_name: str):
+    """Fixture that adds the fake device and ui args to the browser type launch args."""
+    # The browser context fixture in pytest-playwright is defined in session scope, and
+    # depends on the browser_type_launch_args fixture. This means that we can't
+    # redefine the browser_type_launch_args fixture more narrow scope
+    # e.g. function or module scope.
+    # https://github.com/microsoft/playwright-pytest/blob/ef99541352b307411dbc15c627e50f95de30cc71/pytest_playwright/pytest_playwright.py#L128
+
+    # We need to extend browser launch args to support fake video stream for
+    # st.camera_input test.
+    # https://github.com/microsoft/playwright/issues/4532#issuecomment-1491761713
+
     if browser_name == "chromium":
         browser_type_launch_args = {
             **browser_type_launch_args,
@@ -259,7 +270,7 @@ def browser_type_launch_args(browser_type_launch_args: Dict, browser_name: str):
             ],
         }
 
-    if browser_name == "firefox":
+    elif browser_name == "firefox":
         browser_type_launch_args = {
             **browser_type_launch_args,
             "firefox_user_prefs": {
@@ -268,10 +279,6 @@ def browser_type_launch_args(browser_type_launch_args: Dict, browser_name: str):
                 "permissions.default.microphone": 1,
                 "permissions.default.camera": 1,
             },
-            "args": [
-                "--use-fake-device-for-media-stream",
-                "--use-fake-ui-for-media-stream",
-            ],
         }
     return browser_type_launch_args
 
