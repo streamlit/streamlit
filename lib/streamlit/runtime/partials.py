8975b75c0679b1d90320e117e76f8defe6aaec61
lukasmasuch <lukas.masuch@gmail.com>
2023-10-26 15:31:09 -0700

    More changes

--- lib/streamlit/runtime/partials.py
+++ lib/streamlit/runtime/partials.py
@@ -1,3 +1,19 @@
+# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from __future__ import annotations
+
 import hashlib
 from functools import wraps
 from typing import (
@@ -79,10 +95,23 @@ def partial(func: Optional[F] = None) -> Union[Callable[[F], F], F]:
             ctx.current_partial_id = None
             return result
 
-        partial_storage[partial_id] = cloudpickle.dumps(wrapped_group)
+        save_partial(partial_id, wrapped_group)
         return wrapped_group()
 
     return wrap
+
+
+def load_partial(partial_id: str) -> Callable[[], Any] | None:
+    partial_storage = PartialsStorage()
+    if partial_id not in partial_storage:
+        return None
+
+    return cloudpickle.loads(partial_storage[partial_id])  # type: ignore
+
+
+def save_partial(partial_id: str, partial_function: Callable[[], Any]) -> None:
+    partial_storage = PartialsStorage()
+    partial_storage[partial_id] = cloudpickle.dumps(partial_function)
 
 
 class PartialsStorage(MutableMapping[str, bytes]):
@@ -119,6 +148,9 @@ class PartialsStorage(MutableMapping[str, bytes]):
     def __delitem__(self, key: str) -> None:
         """Delete the bytes of a given partial."""
         del self._get_partials_state()[key]
+
+    def __contains__(self, key: str) -> bool:
+        return key in self._get_partials_state()
 
     def clear(self) -> None:
         return self._get_partials_state().clear()
