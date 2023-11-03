8ef684bb816cb59e25f1fc094b397c1c9fa003f9
Alex Howard <thezanke@gmail.com>
2023-11-03 19:14:35 -0400

    Fix how connection is declared for typing purposes (#7671)
    Fixes the `"connection" is not a known member of module "streamlit"` error

--- lib/streamlit/__init__.py
+++ lib/streamlit/__init__.py
@@ -62,7 +62,7 @@ from streamlit.runtime.caching import (
     experimental_memo as _experimental_memo,
 )
 from streamlit.runtime.connection_factory import (
-    connection_factory as connection,
+    connection_factory as _connection,
 )
 from streamlit.runtime.metrics_util import gather_metrics as _gather_metrics
 from streamlit.runtime.secrets import secrets_singleton as _secrets_singleton
@@ -198,6 +198,9 @@ cache_resource = _cache_resource
 
 # Namespaces
 column_config = _column_config
+
+# Connection
+connection = _connection
 
 # Experimental APIs
 experimental_user = _UserInfoProxy()
