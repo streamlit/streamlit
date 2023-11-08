c77cb387e64818f44b02375a5af2331d9b907a77
Maya Barnes <63436329+mayagbarnes@users.noreply.github.com>
2023-11-07 18:27:19 -0800

    Migrate Selectbox to RTL (#7673)
    Convert Selectbox.test.tsx to RTL

--- frontend/lib/src/components/shared/Dropdown/Selectbox.test.tsx
+++ frontend/lib/src/components/shared/Dropdown/Selectbox.test.tsx
@@ -15,10 +15,10 @@
  */
 
 import React from "react"
-import { ShallowWrapper } from "enzyme"
-import { shallow, mount } from "@streamlit/lib/src/test_util"
+import "@testing-library/jest-dom"
+import { screen, fireEvent } from "@testing-library/react"
+import { render } from "@streamlit/lib/src/test_util"
 
-import { Select as UISelect } from "baseui/select"
 import { LabelVisibilityOptions } from "@streamlit/lib/src/util/utils"
 import { Selectbox, Props, fuzzyFilterSelectOptions } from "./Selectbox"
 import { mockTheme } from "@streamlit/lib/src/mocks/mockTheme"
@@ -39,43 +39,38 @@ const getProps = (props: Partial<Props> = {}): Props => ({
 
 describe("Selectbox widget", () => {
   let props: Props
-  let wrapper: ShallowWrapper
 
   beforeEach(() => {
     props = getProps()
-    wrapper = shallow(<Selectbox {...props} />)
   })
 
   it("renders without crashing", () => {
-    expect(wrapper.find(UISelect).length).toBeTruthy()
+    render(<Selectbox {...props} />)
+    expect(screen.getByRole("combobox")).toBeInTheDocument()
   })
 
   it("has correct className and style", () => {
-    const wrappedDiv = wrapper.find("div").first()
-
-    const { className, style } = wrappedDiv.props()
-    // @ts-expect-error
-    const splittedClassName = className.split(" ")
-
-    expect(splittedClassName).toContain("row-widget")
-    expect(splittedClassName).toContain("stSelectbox")
-
-    // @ts-expect-error
-    expect(style.width).toBe(getProps().width)
+    render(<Selectbox {...props} />)
+    const selectbox = screen.getByTestId("stSelectbox")
+    expect(selectbox).toHaveClass("stSelectbox")
+    expect(selectbox).toHaveClass("row-widget")
+    expect(selectbox).toHaveStyle(`width: ${props.width}px`)
   })
 
   it("renders a label", () => {
-    const wrapper = mount(<Selectbox {...props} />)
-    expect(wrapper.find("StyledWidgetLabel").text()).toBe(props.label)
+    render(<Selectbox {...props} />)
+    expect(screen.getByTestId("stWidgetLabel")).toHaveTextContent(
+      `${props.label}`
+    )
   })
 
   it("pass labelVisibility prop to StyledWidgetLabel correctly when hidden", () => {
     const props = getProps({
       labelVisibility: LabelVisibilityOptions.Hidden,
     })
-    const wrapper = mount(<Selectbox {...props} />)
-    expect(wrapper.find("StyledWidgetLabel").prop("labelVisibility")).toEqual(
-      LabelVisibilityOptions.Hidden
+    render(<Selectbox {...props} />)
+    expect(screen.getByTestId("stWidgetLabel")).toHaveStyle(
+      "visibility: hidden"
     )
   })
 
@@ -83,108 +78,81 @@ describe("Selectbox widget", () => {
     const props = getProps({
       labelVisibility: LabelVisibilityOptions.Collapsed,
     })
-    const wrapper = mount(<Selectbox {...props} />)
-    expect(wrapper.find("StyledWidgetLabel").prop("labelVisibility")).toEqual(
-      LabelVisibilityOptions.Collapsed
-    )
+    render(<Selectbox {...props} />)
+    expect(screen.getByTestId("stWidgetLabel")).toHaveStyle("display: none")
   })
 
   it("pass placeholder prop correctly", () => {
     props = getProps({
+      value: undefined,
       placeholder: "Please select",
     })
-    wrapper = shallow(<Selectbox {...props} />)
-
-    expect(wrapper.find(UISelect).prop("placeholder")).toEqual("Please select")
+    render(<Selectbox {...props} />)
+    expect(screen.getByText("Please select")).toBeInTheDocument()
   })
 
   it("renders a placeholder with empty options", () => {
     props = getProps({
       options: [],
     })
-    wrapper = shallow(<Selectbox {...props} />)
+    render(<Selectbox {...props} />)
 
-    expect(wrapper.find(UISelect).prop("options")).toStrictEqual([
-      {
-        label: "No options to select.",
-        value: "0",
-      },
-    ])
+    expect(screen.getByText("No options to select.")).toBeInTheDocument()
+    expect(screen.getByRole("combobox")).toBeDisabled()
   })
 
   it("renders options", () => {
-    const options = wrapper.find(UISelect).prop("options") || []
+    render(<Selectbox {...props} />)
+    const selectbox = screen.getByRole("combobox")
+    fireEvent.click(selectbox)
+    const options = screen.getAllByRole("option")
 
-    // @ts-expect-error
-    options.forEach(option => {
-      expect(option).toHaveProperty("label")
-      expect(option).toHaveProperty("value")
+    expect(options).toHaveLength(props.options.length)
+    options.forEach((option, index) => {
+      expect(option).toHaveTextContent(props.options[index])
     })
-
-    expect(options.length).toBe(props.options.length)
-    expect(wrapper.find(UISelect).prop("labelKey")).toBe("label")
-    expect(wrapper.find(UISelect).prop("valueKey")).toBe("value")
   })
 
   it("could be disabled", () => {
-    expect(wrapper.find(UISelect).prop("disabled")).toBe(props.disabled)
+    props = getProps({
+      disabled: true,
+    })
+    render(<Selectbox {...props} />)
+    expect(screen.getByRole("combobox")).toBeDisabled()
   })
 
   it("is able to select an option", () => {
-    // @ts-expect-error
-    wrapper.find(UISelect).prop("onChange")({
-      value: [{ label: "b", value: "1" }],
-      option: { label: "b", value: "1" },
-      type: "select",
-    })
+    render(<Selectbox {...props} />)
+    const selectbox = screen.getByRole("combobox")
+    // Open the dropdown
+    fireEvent.click(selectbox)
+    const options = screen.getAllByRole("option")
+    fireEvent.click(options[1])
 
-    expect(wrapper.find(UISelect).prop("value")).toContainEqual({
-      label: "b",
-      value: "1",
-    })
-  })
-
-  it("doesn't lose the selected value when the input is empty", () => {
-    const wrapper = mount(<Selectbox {...props} />)
-
-    const input = wrapper.find("input")
-
-    input.simulate("change", { target: { value: "" } })
-    expect(wrapper.state("isEmpty")).toBe(true)
-    expect(wrapper.state("value")).toEqual(0)
-
-    input.simulate("change", { target: { value: "a" } })
-    expect(wrapper.state("isEmpty")).toBe(false)
-    expect(wrapper.state("value")).toEqual(0)
+    expect(props.onChange).toHaveBeenCalledWith(1)
+    expect(screen.getByText(props.options[1])).toBeInTheDocument()
   })
 
   it("doesn't filter options based on index", () => {
-    const options = wrapper.find(UISelect).prop("options")
-    const filterOptionsFn = wrapper.find(UISelect).prop("filterOptions")
+    render(<Selectbox {...props} />)
 
-    // @ts-expect-error
-    const filteredOptions = filterOptionsFn(options, "1")
-    expect(filteredOptions).toEqual([])
+    fireEvent.change(screen.getByRole("combobox"), { target: { value: "1" } })
+    expect(screen.getByText("No results")).toBeInTheDocument()
   })
 
   it("filters options based on label with case insensitive", () => {
-    const options = wrapper.find(UISelect).prop("options")
-    const filterOptionsFn = wrapper.find(UISelect).prop("filterOptions")
+    render(<Selectbox {...props} />)
+    const selectbox = screen.getByRole("combobox")
 
-    // @ts-expect-error
-    expect(filterOptionsFn(options, "b")).toEqual([
-      {
-        label: "b",
-        value: "1",
-      },
-    ])
-    // @ts-expect-error
-    expect(filterOptionsFn(options, "B")).toEqual([
-      {
-        label: "b",
-        value: "1",
-      },
-    ])
+    fireEvent.change(selectbox, { target: { value: "b" } })
+    let options = screen.getAllByRole("option")
+    expect(options).toHaveLength(1)
+    expect(options[0]).toHaveTextContent("b")
+
+    fireEvent.change(selectbox, { target: { value: "B" } })
+    options = screen.getAllByRole("option")
+    expect(options).toHaveLength(1)
+    expect(options[0]).toHaveTextContent("b")
   })
 
   it("fuzzy filters options correctly", () => {
@@ -222,23 +190,13 @@ describe("Selectbox widget", () => {
   })
 
   it("updates value if new value provided from parent", () => {
-    // @ts-expect-error
-    wrapper.find(UISelect).prop("onChange")({
-      value: [{ label: "b", value: "1" }],
-      option: { label: "b", value: "1" },
-      type: "select",
-    })
+    const { rerender } = render(<Selectbox {...props} />)
+    // Original value passed is 0
+    expect(screen.getByText(props.options[0])).toBeInTheDocument()
 
-    expect(wrapper.find(UISelect).prop("value")).toContainEqual({
-      label: "b",
-      value: "1",
-    })
-
-    wrapper.setProps({ value: "2" })
-    expect(wrapper.find(UISelect).prop("value")).toContainEqual({
-      label: "c",
-      value: "2",
-    })
+    props = getProps({ value: 1 })
+    rerender(<Selectbox {...props} />)
+    expect(screen.getByText(props.options[1])).toBeInTheDocument()
   })
 })
 
@@ -246,15 +204,15 @@ describe("Selectbox widget with optional props", () => {
   // This goes against the previous solution to bug #3220, but that's on purpose.
   it("renders no label element if no text provided", () => {
     const props = getProps({ label: undefined })
-    const wrapper = shallow(<Selectbox {...props} />)
+    render(<Selectbox {...props} />)
 
-    expect(wrapper.find("StyledWidgetLabel").exists()).toBeFalsy()
+    expect(screen.queryByTestId("stWidgetLabel")).not.toBeInTheDocument()
   })
 
   it("renders TooltipIcon if help text provided", () => {
     const props = getProps({ help: "help text" })
-    const wrapper = shallow(<Selectbox {...props} />)
+    render(<Selectbox {...props} />)
 
-    expect(wrapper.find("TooltipIcon").prop("content")).toBe("help text")
+    expect(screen.getByTestId("stTooltipIcon")).toBeInTheDocument()
   })
 })
