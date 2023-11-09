c9169911144bf23bf7528330c1e096251c613eae
willhuang1997 <willhuang1997@gmail.com>
2023-11-09 14:53:05 -0800

    Add query params class as it doesn't seem like session state internals are reusable

---
+++ lib/streamlit/runtime/state/query_params.py
@@ -0,0 +1,126 @@
+# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from dataclasses import dataclass, field
+from typing import Any, Dict, Iterator, List, MutableMapping, Union
+
+from streamlit.proto.ForwardMsg_pb2 import ForwardMsg
+
+
+@dataclass
+class QueryParams(MutableMapping[str, Any]):
+    """A lightweight wrapper of a dict that sends forwardMsgs when state changes.
+    It stores str keys with str and List[str] values.
+    """
+
+    _query_params: Dict[str, Union[List[str], str]] = field(default_factory=dict)
+
+    def __init__(self, query_params: Dict[str, Union[List[str], str]] = {}):
+        # avoid using ._query_params as that will use __setattr__,
+        # which itself relies on `_query_params` being defined
+        self.__dict__["_query_params"] = query_params
+
+    def __iter__(self) -> Iterator[Any]:
+        return iter(self._query_params.keys())
+
+    def __getitem__(self, key: str) -> str:
+        return self._getitem(key)
+
+    def _getitem(self, key: str) -> str:
+        try:
+            value = self._query_params[key]
+            if isinstance(value, list):
+                if len(value) == 0:
+                    return ""
+                else:
+                    return value[-1]
+            return value
+        except:
+            raise KeyError(_missing_key_error_message_query_params(key))
+
+    def _setitem(self, key: str, value: Union[str, List]) -> None:
+        if isinstance(value, list):
+            self._query_params[key] = [str(item) for item in value]
+        else:
+            self._query_params[key] = str(value)
+        self._send_query_param_msg()
+
+    def __setitem__(self, key: str, value: str) -> None:
+        self._setitem(key, value)
+
+    def get_all(self, key: str) -> List[str]:
+        try:
+            if key not in self._query_params:
+                return []
+            query_params = self._query_params[key]
+            return query_params if isinstance(query_params, list) else [query_params]
+        except KeyError:
+            raise KeyError(_missing_key_error_message_query_params(key))
+
+    def __getattr__(self, key: str) -> str:
+        try:
+            return self._getitem(key)
+        except KeyError:
+            raise AttributeError(_missing_key_error_message_query_params(key))
+
+    def __setattr__(self, key: str, value: str) -> None:
+        self._setitem(key, value)
+
+    def __delattr__(self, key: str) -> None:
+        try:
+            self._delitem(key)
+        except KeyError:
+            raise AttributeError(_missing_key_error_message_query_params(key))
+
+    def _delitem(self, key):
+        if key in self._query_params:
+            del self._query_params[key]
+            self._send_query_param_msg()
+        else:
+            raise KeyError(_missing_key_error_message_query_params(key))
+
+    def __contains__(self, key: str) -> bool:
+        return key in self._query_params
+
+    def __len__(self) -> int:
+        return len(self._query_params)
+
+    def _send_query_param_msg(self) -> None:
+        # Avoid circular imports
+        from streamlit.commands.query_params import _ensure_no_embed_params
+        from streamlit.runtime.scriptrunner import get_script_run_ctx
+
+        ctx = get_script_run_ctx()
+        if ctx is None:
+            return
+        msg = ForwardMsg()
+        msg.page_info_changed.query_string = _ensure_no_embed_params(
+            self._query_params, ctx.query_string
+        )
+        ctx.query_string = msg.page_info_changed.query_string
+        ctx.enqueue(msg)
+
+    def clear(self) -> None:
+        self._query_params.clear()
+        self._send_query_param_msg()
+
+    def __delitem__(self, key: str) -> None:
+        self._delitem(key)
+
+    def to_dict(self) -> Dict[str, List[Any]]:
+        return self._query_params
+
+
+def _missing_key_error_message_query_params(key: str) -> str:
+    return f'st.query_params has no key "{key}". Did you forget to initialize it?'
---
+++ lib/tests/streamlit/runtime/state/query_params_test.py
@@ -0,0 +1,132 @@
+# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import pytest
+
+from streamlit.runtime.state.query_params import QueryParams
+from tests.delta_generator_test_case import DeltaGeneratorTestCase
+
+
+class QueryParamsMethodTests(DeltaGeneratorTestCase):
+    def setUp(self):
+        super().setUp()
+        self.q_params = {"foo": "bar", "two": ["x", "y"]}
+        self._query_params = QueryParams(self.q_params)
+
+    def test_getitem_nonexistent(self):
+        with pytest.raises(KeyError) as exception_message:
+            self._query_params["nonexistent"]
+            assert (
+                exception_message
+                == 'st.query_params has no key "nonexistent". Did you forget to initialize it? '
+            )
+
+    def test__getattr__nonexistent(self):
+        with pytest.raises(AttributeError) as exception_message:
+            self._query_params.nonexistent
+            assert (
+                exception_message
+                == 'st.query_params has no key "nonexistent". Did you forget to initialize it? '
+            )
+
+    def test_getitem_list(self):
+        # get the last item in the array
+        assert self._query_params["two"] == "y"
+
+    def test_getitem_single_element_int(self):
+        # Internal state is always a string so we test setting it here
+        self._query_params["baz"] = 1
+        assert self._query_params["baz"] == "1"
+
+    def test_getitem_single_element_float(self):
+        # Internal state is always a string so we test setting it here
+        self._query_params["corge"] = 1.23
+        assert self._query_params["corge"] == "1.23"
+
+    def test__getitem__value(self):
+        assert self._query_params["foo"] == "bar"
+
+    def test_get(self):
+        assert self._query_params.get("foo") == "bar"
+
+    def test_get_default(self):
+        return_val = self._query_params.get("dog", default="bark")
+        assert return_val == "bark"
+
+    def test__getattr__(self):
+        assert self._query_params.foo == "bar"
+
+    def test__setitem__query_params(self):
+        assert "test" not in self._query_params
+        self._query_params["test"] = "test"
+        assert self._query_params.get("test") == "test"
+        message = self.get_message_from_queue(0)
+        assert (message.page_info_changed.query_string, "test=test")
+
+    def test__setitem_empty_string(self):
+        assert "test" not in self._query_params
+        self._query_params["test"] = ""
+        assert self._query_params["test"] == ""
+        message = self.get_message_from_queue(0)
+        assert (message.page_info_changed.query_string, "test=")
+
+    def test__setattr__(self):
+        self._query_params.test = "test"
+        assert self._query_params.get("test") == "test"
+
+    def test__setattr__list(self):
+        self._query_params.test = ["test", "test2"]
+        print(self._query_params)
+        assert self._query_params["test"] == "test2"
+        message = self.get_message_from_queue(0)
+        assert (message.page_info_changed.query_string, "test=test&test=test2")
+
+    def test_getall_nonexistent(self):
+        assert self._query_params.get_all("nonexistent") == []
+
+    def test_getall_single_element(self):
+        assert self._query_params.get_all("foo") == ["bar"]
+
+    def test_getall_list(self):
+        assert self._query_params.get_all("two") == ["x", "y"]
+
+    def test_getall_list_non_str_and_empty_str(self):
+        self._query_params["test"] = ["", "a", 1, 1.23]
+        assert self._query_params.get_all("test") == ["", "a", "1", "1.23"]
+
+    def test_contains_valid(self):
+        assert "foo" in self._query_params
+
+    def test_contains_invalid(self):
+        assert "baz" not in self._query_params
+
+    def test_clear(self):
+        self._query_params.clear()
+        assert len(self._query_params) == 0
+        message = self.get_message_from_queue(0)
+        self.assertEqual(message.page_info_changed.query_string, "")
+
+    def test_del_valid(self):
+        del self._query_params["foo"]
+        assert "foo" not in self._query_params
+        message = self.get_message_from_queue(0)
+        self.assertEqual(message.page_info_changed.query_string, "two=x&two=y")
+
+    def test_del_invalid(self):
+        with pytest.raises(KeyError):
+            del self._query_params["nonexistent"]
+
+        # no forward message should be sent
+        with pytest.raises(IndexError):
+            self.get_message_from_queue(0)
