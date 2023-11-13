7b9b6c4835f55574fdb8b4b349307bc8e8716b09
Lukas Masuch <Lukas.Masuch@gmail.com>
2023-11-13 15:33:16 -0500

    Enforce pyarrow version for arrow-based custom components (#7695)

--- lib/streamlit/components/v1/component_arrow.py
+++ lib/streamlit/components/v1/component_arrow.py
@@ -110,6 +110,12 @@ def arrow_proto_to_dataframe(proto: ArrowTableProto) -> pd.DataFrame:
         Output. pandas.DataFrame
 
     """
+    if type_util.is_pyarrow_version_less_than("14.0.1"):
+        raise RuntimeError(
+            "The installed pyarrow version is not compatible with this component. "
+            "Please upgrade to 14.0.1 or higher: pip install -U pyarrow"
+        )
+
     data = type_util.bytes_to_data_frame(proto.data)
     index = type_util.bytes_to_data_frame(proto.index)
     columns = type_util.bytes_to_data_frame(proto.columns)
