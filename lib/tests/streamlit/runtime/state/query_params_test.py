24228b07f04abc379edc6bb07d58f8c5127240eb
willhuang1997 <willhuang1997@gmail.com>
2023-11-17 00:11:52 -0800

    Remove unnecessary tests and fix function definitions of tests

--- lib/tests/streamlit/runtime/state/query_params_test.py
+++ lib/tests/streamlit/runtime/state/query_params_test.py
@@ -37,26 +37,23 @@ class QueryParamsMethodTests(DeltaGeneratorTestCase):
     def test__getitem__returns_last_element_of_list(self):
         assert self._query_params["two"] == "y"
 
-    def test__getitem__converts_int_value_to_string(self):
-        self._query_params["baz"] = 1
-        assert self._query_params["baz"] == "1"
-
-    def test__getitem__converts_float_value_to_string(self):
-        self._query_params["corge"] = 1.23
-        assert self._query_params["corge"] == "1.23"
-
     def test__getitem__retrieves_existing_key(self):
         assert self._query_params["foo"] == "bar"
 
-    def test_get_method_retrieves_existing_key(self):
-        assert self._query_params.get("foo") == "bar"
-
-    def test_get_method_returns_default_for_nonexistent_key(self):
-        return_val = self._query_params.get("dog", default="bark")
-        assert return_val == "bark"
-
-    def test__getitem__retrieves_existing_key(self):
+    def test__getattr__retrieves_existing_key(self):
         assert self._query_params.foo == "bar"
+
+    def test__setitem__converts_int_value_to_string(self):
+        self._query_params["baz"] = 1
+        assert self._query_params["baz"] == "1"
+        message = self.get_message_from_queue(0)
+        assert "baz=1" in message.page_info_changed.query_string
+
+    def test__setitem__converts_float_value_to_string(self):
+        self._query_params["corge"] = 1.23
+        assert self._query_params["corge"] == "1.23"
+        message = self.get_message_from_queue(0)
+        assert "corge=1.23" in message.page_info_changed.query_string
 
     def test__setitem__adds_new_query_param(self):
         assert "test" not in self._query_params
@@ -72,19 +69,24 @@ class QueryParamsMethodTests(DeltaGeneratorTestCase):
         message = self.get_message_from_queue(0)
         assert "test=" in message.page_info_changed.query_string
 
+    def test__setitem__adds_list_value(self):
+        self._query_params["test"] = ["test", "test2"]
+        assert self._query_params["test"] == "test2"
+        message = self.get_message_from_queue(0)
+        assert "test=test&test=test2" in message.page_info_changed.query_string
+
     def test__setattr__adds_new_query_param(self):
+        assert "test" not in self._query_params
         self._query_params.test = "test"
         assert self._query_params.get("test") == "test"
+        message = self.get_message_from_queue(0)
+        assert "test=test" in message.page_info_changed.query_string
 
     def test__setattr__sets_old_query_param_key(self):
         self._query_params.foo = "test"
         assert self._query_params.get("foo") == "test"
-
-    def test__setattr__adds_list_value(self):
-        self._query_params.test = ["test", "test2"]
-        assert self._query_params["test"] == "test2"
         message = self.get_message_from_queue(0)
-        assert "test=test&test=test2" in message.page_info_changed.query_string
+        assert "foo=test" in message.page_info_changed.query_string
 
     def test_get_all_returns_empty_list_for_nonexistent_key(self):
         assert self._query_params.get_all("nonexistent") == []
@@ -98,12 +100,6 @@ class QueryParamsMethodTests(DeltaGeneratorTestCase):
     def test_get_all_handles_mixed_type_values(self):
         self._query_params["test"] = ["", "a", 1, 1.23]
         assert self._query_params.get_all("test") == ["", "a", "1", "1.23"]
-
-    def test__contains__returns_true_for_present_key(self):
-        assert "foo" in self._query_params
-
-    def test__contains__returns_false_for_absent_key(self):
-        assert "baz" not in self._query_params
 
     def test_clear_removes_all_query_params(self):
         self._query_params.clear()
