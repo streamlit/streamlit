6b86bd40ccdbdd261c8eb10be058296e616f2a93
Ken McGrady <ken.mcgrady@gmail.com>
2023-11-29 11:06:06 -0800

    Addressed PR comments

--- e2e_playwright/st_arrow_vega_lite_chart_test.py
+++ e2e_playwright/st_arrow_vega_lite_chart_test.py
@@ -17,9 +17,9 @@ from playwright.sync_api import Page, expect
 from e2e_playwright.conftest import ImageCompareFunction
 
 
-def test_arrow_vega_lite_chart(themed_app: Page):
+def test_arrow_vega_lite_chart(app: Page):
     """Tests that it displays charts on the DOM"""
-    arrow_vega_lite_charts = themed_app.locator("[data-testid=stArrowVegaLiteChart]")
+    arrow_vega_lite_charts = app.get_by_test_id("stArrowVegaLiteChart")
     expect(arrow_vega_lite_charts).to_have_count(13)
 
     for idx in range(13):
@@ -31,7 +31,7 @@ def test_arrow_vega_lite_chart(themed_app: Page):
 
 def test_arrow_vega_lite_chart_sets_chart_width(themed_app: Page):
     """Tests that it sets the correct chart width"""
-    arrow_vega_lite_charts = themed_app.locator("[data-testid=stArrowVegaLiteChart]")
+    arrow_vega_lite_charts = themed_app.get_by_test_id("stArrowVegaLiteChart")
     expect(arrow_vega_lite_charts).to_have_count(13)
 
     expect(arrow_vega_lite_charts.nth(0).locator("canvas").nth(0)).to_have_css(
@@ -52,7 +52,7 @@ def test_arrow_vega_lite_chart_displays_interactive_charts(
     themed_app: Page, assert_snapshot: ImageCompareFunction
 ):
     """Tests that it displays interactive charts on the DOM"""
-    arrow_vega_lite_charts = themed_app.locator("[data-testid=stArrowVegaLiteChart]")
+    arrow_vega_lite_charts = themed_app.get_by_test_id("stArrowVegaLiteChart")
     expect(arrow_vega_lite_charts).to_have_count(13)
 
     assert_snapshot(
@@ -64,7 +64,7 @@ def test_arrow_vega_lite_chart_same_plot_different_ways(
     themed_app: Page, assert_snapshot: ImageCompareFunction
 ):
     """Tests that it displays the same plot in different ways"""
-    arrow_vega_lite_charts = themed_app.locator("[data-testid=stArrowVegaLiteChart]")
+    arrow_vega_lite_charts = themed_app.get_by_test_id("stArrowVegaLiteChart")
     expect(arrow_vega_lite_charts).to_have_count(13)
 
     for idx in range(5, 9):
@@ -77,7 +77,7 @@ def test_arrow_vega_lite_chart_streamlit_theme(
     themed_app: Page, assert_snapshot: ImageCompareFunction
 ):
     """Tests that arrow_vega_lite_chart supports the Streamlit theme"""
-    arrow_vega_lite_charts = themed_app.locator("[data-testid=stArrowVegaLiteChart]")
+    arrow_vega_lite_charts = themed_app.get_by_test_id("stArrowVegaLiteChart")
     expect(arrow_vega_lite_charts).to_have_count(13)
 
     for idx in range(9, 11):
@@ -90,7 +90,7 @@ def test_arrow_vega_lite_chart_default_theme(
     themed_app: Page, assert_snapshot: ImageCompareFunction
 ):
     """Tests that arrow_vega_lite_chart supports the default theme"""
-    arrow_vega_lite_charts = themed_app.locator("[data-testid=stArrowVegaLiteChart]")
+    arrow_vega_lite_charts = themed_app.get_by_test_id("stArrowVegaLiteChart")
     expect(arrow_vega_lite_charts).to_have_count(13)
 
     assert_snapshot(
@@ -102,7 +102,7 @@ def test_arrow_vega_lite_chart_user_supplied_colors(
     themed_app: Page, assert_snapshot: ImageCompareFunction
 ):
     """Tests that arrow_vega_lite_chart respects user configuration"""
-    arrow_vega_lite_charts = themed_app.locator("[data-testid=stArrowVegaLiteChart]")
+    arrow_vega_lite_charts = themed_app.get_by_test_id("stArrowVegaLiteChart")
     expect(arrow_vega_lite_charts).to_have_count(13)
 
     assert_snapshot(
