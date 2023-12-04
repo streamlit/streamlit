5db179bb8f4190276b0317b57f9aae419fc7b99a
Ken McGrady <ken.mcgrady@gmail.com>
2023-12-04 11:36:21 -0800

    Remove Enzyme from Arrow Vega Lite Charts (#7763)
    * Remove Enzyme from Arrow Vega Lite Charts
    
    * Favor snapshot testing over canvas checks
    
    * Remove snapshots
    
    * Remove unnecessary imports
    
    * Add snapshots
    
    * Update test to remove iterable
    
    * Addressed PR comments
    
    * Try change in preloading fonts

---
+++ e2e_playwright/st_arrow_vega_lite_chart_test.py
@@ -0,0 +1,111 @@
+# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from playwright.sync_api import Page, expect
+
+from e2e_playwright.conftest import ImageCompareFunction
+
+
+def test_arrow_vega_lite_chart(app: Page):
+    """Tests that it displays charts on the DOM"""
+    arrow_vega_lite_charts = app.get_by_test_id("stArrowVegaLiteChart")
+    expect(arrow_vega_lite_charts).to_have_count(13)
+
+    for idx in range(13):
+        chart = arrow_vega_lite_charts.nth(idx)
+        canvas = chart.locator("canvas").nth(0)
+        expect(canvas).to_be_visible()
+        expect(canvas).to_have_class("marks")
+
+
+def test_arrow_vega_lite_chart_sets_chart_width(themed_app: Page):
+    """Tests that it sets the correct chart width"""
+    arrow_vega_lite_charts = themed_app.get_by_test_id("stArrowVegaLiteChart")
+    expect(arrow_vega_lite_charts).to_have_count(13)
+
+    expect(arrow_vega_lite_charts.nth(0).locator("canvas").nth(0)).to_have_css(
+        "width", "704px"
+    )
+    expect(arrow_vega_lite_charts.nth(1).locator("canvas").nth(0)).to_have_css(
+        "width", "704px"
+    )
+    expect(arrow_vega_lite_charts.nth(2).locator("canvas").nth(0)).to_have_css(
+        "width", "400px"
+    )
+    expect(arrow_vega_lite_charts.nth(3).locator("canvas").nth(0)).to_have_css(
+        "width", "500px"
+    )
+
+
+def test_arrow_vega_lite_chart_displays_interactive_charts(
+    themed_app: Page, assert_snapshot: ImageCompareFunction
+):
+    """Tests that it displays interactive charts on the DOM"""
+    arrow_vega_lite_charts = themed_app.get_by_test_id("stArrowVegaLiteChart")
+    expect(arrow_vega_lite_charts).to_have_count(13)
+
+    assert_snapshot(
+        arrow_vega_lite_charts.nth(4), name="interactive_arrow_vega_lite_chart"
+    )
+
+
+def test_arrow_vega_lite_chart_same_plot_different_ways(
+    themed_app: Page, assert_snapshot: ImageCompareFunction
+):
+    """Tests that it displays the same plot in different ways"""
+    arrow_vega_lite_charts = themed_app.get_by_test_id("stArrowVegaLiteChart")
+    expect(arrow_vega_lite_charts).to_have_count(13)
+
+    for idx in range(5, 9):
+        assert_snapshot(
+            arrow_vega_lite_charts.nth(idx), name=f"arrow_vega_lite_chart{idx}"
+        )
+
+
+def test_arrow_vega_lite_chart_streamlit_theme(
+    themed_app: Page, assert_snapshot: ImageCompareFunction
+):
+    """Tests that arrow_vega_lite_chart supports the Streamlit theme"""
+    arrow_vega_lite_charts = themed_app.get_by_test_id("stArrowVegaLiteChart")
+    expect(arrow_vega_lite_charts).to_have_count(13)
+
+    for idx in range(9, 11):
+        assert_snapshot(
+            arrow_vega_lite_charts.nth(idx), name=f"arrow_vega_lite_chart_theming_{idx}"
+        )
+
+
+def test_arrow_vega_lite_chart_default_theme(
+    themed_app: Page, assert_snapshot: ImageCompareFunction
+):
+    """Tests that arrow_vega_lite_chart supports the default theme"""
+    arrow_vega_lite_charts = themed_app.get_by_test_id("stArrowVegaLiteChart")
+    expect(arrow_vega_lite_charts).to_have_count(13)
+
+    assert_snapshot(
+        arrow_vega_lite_charts.nth(11), name=f"arrow_vega_lite_chart_default_theming"
+    )
+
+
+def test_arrow_vega_lite_chart_user_supplied_colors(
+    themed_app: Page, assert_snapshot: ImageCompareFunction
+):
+    """Tests that arrow_vega_lite_chart respects user configuration"""
+    arrow_vega_lite_charts = themed_app.get_by_test_id("stArrowVegaLiteChart")
+    expect(arrow_vega_lite_charts).to_have_count(13)
+
+    assert_snapshot(
+        arrow_vega_lite_charts.nth(12),
+        name=f"arrow_vega_lite_chart_user_supplied_colors",
+    )
--- frontend/lib/src/components/elements/ArrowVegaLiteChart/ArrowVegaLiteChart.test.tsx
+++ frontend/lib/src/components/elements/ArrowVegaLiteChart/ArrowVegaLiteChart.test.tsx
@@ -14,8 +14,6 @@
  * limitations under the License.
  */
 
-import React from "react"
-import { mount } from "@streamlit/lib/src/test_util"
 import {
   CATEGORICAL,
   DATETIME,
@@ -24,207 +22,103 @@ import {
   UINT64,
   RANGE,
   UNICODE,
-  VEGA_LITE,
 } from "@streamlit/lib/src/mocks/arrow"
 import { Quiver } from "@streamlit/lib/src/dataframes/Quiver"
-import { mockTheme } from "@streamlit/lib/src/mocks/mockTheme"
-import {
-  PropsWithHeight,
-  ArrowVegaLiteChart,
-  getDataArray,
-} from "./ArrowVegaLiteChart"
+import { getDataArray } from "./ArrowVegaLiteChart"
 
-const MOCK = {
-  datasets: [],
-  data: new Quiver({
-    data: VEGA_LITE,
-  }),
-  spec: JSON.stringify({
-    mark: "circle",
-    encoding: {
-      x: { field: "a", type: "quantitative" },
-      y: { field: "b", type: "quantitative" },
-      size: { field: "c", type: "quantitative" },
-      color: { field: "c", type: "quantitative" },
-    },
-  }),
-  useContainerWidth: true,
-  vegaLiteTheme: "",
-}
+describe("Types of dataframe indexes as x axis", () => {
+  describe("Supported", () => {
+    test("datetime", () => {
+      const mockElement = { data: DATETIME }
+      const q = new Quiver(mockElement)
 
-const getProps = (props: Partial<PropsWithHeight> = {}): PropsWithHeight => ({
-  element: MOCK,
-  width: 0,
-  height: 0,
-  theme: mockTheme.emotion,
-  ...props,
-})
-
-describe("VegaLiteChart Element", () => {
-  it("renders without crashing", () => {
-    const props = getProps()
-    const wrapper = mount(<ArrowVegaLiteChart {...props} />)
-
-    expect(wrapper.find("StyledVegaLiteChartContainer").length).toBe(1)
-  })
-
-  it("pulls default config values from theme", () => {
-    const props = getProps({ theme: mockTheme.emotion })
-
-    const wrapper = mount(<ArrowVegaLiteChart {...props} />)
-    // @ts-expect-error
-    const generatedSpec = wrapper.instance().generateSpec()
-
-    expect(generatedSpec.config.background).toBe(
-      mockTheme.emotion.colors.bgColor
-    )
-    expect(generatedSpec.config.axis.labelColor).toBe(
-      mockTheme.emotion.colors.bodyText
-    )
-  })
-
-  it("applies Steramlit theme if specified", () => {
-    const props = getProps({
-      element: {
-        ...MOCK,
-        vegaLiteTheme: "streamlit",
-        spec: JSON.stringify({
-          mark: "circle",
-          encoding: {
-            x: { field: "a", type: "quantitative" },
-            y: { field: "b", type: "quantitative" },
-            size: { field: "c", type: "quantitative" },
-            color: { field: "c", type: "quantitative" },
-          },
-        }),
-      },
+      expect(getDataArray(q)).toEqual([
+        {
+          "(index)": 978220800000,
+          "2000-12-31 00:00:00": new Date("2020-01-02T00:00:00.000Z"),
+          "2001-12-31 00:00:00": new Date("2020-10-20T00:00:00.000Z"),
+        },
+        {
+          "(index)": 1009756800000,
+          "2000-12-31 00:00:00": new Date("2020-01-02T00:00:00.000Z"),
+          "2001-12-31 00:00:00": new Date("2020-10-20T00:00:00.000Z"),
+        },
+      ])
     })
 
-    const wrapper = mount(<ArrowVegaLiteChart {...props} />)
-    // @ts-expect-error
-    const generatedSpec = wrapper.instance().generateSpec()
-    // Should have 10 colors defined in range.diverging
-    expect(generatedSpec.config.range?.diverging?.length).toBe(10)
-  })
+    test("float64", () => {
+      const mockElement = { data: FLOAT64 }
+      const q = new Quiver(mockElement)
 
-  it("has user specified config take priority", () => {
-    const props = getProps({ theme: mockTheme.emotion })
-
-    const spec = JSON.parse(props.element.spec)
-    spec.config = { background: "purple", axis: { labelColor: "blue" } }
-
-    props.element = {
-      ...props.element,
-      spec: JSON.stringify(spec),
-    }
-
-    const wrapper = mount(<ArrowVegaLiteChart {...props} />)
-    // @ts-expect-error
-    const generatedSpec = wrapper.instance().generateSpec()
-
-    expect(generatedSpec.config.background).toBe("purple")
-    expect(generatedSpec.config.axis.labelColor).toBe("blue")
-    // Verify that things not overwritten by the user still fall back to the
-    // theme default.
-    expect(generatedSpec.config.axis.titleColor).toBe(
-      mockTheme.emotion.colors.bodyText
-    )
-  })
-
-  describe("Types of dataframe indexes as x axis", () => {
-    describe("Supported", () => {
-      test("datetime", () => {
-        const mockElement = { data: DATETIME }
-        const q = new Quiver(mockElement)
-
-        expect(getDataArray(q)).toEqual([
-          {
-            "(index)": 978220800000,
-            "2000-12-31 00:00:00": new Date("2020-01-02T00:00:00.000Z"),
-            "2001-12-31 00:00:00": new Date("2020-10-20T00:00:00.000Z"),
-          },
-          {
-            "(index)": 1009756800000,
-            "2000-12-31 00:00:00": new Date("2020-01-02T00:00:00.000Z"),
-            "2001-12-31 00:00:00": new Date("2020-10-20T00:00:00.000Z"),
-          },
-        ])
-      })
-
-      test("float64", () => {
-        const mockElement = { data: FLOAT64 }
-        const q = new Quiver(mockElement)
-
-        expect(getDataArray(q)).toEqual([
-          { "(index)": 1.24, "1.24": 1.2, "2.35": 1.3 },
-          { "(index)": 2.35, "1.24": 1.4, "2.35": 1.5 },
-        ])
-      })
-
-      test("int64", () => {
-        const mockElement = { data: INT64 }
-        const q = new Quiver(mockElement)
-        expect(getDataArray(q)).toEqual([
-          {
-            "(index)": 1,
-            "1": 0,
-            "2": 1,
-          },
-          {
-            "(index)": 2,
-            "1": 2,
-            "2": 3,
-          },
-        ])
-      })
-
-      test("range", () => {
-        const mockElement = { data: RANGE }
-        const q = new Quiver(mockElement)
-
-        expect(getDataArray(q)).toEqual([
-          { "(index)": 0, "0": "foo", "1": "1" },
-          { "(index)": 1, "0": "bar", "1": "2" },
-        ])
-      })
-
-      test("uint64", () => {
-        const mockElement = { data: UINT64 }
-        const q = new Quiver(mockElement)
-        expect(getDataArray(q)).toEqual([
-          {
-            "(index)": 1,
-            "1": 1,
-            "2": 2,
-          },
-          {
-            "(index)": 2,
-            "1": 3,
-            "2": 4,
-          },
-        ])
-      })
+      expect(getDataArray(q)).toEqual([
+        { "(index)": 1.24, "1.24": 1.2, "2.35": 1.3 },
+        { "(index)": 2.35, "1.24": 1.4, "2.35": 1.5 },
+      ])
     })
 
-    describe("Unsupported", () => {
-      test("categorical", () => {
-        const mockElement = { data: CATEGORICAL }
-        const q = new Quiver(mockElement)
-        expect(getDataArray(q)).toEqual([
-          { c1: "foo", c2: 100 },
-          { c1: "bar", c2: 200 },
-        ])
-      })
+    test("int64", () => {
+      const mockElement = { data: INT64 }
+      const q = new Quiver(mockElement)
+      expect(getDataArray(q)).toEqual([
+        {
+          "(index)": 1,
+          "1": 0,
+          "2": 1,
+        },
+        {
+          "(index)": 2,
+          "1": 2,
+          "2": 3,
+        },
+      ])
+    })
 
-      test("unicode", () => {
-        const mockElement = { data: UNICODE }
-        const q = new Quiver(mockElement)
+    test("range", () => {
+      const mockElement = { data: RANGE }
+      const q = new Quiver(mockElement)
 
-        expect(getDataArray(q)).toEqual([
-          { c1: "foo", c2: "1" },
-          { c1: "bar", c2: "2" },
-        ])
-      })
+      expect(getDataArray(q)).toEqual([
+        { "(index)": 0, "0": "foo", "1": "1" },
+        { "(index)": 1, "0": "bar", "1": "2" },
+      ])
+    })
+
+    test("uint64", () => {
+      const mockElement = { data: UINT64 }
+      const q = new Quiver(mockElement)
+      expect(getDataArray(q)).toEqual([
+        {
+          "(index)": 1,
+          "1": 1,
+          "2": 2,
+        },
+        {
+          "(index)": 2,
+          "1": 3,
+          "2": 4,
+        },
+      ])
+    })
+  })
+
+  describe("Unsupported", () => {
+    test("categorical", () => {
+      const mockElement = { data: CATEGORICAL }
+      const q = new Quiver(mockElement)
+      expect(getDataArray(q)).toEqual([
+        { c1: "foo", c2: 100 },
+        { c1: "bar", c2: 200 },
+      ])
+    })
+
+    test("unicode", () => {
+      const mockElement = { data: UNICODE }
+      const q = new Quiver(mockElement)
+
+      expect(getDataArray(q)).toEqual([
+        { c1: "foo", c2: "1" },
+        { c1: "bar", c2: "2" },
+      ])
     })
   })
 })
