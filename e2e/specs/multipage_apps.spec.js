0b61de8bab192090307af3743e8063c5f3063203
willhuang1997 <willhuang1997@gmail.com>
2023-12-07 15:37:09 -0800

    Uncomment cypress tests for multipage apps

--- e2e/specs/multipage_apps.spec.js
+++ e2e/specs/multipage_apps.spec.js
@@ -19,83 +19,83 @@ describe("multipage apps", () => {
     cy.loadApp("http://localhost:3000/");
   });
 
-  // it("loads the main streamlit_app script on initial page load", () => {
-  //   cy.get(".element-container .stMarkdown h2").should("contain", "Main Page");
-  // });
+  it("loads the main streamlit_app script on initial page load", () => {
+    cy.get(".element-container .stMarkdown h2").should("contain", "Main Page");
+  });
 
-  // it("renders the SidebarNav correctly", () => {
-  //   cy.prepForElementSnapshots();
+  it("renders the SidebarNav correctly", () => {
+    cy.prepForElementSnapshots();
 
-  //   cy.get("[data-testid='stSidebarNav']").matchThemedSnapshots(
-  //     "multipage-apps-sidebar-nav"
-  //   );
-  // });
+    cy.get("[data-testid='stSidebarNav']").matchThemedSnapshots(
+      "multipage-apps-sidebar-nav"
+    );
+  });
 
-  // it("can switch between pages by clicking on the SidebarNav links", () => {
-  //   cy.getIndexed('[data-testid="stSidebarNav"] a', 1).click();
+  it("can switch between pages by clicking on the SidebarNav links", () => {
+    cy.getIndexed('[data-testid="stSidebarNav"] a', 1).click();
 
-  //   cy.get(".element-container .stMarkdown h2").should("contain", "Page 2");
-  // });
+    cy.get(".element-container .stMarkdown h2").should("contain", "Page 2");
+  });
 
-  // it("supports navigating to a page directly via URL", () => {
-  //   cy.loadApp("http://localhost:3000/page2");
-  //   cy.get(".element-container .stMarkdown h2").should("contain", "Page 2");
-  // });
+  it("supports navigating to a page directly via URL", () => {
+    cy.loadApp("http://localhost:3000/page2");
+    cy.get(".element-container .stMarkdown h2").should("contain", "Page 2");
+  });
 
-  // it("can switch between pages and edit widgets", { retries: { runMode: 1 } }, () => {
-  //   cy.get('.stSlider [role="slider"]')
-  //     .click()
-  //     .type("{rightarrow}", { force: true });
+  it("can switch between pages and edit widgets", { retries: { runMode: 1 } }, () => {
+    cy.get('.stSlider [role="slider"]')
+      .click()
+      .type("{rightarrow}", { force: true });
 
-  //   cy.getIndexed('[data-testid="stSidebarNav"] a', 2).click();
+    cy.getIndexed('[data-testid="stSidebarNav"] a', 2).click();
 
-  //   cy.get(".element-container .stMarkdown h2").should("contain", "Page 3");
+    cy.get(".element-container .stMarkdown h2").should("contain", "Page 3");
 
-  //   // Identical widget on different page should be considered different
-  //   cy.get(".element-container .stMarkdown p").should("contain", "x is 0");
+    // Identical widget on different page should be considered different
+    cy.get(".element-container .stMarkdown p").should("contain", "x is 0");
 
-  //   cy.get('.stSlider [role="slider"]')
-  //     .click()
-  //     .type("{rightarrow}", { force: true });
+    cy.get('.stSlider [role="slider"]')
+      .click()
+      .type("{rightarrow}", { force: true });
 
-  //   cy.get(".element-container .stMarkdown p").should("contain", "x is 1");
-  // });
+    cy.get(".element-container .stMarkdown p").should("contain", "x is 1");
+  });
 
-  // it("can switch to the first page with a duplicate name", () => {
-  //   cy.getIndexed('[data-testid="stSidebarNav"] a', 3).click();
+  it("can switch to the first page with a duplicate name", () => {
+    cy.getIndexed('[data-testid="stSidebarNav"] a', 3).click();
 
-  //   cy.get(".element-container .stMarkdown h2").should("contain", "Page 4");
-  // });
+    cy.get(".element-container .stMarkdown h2").should("contain", "Page 4");
+  });
 
-  // it("can switch to the second page with a duplicate name", () => {
-  //   cy.getIndexed('[data-testid="stSidebarNav"] a', 4).click();
+  it("can switch to the second page with a duplicate name", () => {
+    cy.getIndexed('[data-testid="stSidebarNav"] a', 4).click();
 
-  //   cy.get(".element-container .stMarkdown h2").should("contain", "Page 5");
-  // });
+    cy.get(".element-container .stMarkdown h2").should("contain", "Page 5");
+  });
 
-  // it("runs the first page with a duplicate name if navigating via URL", () => {
-  //   cy.loadApp("http://localhost:3000/page_with_duplicate_name");
-  //   cy.get(".element-container .stMarkdown h2").should("contain", "Page 4");
-  // });
+  it("runs the first page with a duplicate name if navigating via URL", () => {
+    cy.loadApp("http://localhost:3000/page_with_duplicate_name");
+    cy.get(".element-container .stMarkdown h2").should("contain", "Page 4");
+  });
 
-  // it("serves the react app and displays the page not found modal if the page does not exist", () => {
-  //   cy.loadApp("http://localhost:3000/not_a_page");
+  it("serves the react app and displays the page not found modal if the page does not exist", () => {
+    cy.loadApp("http://localhost:3000/not_a_page");
 
-  //   cy.get('[role="dialog"]').should("contain", "Page not found");
-  // });
+    cy.get('[role="dialog"]').should("contain", "Page not found");
+  });
 
-  // it("handles expand & collapse of MPA nav correctly (arrow does not disappear)", () => {
-  //   cy.loadApp("http://localhost:3000/page_7");
+  it("handles expand & collapse of MPA nav correctly (arrow does not disappear)", () => {
+    cy.loadApp("http://localhost:3000/page_7");
 
-  //   // Expand the nav
-  //   cy.get('[data-testid="stSidebarNavSeparator"] svg').click();
+    // Expand the nav
+    cy.get('[data-testid="stSidebarNavSeparator"] svg').click();
 
-  //   // Collapse the nav
-  //   cy.get('[data-testid="stSidebarNavSeparator"] svg').click();
+    // Collapse the nav
+    cy.get('[data-testid="stSidebarNavSeparator"] svg').click();
 
-  //   // Expand the nav again
-  //   cy.get('[data-testid="stSidebarNavSeparator"] svg').click();
-  // });
+    // Expand the nav again
+    cy.get('[data-testid="stSidebarNavSeparator"] svg').click();
+  });
 
   it('removes query params when swapping pages', () => {
     cy.loadApp("http://localhost:3000?foo=bar");
