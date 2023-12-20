281f76255f4ca9ea49263a3df7ba7a8861e7f592
Maya Barnes <63436329+mayagbarnes@users.noreply.github.com>
2023-12-20 11:48:34 -0800

    Update License Headers (#7854)
    Update file license headers for 2024 🎉

--- .github/scripts/decrypt_credentials.sh
+++ .github/scripts/decrypt_credentials.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
+# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022-2024)
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
