bb5cab4f5dbfd6d896997b90949f56a839dba8c0
lukasmasuch <lukas.masuch@gmail.com>
2024-01-04 16:08:54 +0100

    Revert toast change

--- e2e/specs/st_toast.spec.js
+++ e2e/specs/st_toast.spec.js
@@ -14,63 +14,62 @@
  * limitations under the License.
  */
 
-const themes = ["light", "dark"];
+const themes = [ "light", "dark" ]
 
 describe("st.toast", () => {
-  before(() => {
-    cy.loadApp("http://localhost:3000/");
-  });
+    before(() => {
+        cy.loadApp("http://localhost:3000/");
+    });
 
-  beforeEach(() => {
-    // Rerun the script before each test so snapshots have sufficient time
-    cy.get("body").type("r");
-    cy.waitForScriptFinish();
-    cy.get("[data-testid='stToast']").should("have.length", 2);
-  });
-
-  themes.forEach((theme) => {
-    it(`displays default toast correctly - ${theme}`, () => {
-      if (theme === "dark") {
-        cy.changeTheme("Dark");
+    beforeEach(() => {
+        // Rerun the script before each test so snapshots have sufficient time
         cy.get("body").type("r");
-        cy.waitForScriptFinish();
-        cy.get("[data-testid='stToast']").should("have.length", 2);
-      }
-
-      cy.getIndexed("[data-testid='stMarkdownContainer']", 1).should(
-        "contain.text",
-        "This is a default toast message"
-      );
-
-      cy.getIndexed("[data-testid='stToast']", 1).matchImageSnapshot(
-        `toast-default-${theme}`
-      );
+        cy.waitForScriptFinish()
+        cy.get("[data-testid='stToast']").should("have.length", 2)
     });
 
-    it(`displays long message toast correctly - ${theme}`, () => {
-      cy.getIndexed("[data-testid='stMarkdownContainer']", 0).should(
-        "contain.text",
-        "Random toast message that is a really really really really really really really long message, going way"
-      );
+    themes.forEach( theme => {
+        it(`displays default toast correctly - ${theme}`, () => {
+            if (theme === 'dark') {
+                cy.changeTheme('Dark')
+                cy.get("body").type("r");
+                cy.waitForScriptFinish()
+                cy.get("[data-testid='stToast']").should("have.length", 2)
+            }
 
-      cy.getIndexed("[data-testid='stToast']", 0).matchImageSnapshot(
-        `toast-long-${theme}`
-      );
-    });
+            cy.getIndexed("[data-testid='stMarkdownContainer']", 1)
+                .should("contain.text", "This is a default toast message")
 
-    it(`displays expanded long message toast correctly - ${theme}`, () => {
-      cy.getIndexed("[data-testid='stToast']", 0)
-        .find(".toastViewButton")
-        .click();
+            cy.getIndexed("[data-testid='stToast']", 1)
+                .matchImageSnapshot(`toast-default-${theme}`);
+        });
 
-      cy.getIndexed("[data-testid='stMarkdownContainer']", 0).should(
-        "contain.text",
-        "Random toast message that is a really really really really really really really long message, going way past the 3 line limit"
-      );
+        it(`displays long message toast correctly - ${theme}`, () => {
+            cy.getIndexed("[data-testid='stMarkdownContainer']", 0)
+                .should(
+                    "contain.text",
+                    "Random toast message that is a really really really really really really really long message, going way"
+                )
 
-      cy.getIndexed("[data-testid='stToast']", 0).matchImageSnapshot(
-        `toast-expanded-${theme}`
-      );
-    });
-  });
+            cy.getIndexed("[data-testid='stToast']", 0)
+                .matchImageSnapshot(`toast-long-${theme}`);
+        });
+
+        it(`displays expanded long message toast correctly - ${theme}`, () => {
+            cy.getIndexed("[data-testid='stToast']", 0).find('.toastViewButton').click();
+
+            cy.getIndexed("[data-testid='stMarkdownContainer']", 0)
+                .should(
+                    "contain.text",
+                    "Random toast message that is a really really really really really really really long message, going way past the 3 line limit"
+                );
+
+            cy.getIndexed("[data-testid='stToast']", 0)
+                .matchImageSnapshot(`toast-expanded-${theme}`);
+        });
+
+        it(`overlays toasts with st.chat_input - ${theme}`, () => {
+            cy.get("[data-testid='stBottomBlockContainer']").matchImageSnapshot(`toast+chatInput-${theme}`)
+        });
+    })
 });
