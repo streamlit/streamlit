34dac029cf5716c17e9f0ca87dda2ff938338ae8
Karen Javadyan <kajarenc@gmail.com>
2024-01-09 20:43:33 +0400

    make mypy happy

--- lib/streamlit/runtime/stats.py
+++ lib/streamlit/runtime/stats.py
@@ -73,12 +73,12 @@ def group_stats(stats: List[CacheStat]) -> List[CacheStat]:
     sorted_stats = sorted(stats, key=key_function)
     grouped_stats = itertools.groupby(sorted_stats, key=key_function)
 
-    for (category_name, cache_name), stats in grouped_stats:
+    for (category_name, cache_name), single_group_stats in grouped_stats:
         result.append(
             CacheStat(
                 category_name=category_name,
                 cache_name=cache_name,
-                byte_length=sum(map(lambda item: item.byte_length, stats)),
+                byte_length=sum(map(lambda item: item.byte_length, single_group_stats)),
             )
         )
     return result
