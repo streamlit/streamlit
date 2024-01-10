20f31a56f4e2ba58f56e7f0732270fe71c2cc9dd
willhuang1997 <willhuang1997@gmail.com>
2024-01-09 20:59:33 -0800

    Fix comment

--- frontend/app/src/App.tsx
+++ frontend/app/src/App.tsx
@@ -866,7 +866,7 @@ export class App extends PureComponent<Props, State> {
       // e.g. the case where the user clicks the back button.
       // See https://github.com/streamlit/streamlit/pull/6271#issuecomment-1465090690 for the discussion.
       if (prevPageName !== newPageName) {
-        // If embed params need to be changed, make sure to change to other parts of the code that reference parseEmbedUrlString
+        // If embed params need to be changed, make sure to change to other parts of the code that reference preserveEmbedQueryParams
         const queryString = preserveEmbedQueryParams()
         const qs = queryString ? `?${queryString}` : ""
 
