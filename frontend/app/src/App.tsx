ac4886575002137db54636693af03926ddf15ed1
Pat Finnigan <pat.finnigan@snowflake.com>
2024-01-12 16:43:49 -0800

    add ability for host to toggle widgets disabled state

--- frontend/app/src/App.tsx
+++ frontend/app/src/App.tsx
@@ -172,6 +172,7 @@ interface State {
   deployedAppMetadata: DeployedAppMetadata
   libConfig: LibConfig
   appConfig: AppConfig
+  appDisabled: boolean
 }
 
 const ELEMENT_LIST_BUFFER_TIMEOUT_MS = 10
@@ -285,6 +286,7 @@ export class App extends PureComponent<Props, State> {
       deployedAppMetadata: {},
       libConfig: {},
       appConfig: {},
+      appDisabled: false,
     }
 
     this.connectionManager = null
@@ -301,6 +303,9 @@ export class App extends PureComponent<Props, State> {
       rerunScript: this.rerunScript,
       clearCache: this.clearCache,
       sendAppHeartbeat: this.sendAppHeartbeat,
+      setAppDisabled: appDisabled => {
+        this.setState({ appDisabled })
+      },
       themeChanged: this.props.theme.setImportedTheme,
       pageChanged: this.onPageChange,
       isOwnerChanged: isOwner => this.setState({ isOwner }),
@@ -1653,6 +1658,7 @@ export class App extends PureComponent<Props, State> {
       hostToolbarItems,
       libConfig,
       appConfig,
+      appDisabled,
     } = this.state
     const developmentMode = showDevelopmentOptions(
       this.state.isOwner,
@@ -1674,6 +1680,9 @@ export class App extends PureComponent<Props, State> {
           onClose: this.closeDialog,
         })
       : null
+
+    const widgetsDisabled =
+      appDisabled || connectionState !== ConnectionState.CONNECTED
 
     // Attach and focused props provide a way to handle Global Hot Keys
     // https://github.com/greena13/react-hotkeys/issues/41
@@ -1778,7 +1787,7 @@ export class App extends PureComponent<Props, State> {
                 scriptRunId={scriptRunId}
                 scriptRunState={scriptRunState}
                 widgetMgr={this.widgetMgr}
-                widgetsDisabled={connectionState !== ConnectionState.CONNECTED}
+                widgetsDisabled={widgetsDisabled}
                 uploadClient={this.uploadClient}
                 componentRegistry={this.componentRegistry}
                 formsData={this.state.formsData}
