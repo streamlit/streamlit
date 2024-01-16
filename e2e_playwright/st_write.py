fcec52e10bf3ec546e8cbffb0f84cd3ba21a4743
lukasmasuch <lukas.masuch@gmail.com>
2024-01-16 23:42:12 +0100

    Add a lot more tests

--- e2e_playwright/st_write.py
+++ e2e_playwright/st_write.py
@@ -12,13 +12,19 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from collections import namedtuple
+from dataclasses import dataclass
+from datetime import datetime
+
 import altair as alt
+import graphviz
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import plotly.express as px
+import pydeck as pdk
 
 import streamlit as st
-from tests.streamlit import pyspark_mocks
 
 np.random.seed(0)
 
@@ -32,11 +38,35 @@ st.write("This <b>HTML tag</b> is escaped!")
 
 st.write("This <b>HTML tag</b> is not escaped!", unsafe_allow_html=True)
 
+
+class ClassWithReprHtml:
+    def _repr_html_(self):
+        return "This <b>HTML tag</b> is also not escaped!"
+
+
+st.write(ClassWithReprHtml())
+
+
+st.write(100)
+
+st.write(None)
+
+st.write(datetime(2021, 1, 1))
+
+st.write(np.float64(1.0))
+
+
 st.subheader("st.write(dataframe-like)")
 
-st.write(pyspark_mocks.DataFrame())
+st.write(pd.DataFrame(np.random.randn(25, 3), columns=["a", "b", "c"]))
 
-st.write(pd.DataFrame([1, 2, 3]))
+st.write(pd.Series([1, 2, 3]))
+
+st.write(
+    pd.DataFrame(np.random.randn(25, 3), columns=["a", "b", "c"]).style.format("{:.2%}")
+)
+
+st.write(np.arange(25).reshape(5, 5))
 
 st.subheader("st.write(json-like)")
 
@@ -44,9 +74,25 @@ st.write(["foo", "bar"])
 
 st.write({"foo": "bar"})
 
+st.write(st.session_state)
+st.write(st.experimental_user)
+st.write(st.query_params)
+
+Point = namedtuple("Point", ["x", "y"])
+st.write(Point(1, 2))
+
 st.subheader("st.write(help)")
 
-st.write(st.data_editor)
+st.write(st.dataframe)
+
+
+@dataclass
+class ExampleClass:
+    name: str
+    age: int
+
+
+st.write(ExampleClass)
 
 st.subheader("st.write(exception)")
 
@@ -61,6 +107,36 @@ st.write(fig)
 
 st.subheader("st.write(altair)")
 
-df = pd.DataFrame(np.random.randn(200, 3), columns=["a", "b", "c"])
+df = pd.DataFrame(np.random.randn(50, 3), columns=["a", "b", "c"])
 chart = alt.Chart(df).mark_circle().encode(x="a", y="b", size="c", color="c")
 st.write(chart)
+
+st.subheader("st.write(plotly)")
+
+fig = px.scatter(df, x="a", y="b")
+st.write(fig)
+
+st.subheader("st.write(graphviz)")
+
+graph = graphviz.Digraph()
+graph.edge("run", "intr")
+graph.edge("intr", "runbl")
+graph.edge("runbl", "run")
+
+st.write(graph)
+
+# Simple pydeck chart:
+
+st.subheader("st.write(pydeck)")
+
+st.write(
+    pdk.Deck(
+        map_style=None,
+        initial_view_state=pdk.ViewState(
+            latitude=37.76,
+            longitude=-122.4,
+            zoom=11,
+            pitch=50,
+        ),
+    )
+)
--- e2e_playwright/st_write_test.py
+++ e2e_playwright/st_write_test.py
@@ -19,31 +19,38 @@ def test_displays_markdown(app: Page):
     """Test that markdown is displayed correctly."""
 
     markdown_elements = app.get_by_test_id("stMarkdown")
-    expect(markdown_elements).to_have_count(4)
+    expect(markdown_elements).to_have_count(9)
 
     expect(markdown_elements.nth(0)).to_contain_text("Hello World")
     expect(markdown_elements.nth(1)).to_contain_text("This markdown is awesome! 😎")
     expect(markdown_elements.nth(2)).to_contain_text("This <b>HTML tag</b> is escaped!")
     expect(markdown_elements.nth(3)).to_contain_text("This HTML tag is not escaped!")
+    expect(markdown_elements.nth(4)).to_contain_text(
+        "This HTML tag is also not escaped!"
+    )
+    expect(markdown_elements.nth(5)).to_contain_text("100")
+    expect(markdown_elements.nth(6)).to_contain_text("None")
+    expect(markdown_elements.nth(7)).to_contain_text("2021-01-01 00:00:00")
+    expect(markdown_elements.nth(8)).to_contain_text("1.0")
 
 
 def test_display_dataframe(app: Page):
     """Test that st.write displays pyspark.sql.DataFrame and pd.Dataframe via st.dataframe."""
 
     dataframe_element = app.get_by_test_id("stDataFrame")
-    expect(dataframe_element).to_have_count(2)
+    expect(dataframe_element).to_have_count(4)
 
 
 def test_display_json(app: Page):
     """Test that st.write displays dicts and arrays as json data."""
     json_elements = app.get_by_test_id("stJson")
-    expect(json_elements).to_have_count(2)
+    expect(json_elements).to_have_count(6)
 
 
 def test_display_help(app: Page):
     """Test that st.write displays objects via st.help."""
     help_elements = app.get_by_test_id("stDocstring")
-    expect(help_elements).to_have_count(1)
+    expect(help_elements).to_have_count(2)
 
 
 def test_display_exception(app: Page):
@@ -62,3 +69,21 @@ def test_display_altair(app: Page):
     """Test that st.write displays altair charts."""
     altair_elements = app.get_by_test_id("stArrowVegaLiteChart")
     expect(altair_elements).to_have_count(1)
+
+
+def test_display_plotly(app: Page):
+    """Test that st.write displays plotly charts."""
+    plotly_elements = app.locator(".stPlotlyChart")
+    expect(plotly_elements).to_have_count(1)
+
+
+def test_display_graphviz(app: Page):
+    """Test that st.write displays graphviz charts."""
+    plotly_elements = app.get_by_test_id("stGraphVizChart")
+    expect(plotly_elements).to_have_count(1)
+
+
+def test_display_pydeck_chart(app: Page):
+    """Test that st.write displays pydeck charts."""
+    pydeck_elements = app.get_by_test_id("stDeckGlJsonChart")
+    expect(pydeck_elements).to_have_count(1)
