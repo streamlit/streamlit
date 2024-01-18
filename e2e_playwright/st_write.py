81d49e5905a2097a638ce9d8fa96e6ac83e7a7ea
lukasmasuch <lukas.masuch@gmail.com>
2024-01-18 01:02:39 +0100

    Fix unused import

--- e2e_playwright/st_write.py
+++ e2e_playwright/st_write.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import time
 from collections import namedtuple
 from dataclasses import dataclass
 from datetime import datetime
