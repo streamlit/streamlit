272f1ab8564cf66b1af000a9a4c27e5489f95c2a
willhuang1997 <willhuang1997@gmail.com>
2024-01-18 16:20:06 -0800

    Migrate modals cypress test to playwright

--- e2e_playwright/main_menu_test.py
+++ e2e_playwright/main_menu_test.py
@@ -22,3 +22,57 @@ def test_main_menu_images(themed_app: Page, assert_snapshot: ImageCompareFunctio
 
     element = themed_app.get_by_test_id("stMainMenuPopover")
     assert_snapshot(element, name="main_menu")
+
+
+def test_renders_settings_dialog_properly(
+    app: Page, assert_snapshot: ImageCompareFunction
+):
+    app.get_by_test_id("stMainMenu").click()
+
+    app.get_by_text("Settings").click()
+    assert_snapshot(app.get_by_role("dialog"), name="settings_dialog")
+
+
+def test_renders_screencast_dialog_properly(
+    app: Page, assert_snapshot: ImageCompareFunction
+):
+    app.get_by_test_id("stMainMenu").click()
+
+    app.get_by_text("Record a screencast").click()
+    assert_snapshot(app.get_by_role("dialog"), name="record_screencast_dialog")
+
+
+def test_renders_screencast_recorded_dialog_properly(
+    app: Page, assert_snapshot: ImageCompareFunction
+):
+    app.get_by_test_id("stMainMenu").click()
+
+    app.get_by_text("Record a screencast").click()
+    app.get_by_text("Start recording!").click()
+
+    # Wait 5 seconds because there is a 3! 2! 1! on the screen until recording occurs and there may be buffer
+    app.wait_for_timeout(5000)
+    app.get_by_test_id("stMainMenu").click()
+
+    app.get_by_text("Stop recording").click()
+
+    # don't use screenshot as the recording may differ so just check for specific text
+    expect(app.get_by_text("Preview your video below:")).to_be_visible
+
+
+def test_renders_screencast_dialog_properly(
+    app: Page, assert_snapshot: ImageCompareFunction
+):
+    app.get_by_test_id("stMainMenu").click()
+
+    app.get_by_text("About").click()
+    assert_snapshot(app.get_by_role("dialog"), name="about_dialog")
+
+
+def test_renders_screencast_dialog_properly(
+    app: Page, assert_snapshot: ImageCompareFunction
+):
+    app.get_by_test_id("stMainMenu").click()
+
+    app.get_by_text("Clear cache").click()
+    assert_snapshot(app.get_by_role("dialog"), name="clear_cache_dialog")
