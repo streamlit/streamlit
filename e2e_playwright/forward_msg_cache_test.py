a304a867dd3d99d53190ef56351cbba5e95ec4b0
willhuang1997 <willhuang1997@gmail.com>
2024-01-19 15:05:07 -0800

    Address comments

--- e2e_playwright/forward_msg_cache_test.py
+++ e2e_playwright/forward_msg_cache_test.py
@@ -17,7 +17,9 @@ from playwright.sync_api import Page, expect
 from e2e_playwright.conftest import rerun
 
 
-def test_button_labels_handle_markdown(app: Page):
+def test_forward_msg_cache_receives_msg(app: Page):
     app.evaluate("window.streamlitDebug.clearForwardMsgCache()")
     rerun(app)
     expect(app.get_by_role("dialog")).not_to_be_visible()
+
+    app.expect_request("**/_stcore/message/")
