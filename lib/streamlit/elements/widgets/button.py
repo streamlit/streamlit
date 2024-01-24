ad44935b8ea6f292746aa7fadbaf7e2dd96b45e1
Debbie Matthews <debbie.matthews@snowflake.com>
2024-01-24 11:20:07 -0800

    Include disabled example in st.page_link docstring

--- lib/streamlit/elements/widgets/button.py
+++ lib/streamlit/elements/widgets/button.py
@@ -505,7 +505,7 @@ class ButtonMixin:
         >>>
         >>> st.page_link("your_app.py", label="Home", icon="🏠")
         >>> st.page_link("pages/page_1.py", label="Page 1", icon="1️⃣")
-        >>> st.page_link("pages/page_2.py", label="Page 2", icon="2️⃣")
+        >>> st.page_link("pages/page_2.py", label="Page 2", icon="2️⃣", disabled=True)
         >>> st.page_link("http://www.google.com", label="Google", icon="🌎")
 
         The default navigation is shown here for comparison, but you can hide
