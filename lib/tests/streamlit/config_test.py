ef24272d3b0ed6c7d70dd8bd56baac314f172f4a
lukasmasuch <lukas.masuch@gmail.com>
2024-01-24 14:42:08 +0100

    Fix test

--- lib/tests/streamlit/config_test.py
+++ lib/tests/streamlit/config_test.py
@@ -391,6 +391,7 @@ class ConfigTest(unittest.TestCase):
                 "server.maxUploadSize",
                 "server.maxMessageSize",
                 "server.enableStaticServing",
+                "server.enableDataframeTruncation",
                 "server.sslCertFile",
                 "server.sslKeyFile",
                 "ui.hideTopBar",
