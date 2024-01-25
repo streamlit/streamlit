5880c5e62b103bcc6e4a93459203da93dbabe2c0
snehankekre <snehan.minerva@gmail.com>
2024-01-25 19:54:49 +0530

    Mixpanel tracks page views

--- frontend/app/src/App.tsx
+++ frontend/app/src/App.tsx
@@ -987,7 +987,7 @@ export class App extends PureComponent<Props, State> {
 
     if (config.mixpanelToken) {
       const mixpanelScript = `'<script type="text/javascript">(function(e,b){if(!b.__SV){let a,f,i,g;window.mixpanel=b;a=e.createElement("script");a.type="text/javascript";a.async=!0;a.src="https://cdn.mxpnl.com/libs/mixpanel-2-latest.min.js";f=e.getElementsByTagName("script")[0];f.parentNode.insertBefore(a,f);b._i=[];b.init=function(a,e,d){function f(b,h){const a=h.split(".");2==a.length&&(b=b[a[0]],h=a[1]);b[h]=function(){b.push([h].concat(Array.prototype.slice.call(arguments,0)))}}let c=b;"undefined"!==typeof d?c=b[d]=[]:d="mixpanel";c.people=c.people||[];c.toString=function(b){let a="mixpanel";"mixpanel"!==d&&(a+="."+d);b||(a+=" (stub)");return a};c.people.toString=function(){return c.toString(1)+".people (stub)"};i="disable time_event track track_pageview track_links track_forms register register_once alias unregister identify name_tag set_config people.set people.set_once people.unset people.increment people.append people.union people.track_charge people.clear_charges people.delete_user".split(" ");for(g=0;g<i.length;g++)f(c,i[g]);b._i.push([a,e,d])};b.__SV=1.2}})(document,window.mixpanel||[]);</script>'`
-      const mixpanelInit = `'<script type="text/javascript">mixpanel.init("${config.mixpanelToken}");</script>'`
+      const mixpanelInit = `'<script type="text/javascript">mixpanel.init("${config.mixpanelToken}", { track_pageview: true, persistence: 'localStorage' });</script>'`
       addScriptsToHead(mixpanelScript)
       addScriptsToHead(mixpanelInit)
     }
