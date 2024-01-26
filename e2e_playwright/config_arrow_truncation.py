9908aea1928f1c597955f36726ca411cc1800aef
lukasmasuch <lukas.masuch@gmail.com>
2024-01-26 17:05:08 +0100

    Fix copy paste error

--- e2e_playwright/config_arrow_truncation.py
+++ e2e_playwright/config_arrow_truncation.py
@@ -12,9 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Nothing to do here since the deploy button is part of the frontend even
-# without any Streamlit element being rendered.
-
 import numpy as np
 import pandas as pd
 
