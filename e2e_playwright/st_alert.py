5a3af2367fd7ce1815d2b25d2d32114fcb930d92
lukasmasuch <lukas.masuch@gmail.com>
2024-01-27 13:33:05 +0100

    Simplify alert test

--- e2e_playwright/st_alert.py
+++ e2e_playwright/st_alert.py
@@ -35,11 +35,6 @@ st.error("If I repeat myself enough the line should " + 20 * "wrap ")
 text = """
     This is an example error from caching.py
 
-    **More information:** to prevent unexpected behavior, Streamlit tries
-    to detect mutations in cached objects defined in your local files so
-    it can alert you when the cache is used incorrectly. However, something
-    went wrong while performing this check.
-
     This error can occur when your virtual environment lives in the same
     folder as your project, since that makes it hard for Streamlit to
     understand which files it should check. If you think that's what caused
@@ -63,7 +58,7 @@ text = """
     * Or add the argument `allow_output_mutation=True` to the `st.cache` decorator.
     """
 
+st.error(text)
+st.warning(text)
 st.info(text)
 st.success(text)
-st.warning(text)
-st.error(text)
