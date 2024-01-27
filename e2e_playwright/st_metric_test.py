b831efbf9ef239de39bab272bc5ee2e2a811c97a
lukasmasuch <lukas.masuch@gmail.com>
2024-01-27 12:15:27 +0100

    Use get_by_test_id in metric test

--- e2e_playwright/st_metric_test.py
+++ e2e_playwright/st_metric_test.py
@@ -17,37 +17,35 @@ from e2e_playwright.conftest import ImageCompareFunction
 
 
 def test_first_metric_in_first_row(app: Page):
-    expect(app.locator("[data-testid='stMetricLabel']").nth(0)).to_have_text(
-        "User growth"
-    )
-    expect(app.locator("[data-testid='stMetricValue']").nth(0)).to_have_text(" 123 ")
-    expect(app.locator("[data-testid='stMetricDelta']").nth(0)).to_have_text(" 123 ")
+    expect(app.get_by_test_id("stMetricLabel").nth(0)).to_have_text("User growth")
+    expect(app.get_by_test_id("stMetricValue").nth(0)).to_have_text(" 123 ")
+    expect(app.get_by_test_id("stMetricDelta").nth(0)).to_have_text(" 123 ")
 
 
 def test_second_metric_in_first_row(app: Page):
-    expect(app.locator("[data-testid='stMetricLabel']").nth(2)).to_have_text("S&P 500")
-    expect(app.locator("[data-testid='stMetricValue']").nth(2)).to_have_text(" -4.56 ")
-    expect(app.locator("[data-testid='stMetricDelta']").nth(2)).to_have_text(" -50 ")
+    expect(app.get_by_test_id("stMetricLabel").nth(2)).to_have_text("S&P 500")
+    expect(app.get_by_test_id("stMetricValue").nth(2)).to_have_text(" -4.56 ")
+    expect(app.get_by_test_id("stMetricDelta").nth(2)).to_have_text(" -50 ")
 
 
 def test_third_metric_in_first_row(app: Page):
-    expect(app.locator("[data-testid='stMetricLabel']").nth(4)).to_have_text(
+    expect(app.get_by_test_id("stMetricLabel]").nth(4)).to_have_text(
         "Apples I've eaten"
     )
-    expect(app.locator("[data-testid='stMetricValue']").nth(4)).to_have_text(" 23k ")
-    expect(app.locator("[data-testid='stMetricDelta']").nth(4)).to_have_text(" -20 ")
+    expect(app.get_by_test_id("stMetricValue").nth(4)).to_have_text(" 23k ")
+    expect(app.get_by_test_id("stMetricDelta").nth(4)).to_have_text(" -20 ")
 
 
 def test_green_up_arrow_render(themed_app: Page, assert_snapshot: ImageCompareFunction):
     assert_snapshot(
-        themed_app.locator('[data-testid="stMetric"]').nth(0),
+        themed_app.get_by_test_id("stMetric").nth(0),
         name="stMetric-green",
     )
 
 
 def test_red_down_arrow_render(themed_app: Page, assert_snapshot: ImageCompareFunction):
     assert_snapshot(
-        themed_app.locator('[data-testid="stMetric"]').nth(2),
+        themed_app.get_by_test_id("stMetric").nth(2),
         name="stMetric-red",
     )
 
@@ -56,7 +54,7 @@ def test_gray_down_arrow_render(
     themed_app: Page, assert_snapshot: ImageCompareFunction
 ):
     assert_snapshot(
-        themed_app.locator('[data-testid="stMetric"]').nth(4),
+        themed_app.get_by_test_id("stMetric").nth(4),
         name="stMetric-gray",
     )
 
@@ -65,7 +63,7 @@ def test_help_shows_up_without_columns(
     themed_app: Page, assert_snapshot: ImageCompareFunction
 ):
     assert_snapshot(
-        themed_app.locator('[data-testid="stMetric"]').nth(6),
+        themed_app.get_by_test_id("stMetric").nth(6),
         name="metric-with-help",
     )
 
@@ -74,7 +72,7 @@ def test_none_results_in_dash_in_value(
     themed_app: Page, assert_snapshot: ImageCompareFunction
 ):
     assert_snapshot(
-        themed_app.locator('[data-testid="stMetric"]').nth(7),
+        themed_app.get_by_test_id("stMetric").nth(7),
         name="metric-with-none-value",
     )
 
@@ -82,11 +80,9 @@ def test_none_results_in_dash_in_value(
 def test_label_visibility_set_to_hidden(
     themed_app: Page, assert_snapshot: ImageCompareFunction
 ):
-    expect(themed_app.locator("[data-testid='stMetricLabel']").nth(3)).to_have_text(
-        "Test 4"
-    )
+    expect(themed_app.get_by_test_id("stMetricLabel").nth(3)).to_have_text("Test 4")
     assert_snapshot(
-        themed_app.locator('[data-testid="stMetric"]').nth(3),
+        themed_app.get_by_test_id("stMetric").nth(3),
         name="metric-label-hidden",
     )
 
@@ -94,11 +90,9 @@ def test_label_visibility_set_to_hidden(
 def test_label_visibility_set_to_collapse(
     themed_app: Page, assert_snapshot: ImageCompareFunction
 ):
-    expect(themed_app.locator("[data-testid='stMetricLabel']").nth(5)).to_have_text(
-        "Test 5"
-    )
+    expect(themed_app.get_by_test_id("stMetricLabel").nth(5)).to_have_text("Test 5")
     assert_snapshot(
-        themed_app.locator('[data-testid="stMetric"]').nth(5),
+        themed_app.get_by_test_id("stMetric").nth(5),
         name="metric-label-collapse",
     )
 
@@ -107,6 +101,6 @@ def test_ellipses_and_help_shows_up_properly(
     themed_app: Page, assert_snapshot: ImageCompareFunction
 ):
     assert_snapshot(
-        themed_app.locator('[data-testid="stMetric"]').nth(8),
+        themed_app.get_by_test_id("stMetric").nth(8),
         name="metric-help-and-ellipses",
     )
