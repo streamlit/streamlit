1f46baf66901b98a9ec951054baa49ee9a537e26
Debbie Matthews <debbie.matthews@snowflake.com>
2024-01-30 15:06:31 -0800

    Revert "Use container instead of sidebar for inline chat_input example"
    This reverts commit 14a6a6d6f7d810e714254dbdaefa5160131456b3.

--- lib/streamlit/elements/widgets/chat.py
+++ lib/streamlit/elements/widgets/chat.py
@@ -277,7 +277,7 @@ class ChatMixin:
 
         >>> import streamlit as st
         >>>
-        >>> with st.container():
+        >>> with st.sidebar:
         >>>     prompt = st.chat_input("Say something")
         >>> if prompt:
         ...     st.write(f"User has sent the following prompt: {prompt}")
