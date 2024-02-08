ed4122e63ecab82d021745e6759041cbca5e8626
Maya Barnes <63436329+mayagbarnes@users.noreply.github.com>
2024-02-08 11:12:50 -0800

    Fix: `st.page_link` & `st.switch_page` handling / prefixed paths (#8085)
    Remove the convenience handling of preceding "/"

--- e2e_playwright/multipage_apps/pages/02_page2.py
+++ e2e_playwright/multipage_apps/pages/02_page2.py
@@ -16,6 +16,6 @@ import streamlit as st
 
 st.header("Page 2")
 
-page_6 = st.button("`/pages/06_page_6.py`")
+page_6 = st.button("`pages/06_page_6.py`")
 if page_6:
-    st.switch_page("/pages/06_page_6.py")
+    st.switch_page("pages/06_page_6.py")
