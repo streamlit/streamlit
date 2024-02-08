c000b7cfdf2ff9f6a1258b13d9ff0dcc08d95c4a
Debbie Matthews <debbie.matthews@snowflake.com>
2024-02-07 19:14:04 -0800

    Fix `stream` parameter for `st.write_stream`

--- lib/streamlit/elements/write.py
+++ lib/streamlit/elements/write.py
@@ -78,7 +78,7 @@ class WriteMixin:
 
         Parameters
         ----------
-        arg : Callable, Generator, Iterable, OpenAI Stream, or LangChain Stream
+        stream : Callable, Generator, Iterable, OpenAI Stream, or LangChain Stream
             The generator or iterable to stream.
 
         Returns
