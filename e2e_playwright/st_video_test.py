603387cf0deae16f20ff23958623895f0d2ba68b
lukasmasuch <lukas.masuch@gmail.com>
2024-02-11 19:27:57 +0100

    Fix test

--- e2e_playwright/st_video_test.py
+++ e2e_playwright/st_video_test.py
@@ -56,7 +56,7 @@ def test_handles_changes_in_start_time(
 ):
     themed_app.wait_for_timeout(2000)
     # Change the start time of second video from 6 to 5
-    themed_app.get_by_test_id("stNumberInput").locator("button.step-up").click()
+    themed_app.get_by_test_id("stNumberInput").locator("button.step-down").click()
     # Wait for the video start time to update
     themed_app.wait_for_timeout(2000)
 
