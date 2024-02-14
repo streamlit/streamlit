ec957e37c3419d0bd99d5f262cd6e159b5f17440
Karen Javadyan <kajarenc@gmail.com>
2024-02-14 18:33:32 +0400

    more specific rule for exclude video from header check only from `e2e_playwright/video_test_asset`

--- scripts/check_license_headers.py
+++ scripts/check_license_headers.py
@@ -35,9 +35,9 @@ IGNORE_PATTERN = re.compile(
     r"^\.(github)/"
     # Exclude images.
     r"|\.(?:png|jpg|jpeg|gif|ttf|woff|otf|eot|woff2|ico|svg)$"
-    # Exclude videos.
-    r"|\.(?:mp4|webm)$"
-    # Exclude subtitle files we use for testing video.
+    # Exclude videos we use for testing st.video.
+    r"|e2e_playwright/video_test_assets/.*\.(mp4|webm)$"
+    # Exclude subtitle files we use for testing st.video.
     r"|e2e_playwright/video_test_assets/.*\.(vtt|srt)$"
     # Exclude files, because they make it obvious which product they relate to.
     r"|(LICENSE|NOTICES|CODE_OF_CONDUCT\.md|README\.md|CONTRIBUTING\.md|SECURITY.md)$"
