d38c79ad290308b1f172ac99bfbb5085cc2c1110
Karen Javadyan <kajarenc@gmail.com>
2024-02-14 17:25:54 +0400

    revert commit to trigger CI

--- lib/streamlit/elements/lib/subtitle_utils.py
+++ lib/streamlit/elements/lib/subtitle_utils.py
@@ -123,7 +123,7 @@ def process_subtitle_data(
 
         content_string = data_str.strip()
 
-        if content_string.startswith("WEBVTT") or not content_string:
+        if content_string.startswith("WEBVTT"):
             return content_string.encode("utf-8")
         elif _is_srt(content_string):
             return srt_to_vtt(content_string)
--- lib/streamlit/elements/media.py
+++ lib/streamlit/elements/media.py
@@ -351,7 +351,7 @@ def marshall_video(
 
         for label, subtitle_data in subtitle_items:
             sub = proto.subtitles.add()
-            sub.label = label or ""
+            sub.label = label
             # Coordinates used in media_file_manager to identify the place of element,
             # in case of subtitle, we use same video coordinates with suffix.
             # It is not aligned with common coordinates format, but in
