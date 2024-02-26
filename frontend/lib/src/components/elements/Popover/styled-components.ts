8e7b4f9aa1472c065d4d45678aacdc973d383af0
lukasmasuch <lukas.masuch@gmail.com>
2024-02-26 16:13:01 +0100

    Change margin

--- frontend/lib/src/components/elements/Popover/styled-components.ts
+++ frontend/lib/src/components/elements/Popover/styled-components.ts
@@ -17,7 +17,7 @@
 import styled from "@emotion/styled"
 
 export const StyledPopoverButtonIcon = styled.div(({ theme }) => ({
-  marginLeft: theme.spacing.twoXSPx,
+  marginLeft: theme.spacing.threeXSPx,
   // This is a hacky way to offset the 5px "padding" of the expansion svg
   // icon. Reason is that we want to use the same padding to the right side
   // as the text on the left side. The alternative would be to overwrite the
