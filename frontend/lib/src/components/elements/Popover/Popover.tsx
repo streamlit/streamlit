a55d7e5ba20919ae2ed42bd0ff0d2e5ff7e247bf
braethlein <benjamin.raethlein@gmail.com>
2024-03-11 14:56:24 +0100

    Update size of large dialog

--- frontend/lib/src/components/elements/Popover/Popover.tsx
+++ frontend/lib/src/components/elements/Popover/Popover.tsx
@@ -86,7 +86,7 @@ const Popover: React.FC<React.PropsWithChildren<PopoverProps>> = ({
 
               maxHeight: "70vh",
               overflow: "auto",
-              maxWidth: `calc(${theme.sizes.contentMaxWidth} - 2rem)`,
+              maxWidth: `calc(${theme.sizes.contentMaxWidth} - 2*${theme.spacing.lg})`,
               minWidth: element.useContainerWidth
                 ? // If use_container_width==True, we use the container width as minimum:
                   `${Math.max(width, 160)}px` // 10rem ~= 160px
