b9bfc424021f8d574eb07461c8081bd7984a9cbe
braethlein <benjamin.raethlein@gmail.com>
2024-03-13 22:07:10 +0100

    Update error messages

--- lib/streamlit/elements/dialog_decorator.py
+++ lib/streamlit/elements/dialog_decorator.py
@@ -26,21 +26,22 @@ def dialog_decorator(
 ) -> Callable[[Callable[..., None]], Callable[..., None]]:
     if title is None or title == "":
         raise StreamlitAPIException(
-            'A non-empty `title` argument has to be provided for dialogs, for example `@st.dialog("Example Title")`.'
+            'A non-empty `title` argument has to be provided for dialogs, for example `@st.experimental_dialog("Example Title")`.'
         )
 
     def inner_decorator(fn: Callable[..., None], *args) -> Callable[..., None]:
         # This check is for the scenario where @st.dialog is used without parentheses
         if fn is None or len(args) > 0:
             raise StreamlitAPIException(
-                'The dialog decoration failed. A common error for this to happen is when the dialog decorator is used like `@st.dialog` instead of `@st.dialog(title="My Title")`.'
+                "The dialog decoration failed. A common error for this to happen is when the dialog decorator is used without a title, i.e. `@st.experimental_dialog` instead of `@st.experimental_dialog(”My title”)`."
             )
 
         def decorated_fn(*args, **kwargs) -> None:
             dialog = st._main.dialog(title=title, dismissible=True, width=width)
             dialog.open()
 
-            # TODO: here we add the @st.fragment annotation
+            # TODO: here we add the @st.fragment annotation:
+            # @st.experimental_fragment
             def dialog_content() -> None:
                 # if the dialog should be closed, st.rerun() has to be called (same behavior as with st.fragment)
                 _ = fn(*args, **kwargs)
