0862d5212f3e750f97f09c81a9a185862ed33072
willhuang1997 <willhuang1997@gmail.com>
2024-03-19 13:08:55 -0400

    Migrate st_divider to playwright (#8313)
    <!--
    ⚠️ BEFORE CONTRIBUTING PLEASE READ OUR CONTRIBUTING GUIDELINES!
    https://github.com/streamlit/streamlit/wiki/Contributing
    -->
    
    ## Describe your changes
    - migrate st_divider to playwright
    ## GitHub Issue Link (if applicable)
    
    ## Testing Plan
    
    - Explanation of why no additional tests are needed
    - Unit Tests (JS and/or Python)
    - E2E Tests
    - Any manual testing needed?
    
    ---
    
    **Contribution License Agreement**
    
    By submitting this pull request you agree that all contributions to this
    project are made under the Apache 2.0 license.

---
+++ e2e_playwright/st_divider_test.py
@@ -0,0 +1,22 @@
+# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022-2024)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from playwright.sync_api import Page, expect
+
+from e2e_playwright.conftest import ImageCompareFunction
+
+
+def test_divider_renders(themed_app: Page, assert_snapshot: ImageCompareFunction):
+    expect(themed_app.locator(".element-container .stMarkdown hr")).to_be_visible()
+    assert_snapshot(themed_app.get_by_test_id("stMarkdown"), name="st_divider")
