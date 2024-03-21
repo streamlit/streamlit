4122a9316e22106461bfd9e5ea33dbc45f13eb1f
willhuang1997 <willhuang1997@gmail.com>
2024-03-21 13:29:15 -0700

    Move double click into another test

--- e2e_playwright/st_plotly_chart_select_test.py
+++ e2e_playwright/st_plotly_chart_select_test.py
@@ -82,25 +82,9 @@ from e2e_playwright.conftest import ImageCompareFunction, wait_for_app_run
 #     expect(app.get_by_test_id("stDataFrame")).to_have_count(1)
 
 
-# def test_lasso_select_on_histogram_chart_displays_a_df(app: Page):
-#     chart = app.locator(".stPlotlyChart").nth(5)
-#     chart.scroll_into_view_if_needed()
-#     expect(chart).to_be_visible()
-#     chart.hover()
-#     app.mouse.down()
-#     app.mouse.move(350, 350)
-#     app.mouse.down()
-#     app.mouse.move(375, 375)
-#     app.mouse.down()
-#     app.mouse.move(400, 400)
-#     app.mouse.down()
-#     app.mouse.move(435, 500)
-#     app.mouse.up()
-#     wait_for_app_run(app, 3000)
-#     expect(app.get_by_test_id("stDataFrame")).to_have_count(1)
-
-
-def test_double_click_resets_graph(app: Page, assert_snapshot: ImageCompareFunction):
+def test_lasso_select_on_histogram_chart_displays_a_df_and_resets_when_double_clicked(
+    app: Page, assert_snapshot: ImageCompareFunction
+):
     chart = app.locator(".stPlotlyChart").nth(5)
     chart.scroll_into_view_if_needed()
     expect(chart).to_be_visible()
@@ -115,5 +99,11 @@ def test_double_click_resets_graph(app: Page, assert_snapshot: ImageCompareFunct
     app.mouse.move(435, 500)
     app.mouse.up()
     wait_for_app_run(app, 3000)
-    chart.dblclick()
+    expect(app.get_by_test_id("stDataFrame")).to_have_count(1)
+
+    chart = app.locator(".stPlotlyChart").nth(5)
+    chart.scroll_into_view_if_needed()
+
+    app.mouse.dblclick(350, 350)
+    wait_for_app_run(app, 10000)
     assert_snapshot(chart, name="st_plotly_chart-reset")
