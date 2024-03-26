16c3a7ed1d83f25ecadab6472afbf9c4e40bf785
willhuang1997 <willhuang1997@gmail.com>
2024-03-25 17:23:54 -0700

    uncomment tests and only use chromium for double click

--- e2e_playwright/st_plotly_chart_select_test.py
+++ e2e_playwright/st_plotly_chart_select_test.py
@@ -12,76 +12,79 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
+import pytest
 from playwright.sync_api import Page, expect
 
 from e2e_playwright.conftest import ImageCompareFunction, wait_for_app_run
 
-# def test_box_select_on_scatter_chart_displays_a_df(app: Page):
-#     chart = app.locator(".stPlotlyChart").nth(0)
-#     expect(chart).to_be_visible()
-#     chart.hover()
-#     app.mouse.down()
-#     app.mouse.move(50, 50)
-#     app.mouse.down()
-#     app.mouse.move(150, 150)
-#     app.mouse.up()
-#     expect(app.get_by_test_id("stDataFrame")).to_have_count(1)
+
+def test_box_select_on_scatter_chart_displays_a_df(app: Page):
+    chart = app.locator(".stPlotlyChart").nth(0)
+    expect(chart).to_be_visible()
+    chart.hover()
+    app.mouse.down()
+    app.mouse.move(50, 50)
+    app.mouse.down()
+    app.mouse.move(150, 150)
+    app.mouse.up()
+    expect(app.get_by_test_id("stDataFrame")).to_have_count(1)
 
 
-# def test_lasso_select_on_line_chart_displays_a_df(app: Page):
-#     chart = app.locator(".stPlotlyChart").nth(1)
-#     chart.scroll_into_view_if_needed()
-#     expect(chart).to_be_visible()
-#     chart.hover()
-#     app.mouse.down()
-#     app.mouse.move(350, 350)
-#     app.mouse.down()
-#     app.mouse.move(375, 375)
-#     app.mouse.down()
-#     app.mouse.move(400, 400)
-#     app.mouse.down()
-#     app.mouse.move(435, 500)
-#     app.mouse.up()
-#     expect(app.get_by_test_id("stDataFrame")).to_have_count(1)
+def test_lasso_select_on_line_chart_displays_a_df(app: Page):
+    chart = app.locator(".stPlotlyChart").nth(1)
+    chart.scroll_into_view_if_needed()
+    expect(chart).to_be_visible()
+    chart.hover()
+    app.mouse.down()
+    app.mouse.move(350, 350)
+    app.mouse.down()
+    app.mouse.move(375, 375)
+    app.mouse.down()
+    app.mouse.move(400, 400)
+    app.mouse.down()
+    app.mouse.move(435, 500)
+    app.mouse.up()
+    expect(app.get_by_test_id("stDataFrame")).to_have_count(1)
 
 
-# # This test could be flakey because https://github.com/plotly/plotly.js/issues/6898
-# def test_click_on_bar_chart_displays_a_df(app: Page):
-#     chart = app.locator(".stPlotlyChart").nth(2)
-#     chart.scroll_into_view_if_needed()
-#     expect(chart).to_be_visible()
-#     chart.hover()
-#     app.mouse.down()
-#     app.mouse.up()
+# This test could be flakey because https://github.com/plotly/plotly.js/issues/6898
+def test_click_on_bar_chart_displays_a_df(app: Page):
+    chart = app.locator(".stPlotlyChart").nth(2)
+    chart.scroll_into_view_if_needed()
+    expect(chart).to_be_visible()
+    chart.hover()
+    app.mouse.down()
+    app.mouse.up()
 
-#     expect(app.get_by_test_id("stDataFrame")).to_have_count(1)
-
-
-# def test_box_select_on_stacked_bar_chart_displays_a_df(app: Page):
-#     chart = app.locator(".stPlotlyChart").nth(3)
-#     chart.scroll_into_view_if_needed()
-#     expect(chart).to_be_visible()
-#     chart.hover()
-#     app.mouse.down()
-#     app.mouse.move(50, 50)
-#     app.mouse.down()
-#     app.mouse.move(150, 150)
-#     app.mouse.up()
-#     expect(app.get_by_test_id("stDataFrame")).to_have_count(1)
+    expect(app.get_by_test_id("stDataFrame")).to_have_count(1)
 
 
-# # This test could be flakey because https://github.com/plotly/plotly.js/issues/6898
-# def test_click_on_choroleth_chart_displays_a_df(app: Page):
-#     chart = app.locator(".stPlotlyChart").nth(4)
-#     chart.scroll_into_view_if_needed()
-#     expect(chart).to_be_visible()
-#     chart.hover()
-#     app.mouse.down()
-#     app.mouse.up()
-#     expect(app.get_by_test_id("stDataFrame")).to_have_count(1)
+def test_box_select_on_stacked_bar_chart_displays_a_df(app: Page):
+    chart = app.locator(".stPlotlyChart").nth(3)
+    chart.scroll_into_view_if_needed()
+    expect(chart).to_be_visible()
+    chart.hover()
+    app.mouse.down()
+    app.mouse.move(50, 50)
+    app.mouse.down()
+    app.mouse.move(150, 150)
+    app.mouse.up()
+    expect(app.get_by_test_id("stDataFrame")).to_have_count(1)
 
 
+# This test could be flakey because https://github.com/plotly/plotly.js/issues/6898
+def test_click_on_choroleth_chart_displays_a_df(app: Page):
+    chart = app.locator(".stPlotlyChart").nth(4)
+    chart.scroll_into_view_if_needed()
+    expect(chart).to_be_visible()
+    chart.hover()
+    app.mouse.down()
+    app.mouse.up()
+    expect(app.get_by_test_id("stDataFrame")).to_have_count(1)
+
+
+# TODO(willhuang1997): Looks like webkit is not working but it is working locally
+@pytest.mark.only_browser("chromium")
 def test_lasso_select_on_histogram_chart_displays_a_df_and_resets_when_double_clicked(
     app: Page, assert_snapshot: ImageCompareFunction
 ):
