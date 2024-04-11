250c579d27fe77513376864f29a42c7fa71ba9b8
willhuang1997 <willhuang1997@gmail.com>
2024-04-11 13:42:24 -0700

    Fix build frontend step

--- frontend/lib/src/components/elements/PlotlyChart/PlotlyChart.test.tsx
+++ frontend/lib/src/components/elements/PlotlyChart/PlotlyChart.test.tsx
@@ -32,6 +32,7 @@ const getProps = (
   }),
   width: 0,
   height: 0,
+  isFullScreen: false,
 })
 
 // eslint-disable-next-line testing-library/no-node-access -- There's no other way to get the parent element
