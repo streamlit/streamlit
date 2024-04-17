57c58bad73565d3d414ef443e52f7d7bdf7ca940
lukasmasuch <lukas.masuch@gmail.com>
2024-04-17 21:52:41 +0200

    Remove cypress comment

--- e2e_playwright/main_menu.py
+++ e2e_playwright/main_menu.py
@@ -13,10 +13,6 @@
 # limitations under the License.
 
 import streamlit as st
-
-# Not possible to test the urls in the menu as they are hidden behind
-# the click handler of the button
-# https://github.com/cypress-io/cypress-example-recipes/blob/master/examples/testing-dom__tab-handling-links/cypress/integration/tab_handling_anchor_links_spec.js
 from streamlit.commands.page_config import MenuItems
 
 menu_items: MenuItems = {"about": "_*This can be markdown!*_"}
