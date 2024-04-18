80f3634f776b0c685f76461c0e60ccee54e94e75
willhuang1997 <willhuang1997@gmail.com>
2024-04-18 16:28:50 -0700

    migrate st color picker to playwright

---
+++ e2e_playwright/st_color_picker_test.py
@@ -0,0 +1,29 @@
+# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022-2024)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+from playwright.sync_api import Page, expect
+
+from e2e_playwright.conftest import ImageCompareFunction
+
+
+def test_color_picker_widget_display(
+    themed_app: Page, assert_snapshot: ImageCompareFunction
+):
+    """Test that st.color_picker renders correctly."""
+    color_pickers = themed_app.get_by_test_id("stColorPicker")
+    expect(color_pickers).to_have_count(5)
+
+    for i, element in enumerate(color_pickers.all()):
+        assert_snapshot(element, name=f"st_color_picker-{i}")
