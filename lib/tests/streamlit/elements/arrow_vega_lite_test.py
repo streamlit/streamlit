29b54d444af1f75f3c514b5498947279a767b1c0
willhuang1997 <willhuang1997@gmail.com>
2024-04-18 18:43:13 -0400

    Fix vega_lite_test as it was using altair_chart (#8537)
    <!--
    ⚠️ BEFORE CONTRIBUTING PLEASE READ OUR CONTRIBUTING GUIDELINES!
    https://github.com/streamlit/streamlit/wiki/Contributing
    -->
    
    ## Describe your changes
    - title
    ## GitHub Issue Link (if applicable)
    
    ## Testing Plan
    
    - Explanation of why no additional tests are needed
    - Unit Tests (JS and/or Python)
    - E2E Tests
    - Any manual testing needed?
    
    ---
    
    **Contribution License Agreement**
    
    By submitting this pull request you agree that all contributions to this
    project are made under the Apache 2.0 license.

--- lib/tests/streamlit/elements/arrow_vega_lite_test.py
+++ lib/tests/streamlit/elements/arrow_vega_lite_test.py
@@ -194,7 +194,7 @@ class ArrowVegaLiteTest(DeltaGeneratorTestCase):
 
     def test_bad_theme(self):
         with self.assertRaises(StreamlitAPIException) as exc:
-            st.altair_chart(df1, theme="bad_theme")
+            st.vega_lite_chart(df1, theme="bad_theme")
 
         self.assertEqual(
             f'You set theme="bad_theme" while Streamlit charts only support theme=”streamlit” or theme=None to fallback to the default library theme.',
