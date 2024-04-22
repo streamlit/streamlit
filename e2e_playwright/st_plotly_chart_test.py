4023c5b1e4b4befa8552c1eea0a69e9b4fb95be6
willhuang1997 <willhuang1997@gmail.com>
2024-04-22 11:29:02 -0700

    Add plotly chart tests i commented out

--- e2e_playwright/st_plotly_chart_test.py
+++ e2e_playwright/st_plotly_chart_test.py
@@ -18,49 +18,50 @@ from playwright.sync_api import Page, expect
 
 from e2e_playwright.conftest import ImageCompareFunction, wait_for_app_run
 
+
 # Only do chromium as this can create a lot of screenshots
 # there should be no differences between chrome and safari and firefox
-# @pytest.mark.only_browser("chromium")
-# def test_plotly_has_consistent_visuals(
-#     themed_app: Page, assert_snapshot: ImageCompareFunction
-# ):
-#     snapshot_names = [
-#         "st_plotly_chart-none-theme",
-#         "st_plotly_chart-streamlit-theme-use-container-width",
-#         "st_plotly_chart-candlestick-streamlit-theme",
-#         "st_plotly_chart-sunburst-custom-color",
-#         "st_plotly_chart-contour-heatmap-together",
-#         "st_plotly_chart-waterfall-chart-custom-height-and-width",
-#         "st_plotly_chart-ternary-chart",
-#         "st_plotly_chart-table-plot",
-#         "st_plotly_chart-electric-colorscale",
-#         "st_plotly_chart-discrete-sequence",
-#         "st_plotly_chart-layout-customization",
-#         "st_plotly_chart-template-customization",
-#         "st_plotly_chart-histogram-chart",
-#     ]
-#     expect(themed_app.locator(".stPlotlyChart")).to_have_count(16)
-#     for i, name in enumerate(snapshot_names):
-#         assert_snapshot(
-#             themed_app.locator(".stPlotlyChart").nth(i),
-#             name=name,
-#         )
+@pytest.mark.only_browser("chromium")
+def test_plotly_has_consistent_visuals(
+    themed_app: Page, assert_snapshot: ImageCompareFunction
+):
+    snapshot_names = [
+        "st_plotly_chart-none-theme",
+        "st_plotly_chart-streamlit-theme-use-container-width",
+        "st_plotly_chart-candlestick-streamlit-theme",
+        "st_plotly_chart-sunburst-custom-color",
+        "st_plotly_chart-contour-heatmap-together",
+        "st_plotly_chart-waterfall-chart-custom-height-and-width",
+        "st_plotly_chart-ternary-chart",
+        "st_plotly_chart-table-plot",
+        "st_plotly_chart-electric-colorscale",
+        "st_plotly_chart-discrete-sequence",
+        "st_plotly_chart-layout-customization",
+        "st_plotly_chart-template-customization",
+        "st_plotly_chart-histogram-chart",
+    ]
+    expect(themed_app.locator(".stPlotlyChart")).to_have_count(16)
+    for i, name in enumerate(snapshot_names):
+        assert_snapshot(
+            themed_app.locator(".stPlotlyChart").nth(i),
+            name=name,
+        )
 
 
-# def test_plotly_has_correct_visuals(
-#     themed_app: Page, assert_snapshot: ImageCompareFunction
-# ):
-#     snapshot_names = [
-#         "st_plotly_chart-line-chart-specific-height-width",
-#         "st_plotly_chart-use-container-width-false-and-specified-height",
-#         "st_plotly_chart-none-theme-and-use-container-width",
-#     ]
-#     plotly_indices = [13, 14, 15]
-#     for i, name in enumerate(snapshot_names):
-#         assert_snapshot(
-#             themed_app.locator(".stPlotlyChart").nth(plotly_indices[i]),
-#             name=name,
-#         )
+def test_plotly_has_correct_visuals(
+    themed_app: Page, assert_snapshot: ImageCompareFunction
+):
+    snapshot_names = [
+        "st_plotly_chart-line-chart-specific-height-width",
+        "st_plotly_chart-use-container-width-false-and-specified-height",
+        "st_plotly_chart-none-theme-and-use-container-width",
+    ]
+    plotly_indices = [13, 14, 15]
+    for i, name in enumerate(snapshot_names):
+        assert_snapshot(
+            themed_app.locator(".stPlotlyChart").nth(plotly_indices[i]),
+            name=name,
+        )
 
 
 def test_plotly_use_container_width_false_fullscreen(
