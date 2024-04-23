10b9f14c0e48c51e2b2b124ce1658156528e637d
willhuang1997 <willhuang1997@gmail.com>
2024-04-23 10:36:19 -0700

    Add extra check for removing plotly config buttons

--- frontend/lib/src/components/elements/PlotlyChart/PlotlyChart.tsx
+++ frontend/lib/src/components/elements/PlotlyChart/PlotlyChart.tsx
@@ -448,7 +448,7 @@ export function PlotlyChart({
       ]
     }
 
-    if (!config.modeBarButtonsToRemove) {
+    if (!config.modeBarButtonsToRemove && isSelectionActivated) {
       // Only modify the mode bar buttons if it's not already set
       // in the config provided by the user.
 
