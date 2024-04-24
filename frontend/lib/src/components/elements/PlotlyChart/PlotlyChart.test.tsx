57352f4538edab8def3c341a7d1383d6bcd634c1
willhuang1997 <willhuang1997@gmail.com>
2024-04-24 12:41:44 -0400

    Add Interactive Plotly typescript unit tests (#8557)
    ## Describe your changes
    - title
    - added tests for:
      - `sendEmptySelection`
      - `handleSelection`
      - `applyTheming`
    ## GitHub Issue Link (if applicable)
    
    ## Testing Plan
    
    - Explanation of why no additional tests are needed
    - Unit Tests (JS and/or Python)
    - E2E Tests
    - Any manual testing needed?
    
    ---
    
    **Contribution License Agreement**
    
    By submitting this pull request you agree that all contributions to this
    project are made under the Apache 2.0 license.

--- frontend/lib/src/components/elements/PlotlyChart/PlotlyChart.test.tsx
+++ frontend/lib/src/components/elements/PlotlyChart/PlotlyChart.test.tsx
@@ -15,7 +15,23 @@
  */
 import "@testing-library/jest-dom"
 
-import { parseLassoPath, parseBoxSelection } from "./PlotlyChart"
+import {
+  parseLassoPath,
+  parseBoxSelection,
+  sendEmptySelection,
+  handleSelection,
+  applyTheming,
+} from "./PlotlyChart"
+import { PlotlyChart as PlotlyChartProto } from "@streamlit/lib/src/proto"
+import { WidgetStateManager } from "@streamlit/lib/src/WidgetStateManager"
+import { applyStreamlitTheme, layoutWithThemeDefaults } from "./CustomTheme"
+import { mockTheme } from "@streamlit/lib/src/mocks/mockTheme"
+
+jest.mock("./CustomTheme", () => ({
+  replaceTemporaryColors: jest.fn().mockReturnValue("{}"),
+  applyStreamlitTheme: jest.fn(),
+  layoutWithThemeDefaults: jest.fn().mockReturnValue({}),
+}))
 
 /**
  * PlotlyChart.test.tsx does not contain any React-testing-library tests because Plotly doesn't support it
@@ -69,5 +85,230 @@ describe("parsePlotlySelections", () => {
         y: [],
       })
     })
+  })
+})
+
+const getWidgetMgr = (): WidgetStateManager => {
+  const sendRerunBackMsg = jest.fn()
+  const formsDataChanged = jest.fn()
+  return new WidgetStateManager({
+    sendRerunBackMsg,
+    formsDataChanged,
+  })
+}
+
+describe("sendEmptySelection", () => {
+  it("sends a rerun msg if widget_state is empty", () => {
+    const widgetMgr = getWidgetMgr()
+    jest.spyOn(widgetMgr, "setStringValue")
+
+    sendEmptySelection(
+      widgetMgr,
+      { id: "plotly_chart" } as PlotlyChartProto,
+      undefined
+    )
+
+    expect(widgetMgr.setStringValue).toHaveBeenCalledTimes(1)
+  })
+
+  it("does not send a rerun msg if widget_state is empty", () => {
+    const widgetMgr = getWidgetMgr()
+    jest.spyOn(widgetMgr, "setStringValue")
+
+    const plotlyProto = { id: "plotly_chart" } as PlotlyChartProto
+
+    widgetMgr.setStringValue(
+      plotlyProto,
+      '{"select":{"points":[],"point_indices":[],"box":[],"lasso":[]}}',
+      { fromUi: true }
+    )
+
+    sendEmptySelection(
+      widgetMgr,
+      { id: "plotly_chart" } as PlotlyChartProto,
+      undefined
+    )
+
+    // setStringValue is not called again
+    expect(widgetMgr.setStringValue).toHaveBeenCalledTimes(1)
+  })
+})
+
+describe("handleSelection", () => {
+  const mockFragmentId = "testFragment"
+  const proto = {
+    id: "plotly_chart",
+    selectionMode: [0, 1, 2],
+  } as PlotlyChartProto
+
+  it("should return early if no event is provided", () => {
+    const widgetMgr = getWidgetMgr()
+    jest.spyOn(widgetMgr, "setStringValue")
+
+    // @ts-expect-error
+    handleSelection(undefined, widgetMgr, proto, mockFragmentId)
+    expect(widgetMgr.setStringValue).not.toHaveBeenCalled()
+  })
+
+  it("should handle an event with no points or selections", () => {
+    const event = { points: undefined, selections: undefined } as any
+    const widgetMgr = getWidgetMgr()
+
+    jest.spyOn(widgetMgr, "setStringValue")
+
+    handleSelection(event, widgetMgr, proto, mockFragmentId)
+    expect(widgetMgr.setStringValue).toHaveBeenCalledTimes(1)
+  })
+
+  it("should process events with points correctly", () => {
+    const event = {
+      points: [
+        { pointIndex: 1, data: { legendgroup: "group1" }, pointIndices: [1] },
+      ],
+    } as any
+    const widgetMgr = getWidgetMgr()
+
+    jest.spyOn(widgetMgr, "setStringValue")
+
+    handleSelection(event, widgetMgr, proto, mockFragmentId)
+    expect(widgetMgr.setStringValue).toHaveBeenCalledWith(
+      { id: "plotly_chart", selectionMode: [0, 1, 2] },
+      '{"select":{"points":[{"point_index":1,"point_indices":[1],"legendgroup":"group1"}],"point_indices":[1],"box":[],"lasso":[]}}',
+      { fromUi: true },
+      "testFragment"
+    )
+  })
+
+  it("should process box selections correctly", () => {
+    const event = {
+      selections: [
+        {
+          type: "rect",
+          xref: "x",
+          yref: "y",
+          x0: "0",
+          x1: "1",
+          y0: "0",
+          y1: "1",
+        },
+      ],
+    } as any
+    const widgetMgr = getWidgetMgr()
+
+    jest.spyOn(widgetMgr, "setStringValue")
+
+    handleSelection(event, widgetMgr, proto, undefined)
+    expect(widgetMgr.setStringValue).toHaveBeenCalledWith(
+      { id: "plotly_chart", selectionMode: [0, 1, 2] },
+      '{"select":{"points":[],"point_indices":[],"box":[{"xref":"x","yref":"y","x":["0","1"],"y":["0","1"]}],"lasso":[]}}',
+      { fromUi: true },
+      undefined
+    )
+  })
+
+  it("should process lasso selections correctly", () => {
+    const event = {
+      selections: [
+        { type: "path", xref: "x", yref: "y", path: "M4.0,8.0L4.0,7.8Z" },
+      ],
+    } as any
+    const widgetMgr = getWidgetMgr()
+
+    jest.spyOn(widgetMgr, "setStringValue")
+
+    handleSelection(event, widgetMgr, proto, mockFragmentId)
+    expect(widgetMgr.setStringValue).toHaveBeenCalledWith(
+      { id: "plotly_chart", selectionMode: [0, 1, 2] },
+      '{"select":{"points":[],"point_indices":[],"box":[],"lasso":[{"xref":"x","yref":"y","x":[4,4],"y":[8,7.8]}]}}',
+      { fromUi: true },
+      "testFragment"
+    )
+  })
+
+  it("should not rerun if lasso selection is present but has no lasso selection mode", () => {
+    const event = {
+      selections: [
+        { type: "path", xref: "x", yref: "y", path: "M4.0,8.0L4.0,7.8Z" },
+      ],
+    } as any
+    const widgetMgr = getWidgetMgr()
+
+    jest.spyOn(widgetMgr, "setStringValue")
+
+    handleSelection(
+      event,
+      widgetMgr,
+      // @ts-expect-error
+      { ...proto, selectionMode: [] },
+      mockFragmentId
+    )
+    expect(widgetMgr.setStringValue).not.toHaveBeenCalled()
+  })
+
+  it("should not rerun if box selection is present but has no box selection mode", () => {
+    const event = {
+      selections: [
+        {
+          type: "rect",
+          xref: "x",
+          yref: "y",
+          x0: "0",
+          x1: "1",
+          y0: "0",
+          y1: "1",
+        },
+      ],
+    } as any
+    const widgetMgr = getWidgetMgr()
+
+    jest.spyOn(widgetMgr, "setStringValue")
+
+    handleSelection(
+      event,
+      widgetMgr,
+      // @ts-expect-error
+      { ...proto, selectionMode: [] },
+      mockFragmentId
+    )
+    expect(widgetMgr.setStringValue).not.toHaveBeenCalled()
+  })
+
+  it("should not rerun if the return value is the same", () => {
+    const event = {
+      points: [],
+      selections: [],
+    } as any
+    const widgetMgr = getWidgetMgr()
+
+    jest.spyOn(widgetMgr, "setStringValue")
+
+    widgetMgr.setStringValue(
+      proto,
+      '{"select":{"points":[],"point_indices":[],"box":[],"lasso":[]}}',
+      { fromUi: true }
+    )
+
+    handleSelection(event, widgetMgr, proto, mockFragmentId)
+    expect(widgetMgr.setStringValue).toHaveBeenCalledTimes(1)
+  })
+})
+
+describe("applyTheming", () => {
+  it("applies Streamlit theme when theme is streamlit", () => {
+    const mockPlotlyFigure = { data: [{}], layout: {}, frames: [] }
+    const chartTheme = "streamlit"
+
+    applyTheming(mockPlotlyFigure, chartTheme, mockTheme.emotion)
+
+    expect(applyStreamlitTheme).toHaveBeenCalled()
+  })
+
+  it("applies default theme when not using the default plotly theme", () => {
+    const mockPlotlyFigure = { data: [{}], layout: {}, frames: [] }
+    const chartTheme = "default"
+
+    applyTheming(mockPlotlyFigure, chartTheme, mockTheme.emotion)
+
+    expect(layoutWithThemeDefaults).toHaveBeenCalled()
   })
 })
