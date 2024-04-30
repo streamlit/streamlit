b87efa2625007a7f90b0e9d7f795e92807732eda
Debbie Matthews <debbie.matthews@snowflake.com>
2024-04-29 17:04:07 -0700

    Update docstrings for Material icons

--- lib/streamlit/elements/toast.py
+++ lib/streamlit/elements/toast.py
@@ -74,11 +74,22 @@ class ToastMixin:
               Text and background colors can be nested (e.g.
               ``:orange[:blue-background[Hello]]``).
 
-        icon : str or None
-            An optional argument that specifies an emoji to use as
-            the icon for the toast. Shortcodes are not allowed, please use a
-            single character instead. E.g. "🚨", "🔥", "🤖", etc.
-            Defaults to None, which means no icon is displayed.
+        icon : str, None
+            An optional emoji or icon to display next to the alert. If ``icon``
+            is ``None`` (default), no icon is displayed. If ``icon`` is a
+            string, the following options are valid:
+
+            * A single-character emoji. For example, you can set ``icon="🚨"``
+              or ``icon="🔥"``. Emoji short codes are not supported.
+
+            * An icon from the Material Symbols library (outlined style) in the
+              format ``":material/icon_name:"`` where "icon_name" is the name
+              of the icon in snake case.
+
+              For example, ``icon=":material/thumb_up:"`` will display the
+              Thumb Up icon. Find additional icons in the `Material Symbols \
+              <https://fonts.google.com/icons?icon.set=Material+Symbols&icon.style=Outlined>`_
+              font library.
 
         Example
         -------
