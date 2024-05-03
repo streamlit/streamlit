480cc80b9cac911c3291dd495666542ce4a37441
Benjamin Räthlein <benjamin.raethlein@gmail.com>
2024-05-03 11:02:30 +0200

    Migrate typography-cypress test to playwright (#8594)
    ## Describe your changes
    
    This PR migrates the existing cypress test "typography" to playwright.
    The test code and cases are merged into the `st_markdown` test as that
    test file bundles the different markdown scenarios already.
    In this context, the existing `st_markdown` test is cleaned up a little
    bit to not double-test headers etc.
    
    The migration happens in preparation of the
    https://github.com/streamlit/streamlit/pull/8587 PR. In that PR a couple
    of screenshots need to be updated but I don't want to touch the legacy
    cypress test artifacts.
    ## GitHub Issue Link (if applicable)
    
    ---
    
    **Contribution License Agreement**
    
    By submitting this pull request you agree that all contributions to this
    project are made under the Apache 2.0 license.

--- e2e_playwright/st_markdown_test.py
+++ e2e_playwright/st_markdown_test.py
@@ -12,31 +12,36 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from playwright.sync_api import Page, expect
+from playwright.sync_api import Locator, Page, expect
 
 from e2e_playwright.conftest import ImageCompareFunction
 
 
-def test_markdown_all_elements_displayed(
+def test_different_markdown_elements_in_one_block_displayed(
     themed_app: Page, assert_snapshot: ImageCompareFunction
 ):
-    """Displays correct number of markdown elements"""
+    """Test that the block containing a mixture of different markdown elements is displayed correctly."""
 
     markdown_elements = themed_app.get_by_test_id("stMarkdown")
 
-    expect(markdown_elements).to_have_count(23)
+    expect(markdown_elements).to_have_count(50)
 
     # Snapshot one big markdown block containing a variety of elements to reduce number of snapshots
+    multi_markdown_format_container = markdown_elements.nth(14)
+    multi_markdown_format_container.scroll_into_view_if_needed()
     assert_snapshot(
-        markdown_elements.nth(markdown_elements.count() - 1),
-        name=f"st_markdown-num_elements_displayed",
+        multi_markdown_format_container,
+        name="st_markdown-many_elements_in_one_block",
     )
 
 
-def test_displays_markdown(app: Page):
+def test_displays_individual_markdowns(app: Page):
     """Verifies the correct text content of markdown elements."""
 
-    markdown_elements = app.get_by_test_id("stMarkdown")
+    # get markdown elements in main app view, not sidebar
+    markdown_elements = app.get_by_test_id("stAppViewBlockContainer").get_by_test_id(
+        "stMarkdown"
+    )
 
     # Assert the text content of each markdown element
     text = [
@@ -48,9 +53,6 @@ def test_displays_markdown(app: Page):
         "[][]",
         "Inline math with KaTeX\\KaTeXKATE​X",
         "ax2+bx+c=0ax^2 + bx + c = 0ax2+bx+c=0",
-        "Some header 1",
-        "Some header 2",
-        "Some header 3",
         "Col1Col2SomeData",
         "Bold text within blue background",
         "Italic text within red background",
@@ -62,10 +64,10 @@ def test_displays_markdown(app: Page):
         expect(markdown_elements.nth(i)).to_have_text(text[i])
 
     # Check that the style contains the correct background color
-    blue_background = markdown_elements.nth(12).locator("span").first
-    red_background = markdown_elements.nth(13).locator("span").first
-    rainbow_background = markdown_elements.nth(14).locator("span").first
-    green_background = markdown_elements.nth(15).locator("span").first
+    blue_background = markdown_elements.nth(9).locator("span").first
+    red_background = markdown_elements.nth(10).locator("span").first
+    rainbow_background = markdown_elements.nth(11).locator("span").first
+    green_background = markdown_elements.nth(12).locator("span").first
 
     expect(blue_background).to_have_css("background-color", "rgba(28, 131, 225, 0.1)")
     expect(red_background).to_have_css("background-color", "rgba(255, 43, 43, 0.1)")
@@ -80,20 +82,7 @@ def test_displays_markdown(app: Page):
     expect(markdown_elements.nth(4).locator("a")).to_have_attribute("href", "href")
 
 
-def test_markdown_displays_headers_anchors(app: Page):
-    """Displays headers with anchors"""
-
-    h1 = app.locator("h1#some-header-1")
-    h2 = app.locator("h2#some-header-2")
-    h3 = app.locator("h3#some-header-3")
-
-    expect(h1).to_have_count(2)
-    expect(h2).to_have_count(2)
-    expect(h3).to_have_count(2)
-
-    expect(h1.first).to_have_attribute("id", "some-header-1")
-    expect(h2.first).to_have_attribute("id", "some-header-2")
-    expect(h3.first).to_have_attribute("id", "some-header-3")
+# Headers in markdown tests
 
 
 def test_markdown_displays_long_headers_above_other_elements(
@@ -105,18 +94,86 @@ def test_markdown_displays_long_headers_above_other_elements(
         app.get_by_test_id("stVerticalBlock").get_by_test_id("stVerticalBlock").nth(0)
     )
 
-    assert_snapshot(long_header, name="st_markdown-long-markdown-header-above-table")
+    assert_snapshot(long_header, name="st_markdown-header_long_above_markdown_table")
 
 
-def test_markdown_displays_headings_and_markdown_together(
+def _get_container_of_text(app: Page, text: str) -> Locator:
+    """Get the parent container in which the passed text is located.
+    The tests are written in a way that the text and the headers are put
+    into the same container.
+    """
+
+    # take the 2nd match because the first would be the most outer block
+    return (
+        app.get_by_test_id("stVerticalBlock")
+        .filter(has=app.get_by_text(text, exact=True))
+        .nth(1)
+    )
+
+
+def test_header_attributes(app: Page):
+    # Test that headers with ids exist
+    h1 = app.locator("h1#header-header1")
+    h2 = app.locator("h2#header-header2")
+    h3 = app.locator("h3#header-header3")
+    h4 = app.locator("h4#header-header4")
+    h5 = app.locator("h5#header-header5")
+    h6 = app.locator("h6#header-header6")
+
+    expect(h1).to_have_count(7)
+    expect(h2).to_have_count(7)
+    expect(h3).to_have_count(7)
+    expect(h4).to_have_count(7)
+    expect(h5).to_have_count(7)
+    expect(h6).to_have_count(7)
+
+
+def test_match_snapshot_for_headers_in_sidebar(
     app: Page, assert_snapshot: ImageCompareFunction
 ):
-    """Checks how headings and markdown are displayed when called separately or together."""
+    """Test that headers in sidebar are rendered correctly."""
+    # expand the sidebar
+    app.get_by_test_id("collapsedControl").click()
+    sidebar = app.get_by_test_id("stSidebar")
+    expect(sidebar).to_be_visible()
+    assert_snapshot(sidebar, name="st_markdown-headers_in_sidebar")
 
-    heading_and_markdown_block = (
-        app.get_by_test_id("stVerticalBlock").get_by_test_id("stVerticalBlock").nth(1)
-    )
 
-    assert_snapshot(
-        heading_and_markdown_block, name="st_markdown-heading_and_markdown_combinations"
+def test_match_snapshot_for_headers_in_single_markdown_command(
+    app: Page, assert_snapshot: ImageCompareFunction
+):
+    """Test that snapshot of headers joined in a single string and written in a single st.markdown command is correct."""
+    container = _get_container_of_text(app, "Headers in single st.markdown command")
+    assert_snapshot(container, name="st_markdown-headers_joined_in_single_command")
+
+
+def test_match_snapshot_for_headers_in_multiple_markdown_commands(
+    app: Page, assert_snapshot: ImageCompareFunction
+):
+    """Test that snapshot of headers written in multiple st.markdown commands is correct"""
+    container = _get_container_of_text(app, "Headers in multiple st.markdown command")
+    assert_snapshot(container, name="st_markdown-headers_via_multiple_commands")
+
+
+def test_match_snapshot_for_columns(app: Page, assert_snapshot: ImageCompareFunction):
+    """Test that the st.markdown columns snapshot is correct."""
+    container = _get_container_of_text(app, "Headers in columns")
+    assert_snapshot(container, name="st_markdown-headers_in_columns")
+
+
+def test_match_snapshot_for_columns_with_elements_above(
+    app: Page, assert_snapshot: ImageCompareFunction
+):
+    """Test that the st.markdown columns with elements above snapshot is correct."""
+    container = _get_container_of_text(
+        app, "Headers in columns with other elements above"
     )
+    assert_snapshot(container, name="st_markdown-headers_in_labeled_columns")
+
+
+def test_match_snapshot_for_column_beside_widget(
+    app: Page, assert_snapshot: ImageCompareFunction
+):
+    """Test that the st.markdown columns beside widget snapshot is correct."""
+    container = _get_container_of_text(app, "Headers in column beside widget")
+    assert_snapshot(container, name="st_markdown-headers_beside_widget")
