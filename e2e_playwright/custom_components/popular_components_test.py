streamlit/streamlit

commit 9d98fe907e319cfccab71fa83da88ba40db9c775

Use uv installer in CI pipelines (#8437)
## Describe your changes

Use `uv` instead of `pip` for installing dependencies if `uv` is
installed since it provides a huge speed up in install time.

---

**Contribution License Agreement**

By submitting this pull request you agree that all contributions to this
project are made under the Apache 2.0 license.

Makefile
@@ -112,8 +112,13 @@ python-init:
 	if [ "${INSTALL_TEST_REQS}" = "true" ] ; then\
 		pip_args+=("--requirement" "lib/test-requirements.txt"); \
 	fi;\
-	echo "Running command: pip install $${pip_args[@]}";\
-	pip install $${pip_args[@]};
+	if command -v "uv" > /dev/null; then \
+		echo "Running command: uv pip install $${pip_args[@]}"; \
+		uv pip install $${pip_args[@]}; \
+	else \
+		echo "Running command: pip install $${pip_args[@]}"; \
+		pip install $${pip_args[@]}; \
+	fi;\
 	if [ "${INSTALL_TEST_REQS}" = "true" ] ; then\
 		python -m playwright install --with-deps; \
 	fi;\
@@ -348,7 +353,14 @@ playwright:
 playwright-custom-components:
 	cd e2e_playwright; \
 	rm -rf ./test-results; \
-	pip install extra-streamlit-components streamlit-ace streamlit-antd-components streamlit-aggrid streamlit-autorefresh streamlit-chat streamlit-echarts streamlit-folium streamlit-lottie streamlit-option-menu streamlit-url-fragment; \
+	pip_args="extra-streamlit-components streamlit-ace streamlit-antd-components streamlit-aggrid streamlit-autorefresh streamlit-chat streamlit-echarts streamlit-folium streamlit-lottie streamlit-option-menu streamlit-url-fragment";\
+	if command -v "uv" > /dev/null; then \
+		echo "Running command: uv pip install $(pip_args)"; \
+		uv pip install $(pip_args); \
+	else \
+		echo "Running command: pip install $(pip_args)"; \
+		pip install $(pip_args); \
+	fi;\
 	pytest ${custom_components_test_folder} --browser webkit --browser chromium --browser firefox --video retain-on-failure --screenshot only-on-failure --output ./test-results/ -n auto --reruns 1 --reruns-delay 1 --rerun-except "Missing snapshot" --durations=5 -r aR -v
 
 .PHONY: loc

.github/actions/make_init/action.yml
@@ -70,6 +70,7 @@ runs:
         md5sum lib/dev-requirements.txt >> $GITHUB_WORKSPACE/python_cache_key.md5
         md5sum lib/test-requirements.txt >> $GITHUB_WORKSPACE/python_cache_key.md5
         md5sum lib/setup.py >> $GITHUB_WORKSPACE/python_cache_key.md5
+        md5sum Makefile >> $GITHUB_WORKSPACE/python_cache_key.md5
         date +%F >> $GITHUB_WORKSPACE/python_cache_key.md5
       shell: bash --login -eo pipefail {0}
     - if: inputs.use_cached_venv == 'true'
@@ -85,6 +86,7 @@ runs:
         python -m venv venv
         source venv/bin/activate
         pip install --upgrade pip
+        pip install uv
         make python-init
         deactivate
       shell: bash --login -eo pipefail {0}

.github/workflows/python-min-deps.yml
@@ -109,6 +109,7 @@ jobs:
           md5sum lib/min-constraints-gen.txt >> $GITHUB_WORKSPACE/python_cache_key.md5
           md5sum lib/test-requirements.txt >> $GITHUB_WORKSPACE/python_cache_key.md5
           md5sum lib/setup.py >> $GITHUB_WORKSPACE/python_cache_key.md5
+          md5sum Makefile >> $GITHUB_WORKSPACE/python_cache_key.md5
           date +%F >> $GITHUB_WORKSPACE/python_cache_key.md5
       - name: Restore virtualenv from cache
         id: cache-virtualenv
@@ -121,6 +122,7 @@ jobs:
           python -m venv venv
           source venv/bin/activate
           pip install --upgrade pip
+          pip install uv
           deactivate
       - name: Activate virtualenv
         run: echo 'source venv/bin/activate' >> $HOME/.bash_profile
@@ -131,7 +133,7 @@ jobs:
       # End of inlined make_init action
 
       - name: Make local modules visible
-        run: pip install --editable lib --no-deps
+        run: uv pip install --editable lib --no-deps
       - name: Run Python Tests
         run: make pytest
       - name: Run Integration Tests

commit 8a0ff4b947dbd47f2c83261884f5c32a16437dd6

Fix Makefile and e2e_playwright test for CustomComponents (#8617)
## Describe your changes

The `Makefile` step for the e2e_playwright custom component step was
broken since https://github.com/streamlit/streamlit/pull/8437 and is
fixed here.
Also, the popular_custom_components test did not wait for an app run,
which means that an exception triggered by using a custom component
would not have been caught properly.
Extracts these fixes from the PR
https://github.com/streamlit/streamlit/pull/8610

## GitHub Issue Link (if applicable)

## Testing Plan

- fixes an existing e2e test

---

**Contribution License Agreement**

By submitting this pull request you agree that all contributions to this
project are made under the Apache 2.0 license.

Makefile
@@ -353,14 +353,14 @@ playwright:
 playwright-custom-components:
 	cd e2e_playwright; \
 	rm -rf ./test-results; \
-	pip_args="extra-streamlit-components streamlit-ace streamlit-antd-components streamlit-aggrid streamlit-autorefresh streamlit-chat streamlit-echarts streamlit-folium streamlit-lottie streamlit-option-menu streamlit-url-fragment";\
+	pip_args="extra-streamlit-components streamlit-ace streamlit-antd-components streamlit-aggrid streamlit-autorefresh streamlit-chat streamlit-echarts streamlit-folium streamlit-lottie streamlit-option-menu streamlit-url-fragment"; \
 	if command -v "uv" > /dev/null; then \
-		echo "Running command: uv pip install $(pip_args)"; \
-		uv pip install $(pip_args); \
+		echo "Running command: uv pip install $${pip_args}"; \
+		uv pip install $${pip_args}; \
 	else \
-		echo "Running command: pip install $(pip_args)"; \
-		pip install $(pip_args); \
-	fi;\
+		echo "Running command: pip install $${pip_args}"; \
+		pip install $${pip_args}; \
+	fi; \
 	pytest ${custom_components_test_folder} --browser webkit --browser chromium --browser firefox --video retain-on-failure --screenshot only-on-failure --output ./test-results/ -n auto --reruns 1 --reruns-delay 1 --rerun-except "Missing snapshot" --durations=5 -r aR -v
 
 .PHONY: loc

e2e_playwright/custom_components/popular_components.py
@@ -127,8 +127,7 @@ def use_echarts():
 def use_extra_streamlit_components():
     from extra_streamlit_components import CookieManager
 
-    st.title("Cookies!")
-    st.help(CookieManager)
+    CookieManager()
 
 
 def use_folium():

e2e_playwright/custom_components/popular_components_test.py
@@ -14,6 +14,8 @@
 
 from playwright.sync_api import Page, expect
 
+from e2e_playwright.conftest import wait_for_app_run
+
 
 def _select_component(app: Page, component: str):
     selectbox_input = app.get_by_test_id("stSelectbox").locator("input")
@@ -21,16 +23,24 @@ def _select_component(app: Page, component: str):
     # Type an option (defined in the test app):
     selectbox_input.type(component)
     selectbox_input.press("Enter")
+    wait_for_app_run(app)
 
 
 def _expect_no_exception(app: Page):
+    """If there is an issue with importing / using the custom component, Streamlit throws an exception. So, expect that no exception was thrown."""
     expect(app.get_by_test_id("stException")).not_to_be_visible()
+
+
+def _expect_iframe_attached(app: Page):
+    """Expect the CustomComponent iframe to be attached to the DOM."""
+    expect(app.locator("iframe")).to_be_attached()
 
 
 def test_components_html(app: Page):
     """Test that components.html can be imported and used"""
     _select_component(app, "componentsHtml")
     _expect_no_exception(app)
+    _expect_iframe_attached(app)
     iframe = app.frame_locator("iframe")
     expect(iframe.locator("div", has_text="Hello World!")).to_be_attached()
 
@@ -39,18 +49,21 @@ def test_ace(app: Page):
     """Test that the ace component renders"""
     _select_component(app, "ace")
     _expect_no_exception(app)
+    _expect_iframe_attached(app)
 
 
 def test_aggrid(app: Page):
     """Test that the aggrid component renders"""
     _select_component(app, "aggrid")
     _expect_no_exception(app)
+    _expect_iframe_attached(app)
 
 
 def test_antd(app: Page):
     """Test that the ace component renders"""
     _select_component(app, "antd")
     _expect_no_exception(app)
+    _expect_iframe_attached(app)
 
 
 def test_autorefresh(app: Page):
@@ -62,6 +75,7 @@ def test_autorefresh(app: Page):
 def test_chat(app: Page):
     """Test that the chat component renders"""
     _select_component(app, "chat")
+    _expect_iframe_attached(app)
 
 
 def test_echarts(app: Page):
@@ -74,26 +88,31 @@ def test_extra_streamlit_components(app: Page):
     """Test that the extra-strealit-components component renders"""
     _select_component(app, "extraStreamlitComponents")
     _expect_no_exception(app)
+    _expect_iframe_attached(app)
 
 
 def test_folium(app: Page):
     """Test that the folium component renders"""
     _select_component(app, "folium")
+    _expect_iframe_attached(app)
 
 
 def test_lottie(app: Page):
     """Test that the lottie component renders"""
     _select_component(app, "lottie")
     _expect_no_exception(app)
+    _expect_iframe_attached(app)
 
 
 def test_option_menu(app: Page):
     """Test that the option-menu component renders"""
     _select_component(app, "optionMenu")
     _expect_no_exception(app)
+    _expect_iframe_attached(app)
 
 
 def test_url_fragment(app: Page):
     """Test that the url-fragment component renders"""
     _select_component(app, "urlFragment")
     _expect_no_exception(app)
+    _expect_iframe_attached(app)