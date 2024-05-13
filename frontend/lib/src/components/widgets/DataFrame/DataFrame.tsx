f50170f835aace1cc655036bd45fc668ba968288
Lukas Masuch <Lukas.Masuch@gmail.com>
2024-05-13 17:49:32 +0200

    Stylistic refactoring of dataframe widget state loading (#8648)
    ## Describe your changes
    
    This change was supposed to be included in the dataframe selections PR,
    but I pushed it to the wrong PR :(
    
    It just contains some stylistic changes.
    
    ---
    
    **Contribution License Agreement**
    
    By submitting this pull request you agree that all contributions to this
    project are made under the Apache 2.0 license.

--- frontend/lib/src/components/widgets/DataFrame/DataFrame.tsx
+++ frontend/lib/src/components/widgets/DataFrame/DataFrame.tsx
@@ -214,22 +214,34 @@ function DataFrame({
   const { columns: originalColumns } = useColumnLoader(element, data, disabled)
 
   /**
-   * On the first rendering, try to load the initial editing state
-   * from widget state if it exists. This is required in the case
-   * that other elements are inserted before this widget.
+   * On the first rendering, try to load initial widget state if
+   * it exists. This is required in the case that other elements
+   * are inserted before this widget. In this case, it can happen
+   * that the dataframe component is unmounted and thereby loses
+   * its state. Once the same element is rendered again, we try to
+   * reconstruct the state from the widget manager values.
    */
   React.useEffect(
     () => {
-      if (element.editingMode !== READ_ONLY) {
-        const initialWidgetValue = widgetMgr.getStringValue({
-          id: element.id,
-          formId: element.formId,
-        } as WidgetInfo)
-        if (initialWidgetValue) {
-          editingState.current.fromJson(initialWidgetValue, originalColumns)
-          setNumRows(editingState.current.getNumRows())
-        }
+      if (element.editingMode === READ_ONLY) {
+        // We don't need to load the initial widget state
+        // for read-only dataframes.
+        return
       }
+
+      const initialWidgetValue = widgetMgr.getStringValue({
+        id: element.id,
+        formId: element.formId,
+      } as WidgetInfo)
+
+      if (!initialWidgetValue) {
+        // No initial widget value was saved in the widget manager.
+        // No need to reconstruct something.
+        return
+      }
+
+      editingState.current.fromJson(initialWidgetValue, originalColumns)
+      setNumRows(editingState.current.getNumRows())
     },
     // We only want to run this effect once during the initial component load
     // so we disable the eslint rule.
@@ -881,7 +893,12 @@ function DataFrame({
               ? "multi"
               : "single",
             rowSelectionBlending: "mixed",
-            rangeSelectionBlending: "mixed",
+            // Deactivate the combination of row selections
+            // and cell selections. This will automatically clear
+            // selected cells when a row is selected.
+            // We are doing this to prevent some issues with drag
+            // and drop selection.
+            rangeSelectionBlending: "exclusive",
           })}
           // Activate features required for column selection:
           {...(isColumnSelectionActivated && {
@@ -891,7 +908,12 @@ function DataFrame({
               ? "multi"
               : "single",
             columnSelectionBlending: "mixed",
-            rangeSelectionBlending: "mixed",
+            // Deactivate the combination of column selections
+            // and cell selections. This will automatically clear
+            // selected cells when a column is selected.
+            // We are doing this to prevent some issues with drag
+            // and drop selection.
+            rangeSelectionBlending: "exclusive",
           })}
           // If element is editable, enable editing features:
           {...(!isEmptyTable &&
