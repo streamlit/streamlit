6b45d6858543b9648f2d3a28aa05131c2da43c44
lukasmasuch <lukas.masuch@gmail.com>
2024-05-13 20:34:02 +0200

    Add future annotations

--- lib/tests/streamlit/elements/vega_charts_test.py
+++ lib/tests/streamlit/elements/vega_charts_test.py
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from __future__ import annotations
+
 import json
 import unittest
 from typing import Any, Callable
