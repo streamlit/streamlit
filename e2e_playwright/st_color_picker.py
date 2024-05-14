9b9fc9b61e7e1944053e8f3cde2cf8b2fbec505d
willhuang1997 <willhuang1997@gmail.com>
2024-05-14 07:37:52 -0700

    Add a help into script

--- e2e_playwright/st_color_picker.py
+++ e2e_playwright/st_color_picker.py
@@ -17,7 +17,7 @@ import streamlit as st
 c1 = st.color_picker("Default Color")
 st.write("Color 1", c1)
 
-c2 = st.color_picker("New Color", "#EB144C")
+c2 = st.color_picker("New Color", "#EB144C", help="help string")
 st.write("Color 2", c2)
 
 c3 = st.color_picker("Disabled", disabled=True)
