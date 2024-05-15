7eac08df60d0ece29bf6164df8d1a10b52dd4840
Ken McGrady <ken.mcgrady@gmail.com>
2024-05-15 15:50:11 -0700

    Make the main script hash optional in AppRoot.empty()

--- frontend/lib/src/AppNode.ts
+++ frontend/lib/src/AppNode.ts
@@ -558,7 +558,7 @@ export class AppRoot {
    * Create an empty AppRoot with a placeholder "skeleton" element.
    */
   public static empty(
-    mainScriptHash: string,
+    mainScriptHash = "",
     isInitialRender = true,
     sidebarElements?: BlockNode | undefined
   ): AppRoot {
