d42186141fc9a1851fe7e1baad344aae75e18f4e
William Wei Huang <william.huang@snowflake.com>
2024-05-21 12:15:43 -0700

    Migrate select slider to playwright (#8703)
    ## Describe your changes
    - added fragment tests
    - added form tests
    - add clicking functionality tests
    - added dragging functionality tests
    - added left and right keys functionality tests
    - added tests for session state
    - added tests for key and help
    ## GitHub Issue Link (if applicable)
    
    ## Testing Plan
    
    - Explanation of why no additional tests are needed
    - Unit Tests (JS and/or Python)
    - E2E Tests
    - Any manual testing needed?
    
    ---
    
    **Contribution License Agreement**
    
    By submitting this pull request you agree that all contributions to this
    project are made under the Apache 2.0 license.
    
    ---------
    
    Co-authored-by: willhuang1997 <willhuang1997@gmail.com>

--- e2e_playwright/st_select_slider.py
+++ e2e_playwright/st_select_slider.py
@@ -18,28 +18,38 @@ import pandas as pd
 import streamlit as st
 from streamlit import runtime
 
+
+def format_option(option):
+    return f"Color: {option}"
+
+
 w1 = st.select_slider(
-    "Label 1",
+    "Label 1 (format_func with key and help)",
     value=("orange", "blue"),
     options=["red", "orange", "yellow", "green", "blue", "indigo", "violet"],
+    format_func=format_option,
+    key="first_select_slider",
+    help="Help in a select slider",
 )
+if "first_select_slider" in st.session_state:
+    st.write("Value 1:", st.session_state.first_select_slider)
 st.write("Value 1:", w1)
 
 w2 = st.select_slider(
-    "Label 2",
+    "Label 2 (no default)",
     options=np.array([1, 2, 3, 4, 5]),
 )
 st.write("Value 2:", w2)
 
 w3 = st.select_slider(
-    "Label 3",
+    "Label 3 (default with ints and series)",
     value=[2, 5],
     options=pd.Series([1, 2, 3, 4, 5, 6, 7, 8, 9]),
 )
 st.write("Value 3:", w3)
 
 w4 = st.select_slider(
-    "Label 4",
+    "Label 4 (default with pandas df)",
     value=5,
     options=pd.DataFrame(
         {
@@ -51,7 +61,7 @@ w4 = st.select_slider(
 st.write("Value 4:", w4)
 
 w5 = st.select_slider(
-    "Label 5",
+    "Label 5 (disabled)",
     value=("orange", "blue"),
     options=["red", "orange", "yellow", "green", "blue", "indigo", "violet"],
     disabled=True,
@@ -59,7 +69,7 @@ w5 = st.select_slider(
 st.write("Value 5:", w5)
 
 w6 = st.select_slider(
-    "Label 6",
+    "Label 6 (hidden visibility)",
     options=["red", "orange", "yellow", "green", "blue", "indigo", "violet"],
     label_visibility="hidden",
 )
@@ -68,7 +78,7 @@ st.write("Value 6:", w6)
 
 
 w7 = st.select_slider(
-    "Label 7",
+    "Label 7 (collapsed visibility)",
     options=["red", "orange", "yellow", "green", "blue", "indigo", "violet"],
     label_visibility="collapsed",
 )
@@ -79,9 +89,10 @@ if runtime.exists():
 
     def on_change():
         st.session_state.select_slider_changed = True
+        st.write("Hello world")
 
     st.select_slider(
-        "Label 8",
+        "Label 8 (on change)",
         options=np.array([1, 2, 3, 4, 5]),
         key="select_slider8",
         on_change=on_change,
@@ -91,9 +102,35 @@ if runtime.exists():
 
 with st.expander("Expander", expanded=True):
     w9 = st.select_slider(
-        label="Label 9",
+        label="Label 9 (expander)",
         options=["foo", "bar", "baz", "This is a very, very long option"],
         value="This is a very, very long option",
     )
 
     st.write("Value 9:", w9)
+
+with st.form(key="my_form", clear_on_submit=True):
+    selection = st.select_slider(
+        label="Label 10 (form)",
+        options=np.array([1, 2, 3, 4, 5]),
+    )
+    st.form_submit_button("Submit")
+
+st.write("select_slider-in-form selection:", str(selection))
+
+
+@st.experimental_fragment()
+def test_fragment():
+    selection = st.select_slider(
+        label="Label 11 (fragment)",
+        options=np.array([1, 2, 3, 4, 5]),
+    )
+    st.write("select_slider-in-fragment selection:", str(selection))
+
+
+test_fragment()
+
+if "runs" not in st.session_state:
+    st.session_state.runs = 0
+st.session_state.runs += 1
+st.write("Runs:", st.session_state.runs)
