05996b451f74086c7d426423e2579d33d65a7416
Debbie Matthews <debbie.matthews@snowflake.com>
2024-05-21 23:14:51 -0700

    Typo in logo example; copy pasta

--- lib/streamlit/commands/logo.py
+++ lib/streamlit/commands/logo.py
@@ -99,7 +99,7 @@ def logo(
     >>>
     >>> options = [HORIZONTAL_RED, ICON_RED, HORIZONTAL_BLUE, ICON_BLUE]
     >>> sidebar_logo = st.selectbox("Sidebar logo", options, 0)
-    >>> main_body_logo = st.selectbox("Sidebar logo", options, 1)
+    >>> main_body_logo = st.selectbox("Main body logo", options, 1)
     >>>
     >>> st.logo(sidebar_logo, icon_image=main_body_logo)
 
