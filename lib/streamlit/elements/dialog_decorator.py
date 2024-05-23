2fc7ea13ed7f6043557454914a55c30b63608d32
braethlein <benjamin.raethlein@gmail.com>
2024-05-23 15:12:47 +0200

    Fix mypy error

--- lib/streamlit/elements/dialog_decorator.py
+++ lib/streamlit/elements/dialog_decorator.py
@@ -64,14 +64,16 @@ def _dialog_decorator(
         dialog = event_dg._dialog(title=title, dismissible=True, width=width)
         dialog.open()
 
-        @_fragment
         def dialog_content() -> None:
             # if the dialog should be closed, st.rerun() has to be called (same behavior as with st.fragment)
             _ = non_optional_func(*args, **kwargs)
             return None
 
+        # the fragment decorator has multiple return types so that you can pass arguments to it. Here we know the return type, so we cast
+        fragmented_dialog_content = cast(Callable[[], None], _fragment(dialog_content))
         with dialog:
-            return dialog_content()
+            fragmented_dialog_content()
+            return None
 
     return cast(F, wrap)
 
