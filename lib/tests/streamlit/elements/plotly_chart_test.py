4177f2f8df22b773df384a21f4183b9f90fae566
lukasmasuch <lukas.masuch@gmail.com>
2024-05-25 02:42:21 +0200

    Add more tests

--- lib/tests/streamlit/elements/plotly_chart_test.py
+++ lib/tests/streamlit/elements/plotly_chart_test.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from unittest.mock import MagicMock, Mock, patch
+from unittest.mock import MagicMock, patch
 
 import plotly.express as px
 from parameterized import parameterized
