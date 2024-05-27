e1f4495297027dd7d90d9c25990c69656aa882e8
lukasmasuch <lukas.masuch@gmail.com>
2024-05-27 21:22:09 +0200

    Try parameterized

--- e2e_playwright/st_form_container_association_test.py
+++ e2e_playwright/st_form_container_association_test.py
@@ -14,9 +14,9 @@
 
 import re
 
+from parameterized import parameterized
 from playwright.sync_api import Page
 
-from e2e_playwright.conftest import wait_for_app_run
 from e2e_playwright.shared.app_utils import (
     click_checkbox,
     click_form_button,
@@ -24,39 +24,55 @@ from e2e_playwright.shared.app_utils import (
 )
 
 
-def test_form_container_association(app: Page):
+@parameterized.expand(
+    [
+        ["form_0", True],
+        ["form_1", False],
+        ["form_2", False],
+        ["form_3", True],
+        ["form_4", True],
+        ["form_5", False],
+        ["form_6", True],
+        ["form_7", True],
+        ["form_8", True],
+        ["form_9", True],
+    ]
+)
+def test_form_container_association(
+    app: Page, form_key: str, is_checkbox_inside_form: bool
+):
     """Test association of elements to a form with different cases
     with elements created in the form container and outside.
     """
+    # Check the initial state of the checkbox to be False
+    expect_prefixed_markdown(app, f"{form_key} value:", "False", exact_match=True)
 
-    # Form id mapping whether a checkbox is inside a form
-    checkbox_inside_form = {
-        "form_0": True,
-        "form_1": False,
-        "form_2": False,
-        "form_3": True,
-        "form_4": True,
-        "form_5": False,
-        "form_6": True,
-        "form_7": True,
-        "form_8": True,
-        "form_9": True,
-    }
+    # Click on the checkbox
+    click_checkbox(app, re.compile(f"in {form_key}"))
 
-    for form_id, is_checkbox_inside_form in checkbox_inside_form.items():
-        # Check the initial state of the checkbox to be False
-        expect_prefixed_markdown(app, f"{form_id} value:", "False", exact_match=True)
+    # Check that only checkbox values have been updated that are outside a form
+    expect_prefixed_markdown(
+        app, f"{form_key} value:", str(not is_checkbox_inside_form), exact_match=True
+    )
 
-        # Click on the checkbox
-        click_checkbox(app, re.compile(f"in {form_id}"))
+    # Submit the form
+    click_form_button(app, f"{form_key} submit")
 
-        # Check that only checkbox values have been updated that are outside a form
-        expect_prefixed_markdown(
-            app, f"{form_id} value:", str(not is_checkbox_inside_form), exact_match=True
-        )
+    # Check the checkbox value has been updated to True for all cases
+    expect_prefixed_markdown(app, f"{form_key} value:", "True", exact_match=True)
 
-        # Submit the form
-        click_form_button(app, f"{form_id} submit")
+    # # Form id mapping whether a checkbox is inside a form
+    # checkbox_inside_form = {
+    #     "form_0": True,
+    #     "form_1": False,
+    #     "form_2": False,
+    #     "form_3": True,
+    #     "form_4": True,
+    #     "form_5": False,
+    #     "form_6": True,
+    #     "form_7": True,
+    #     "form_8": True,
+    #     "form_9": True,
+    # }
 
-        # Check the checkbox value has been updated to True for all cases
-        expect_prefixed_markdown(app, f"{form_id} value:", "True", exact_match=True)
+    # for form_id, is_checkbox_inside_form in checkbox_inside_form.items():
