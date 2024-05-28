a1580bc9b6553336af471a4e7f4ec92f99957cc4
lukasmasuch <lukas.masuch@gmail.com>
2024-05-28 16:31:49 +0200

    Add more tests

--- e2e_playwright/st_columns_test.py
+++ e2e_playwright/st_columns_test.py
@@ -17,7 +17,15 @@ import re
 from playwright.sync_api import Page, expect
 
 from e2e_playwright.conftest import ImageCompareFunction
-from e2e_playwright.shared.app_utils import click_button, expect_exception
+from e2e_playwright.shared.app_utils import click_button, expect_exception, get_expander
+
+
+def _get_basic_column_container(
+    app: Page,
+):
+    column_container = app.get_by_test_id("stHorizontalBlock").nth(0)
+    expect(column_container).to_be_visible()
+    return column_container
 
 
 def test_show_columns_horizontally_when_viewport_allows(
@@ -25,10 +33,10 @@ def test_show_columns_horizontally_when_viewport_allows(
 ):
     """shows columns horizontally when viewport > 640"""
     app.set_viewport_size({"width": 641, "height": 800})
-    horizontal_blocks = app.get_by_test_id("stHorizontalBlock")
-    expect(horizontal_blocks).to_have_count(2)
-
-    assert_snapshot(horizontal_blocks.nth(0), name="st_columns-layout_horizontal")
+    column_container = _get_basic_column_container(app)
+    assert_snapshot(
+        column_container.nth(0), name="st_columns-responsive_layout_horizontal"
+    )
 
 
 def test_show_columns_vertically_when_viewport_requires(
@@ -36,33 +44,14 @@ def test_show_columns_vertically_when_viewport_requires(
 ):
     """stacks columns vertically when viewport <= 640"""
     app.set_viewport_size({"width": 640, "height": 800})
-    horizontal_blocks = app.get_by_test_id("stHorizontalBlock")
-    expect(horizontal_blocks).to_have_count(2)
-
-    assert_snapshot(horizontal_blocks.nth(0), name="st_columns-layout_vertical")
-
-
-def test_columns_always_take_up_space(app: Page, assert_snapshot: ImageCompareFunction):
-    """columns still takes up space with no elements present"""
-    horizontal_blocks = app.get_by_test_id("stHorizontalBlock")
-    expect(horizontal_blocks).to_have_count(2)
-
-    assert_snapshot(horizontal_blocks.nth(1), name="st_columns-with_one_element")
-
-
-def test_shows_nested_columns_correctly(
-    app: Page, assert_snapshot: ImageCompareFunction
-):
-    assert_snapshot(
-        app.get_by_test_id("stVerticalBlock").nth(0),
-        name="st_columns-long_texts",
-    )
+    column_container = _get_basic_column_container(app)
+    assert_snapshot(column_container, name="st_columns-responsive_layout_vertical")
 
 
 def test_two_level_nested_columns_shows_exception(app: Page):
     """Shows exception when trying to nest columns more than one level deep."""
 
-    click_button(app, "Nested columns - two levels")
+    click_button(app, "Nested columns - two levels (raises exception)")
     expect_exception(
         app,
         re.compile(
@@ -74,10 +63,68 @@ def test_two_level_nested_columns_shows_exception(app: Page):
 def test_nested_columns_in_sidebar_shows_exception(app: Page):
     """Shows exception when trying to nest columns in the sidebar."""
 
-    click_button(app, "Nested columns - in sidebar")
+    click_button(app, "Nested columns - in sidebar (raises exception)")
     expect_exception(
         app,
         re.compile(
             "Columns cannot be placed inside other columns in the sidebar. This is only possible in the main area of the app."
         ),
     )
+
+
+def test_column_gap_small_is_correctly_applied(
+    app: Page, assert_snapshot: ImageCompareFunction
+):
+    """Test that the small column gap is correctly applied."""
+    column_gap_small = (
+        get_expander(app, "Column gap small").get_by_test_id("stHorizontalBlock").nth(0)
+    )
+    expect(column_gap_small).to_have_css("gap", "1rem")
+    column_gap_small.scroll_into_view_if_needed()
+    assert_snapshot(column_gap_small, name="st_columns-column_gap_small")
+
+
+def test_column_gap_medium_is_correctly_applied(
+    app: Page, assert_snapshot: ImageCompareFunction
+):
+    """Test that the medium column gap is correctly applied."""
+    column_gap_small = (
+        get_expander(app, "Column gap medium")
+        .get_by_test_id("stHorizontalBlock")
+        .nth(0)
+    )
+    expect(column_gap_small).to_have_css("gap", "2rem")
+    column_gap_small.scroll_into_view_if_needed()
+    assert_snapshot(column_gap_small, name="st_columns-column_gap_medium")
+
+
+def test_column_gap_large_is_correctly_applied(
+    app: Page, assert_snapshot: ImageCompareFunction
+):
+    """Test that the large column gap is correctly applied."""
+    column_gap_small = (
+        get_expander(app, "Column gap large").get_by_test_id("stHorizontalBlock").nth(0)
+    )
+    expect(column_gap_small).to_have_css("gap", "4rem")
+    column_gap_small.scroll_into_view_if_needed()
+    assert_snapshot(column_gap_small, name="st_columns-column_gap_large")
+
+
+# ------------------------------
+
+
+# def test_columns_always_take_up_space(app: Page, assert_snapshot: ImageCompareFunction):
+#     """columns still takes up space with no elements present"""
+#     horizontal_blocks = app.get_by_test_id("stHorizontalBlock")
+#     expect(horizontal_blocks).to_have_count(2)
+
+#     assert_snapshot(horizontal_blocks.nth(1), name="st_columns-with_one_element")
+
+
+# def test_shows_nested_columns_correctly(
+#     app: Page, assert_snapshot: ImageCompareFunction
+# ):
+#     assert_snapshot(
+#         app.get_by_test_id("stVerticalBlock").nth(0),
+#         name="st_columns-long_texts",
+#     )
