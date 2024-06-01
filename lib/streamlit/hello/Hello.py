83d26f4eb409b6638f0beaaf94301772033ca784
Ken McGrady <ken.mcgrady@gmail.com>
2024-05-31 17:41:21 -0700

    Re-add set_page_config for Hello

--- lib/streamlit/hello/Hello.py
+++ lib/streamlit/hello/Hello.py
@@ -14,6 +14,11 @@
 
 import streamlit as st
 
+st.set_page_config(
+    page_title="Hello",
+    page_icon="👋",
+)
+
 st.write("# Welcome to Streamlit! 👋")
 
 st.sidebar.success("Select a demo above.")
