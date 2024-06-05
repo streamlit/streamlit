c48a2c82adf63d97e9b40951e34f081708c3ee17
lukasmasuch <lukas.masuch@gmail.com>
2024-06-05 14:59:08 +0200

    Add additional command

--- lib/tests/streamlit/runtime/metrics_util_test.py
+++ lib/tests/streamlit/runtime/metrics_util_test.py
@@ -198,6 +198,7 @@ class PageTelemetryTest(DeltaGeneratorTestCase):
         def test_function(param1: int, param2: str, param3: float = 0.1) -> str:
             st.markdown("This command should not be tracked")
             st.text_input("This command should also not be tracked")
+            st.text("This command should also not be tracked")
             return "foo"
 
         test_function(param1=10, param2="foobar")
