db57a2465551d5327613bd9a657c539bfb5c3fee
Benjamin Räthlein <benjamin.raethlein@gmail.com>
2024-06-11 20:16:35 +0200

    Update custom-components import paths and tests (#8666)
    ## Describe your changes
    
    Closes https://github.com/streamlit/streamlit/issues/8644
    
    From [this
    comment](https://github.com/streamlit/streamlit/issues/8644#issuecomment-2107061961):
    The usage of `components.html` via `st.components.v1.html` was possible
    prior to `1.34` due to [this
    import](https://github.com/streamlit/streamlit/pull/8457/files#diff-3f73cf34a1595773b3b79ea85c5efd0c208e23beec4f812fa05419f87cff597eL32):
    `from streamlit.components.v1.components import ComponentRegistry`,
    which made it transiently available. This means that the usage via
    `st.components.v1.html` is only possible when `server.py` is imported at
    one point, which is not the case for example when running Streamlit as a
    bare-script.
    
    We have discussed this and thought about bringing the behavior back, but
    this time explicitly and with tests 🙂
    Final decision is still pending.
    
    ## Testing Plan
    
    - E2E Tests
    - Added new e2e test to test this import behavior. The tests are added
    as extra files in order to start with a fresh context and not
    accidentally have transient imports that make it appear as if it works.
    
    ---
    
    **Contribution License Agreement**
    
    By submitting this pull request you agree that all contributions to this
    project are made under the Apache 2.0 license.

--- e2e_playwright/shared/app_utils.py
+++ e2e_playwright/shared/app_utils.py
@@ -145,7 +145,7 @@ def get_markdown(locator: Locator, text_inside_markdown: str | Pattern[str]) -> 
 
 
 def expect_prefixed_markdown(
-    locator: Locator,
+    locator: Locator | Page,
     expected_prefix: str,
     expected_markdown: str | Pattern[str],
     exact_match: bool = False,
@@ -192,8 +192,29 @@ def expect_prefixed_markdown(
         expect(selection_text).to_contain_text(expected_markdown)
 
 
+def expect_markdown(
+    locator: Locator | Page,
+    expected_message: str | Pattern[str],
+) -> None:
+    """Expect an exception to be displayed in the app.
+
+    Parameters
+    ----------
+
+    locator : Locator
+        The locator to search for the exception element.
+
+    expected_markdown : str or Pattern[str]
+        The expected message to be displayed in the exception.
+    """
+    markdown_el = locator.get_by_test_id("stMarkdownContainer").filter(
+        has_text=expected_message
+    )
+    expect(markdown_el).to_be_visible()
+
+
 def expect_exception(
-    locator: Locator,
+    locator: Locator | Page,
     expected_message: str | Pattern[str],
 ) -> None:
     """Expect an exception to be displayed in the app.
