7a8c26c15881173d9d2d00dab3807bc2beacd366
Lukas Masuch <Lukas.Masuch@gmail.com>
2024-06-19 16:06:32 +0200

    Add support for numpy 2.x (#8940)
    ## Describe your changes
    
    Numpy 2.0 was released a couple of days ago. This PR adds support for
    numpy 2.x.
    
    Streamlit not supporting numpy 2.0 likely leads to the side-effect that
    Streamlit 1.19 gets installed by the resolver if Streamlit & numpy are
    unpinned. The reason:
    
    We added major version pins in Streamlit >= 1.23. This can lead to the
    interpretation (by the resolver) that Streamlit < 1.23 is compatible
    with numpy 2.0, but >= 1.23 isn't. So maybe the resolver selects Numpy
    2.0, and then resolves to a compatible Streamlit version which would be
    < 1.23. But the version that actually gets selected is 1.19 because we
    added a major version pin to altair <5 in 1.20. Thereby, the latest
    Streamlit version that is semantically compatible with Numpy 2.x and
    Altair 5.x is 1.19.
    
    ## GitHub Issue Link (if applicable)
    
    - https://github.com/streamlit/streamlit/issues/8927
    - https://github.com/streamlit/streamlit/issues/8917
    - https://github.com/streamlit/streamlit/issues/8935
    - https://github.com/streamlit/streamlit/issues/8936
    
    
    ## Testing Plan
    
    - Not logic changed -> no tests are required. 
    
    ---
    
    **Contribution License Agreement**
    
    By submitting this pull request you agree that all contributions to this
    project are made under the Apache 2.0 license.

--- lib/tests/streamlit/elements/bokeh_test.py
+++ lib/tests/streamlit/elements/bokeh_test.py
@@ -14,10 +14,11 @@
 
 """Bokeh unit test."""
 
+import numpy as np
+import unittest
 from unittest.mock import patch
 
-from bokeh.plotting import figure
-
+from streamlit.type_util import is_version_less_than
 import streamlit as st
 from streamlit.errors import StreamlitAPIException
 from tests.delta_generator_test_case import DeltaGeneratorTestCase
@@ -26,8 +27,16 @@ from tests.delta_generator_test_case import DeltaGeneratorTestCase
 class BokehTest(DeltaGeneratorTestCase):
     """Test ability to marshall bokeh_chart protos."""
 
+    @unittest.skipIf(
+        is_version_less_than(np.__version__, "2.0.0") is False,
+        "This test only runs if numpy is < 2.0.0. The bokeh version supported "
+        "by Streamlit is not compatible with numpy 2.x.",
+    )
     def test_figure(self):
         """Test that it can be called with figure."""
+
+        from bokeh.plotting import figure
+
         plot = figure()
         plot.line([1], [1])
         st.bokeh_chart(plot)
@@ -35,7 +44,14 @@ class BokehTest(DeltaGeneratorTestCase):
         c = self.get_delta_from_queue().new_element.bokeh_chart
         self.assertEqual(hasattr(c, "figure"), True)
 
+    @unittest.skipIf(
+        is_version_less_than(np.__version__, "2.0.0") is False,
+        "This test only runs if numpy is < 2.0.0. The bokeh version supported "
+        "by Streamlit is not compatible with numpy 2.x.",
+    )
     def test_bokeh_version_failure(self):
+        from bokeh.plotting import figure
+
         with patch("bokeh.__version__", return_value="2.4.0"):
             plot = figure()
             with self.assertRaises(StreamlitAPIException):
