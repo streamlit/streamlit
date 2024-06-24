584b083384f2cd2141f7ab01de504a0c05c33e96
lukasmasuch <lukas.masuch@gmail.com>
2024-06-24 16:01:02 +0200

    Cleaning up console logs at 2700m 🇫🇷🏔️

--- frontend/lib/src/dataframes/Quiver.ts
+++ frontend/lib/src/dataframes/Quiver.ts
@@ -438,17 +438,12 @@ export class Quiver {
 
   constructor(element: IArrow) {
     const table = tableFromIPC(element.data)
-    console.log(table)
     const pandasSchema = Quiver.parsePandasSchema(table)
 
     const rawColumns = Quiver.getRawColumns(table.schema, pandasSchema)
-    console.log(rawColumns)
     const fields = Quiver.parseFields(table.schema)
     const columns = Quiver.parseColumns(table.schema, pandasSchema)
-    console.log("columns", columns)
     const data = Quiver.parseData(table, columns, rawColumns)
-    console.log("data", data)
-
     const index: Index = pandasSchema
       ? Quiver.parseIndex(table, pandasSchema)
       : []
@@ -670,8 +665,6 @@ export class Quiver {
     arrowSchema: ArrowSchema,
     pandasSchema: PandasSchema | undefined
   ): Type[] {
-    console.log(arrowSchema)
-    console.log(pandasSchema)
     if (pandasSchema) {
       return (
         pandasSchema.columns
