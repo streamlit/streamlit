ef1756cf0b97dc9595180cbb8fe7bd6f0c7c96d5
braethlein <benjamin.raethlein@gmail.com>
2024-07-02 15:52:23 +0200

    Add additional e2e_tests

--- e2e_playwright/st_feedback.py
+++ e2e_playwright/st_feedback.py
@@ -12,15 +12,51 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import time
+
 import streamlit as st
 
 st.feedback()
-
-sentiment = st.feedback(
+st.feedback(
     "faces",
     key="faces_feedback",
     on_change=lambda: st.write(f"Faces sentiment: {st.session_state.faces_feedback}"),
 )
-
 sentiment = st.feedback("stars")
 st.write(f"Star sentiment: {sentiment}")
+
+
+sentiment = st.feedback("stars", disabled=True, key="disabled_feedback")
+st.write("feedback-disabled:", str(sentiment))
+
+with st.form(key="my_form", clear_on_submit=True):
+    sentiment = st.feedback()
+    st.form_submit_button("Submit")
+
+st.write("feedback-in-form:", str(sentiment))
+
+
+@st.experimental_fragment()
+def test_fragment():
+    sentiment = st.feedback(key="fragment_feedback")
+    st.write("feedback-in-fragment:", str(sentiment))
+
+
+test_fragment()
+
+
+if st.button("Create some elements to unmount component"):
+    for _ in range(3):
+        # The sleep here is needed, because it won't unmount the
+        # component if this is too fast.
+        time.sleep(1)
+        st.write("Another element")
+
+sentiment = st.feedback(key="after_sleep_feedback")
+st.write("feedback-after-sleep:", str(sentiment))
+
+
+if "runs" not in st.session_state:
+    st.session_state.runs = 0
+st.session_state.runs += 1
+st.write("Runs:", st.session_state.runs)
--- e2e_playwright/st_feedback_test.py
+++ e2e_playwright/st_feedback_test.py
@@ -12,22 +12,24 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import re
+
 from playwright.sync_api import Locator, Page, expect
 
 from e2e_playwright.conftest import ImageCompareFunction, wait_for_app_run
-from e2e_playwright.shared.app_utils import get_markdown
+from e2e_playwright.shared.app_utils import click_button, get_markdown
 
 
 def get_button_group(app: Page, index: int) -> Locator:
     return app.get_by_test_id("stButtonGroup").nth(index)
 
 
+def get_feedback_icon_buttons(locator: Locator, type: str) -> Locator:
+    return locator.get_by_test_id("baseButton-buttonGroup").filter(has_text=type)
+
+
 def get_feedback_icon_button(locator: Locator, type: str, index: int = 0) -> Locator:
-    return (
-        locator.get_by_test_id("baseButton-buttonGroup")
-        .filter(has_text=type)
-        .nth(index)
-    )
+    return get_feedback_icon_buttons(locator, type).nth(index)
 
 
 def test_click_thumbsup_and_take_snapshot(
@@ -59,3 +61,62 @@ def test_clicking_on_stars_shows_sentiment_and_take_snapshot(
     text = get_markdown(app, "Star sentiment: 4")
     expect(text).to_be_attached()
     assert_snapshot(stars, name="st_feedback-stars")
+
+
+def test_feedback_buttons_are_disabled(app: Page):
+    """Test that feedback buttons are disabled when `disabled=True` and that
+    they cannot be interacted with."""
+
+    stars = get_button_group(app, 3)
+    star_buttons = get_feedback_icon_buttons(stars, "star")
+    for star_button in star_buttons.all():
+        expect(star_button).to_have_js_property("disabled", True)
+    selected_button = star_buttons.nth(4)
+    selected_button.click(force=True)
+    expect(selected_button).not_to_have_css(
+        "background-color", re.compile("rgb\\(\\d+, \\d+, \\d+\\)")
+    )
+    text = get_markdown(app, "feedback-disabled: None")
+    expect(text).to_be_attached()
+
+
+def test_feedback_works_in_forms(app: Page):
+    expect(app.get_by_text("feedback-in-form: None")).to_be_visible()
+    thumbs = get_button_group(app, 4)
+    get_feedback_icon_button(thumbs, "thumb_up").click()
+    expect(app.get_by_text("feedback-in-form: None")).to_be_visible()
+    app.get_by_test_id("baseButton-secondaryFormSubmit").click()
+    wait_for_app_run(app)
+
+    text = get_markdown(app, "feedback-in-form: 1")
+    expect(text).to_be_attached()
+
+
+def test_feedback_works_with_fragments(app: Page):
+    expect(app.get_by_text("Runs: 1")).to_be_visible()
+    expect(app.get_by_text("feedback-in-fragment: None")).to_be_visible()
+    thumbs = get_button_group(app, 5)
+    get_feedback_icon_button(thumbs, "thumb_up").click()
+    wait_for_app_run(app)
+    expect(app.get_by_text("feedback-in-fragment: 1")).to_be_visible()
+    expect(app.get_by_text("Runs: 1")).to_be_visible()
+
+
+def test_feedback_remount_keep_value(app: Page):
+    """Test that `st.feedback` remounts correctly without resetting value."""
+    expect(app.get_by_text("feedback-after-sleep: None")).to_be_visible()
+
+    thumbs = get_button_group(app, 6)
+    selected_button = get_feedback_icon_button(thumbs, "thumb_up")
+    selected_button.click()
+    wait_for_app_run(app)
+    expect(app.get_by_text("feedback-after-sleep: 1")).to_be_visible()
+
+    expect(selected_button).to_have_css(
+        "background-color", re.compile("rgb\\(\\d+, \\d+, \\d+\\)")
+    )
+    click_button(app, "Create some elements to unmount component")
+    expect(selected_button).to_have_css(
+        "background-color", re.compile("rgb\\(\\d+, \\d+, \\d+\\)")
+    )
+    expect(app.get_by_text("feedback-after-sleep: 1")).to_be_visible()
