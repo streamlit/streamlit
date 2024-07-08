26556b799beb9dac3ab562148020b395ec3323c2
Benjamin Räthlein <benjamin.raethlein@gmail.com>
2024-07-08 19:57:45 +0200

    Update lib/streamlit/elements/layouts.py
    Co-authored-by: Debbie Matthews <debbie.matthews@snowflake.com>

--- lib/streamlit/elements/layouts.py
+++ lib/streamlit/elements/layouts.py
@@ -207,6 +207,7 @@ class LayoutsMixin:
         >>> with col2:
         ...     st.header("A dog")
         ...     st.image("https://static.streamlit.io/examples/dog.jpg")
+        >>>
         >>> with col3:
         ...     st.header("An owl")
         ...     st.image("https://static.streamlit.io/examples/owl.jpg")
