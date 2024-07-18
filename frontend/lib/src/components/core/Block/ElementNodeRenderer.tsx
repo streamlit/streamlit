b723f28fc1050f62948e6b461bbdca8cf93639a3
Nico Bellante <nico.bellante@snowflake.com>
2024-07-18 15:25:41 -0700

    clean it up a lil

--- frontend/lib/src/components/core/Block/ElementNodeRenderer.tsx
+++ frontend/lib/src/components/core/Block/ElementNodeRenderer.tsx
@@ -246,8 +246,6 @@ const RawElementNodeRenderer = (
 ): ReactElement => {
   const { node } = props
 
-  console.log({ props })
-
   if (!node) {
     throw new Error("ElementNode not found.")
   }
@@ -263,8 +261,6 @@ const RawElementNodeRenderer = (
     disabled: props.widgetsDisabled,
     fragmentId: node.fragmentId,
   }
-
-  console.log({ type: node.element.type })
 
   switch (node.element.type) {
     case "alert": {
@@ -507,7 +503,6 @@ const RawElementNodeRenderer = (
       )
 
     case "audioInput": {
-      console.log("hereeeeeeeeeeeeeee")
       const audioInputProto = node.element.audioInput as AudioInputProto
       return (
         <AudioInput key={audioInputProto.id} {...widgetProps}></AudioInput>
