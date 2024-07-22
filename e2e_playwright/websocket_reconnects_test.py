95d05e0c2214ce42882b95fd141e8f98cbf3ee14
braethlein <benjamin.raethlein@gmail.com>
2024-07-22 18:35:56 +0200

    Add test for file uploader and camera input

--- e2e_playwright/websocket_reconnects_test.py
+++ e2e_playwright/websocket_reconnects_test.py
@@ -15,7 +15,7 @@
 from typing import Final
 
 import pytest
-from playwright.sync_api import Error, Page, expect
+from playwright.sync_api import Error, FilePayload, Page, expect
 
 from e2e_playwright.shared.app_utils import click_button, expect_markdown
 
@@ -121,3 +121,64 @@ def test_dont_observe_invalid_status(
     with pytest.raises(Error) as e:
         _get_status(app, "Connecting2", DISCONNECT_WEBSOCKET_ACTION)
     assert "timeout: did not observe status 'Connecting2'" in e.value.message
+
+
+def test_retain_uploaded_files_when_websocket_connection_drops_and_reconnects(
+    app: Page,
+):
+    file_name1 = "file1.txt"
+    file_content1 = b"blob"
+    uploader_index = 0
+
+    app.get_by_test_id("stFileUploaderDropzoneInput").nth(
+        uploader_index
+    ).set_input_files(
+        [
+            FilePayload(name=file_name1, buffer=file_content1, mimeType="text/plain"),
+        ],
+    )
+
+    expect(app.get_by_test_id("stFileUploaderFileName")).to_have_text(file_name1)
+    expect(app.get_by_test_id("stText").nth(uploader_index)).to_have_text(
+        str(file_content1)
+    )
+
+    status = _get_status(app, "Connecting", DISCONNECT_WEBSOCKET_ACTION)
+    assert status == "Connecting"
+
+    # wait until re-connected
+    expect(app.get_by_test_id("stStatusWidget")).not_to_be_attached()
+
+    # Confirm that our uploaded file is still there.
+    expect(app.get_by_test_id("stText").nth(uploader_index)).to_have_text(
+        str(file_content1)
+    )
+
+
+def test_retain_captured_pictures_when_websocket_connection_drops_and_reconnects(
+    app: Page,
+):
+    camera_input_button = app.get_by_test_id("stCameraInputButton").nth(0)
+    expect(camera_input_button).to_be_visible()
+    expect(camera_input_button).to_contain_text("Take Photo")
+    camera_input_button.click()
+
+    app.wait_for_function("document.querySelectorAll('img').length >= 2")
+    app.wait_for_function(
+        "document.querySelectorAll('[data-testid=\"stImage\"]').length >= 1"
+    )
+
+    # Wait for the image to be displayed
+    expect(app.get_by_test_id("stImage")).to_be_visible()
+
+    # Disconnect
+    status = _get_status(app, "Connecting", DISCONNECT_WEBSOCKET_ACTION)
+    assert status == "Connecting"
+
+    # wait until re-connected
+    expect(app.get_by_test_id("stStatusWidget")).not_to_be_attached()
+
+    # Confirm that our picture is still there.
+    app.wait_for_function(
+        "document.querySelectorAll('[data-testid=\"stImage\"]').length >= 1"
+    )
