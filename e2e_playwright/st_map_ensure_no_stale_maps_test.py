3975d3812261705ff42b025d07815c17326f5656
Nico Bellante <nico.bellante@snowflake.com>
2024-07-23 16:30:03 -0700

    seeing if this will work in CI

---
+++ e2e_playwright/st_map_ensure_no_stale_maps_test.py
@@ -0,0 +1,43 @@
+# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022-2024)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from playwright.sync_api import Page, expect
+
+from e2e_playwright.conftest import wait_for_app_run
+
+
+def test_st_map_has_no_stale_elements(
+    themed_app: Page,
+):
+    maps = themed_app.get_by_test_id("stDeckGlJsonChart")
+    expect(maps).to_have_count(3)
+
+    wait_for_app_run(themed_app, 15000)
+
+    selectbox = themed_app.get_by_test_id("stSelectbox").first
+    selectbox.locator("input").first.click()
+    selection_dropdown = themed_app.locator('[data-baseweb="popover"]').first
+    selection_dropdown.locator("li").nth(1).click()
+
+    wait_for_app_run(themed_app, 15000)
+
+    expect(maps).to_have_count(3)
+
+    selectbox.locator("input").first.click()
+    selection_dropdown = themed_app.locator('[data-baseweb="popover"]').first
+    selection_dropdown.locator("li").nth(0).click()
+
+    wait_for_app_run(themed_app, 15000)
+
+    expect(maps).to_have_count(3)
