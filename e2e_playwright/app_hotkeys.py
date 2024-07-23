a2ca56027df143e5892403ebab326ed0465b2858
lukasmasuch <lukas.masuch@gmail.com>
2024-07-23 17:48:41 +0200

    Add sleep to app hotkeys script

--- e2e_playwright/app_hotkeys.py
+++ e2e_playwright/app_hotkeys.py
@@ -12,6 +12,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import time
+
 import streamlit as st
 
 st.text_input("text_input")
+
+# Add sleep to allow capturing the running state more reliably:
+time.sleep(1000)
