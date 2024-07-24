181372b3e40eb124f7139d25a814aca0a598d9ce
Nico Bellante <nico.bellante@snowflake.com>
2024-07-24 16:26:54 -0700

    more cleanup and actually working e2e

--- frontend/lib/src/components/widgets/AudioInput/AudioInput.tsx
+++ frontend/lib/src/components/widgets/AudioInput/AudioInput.tsx
@@ -17,23 +17,27 @@
 import React, { ReactElement } from "react"
 import { withTheme } from "@emotion/react"
 import { useReactMediaRecorder } from "react-media-recorder"
-import WavesurferPlayer from "@wavesurfer/react"
 import WaveSurfer from "wavesurfer.js"
 import BaseButton, {
   BaseButtonKind,
 } from "@streamlit/lib/src/components/shared/BaseButton"
-import { FileUploadClient } from "src/FileUploadClient"
-import { AudioInput as AudioInputProto } from "src/proto"
+import { FileUploadClient, WidgetStateManager } from "@streamlit/lib"
+import { AudioInput as AudioInputProto } from "@streamlit/lib/src/proto"
 import MediaStreamVisualizer from "./MediaStreamVisualizer"
+
+import WaveSurferVisualization from "./WaveSurferVisualization"
+import { uploadFiles } from "./uploadFiles"
 
 interface Props {
   element: AudioInputProto
   uploadClient: FileUploadClient
+  widgetMgr: WidgetStateManager
 }
 
 const AudioInput: React.FC<Props> = ({
   element,
   uploadClient,
+  widgetMgr,
 }): ReactElement => {
   const {
     status,
@@ -44,43 +48,31 @@ const AudioInput: React.FC<Props> = ({
     previewAudioStream,
   } = useReactMediaRecorder({ audio: true, video: false })
 
+  // WAVE SURFER SPECIFIC STUFF
   const [wavesurfer, setWavesurfer] = React.useState<WaveSurfer | null>(null)
+  const onPlayPause = () => {
+    wavesurfer && wavesurfer.playPause()
+  }
+
   const [isPlaying, setIsPlaying] = React.useState(false)
 
   const isRecording = status === "recording"
   const buttonDisabled = status !== "idle" && status !== "recording"
 
-  console.log({ isRecording, previewAudioStream })
-
-  const onReady = (ws: WaveSurfer) => {
-    setWavesurfer(ws)
-    setIsPlaying(false)
-  }
-
-  const onPlayPause = () => {
-    wavesurfer && wavesurfer.playPause()
-  }
-
   const onSubmit = async () => {
     if (!mediaBlobUrl) {
       return
     }
+
     const blob = await (await fetch(mediaBlobUrl)).blob()
     const file = new File([blob], "audio.wav", { type: blob.type })
 
-    const [fileUrl] = await uploadClient.fetchFileURLs([file])
-
-    if (!fileUrl.uploadUrl) {
-      console.error("No upload URL found")
-      return
-    }
-
-    uploadClient.uploadFile(
-      { id: "123", formId: "testtest" },
-      fileUrl.uploadUrl,
-      file,
-      e => console.log(e)
-    )
+    uploadFiles({
+      files: [file],
+      uploadClient,
+      widgetMgr,
+      widgetInfo: element,
+    }) // TODO error handling
   }
 
   return (
@@ -103,16 +95,11 @@ const AudioInput: React.FC<Props> = ({
               heightPx={120}
             />
           ) : (
-            <WavesurferPlayer
-              barWidth={2}
-              barGap={2}
-              height={120}
-              waveColor="red"
-              url={mediaBlobUrl}
-              onReady={onReady}
-              onPlay={() => setIsPlaying(true)}
-              onPause={() => setIsPlaying(false)}
-            />
+            <WaveSurferVisualization
+              setWavesurfer={setWavesurfer}
+              setIsPlaying={setIsPlaying}
+              mediaBlobUrl={mediaBlobUrl}
+            ></WaveSurferVisualization>
           )}
         </div>
         <p style={{ position: "absolute", top: 20, right: 20 }}>{status}</p>
