d12d104f0e277db2da717e414de694cf861f6687
wyattscarpenter <wyattscarpenter@gmail.com>
2024-07-25 20:19:14 -0700

    Update time_widgets.py: easily remove type-ignore

--- lib/streamlit/elements/widgets/time_widgets.py
+++ lib/streamlit/elements/widgets/time_widgets.py
@@ -521,7 +521,7 @@ class TimeWidgetsMixin:
     def date_input(
         self,
         label: str,
-        value: DateValue | Literal["today"] = "default_value_today",  # type: ignore[assignment]
+        value: DateValue | Literal["today"] | Literal["default_value_today"] = "default_value_today",
         min_value: SingleDateValue = None,
         max_value: SingleDateValue = None,
         key: Key | None = None,
