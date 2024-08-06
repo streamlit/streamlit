ab694e48d0b9d69d6306feb27a7374c0e37a2063
lukasmasuch <lukas.masuch@gmail.com>
2024-08-06 15:44:52 +0200

    Add test case

--- lib/tests/streamlit/dataframe_util_test.py
+++ lib/tests/streamlit/dataframe_util_test.py
@@ -375,6 +375,7 @@ class DataframeUtilTest(unittest.TestCase):
         assert dataframe_util.is_pandas_data_object(pd.Series()) is True
         assert dataframe_util.is_pandas_data_object(pd.Index(["a", "b"])) is True
         assert dataframe_util.is_pandas_data_object(pd.array(["a", "b"])) is True
+        assert dataframe_util.is_pandas_data_object(["a", "b"]) is False
 
     def test_is_snowpandas_data_object(self):
         df = pd.DataFrame([1, 2, 3])
