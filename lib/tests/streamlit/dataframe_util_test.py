4c29fa4c4ffea449258e6c077d410488d5e8708f
lukasmasuch <lukas.masuch@gmail.com>
2024-08-08 16:54:27 +0200

    Add dask integration test

--- lib/tests/streamlit/dataframe_util_test.py
+++ lib/tests/streamlit/dataframe_util_test.py
@@ -515,6 +515,31 @@ class DataframeUtilTest(unittest.TestCase):
                 pd.DataFrame,
             )
 
+    def test_verify_dask_integration(self):
+        dask = pytest.importorskip("dask")
+
+        dask_df = dask.datasets.timeseries()
+
+        assert dataframe_util.is_dask_object(dask_df) is True
+        assert isinstance(
+            dataframe_util.convert_anything_to_pandas_df(dask_df),
+            pd.DataFrame,
+        )
+
+        dask_series = dask_df["x"]
+        assert dataframe_util.is_dask_object(dask_series) is True
+        assert isinstance(
+            dataframe_util.convert_anything_to_pandas_df(dask_series),
+            pd.DataFrame,
+        )
+
+        dask_index = dask_df.index
+        assert dataframe_util.is_dask_object(dask_index) is True
+        assert isinstance(
+            dataframe_util.convert_anything_to_pandas_df(dask_index),
+            pd.DataFrame,
+        )
+
     @parameterized.expand(
         SHARED_TEST_CASES,
     )
