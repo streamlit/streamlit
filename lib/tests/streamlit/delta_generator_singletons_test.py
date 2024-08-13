656227d79b0a0bb2833973b0524fc9e47d97b896
raethlein <benjamin.raethlein@gmail.com>
2024-08-13 17:11:13 +0200

    Add initialization tests to dg_singleton_tests

--- lib/tests/streamlit/delta_generator_singletons_test.py
+++ lib/tests/streamlit/delta_generator_singletons_test.py
@@ -12,19 +12,25 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import unittest
+
 import streamlit as st
 from streamlit.delta_generator import DeltaGenerator
 from streamlit.delta_generator_singletons import (
+    bottom_dg,
     context_dg_stack,
+    create_dialog,
+    create_status_container,
+    event_dg,
     get_default_dg_stack,
     get_last_dg_added_to_context_stack,
     main_dg,
+    sidebar_dg,
 )
 from streamlit.proto.RootContainer_pb2 import RootContainer
-from tests.delta_generator_test_case import DeltaGeneratorTestCase
 
 
-class DeltaGeneratorSingletonsTest(DeltaGeneratorTestCase):
+class DeltaGeneratorSingletonsTest(unittest.TestCase):
     def test_get_last_dg_added_to_context_stack(self):
         last_dg_added_to_context_stack = get_last_dg_added_to_context_stack()
         self.assertIsNone(last_dg_added_to_context_stack)
@@ -36,14 +42,13 @@ class DeltaGeneratorSingletonsTest(DeltaGeneratorTestCase):
         last_dg_added_to_context_stack = get_last_dg_added_to_context_stack()
         self.assertNotEqual(sidebar, last_dg_added_to_context_stack)
 
-    def test_get_dg_stack_or_default(self):
+    def test_context_dg_stack(self):
         dg_stack = context_dg_stack.get()
         self.assertEqual(get_default_dg_stack(), dg_stack)
-        self.assertEqual(context_dg_stack.get(), dg_stack)
         self.assertEqual(len(dg_stack), 1)
 
         new_dg = DeltaGenerator(root_container=RootContainer.MAIN, parent=main_dg)
-        token = context_dg_stack.set(get_default_dg_stack() + (new_dg,))
+        token = context_dg_stack.set(context_dg_stack.get() + (new_dg,))
 
         # get the updated dg_stack for current context
         dg_stack = context_dg_stack.get()
@@ -51,3 +56,27 @@ class DeltaGeneratorSingletonsTest(DeltaGeneratorTestCase):
 
         # reset for the other tests
         context_dg_stack.reset(token)
+        dg_stack = context_dg_stack.get()
+        self.assertEqual(len(dg_stack), 1)
+
+
+class DeltaGeneratorSingletonsVariablesAreInitializedTest(unittest.TestCase):
+    """dg variables are initialized by Streamlit.__init__.py"""
+
+    def test_main_dg_is_initialized(self):
+        self.assertIsNotNone(main_dg)
+
+    def test_sidebar_dg_is_initialized(self):
+        self.assertIsNotNone(sidebar_dg)
+
+    def test_event_dg_is_initialized(self):
+        self.assertIsNotNone(event_dg)
+
+    def test_bottom_dg_is_initialized(self):
+        self.assertIsNotNone(bottom_dg)
+
+    def test_create_status_container_is_initialized(self):
+        self.assertIsNotNone(create_status_container)
+
+    def test_create_dialog_is_initialized(self):
+        self.assertIsNotNone(create_dialog)
