4d4267196af9916e3365162dbcca50efb1106d1d
lukasmasuch <lukas.masuch@gmail.com>
2024-08-20 02:28:54 +0200

    Revert test change

--- e2e_playwright/st_data_editor_index_types.py
+++ e2e_playwright/st_data_editor_index_types.py
@@ -12,119 +12,88 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import random
+
+import numpy as np
+
 import streamlit as st
+from shared.data_mocks import BASE_TYPES_DF, DATETIME_TYPES_DF, NUMBER_TYPES_DF
 
+np.random.seed(0)
+random.seed(0)
 
-class Message:
-    def __init__(self, type, content):
-        self.type = type
-        self.content = content
+st.set_page_config(layout="wide")
 
+st.header("Index types")
 
-class MessageHistory:
-    def __init__(self):
-        if "messages" not in st.session_state:
-            st.session_state["messages"] = []
+st.subheader("String Index (pd.Index)")
+st.data_editor(
+    BASE_TYPES_DF.set_index("string"), use_container_width=True, num_rows="dynamic"
+)
 
-    def add_user_message(self, content):
-        st.session_state["messages"].append(Message("user", content))
+st.subheader("Float64 Index (pd.Float64Index)")
+st.data_editor(
+    NUMBER_TYPES_DF.set_index("float64"), use_container_width=True, num_rows="dynamic"
+)
 
-    def add_ai_message(self, content):
-        st.session_state["messages"].append(Message("ai", content))
+st.subheader("Int64 Index (pd.Int64Index)")
+st.data_editor(
+    NUMBER_TYPES_DF.set_index("int64"), use_container_width=True, num_rows="dynamic"
+)
 
-    def add_chat_message(self, role, content):
-        st.session_state["messages"].append(Message(role, content))
+st.subheader("Uint64 Index (pd.UInt64Index)")
+st.data_editor(
+    NUMBER_TYPES_DF.set_index("uint64"), use_container_width=True, num_rows="dynamic"
+)
 
-    @property
-    def messages(self):
-        return st.session_state["messages"]
+st.subheader("Date Index (pd.Index)")
+st.data_editor(
+    DATETIME_TYPES_DF.set_index("date"), use_container_width=True, num_rows="dynamic"
+)
 
+st.subheader("Time Index (pd.Index)")
+st.data_editor(
+    DATETIME_TYPES_DF.set_index("time"), use_container_width=True, num_rows="dynamic"
+)
 
-msgs = MessageHistory()
+st.subheader("Datetime Index (pd.DatetimeIndex)")
+st.data_editor(
+    DATETIME_TYPES_DF.set_index("datetime"),
+    use_container_width=True,
+    num_rows="dynamic",
+)
 
-# Start streamlit app
-if len(msgs.messages) == 0:
-    intro_message = "Hello! How can I help you?"
-    msgs.add_ai_message(intro_message)
-
-for msg in msgs.messages:
-    with st.chat_message(msg.type):
-        st.write(msg.content)
-
-if user_input := st.chat_input("Ask a question:"):
-    st.chat_message("human").write(user_input.replace("$", r"\$"))
-    msgs.add_user_message(user_input)
-
-    with st.chat_message("ai"):
-        with st.spinner("Thinking..."):
-            import time
-
-            time.sleep(3)
-            response = {"answer": "I am a response from AI"}
-        st.write(response["answer"])
-        msgs.add_ai_message(response["answer"])
-# st.write("Hello, world!")
-# st.set_page_config(
-#     page_title="My Streamlit App",
-#     page_icon=":shark:",
-#     layout="wide",
-#     initial_sidebar_state="expanded",
+# Categorical index doesn't work since arrow
+# does serialize the options:
+# st.subheader("Categorical Index (pd.CategoricalIndex)")
+# st.data_editor(
+#     SPECIAL_TYPES_DF.set_index("categorical"),
+#     use_container_width=True,
+#     num_rows="dynamic",
 # )
 
-# with st.spinner(text="In progress..."):
-#     time.sleep(100)
+# List index isn't editable currently:
+# st.subheader("List Index (pd.Index)")
+# st.data_editor(
+#     LIST_TYPES_DF.set_index("string_list"), use_container_width=True, num_rows="dynamic"
+# )
 
-# # import geodatasets
-# # import geopandas
+# Interval type isn't editable currently:
+# st.subheader("Interval Index (pd.IntervalIndex)")
+# st.data_editor(INTERVAL_TYPES_DF.set_index("int64_both"), use_container_width=True)
 
-# # colombia = geopandas.read_file(geodatasets.get_path("geoda.malaria"))
-# # st.write(colombia)
-# # st.dataframe(colombia)
-# # st.data_editor(colombia)
+# Multi index is not yet supported for editing:
+# st.subheader("Multi Index (pd.MultiIndex)")
+# st.data_editor(
+#     BASE_TYPES_DF.set_index(["string", "int64"]),
+#     use_container_width=True,
+#     num_rows="dynamic",
+# )
 
-# # from datasets import load_dataset
-
-# # ds = load_dataset("rotten_tomatoes", split="validation")
-# # st.dataframe(ds)
-# # st.write(ds)
-# # st.write(str(type(ds)))
-# # return_val = st.data_editor(ds)
-# # st.write(str(type(return_val)))
-
-# # import ibis
-
-# # t = ibis.examples.penguins.fetch()
-# # st.write(t)
-# # st.dataframe(t)
-# # st.write(str(type(t)))
-
-# from pydantic import BaseModel, validator
-
-# import streamlit as st
-
-
-# class UserModel(BaseModel):
-#     name: str
-#     username: str
-#     password1: str
-#     password2: str
-
-#     @validator("name")
-#     def name_must_contain_space(cls, v):
-#         if " " not in v:
-#             raise ValueError("must contain a space")
-#         return v.title()
-# col1, col2, col3, col4, col5, col6 = st.columns(6)
-# with col1:
-#     st.button(":material/delete: Delete", use_container_width=True)
-# with col2:
-#     st.button(":material/edit: Edit", use_container_width=True)
-# with col3:
-#     st.button(":material/visibility: Show", use_container_width=True)
-
-# # generate random dataframe :
-# import numpy as np
-
-# df = pd.DataFrame(np.random.randn(50, 20), columns=[f"col{i}" for i in range(20)])
-
-# st.dataframe(df)
+# Timedelta Index are currently not supported for editing:
+# st.subheader("Timedelta Index (pd.TimedeltaIndex)")
+# st.data_editor(
+#     SPECIAL_TYPES_DF.set_index("timedelta"),
+#     use_container_width=True,
+#     num_rows="dynamic",
+# )
