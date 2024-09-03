1ef2841da22782912e61c6f56e6cc8148b6cd88f
raethlein <benjamin.raethlein@gmail.com>
2024-09-03 11:34:28 +0200

    Add comment

--- frontend/app/src/App.tsx
+++ frontend/app/src/App.tsx
@@ -1207,8 +1207,12 @@ export class App extends PureComponent<Props, State> {
           ForwardMsg.ScriptFinishedStatus.FINISHED_FRAGMENT_RUN_SUCCESSFULLY
       ) {
         // Clear any stale elements left over from the previous run.
-        // (We don't do this if our script had a compilation error and didn't
-        // finish successfully.)
+        // We only do that for completed runs, not for runs that were finished early
+        // due to reruns; this is to avoid flickering of elements where they disappear for
+        // a moment and then are readded by a new session. After the new session finished,
+        // leftover elements will be cleared after finished successfully.
+        // We also don't do this if our script had a compilation error and didn't
+        // finish successfully.
         this.setState(
           ({ scriptRunId, fragmentIdsThisRun }) => ({
             // Apply any pending elements that haven't been applied.
