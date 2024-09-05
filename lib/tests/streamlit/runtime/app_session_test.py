2351b54056ac7f1c3500076f258e04dac7b65e57
Ken McGrady <ken.mcgrady@gmail.com>
2024-09-05 12:43:57 -0700

    Update page script hash on script start if necessary (#9408)
    ## Describe your changes
    
    We use `client_state` in the app session in order to handle when a
    source file changes. We update the page script hash when a script
    finished event occurs.
    
    The bug this fixes is a scenario where the page script never finishes
    successfully (in this case, the page always calls `st.rerun()`). We can
    improve this flow by updating the page script hash on script start event
    (which we should know by now).
    
    ## GitHub Issue Link (if applicable)
    Closes #8975
    
    ## Testing Plan
    
    - Added Python unit test
    - Tested the scenario manually
    
    ---
    
    **Contribution License Agreement**
    
    By submitting this pull request you agree that all contributions to this
    project are made under the Apache 2.0 license.

--- lib/tests/streamlit/runtime/app_session_test.py
+++ lib/tests/streamlit/runtime/app_session_test.py
@@ -827,6 +827,27 @@ class AppSessionScriptEventTest(IsolatedAsyncioTestCase):
 
         session._clear_queue.assert_called_once()
 
+    async def test_updates_page_script_hash_in_client_state_on_script_start(self):
+        session = _create_test_session(asyncio.get_running_loop())
+        session._client_state.page_script_hash = "some_page_script_hash"
+
+        mock_scriptrunner = MagicMock(spec=ScriptRunner)
+        session._scriptrunner = mock_scriptrunner
+        session._clear_queue = MagicMock()
+
+        # Send a mock SCRIPT_STARTED event.
+        session._on_scriptrunner_event(
+            sender=mock_scriptrunner,
+            event=ScriptRunnerEvent.SCRIPT_STARTED,
+            page_script_hash="some_other_page_script_hash",
+            fragment_ids_this_run=None,
+        )
+
+        # Yield to let the AppSession's callbacks run.
+        await asyncio.sleep(0)
+
+        assert session._client_state.page_script_hash == "some_other_page_script_hash"
+
     async def test_events_handled_on_event_loop(self):
         """ScriptRunner events should be handled on the main thread only."""
         session = _create_test_session(asyncio.get_running_loop())
