71e34807b01b506f1a5351427d89692691ea2ee3
Nico Bellante <nico.bellante@snowflake.com>
2024-09-04 18:16:55 -0700

    ughhhhhhhhh

--- e2e_playwright/st_audio_input_test.py
+++ e2e_playwright/st_audio_input_test.py
@@ -26,7 +26,7 @@ def test_audio_input_renders(app: Page):
 
 def test_audio_input_basic_flow(app: Page):
     if app.context.browser in ["chromium", "firefox"]:
-        app.context.grant_permissions(["microphone"])
+        app.context.grant_permissions("microphone")
 
     app.wait_for_timeout(2000)
 
