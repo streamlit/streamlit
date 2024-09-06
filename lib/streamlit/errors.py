a6765c9edb4839c86351ee7c42b3dc1f80e2b204
Nico Bellante <nico.bellante@snowflake.com>
2024-09-06 11:34:44 -0700

    fix python typing error

--- lib/streamlit/errors.py
+++ lib/streamlit/errors.py
@@ -383,12 +383,10 @@ class StreamlitPageNotFoundError(LocalizableStreamlitException):
     def __init__(self, page: str, main_script_directory: str, is_mpa_v2: bool):
         directory = os.path.basename(main_script_directory)
         message: str = (
-            (
-                "Could not find page: `{page}`. You must provide a `StreamlitPage` "
-                "object or file path relative to the entrypoint file. Only pages "
-                "previously defined by `st.Page` and passed to `st.navigation` are "
-                "allowed."
-            )
+            "Could not find page: `{page}`. You must provide a `StreamlitPage` "
+            "object or file path relative to the entrypoint file. Only pages "
+            "previously defined by `st.Page` and passed to `st.navigation` are "
+            "allowed."
             if is_mpa_v2
             else (
                 "Could not find page: `{page}`. You must provide a file path "
