9d6fff69e3c37ebb9814159b2b47e125d19a665c
lukasmasuch <lukas.masuch@gmail.com>
2024-09-09 12:09:46 +0200

    Fix test

--- e2e_playwright/st_altair_chart_basic_select.py
+++ e2e_playwright/st_altair_chart_basic_select.py
@@ -24,10 +24,12 @@ import streamlit as st
 # SCATTER CHART
 st.header("Altair Chart with point and interval selection")
 
+
 # taken from vega_datasets cars example
-@st.cache_data
+@st.cache_data  # use caching to avoid a potential issue with flakiness
 def get_cars_data():
     return data.cars()
+
 
 cars = get_cars_data()
 interval = alt.selection_interval()
