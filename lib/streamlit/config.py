3e1bab0205869b5d26b38d0a0359a15b46bdd2d5
Debbie Matthews <debbie.matthews@snowflake.com>
2024-09-14 14:38:58 -0700

    Update Python docs link to version 3
    Co-authored-by: Karen Javadyan <kajarenc@gmail.com>

--- lib/streamlit/config.py
+++ lib/streamlit/config.py
@@ -398,7 +398,7 @@ def _logger_message_format() -> str:
     """String format for logging messages. If logger.datetimeFormat is set,
     logger messages will default to `%(asctime)s.%(msecs)03d %(message)s`. See
     Python's documentation for available attributes:
-    https://docs.python.org/2.6/library/logging.html#formatter-objects
+    https://docs.python.org/3/library/logging.html#formatter-objects
 
     Default: "%(asctime)s %(message)s"
     """
