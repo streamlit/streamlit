82c2cbd3bc6b787e76f0985c57bf5edecc52b9de
Eric Chiu <e.chiu@snowflake.com>
2024-09-17 19:14:30 -0700

    playwright test

--- e2e_playwright/st_segments_test.py
+++ e2e_playwright/st_segments_test.py
@@ -17,7 +17,15 @@ import re
 from playwright.sync_api import Locator, Page, expect
 
 from e2e_playwright.conftest import ImageCompareFunction, wait_for_app_run
-from e2e_playwright.shared.app_utils import click_checkbox, get_markdown
+from e2e_playwright.shared.app_utils import (
+    check_top_level_class,
+    click_button,
+    click_checkbox,
+    click_form_button,
+    expect_markdown,
+    get_element_by_key,
+    get_markdown,
+)
 
 
 def get_button_group(app: Page, index: int) -> Locator:
@@ -102,7 +110,6 @@ def test_click_single_icon_segment_and_take_snapshot(
     Click on same element to test unselect.
     Click on two different elements to validate single select.
     """
-    pass
     segments = get_button_group(themed_app, 2)
 
     # the icon's span element has the respective text
@@ -146,3 +153,63 @@ def test_pass_default_selections(app: Page):
     wait_for_app_run(app)
     text = get_markdown(app, "Multi selection: None")
     expect(text).to_be_visible()
+
+
+def test_selection_via_on_change_callback(app: Page):
+    """Test that the on_change callback is triggered when a segment butoon is clicked."""
+    segments = get_button_group(app, 3)
+    get_segment_button(segments, "Sadness").click()
+    wait_for_app_run(app)
+    expect_markdown(app, "on_change selection: ['Sadness']")
+
+
+def test_segments_are_disabled(app: Page):
+    segments = get_button_group(app, 4)
+    for segment in segments.locator("button").all():
+        expect(segment).to_have_js_property("disabled", True)
+    selected_segment = get_segment_button(segments, "Sadness")
+    selected_segment.click(force=True)
+    wait_for_app_run(app)
+    expect(selected_segment).not_to_have_css(
+        "color", re.compile("rgb\\(\\d+, \\d+, \\d+\\)")
+    )
+    expect_markdown(app, "segments-disabled: None")
+
+
+def test_segments_work_in_forms(app: Page):
+    expect_markdown(app, "segments-in-form: []")
+    segments = get_button_group(app, 5)
+    get_segment_button(segments, "Sadness").click()
+    click_form_button(app, "Submit")
+    wait_for_app_run(app)
+    expect_markdown(app, "segments-in-form: ['Sadness']")
+
+
+def test_segments_work_with_fragments(app: Page):
+    expect_markdown(app, "segments-in-fragment: None")
+    segments = get_button_group(app, 6)
+    get_segment_button(segments, "Sadness").click()
+    wait_for_app_run(app)
+    expect_markdown(app, "segments-in-fragment: Sadness")
+    expect(app.get_by_text("Runs: 1")).to_be_visible()
+
+
+def test_segments_remount_keep_value(app: Page):
+    expect_markdown(app, "segments-after-sleep: None")
+    segments = get_button_group(app, 7)
+    selected_segment = get_segment_button(segments, "Sadness")
+    selected_segment.click()
+    wait_for_app_run(app)
+    expect_markdown(app, "segments-after-sleep: Sadness")
+    click_button(app, "Create some elements to unmount component")
+    expect_markdown(app, "segments-after-sleep: Sadness")
+
+
+def test_check_top_level_class(app: Page):
+    """Check that the top level class is correctly set."""
+    check_top_level_class(app, "stButtonGroup")
+
+
+def test_custom_css_class_via_key(app: Page):
+    """Test that the element can have a custom css class via the key argument."""
+    expect(get_element_by_key(app, "segments")).to_be_visible()
