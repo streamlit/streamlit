d1e018fb28e7ad1256ddbc5a6185091f26860502
Nico Bellante <nico.bellante@snowflake.com>
2024-09-19 15:10:48 -0700

    adding comment to useWidgetManagerElementState

--- frontend/lib/src/hooks/useWidgetManagerElementState.tsx
+++ frontend/lib/src/hooks/useWidgetManagerElementState.tsx
@@ -22,6 +22,15 @@ import {
   notNullOrUndefined,
 } from "@streamlit/lib/src/util/utils"
 
+/**
+ * Think of useState, but the state is also persisted in the widget manager.
+ * This allows you to have the state be persisted between mounting and unmounting of the component.
+ * @param widgetMgr - The widget manager instance to use
+ * @param id - The id of the widget to store the state for
+ * @param key - The key of the state to store
+ * @param defaultValue - The default value to use if the state is not set in the widget manager
+ * @returns A tuple containing the current state and a function to set the state
+ */
 const useWidgetManagerElementState = <T,>({
   widgetMgr,
   id,
