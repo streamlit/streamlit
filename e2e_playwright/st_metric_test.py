243a04d6f6177682e00512dbada09b39d761d717
Maya Barnes <maya8617@gmail.com>
2024-09-24 14:24:53 -0700

    Whoops...Comment back in old tests

--- e2e_playwright/st_metric_test.py
+++ e2e_playwright/st_metric_test.py
@@ -14,96 +14,97 @@
 from playwright.sync_api import Page, expect
 
 from e2e_playwright.conftest import ImageCompareFunction
-from e2e_playwright.shared.app_utils import check_top_level_class
-
-# def test_first_metric_in_first_row(app: Page):
-#     expect(app.get_by_test_id("stMetricLabel").nth(0)).to_have_text("User growth")
-#     expect(app.get_by_test_id("stMetricValue").nth(0)).to_have_text(" 123 ")
-#     expect(app.get_by_test_id("stMetricDelta").nth(0)).to_have_text(" 123 ")
+from e2e_playwright.shared.app_utils import check_top_level_class, expect_help_tooltip
 
 
-# def test_second_metric_in_first_row(app: Page):
-#     expect(app.get_by_test_id("stMetricLabel").nth(2)).to_have_text("S&P 500")
-#     expect(app.get_by_test_id("stMetricValue").nth(2)).to_have_text(" -4.56 ")
-#     expect(app.get_by_test_id("stMetricDelta").nth(2)).to_have_text(" -50 ")
+def test_first_metric_in_first_row(app: Page):
+    expect(app.get_by_test_id("stMetricLabel").nth(0)).to_have_text("User growth")
+    expect(app.get_by_test_id("stMetricValue").nth(0)).to_have_text(" 123 ")
+    expect(app.get_by_test_id("stMetricDelta").nth(0)).to_have_text(" 123 ")
 
 
-# def test_third_metric_in_first_row(app: Page):
-#     expect(app.get_by_test_id("stMetricLabel").nth(4)).to_have_text("Apples I've eaten")
-#     expect(app.get_by_test_id("stMetricValue").nth(4)).to_have_text(" 23k ")
-#     expect(app.get_by_test_id("stMetricDelta").nth(4)).to_have_text(" -20 ")
+def test_second_metric_in_first_row(app: Page):
+    expect(app.get_by_test_id("stMetricLabel").nth(2)).to_have_text("S&P 500")
+    expect(app.get_by_test_id("stMetricValue").nth(2)).to_have_text(" -4.56 ")
+    expect(app.get_by_test_id("stMetricDelta").nth(2)).to_have_text(" -50 ")
 
 
-# def test_green_up_arrow_render(themed_app: Page, assert_snapshot: ImageCompareFunction):
-#     assert_snapshot(
-#         themed_app.get_by_test_id("stMetric").nth(0),
-#         name="st_metric-green",
-#     )
+def test_third_metric_in_first_row(app: Page):
+    expect(app.get_by_test_id("stMetricLabel").nth(4)).to_have_text("Apples I've eaten")
+    expect(app.get_by_test_id("stMetricValue").nth(4)).to_have_text(" 23k ")
+    expect(app.get_by_test_id("stMetricDelta").nth(4)).to_have_text(" -20 ")
 
 
-# def test_red_down_arrow_render(themed_app: Page, assert_snapshot: ImageCompareFunction):
-#     assert_snapshot(
-#         themed_app.get_by_test_id("stMetric").nth(2),
-#         name="st_metric-red",
-#     )
+def test_green_up_arrow_render(themed_app: Page, assert_snapshot: ImageCompareFunction):
+    assert_snapshot(
+        themed_app.get_by_test_id("stMetric").nth(0),
+        name="st_metric-green",
+    )
 
 
-# def test_gray_down_arrow_render(
-#     themed_app: Page, assert_snapshot: ImageCompareFunction
-# ):
-#     assert_snapshot(
-#         themed_app.get_by_test_id("stMetric").nth(4),
-#         name="st_metric-gray",
-#     )
+def test_red_down_arrow_render(themed_app: Page, assert_snapshot: ImageCompareFunction):
+    assert_snapshot(
+        themed_app.get_by_test_id("stMetric").nth(2),
+        name="st_metric-red",
+    )
 
 
-# def test_help_shows_up_without_columns(
-#     themed_app: Page, assert_snapshot: ImageCompareFunction
-# ):
-#     assert_snapshot(
-#         themed_app.get_by_test_id("stMetric").nth(6),
-#         name="st_metric-with_help",
-#     )
+def test_gray_down_arrow_render(
+    themed_app: Page, assert_snapshot: ImageCompareFunction
+):
+    assert_snapshot(
+        themed_app.get_by_test_id("stMetric").nth(4),
+        name="st_metric-gray",
+    )
 
 
-# def test_none_results_in_dash_in_value(
-#     themed_app: Page, assert_snapshot: ImageCompareFunction
-# ):
-#     assert_snapshot(
-#         themed_app.get_by_test_id("stMetric").nth(7),
-#         name="st_metric-with_none_value",
-#     )
+def test_help_shows_up_without_columns(
+    themed_app: Page, assert_snapshot: ImageCompareFunction
+):
+    assert_snapshot(
+        themed_app.get_by_test_id("stMetric").nth(6),
+        name="st_metric-with_help",
+    )
 
 
-# def test_label_visibility_set_to_hidden(
-#     themed_app: Page, assert_snapshot: ImageCompareFunction
-# ):
-#     expect(themed_app.get_by_test_id("stMetricLabel").nth(3)).to_have_text("Test 4")
-#     assert_snapshot(
-#         themed_app.get_by_test_id("stMetric").nth(3),
-#         name="st_metric-label_hidden",
-#     )
+def test_none_results_in_dash_in_value(
+    themed_app: Page, assert_snapshot: ImageCompareFunction
+):
+    assert_snapshot(
+        themed_app.get_by_test_id("stMetric").nth(7),
+        name="st_metric-with_none_value",
+    )
 
 
-# def test_label_visibility_set_to_collapse(
-#     themed_app: Page, assert_snapshot: ImageCompareFunction
-# ):
-#     expect(themed_app.get_by_test_id("stMetricLabel").nth(5)).to_have_text("Test 5")
-#     assert_snapshot(
-#         themed_app.get_by_test_id("stMetric").nth(5),
-#         name="st_metric-label_collapse",
-#     )
+def test_label_visibility_set_to_hidden(
+    themed_app: Page, assert_snapshot: ImageCompareFunction
+):
+    expect(themed_app.get_by_test_id("stMetricLabel").nth(3)).to_have_text("Test 4")
+    assert_snapshot(
+        themed_app.get_by_test_id("stMetric").nth(3),
+        name="st_metric-label_hidden",
+    )
 
 
-# def test_ellipses_and_help_shows_up_properly(
-#     themed_app: Page, assert_snapshot: ImageCompareFunction
-# ):
-#     metric_element = themed_app.get_by_test_id("stMetric").nth(8)
-#     expect_help_tooltip(themed_app, metric_element, "Something should feel right")
-#     assert_snapshot(
-#         metric_element,
-#         name="st_metric-help_and_ellipses",
-#     )
+def test_label_visibility_set_to_collapse(
+    themed_app: Page, assert_snapshot: ImageCompareFunction
+):
+    expect(themed_app.get_by_test_id("stMetricLabel").nth(5)).to_have_text("Test 5")
+    assert_snapshot(
+        themed_app.get_by_test_id("stMetric").nth(5),
+        name="st_metric-label_collapse",
+    )
+
+
+def test_ellipses_and_help_shows_up_properly(
+    themed_app: Page, assert_snapshot: ImageCompareFunction
+):
+    metric_element = themed_app.get_by_test_id("stMetric").nth(8)
+    expect_help_tooltip(themed_app, metric_element, "Something should feel right")
+    assert_snapshot(
+        metric_element,
+        name="st_metric-help_and_ellipses",
+    )
 
 
 def test_code_in_help_shows_up_properly(
