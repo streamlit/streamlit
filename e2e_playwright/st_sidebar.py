c762e9a8a7240d11400a8f2ab088ca61f76cfedd
Maya Barnes <63436329+mayagbarnes@users.noreply.github.com>
2024-09-27 11:32:52 -0700

    [Fix] Tooltip visibility in `st.sidebar` & `st.dialog` (#9554)
    Chart tooltips are rendered under the sidebar / dialog modal - update CSS to resolve.

--- e2e_playwright/st_sidebar.py
+++ e2e_playwright/st_sidebar.py
@@ -14,7 +14,13 @@
 
 from datetime import date, datetime
 
+import numpy as np
+import pandas as pd
+
 import streamlit as st
+
+np.random.seed(0)
+data = np.random.randint(low=0, high=20, size=(20, 3))
 
 w1 = st.sidebar.date_input("Label 1", date(1970, 1, 1))
 st.write("Value 1:", w1)
@@ -29,3 +35,4 @@ y = st.sidebar.text_input("type here")
 with st.sidebar:
     st.header("hello world")
     st.markdown("hello world")
+    st.bar_chart(pd.DataFrame(data, columns=["a", "b", "c"]))
