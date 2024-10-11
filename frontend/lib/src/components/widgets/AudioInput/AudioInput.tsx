7e61e634da9edbc05e28e7012e0d5b5b92420a50
Nico Bellante <nico.bellante@snowflake.com>
2024-10-11 12:44:39 -0700

    adding download button to audio_input (#9637)
    ## Describe your changes
    
    This PR adds a download button to the toolbar of the `st.audio_input`. 
    
    ## Testing Plan
    - E2E Tests
    - I have added a line to the tests to ensure that the download button is
    rendered in the toolbar when expected. I could also add further testing
    to try to download the file, but I figured that was not worth the
    effort. Happy to hear other opinions here though!
    - Any manual testing needed?
    - I manually tested the flow and verified a working wav file was
    produced.
    
    ---
    
    **Contribution License Agreement**
    
    By submitting this pull request you agree that all contributions to this
    project are made under the Apache 2.0 license.

--- frontend/lib/src/components/widgets/AudioInput/AudioInput.tsx
+++ frontend/lib/src/components/widgets/AudioInput/AudioInput.tsx
@@ -25,7 +25,7 @@ import React, {
 import { useTheme } from "@emotion/react"
 import WaveSurfer from "wavesurfer.js"
 import RecordPlugin from "wavesurfer.js/dist/plugins/record"
-import { Delete } from "@emotion-icons/material-outlined"
+import { Delete, Download } from "@emotion-icons/material-outlined"
 import isEqual from "lodash/isEqual"
 
 import { FormClearHelper } from "@streamlit/lib/src/components/widgets/Form"
@@ -48,6 +48,7 @@ import { Placement } from "@streamlit/lib/src/components/shared/Tooltip"
 import { WidgetLabel } from "@streamlit/lib/src/components/widgets/BaseWidget"
 import { usePrevious } from "@streamlit/lib/src/util/Hooks"
 import useWidgetManagerElementState from "@streamlit/lib/src/hooks/useWidgetManagerElementState"
+import useDownloadUrl from "@streamlit/lib/src/hooks/useDownloadUrl"
 
 import {
   StyledAudioInputContainerDiv,
@@ -400,6 +401,8 @@ const AudioInput: React.FC<Props> = ({
     })
   }, [recordPlugin, wavesurfer, theme])
 
+  const downloadRecording = useDownloadUrl(recordingUrl, "recording.wav")
+
   const isRecording = Boolean(recordPlugin?.isRecording())
   const isPlaying = Boolean(wavesurfer?.isPlaying())
 
@@ -435,12 +438,18 @@ const AudioInput: React.FC<Props> = ({
           disableFullscreenMode={true}
           target={StyledWaveformContainerDiv}
         >
+          {recordingUrl && (
+            <ToolbarAction
+              label="Download recording"
+              icon={Download}
+              onClick={() => downloadRecording()}
+            />
+          )}
           {deleteFileUrl && (
             <ToolbarAction
               label="Clear recording"
               icon={Delete}
               onClick={() => handleClear({ updateWidgetManager: true })}
-              data-testid="stAudioInputClearRecordingButton"
             />
           )}
         </Toolbar>
