1f8fe5072969770e499f5932545fe78b8bf9e21b
raethlein <benjamin.raethlein@gmail.com>
2024-10-16 20:36:34 +0200

    Update demo

--- e2e_playwright/st_dataframe_chunking.py
+++ e2e_playwright/st_dataframe_chunking.py
@@ -14,40 +14,40 @@
 
 from __future__ import annotations
 
-import random
-import time
-
-import numpy as np
-import pandas as pd
+from typing import TYPE_CHECKING
 
 import streamlit as st
 
-np.random.seed(0)
-random.seed(0)
+if TYPE_CHECKING:
+    import pandas as pd
+
+
+# np.random.seed(0)
+# random.seed(0)
 
 # Generate a random dataframe
-df = pd.DataFrame(
-    np.random.randn(5000, 5),
-    columns=("col_%d" % i for i in range(5)),
-)
+# df = pd.DataFrame(
+#     np.random.randn(5000, 5),
+#     columns=("col_%d" % i for i in range(5)),
+# )
 
 
-def get_data():
-    chunk_size = 500
-    total_rows = df.shape[0]
+# def get_data():
+#     chunk_size = 500
+#     total_rows = df.shape[0]
 
-    def get_chunk(chunk_index: int) -> pd.DataFrame:
-        time.sleep(0.5)
-        # Get a chunk of data from the database
-        return df.iloc[chunk_index * chunk_size : (chunk_index + 1) * chunk_size]
+#     def get_chunk(chunk_index: int) -> pd.DataFrame:
+#         time.sleep(0.5)
+#         # Get a chunk of data from the database
+#         return df.iloc[chunk_index * chunk_size : (chunk_index + 1) * chunk_size]
 
-    return total_rows, get_chunk
-
-
-st.dataframe(get_data)
+#     return total_rows, get_chunk
 
 
-st.header("From Snowflake")
+# st.dataframe(get_data)
+
+
+st.header("Snowflake Table")
 
 # Initialize connection.
 conn = st.connection("snowflake")
@@ -55,34 +55,50 @@ conn = st.connection("snowflake")
 # Perform query.
 # df = conn.query("SELECT * from streamlit.streamlit.menu_clicks;", ttl=600)
 
+enable_chunking = st.toggle("Enable lazy loading", False)
 
-def get_snowflake_data():
-    chunk_size = 500
+if enable_chunking:
+
+    def get_snowflake_data():
+        chunk_size = 500
+        df = conn.query(
+            "SELECT ROW_COUNT FROM streamlit.information_schema.tables where table_name = 'MENU_CLICKS' and table_schema = 'APPS';"
+        )
+        # st.dataframe(df)
+        total_rows = int(df["ROW_COUNT"].values[0])
+        st.caption(f"Scroll to se all {total_rows} rows.")
+        print(total_rows)
+        # cur = conn.cursor().execute("SELECT * from streamlit.apps.menu_clicks;")
+        data_context = {"reached_end": False, "cursor": None}
+
+        def get_chunk(
+            chunk_index: int,
+        ) -> pd.DataFrame | list[tuple] | list[dict] | None:
+            if data_context["reached_end"]:
+                return None
+            # if (chunk_index * chunk_size) + chunk_size >= total_rows:
+            #     data_context["reached_end"] = True
+            # use limit and offset
+            return conn.query(
+                f"SELECT * from streamlit.apps.menu_clicks limit {chunk_size} offset {chunk_index * chunk_size};"
+            )
+            # _cur = data_context["cursor"]
+            # res = _cur.fetchmany(chunk_size)
+            # if len(res) == 0:
+            #     data_context["reached_end"] = True
+            # return res
+
+        return total_rows, get_chunk
+
+    st.dataframe(get_snowflake_data, hide_index=True)
+else:
+    limit = 200000
     df = conn.query(
         "SELECT ROW_COUNT FROM streamlit.information_schema.tables where table_name = 'MENU_CLICKS' and table_schema = 'APPS';"
     )
-    st.dataframe(df)
     total_rows = int(df["ROW_COUNT"].values[0])
-    print(total_rows)
-    # cur = conn.cursor().execute("SELECT * from streamlit.apps.menu_clicks;")
-    data_context = {"reached_end": False, "cursor": None}
-
-    def get_chunk(chunk_index: int) -> pd.DataFrame | list[tuple] | list[dict] | None:
-        if data_context["reached_end"]:
-            return None
-        # if (chunk_index * chunk_size) + chunk_size >= total_rows:
-        #     data_context["reached_end"] = True
-        # use limit and offset
-        return conn.query(
-            f"SELECT * from streamlit.apps.menu_clicks limit {chunk_size} offset {chunk_index * chunk_size};"
-        )
-        # _cur = data_context["cursor"]
-        # res = _cur.fetchmany(chunk_size)
-        # if len(res) == 0:
-        #     data_context["reached_end"] = True
-        # return res
-
-    return total_rows, get_chunk
-
-
-st.dataframe(get_snowflake_data, hide_index=True)
+    df = conn.query(f"SELECT * from streamlit.apps.menu_clicks limit {limit};")
+    st.caption(
+        f"⚠️ Showing {limit} out of {total_rows} " "rows due to data size limitations."
+    )
+    st.dataframe(df, hide_index=False)
