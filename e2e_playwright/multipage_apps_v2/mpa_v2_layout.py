d211348a748c8706f024b64d8fb26658ef472899
Eric Chiu <e.chiu@snowflake.com>
2024-10-23 18:09:13 -0700

    add new test

--- e2e_playwright/multipage_apps_v2/mpa_v2_layout.py
+++ e2e_playwright/multipage_apps_v2/mpa_v2_layout.py
@@ -15,6 +15,13 @@
 import streamlit as st
 
 
+@st.fragment(run_every="0.25s")
+def rerun():
+    count = 0
+    for i in range(100):
+        count += i
+
+
 def a():
     st.header("Page A: default")
 
@@ -35,11 +42,19 @@ def d():
     st.button("centered button", on_click=lambda: st.set_page_config(layout="centered"))
 
 
+def e():
+    st.header("Page E: fragment")
+    st.button("wide button", on_click=lambda: st.set_page_config(layout="wide"))
+    st.button("centered button", on_click=lambda: st.set_page_config(layout="centered"))
+    rerun()
+
+
 st.navigation(
     [
         st.Page(a, title="Page A: default"),
         st.Page(b, title="Page B: wide"),
         st.Page(c, title="Page C: centered"),
         st.Page(d, title="Page D: dynamic"),
+        st.Page(e, title="Page E: fragment"),
     ]
 ).run()
