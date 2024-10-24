21a05453dd066a49ca4eb87b85f481486a03175f
Johannes Rieke <johannes.rieke@snowflake.com>
2024-10-23 23:38:56 -0600

    Fix tests

--- lib/tests/streamlit/commands/page_config_test.py
+++ lib/tests/streamlit/commands/page_config_test.py
@@ -65,7 +65,7 @@ class PageConfigTest(DeltaGeneratorTestCase):
     def test_set_page_config_icon_calls_image_to_url(self, icon: PageIcon):
         """For all other page_config icon inputs, we just call image_to_url."""
         with mock.patch(
-            "streamlit.commands.page_config.image.image_to_url",
+            "streamlit.commands.page_config.image_to_url",
             return_value="https://mock.url",
         ):
             st.set_page_config(page_icon=icon)
