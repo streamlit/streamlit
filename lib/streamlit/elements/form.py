6b6b610f3496aea179ed4fdbf278a6dd6d74cc66
Debbie Matthews <debbie.matthews@snowflake.com>
2025-06-16 13:57:04 -0700

    Remove "stretch" height

--- lib/streamlit/elements/form.py
+++ lib/streamlit/elements/form.py
@@ -144,15 +144,11 @@ class FormMixin:
               the parent container, the width of the container matches the width
               of the parent container.
 
-        height : "content", "stretch", or int
+        height : "content" or int
             The height of the form container. This can be one of the following:
 
             - ``"content"`` (default): The height of the container matches the
               height of its content.
-            - ``"stretch"``: The height of the container matches the height of
-              its content or the height of the parent container, whichever is
-              larger. If the container is not in a parent container, the height
-              of the container matches the height of its content.
             - An integer specifying the height in pixels: The container has a
               fixed height. If the content is larger than the specified
               height, scrolling is enabled.
