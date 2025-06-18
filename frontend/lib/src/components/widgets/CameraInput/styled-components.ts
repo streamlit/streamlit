6303c605ebccf27b8f5e2c3e83690f7bbbae9938
Johannes Rieke <johannes.rieke@gmail.com>
2025-06-19 01:44:38 +0200

    Change camera permission link style

--- frontend/lib/src/components/widgets/CameraInput/styled-components.ts
+++ frontend/lib/src/components/widgets/CameraInput/styled-components.ts
@@ -100,9 +100,9 @@ export const StyledImg = styled.img<StyledImgProps>(({ theme, opacity }) => ({
 }))
 
 export const StyledLink = styled.a(({ theme }) => ({
-  color: theme.colors.primary,
+  color: theme.colors.link,
+  textDecoration: "underline",
   display: "block",
-  textDecoration: "none",
 }))
 
 export const StyledSpan = styled.span({
