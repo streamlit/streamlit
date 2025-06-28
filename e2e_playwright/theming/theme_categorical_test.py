0745997a2433697fd265f3df363685027f84f0c6
Maya Barnes <maya8617@gmail.com>
2025-06-28 14:37:01 -0700

    Add reset hovering

--- e2e_playwright/theming/theme_categorical_test.py
+++ e2e_playwright/theming/theme_categorical_test.py
@@ -20,7 +20,7 @@ import pytest
 from playwright.sync_api import Page
 
 from e2e_playwright.conftest import ImageCompareFunction
-from e2e_playwright.shared.app_utils import expect_no_skeletons
+from e2e_playwright.shared.app_utils import expect_no_skeletons, reset_hovering
 
 
 @pytest.fixture(scope="module")
@@ -51,6 +51,8 @@ def test_custom_theme_font_weights(app: Page, assert_snapshot: ImageCompareFunct
     app.set_viewport_size({"width": 1280, "height": 1000})
     # Make sure that all elements are rendered and no skeletons are shown:
     expect_no_skeletons(app, timeout=25000)
+    # Reset hovering to avoid flakiness from plotly toolbar
+    reset_hovering(app)
     # Add some additional timeout to ensure that fonts can load without
     # creating flakiness:
     app.wait_for_timeout(10000)
