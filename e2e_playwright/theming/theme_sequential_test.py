f1195b26fc58f69f780d9325f093e69d1d46ab46
Maya Barnes <maya8617@gmail.com>
2025-06-30 11:41:08 -0700

    Update viewport for test

--- e2e_playwright/theming/theme_sequential_test.py
+++ e2e_playwright/theming/theme_sequential_test.py
@@ -46,6 +46,8 @@ def configure_custom_sequential_colors():
 
 @pytest.mark.usefixtures("configure_custom_sequential_colors")
 def test_custom_theme_font_weights(app: Page, assert_snapshot: ImageCompareFunction):
+    # Set bigger viewport to better show the charts
+    app.set_viewport_size({"width": 1280, "height": 1000})
     # Make sure that all elements are rendered and no skeletons are shown:
     expect_no_skeletons(app, timeout=25000)
     # Reset hovering to avoid flakiness from plotly toolbar
