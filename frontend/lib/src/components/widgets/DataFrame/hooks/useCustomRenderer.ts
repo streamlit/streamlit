942ea91aaf388f2128ac1af35da0195c03700c03
Lukas Masuch <Lukas.Masuch@gmail.com>
2025-07-02 11:17:37 +0200

    Update frontend/lib/src/components/widgets/DataFrame/hooks/useCustomRenderer.ts
    Co-authored-by: Laura Wilby <laura.wilby@snowflake.com>

--- frontend/lib/src/components/widgets/DataFrame/hooks/useCustomRenderer.ts
+++ frontend/lib/src/components/widgets/DataFrame/hooks/useCustomRenderer.ts
@@ -53,7 +53,7 @@ export function drawAttentionIndicator(
   ctx.save()
   ctx.beginPath()
   // We are first moving the drawing position under the top right corner
-  // number pixels equal to theme.cellHorizontalPadding
+  // by the number of pixels equal to theme.cellHorizontalPadding
   // from left side (this is the size triangle)
   // and 1 pixel from top side (to be under the cell border).
   ctx.moveTo(rect.x + rect.width - theme.cellHorizontalPadding, rect.y + 1)
