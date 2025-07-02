eea1dde4a04e40a6aa2eff0b06a6edcaa65e3b8f
Lukas Masuch <Lukas.Masuch@gmail.com>
2025-07-02 11:18:30 +0200

    Update frontend/lib/src/components/widgets/DataFrame/hooks/useCustomRenderer.ts
    Co-authored-by: Laura Wilby <laura.wilby@snowflake.com>

--- frontend/lib/src/components/widgets/DataFrame/hooks/useCustomRenderer.ts
+++ frontend/lib/src/components/widgets/DataFrame/hooks/useCustomRenderer.ts
@@ -60,7 +60,7 @@ export function drawAttentionIndicator(
   // We draw the first line to the top right corner.
   ctx.lineTo(rect.x + rect.width, rect.y + 1)
   // We draw the second line with the number of pixels equal to theme.cellHorizontalPadding
-  // down on the right cell border
+  // down the right cell border
   ctx.lineTo(rect.x + rect.width, rect.y + 1 + theme.cellHorizontalPadding)
   // And now its enough to just fill it with a color to get a triangle.
   ctx.fillStyle = theme.accentColor
