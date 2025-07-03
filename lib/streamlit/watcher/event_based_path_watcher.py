e53db3f7cd9bab14c01528a7146d38d3f5993d9c
lukasmasuch <lukas.masuch@gmail.com>
2025-07-03 03:15:12 +0200

    Update

--- lib/streamlit/watcher/event_based_path_watcher.py
+++ lib/streamlit/watcher/event_based_path_watcher.py
@@ -228,9 +228,13 @@ class _MultiPathWatcher:
     def close(self) -> None:
         with self._lock:
             """Close this _MultiPathWatcher object forever."""
-            # TODO: Change:
-            if len(self._folder_handlers) != 0:
-                self._folder_handlers = {}
+            if self._folder_handlers:
+                _LOGGER.debug(
+                    "Stopping observer thread even though there is a non-zero "
+                    "number of event observers!"
+                )
+                self._observer.unschedule_all()
+                self._folder_handlers.clear()
 
             _LOGGER.debug("Stopping observer thread")
             self._observer.stop()
