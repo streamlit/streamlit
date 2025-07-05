ca2fa7108e649fdbb1d110692d8111c95ed60b7f
Laura Wilby <laura.wilby@snowflake.com>
2025-07-05 10:59:52 +0200

    remove sidebar fun dependency

--- frontend/lib/src/components/core/Block/StyledElementContainerLayoutWrapper.tsx
+++ frontend/lib/src/components/core/Block/StyledElementContainerLayoutWrapper.tsx
@@ -67,7 +67,7 @@ export const StyledElementContainerLayoutWrapper: FC<
     }
 
     return {}
-  }, [node.element.type, isInSidebar, node.element.heightConfig?.useStretch])
+  }, [node.element.type, node.element.heightConfig?.useStretch])
 
   const styles = useLayoutStyles({
     element: node.element,
