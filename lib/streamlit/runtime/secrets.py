b1a0f70e00aa07281cf2ab813d40e57a67d90288
lukasmasuch <lukas.masuch@gmail.com>
2025-07-05 14:02:29 +0200

    Improve file watcher exception handling

--- lib/streamlit/runtime/secrets.py
+++ lib/streamlit/runtime/secrets.py
@@ -429,14 +429,9 @@ class Secrets(Mapping[str, Any]):
                             self._on_secrets_changed,
                             watcher_type="poll",
                         )
-                except Exception:  # noqa: PERF203
-                    _LOGGER.debug(
-                        "Failed to install file watcher for secret file path %s",
-                        path,
-                        exc_info=True,
-                    )
+                except Exception:  # noqa: PERF203, S110
                     # A user may only have one secrets.toml file defined, so we'd expect
-                    # exceptions to be raised when attempting to install a
+                    # exceptions to be raised here when attempting to install a
                     # watcher on the nonexistent ones.
                     pass
 
