4c945da5c41fd5453f95bdd8f7fec96f545edfbf
Johannes Rieke <johannes.rieke@gmail.com>
2025-07-17 16:48:31 +0200

    Format

--- frontend/lib/src/components/shared/BaseButton/styled-components.ts
+++ frontend/lib/src/components/shared/BaseButton/styled-components.ts
@@ -124,27 +124,27 @@ export const StyledBaseButton = styled.button<RequiredBaseButtonProps>(
   }
 )
 
-export const StyledPrimaryButton = styled(StyledBaseButton)<RequiredBaseButtonProps>(
-  ({ theme }) => ({
+export const StyledPrimaryButton = styled(
+  StyledBaseButton
+)<RequiredBaseButtonProps>(({ theme }) => ({
+  backgroundColor: theme.colors.primary,
+  color: theme.colors.white,
+  border: `${theme.sizes.borderWidth} solid ${theme.colors.primary}`,
+  "&:hover": {
+    backgroundColor: darken(theme.colors.primary, 0.15),
+    borderColor: darken(theme.colors.primary, 0.15),
+  },
+  "&:active": {
     backgroundColor: theme.colors.primary,
-    color: theme.colors.white,
-    border: `${theme.sizes.borderWidth} solid ${theme.colors.primary}`,
-    "&:hover": {
-      backgroundColor: darken(theme.colors.primary, 0.15),
-      borderColor: darken(theme.colors.primary, 0.15),
-    },
-    "&:active": {
-      backgroundColor: theme.colors.primary,
-      borderColor: darken(theme.colors.primary, 0.15),
-    },
-    "&:disabled, &:disabled:hover, &:disabled:active": {
-      borderColor: theme.colors.borderColor,
-      backgroundColor: theme.colors.transparent,
-      color: theme.colors.fadedText40,
-      cursor: "not-allowed",
-    },
-  })
-)
+    borderColor: darken(theme.colors.primary, 0.15),
+  },
+  "&:disabled, &:disabled:hover, &:disabled:active": {
+    borderColor: theme.colors.borderColor,
+    backgroundColor: theme.colors.transparent,
+    color: theme.colors.fadedText40,
+    cursor: "not-allowed",
+  },
+}))
 
 export const StyledSecondaryButton = styled(
   StyledBaseButton
