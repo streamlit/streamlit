7cea80903b0b231fed209ce3324a5636389afc95
lukasmasuch <lukas.masuch@gmail.com>
2025-07-23 20:13:51 +0200

    Use rounded corners

--- frontend/lib/src/components/elements/Metric/Metric.tsx
+++ frontend/lib/src/components/elements/Metric/Metric.tsx
@@ -22,7 +22,6 @@ import { expressionInterpreter } from "vega-interpreter"
 import embed from "vega-embed"
 import { Global } from "@emotion/react"
 import { TopLevelSpec } from "vega-lite"
-import { transparentize } from "color2k"
 
 import { Metric as MetricProto } from "@streamlit/protobuf"
 import { convertRemToPx, EmotionTheme, useEmotionTheme } from "@streamlit/lib"
@@ -77,12 +76,16 @@ function getSparklineSpec(
     layer: [
       {
         name: `${baseName}_line`,
-        mark: "line",
         // mark: {
         //   type: "area",
         //   opacity: 0.2,
         //   line: { color: getMetricColor(theme, metricColor) },
         // },
+        mark: {
+          type: "line",
+          strokeCap: "round",
+          strokeWidth: 2,
+        },
         encoding: {
           x: {
             field: "x",
