a7bc9c86dac41c03f17f9b7ed1bdc6d8fff31170
lukasmasuch <lukas.masuch@gmail.com>
2025-07-29 19:57:47 +0200

    Update

--- frontend/lib/src/components/widgets/DataFrame/DataFrame.tsx
+++ frontend/lib/src/components/widgets/DataFrame/DataFrame.tsx
@@ -685,20 +685,6 @@ function DataFrame({
                 // eslint-disable-next-line streamlit-custom/no-force-reflow-access -- Existing usage
                 resizableContainerRef.current.clientWidth
             )
-
-            // eslint-disable-next-line no-console
-            console.log(
-              scrollAreaBounds.width,
-              // eslint-disable-next-line streamlit-custom/no-force-reflow-access
-              resizableContainerRef.current.clientWidth,
-              // eslint-disable-next-line streamlit-custom/no-force-reflow-access
-              resizableContainerRef.current.offsetWidth,
-              scrollAreaBounds.height,
-              // eslint-disable-next-line streamlit-custom/no-force-reflow-access
-              resizableContainerRef.current.clientHeight,
-              // eslint-disable-next-line streamlit-custom/no-force-reflow-access
-              resizableContainerRef.current.offsetHeight
-            )
           }
         }
       }, 0)
@@ -717,9 +703,6 @@ function DataFrame({
       setShowColumnVisibilityMenu(false)
     }
   }, [allColumns.length, columns.length])
-
-  // eslint-disable-next-line no-console
-  console.log(hasHorizontalScroll, hasVerticalScroll, scrollbarGutterSize)
 
   return (
     <StyledResizableContainer
