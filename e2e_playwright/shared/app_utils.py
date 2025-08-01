0f3fdac4950878f78e1a953f60701388288b765e
Lukas Masuch <Lukas.Masuch@gmail.com>
2025-08-01 22:05:26 +0200

    Refactor text input e2e test (#12097)
    ## Describe your changes
    
    Refactoring the text input e2e test to use label & key based access
    instead of index based access to get elements.
    
    
    ---
    
    **Contribution License Agreement**
    
    By submitting this pull request you agree that all contributions to this
    project are made under the Apache 2.0 license.

--- e2e_playwright/shared/app_utils.py
+++ e2e_playwright/shared/app_utils.py
@@ -27,6 +27,27 @@ from e2e_playwright.conftest import wait_for_app_loaded, wait_for_app_run
 COMMAND_KEY = "Meta" if platform.system() == "Darwin" else "Control"  # ty: ignore[unresolved-attribute]
 
 
+def get_text_input(locator: Locator | Page, label: str | Pattern[str]) -> Locator:
+    """Get a text input with the given label.
+
+    Parameters
+    ----------
+    locator : Locator
+        The locator to search for the element.
+
+    label : str or Pattern[str]
+        The label of the element to get.
+
+    Returns
+    -------
+    Locator
+        The element.
+    """
+    element = locator.get_by_test_id("stTextInput").filter(has_text=label)
+    expect(element).to_be_visible()
+    return element
+
+
 def get_checkbox(locator: Locator | Page, label: str | Pattern[str]) -> Locator:
     """Get a checkbox widget with the given label.
 
