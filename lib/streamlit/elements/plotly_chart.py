d63c186fbe03c3684108883c18da98a2f3541aae
Debbie Matthews <debbie.matthews@snowflake.com>
2025-08-05 14:22:11 -0700

    Continuation lines

--- lib/streamlit/elements/plotly_chart.py
+++ lib/streamlit/elements/plotly_chart.py
@@ -442,10 +442,10 @@ class PlotlyMixin:
         >>> from numpy.random import default_rng as rng
         >>>
         >>> hist_data = [
-        >>>     rng(0).standard_normal(200) - 2,
-        >>>     rng(1).standard_normal(200),
-        >>>     rng(2).standard_normal(200) + 2,
-        >>> ]
+        ...     rng(0).standard_normal(200) - 2,
+        ...     rng(1).standard_normal(200),
+        ...     rng(2).standard_normal(200) + 2,
+        ... ]
         >>> group_labels = ["Group 1", "Group 2", "Group 3"]
         >>>
         >>> fig = ff.create_distplot(
