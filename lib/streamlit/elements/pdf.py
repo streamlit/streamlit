707cc86d17db3193d1400702280aa435fc287fc3
Siddhant Mohile <siddhant.mohile@snowflake.com>
2025-08-08 15:50:06 -0700

    Fix type error

--- lib/streamlit/elements/pdf.py
+++ lib/streamlit/elements/pdf.py
@@ -42,7 +42,7 @@ def _get_pdf_component() -> Any | None:
         None otherwise.
     """
     try:
-        import streamlit_pdf  # type: ignore[import-not-found]
+        import streamlit_pdf  # type: ignore
 
         return streamlit_pdf.pdf_viewer
     except ImportError:
