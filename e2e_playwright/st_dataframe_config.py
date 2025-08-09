cbdaa0026763a7ad9ff6bd5b195d1fdca66a20fc
lukasmasuch <lukas.masuch@gmail.com>
2025-08-09 02:46:54 +0200

    Clean up

--- e2e_playwright/st_dataframe_config.py
+++ e2e_playwright/st_dataframe_config.py
@@ -437,12 +437,6 @@ st.dataframe(
                 [
                     "Foo Foo Foo Foo Foo Foo Foo Foo Foo Foo Foo Foo Foo Foo Foo Foo Foo Foo Foo Foo Foo Foo",
                     "Bar Bar Bar Bar Bar Bar Bar Bar Bar Bar Bar Bar Bar Bar Bar Bar Bar Bar Bar Bar Bar Bar",
-                    "Baz Baz Baz Baz Baz Baz Baz Baz Baz Baz Baz Baz Baz Baz Baz Baz Baz Baz Baz Baz Baz Baz",
-                    "Qux Qux Qux Qux Qux Qux Qux Qux Qux Qux Qux Qux Qux Qux Qux Qux Qux Qux Qux Qux Qux",
-                    "Quux Quux Quux Quux Quux Quux Quux Quux Quux Quux Quux Quux Quux Quux Quux Quux Quux Quux Quux",
-                    "Quuz Quuz Quuz Quuz Quuz Quuz Quuz Quuz Quuz Quuz Quuz Quuz Quuz Quuz Quuz Quuz Quuz Quuz Quuz",
-                    "Corge Corge Corge Corge Corge Corge Corge Corge Corge Corge Corge Corge Corge Corge Corge Corge Corge Corge Corge",
-                    "Grault Grault Grault Grault Grault Grault Grault Grault Grault Grault Grault Grault Grault Grault Grault Grault Grault Grault Grault",
                 ],
                 [],
                 [],
