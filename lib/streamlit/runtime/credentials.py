262b6d0521966787bc553e663214c6a3310a3416
wyattscarpenter <wyattscarpenter@gmail.com>
2025-08-13 18:47:28 -0400

    [nit] noqa: TRY301 should not be necessary in this position
    TRY301 triggering when you raise a different exception type was a bug in TRY301 that has now been fixed https://github.com/astral-sh/ruff/issues/5246

--- lib/streamlit/runtime/credentials.py
+++ lib/streamlit/runtime/credentials.py
@@ -151,7 +151,7 @@ class Credentials:
             with open(self._conf_file) as f:
                 data = toml.load(f).get("general")
             if data is None:
-                raise RuntimeError  # noqa: TRY301
+                raise RuntimeError
             self.activation = _verify_email(data.get("email"))
         except FileNotFoundError:
             if auto_resolve:
