2fe87c045f0fff89f855c1f53d15b24aef8db5a8
lukasmasuch <lukas.masuch@gmail.com>
2025-08-13 12:51:51 +0200

    Add marker

--- lib/tests/streamlit/elements/pdf_test.py
+++ lib/tests/streamlit/elements/pdf_test.py
@@ -66,6 +66,7 @@ class PdfComponentAvailabilityTest(DeltaGeneratorTestCase):
             assert "pip install streamlit[pdf]" in str(exc_info.value)
 
 
+@pytest.mark.require_integration
 class PdfTest(DeltaGeneratorTestCase):
     """Test ability to marshall PDF protos."""
 
