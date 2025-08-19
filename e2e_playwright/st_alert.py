cd9e9f19ce77de19a49e3a3f50a16b8d274f60b8
Lukas Masuch <Lukas.Masuch@gmail.com>
2025-08-19 17:51:40 +0300

    Update material symbols (#12264)
    ## Describe your changes
    
    The material icons update pipeline was broken for some time in
    https://github.com/google/material-design-icons.
    This PR updates the icon font to the latest version
    
    Existing number of icon names: 3901
    New number of icon names:  4094
    
    ## Testing Plan
    
    - Update e2e test.
    
    ---
    
    **Contribution License Agreement**
    
    By submitting this pull request you agree that all contributions to this
    project are made under the Apache 2.0 license.

--- e2e_playwright/st_alert.py
+++ e2e_playwright/st_alert.py
@@ -96,7 +96,7 @@ This is a big error message.
 ### LATEST MATERIAL ICON TEST START ###
 st.success(
     "Success message to test material icon from latest material symbols font",
-    icon=":material/computer_arrow_up:",
+    icon=":material/mobile_dots:",
 )
 ### LATEST MATERIAL ICON TEST END ###
 
