097edbda0d18c5813106c2c9c052c4c8b84edb8c
lukasmasuch <lukas.masuch@gmail.com>
2025-08-29 21:51:35 +0200

    Refactor the `st.color_picker` e2e test

--- e2e_playwright/shared/app_utils.py
+++ e2e_playwright/shared/app_utils.py
@@ -48,6 +48,27 @@ def get_time_input(locator: Locator | Page, label: str | Pattern[str]) -> Locato
     return element
 
 
+def get_color_picker(locator: Locator | Page, label: str | Pattern[str]) -> Locator:
+    """Get a color picker with the given label.
+
+    Parameters
+    ----------
+    locator : Locator | Page
+        The locator to search for the element.
+
+    label : str or Pattern[str]
+        The label of the element to get.
+
+    Returns
+    -------
+    Locator
+        The element.
+    """
+    element = locator.get_by_test_id("stColorPicker").filter(has_text=label)
+    expect(element).to_be_visible()
+    return element
+
+
 def get_text_input(locator: Locator | Page, label: str | Pattern[str]) -> Locator:
     """Get a text input with the given label.
 
