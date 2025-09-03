c748867313de8f5923f22a819726fa7dd0c992a0
Nico Bellante <nico.bellante@snowflake.com>
2025-09-03 14:43:00 -0700

    adding comment back

--- frontend/lib/src/components/widgets/AudioInput/convertAudioToWav.ts
+++ frontend/lib/src/components/widgets/AudioInput/convertAudioToWav.ts
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+// code in this file was adapted from recorder.js library
 
 import { getLogger } from "loglevel"
 
