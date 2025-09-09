985af1fd341a39b0355ec2555474b715c2634373
Nico Bellante <nico.bellante@snowflake.com>
2025-09-09 13:43:10 -0400

    [chore] Adding latest material icons (#12473)
    ## Describe your changes
    
    Updating the latest material icons
    
    ---
    
    **Contribution License Agreement**
    
    By submitting this pull request you agree that all contributions to this
    project are made under the Apache 2.0 license.

--- e2e_playwright/st_alert.py
+++ e2e_playwright/st_alert.py
@@ -96,7 +96,7 @@ This is a big error message.
 ### LATEST MATERIAL ICON TEST START ###
 st.success(
     "Success message to test material icon from latest material symbols font",
-    icon=":material/mobile_dots:",
+    icon=":material/chess_knight:",
 )
 ### LATEST MATERIAL ICON TEST END ###
 
