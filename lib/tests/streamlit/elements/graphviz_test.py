e347906c38fbf26b853c3337438b0802a2739b4f
Laura Wilby <laura.wilby@snowflake.com>
2025-09-17 13:16:36 +0200

    Apply suggested changes
    Apply suggested changes

--- lib/tests/streamlit/elements/graphviz_test.py
+++ lib/tests/streamlit/elements/graphviz_test.py
@@ -12,8 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Graphviz unit test."""
-
 from __future__ import annotations
 
 import graphviz
