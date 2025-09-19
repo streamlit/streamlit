6c9c79afb2cb17e63c7c6a9288c1733dc9970311
Laura Wilby <laura.wilby@snowflake.com>
2025-09-19 13:37:04 +0200

    add comment explaining need for tolerance variable

--- e2e_playwright/st_vega_charts_dimensions_test.py
+++ e2e_playwright/st_vega_charts_dimensions_test.py
@@ -63,7 +63,9 @@ def test_vega_chart_width_behavior(themed_app: Page):
         expect(chart).to_be_visible()
 
         chart_description = f"Chart {i}: {descriptions[i]}"
-        assert_vega_chart_width(chart, expected_width, chart_description, 3)
+        # Note: tolerance is included below to account for some small pixel differences that can
+        # occur when the width is calculated by the charting library when width="content".
+        assert_vega_chart_width(chart, expected_width, chart_description, tolerance=3)
 
 
 def test_vega_chart_width_content_snapshot(
