9a8c18dc25092117fc5cc9bcb2d312c2ada3ee89
Thiago Teixeira <103002884+sfc-gh-tteixeira@users.noreply.github.com>
2025-09-22 09:52:37 -0700

    Update lib/streamlit/elements/layouts.py
    Co-authored-by: graphite-app[bot] <96075541+graphite-app[bot]@users.noreply.github.com>

--- lib/streamlit/elements/layouts.py
+++ lib/streamlit/elements/layouts.py
@@ -994,7 +994,7 @@ class LayoutsMixin:
         # Checks whether the entered button type is one of the allowed options
         if type not in ["primary", "secondary", "tertiary"]:
             raise StreamlitAPIException(
-                'The type argument to st.button must be "primary", "secondary", or "tertiary". '
+                'The type argument to st.popover must be "primary", "secondary", or "tertiary". '
                 f'\nThe argument passed was "{type}".'
             )
 
