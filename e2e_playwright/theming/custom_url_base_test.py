ee465c0dcbfb0a17360d444a610f9619834fcf0c
Maya Barnes <maya8617@gmail.com>
2025-09-23 18:06:49 -0700

    Update test to check font

--- e2e_playwright/theming/custom_url_base_test.py
+++ e2e_playwright/theming/custom_url_base_test.py
@@ -19,7 +19,7 @@ import pytest
 from playwright.sync_api import Page
 
 from e2e_playwright.conftest import ImageCompareFunction
-from e2e_playwright.shared.app_utils import expect_no_skeletons
+from e2e_playwright.shared.app_utils import expect_font, expect_no_skeletons
 
 # ruff: noqa: ERA001
 # The theme.base points to a toml file hosted on our streamlit s3 bucket
@@ -62,8 +62,11 @@ def test_custom_theme_with_url_base(app: Page, assert_snapshot: ImageCompareFunc
 
     # Make sure that all elements are rendered and no skeletons are shown:
     expect_no_skeletons(app, timeout=25000)
-    # Add some additional timeout to ensure that fonts can load without
-    # creating flakiness:
-    app.wait_for_timeout(10000)
+
+    # Verify that fonts from external TOML file have loaded:
+    expect_font(app, "Ojuju", style="normal")
+
+    # Verify that fonts from local config overrides have loaded:
+    expect_font(app, "Oswald", style="normal")
 
     assert_snapshot(app, name="custom_theme_with_url_base", image_threshold=0.0003)
