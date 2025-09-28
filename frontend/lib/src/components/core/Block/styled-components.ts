ccc47639c759b7c9e8ad383754d725ce048098c9
MacTaylor <hpcslag@users.noreply.github.com>
2025-09-28 13:03:26 +0800

    add minWith to argument type

--- frontend/lib/src/components/core/Block/styled-components.ts
+++ frontend/lib/src/components/core/Block/styled-components.ts
@@ -46,18 +46,28 @@ export interface StyledElementContainerProps {
   elementType: string
   overflow: React.CSSProperties["overflow"]
   flex?: React.CSSProperties["flex"]
+  minWidth?: React.CSSProperties["minWidth"]
 }
 
 const GLOBAL_ELEMENTS = ["balloons", "snow"]
 export const StyledElementContainer = styled.div<StyledElementContainerProps>(
-  ({ theme, isStale, width, height, elementType, overflow, flex }) => ({
+  ({
+    theme,
+    isStale,
     width,
+    height,
+    elementType,
+    overflow,
+    flex,
+    minWidth,
+  }) => ({
+    width: width,
     height,
     maxWidth: "100%",
     // Important so that individual elements don't take up too much space
     // in horizontal layouts. Particularly when an element uses the full screen wrapper.
     // Some components support zero width (e.g. iframe).
-    minWidth: width === "0px" ? 0 : "1rem",
+    minWidth: minWidth ? minWidth : width === "0px" ? 0 : "1rem",
     // Allows to have absolutely-positioned nodes inside app elements, like
     // floating buttons.
     position: "relative",
