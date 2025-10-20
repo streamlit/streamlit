406d68b46e870f1b18bc7aeab026a2dbb190c2c0
Ken McGrady <ken.mcgrady@gmail.com>
2025-10-20 07:39:21 -0700

    Migrate spinner to a Mixin

--- lib/streamlit/elements/spinner.py
+++ lib/streamlit/elements/spinner.py
@@ -16,9 +16,8 @@ from __future__ import annotations
 
 import contextlib
 import threading
-from typing import TYPE_CHECKING, Final
+from typing import TYPE_CHECKING, Final, cast
 
-import streamlit as st
 from streamlit.elements.lib.layout_utils import (
     LayoutConfig,
     Width,
@@ -29,110 +28,119 @@ from streamlit.runtime.scriptrunner import add_script_run_ctx
 if TYPE_CHECKING:
     from collections.abc import Iterator
 
+    from streamlit.delta_generator import DeltaGenerator
+
 # Set the message 0.5 seconds in the future to avoid annoying
 # flickering if this spinner runs too quickly.
 DELAY_SECS: Final = 0.5
 
 
-@contextlib.contextmanager
-def spinner(
-    text: str = "In progress...",
-    *,
-    show_time: bool = False,
-    _cache: bool = False,
-    width: Width = "content",
-) -> Iterator[None]:
-    """Display a loading spinner while executing a block of code.
+class SpinnerMixin:
+    @contextlib.contextmanager
+    def spinner(
+        self,
+        text: str = "In progress...",
+        *,
+        show_time: bool = False,
+        _cache: bool = False,
+        width: Width = "content",
+    ) -> Iterator[None]:
+        """Display a loading spinner while executing a block of code.
 
-    Parameters
-    ----------
-    text : str
-        The text to display next to the spinner. This defaults to
-        ``"In progress..."``.
+        Parameters
+        ----------
+        text : str
+            The text to display next to the spinner. This defaults to
+            ``"In progress..."``.
 
-        The text can optionally contain GitHub-flavored Markdown. Syntax
-        information can be found at: https://github.github.com/gfm.
+            The text can optionally contain GitHub-flavored Markdown. Syntax
+            information can be found at: https://github.github.com/gfm.
 
-        See the ``body`` parameter of |st.markdown|_ for additional, supported
-        Markdown directives.
+            See the ``body`` parameter of |st.markdown|_ for additional, supported
+            Markdown directives.
 
-        .. |st.markdown| replace:: ``st.markdown``
-        .. _st.markdown: https://docs.streamlit.io/develop/api-reference/text/st.markdown
+            .. |st.markdown| replace:: ``st.markdown``
+            .. _st.markdown: https://docs.streamlit.io/develop/api-reference/text/st.markdown
 
-    show_time : bool
-        Whether to show the elapsed time next to the spinner text. If this is
-        ``False`` (default), no time is displayed. If this is ``True``,
-        elapsed time is displayed with a precision of 0.1 seconds. The time
-        format is not configurable.
+        show_time : bool
+            Whether to show the elapsed time next to the spinner text. If this is
+            ``False`` (default), no time is displayed. If this is ``True``,
+            elapsed time is displayed with a precision of 0.1 seconds. The time
+            format is not configurable.
 
-    width : "content", "stretch", or int
-        The width of the spinner element. This can be one of the following:
+        width : "content", "stretch", or int
+            The width of the spinner element. This can be one of the following:
 
-        - ``"content"`` (default): The width of the element matches the
-          width of its content, but doesn't exceed the width of the parent
-          container.
-        - ``"stretch"``: The width of the element matches the width of the
-          parent container.
-        - An integer specifying the width in pixels: The element has a
-          fixed width. If the specified width is greater than the width of
-          the parent container, the width of the element matches the width
-          of the parent container.
+            - ``"content"`` (default): The width of the element matches the
+              width of its content, but doesn't exceed the width of the parent
+              container.
+            - ``"stretch"``: The width of the element matches the width of the
+              parent container.
+            - An integer specifying the width in pixels: The element has a
+              fixed width. If the specified width is greater than the width of
+              the parent container, the width of the element matches the width
+              of the parent container.
 
-    Example
-    -------
-    >>> import streamlit as st
-    >>> import time
-    >>>
-    >>> with st.spinner("Wait for it...", show_time=True):
-    >>>     time.sleep(5)
-    >>> st.success("Done!")
-    >>> st.button("Rerun")
+        Example
+        -------
+        >>> import streamlit as st
+        >>> import time
+        >>>
+        >>> with st.spinner("Wait for it...", show_time=True):
+        >>>     time.sleep(5)
+        >>> st.success("Done!")
+        >>> st.button("Rerun")
 
-    .. output ::
-        https://doc-spinner.streamlit.app/
-        height: 210px
+        .. output ::
+            https://doc-spinner.streamlit.app/
+            height: 210px
 
-    """
-    from streamlit.proto.Spinner_pb2 import Spinner as SpinnerProto
-    from streamlit.string_util import clean_text
+        """
+        from streamlit.proto.Spinner_pb2 import Spinner as SpinnerProto
+        from streamlit.string_util import clean_text
 
-    validate_width(width, allow_content=True)
-    layout_config = LayoutConfig(width=width)
+        validate_width(width, allow_content=True)
+        layout_config = LayoutConfig(width=width)
 
-    message = st.empty()
+        message = self.dg.empty()
 
-    display_message = True
-    display_message_lock = threading.Lock()
+        display_message = True
+        display_message_lock = threading.Lock()
 
-    try:
+        try:
 
-        def set_message() -> None:
-            with display_message_lock:
-                if display_message:
-                    spinner_proto = SpinnerProto()
-                    spinner_proto.text = clean_text(text)
-                    spinner_proto.cache = _cache
-                    spinner_proto.show_time = show_time
-                    message._enqueue(
-                        "spinner", spinner_proto, layout_config=layout_config
-                    )
+            def set_message() -> None:
+                with display_message_lock:
+                    if display_message:
+                        spinner_proto = SpinnerProto()
+                        spinner_proto.text = clean_text(text)
+                        spinner_proto.cache = _cache
+                        spinner_proto.show_time = show_time
+                        message._enqueue(
+                            "spinner", spinner_proto, layout_config=layout_config
+                        )
 
-        add_script_run_ctx(threading.Timer(DELAY_SECS, set_message)).start()
+            add_script_run_ctx(threading.Timer(DELAY_SECS, set_message)).start()
 
-        # Yield control back to the context.
-        yield
-    finally:
-        if display_message_lock:
-            with display_message_lock:
-                display_message = False
-            if "chat_message" in set(message._active_dg._ancestor_block_types):
-                # Temporary stale element fix:
-                # For chat messages, we are resetting the spinner placeholder to an
-                # empty container instead of an empty placeholder (st.empty) to have
-                # it removed from the delta path. Empty containers are ignored in the
-                # frontend since they are configured with allow_empty=False. This
-                # prevents issues with stale elements caused by the spinner being
-                # rendered only in some situations (e.g. for caching).
-                message.container()
-            else:
-                message.empty()
+            # Yield control back to the context.
+            yield
+        finally:
+            if display_message_lock:
+                with display_message_lock:
+                    display_message = False
+                if "chat_message" in set(message._active_dg._ancestor_block_types):
+                    # Temporary stale element fix:
+                    # For chat messages, we are resetting the spinner placeholder to an
+                    # empty container instead of an empty placeholder (st.empty) to have
+                    # it removed from the delta path. Empty containers are ignored in the
+                    # frontend since they are configured with allow_empty=False. This
+                    # prevents issues with stale elements caused by the spinner being
+                    # rendered only in some situations (e.g. for caching).
+                    message.container()
+                else:
+                    message.empty()
+
+    @property
+    def dg(self) -> DeltaGenerator:
+        """Get our DeltaGenerator."""
+        return cast("DeltaGenerator", self)
