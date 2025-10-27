9bb1049cf38481fcc42f67003632a09422a50fe1
kkchemboli <krishnakoundinyachemboli@Krishnas-MacBook-Pro.local>
2025-10-27 20:36:01 +0530

    Move test into data_editor_test.py and remove old multiselect_column_test.py

--- lib/tests/streamlit/elements/data_editor_test.py
+++ lib/tests/streamlit/elements/data_editor_test.py
@@ -219,12 +219,29 @@ class DataEditorUtilTest(unittest.TestCase):
                     datetime.datetime.now(),
                     datetime.datetime.now(),
                 ],
+                "col5": [
+                    ["x", "y"],
+                    ["z"],
+                    ["y"],
+                ],
             }
         )
 
         added_rows: list[dict[str, Any]] = [
-            {"col1": 10, "col2": "foo", "col3": False, "col4": "2020-03-20T14:28:23"},
-            {"col1": 11, "col2": "bar", "col3": True, "col4": "2023-03-20T14:28:23"},
+            {
+                "col1": 10,
+                "col2": "foo",
+                "col3": False,
+                "col4": "2020-03-20T14:28:23",
+                "col5": ["x", "y"],
+            },
+            {
+                "col1": 11,
+                "col2": "bar",
+                "col3": True,
+                "col4": "2023-03-20T14:28:23",
+                "col5": ["z"],
+            },
         ]
 
         _apply_row_additions(
@@ -232,6 +249,7 @@ class DataEditorUtilTest(unittest.TestCase):
         )
 
         assert len(df) == 5
+        assert df["col5"].tolist() == [["x", "y"], ["z"], ["y"], ["x", "y"], ["z"]]
 
     def test_apply_row_deletions(self):
         """Test applying row deletions to a DataFrame."""
