d75785e720ff264fa8cf0c337e122f6776a8b6cb
lukasmasuch <lukas.masuch@gmail.com>
2025-11-04 19:45:42 -0800

    Update

--- e2e_playwright/conftest.py
+++ e2e_playwright/conftest.py
@@ -826,7 +826,8 @@ def assert_snapshot(
 
             error_msg = (
                 f"Snapshot mismatch for {snapshot_file_name} ({mismatch} pixels difference;"
-                f" {mismatch / total_pixels * 100:.2f}%)"
+                f" {mismatch / total_pixels * 100:.2f}%). "
+                f"Find the diff, actual, and expected screenshots in {test_failures_dir}."
             )
 
             # Create new failures folder for this test:
@@ -860,7 +861,8 @@ def assert_snapshot(
                 f"Wrong size: expected={img_b.size}, actual={img_a.size} "
                 f"({pixel_diff} pixels difference; "
                 f"{pixel_diff / expected_pixels * 100:.2f}%). "
-                f"Error: {ex}"
+                f"Find the actual and expected screenshots in {test_failures_dir}. "
+                f"Error: {ex}. "
             )
 
         if is_last_rerun:
