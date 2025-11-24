e45ce51ee610cf349210fcadb3080cde02535641
lukasmasuch <lukas.masuch@gmail.com>
2025-11-24 11:28:45 +0100

    Update min pinned version

--- lib/setup.py
+++ lib/setup.py
@@ -108,7 +108,7 @@ EXTRA_REQUIRES = {
         # orjson speeds up large plotly figure processing by 5-10x:
         "orjson>=3.5.0",
         # uvloop speeds up the event loop:
-        "uvloop>=0.15.1; sys_platform != 'win32' and (sys_platform != 'cygwin' and platform_python_implementation != 'PyPy')",  # noqa: E501
+        "uvloop>=0.15.2; sys_platform != 'win32' and (sys_platform != 'cygwin' and platform_python_implementation != 'PyPy')",  # noqa: E501
     ],
     # Install all optional dependencies:
     "all": [
