db20a0aff50ee7fdb985c8a6f1963b18de8e4edc
lukasmasuch <lukas.masuch@gmail.com>
2025-12-03 12:10:18 +0100

    Revert

--- frontend/lib/src/components/shared/BaseButton/styled-components.ts
+++ frontend/lib/src/components/shared/BaseButton/styled-components.ts
@@ -79,7 +79,7 @@ function getSizeStyle(size: BaseButtonSize, theme: EmotionTheme): CSSObject {
       return {
         padding: `${theme.spacing.md} ${theme.spacing.md}`,
       }
-    case BaseButtonSize.MEDIUM:
+    default:
       return {
         padding: `${theme.spacing.xs} ${theme.spacing.md}`,
       }
