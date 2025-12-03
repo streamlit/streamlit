cee13fda92c8cac43f72ef99a4a900e32e0569de
Bob Nisco <bob.nisco@snowflake.com>
2025-12-03 13:57:34 -0800

    Tweak the exclude

--- frontend/vitest.config.ts
+++ frontend/vitest.config.ts
@@ -39,6 +39,7 @@ export default defineConfig({
     },
     exclude: [
       ...configDefaults.exclude,
+      "**/src/assets/**",
       "**/dist/**",
       "**/cypress/**",
       "**/.{idea,git,cache,output,temp}/**",
