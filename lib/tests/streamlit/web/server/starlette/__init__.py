0961d80ab847aee4db3d0db1ab1c165e1397d9e9
lukasmasuch <lukas.masuch@gmail.com>
2025-12-16 00:22:23 +0100

    Update

--- lib/tests/streamlit/web/server/starlette/__init__.py
+++ lib/tests/streamlit/web/server/starlette/__init__.py
@@ -11,11 +11,3 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-from streamlit.web.server.starlette.starlette_app import create_starlette_app
-from streamlit.web.server.starlette.starlette_server import StarletteServer
-
-__all__ = [
-    "StarletteServer",
-    "create_starlette_app",
-]
