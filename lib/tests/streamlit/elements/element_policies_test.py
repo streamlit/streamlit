fcaf032bf3716a98acdae0d5881733e06ba9815d
lukasmasuch <lukas.masuch@gmail.com>
2026-01-22 20:18:25 +0100

    Fix issue

--- lib/tests/streamlit/elements/element_policies_test.py
+++ lib/tests/streamlit/elements/element_policies_test.py
@@ -21,7 +21,7 @@ from unittest.mock import MagicMock, patch
 import pytest
 
 from streamlit import config
-from streamlit.elements.lib import utils
+from streamlit.elements.lib import policies
 from streamlit.elements.lib.policies import (
     check_cache_replay_rules,
     check_callback_rules,
@@ -163,8 +163,8 @@ class SpecialSessionStatesTest(ElementPoliciesTest):
         mock_session_state = MagicMock()
         mock_session_state.is_new_state_value.return_value = True
         patched_get_session_state.return_value = mock_session_state
-        # Reset globale flag:
-        utils._shown_default_value_warning = False
+        # Reset global flag:
+        policies._shown_default_value_warning = False
 
         check_session_state_rules(5, key=_KEY)
 
