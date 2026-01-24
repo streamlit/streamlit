060d5e74e4f0b3ff13f3c25891e4a0b29ac36e0f
Ken McGrady <ken.mcgrady@gmail.com>
2026-01-24 11:01:39 -0800

    Up Pandas Max Version to 4

--- lib/setup.py
+++ lib/setup.py
@@ -46,7 +46,7 @@ INSTALL_REQUIRES = [
     "packaging>=20",
     # Pandas <1.4 has a bug related to deleting columns in a DataFrame changing
     # the index dtype.
-    "pandas>=1.4.0, <3",
+    "pandas>=1.4.0, <4",
     "pillow>=7.1.0, <13",
     # `protoc` < 3.20 is not able to generate protobuf code compatible with protobuf >= 3.20.
     "protobuf>=3.20, <7",
