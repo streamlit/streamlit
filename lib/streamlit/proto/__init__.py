63395a9cad30130c5a464fbf3e80bb914cb24809
lukasmasuch <lukas.masuch@gmail.com>
2026-02-10 11:46:05 +0100

    Update

--- lib/streamlit/proto/__init__.py
+++ lib/streamlit/proto/__init__.py
@@ -11,3 +11,5 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+# (This comment is here so the file exists in Git)
