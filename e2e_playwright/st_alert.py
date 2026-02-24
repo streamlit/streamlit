bdc577552bf1e59762da0facb93288d9e9c49cf1
mayagbarnes <mayagbarnes@users.noreply.github.com>
2026-02-24 06:48:05 +0000

    Update emojis/material icons

--- e2e_playwright/st_alert.py
+++ e2e_playwright/st_alert.py
@@ -96,7 +96,7 @@ This is a big error message.
 ### LATEST MATERIAL ICON TEST START ###
 st.success(
     "Success message to test material icon from latest material symbols font",
-    icon=":material/mood_heart:",
+    icon=":material/horizontal_align_center:",
 )
 ### LATEST MATERIAL ICON TEST END ###
 
