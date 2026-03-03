47efa95736c486fe36e191e39e2e83468e861652
lukasmasuch <lukas.masuch@gmail.com>
2026-03-03 18:00:34 +0100

    Add warning log when clearing all cache_resource caches
    Log a warning when st.cache_resource.clear() is called to help track
    cache clearing operations.

--- lib/streamlit/runtime/caching/cache_resource_api.py
+++ lib/streamlit/runtime/caching/cache_resource_api.py
@@ -631,6 +631,7 @@ class CacheResourceAPI:
     @gather_metrics("clear_resource_caches")
     def clear(self) -> None:
         """Clear all cache_resource caches."""
+        _LOGGER.warning("Clearing all cache_resource caches")
         _resource_caches.clear_all()
 
 
