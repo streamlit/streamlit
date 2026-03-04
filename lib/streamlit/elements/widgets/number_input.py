555559df7cd2ca410796f733c8b4588af26418cc
lukasmasuch <lukas.masuch@gmail.com>
2026-03-04 22:18:35 +0100

    Remove unsupported thousand separators from number_input docs

--- lib/streamlit/elements/widgets/number_input.py
+++ lib/streamlit/elements/widgets/number_input.py
@@ -313,8 +313,7 @@ class NumberInputMixin:
             <https://github.com/alexei/sprintf.js?tab=readme-ov-file#format-specification>`_.
 
             For example, ``format="%0.1f"`` adjusts the displayed decimal
-            precision to only show one digit after the decimal. Use ``,`` for
-            thousand separators (e.g. ``"%,d"`` yields ``"1,234"``).
+            precision to only show one digit after the decimal.
 
         key : str, int, or None
             An optional string or integer to use as the unique key for
