johnmuthu08-hash/Streamlitapp

Fix file uploader JPG/JPEG display redundancy
- Modified FileDropzoneInstructions to deduplicate JPG/JPEG extensions
- When both JPG and JPEG are present, only show JPG in the UI
- Added test cases for the deduplication logic
- Resolves issue where both extensions were shown redundantly

Fixes #11991

frontend/lib/src/components/widgets/FileUploader/FileDropzoneInstructions.test.tsx
@@ -111,4 +111,32 @@ describe("FileDropzoneInstructions widget", () => {
 
     expect(screen.getByText("Limit 5KB per file")).toBeVisible()
   })
+
+  it("deduplicates JPG and JPEG extensions, showing only JPG", () => {
+    const props = getProps({
+      acceptedExtensions: [".jpg", ".jpeg", ".png"],
+    })
+    render(<FileDropzoneInstructions {...props} />)
+
+    expect(screen.getByText(/• JPG, PNG/)).toBeInTheDocument()
+    expect(screen.queryByText(/JPEG/)).not.toBeInTheDocument()
+  })
+
+  it("shows JPG when only JPEG is provided", () => {
+    const props = getProps({
+      acceptedExtensions: [".jpeg"],
+    })
+    render(<FileDropzoneInstructions {...props} />)
+
+    expect(screen.getByText(/• JPEG/)).toBeInTheDocument()
+  })
+
+  it("shows JPG when only JPG is provided", () => {
+    const props = getProps({
+      acceptedExtensions: [".jpg"],
+    })
+    render(<FileDropzoneInstructions {...props} />)
+
+    expect(screen.getByText(/• JPG/)).toBeInTheDocument()
+  })
 })

streamlit/streamlit

frontend/lib/src/components/widgets/FileUploader/FileDropzoneInstructions.test.tsx
@@ -143,5 +143,33 @@ describe("FileDropzoneInstructions widget", () => {
     expect(
       screen.getByText(/• image, application\/pdf, JSON/)
     ).toBeInTheDocument()
+  it("deduplicates JPG and JPEG extensions, showing only JPG", () => {
+    const props = getProps({
+      acceptedTypes: [".jpg", ".jpeg", ".png"],
+    })
+    render(<FileDropzoneInstructions {...props} />)
+
+    expect(screen.getByText(/• JPG, PNG/)).toBeInTheDocument()
+    expect(screen.queryByText(/JPEG/)).not.toBeInTheDocument()
+  })
+
+  it("shows JPEG when only JPEG is provided", () => {
+    const props = getProps({
+      acceptedTypes: [".jpeg"],
+    })
+    render(<FileDropzoneInstructions {...props} />)
+
+    expect(screen.getByText(/• JPEG/)).toBeInTheDocument()
+  })
+
+  it("shows JPG when only JPG is provided", () => {
+    const props = getProps({
+      acceptedTypes: [".jpg"],
+    })
+    render(<FileDropzoneInstructions {...props} />)
+
+    expect(screen.getByText(/• JPG/)).toBeInTheDocument()
+  })
+
   })
 })