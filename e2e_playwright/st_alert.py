4f6ba6ed207c85a968ca973b0264eb7f8643c661
mayagbarnes <mayagbarnes@users.noreply.github.com>
2026-03-10 06:46:12 +0000

    Update emojis/material icons

--- e2e_playwright/st_alert.py
+++ e2e_playwright/st_alert.py
@@ -96,7 +96,7 @@ This is a big error message.
 ### LATEST MATERIAL ICON TEST START ###
 st.success(
     "Success message to test material icon from latest material symbols font",
-    icon=":material/sql:",
+    icon=":material/snowflake:",
 )
 ### LATEST MATERIAL ICON TEST END ###
 
