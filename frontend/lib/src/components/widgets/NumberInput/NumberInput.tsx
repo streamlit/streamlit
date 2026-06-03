3567e612b1ba4869dd296cff9702e04c044f28c5
Maya Barnes <maya8617@gmail.com>
2026-06-02 21:03:24 -0700

    Styling fixes

--- frontend/lib/src/components/widgets/NumberInput/NumberInput.tsx
+++ frontend/lib/src/components/widgets/NumberInput/NumberInput.tsx
@@ -52,6 +52,7 @@ import { WidgetStateManager } from "~lib/WidgetStateManager"
 
 import {
   StyledClearButton,
+  StyledClearSvg,
   StyledInputContainer,
   StyledInputControl,
   StyledInputControls,
@@ -459,7 +460,19 @@ const NumberInput: React.FC<Props> = ({
               onMouseDown={e => e.preventDefault()}
               onClick={handleClear}
             >
-              <DynamicIcon iconValue=":material/close:" size="xs" />
+              {/* Inline SVG instead of a Material icon: ":material/cancel:"
+                always renders the outline variant instead of desired filled version */}
+              <StyledClearSvg
+                viewBox="0 0 24 24"
+                aria-hidden
+                focusable="false"
+              >
+                <path
+                  fillRule="evenodd"
+                  clipRule="evenodd"
+                  d="M12 20C16.4183 20 20 16.4183 20 12C20 7.58173 16.4183 4 12 4C7.58173 4 4 7.58173 4 12C4 16.4183 7.58173 20 12 20ZM10.0303 8.96967C9.73743 8.67679 9.26257 8.67679 8.96967 8.96967C8.67676 9.26257 8.67676 9.73743 8.96967 10.0303L10.9393 12L8.96967 13.9697C8.67676 14.2626 8.67676 14.7374 8.96967 15.0303C9.26257 15.3232 9.73743 15.3232 10.0303 15.0303L12 13.0607L13.9697 15.0303C14.2626 15.3232 14.7374 15.3232 15.0303 15.0303C15.3232 14.7374 15.3232 14.2626 15.0303 13.9697L13.0607 12L15.0303 10.0303C15.3232 9.73743 15.3232 9.26257 15.0303 8.96967C14.7374 8.67679 14.2626 8.67679 13.9697 8.96967L12 10.9393L10.0303 8.96967Z"
+                />
+              </StyledClearSvg>
             </StyledClearButton>
           )}
           {/* Show the increment/decrement controls only when there is sufficient room. */}
